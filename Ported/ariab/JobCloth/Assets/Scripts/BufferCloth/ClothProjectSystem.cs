04b995966d26a90f107a248f3661e61851d3d075
Aria Bonczek <aria.bonczek@gmail.com>
2019-06-13 14:43:34 +0200

    Updated projection system to use the WorldToLocal of the original GameObject to correctly process gravity for rotated cloth meshes

--- Ported/ariab/JobCloth/Assets/Scripts/BufferCloth/ClothProjectSystem.cs
+++ Ported/ariab/JobCloth/Assets/Scripts/BufferCloth/ClothProjectSystem.cs
@@ -1,23 +1,22 @@
 ﻿using Unity.Burst;
-using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
-using UnityEngine;
 
 [UpdateInGroup(typeof(SimulationSystemGroup))]
 [UpdateAfter(typeof(ClothTimestepSystem))]
 public class ClothProjectSystem : JobComponentSystem
 {
     [BurstCompile]
-    struct ProjectClothPositionsJob : IJobForEach_BBBBC<ClothProjectedPosition, ClothCurrentPosition, ClothPreviousPosition, ClothPinWeight, ClothTimestepData>
+    struct ProjectClothPositionsJob : IJobForEach_BBBBCC<ClothProjectedPosition, ClothCurrentPosition, ClothPreviousPosition, ClothPinWeight, ClothTimestepData, ClothWorldToLocal>
     {
         public void Execute(
             DynamicBuffer<ClothProjectedPosition> projectedPositions, 
             DynamicBuffer<ClothCurrentPosition> currentPositions, 
             DynamicBuffer<ClothPreviousPosition> previousPositions, 
             DynamicBuffer<ClothPinWeight> pinWeights, 
-            ref ClothTimestepData timestepData)
+            ref ClothTimestepData timestepData,
+            ref ClothWorldToLocal worldToLocal)
         {
             var fixedStepSq = timestepData.FixedTimestep * timestepData.FixedTimestep;
             
@@ -27,7 +26,7 @@ public class ClothProjectSystem : JobComponentSystem
                 var currentPosition = currentPositions[vertexIndex].Value;
                 
                 var velocity = currentPosition - previousPositions[vertexIndex].Value;
-                var gravity = new float3(0.0f, -9.8f, 0.0f) * fixedStepSq;
+                var gravity = math.mul(worldToLocal.Value, new float4(0.0f, -9.8f, 0.0f, 0.0f)).xyz * fixedStepSq;
                 velocity += gravity;
 
                 var newProjected = currentPosition + velocity * pinWeights[vertexIndex].InvPinWeight;
@@ -39,6 +38,7 @@ public class ClothProjectSystem : JobComponentSystem
     protected override JobHandle OnUpdate(JobHandle inputDependencies)
     {
         var job = new ProjectClothPositionsJob();
+        
         return job.Schedule(this, inputDependencies);
     }
 }
