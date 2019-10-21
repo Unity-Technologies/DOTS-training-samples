f11ab9d816762bf42fda4f2abb5945721a2bbb09
Aude Giraud <aude.giraud@unity3d.com>
2019-10-21 14:14:04 -0400

    [thrower-dots] First commit.

---
+++ Ported/agiraud/ThrowerArms/Assets/Scripts/CameraOrbit.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour {
+	public float zoomDuration;
+	public float minCamDist;
+	public float maxCamDist;
+	public float swayXDuration;
+	public float swayXAmount;
+	public float spinDuration;
+	public float spinAmount;
+	public float swayTiltDuration;
+	public float tiltAmount;
+	public float tiltOffset;
+	public Vector3 posOffset;
+
+	float zoomTimer = 0f;
+	float swayXTimer = 0f;
+	float swayTiltTimer = 0f;
+	float spinTimer = 0f;
+
+	Transform cam;
+
+	void Start () {
+		cam = GetComponentInChildren<Camera>().transform;
+	}
+	
+	void Update () {
+		zoomTimer += Time.deltaTime / zoomDuration;
+		swayXTimer += Time.deltaTime / swayXDuration;
+		swayTiltTimer += Time.deltaTime / swayTiltDuration;
+		spinTimer += Time.deltaTime / spinDuration;
+
+		transform.eulerAngles = new Vector3(Mathf.Sin(swayTiltTimer*2f*Mathf.PI)*tiltAmount+tiltOffset,
+											Mathf.Sin(spinTimer*2f*Mathf.PI)*spinAmount,
+											0f);
+
+		float x = .5f - Mathf.Cos(swayXTimer * 2f * Mathf.PI) * .5f;
+		transform.position = new Vector3(ArmManager.armRowWidth*x,0f,0f)+posOffset;
+
+		float zoomT = .5f-Mathf.Cos(zoomTimer*2f*Mathf.PI)*.5f;
+		cam.localPosition = new Vector3(0f,0f,-Mathf.Lerp(minCamDist,maxCamDist,zoomT));
+	}
+}
---
+++ Ported/agiraud/ThrowerArms/Assets/Scripts/FABRIK.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// FABRIK: Forward-And-Backward-Reaching Inverse Kinematics
+// "Each tick:  Drag the chain (from the end) to the target point,
+//  then drag the chain (from the root) back to the anchor point"
+public static class FABRIK {
+	
+	public static void Solve(Vector3[] chain, float boneLength, Vector3 anchor, Vector3 target, Vector3 bendHint) {
+		chain[chain.Length - 1] = target;
+		for (int i=chain.Length-2;i>=0;i--) {
+			chain[i] += bendHint;
+			Vector3 delta = chain[i] - chain[i + 1];
+			chain[i] = chain[i + 1] + delta.normalized * boneLength;
+		}
+
+		chain[0] = anchor;
+		for (int i = 1; i<chain.Length; i++) {
+			Vector3 delta = chain[i] - chain[i - 1];
+			chain[i] = chain[i - 1] + delta.normalized * boneLength;
+		}
+	}
+}
