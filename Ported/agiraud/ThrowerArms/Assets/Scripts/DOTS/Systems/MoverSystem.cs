ba33b46190d4091ab098402a668db1747035db08
Fabien Houlmann <fabien@unity3d.com>
2019-10-22 10:31:44 -0400

    add MoverSystem

---
+++ Ported/agiraud/ThrowerArms/Assets/Scripts/DOTS/Systems/MoverSystem.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using UnityEngine;
+
+//public class MoverSystem : ComponentSystem
+//{
+//    protected override void OnUpdate()
+//    {
+//        Entities.WithAll<Mover>().WithAll<Translation>().ForEach((Entity e, ref Mover mover, ref Translation translation) =>
+//        {
+//            translation.Value += mover.velocity * Time.deltaTime;
+//        });
+//    }
+//}
+
+
+public class MoverSystem : JobComponentSystem
+{
+    [BurstCompile]
+    struct MoverSystemJob : IJobForEach<Mover, Translation>
+    {
+        public float deltaTime;
+
+        public void Execute([ReadOnly] ref Mover mover, ref Translation translation)
+        {
+            translation.Value += mover.velocity * deltaTime;
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var job = new MoverSystemJob();
+        job.deltaTime = Time.deltaTime;
+        return job.Schedule(this, inputDeps);
+    }
+}
