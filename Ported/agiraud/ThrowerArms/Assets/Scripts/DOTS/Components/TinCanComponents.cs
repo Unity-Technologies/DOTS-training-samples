8aed17d7f9ee8a26396bd9f7f5701174f62f9908
sebastienc <sebastienc@unity3d.com>
2019-10-22 11:43:34 -0400

    [thrower-dots] Adding Tincan/Rock tags + removing unused states.

--- Ported/agiraud/ThrowerArms/Assets/Scripts/DOTS/Components/TinCanComponents.cs
+++ Ported/agiraud/ThrowerArms/Assets/Scripts/DOTS/Components/TinCanComponents.cs
@@ -1,19 +1,7 @@
 ﻿using Unity.Entities;
 
-public enum TinCanState {
-    Reset,
-    Sitting,
-    Falling
-}
-
 public struct IsReserved : IComponentData
 {
     public bool Value;
 }
-
-public struct TinCanStatus : IComponentData
-{
-    public TinCanState Value;
-}
-
 
