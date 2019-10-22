e68c0ac956a98e07a0f4f2872f10e1b41d696381
Sébastien L'Heureux <sebastienlh@unity3d.com>
2019-10-22 11:22:53 -0400

    Conversion stub

---
+++ Ported/Metro-DOTS/Assets/src/Conversion/LevelConversion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+public class LevelConversion : GameObjectConversionSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach<Metro>(Convert);
+    }
+
+    void Convert(Entity entity, Metro metroComponent)
+    {
+        Debug.Log("Start Metro Conversion");
+        GeneratePathfindingData(entity, metroComponent);
+        GenerateTrainTracksBezierData(entity, metroComponent);
+    }
+
+    void GeneratePathfindingData(Entity entity, Metro metroComponent)
+    {
+        Debug.Log("GeneratePathfindingData");
+    }
+
+    void GenerateTrainTracksBezierData(Entity entity, Metro metroComponent)
+    {
+        Debug.Log("GenerateTrainTracksBezierData");
+    }
+}
