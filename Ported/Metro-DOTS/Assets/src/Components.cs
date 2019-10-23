35f88859fa9017dce6c2a9a10b2e68ee5e5bed06
Julien Blais <julienb@unity3d.com>
2019-10-23 11:29:56 -0400

    Added components

---
+++ Ported/Metro-DOTS/Assets/src/Components.cs
@@ -0,0 +1,40 @@
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct PlatformId : IComponentData
+{
+    public uint value;
+}
+
+public struct CurrentPath : IComponentData
+{
+    public int index;
+    public PathLookup path;
+}
+
+public struct Direction : IComponentData
+{
+    public float3 value;
+}
+
+public struct Speed : IComponentData
+{
+    public float value;
+}
+
+// TRAIN STATES
+public struct EN_ROUTE : IComponentData { }
+public struct ARRIVING : IComponentData { }
+public struct DOORS_OPEN : IComponentData { }
+public struct UNLOADING : IComponentData { }
+public struct LOADING : IComponentData { }
+public struct DOORS_CLOSE : IComponentData { }
+public struct DEPARTING : IComponentData { }
+public struct EMERGENCY_STOP : IComponentData { }
+
+// COMMUTER STATES
+public struct WALK : IComponentData { }
+public struct QUEUE : IComponentData { }
+public struct GET_ON_TRAIN : IComponentData { }
+public struct WAIT_FOR_STOP : IComponentData { }
+public struct GET_OFF_TRAIN : IComponentData { }
