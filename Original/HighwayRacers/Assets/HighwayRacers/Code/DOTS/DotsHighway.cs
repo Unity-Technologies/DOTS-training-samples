dfb4392c2db9776e93ff08df923d4a51ea39e445
Greg Labute <gregoryl@unity3d.com>
2019-10-23 12:51:26 -0400

    Added SetCarTransformSystem

--- Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/DotsHighway.cs
+++ Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/DotsHighway.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections;
+﻿using Unity.Collections;
 using Unity.Mathematics;
-using UnityEngine;
 
 namespace HighwayRacers
 {
---
+++ Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/SetCarTransformSystem.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace HighwayRacers
+{
+    [UpdateAfter(typeof(ColorSystem))]
+    public class SetCarTransformSystem : JobComponentSystem
+    {
+        struct SetTransformJob : IJobForEach<CarState, Translation, Rotation>
+        {
+            public DotsHighway DotsHighway;
+
+            public void Execute(
+                [ReadOnly] ref CarState state,
+                ref Translation translation,
+                ref Rotation rotation)
+            {
+                DotsHighway.GetWorldPosition(
+                    state.PositionOnTrack, state.Lane, out float3 pos, out quaternion q);
+                translation.Value = pos;
+                rotation.Value = q;
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            var job = new SetTransformJob
+            {
+                DotsHighway = Highway.instance.DotsHighway
+            };
+            return job.Schedule(this, inputDeps);
+        }
+    }
+}
