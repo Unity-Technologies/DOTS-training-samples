1784579f05418a5d4133d83d11437904b34c4419
Sébastien L'Heureux <sebastienlh@unity3d.com>
2019-10-24 11:37:21 -0400

    Cleaning up when destination is reached

--- Ported/Metro-DOTS/Assets/src/Systems/Motion/DestinationSystem.cs
+++ Ported/Metro-DOTS/Assets/src/Systems/Motion/DestinationSystem.cs
@@ -1,5 +1,3 @@
-using Unity.Burst;
-using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
@@ -7,21 +5,35 @@ using Unity.Transforms;
 
 class DestinationSystem: JobComponentSystem
 {
-    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    EntityCommandBufferSystem m_ECBSystem;
+
+    protected override void OnCreate()
     {
-        return new TestDestinationJob().Schedule(this, inputDeps);
+        m_ECBSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
     }
 
-    [BurstCompile(FloatMode = FloatMode.Fast)]
-    struct TestDestinationJob : IJobForEach<TargetPosition, Translation, Speed>
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        public void Execute([ReadOnly] ref TargetPosition target, ref Translation translation, ref Speed speed)
+        var handle = new TestDestinationJob
+        {
+            commandBuffer = m_ECBSystem.CreateCommandBuffer().ToConcurrent()
+        }.Schedule(this, inputDeps);
+        m_ECBSystem.AddJobHandleForProducer(handle);
+        return handle;
+    }
+
+    struct TestDestinationJob : IJobForEachWithEntity<TargetPosition, Translation, Speed>
+    {
+        public EntityCommandBuffer.Concurrent commandBuffer;
+        public void Execute(Entity entity, int jobIndex,
+            ref TargetPosition target, ref Translation translation, ref Speed speed)
         {
             var distance = math.length(target.value - translation.Value);
             if (distance < 0.1f)
             {
                 speed.value = 0.0f;
                 translation.Value = target.value;
+                commandBuffer.RemoveComponent<TargetPosition>(jobIndex, entity);
             }
         }
     }
