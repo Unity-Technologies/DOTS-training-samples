e3801c8f518991ec9461c4a13241f67580e2b306
Aude Giraud <aude.giraud@unity3d.com>
2019-10-24 10:38:52 -0400

    [thrower-dots] Fixed thumb offset.

--- Ported/agiraud/ThrowerArms/Assets/Scripts/DOTS/Systems/ThumbIKSolver.cs
+++ Ported/agiraud/ThrowerArms/Assets/Scripts/DOTS/Systems/ThumbIKSolver.cs
@@ -15,29 +15,45 @@ public class ThumbIKSolver : JobComponentSystem
     private const float thumbXOffset = -0.05f;
 
     [BurstCompile]
-    struct ArmIKSolverJob : IJobForEachWithEntity_EBCC<BoneJoint, ArmTarget, HandAxis>
+    struct ThumbIKSolverJob : IJobForEachWithEntity_EBCC<BoneJoint, ArmTarget, HandAxis>
     {
+        public float Time;
+        
         public void Execute(Entity entity, int index, DynamicBuffer<BoneJoint> boneJoints,
             [ReadOnly] ref ArmTarget armTarget, [ReadOnly] ref HandAxis handAxis)
         {
+            // Arm + 4 fingers.
+            var bufferOffset = 3 + 4 * 4 - 1;
+            
             NativeArray<float3> chainPositions = new NativeArray<float3>(4, Allocator.Temp);
             for (int i = 0; i < 4; i++)
             {
-                chainPositions[i] = boneJoints[i + 2 + 4*3].JointPos;
+                chainPositions[i] = boneJoints[i + bufferOffset].JointPos;
             }
 
-            var thumbPos = boneJoints[2].JointPos+handAxis.Right*thumbXOffset;
-            var thumbTarget = thumbPos - handAxis.Right * .15f + handAxis.Forward - handAxis.Up*.1f;
+            float fingerGrabT = 1.0f;
+//            if (heldRock!=null) {
+//                // move our held rock to match our new hand position
+//                heldRock.position = handMatrix.MultiplyPoint3x4(heldRockOffset);
+//                lastIntendedRockPos = heldRock.position;
+//
+//                // if we're holding a rock, we're always gripping
+//                fingerGrabT = 1f;
+//            }
+            
+            var thumbPos = boneJoints[2].JointPos + handAxis.Right * thumbXOffset;
+            var thumbTarget = thumbPos - handAxis.Right * .15f + handAxis.Forward * (.2f+.1f*fingerGrabT) - handAxis.Up*.1f;
+            thumbTarget += handAxis.Right * Mathf.Sin(Time*3f + .5f) * .1f*(1f-fingerGrabT);
             
             // thumb bends away from the palm, instead of "upward" like the fingers
-            Vector3 thumbBendHint = (-handAxis.Right - handAxis.Forward * .5f);
+            Vector3 thumbBendHint = -handAxis.Right - handAxis.Forward * .5f;
             
             ConstantManager.IKSolve(ref chainPositions, thumbBoneLength,thumbPos, thumbTarget,
                 thumbBendHint*thumbBendStrength);
             
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 4; i++)
             {
-                boneJoints[i + 2 + 4*3] = new BoneJoint
+                boneJoints[i + bufferOffset] = new BoneJoint
                 {
                     JointPos = chainPositions[i]
                 };
@@ -48,6 +64,10 @@ public class ThumbIKSolver : JobComponentSystem
     }
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        return new ArmIKSolverJob().Schedule(this, inputDeps);
+        var job = new ThumbIKSolverJob
+        {
+            Time = UnityEngine.Time.time // + timeOffset
+        };
+        return job.Schedule(this, inputDeps);
     }
 }
