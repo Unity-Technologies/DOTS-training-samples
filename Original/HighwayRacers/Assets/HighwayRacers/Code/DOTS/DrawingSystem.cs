254ea523028096ff850ca029b80fb714ec3265f3
Neil Devine <neil@unity3d.com>
2019-10-24 10:36:19 -0400

    can draw more than 1023

--- Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/DrawingSystem.cs
+++ Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/DrawingSystem.cs
@@ -18,6 +18,9 @@ public class DrawingSystem : ComponentSystem
         m_Query = GetEntityQuery(ComponentType.ReadOnly<LocalToWorld>(), ComponentType.ReadOnly<ColorComponent>());
     }
 
+    Matrix4x4[] matrices = new Matrix4x4[1023];
+    Vector4[] colorProperties = new Vector4[1023];
+
     protected override void OnUpdate()
     {
         mat = Game.instance.entityMaterial;
@@ -25,19 +28,40 @@ public class DrawingSystem : ComponentSystem
 
 
         var positions = m_Query.ToComponentDataArray<LocalToWorld>(Allocator.TempJob);
-        var matrices = new Matrix4x4[positions.Length];
         var colors = m_Query.ToComponentDataArray<ColorComponent>(Allocator.TempJob);
         var properties = new MaterialPropertyBlock();
-        var colorProperties = new Vector4[colors.Length];
 
         Assert.IsTrue(colors.Length == positions.Length);
-        for (var i = 0; i != positions.Length; i++)
+
+        var numCars = positions.Length;
+        var numBatches = numCars / 1023;
+
+        for (int batch = 0; batch < numBatches; batch++)
         {
-            matrices[i] = positions[i].Value;
-            colorProperties[i] = colors[i].Value;
+            for (var i = 0; i < 1023; i++)
+            {
+                var index = batch * 1023 + i;
+                matrices[i] = positions[index].Value;
+                colorProperties[i] = colors[index].Value;
+            }
+
+            properties.SetVectorArray("_Color",colorProperties);
+            Graphics.DrawMeshInstanced(mesh,0,mat,matrices,1023,properties);
         }
-        properties.SetVectorArray("_Color",colorProperties);
-        Graphics.DrawMeshInstanced(mesh,0,mat,matrices,matrices.Length,properties);
+
+        var rest = numCars % 1023;
+        if (rest > 0 )
+        {
+            for (var i = 0; i < rest; i++)
+            {
+                var index = numBatches * 1023 + i;
+                matrices[i] = positions[index].Value;
+                colorProperties[i] = colors[index].Value;
+            }
+
+            properties.SetVectorArray("_Color", colorProperties);
+            Graphics.DrawMeshInstanced(mesh, 0, mat, matrices, rest, properties);
+        }
 
         positions.Dispose();
         colors.Dispose();
