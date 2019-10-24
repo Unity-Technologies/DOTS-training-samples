860f95ebc49631fe449d37dbf72503f6dac8b4c4
Marie Fetiveau <marief@unity3d.com>
2019-10-24 10:08:11 -0400

    [thrower-dots] Slightly better fingers (fixed index + param addition)

--- Ported/agiraud/ThrowerArms/Assets/Scripts/DOTS/Systems/FingerIKSolver.cs
+++ Ported/agiraud/ThrowerArms/Assets/Scripts/DOTS/Systems/FingerIKSolver.cs
@@ -15,6 +15,10 @@ public class FingerIKSolver : JobComponentSystem
     
     private const float fingerXOffset = -0.12f;
     private const float fingerSpacing = 0.08f;
+    // Hardcoded count / offset
+    private const int fingerCount = 4;
+    private const int fingerJointCount = 4;
+    private const int armOffset = 2;
 
     [BurstCompile]
     struct ArmIKSolverJob : IJobForEachWithEntity_EBCC<BoneJoint, ArmTarget, HandAxis>
@@ -22,29 +26,40 @@ public class FingerIKSolver : JobComponentSystem
         public void Execute(Entity entity, int index, DynamicBuffer<BoneJoint> boneJoints,
             [ReadOnly] ref ArmTarget armTarget, [ReadOnly] ref HandAxis handAxis)
         {
-
-            for (var j = 0; j < 4; j++)
+            for (var j = 0; j < fingerCount; j++)
             {
-                NativeArray<float3> chainPositions = new NativeArray<float3>(4, Allocator.Temp);
-                for (var i = 0; i < 4; i++)
+                NativeArray<float3> chainPositions = new NativeArray<float3>(fingerJointCount, Allocator.Temp);
+                for (var i = 0; i < fingerJointCount; i++)
                 {
-                    chainPositions[i] = boneJoints[i + j * 4 + 2].JointPos;
+                    chainPositions[i] = boneJoints[i + j * fingerCount + armOffset].JointPos;
                 }
                 
                 // find knuckle position for this finger
-                var fingerPos = boneJoints[2].JointPos + handAxis.Right * (fingerXOffset + j * fingerSpacing);
+                var fingerPos = boneJoints[armOffset].JointPos + handAxis.Right * (fingerXOffset + j * fingerSpacing);
 
                 // find resting position for this fingertip
-                var fingerTarget = fingerPos; // + handFwd * (.5f-.1f*fingerGrabT);
+                var reachTimer = 0f; // TODO add timer stuff
+                // FYI if we're holding a rock, we're always gripping
+                // fingerGrabT = 1 -> Gripping position
+                var grabT = reachTimer;
+                grabT = 3f * grabT * grabT - 2f * grabT * grabT * grabT;
+                var fingerGrabT = grabT;
 
-                fingerTarget += handAxis.Up * .3f + handAxis.Forward * .1f + handAxis.Right * (j - 1.5f) * .1f;
-                
+                var fingerTarget = fingerPos + handAxis.Forward * (.5f-.1f*fingerGrabT);
+
+                // TODO: add spooky fingers
+                // fingerTarget += handUp * Mathf.Sin((time + i*.2f)*3f) * .2f*(1f-fingerGrabT);
+
+                // apply finger-spreading during throw animation
+                float openPalm = 0f; // TODO throwCurve.Evaluate(throwTimer);
+                fingerTarget += (handAxis.Up * .3f + handAxis.Forward * .1f + handAxis.Right * (j - 1.5f) * .1f) * openPalm ;
+
                 ConstantManager.IKSolve(ref chainPositions, fingerBoneLength,fingerPos, fingerTarget,
                     handAxis.Up*fingerBendStrength);
             
-                for (int i = 0; i < 4; i++)
+                for (int i = 0; i < fingerJointCount; i++)
                 {
-                    boneJoints[i + j * 4 + 3] = new BoneJoint
+                    boneJoints[i + j * fingerCount + armOffset] = new BoneJoint
                     {
                         JointPos = chainPositions[i]
                     };
