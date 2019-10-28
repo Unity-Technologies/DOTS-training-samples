50f136738a96a7cf8a121a1a03eb7d3858bd9ce3
Lewey Geselowitz <lewey.geselowitz@unity3d.com>
2019-10-28 14:20:22 -0700

    Highway racer stuff.

---
+++ Ported/gregoryl/HighwayRacers/Assets/Code/DOTS/EntityDebugDrawSystem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HighwayRacers
+{
+    [UpdateInGroup(typeof(LateSimulationSystemGroup))]
+    public class EntityDebugDrawSystem : Unity.Entities.JobComponentSystem
+    {
+        private List<Vector3> Lines = new List<Vector3>();
+        private EntityQuery QueryToDraw = null;
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            //(new DebugDrawJob()).Schedule(this);
+            //return inputDeps; 
+
+            var _isDoDraw = true;
+            if (_isDoDraw) {
+                if (QueryToDraw == null)
+                {
+                    this.QueryToDraw = this.World.EntityManager.CreateEntityQuery(typeof(LocalToWorld));
+                    //LocalToWorld range = new LocalToWorld() { Value = Matrix4x4.identity; };
+                    //this.QueryToDraw.SetFilter<LocalToWorld>(RangeInt)
+                }
+                var data = this.QueryToDraw.ToComponentDataArray<LocalToWorld>(Allocator.TempJob);
+                //Debug.Log("Data.Length=" + data.Length);
+                for (int i=0; i<data.Length; i++)
+                {
+                    var ob = data[i];
+                    Debug.DrawLine(ob.Position, ob.Position + ((ob.Forward + ob.Up) * 10.0f) );
+                }
+                Debug.DrawLine(Vector3.zero, Vector3.one, Color.red);
+                data.Dispose();
+            }
+
+            return inputDeps;
+        }
+
+        public struct DebugDrawJob : IJobForEach<LocalToWorld>
+        {
+            public void Execute([ReadOnly] ref LocalToWorld ob)
+            {
+                Debug.DrawLine(ob.Position, ob.Position + ((ob.Forward + ob.Up) * 10.0f));
+            }
+        }
+    }
+
+}
---
+++ Ported/gregoryl/HighwayRacers/Assets/Code/DOTS/HybridTestBehav.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class HybridTestBehav : MonoBehaviour
+{
+    private EntityQuery QueryToDraw;
+    public Entity DebugThis;
+    public Object SelectionObj;
+    public System.Type SelectionType;
+    [Multiline]
+    public string SelectionTypeName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEditor.Selection.selectionChanged += (() => this.DebugCurrentSelection());
+    }
+
+    [ContextMenu("Debug Current Item")]
+    public void DebugCurrentSelection()
+    {
+        var ob = UnityEditor.Selection.activeObject;
+        var esp = (ob as Unity.Entities.Editor.EntitySelectionProxy);
+        if (esp)
+        {
+            this.DebugThis = esp.Entity;
+            this.UpdateEntityInfo();
+        }
+    }
+
+    public void UpdateEntityInfo()
+    {
+        this.SelectionTypeName = this.DebugThis.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Debug.DrawLine(Vector3.zero, Vector3.one);
+
+        var world = Unity.Entities.World.Active;
+        if (this.QueryToDraw == null)
+        {
+            this.QueryToDraw = world.EntityManager.CreateEntityQuery(typeof(Unity.Transforms.LocalToWorld));
+        }
+        var eids = this.QueryToDraw.ToEntityArray(Unity.Collections.Allocator.TempJob);
+        this.DebugThis = eids[0];
+        eids.Dispose();
+
+        //Unity.Properties.PropertyContainer
+        //var sel = (new Unity.Entities.Editor.EntitySelectionProxy());
+        //sel.Entity = this.DebugThis;
+        //Unity.Entities.Editor.EntitySelectionProxy
+
+        //UnityEditor.Selection.activeObject = this.DebugThis;
+
+        if (false)
+        {
+            var data = this.QueryToDraw.ToComponentDataArray<Unity.Transforms.LocalToWorld>(Unity.Collections.Allocator.TempJob);
+            Debug.Log("Data.Length=" + data.Length);
+            for (int i = 0; i < data.Length; i++)
+            {
+                var ob = data[i];
+                Debug.DrawLine(ob.Position, ob.Position + (ob.Forward * 10.0f), Color.green);
+            }
+            Debug.DrawLine(Vector3.zero, Vector3.one, Color.red);
+            data.Dispose();
+        }
+
+    }
+}
