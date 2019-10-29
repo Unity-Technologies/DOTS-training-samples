96cc18e97ab0ef809a8efec2c4acdec4e94f2075
Ryan Jones <ryanjo@RYANJONES-MBP.local>
2019-10-29 16:30:59 -0700

    Added ArmComponent for tracking arm and hand positions.

---
+++ Ported/joe-jia-ryan/Assets/ECS/Components/ArmComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+public struct ArmComponent : IComponentData
+{
+    public float3 armPosition;
+    public float3 handPosition;
+    
+    //TODO IK bone weights, finger sizes, etc.
+}
