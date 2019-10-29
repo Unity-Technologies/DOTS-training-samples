7212e927786011590fe47fb0cc6dfac064a289eb
Vincent Chevalier <vincent.chevalier@unity3d.com>
2019-10-29 10:06:25 -0400

    Copied to ports
    I copied the whole farmers project to ports so we always have the original project to use as a reference

---
+++ Ported/vcvcwa/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour {
+
+	public Vector2 viewAngles;
+	public float viewDist;
+	public float mouseSensitivity;
+
+	void Start () {
+		transform.rotation = Quaternion.Euler(viewAngles.y,viewAngles.x,0f);
+	}
+	
+	void LateUpdate () {
+		Vector3 pos = FarmerManager.instance.firstFarmer.GetSmoothWorldPos();
+		viewAngles.x += Input.GetAxis("Mouse X") * mouseSensitivity/Screen.height;
+		viewAngles.y -= Input.GetAxis("Mouse Y") * mouseSensitivity/Screen.height;
+		viewAngles.y = Mathf.Clamp(viewAngles.y,7f,80f);
+		viewAngles.x -= Mathf.Floor(viewAngles.x / 360f) * 360f;
+		transform.rotation = Quaternion.Euler(viewAngles.y,viewAngles.x,0f);
+		transform.position = pos - transform.forward * viewDist;
+	}
+}
