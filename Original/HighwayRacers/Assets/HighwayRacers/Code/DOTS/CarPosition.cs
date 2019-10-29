4d5b38ddfe9c884f467fc57c14f25c3553dbb504
Yao Xiaoling <yao@unity3d.com>
2019-10-29 10:59:05 -0700

    CarSystem skeleton.

--- Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/CarPosition.cs
+++ Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/CarPosition.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 
 [Serializable]
 public struct CarPosition : IComponentData
@@ -12,4 +14,43 @@ public struct CarPosition : IComponentData
 public unsafe struct CarPositionStaticProperties : IComponentData
 {
     public fixed float MergeSpace[4];
+}
+
+public class CarPositionSystem : JobComponentSystem
+{
+    public NativeArray<Entity> SortedCars;
+
+    private EntityQuery m_AllCars;
+
+    private struct SortCarPositionJob : IJob
+    {
+        public NativeArray<CarPosition> CarPositions;
+        public NativeList<CarPosition> Lane1;
+        public NativeList<CarPosition> Lane2;
+        public NativeList<CarPosition> Lane3;
+        public NativeList<CarPosition> Lane4;
+
+        public void Execute()
+        {
+        }
+    }
+
+    protected override void OnCreate()
+    {
+        m_AllCars = GetEntityQuery(typeof(CarPosition));
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var allCars = m_AllCars.ToComponentDataArray<CarPosition>(Allocator.TempJob);
+
+        return new SortCarPositionJob()
+        {
+            CarPositions = allCars,
+            Lane1 = new NativeList<CarPosition>(Allocator.TempJob),
+            Lane2 = new NativeList<CarPosition>(Allocator.TempJob),
+            Lane3 = new NativeList<CarPosition>(Allocator.TempJob),
+            Lane4 = new NativeList<CarPosition>(Allocator.TempJob),
+        }.Schedule(inputDeps);
+    }
 }
---
+++ Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/CarSystem.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+public class CarSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        // 1. Put cars {Entity, CarPosition} into different lanes
+        // 2. Sort the list of cars for each lane => {Entity, index}
+        // 3. Car logic {Entity, index, velocity, state }
+        // 4. Compose the matrices for each mesh instance for correct rendering.
+        return inputDeps;
+    }
+}
