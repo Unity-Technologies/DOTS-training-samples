297913d850b8b19f3f01389c185f7158b5c2d379
Yao Xiaoling <yao@unity3d.com>
2019-10-29 10:40:02 -0700

    CarPosition + CarPositionStaticProperties

---
+++ Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/CarPosition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity.Entities;
+
+[Serializable]
+public struct CarPosition : IComponentData
+{
+    public float Distance;
+    public float Lane;
+}
+
+[Serializable]
+public unsafe struct CarPositionStaticProperties : IComponentData
+{
+    public fixed float MergeSpace[4];
+}
