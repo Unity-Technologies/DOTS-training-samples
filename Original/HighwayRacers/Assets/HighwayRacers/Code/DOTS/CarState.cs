e7b8cc06fb5c7fe809da668afa315a611bf08b3c
Parashar Krishnamachari <parashar@unity3d.com>
2019-10-29 11:02:00 -0700

    CarState and CarVelocity components

---
+++ Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/CarState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unity.Entities;
+
+public enum VehicleState
+{
+    NORMAL,
+    MERGE_RIGHT,
+    MERGE_LEFT,
+    OVERTAKING,
+}
+
+[Serializable]
+public struct CarState : IComponentData
+{
+    public VehicleState state;
+}
---
+++ Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/CarVelocity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Entities;
+
+[Serializable]
+public struct CarVelocity : IComponentData
+{
+    public float InLaneVel;         // Speed within the lane
+    public float CrossLaneVel;      // Speed to move between lanes
+}
+
