975fd3a45937cb4ec02d8b62de9f5d70fdaf7569
Joel Eaton <joel.eaton@unity3d.com>
2019-10-30 13:36:51 -0700

    Chase state system

---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Components/ChaseVelocity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct ChaseVelocity : IComponentData
+{
+    public float Value;
+}
---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/ChaseStateSystem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class ChaseStateSystem : JobComponentSystem
+{
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+
+        var chaseVelocity = GetSingleton<ChaseVelocity>().Value;
+        var attackDistance = GetSingleton<AttackDistance>().Value;
+
+        var translationContainer = GetComponentDataFromEntity<Translation>(true);
+        return Entities.WithReadOnly(translationContainer).ForEach((Entity entity, ref State state, ref TargetVelocity targetVelocity, in TargetEntity targetEntity) =>
+        {
+            if (state.Value == State.StateType.Chasing)
+            {
+                var translation = translationContainer[entity];
+                var targetTranslation = translationContainer[targetEntity.Value];
+                targetVelocity.velocity = chaseVelocity;
+
+                var distance = math.distance(translation.Value, targetTranslation.Value);
+
+                if (distance < attackDistance)
+                {
+                    state.Value = State.StateType.Attacking;
+                }
+            }
+
+        }).Schedule(inputDependencies);
+
+    }
+}
+
+
+
+
+
+//IN
+//State, Position, TargetEntity, AttackDistance, ChaseVelocity
+
+//Out
+//State, TargetVelocity
