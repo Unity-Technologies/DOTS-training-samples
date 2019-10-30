fdac8f287d059873676bf23e4c91c97216a96e65
Joel Eaton <joel.eaton@unity3d.com>
2019-10-30 15:07:15 -0700

    Add attack state system

---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Components/AttackVelocity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct AttackVelocity : IComponentData
+{
+    public float Value;
+}
---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/AttackingStateSystem.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class AttackingStateSystem : JobComponentSystem
+{
+    EndSimulationEntityCommandBufferSystem buffer;
+
+    protected override void OnCreate()
+    {
+        buffer = World.Active.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+
+        
+
+        var attackDistance = GetSingleton<AttackDistance>().Value;
+        var attackVelocity = GetSingleton<AttackVelocity>().Value;
+
+        var translationContainer = GetComponentDataFromEntity<Translation>(true);
+       // var stateContainer = GetComponentDataFromEntity<State>(false);
+        var commonBuffer = buffer.CreateCommandBuffer().ToConcurrent();
+
+        var handle = Entities.WithReadOnly(translationContainer).ForEach((Entity entity, ref TargetVelocity targetVelocity, in TargetEntity targetEntity, in State state) =>
+        {
+            
+
+            if (state.Value == State.StateType.Attacking)
+        {
+              
+                var translation = translationContainer[entity];
+                var targetTranslation = translationContainer[targetEntity.Value];
+                targetVelocity.velocity = attackVelocity;
+
+                var distance = math.distance(translation.Value, targetTranslation.Value);
+
+                if (distance <= (attackDistance * 0.1f))
+                {
+                        commonBuffer.SetComponent(0, targetEntity.Value, new State
+                    {
+                        Value = State.StateType.Dead
+                    });
+
+                }
+
+            }
+        }).Schedule(inputDependencies);
+        buffer.AddJobHandleForProducer(handle);
+        return handle;
+        
+    }
+}
+
+
+// Input
+
+//State, Position, TargetEntity,
+//AttackVelocity
+
+//Out
+// State,
+// TargetVelocity
