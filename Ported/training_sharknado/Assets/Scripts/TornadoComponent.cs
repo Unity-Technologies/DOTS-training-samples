634bf16260c8a1ea0b6fe1a6732bc45b90b66d35
xiaoxi(cici)liu <xiaoxi@unity3d.com>
2019-10-30 16:59:55 -0700

    adding tornado data and authoring

---
+++ Ported/training_sharknado/Assets/Scripts/TornadoComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct TornadoSpawner : IComponentData
+{
+	public Entity particle;
+	public int numOfParticles;
+	public float force;
+	public float maxForceDist;
+	public float height;
+	public float upForce;
+	public float spinRate;
+	public float expForce;
+	public float breakResist;
+	public float particleSpinRate;
+	public float particleUpSpeed;
+	public float friction;
+	public float gravity;
+}
