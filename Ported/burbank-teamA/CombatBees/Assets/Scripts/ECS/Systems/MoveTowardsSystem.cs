f5af3135449cada1167f106c5a978cdb41e260ba
Joel Eaton <joel.eaton@unity3d.com>
2019-10-30 11:57:09 -0700

    Move towards system and TargetVelocity test scene

---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/MoveTowardsSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class MoveTowardsSystem : JobComponentSystem
+{ 
+   
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+       
+        var translationContainer = GetComponentDataFromEntity<Translation>(true);
+        return Entities.WithReadOnly(translationContainer).ForEach((Entity entity, ref Velocity velocity, ref TargetVelocity targetVelocity, in TargetEntity targetEntity) =>
+        {
+            var translation = translationContainer[entity];
+            var targetTranslation = translationContainer[targetEntity.Value];
+            var directionTowards = math.normalize(targetTranslation.Value - translation.Value);
+            var desiredVelocity = directionTowards * targetVelocity.velocity;
+            velocity.Value = math.lerp(velocity.Value, desiredVelocity, 0.5f);
+
+            }).Schedule(inputDependencies);
+
+        }
+}
+
+
