84d5a1187c474c3c461a901dd6400205cd210ca1
Juan Gabriel <juangabriel@frogames.es>
2019-10-30 11:22:03 -0700

    Spawning things

---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/SpawnSystem.cs
@@ -0,0 +1,40 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+
+public class SpawnSystem : JobComponentSystem
+{
+    BeginInitializationEntityCommandBufferSystem bufferSystem;
+
+    protected override void OnCreate()
+    {
+        bufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+    }
+
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+
+        var commandBuffer = bufferSystem.CreateCommandBuffer().ToConcurrent();
+
+
+        var jobHandle = Entities.ForEach((Entity e, ref Spawner spawner) => 
+               {
+                   for (int i = 0; i < spawner.Amount; i++)
+                   {
+                       UnityEngine.Debug.Log("1");
+                       var instance = commandBuffer.Instantiate(0, spawner.Prefab);
+                       commandBuffer.AddComponent(0, instance, new Translation() { Value = spawner.Position });
+                   }
+
+                   commandBuffer.DestroyEntity(0, e);
+
+               })
+            .Schedule(inputDeps);
+
+        bufferSystem.AddJobHandleForProducer(jobHandle);
+
+        return jobHandle;
+    }
+}
