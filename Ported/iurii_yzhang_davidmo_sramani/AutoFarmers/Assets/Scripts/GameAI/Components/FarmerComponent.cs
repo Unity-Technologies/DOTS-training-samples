3a87fb781293ac2d144a1879a7084e4c7f6b4fe1
Sahil Ramani <sahil.ramani@unity3d.com>
2019-10-30 13:26:10 -0700

    Auto Farmers : Fix stuff

--- Ported/iurii_yzhang_davidmo_sramani/AutoFarmers/Assets/Scripts/GameAI/Components/FarmerComponent.cs
+++ Ported/iurii_yzhang_davidmo_sramani/AutoFarmers/Assets/Scripts/GameAI/Components/FarmerComponent.cs
@@ -1,9 +1,15 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 namespace GameAI
 {
+    public struct TargetTag : IComponentData
+    {
+        public Translation TargetPosition;
+    }
+    
     public struct FarmerComponent : IComponentData
     {
         public int2 Position;
---
+++ Ported/iurii_yzhang_davidmo_sramani/AutoFarmers/Assets/Scripts/GameAI/MovementSystem.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace GameAI
+{
+    public struct AnimationCompleteTag : IComponentData {}
+    
+    public class MovementSystem : JobComponentSystem
+    {
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            Entities.WithReadOnly(typeof(TargetTag))
+                    .ForEach((ref AnimationCompleteTag _, ref TargetTag target, ref Translation position) => {
+                             });
+        }
+    }
+}
