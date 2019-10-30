22ccb004174915503858dd8a8d6428ec947bc936
thickiran <tolgay.hickiran@codeo.com.tr>
2019-10-30 10:06:39 -0700

    Target and Target Velocity

---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Components/TargetVelocity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct TargetVelocity : IComponentData
+{
+    public float3 velocity;
+}
