5670b651e919262feba444912753b8e1710629a1
Joel Eaton <joel.eaton@unity3d.com>
2019-10-30 10:10:39 -0700

    Added Team component

---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Components/Team.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+[Serializable]
+public struct Team : IComponentData
+{
+    public int Value;
+}
