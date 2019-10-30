667f90489ac639ef00f7543a232af5659993ab46
William Armstrong <williama@hq.unity3d.com>
2019-10-30 14:33:44 -0700

    Load map from a file
    So much map

--- Ported/vcvcwa/Assets/DotsScripts/GridComponent_Authoring.cs
+++ Ported/vcvcwa/Assets/DotsScripts/GridComponent_Authoring.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
@@ -9,8 +10,10 @@ using UnityEngine;
 [RequiresEntityConversion]
 public class GridComponent_Authoring : MonoBehaviour, IConvertGameObjectToEntity
 {
-    public List<int> values = new List<int>(100);
+    public TextAsset mapFile;
 
+    private List<int> values;
+    
     public int size;
     // The MonoBehaviour data is converted to ComponentData on the entity.
     // We are specifically transforming from a good editor representation of the data (Represented in degrees)
@@ -18,14 +21,27 @@ public class GridComponent_Authoring : MonoBehaviour, IConvertGameObjectToEntity
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
         var data = new GridComponent() {Size = size};
+
+        var textValues = mapFile.text.Split(',','\r');
+        values = new List<int>(textValues.Length);
+
+        foreach (var text in textValues)
+        {
+            int testValue;
+            if (Int32.TryParse(text, out testValue))
+            {
+                values.Add(Int32.Parse(text));
+            }
+        }
         
-        var buffer = dstManager.AddBuffer<GridTile>(entity);
-        for (int i = 0; i < values.Count; ++i)
+        // todo - make this a bit smarter. Add a sub-grid starting from zero
+        DynamicBuffer<GridTile> buffer = dstManager.AddBuffer<GridTile>(entity);
+        for (int i = 0; i < size*size && i < values.Count; ++i)
         {
             buffer.Add(values[i]);
         }
         
-
         dstManager.AddComponentData(entity, data);
+        
     }
 }
