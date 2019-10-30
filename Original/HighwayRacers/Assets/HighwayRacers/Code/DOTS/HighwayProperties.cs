2cafc608363ab56efd06833a28bd9b9faf265924
Parashar Krishnamachari <parashar@unity3d.com>
2019-10-30 09:19:28 -0700

    Car spawn properties, and added the road prefabs to the Highway props.

--- Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/HighwayProperties.cs
+++ Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/HighwayProperties.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using Unity.Entities;
 
 [Serializable]
-public struct HighwayProperties : IComponentData
+public struct HighwayProperties : ISharedComponentData
 {
     public float highwayLength;
     public int numCars;
+    public Entity straightPrefab;
+    public Entity curvePrefab;
 }
 
 [DisallowMultipleComponent]
@@ -20,10 +23,19 @@ public class HighwayPropertiesAuthoring : MonoBehaviour, IConvertGameObjectToEnt
     public const float CURVE_LANE0_RADIUS = MID_RADIUS - LANE_SPACING * (NUM_LANES - 1) / 2f;
     public const float MIN_HIGHWAY_LANE0_LENGTH = CURVE_LANE0_RADIUS * 4;
     public const float MIN_DIST_BETWEEN_CARS = .7f;
-    public const float MAX_HIGHWAY_LENGTH = 500 + LANE_SPACING * (NUM_LANES - 1) / 2f;
+    public const float MAX_HIGHWAY_LENGTH = 1000 + LANE_SPACING * (NUM_LANES - 1) / 2f;
 
     public float highWayLen;
     public int carsCount;
+    public GameObject straightSection;
+    public GameObject curveSection;
+
+    // Referenced prefabs have to be declared so that the conversion system knows about them ahead of time
+    public void DeclareReferencedPrefabs(List<GameObject> gameObjects)
+    {
+        gameObjects.Add(straightSection);
+        gameObjects.Add(curveSection);
+    }
 
     // Convert the MonoBehavior to an Entity
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
@@ -31,10 +43,36 @@ public class HighwayPropertiesAuthoring : MonoBehaviour, IConvertGameObjectToEnt
         var data = new HighwayProperties
         {
             highwayLength = highWayLen,
-            numCars = carsCount
+            numCars = carsCount,
+            straightPrefab = conversionSystem.GetPrimaryEntity(straightSection),
+            curvePrefab = conversionSystem.GetPrimaryEntity(curveSection)
         };
 
         dstManager.AddComponentData(entity, data);
     }
 }
 
+[CustomEditor(typeof(HighwayPropertiesAuthoring))]
+public class HighwayPropertiesEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        HighwayPropertiesAuthoring hpa = (HighwayPropertiesAuthoring)target;
+
+        if (hpa != null)
+        {
+            float initialLen = hpa.highWayLen;
+            int initialCount = hpa.carsCount;
+            GameObject pStraight = hpa.straightSection;
+            GameObject pCurve = hpa.curveSection;
+
+            hpa.highWayLen = EditorGUILayout.Slider("Highway Length", initialLen, HighwayPropertiesAuthoring.MID_RADIUS * 4, HighwayPropertiesAuthoring.MAX_HIGHWAY_LENGTH);
+            hpa.carsCount = EditorGUILayout.IntSlider("Number of Cars", initialCount, 0, (int)Math.Floor(hpa.highWayLen));
+            hpa.straightSection = (GameObject)EditorGUILayout.ObjectField("Straight Section Prefab", pStraight, typeof(GameObject), false);
+            hpa.curveSection = (GameObject)EditorGUILayout.ObjectField("Curve Section Prefab", pCurve, typeof(GameObject), false);
+        }
+    }
+}
---
+++ Original/HighwayRacers/Assets/HighwayRacers/Code/DOTS/SpawnProperties.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+[Serializable]
+public struct CarSpawnProperties : ISharedComponentData
+{
+    public Entity carPrefab;
+    public float defaultSpeedMin;
+    public float defaultSpeedMax;
+    public float overtakeSpeedMin;
+    public float overtakeSpeedMax;
+    public Color defaultColor;
+    public Color slowSpeedColor;
+    public Color highSpeedColor;
+    public float mergeSpaceMin;
+    public float mergeSpaceMax;
+    public float mergeLeftDistanceMin;
+    public float mergeLeftDistanceMax;
+    public float acceleration;
+    public float brakeDeceleration;
+    public float laneSwitchSpeed;
+    public float maxOvertakeTime;
+}
+
