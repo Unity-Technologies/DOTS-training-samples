4368572fb6c94dfbac9b847937db0c3cd8cb923b
Juan Gabriel <juangabriel@frogames.es>
2019-10-30 10:07:28 -0700

    Update ComponentBase.cs

--- SampleCode/ComponentBase.cs
+++ SampleCode/ComponentBase.cs
@@ -3,7 +3,7 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 
-[Serializable]
+[GenerateAuthoringComponent]
 public struct ComponentBase : IComponentData
 {
 	public float Value;
