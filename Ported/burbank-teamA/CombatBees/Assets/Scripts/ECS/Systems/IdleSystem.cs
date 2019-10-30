fbd2ee807b1e67f5712cfca7ffc95bb49c884bd9
thickiran <tolgay.hickiran@codeo.com.tr>
2019-10-30 10:47:07 -0700

    Changed TargetEntity

---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/IdleSystem.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+public class IdleSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        
+        var job = new IdleSystemJob();
+        
+       
+        return job.Schedule(this, inputDependencies);
+    }
+}
