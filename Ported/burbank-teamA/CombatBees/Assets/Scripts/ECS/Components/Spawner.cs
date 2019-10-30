7cdbbc5cdfef384778dbc2876c1e4b1b21d8513d
Juan Gabriel <juangabriel@frogames.es>
2019-10-30 10:02:45 -0700

    Spawner

---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Components/Spawner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct Spawner : IComponentData
+{
+    public Entity Prefab;
+    public float3 Position;
+    public int Amount;
+}
