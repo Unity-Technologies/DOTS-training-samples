e13126668300b41a21ee78c6a8c7f6f78400157e
Juan Gabriel <juangabriel@frogames.es>
2019-10-30 10:10:42 -0700

    Tags (bee, res, particle), in case they are needed

---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Components/Tags.cs
@@ -0,0 +1,8 @@
+using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct Tag_Bee : IComponentData { }
+public struct Tag_Resource : IComponentData { }
+public struct Tag_Particle : IComponentData { }
