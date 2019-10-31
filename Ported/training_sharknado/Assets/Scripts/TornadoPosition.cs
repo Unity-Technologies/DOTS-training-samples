8e8c942f4b5b499ec587eb1f2d61ada4967529ac
xiaoxi(cici)liu <xiaoxi@unity3d.com>
2019-10-31 10:39:12 -0700

    adding tornado position

---
+++ Ported/training_sharknado/Assets/Scripts/TornadoPosition.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+
+public struct TornadoPosition : IComponentData
+{
+	float3 position;
+}
--- Ported/training_sharknado/Assets/Scripts/TornadoSpawnerAuthoring.cs
+++ Ported/training_sharknado/Assets/Scripts/TornadoSpawnerAuthoring.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 public class TornadoSpawnerAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
 {	
 	public float Height = 10.0f;
-	public float Force = 5.0f;
+	public float Force = 5.0f;	
 	public GameObject Particle;
 	public int NumberOfParticle = 100;
 	public float ParticleSpinRate = 1.0f;
 	public float ParticleUpSpeed = 2.0f;
 	public float BreakResistance = 5.0f;
+	public float MaxForceDistance = 10f;
+	public float UpForce = 2f;
+	public float Friction = 0.1f;
+	public float Gravity = 5.0f;
 
 	public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
 	{
@@ -28,6 +32,10 @@ public class TornadoSpawnerAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 		tornado.particleSpinRate = ParticleSpinRate;
 		tornado.particleUpSpeed = ParticleUpSpeed;
 		tornado.breakResist = BreakResistance;
+		tornado.maxForceDist = MaxForceDistance;
+		tornado.upForce = UpForce;
+		tornado.friction = Friction;
+		tornado.gravity = Gravity;
 
 		dstManager.AddComponentData<TornadoSpawner>(entity, tornado);
 	}
