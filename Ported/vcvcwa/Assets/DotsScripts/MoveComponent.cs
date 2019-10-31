fb0af8dfe0b98f3e7c12386157d2e9cf53bc4de5
Vincent Chevalier <vincent.chevalier@unity3d.com>
2019-10-30 20:24:39 -0400

    Added fly component

---
+++ Ported/vcvcwa/Assets/DotsScripts/MoveComponent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Unity.Entities;
+
+// ReSharper disable once InconsistentNaming
+[GenerateAuthoringComponent]
+public struct MoveComponent : IComponentData
+{
+    public bool fly;
+}
