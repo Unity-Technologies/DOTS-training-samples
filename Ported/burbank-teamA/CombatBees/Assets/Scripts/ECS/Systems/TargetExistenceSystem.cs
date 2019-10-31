4227dd1cb4f40f901ce8811185f24925732a6ca9
thickiran <tolgay.hickiran@codeo.com.tr>
2019-10-31 12:12:10 -0700

    AttackingState Removes target entity

--- Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/TargetExistenceSystem.cs
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/TargetExistenceSystem.cs
@@ -8,13 +8,15 @@ using static Unity.Mathematics.math;
 
 public class TargetExistenceSystem : JobComponentSystem
 {
+    
     protected override JobHandle OnUpdate(JobHandle inputDependencies)
     {
-        return Entities
+        var translationContainer = GetComponentDataFromEntity<Translation>(true);
+        return Entities.WithReadOnly(translationContainer)
             .ForEach((ref State state, in TargetEntity targetEntity) => {
                 if (state.Value != State.StateType.Idle)
                 {
-                    if (targetEntity.Value == Entity.Null)
+                    if (!translationContainer.Exists(targetEntity.Value))
                     {
                         state.Value = State.StateType.Idle;
                     }
