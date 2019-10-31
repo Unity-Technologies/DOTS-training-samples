96cbde97a91572c4b252266c2fc578d1c7b76433
Vincent Chevalier <vincent.chevalier@unity3d.com>
2019-10-31 13:24:44 -0400

    Updated movement system for drone hovering

--- Ported/vcvcwa/Assets/DotsScripts/MovementSystem.cs
+++ Ported/vcvcwa/Assets/DotsScripts/MovementSystem.cs
@@ -8,18 +8,38 @@ public class MovementSystem : JobComponentSystem
 {
     protected override JobHandle OnUpdate(JobHandle inputDependencies)
     {
-        float distanceMininal = 10.0f;
+        float distanceMinimal = 0.1f;
         float speed = 1.0f;
         float dt = Time.deltaTime;
         
         var job1Handle = Entities
-            .ForEach((ref Translation translation, ref PositionComponent position,
-                in GoalPositionComponent goalPosition) =>
+            .ForEach((ref Translation translation, ref Rotation rotation, ref PositionComponent position,
+                in GoalPositionComponent goalPosition, in MoveComponent moveComponent, in DotsIntentionComponent dotsIntentionComponent) =>
             {
-                if (Vector2.Distance(goalPosition.position, position.position) > distanceMininal)
+                if (goalPosition.position.x < 0 || goalPosition.position.y < 0)
+                {
+                    return;
+                }
+                float targetY = 1.0f;
+                if (moveComponent.fly)
+                {
+                    if (Vector2.Distance(goalPosition.position, position.position) > distanceMinimal)
+                    {
+                        if (dotsIntentionComponent.intention == DotsIntention.Harvest)
+                        {
+                            targetY = 0.3f;
+                        }
+                        //TODO eventually bank toward target here
+                    }
+                }
+                else
+                {
+                    targetY = 0.0f;
+                }
+                if (Vector2.Distance(goalPosition.position, position.position) > distanceMinimal)
                 {
                     position.position= Vector2.Lerp(position.position, goalPosition.position, dt * speed);
-                    translation.Value = new float3(position.position.x, 0.0f, position.position.y);
+                    translation.Value = new float3(position.position.x, targetY, position.position.y);
                 }
             })
             .Schedule(inputDependencies);
