159ec4468e3fd417a7b20bab6dccc9e0682fbf67
Joel Eaton <joel.eaton@unity3d.com>
2019-10-31 10:17:34 -0700

    Die State System

---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Components/BloodParticlesPrefab.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+public struct BloodParticlesPrefab : IComponentData
+{
+    public Entity Value;
+}
---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Components/BloodParticlesPrefab_Authoring.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class BloodParticlesPrefab_Authoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+    [SerializeField] GameObject prefab;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var prefabEntity = conversionSystem.GetPrimaryEntity(prefab);
+        dstManager.AddComponentData(entity, new BloodParticlesPrefab { Value = prefabEntity });
+    }
+
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+        referencedPrefabs.Add(prefab);
+    }
+}
