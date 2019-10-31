afe655f521320e81ce4a4559dd5f8596417fa669
Vincent Chevalier <vincent.chevalier@unity3d.com>
2019-10-31 18:11:25 -0400

    ActorInteractSystem

---
+++ Ported/vcvcwa/Assets/DotsScripts/ActorInteractSystem.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection;
+using System;
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class ActorInteractSystem : JobComponentSystem
+{
+    private DynamicBuffer<GridTile> buffer;
+    private EntityQuery query;
+
+    protected override void OnCreate()
+    {
+        EntityQueryDesc queryDescription = new EntityQueryDesc();
+        queryDescription.All = new[] {ComponentType.ReadOnly<GridTile>()};
+        query = GetEntityQuery(queryDescription);
+    }
+
+    [BurstCompile]
+    struct GoalPositionJob : IJobForEach<ActorMovementComponent, DotsIntentionComponent>
+    {
+        [ReadOnly] public DynamicBuffer<GridTile> internalBuffer;
+
+        // The [ReadOnly] attribute tells the job scheduler that this job will not write to rotSpeedIJobForEach
+        public void Execute([ReadOnly] ref ActorMovementComponent actor, [ReadOnly] ref DotsIntentionComponent intention)
+        {
+            var atGoal = math.abs(actor.targetPosition.x - actor.position.x) < 0.5f;
+            if (!atGoal)
+            {
+                return;
+            }
+            var gridTile = internalBuffer[Convert.ToInt32(actor.targetPosition.x) + (512 * Convert.ToInt32(actor.targetPosition.y))];
+            if (intention.intention == DotsIntention.Rock && gridTile.IsRock())
+            {
+                //TODO insert bang rock in command buffer
+            } 
+            else if (intention.intention == DotsIntention.Till && gridTile.IsNothing())
+            {
+                //TODO insert till in command buffer
+            } 
+            else if (intention.intention == DotsIntention.Plant && gridTile.IsTilled())
+            {
+                //TODO insert plant in command buffer
+            } 
+            else if (intention.intention == DotsIntention.Harvest && gridTile.IsPlant() && gridTile.GetPlantHealth() > 75f)
+            {
+                //TODO insert harvest in command buffer
+            }
+            else if (intention.intention == DotsIntention.Shop && gridTile.IsShop())
+            {
+                //TODO insert shop in command buffer
+            }
+        }
+    }
+
+
+// OnUpdate runs on the main thread.
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        Entity farm = query.GetSingletonEntity();
+        buffer = EntityManager.GetBuffer<GridTile>(farm);
+
+
+        var job = new GoalPositionJob
+        {
+            internalBuffer = buffer
+            //DeltaTime = Time.deltaTime
+        };
+
+        return job.Schedule(this, inputDependencies);
+    }
+}
