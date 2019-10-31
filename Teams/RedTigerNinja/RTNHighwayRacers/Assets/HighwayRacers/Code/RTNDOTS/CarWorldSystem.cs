67e48fe1d25d47176c2e617df177d40ec2ca1ed3
Lewey Geselowitz <lewey.geselowitz@unity3d.com>
2019-10-31 14:30:04 -0700

    Some diffing tests

---
+++ Teams/RedTigerNinja/RTNHighwayRacers/Assets/HighwayRacers/Code/RTNDOTS/CarWorldSystem.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Scenes;
+
+public class CarWorldSystem : MonoBehaviour
+{
+    private World SavedWorld = null;
+    private Unity.Entities.EntityManagerDiffer SavedDiffState;
+    private bool HasSavedDiff = false;
+
+    [ContextMenu("Do Diff Test")]
+    public void DoDiffTest()
+    {
+        if (!HasSavedDiff)
+        {
+            HasSavedDiff = true;
+            SavedDiffState = new EntityManagerDiffer(World.Active, Unity.Collections.Allocator.Persistent);
+            SavedDiffState.GetChanges(EntityManagerDifferOptions.Default, Unity.Collections.Allocator.Persistent).Dispose();
+            return;
+        }
+        else
+        {
+            var diff = SavedDiffState.GetChanges(EntityManagerDifferOptions.Default, Unity.Collections.Allocator.Persistent);
+            Debug.Log("AnyChanges=" + diff.AnyChanges + " Fwd=" + diff.ForwardChangeSet + " Back=" + diff.HasReverseChangeSet);
+            Debug.Log("All=" + diff.ToString());
+            var fchng = diff.ForwardChangeSet;
+            foreach (var ec in fchng.EntityPatches)
+            {
+                Debug.Log(ec.ToString());
+            }
+            diff.Dispose();
+        }
+    }
+
+
+
+    [ContextMenu("Do World Test!")]
+    public void DoTest()
+    {
+        var world = World.Active;
+        Debug.Log("SN=" + world.SequenceNumber + " Version=" + world.Version + " EV=" + world.EntityManager.Version + " GEV=" + world.EntityManager.GlobalSystemVersion);
+        var em = world.EntityManager;
+
+        if (this.SavedWorld == null)
+        {
+            this.SavedWorld = new World("SavedWorld");
+            this.SavedWorld.EntityManager.CopyAndReplaceEntitiesFrom(em);
+        }
+        else
+        {
+            em.CompleteAllJobs();
+            em.CopyAndReplaceEntitiesFrom(this.SavedWorld.EntityManager);
+            this.SavedWorld = null;
+        }
+
+        //var myWorld = new World("OtherWorld");
+        //myWorld.EntityManager.CopyAndReplaceEntitiesFrom(em);
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
