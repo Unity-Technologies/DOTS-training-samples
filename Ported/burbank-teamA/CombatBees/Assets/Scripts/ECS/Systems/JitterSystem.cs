25b869dd8f1dfb92acd71f3984a0d370696069be
Juan Gabriel <juangabriel@frogames.es>
2019-10-31 15:22:59 -0700

    Jitter System Update

--- Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/JitterSystem.cs
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/JitterSystem.cs
@@ -8,12 +8,14 @@ public class JitterSystem : JobComponentSystem
 {
     protected override JobHandle OnUpdate(JobHandle inputDependencies)
     {
+
+        
         var dt = Time.deltaTime;
         var gravity = GetSingleton<JitterForce>().Value;
-        return Entities
-            .ForEach((ref Velocity velocity, ref Translation translation, in Team team) =>
+        return Entities.WithAll<BeeTag>().ForEach((ref Translation t, in Velocity v) =>
             {
-                velocity.Value = math.lerp(velocity.Value, velocity.Value -= math.up() * gravity * dt, 0.5f);
+                //velocity.Value = math.lerp(velocity.Value, velocity.Value -= noise.cnoise(velocity.Value) * gravity * dt, 0.5f);
+                t.Value += noise.cnoise(v.Value) * gravity * dt;
             })
             .Schedule(inputDependencies);
     }
