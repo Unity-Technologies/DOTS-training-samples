5e27568f5d5e33bf0a8a1877848a63adacc3a6cb
spaztronaut <wsndrs@gmail.com>
2019-10-30 18:43:50 -0700

    hack for bar rotation to look correct

--- Ported/training_sharknado/Assets/Scripts/BarSpawnerSystem.cs
+++ Ported/training_sharknado/Assets/Scripts/BarSpawnerSystem.cs
@@ -31,74 +31,85 @@ public class BarSpawnerSystem : JobComponentSystem
 
             int pointIndex = 0;
             Random r = new Random(4);
-
-            var up = new float3( 0, 1, 0 );
+            // HACK(wyatt): this is used to get bar rotation looking "correct"
+            var left = math.normalize( new float3( 1, 0, 1 ) );
+            float spacing = 2f;
+            float3 point;
 
             for (int i = 0; i < 35; i++)
             {
-                int height =  r.NextInt(4, 12);
+                int height = r.NextInt(4, 12);
+                // height = 1;
+
                 float3 pos = new float3(r.NextFloat(-45f, 45f), 0f, r.NextFloat(-45f, 45f));
-                float spacing = 2f;
-                for (int j = 0; j < height; j++)
+
+                point.x = pos.x + spacing;
+                point.y = 0;
+                point.z = pos.z - spacing;
+                hasAnchors[pointIndex] = true;
+                points[pointIndex] = point;
+                pointIndex++;
+
+                point.x = pos.x - spacing;
+                point.y = 0;
+                point.z = pos.z - spacing;
+                hasAnchors[pointIndex] = true;
+                points[pointIndex] = point;
+                pointIndex++;
+
+                point.x = pos.x + 0f;
+                point.y = 0;
+                point.z = pos.z + spacing;
+                hasAnchors[pointIndex] = true;
+                points[pointIndex] = point;
+                pointIndex++;
+
+                for (int j = 1; j <= height; j++)
                 {
-                    float3 point;
+                    float offset = ( j % 2 ) * .1f;
+                    
                     point.x = pos.x + spacing;
                     point.y = j * spacing;
                     point.z = pos.z - spacing;
-
-                    if (j == 0)
-                    {
-                        hasAnchors[pointIndex] = true;
-                    }
                     points[pointIndex] = point;
-                    pointIndex++; 
-                    
+                    pointIndex++;
+
                     point.x = pos.x - spacing;
                     point.y = j * spacing;
                     point.z = pos.z - spacing;
-
-                    if (j == 0)
-                    {
-                        hasAnchors[pointIndex] = true;
-                    }
-                    points[pointIndex] = point;
+                    points[ pointIndex ] = point;
                     pointIndex++;
-                    
+
                     point.x = pos.x + 0f;
                     point.y = j * spacing;
                     point.z = pos.z + spacing;
-
-                    if (j == 0)
-                    {
-                        hasAnchors[pointIndex] = true;
-                    }
                     points[ pointIndex ] = point;
                     pointIndex++;
                 }
             }
 
             // ground details
-            for (int i = 0; i < 300; i++)
-            {
-                Vector3 pos = new Vector3(r.NextFloat(-55f, 55f), 0f, r.NextFloat(-55f, 55f));
-                float3 point = points[pointIndex];
-                point.x = pos.x + r.NextFloat(-.2f, -.1f);
-                point.y = pos.y + r.NextFloat(0f, 3f);
-                point.z = pos.z + r.NextFloat(.1f, .2f);
-                points[pointIndex] = point;
-                pointIndex++;
+            // for (int i = 0; i < 300; i++)
+            // {
+            //     Vector3 pos = new Vector3(r.NextFloat(-55f, 55f), 0f, r.NextFloat(-55f, 55f));
+            //     point = points[pointIndex];
+            //     point.x = pos.x + r.NextFloat(-.2f, -.1f);
+            //     point.y = pos.y + r.NextFloat(0f, 3f);
+            //     point.z = pos.z + r.NextFloat(.1f, .2f);
+            //     points[pointIndex] = point;
+            //     pointIndex++;
 
-                point = points[pointIndex];
-                point.x = pos.x + r.NextFloat(.2f, .1f);
-                point.y = pos.y + r.NextFloat(0f, .2f);
-                point.z = pos.z + r.NextFloat(-.1f, -.2f);
-                if (r.NextFloat(1) < .1f)
-                {
-                    hasAnchors[pointIndex] = true;
-                }
-                points[pointIndex] = point;
-                pointIndex++;
-            }
+            //     point = points[pointIndex];
+            //     point.x = pos.x + r.NextFloat(.2f, .1f);
+            //     point.y = pos.y + r.NextFloat(0f, .2f);
+            //     point.z = pos.z + r.NextFloat(-.1f, -.2f);
+            //     if (r.NextFloat(1) < .1f)
+            //     {
+            //         hasAnchors[pointIndex] = true;
+            //     }
+            //     points[pointIndex] = point;
+            //     pointIndex++;
+            // }
 
             for (int i = 0; i < pointIndex; i++)
             {
@@ -117,15 +128,16 @@ public class BarSpawnerSystem : JobComponentSystem
                         bar.neighbors2 = 0;
 
                         float3 diff = bar.pos2 - bar.pos1;
-                        var forward = math.normalize( diff );
+                        var forward = math.normalizesafe( diff );
+                        var rot = quaternion.LookRotationSafe( forward, math.cross( forward, left ) );
+
                         var instance = CommandBuffer.Instantiate( jobIndex, spawner.prefab);
-                        var rot = quaternion.LookRotationSafe( forward, math.cross( math.cross( up, forward ), forward ) );
-                        CommandBuffer.SetComponent( jobIndex, instance, new Translation { Value = ( points[i] + points[j] ) / 2 });
-                        CommandBuffer.SetComponent( jobIndex, instance, new Rotation { Value = rot } );
                         CommandBuffer.RemoveComponent( jobIndex, instance, typeof( Scale ) );
+                        CommandBuffer.AddComponent( jobIndex, instance, bar );
                         CommandBuffer.AddComponent( jobIndex, instance, typeof( NonUniformScale ) );
                         CommandBuffer.SetComponent( jobIndex, instance, new NonUniformScale { Value = new float3( .1f, .1f, bar.length ) } );
-                        CommandBuffer.AddComponent( jobIndex, instance, bar );
+                        CommandBuffer.SetComponent( jobIndex, instance, new Translation { Value = ( points[i] + points[j] ) / 2 });
+                        CommandBuffer.SetComponent( jobIndex, instance, new Rotation { Value = rot } );
                     }
                 }
             }
