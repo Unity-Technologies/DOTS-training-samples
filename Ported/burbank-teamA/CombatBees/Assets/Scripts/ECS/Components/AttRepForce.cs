856b6eb6d31e36c3df521f67ff36f34257c4c70f
Joel Eaton <joel.eaton@unity3d.com>
2019-10-31 16:05:47 -0700

    Attraction/Repulsion

---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Components/AttRepForce.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+
+public struct AttRepForce : IComponentData
+{
+    public float Value;
+}
---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/BaseAttractionSystem.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Properties;
+using Unity.Transforms;
+using UnityEngine;
+using static Unity.Mathematics.math;
+
+[DisableAutoCreation]
+public abstract class BaseAttractionSystem: JobComponentSystem
+{
+private EntityQuery resourceGroup;
+private EntityQuery friendT1Group, friendT2Group;
+
+    protected abstract float Direction { get; }
+protected override void OnCreate()
+{
+    friendT1Group = GetEntityQuery(ComponentType.ReadOnly<BeeTag>(), ComponentType.ReadOnly<Translation>(), ComponentType.ReadOnly<Team1Tag>()); //get the bees
+    friendT2Group = GetEntityQuery(ComponentType.ReadOnly<BeeTag>(), ComponentType.ReadOnly<Translation>(), ComponentType.ReadOnly<Team2Tag>()); //get the bees
+}
+
+protected override JobHandle OnUpdate(JobHandle inputDependencies)
+{
+    var friendT1 = friendT1Group.ToComponentDataArray<Translation>(Allocator.TempJob);
+    var friendT2 = friendT2Group.ToComponentDataArray<Translation>(Allocator.TempJob);
+        var delta = Time.deltaTime;
+        var direction = Direction;
+        var attrepf = GetSingleton<AttRepForce>();
+        var rnd = UnityEngine.Random.Range(-100, 100);
+
+    return Entities.WithoutBurst()
+            .WithDeallocateOnJobCompletion(friendT1)
+            .WithDeallocateOnJobCompletion(friendT2)
+
+        .ForEach((Entity entity, ref Velocity velocity, ref TargetEntity targetEntity, in Translation translation, in Team team) =>
+        {
+            float3 friendTranslation = new float3();
+
+            if (team.Value == 1)
+            {
+                int rand = (int)((noise.cnoise(translation.Value + new float3(rnd,rnd,rnd)) + 1) * (friendT1.Length - 1) / 2);
+                 friendTranslation = friendT1[rand].Value;
+
+            }
+
+            if (team.Value == 2)
+            {
+                int rand = (int)((noise.cnoise(translation.Value + new float3(rnd, rnd, rnd)) + 1) * (friendT2.Length - 1) / 2);
+                friendTranslation = friendT2[rand].Value;
+
+            }
+
+
+            var directionTowards = math.normalize(friendTranslation - translation.Value);
+            velocity.Value += directionTowards * attrepf.Value * delta * direction;
+            UnityEngine.Debug.Log(directionTowards * attrepf.Value * delta * direction);
+
+        })
+        .Schedule(inputDependencies);
+}
+}
+
+public class AttractionSystem : BaseAttractionSystem
+{
+    protected override float Direction => 1;
+}
+
+
+public class RepulsionSystem : BaseAttractionSystem
+{
+    protected override float Direction => -1;
+}
