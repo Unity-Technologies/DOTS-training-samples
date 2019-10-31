a7367d8fb0e6641fad3c9078f3cb3fdb449c2667
thickiran <tolgay.hickiran@codeo.com.tr>
2019-10-31 14:50:19 -0700

    JitterSystem

---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Components/JitterForce.cs
@@ -0,0 +1,7 @@
+﻿using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct JitterForce : IComponentData
+{
+    public float Value;
+}
---
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/JitterSystem.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class JitterSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var dt = Time.deltaTime;
+        var gravity = GetSingleton<JitterForce>().Value;
+        return Entities
+            .ForEach((ref Velocity velocity, ref Translation translation, in Team team) =>
+            {
+                velocity.Value = math.lerp(velocity.Value, velocity.Value -= math.up() * gravity * dt, 0.5f);
+            })
+            .Schedule(inputDependencies);
+    }
+}
