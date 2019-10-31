c495908aeb17b5f7abbffca696329e8929603533
Yuping Zhang <draupnir37@gmail.com>
2019-10-31 14:18:08 -0700

    add score system

--- Ported/iurii_yzhang_davidmo_sramani/AutoFarmers/Assets/Scripts/GameAI/PlantSystem.cs
+++ Ported/iurii_yzhang_davidmo_sramani/AutoFarmers/Assets/Scripts/GameAI/PlantSystem.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Burst;
+﻿using System;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
@@ -20,20 +21,39 @@ public class PlantSystem : JobComponentSystem
 	protected override JobHandle OnUpdate(JobHandle inputDeps)
 	{
 		var dt = Time.deltaTime;
+		
+		// if the plant is growing on the ground
 		var ecb = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
-
 		var job = Entities
 			.WithAll<TagPlant>()
 			.WithNone<TagFullyGrownPlant>()
-			.ForEach((Entity e, int entityInQueryIndex, ref PlantGrowSpeed s, ref HealthComponent health) =>
-		{
-			health.Value += s.Value * dt;
-			if (health.Value > 5.0)
+			.ForEach((Entity e, int entityInQueryIndex, in PlantGrowSpeed s, ref HealthComponent health) =>
 			{
-				ecb.AddComponent<TagFullyGrownPlant>(entityInQueryIndex, e);
-			}
-		}).Schedule(inputDeps);
-
-		return job;
+				health.Value += s.Value * dt;
+				if (health.Value > 5.0)
+				{
+					ecb.AddComponent<TagFullyGrownPlant>(entityInQueryIndex, e);
+				}
+			}).Schedule(inputDeps);
+		
+		// if the plant is being picked 
+		// DO nothing
+		
+		// if the plant is being sold
+		var ecb2 = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+		var job2 = Entities
+			.WithAll<TagFullyGrownPlant>()
+			.WithAll<AITagTaskDeliver>()
+			.ForEach((Entity e, int entityInQueryIndex, in PlantGrowSpeed s, ref HealthComponent health) =>
+			{
+				health.Value = -1;
+				ecb.RemoveComponent<TagFullyGrownPlant>(entityInQueryIndex, e);
+			}).Schedule(inputDeps);
+		
+		m_EntityCommandBufferSystem.AddJobHandleForProducer(job);
+		m_EntityCommandBufferSystem.AddJobHandleForProducer(job2);
+		var handle = JobHandle.CombineDependencies(job, job2);
+			
+		return handle;
 	}
 }
---
+++ Ported/iurii_yzhang_davidmo_sramani/AutoFarmers/Assets/Scripts/GameAI/ScoreSystem.cs
@@ -0,0 +1,100 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Jobs.LowLevel.Unsafe;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+namespace GameAI
+{
+    public class ScoreSystem : JobComponentSystem
+    {
+        public int Score;
+        BeginInitializationEntityCommandBufferSystem m_EntityCommandBufferSystem;
+
+        protected override void OnCreate()
+        {
+            // World.GetOrCreateSystem<WorldCreatorSystem>().
+            Score = 0;
+        }
+        
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            // sell plant +1
+            var ecb = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+            // how to know the plant has been sold?
+            // the sub task sell plant 
+            var job = Entities
+                .WithAll<AISubTaskSellPlant>()
+                .WithNone<AISubTaskTagComplete>()
+                .ForEach((Entity entity, int entityInQueryIndex) => 
+                {
+                    ecb.AddComponent<AISubTaskTagComplete>(entityInQueryIndex, entity);
+                    Score++;
+                }).Schedule(inputDeps);
+
+            // smash rock + 1
+            var ecb2 = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+            var job2 = Entities
+                .WithAll<AISubTaskTagClearRock>()
+                .WithNone<AISubTaskTagComplete>()
+                .ForEach((Entity entity, int entityInQueryIndex) => 
+                {
+                    ecb.AddComponent<AISubTaskTagComplete>(entityInQueryIndex, entity);
+                    Score++;
+                }).Schedule(inputDeps);
+
+            // spawn farmer -15
+            // add SpawnFarmerTagComponent
+            // add SpawnPointComponent
+            var ecb3 = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+            var job3 = Entities
+                .WithAll<TagShop>()
+                .WithoutBurst()
+                .ForEach((Entity e, int entityInQueryIndex, in TilePositionable tile) =>
+                {
+                    // random chance it will spawn at this shop
+                    var random = new Unity.Mathematics.Random();
+                    var p = random.NextDouble(1.0);
+                    if (p > 0.5)
+                    {
+                        // get shop position
+                        var pos = tile.Position;
+                        ecb3.AddComponent(entityInQueryIndex, e, new SpawnPointComponent{MapSpawnPosition = pos});
+                        ecb3.AddComponent<SpawnFarmerTagComponent>(entityInQueryIndex, e);
+                    }
+                }).Schedule(inputDeps);
+            
+            // spawn drone -20
+            // add SpawnDroneTagComponent
+            // add SpawnPointComponent
+            var ecb4 = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+            var job4 = Entities
+                .WithAll<TagShop>()
+                .WithoutBurst()
+                .ForEach((Entity e, int entityInQueryIndex, in TilePositionable tile) =>
+                {
+                    // random chance it will spawn at this shop
+                    var random = new Unity.Mathematics.Random();
+                    var p = random.NextDouble(1.0);
+                    if (p < 0.5)
+                    {
+                        // get shop position
+                        var pos = tile.Position;
+                        ecb3.AddComponent(entityInQueryIndex, e, new SpawnPointComponent{MapSpawnPosition = pos});
+                        ecb3.AddComponent<SpawnDroneTagComponent>(entityInQueryIndex, e);
+                    }
+                }).Schedule(inputDeps);
+
+            m_EntityCommandBufferSystem.AddJobHandleForProducer(job);
+            m_EntityCommandBufferSystem.AddJobHandleForProducer(job2);
+            m_EntityCommandBufferSystem.AddJobHandleForProducer(job3);
+            m_EntityCommandBufferSystem.AddJobHandleForProducer(job4);
+
+            return JobHandle.CombineDependencies(JobHandle.CombineDependencies(job, job2), JobHandle.CombineDependencies(job3, job4));
+
+        }
+    }
+}
