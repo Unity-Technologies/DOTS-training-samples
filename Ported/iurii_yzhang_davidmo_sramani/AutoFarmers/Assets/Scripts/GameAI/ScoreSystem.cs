b4b0b25b4caebfd60db9f08f909b6344eaa74cc9
Yuping Zhang <draupnir37@gmail.com>
2019-10-31 15:34:41 -0700

    add score system

--- Ported/iurii_yzhang_davidmo_sramani/AutoFarmers/Assets/Scripts/GameAI/ScoreSystem.cs
+++ Ported/iurii_yzhang_davidmo_sramani/AutoFarmers/Assets/Scripts/GameAI/ScoreSystem.cs
@@ -1,50 +1,60 @@
 ﻿using Unity.Burst;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Jobs.LowLevel.Unsafe;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
 using static Unity.Mathematics.math;
 
 namespace GameAI
 {
     public class ScoreSystem : JobComponentSystem
     {
-        public int Score;
+        //public Entity Score; // singletonEntity
+        private WorldCreatorSystem m_world;
         BeginInitializationEntityCommandBufferSystem m_EntityCommandBufferSystem;
 
         protected override void OnCreate()
         {
-            // World.GetOrCreateSystem<WorldCreatorSystem>().
-            Score = 0;
+            m_world = World.GetOrCreateSystem<WorldCreatorSystem>();
+            m_EntityCommandBufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
         }
         
-        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        protected unsafe override JobHandle OnUpdate(JobHandle inputDeps)
         {
+            var score = m_world.scoreArray;
+            for (int i = 0; i < score.Length; i++)
+            {
+                score[i] = 0;
+            }
             // sell plant +1
             var ecb = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
             // how to know the plant has been sold?
-            // the sub task sell plant 
+            // the sub task sell plant
+            
             var job = Entities
                 .WithAll<AISubTaskSellPlant>()
                 .WithNone<AISubTaskTagComplete>()
-                .ForEach((Entity entity, int entityInQueryIndex) => 
+                .ForEach((Entity entity, int entityInQueryIndex, int nativeThreadIndex) => 
                 {
                     ecb.AddComponent<AISubTaskTagComplete>(entityInQueryIndex, entity);
-                    Score++;
+                    //System.Threading.Interlocked.Increment(ref ((int*)score.GetUnsafePtr())[0]);
+                    score[nativeThreadIndex] = score[nativeThreadIndex] + 1;
                 }).Schedule(inputDeps);
 
-            // smash rock + 1
+            // smash rock +1
             var ecb2 = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
             var job2 = Entities
                 .WithAll<AISubTaskTagClearRock>()
                 .WithNone<AISubTaskTagComplete>()
-                .ForEach((Entity entity, int entityInQueryIndex) => 
+                .ForEach((Entity entity, int entityInQueryIndex, int nativeThreadIndex) => 
                 {
-                    ecb.AddComponent<AISubTaskTagComplete>(entityInQueryIndex, entity);
-                    Score++;
-                }).Schedule(inputDeps);
+                    ecb2.AddComponent<AISubTaskTagComplete>(entityInQueryIndex, entity);
+                    score[nativeThreadIndex] = score[nativeThreadIndex] + 1;
+                }).Schedule(job);
 
             // spawn farmer -15
             // add SpawnFarmerTagComponent
@@ -53,7 +63,7 @@ namespace GameAI
             var job3 = Entities
                 .WithAll<TagShop>()
                 .WithoutBurst()
-                .ForEach((Entity e, int entityInQueryIndex, in TilePositionable tile) =>
+                .ForEach((Entity e, int nativeThreadIndex, int entityInQueryIndex, in TilePositionable tile) =>
                 {
                     // random chance it will spawn at this shop
                     var random = new Unity.Mathematics.Random();
@@ -65,7 +75,9 @@ namespace GameAI
                         ecb3.AddComponent(entityInQueryIndex, e, new SpawnPointComponent{MapSpawnPosition = pos});
                         ecb3.AddComponent<SpawnFarmerTagComponent>(entityInQueryIndex, e);
                     }
-                }).Schedule(inputDeps);
+                    
+                    score[nativeThreadIndex] = score[nativeThreadIndex] - 15;
+                }).Schedule(job2);
             
             // spawn drone -20
             // add SpawnDroneTagComponent
@@ -74,7 +86,7 @@ namespace GameAI
             var job4 = Entities
                 .WithAll<TagShop>()
                 .WithoutBurst()
-                .ForEach((Entity e, int entityInQueryIndex, in TilePositionable tile) =>
+                .ForEach((Entity e, int nativeThreadIndex, int entityInQueryIndex, in TilePositionable tile) =>
                 {
                     // random chance it will spawn at this shop
                     var random = new Unity.Mathematics.Random();
@@ -83,18 +95,30 @@ namespace GameAI
                     {
                         // get shop position
                         var pos = tile.Position;
-                        ecb3.AddComponent(entityInQueryIndex, e, new SpawnPointComponent{MapSpawnPosition = pos});
-                        ecb3.AddComponent<SpawnDroneTagComponent>(entityInQueryIndex, e);
+                        ecb4.AddComponent(entityInQueryIndex, e, new SpawnPointComponent{MapSpawnPosition = pos});
+                        ecb4.AddComponent<SpawnDroneTagComponent>(entityInQueryIndex, e);
                     }
-                }).Schedule(inputDeps);
+                    
+                    score[nativeThreadIndex] = score[nativeThreadIndex] - 20;
+                }).Schedule(job3);
 
             m_EntityCommandBufferSystem.AddJobHandleForProducer(job);
             m_EntityCommandBufferSystem.AddJobHandleForProducer(job2);
             m_EntityCommandBufferSystem.AddJobHandleForProducer(job3);
             m_EntityCommandBufferSystem.AddJobHandleForProducer(job4);
+            
+            var handle = JobHandle.CombineDependencies(JobHandle.CombineDependencies(job, job2), JobHandle.CombineDependencies(job3, job4));
+            handle.Complete();
 
-            return JobHandle.CombineDependencies(JobHandle.CombineDependencies(job, job2), JobHandle.CombineDependencies(job3, job4));
+            int score_tmp = 0;
+            for (int i = 0; i < score.Length; i++)
+            {
+                score_tmp += score[i];
+            }
+            
+            Debug.Log(score_tmp);
 
+            return handle;
         }
     }
 }
