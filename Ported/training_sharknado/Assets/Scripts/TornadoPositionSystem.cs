ecd9652366b919a53ece15a622978aff1c9cdbd7
LandonTownsendUnity <52939698+LandonTownsendUnity@users.noreply.github.com>
2019-10-31 17:35:52 -0700

    fixed error in tornado position system

--- Ported/training_sharknado/Assets/Scripts/TornadoPositionSystem.cs
+++ Ported/training_sharknado/Assets/Scripts/TornadoPositionSystem.cs
@@ -11,17 +11,18 @@ public class TornadoPositionSystem : JobComponentSystem
     [BurstCompile]
     public struct ChangePosition : IJobForEach<TornadoPosition>
     {
+        public float time;
         public void Execute(ref TornadoPosition tornadoPos)
         {
-            tornadoPos.position = new float3(Mathf.Cos(Time.time / 6f) * 30f, 0,
-            Mathf.Sin(Time.time / 6f * 1.618f) * 30f);
+            tornadoPos.position = new float3(Mathf.Cos(time / 6f) * 30f, 0,
+            Mathf.Sin(time / 6f * 1.618f) * 30f);
         }
     }
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
         var job = new ChangePosition()
         {
-
+            time = Time.time
         }.Schedule(this, inputDeps);
         return job;
     }
