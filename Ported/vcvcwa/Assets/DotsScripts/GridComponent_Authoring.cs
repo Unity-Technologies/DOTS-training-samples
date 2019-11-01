e9e3db3e1c689d4ea7e26a95c7a83794f6a482d2
William Armstrong <williama@hq.unity3d.com>
2019-10-31 17:36:17 -0700

    Move camera location - Better authoring
    Guaranteed size for board

--- Ported/vcvcwa/Assets/DotsScripts/GridComponent_Authoring.cs
+++ Ported/vcvcwa/Assets/DotsScripts/GridComponent_Authoring.cs
@@ -20,7 +20,7 @@ public class GridComponent_Authoring : MonoBehaviour, IConvertGameObjectToEntity
     {
         var data = new GridComponent() {Size = size};
 
-        var textValues = mapFile.text.Split(',','\r');
+        var textValues = mapFile.text.Split(',','\r','\n',' ');
         values = new List<int>(textValues.Length);
 
         foreach (var text in textValues)
@@ -30,13 +30,24 @@ public class GridComponent_Authoring : MonoBehaviour, IConvertGameObjectToEntity
             {
                 values.Add(Int32.Parse(text));
             }
+            else
+            {
+                Debug.AssertFormat(true, "Importing the grid got broken.");
+            }
         }
         
         // todo - make this a bit smarter. Add a sub-grid starting from zero
         DynamicBuffer<GridTile> buffer = dstManager.AddBuffer<GridTile>(entity);
-        for (int i = 0; i < size*size && i < values.Count; ++i)
+        for (int i = 0; i < size*size; ++i)
         {
-            buffer.Add(values[i]);
+            if (i < values.Count)
+            {
+                buffer.Add(values[i]);
+            }
+            else
+            {
+                buffer.Add(0);
+            }
         }
         
         dstManager.AddComponentData(entity, data);
