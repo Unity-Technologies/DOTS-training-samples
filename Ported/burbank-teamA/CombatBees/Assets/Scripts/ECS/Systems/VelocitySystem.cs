1f19245f3657e5e9c9ad8c3743cf8f338b278c2f
Juan Gabriel <juangabriel@frogames.es>
2019-11-01 09:40:44 -0700

    Scaling bees

--- Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/VelocitySystem.cs
+++ Ported/burbank-teamA/CombatBees/Assets/Scripts/ECS/Systems/VelocitySystem.cs
@@ -9,9 +9,10 @@ public class VelocitySystem : JobComponentSystem
         var dt = Time.deltaTime;
         
         return Entities
-            .ForEach((ref Translation translation,/*ref NonUniformScale s,*/ in Velocity velocity) => { 
+            .ForEach((ref Translation translation, ref NonUniformScale s, in Velocity velocity) => { 
                 translation.Value += velocity.Value * dt;
-                /*s.Value = math.normalize(velocity.Value);*/
+                //s.Value = math.max(math.normalize(math.abs(velocity.Value))*2.0f, new float3(0.5f, 0.5f, 0.5f));
+                s.Value.x = math.min(math.abs(velocity.Value.x)/15.0f, 2.0f);
             })
             .Schedule(inputDependencies);
     }
