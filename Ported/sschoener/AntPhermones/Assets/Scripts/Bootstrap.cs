e7864fae715c94d46bb69bfa5e45a3cacb91870f
Sebastian Schöner <sebastian.schoner@unity3d.com>
2019-11-21 15:11:18 +0100

    Remove transform systems

---
+++ Ported/sschoener/AntPhermones/Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Entities;
+
+namespace Systems
+{
+    public class Bootstrap : ICustomBootstrap
+    {
+        public List<Type> Initialize(List<Type> systems)
+        {
+            // this system seems to create garbage
+            systems.Remove(typeof(Unity.Transforms.TransformSystemGroup));
+            return systems;
+        }
+    }
+}
