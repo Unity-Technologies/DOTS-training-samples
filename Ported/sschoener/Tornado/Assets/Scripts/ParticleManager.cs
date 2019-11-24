5d14e7c4d76ddf80b4f36ed3dd111f6f37abac84
Sebastian Schöner <sebastian.schoner@unity3d.com>
2019-11-24 13:54:33 +0100

    Clean up particle manager

--- Ported/sschoener/Tornado/Assets/Scripts/ParticleManager.cs
+++ Ported/sschoener/Tornado/Assets/Scripts/ParticleManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ParticleManager : MonoBehaviour
 {
@@ -6,21 +8,23 @@ public class ParticleManager : MonoBehaviour
     public Material particleMaterial;
     public float spinRate;
     public float upwardSpeed;
-    Vector3[] m_Points;
+    float3[] m_Points;
     Matrix4x4[] m_Matrices;
     MaterialPropertyBlock m_MatProps;
     float[] m_RadiusMults;
-
+    static readonly int k_Color = Shader.PropertyToID("_Color");
+    const int k_NumParticles = 1000;
+    
     void Start()
     {
-        m_Points = new Vector3[1000];
-        m_Matrices = new Matrix4x4[1000];
-        m_RadiusMults = new float[1000];
-        Vector4[] colors = new Vector4[1000];
+        m_Points = new float3[k_NumParticles];
+        m_Matrices = new Matrix4x4[k_NumParticles];
+        m_RadiusMults = new float[k_NumParticles];
+        Vector4[] colors = new Vector4[k_NumParticles];
 
-        for (int i = 0; i < m_Points.Length; i++)
+        for (int i = 0; i < k_NumParticles; i++)
         {
-            Vector3 pos = new Vector3(Random.Range(-50f, 50f), Random.Range(0f, 50f), Random.Range(-50f, 50f));
+            float3 pos = new float3(Random.Range(-50f, 50f), Random.Range(0f, 50f), Random.Range(-50f, 50f));
             m_Points[i] = pos;
             m_Matrices[i] = Matrix4x4.TRS(m_Points[i], Quaternion.identity, Vector3.one * Random.Range(.2f, .7f));
             m_RadiusMults[i] = Random.value;
@@ -28,22 +32,22 @@ public class ParticleManager : MonoBehaviour
         }
 
         m_MatProps = new MaterialPropertyBlock();
-        m_MatProps.SetVectorArray("_Color", colors);
+        m_MatProps.SetVectorArray(k_Color, colors);
     }
 
     void Update()
     {
         for (int i = 0; i < m_Points.Length; i++)
         {
-            Vector3 tornadoPos = new Vector3(PointManager.tornadoX + PointManager.TornadoSway(m_Points[i].y), m_Points[i].y, PointManager.tornadoZ);
-            Vector3 delta = (tornadoPos - m_Points[i]);
-            float dist = delta.magnitude;
+            float3 tornadoPos = new float3(PointManager.tornadoX + PointManager.TornadoSway(m_Points[i].y), m_Points[i].y, PointManager.tornadoZ);
+            float3 delta = (tornadoPos - m_Points[i]);
+            float dist = math.length(delta);
             delta /= dist;
-            float inForce = dist - Mathf.Clamp01(tornadoPos.y / 50f) * 30f * m_RadiusMults[i] + 2f;
-            m_Points[i] += new Vector3(-delta.z * spinRate + delta.x * inForce, upwardSpeed, delta.x * spinRate + delta.z * inForce) * Time.deltaTime;
+            float inForce = dist - math.clamp(tornadoPos.y / 50f, 0, 1) * 30f * m_RadiusMults[i] + 2f;
+            m_Points[i] += new float3(-delta.z * spinRate + delta.x * inForce, upwardSpeed, delta.x * spinRate + delta.z * inForce) * Time.deltaTime;
             if (m_Points[i].y > 50f)
             {
-                m_Points[i] = new Vector3(m_Points[i].x, 0f, m_Points[i].z);
+                m_Points[i] = new float3(m_Points[i].x, 0f, m_Points[i].z);
             }
 
             Matrix4x4 matrix = m_Matrices[i];
