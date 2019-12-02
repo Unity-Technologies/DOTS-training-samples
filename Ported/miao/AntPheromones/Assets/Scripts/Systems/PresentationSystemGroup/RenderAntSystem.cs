7e2d1582740ff9c7f21a70f46768448878f1ba27
Miao Yu Goh <57358879+MiaoYu-Goh@users.noreply.github.com>
2019-12-02 17:00:48 +0100

    Ant rendering

--- Ported/miao/AntPheromones/Assets/Scripts/Systems/PresentationSystemGroup/RenderAntSystem.cs
+++ Ported/miao/AntPheromones/Assets/Scripts/Systems/PresentationSystemGroup/RenderAntSystem.cs
@@ -1,24 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using AntPheromones_ECS;
+using Unity.Burst;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Jobs;
-using Unity.Profiling;
 using Unity.Transforms;
 using UnityEngine;
 
 [UpdateInGroup(typeof(PresentationSystemGroup))]
-[UpdateAfter(typeof(UpdateAntColourSystem))]
 public class RenderAntSystem : JobComponentSystem
 {
     private EntityQuery _antQuery;
     private EntityQuery _antSharedRenderingQuery;
     private bool _allocatedBatchData;
-
+    
     private readonly List<Matrix4x4[]> _matrices = new List<Matrix4x4[]>();
     private readonly List<Vector4[]> _colours = new List<Vector4[]>();
+    private readonly List<GCHandle> _gcHandlesToFree = new List<GCHandle>();
     private readonly List<MaterialPropertyBlock> _materialPropertyBlocks = new List<MaterialPropertyBlock>();
-
+    
     protected override void OnCreate()
     {
         base.OnCreate();
@@ -44,46 +47,55 @@ public class RenderAntSystem : JobComponentSystem
             AllocateBatchData(numBatches);
             this._allocatedBatchData = true;
         }
+
+        var antRenderer = 
+            EntityManager.GetSharedComponentData<AntSharedRendering>(this._antSharedRenderingQuery.GetSingletonEntity());
         
-        NativeArray<Matrix4x4> localToWorlds;
-        NativeArray<Vector4> colours;
+        var matrices = new NativeArray<IntPtr>(numBatches, Allocator.Temp);
+        var colors = new NativeArray<IntPtr>(numBatches, Allocator.Temp);
         
-        using (new ProfilerMarker("ToComponentArray").Auto())
+        unsafe
         {
-            localToWorlds = this._antQuery.ToComponentDataArray<LocalToWorld>(Allocator.TempJob).Reinterpret<Matrix4x4>();
-            colours = this._antQuery.ToComponentDataArray<Colour>(Allocator.TempJob).Reinterpret<Vector4>();
+            for (int batch = 0; batch < numBatches; batch++)
+            {
+                GCHandle matricesHandle = GCHandle.Alloc(this._matrices[batch], GCHandleType.Pinned);
+                GCHandle coloursHandle = GCHandle.Alloc(this._colours[batch], GCHandleType.Pinned);
+                
+                matrices[batch] = matricesHandle.AddrOfPinnedObject();
+                colors[batch] = coloursHandle.AddrOfPinnedObject();
+                
+                this._gcHandlesToFree.Add(matricesHandle);
+                this._gcHandlesToFree.Add(coloursHandle);
+            }
+
+            new Job
+            {
+                Colours = (Vector4**)colors.GetUnsafePtr(),
+                Matrices = (Matrix4x4**)matrices.GetUnsafePtr(),
+            }.Schedule(this, inputDeps).Complete();
+
+            foreach (GCHandle gcHandle in this._gcHandlesToFree)
+            {
+                gcHandle.Free();
+            }
+            
+            this._gcHandlesToFree.Clear();
         }
 
         for (int batch = 0; batch < numBatches; batch++)
         {
             int batchSize = batch == numBatches - 1 ? modulo : Rendering.MaxNumMeshesPerDrawCall;
-            
-            NativeArray<Matrix4x4>.Copy(
-                src: localToWorlds.GetSubArray(start: batch * Rendering.MaxNumMeshesPerDrawCall, length: batchSize),
-                dst: this._matrices[batch],
-                length: batchSize);
-            NativeArray<Vector4>.Copy(
-                src: colours.GetSubArray(start: batch * Rendering.MaxNumMeshesPerDrawCall, length: batchSize),
-                dst: this._colours[batch],
-                length: batchSize);
-            
             this._materialPropertyBlocks[batch].SetVectorArray(Rendering.ColourId, this._colours[batch]);
             
-            var sharedRenderingData =
-                EntityManager.GetSharedComponentData<AntSharedRendering>(this._antSharedRenderingQuery.GetSingletonEntity());
-
             Graphics.DrawMeshInstanced(
-                sharedRenderingData.Mesh, 
-                submeshIndex: 0, 
-                material: sharedRenderingData.Material, 
-                matrices: this._matrices[batch],
-                count:batchSize,
-                this._materialPropertyBlocks[batch]);
+                antRenderer.Mesh,
+                submeshIndex:0,
+                antRenderer.Material,
+                this._matrices[batch],
+                count: batchSize,
+                this._materialPropertyBlocks[batch]
+            );
         }
-
-        localToWorlds.Dispose();
-        colours.Dispose();
-        
         return default;
     }
 
@@ -100,6 +112,26 @@ public class RenderAntSystem : JobComponentSystem
             this._materialPropertyBlocks.Add(propertyBlock);
 
             propertyBlock.SetVectorArray(nameID: Rendering.ColourId, batchColors);
+        }
+    }
+
+    [BurstCompile]
+    private unsafe struct Job : IJobForEachWithEntity<LocalToWorld, Colour>
+    {
+        [NativeDisableUnsafePtrRestriction] public Matrix4x4** Matrices;
+        [NativeDisableUnsafePtrRestriction] public Vector4** Colours;
+        
+        public void Execute(
+            Entity entity, 
+            int index,
+            [ReadOnly] ref LocalToWorld localToWorld,
+            [ReadOnly] ref Colour colour)
+        {
+            int batch = index / Rendering.MaxNumMeshesPerDrawCall;
+            int modulo = index % Rendering.MaxNumMeshesPerDrawCall;
+            
+            this.Matrices[batch][modulo] = localToWorld.Value;
+            this.Colours[batch][modulo] = colour.Value;
         }
     }
 }
