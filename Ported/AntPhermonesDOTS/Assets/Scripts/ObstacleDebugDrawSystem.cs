5da64910441bcb73b56fd7e0c44831e511933114
Kasper Storm Engelstoft <kaspere@unity3d.com>
2019-12-10 17:08:56 +0100

    debug drawing of obstacles

---
+++ Ported/AntPhermonesDOTS/Assets/Scripts/ObstacleDebugDrawSystem.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[ExecuteAlways]
+[UpdateInGroup(typeof(PresentationSystemGroup))]
+public class ObstacleDebugDrawSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((Entity e, ref ObstacleSet obstacles) =>
+        {
+            var radius = obstacles.blob.Value.radius;
+            var positions = obstacles.blob.Value.positions;
+
+            for (int i = 0; i < positions.Length; ++i)
+            {
+                var position = positions[i];
+                Gizmos.DrawWireSphere(position, radius);
+            }
+        });
+    }
+}
