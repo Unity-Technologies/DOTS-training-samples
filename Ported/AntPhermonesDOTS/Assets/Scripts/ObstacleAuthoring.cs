d085f74eb7535c23970b8f3da8172ebce9f81334
rasmusn <rasmusn@unity3d.com>
2019-12-10 16:38:13 +0100

    Add skeleton code for Obstacle blob creation

--- Ported/AntPhermonesDOTS/Assets/Scripts/ObstacleAuthoring.cs
+++ Ported/AntPhermonesDOTS/Assets/Scripts/ObstacleAuthoring.cs
@@ -1,11 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
+using Unity.Collections;
 
-public class ObstacleAuthoring : MonoBehaviour
+struct ObstacleBlobAsset
+{
+    public float radius;
+    public BlobArray<float3> positions;
+}
+
+struct ObstacleSet : IComponentData
+{
+    public BlobAssetReference<ObstacleBlobAsset> blob;
+}
+
+[RequiresEntityConversion]
+[ConverterVersion("kasperrasmus", 1)]
+public class ObstacleAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
     public int obstacleRingCount;
     [Range(0f,1f)]
     public float obstaclesPerRing;
     public float obstacleRadius;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var builder = new BlobBuilder(Allocator.Temp);
+        ref var root = ref builder.ConstructRoot<ObstacleBlobAsset>();
+
+        var blobReference = builder.CreateBlobAssetReference<ObstacleBlobAsset>(Allocator.Persistent);
+
+        dstManager.AddComponentData(entity, new ObstacleSet
+        {
+            blob = blobReference
+        });
+    }
 }
