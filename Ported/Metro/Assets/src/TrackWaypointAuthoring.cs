28e0e91733cd2f1fb5f2bab180a749092fefcbf4
Martin Tilo Schmitz <martinsch@unity3d.com>
2019-12-10 13:43:52 +0100

    added waypoints

---
+++ Ported/Metro/Assets/src/TrackWaypointAuthoring.cs
@@ -0,0 +1,54 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class TrackWaypointAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    // Add fields to your component here. Remember that:
+    //
+    // * The purpose of this class is to store data for authoring purposes - it is not for use while the game is
+    //   running.
+    // 
+    // * Traditional Unity serialization rules apply: fields must be public or marked with [SerializeField], and
+    //   must be one of the supported types.
+    //
+    // For example,
+    //    public float scale;
+    
+    
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        // Call methods on 'dstManager' to create runtime components on 'entity' here. Remember that:
+        //
+        // * You can add more than one component to the entity. It's also OK to not add any at all.
+        //
+        // * If you want to create more than one entity from the data in this class, use the 'conversionSystem'
+        //   to do it, instead of adding entities through 'dstManager' directly.
+        //
+        // For example,
+        //   dstManager.AddComponentData(entity, new Unity.Transforms.Scale { Value = scale });
+        dstManager.AddComponentData(entity, new Unity.Transforms.Translation { Value = transform.position });
+        dstManager.AddComponentData(entity, new TrackWaypoint { });
+
+    }
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        for (int i = 0; i < transform.parent.childCount; i++)
+        {
+            if (transform.parent.GetChild(i) == transform)
+            {
+                Gizmos.DrawSphere(transform.position, 0.1f);
+                if (i+1 < transform.parent.childCount)
+                {
+                    Gizmos.DrawLine(transform.position, transform.parent.GetChild(i+1).position);
+                    break;
+                }
+            }
+        }
+    }
+#endif
+}
