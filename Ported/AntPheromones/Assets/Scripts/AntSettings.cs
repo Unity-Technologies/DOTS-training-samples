13bc8131427abeb2476708cd296df2faafac1825
vlad-andreev <vlad-andreev@users.noreply.github.com>
2019-12-10 13:55:57 +0100

    add settings + conversion

--- Ported/AntPheromones/Assets/Scripts/AntSettings.cs
+++ Ported/AntPheromones/Assets/Scripts/AntSettings.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.Entities;
 using UnityEngine;
 
-public class Settings : MonoBehaviour
+[GenerateAuthoringComponent]
+public struct AntSettings : IComponentData
 {
-	public Material basePheromoneMaterial;
-	public Renderer pheromoneRenderer;
-	public Material antMaterial;
-	public Material obstacleMaterial;
-	public Material resourceMaterial;
-	public Material colonyMaterial;
-	public Mesh antMesh;
-	public Mesh obstacleMesh;
-	public Mesh colonyMesh;
-	public Mesh resourceMesh;
+	//public Material basePheromoneMaterial;
+	//public Renderer pheromoneRenderer;
+	//public Material antMaterial;
+	//public Material obstacleMaterial;
+	//public Material resourceMaterial;
+	//public Material colonyMaterial;
+	//public Mesh antMesh;
+	//public Mesh obstacleMesh;
+	//public Mesh colonyMesh;
+	//public Mesh resourceMesh;
 	public Color searchColor;
 	public Color carryColor;
 	public int antCount;
-	public int mapSize = 128;
+	[DefaultValue(128)]
+	public int mapSize;
 	public int bucketResolution;
 	public Vector3 antSize;
 	public float antSpeed;
@@ -32,7 +36,8 @@ public class Settings : MonoBehaviour
 	public float goalSteerStrength;
 	public float outwardStrength;
 	public float inwardStrength;
-	public int rotationResolution = 360;
+	[DefaultValue(360)]
+	public int rotationResolution;
 	public int obstacleRingCount;
 	[Range(0f, 1f)]
 	public float obstaclesPerRing;
---
+++ Ported/AntPheromones/Assets/Scripts/InitializeSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+public class InitializeSystem : JobComponentSystem
+{
+    protected override void OnCreate()
+    {
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        inputDeps.Complete();
+
+        AntSettings settings = GetSingleton<AntSettings>();
+
+        return new JobHandle();
+    }
+
+}
