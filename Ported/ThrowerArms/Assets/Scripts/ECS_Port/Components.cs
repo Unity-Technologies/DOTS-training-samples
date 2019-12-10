62b569d6a5598487ee4e71e998b8f12775abb54f
MiaoYu-Goh <57358879+MiaoYu-Goh@users.noreply.github.com>
2019-12-10 12:59:40 +0100

    WIP: Creating components

---
+++ Ported/ThrowerArms/Assets/Scripts/ECS_Port/Components.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public struct MovingTag : IComponentData
+{
+}
+
+public struct FallingTag : IComponentData
+{
+}
+
+public struct GrabbedTag : IComponentData
+{
+}
+
+public struct Diameter : IComponentData
+{
+    public float Value;
+}
+
+public struct Velocity : IComponentData
+{
+    public float3 Value;
+}
+
+public struct AngularVelocity : IComponentData
+{
+    public float3 Value;
+}
+
+public struct TinCanRendering : ISharedComponentData, IEquatable<TinCanRendering>
+{
+    public Material Material;
+    public Mesh Mesh;
+    public float Height;
+    public float Diameter;
+
+    public bool Equals(TinCanRendering other)
+    {
+        return Equals(Material, other.Material)
+               && Equals(Mesh, other.Mesh)
+               && Height.Equals(other.Height) 
+               && Diameter.Equals(other.Diameter);
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is TinCanRendering other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            var hashCode = Material != null ? Material.GetHashCode() : 0;
+            hashCode = (hashCode * 397) ^ (Mesh != null ? Mesh.GetHashCode() : 0);
+            hashCode = (hashCode * 397) ^ Height.GetHashCode();
+            hashCode = (hashCode * 397) ^ Diameter.GetHashCode();
+            return hashCode;
+        }
+    }
+}
