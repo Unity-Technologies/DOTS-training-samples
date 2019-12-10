8b4640ca773c5d43bcd35af260dfb15a641ef040
Martin Tilo Schmitz <martinsch@unity3d.com>
2019-12-10 15:01:28 +0100

    line creation

---
+++ Ported/Metro/Assets/src/Line.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+[InternalBufferCapacity(64)]
+public struct LinePositionBufferElement : IBufferElementData
+{
+    public float3 Value;
+
+    public static implicit operator float3(LinePositionBufferElement e)
+    {
+        return e.Value;
+    }
+
+    public static implicit operator LinePositionBufferElement(float3 e)
+    {
+        return new LinePositionBufferElement { Value = e };
+    }
+}
+
+[Serializable]
+public struct Line : IComponentData
+{
+    // Add fields to your component here. Remember that:
+    //
+    // * A component itself is for storing data and doesn't 'do' anything.
+    //
+    // * To act on the data, you will need a System.
+    //
+    // * Data in a component must be blittable, which means a component can
+    //   only contain fields which are primitive types or other blittable
+    //   structs; they cannot contain references to classes.
+    //
+    // * You should focus on the data structure that makes the most sense
+    //   for runtime use here. Authoring Components will be used for 
+    //   authoring the data in the Editor.
+    
+    
+}
---
+++ Ported/Metro/Assets/src/LineAuthoring.cs
@@ -0,0 +1,77 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class LineAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    // Add fields to your component here. Remember that:
+    //
+    // * The purpose of this class is to store data for authoring purposes - it is not for use while the game is
+    //   running.
+    // 
+    // * Traditional Unity serialization rules apply: fields must be public or marked with [SerializeField], and
+    //   must be one of the supported types.
+    //
+    // For example,
+    //    public float scale;
+
+    const float k_StepSize = 0.1f;
+
+    Entity m_Entity;
+    EntityManager m_EntityManager;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        // Call methods on 'dstManager' to create runtime components on 'entity' here. Remember that:
+        //
+        // * You can add more than one component to the entity. It's also OK to not add any at all.
+        //
+        // * If you want to create more than one entity from the data in this class, use the 'conversionSystem'
+        //   to do it, instead of adding entities through 'dstManager' directly.
+        //
+        // For example,
+        //   dstManager.AddComponentData(entity, new Unity.Transforms.Scale { Value = scale });
+
+        var buffer = dstManager.AddBuffer<LinePositionBufferElement>(entity);
+        var currentWaypointIdx = 1;
+        float3 currentPosition = transform.GetChild(0).position;
+        buffer.Add(currentPosition);
+        while (currentWaypointIdx < transform.childCount)
+        {
+            var nextWaypoint = transform.GetChild(currentWaypointIdx);
+            var diff = (float3)nextWaypoint.position - currentPosition;
+            while(math.length(diff) < k_StepSize)
+            {
+                if (++currentWaypointIdx < transform.childCount)
+                {
+                    nextWaypoint = transform.GetChild(currentWaypointIdx);
+                    diff = (float3)nextWaypoint.position - currentPosition;
+                }
+                else
+                    return;
+            }
+            var step = math.normalize(diff) * k_StepSize;
+            currentPosition += step;
+            buffer.Add(currentPosition);
+        }
+        dstManager.AddComponentData(entity, new Line{ });
+        m_Entity = entity;
+        m_EntityManager = dstManager;
+    }
+
+    //void Update()
+    //{
+    //    m_EntityManager = World.Active.EntityManager;
+    //    if (m_EntityManager != null)
+    //    {
+    //        m_Entity = m_EntityManager.CreateEntityQuery(typeof(Line)).ToEntityArray(Unity.Collections.Allocator.TempJob)[0];
+    //        var buffer = m_EntityManager.GetBuffer<LinePositionBufferElement>(m_Entity);
+    //        for (int i = 0; i <  buffer.Length-1; i++)
+    //        {
+    //            Debug.DrawLine(buffer[i].Value, buffer[i + 1].Value,Color.red);
+    //        }
+    //    }
+    //}
+}
