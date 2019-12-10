f5168203fe9e296a9634972595150000b2e2cb66
Alexandru Busnita <alexandru@unity3d.com>
2019-12-10 16:26:22 +0100

    added debug system

---
+++ Ported/Metro/Assets/src/LineDebugDrawSystem.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+
+public class LineDebugDrawSystem : ComponentSystem
+{
+    EntityQuery query;
+    protected override void OnCreate()
+    {
+        query = EntityManager.CreateEntityQuery(typeof(Line));
+    }
+
+    protected override void OnUpdate()
+    {
+        var buffer = EntityManager.GetBuffer<LinePositionBufferElement>(query.GetSingletonEntity());
+
+        if (buffer.IsCreated)
+        {
+            for(int i = 0; i < buffer.Length - 1; ++i)
+            {
+                float3 pos = buffer[i];
+                float3 nextPos = buffer[i + 1];
+
+                Debug.DrawLine(pos, nextPos, Color.red);
+            }
+        }
+
+
+    }
+}
