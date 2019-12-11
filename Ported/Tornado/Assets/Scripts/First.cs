c157a26ff35b4e26087717e4611d346719482296
Steen Lund <steenl@unity3d.com>
2019-12-11 09:06:18 +0100

    Startup porting

---
+++ Ported/Tornado/Assets/Scripts/First.cs
@@ -0,0 +1,58 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+[InternalBufferCapacity(4)]
+struct ConstrainedPoint : IBufferElementData
+{
+    float3 position;
+    int neightbours;
+}
+
+struct ConstrainedBar : IComponentData
+{
+    int p1Index, p2Index;
+    float lenght;
+    Color color;
+}
+
+Entity {
+DynamicBuffer<ConstrainedPoint> points;
+dynamicbuffer<ConstrainedBar> bars;
+}
+
+
+
+struct FreeBar : IComponentData
+{
+    float3 p1;
+    float3 p2;
+    float lenght;
+    Color color;
+}
+
+float3[] freePoinst;
+
+struct FreeBar : IComponentData
+{
+    float lenght;
+    Color color;
+}
+
+FreeBar[] freeBars;
+
+
+class TornadoSystem : JobComponentSystem
+{
+    int anchorsEndIndex;
+
+    
+    
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+
+        Entities.WithName().
+        return inputDeps;
+    }
+}
