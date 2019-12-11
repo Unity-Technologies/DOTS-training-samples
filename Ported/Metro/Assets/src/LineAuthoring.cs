fe5b53e483e66982e2151fbb8552f2ad97183dc8
Christian Warnecke <christianw@unity3d.com>
2019-12-11 15:34:48 +0100

    Bezier line segments generated

--- Ported/Metro/Assets/src/LineAuthoring.cs
+++ Ported/Metro/Assets/src/LineAuthoring.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using src;
 using Unity.Collections;
 using Unity.Entities;
@@ -10,21 +11,12 @@ using UnityEngine;
 [ConverterVersion("martinsch", 8)]
 public class LineAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
-    // Add fields to your component here. Remember that:
-    //
-    // * The purpose of this class is to store data for authoring purposes - it is not for use while the game is
-    //   running.
-    // 
-    // * Traditional Unity serialization rules apply: fields must be public or marked with [SerializeField], and
-    //   must be one of the supported types.
-    //
-    // For example,
-    //    public float scale;
-
     [SerializeField]
     int m_LineIndex = 0;
 
     public const float k_StepSize = 0.1f;
+    [Range(0.001f, 0.5f)]
+    public float BezierFactor = 0.5f;
 
     Entity m_Entity;
     EntityManager m_EntityManager;
@@ -33,89 +25,43 @@ public class LineAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     public Color RailMarkerColor = Color.white;
     public Vector3 RailMarkerScale = new Vector3(0.1f, 1f, 0.025f);
 
-
-    struct GenerationStep
-    {
-        public GenerationStep(int nextWaypointIdx, Transform currentWaypoint)
-        {
-            NextWaypointIdx = nextWaypointIdx;
-            CurrentWaypoint = currentWaypoint;
-            NextWaypoint = currentWaypoint.parent.GetChild(nextWaypointIdx);
-            FullDistanceBetweenWaypoints = NextWaypoint.position - currentWaypoint.position;
-        }
-
-        public readonly int NextWaypointIdx;
-        public readonly Transform CurrentWaypoint;
-        public readonly Transform NextWaypoint;
-        public readonly float3 FullDistanceBetweenWaypoints;
-
-        public bool HasNext
-        {
-            get
-            {
-                return CurrentWaypoint.parent.childCount > NextWaypointIdx + 1;
-            }
-        }
-
-        public GenerationStep Next
-        {
-            get {
-                return new GenerationStep(NextWaypointIdx + 1, NextWaypoint);
-            }
-        }
-    }
-
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
-        // Call methods on 'dstManager' to create runtime components on 'entity' here. Remember that:
-        //
-        // * You can add more than one component to the entity. It's also OK to not add any at all.
-        //
-        // * If you want to create more than one entity from the data in this class, use the 'conversionSystem'
-        //   to do it, instead of adding entities through 'dstManager' directly.
-        //
-        // For example,
-        //   dstManager.AddComponentData(entity, new Unity.Transforms.Scale { Value = scale });
-
         dstManager.AddComponentData(entity, new Line { ID = m_LineIndex });
+
+        var points = gameObject.GetComponentsInChildren<TrackWaypointAuthoring>().Select(wayPoint => wayPoint.transform.position).ToArray();
+        var segments = new NativeArray<LineSegmentBufferElement>(points.Length, Allocator.Temp);
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            var pStartPrev = i > 0 ? points[i - 1] : points[points.Length - 1];
+            var pStart = points[i];
+            var pEnd = i + 1 < points.Length ? points[i + 1] : points[0];
+            var pEndNext = i + 2 < points.Length ? points[i + 2] : points[i + 2 - points.Length];
+
+            var startDir = Vector3.Lerp((pStart - pStartPrev).normalized, (pEnd - pStart).normalized, 0.5f);
+            var endDir = Vector3.Lerp((pEnd - pEndNext).normalized, (pStart - pEnd).normalized, 0.5f);
+
+            var controlPointDistance = Vector3.Magnitude(pEnd - pStart) * BezierFactor;
+
+            segments[i] = new LineSegmentBufferElement()
+            {
+                p0 = pStart,
+                p1 = pStart + startDir * controlPointDistance,
+                p2 = pEnd + endDir * controlPointDistance,
+                p3 = pEnd,
+            };
+        }
+        
+        dstManager.AddBuffer<LineSegmentBufferElement>(entity).CopyFrom(segments);
+        
+        
+        
         var buffer = new List<LinePositionBufferElement>();
         for (int i = 0; i < transform.childCount; i++)
         {
             buffer.Add((float3)transform.GetChild(i).position);
-        }
-        //var generationStep = new GenerationStep(1, transform.GetChild(0));
-        //float3 currentPosition = generationStep.CurrentWaypoint.position;
-        //float3 currentHeading = math.normalize(generationStep.FullDistanceBetweenWaypoints);
-        //buffer.Add(currentPosition);
-
-        //bool done = false;
-        //while (!done)
-        //{
-        //    var diff = (float3)generationStep.NextWaypoint.position - currentPosition;
-        //    while(math.length(diff) < k_StepSize)
-        //    {
-        //        if (generationStep.HasNext)
-        //        {
-        //            generationStep = generationStep.Next;
-        //            currentPosition = generationStep.CurrentWaypoint.position;
-        //            diff = (float3)generationStep.NextWaypoint.position - currentPosition;
-        //        }
-        //        else
-        //        {
-
-        //            done = true;
-        //            break;
-        //        }
-        //    }
-        //    if(!done)
-        //    {
-        //        var t = 1 - (math.length(diff) - k_StepSize) / math.length(generationStep.FullDistanceBetweenWaypoints);
-        //        currentHeading = math.normalize(math.lerp(currentHeading, math.normalize(diff), t));
-        //        var step = currentHeading * k_StepSize;
-        //        currentPosition += step;
-        //        buffer.Add(currentPosition);
-        //    }
-        //}
+        } 
         m_Entity = entity;
         m_EntityManager = dstManager;
         var b = dstManager.AddBuffer<LinePositionBufferElement>(entity);
@@ -154,18 +100,4 @@ public class LineAuthoring : MonoBehaviour, IConvertGameObjectToEntity
             });
         }
     }
-
-    //void Update()
-    //{
-    //    m_EntityManager = World.Active.EntityManager;
-    //    if (m_EntityManager != null)
-    //    {
-    //        m_Entity = m_EntityManager.CreateEntityQuery(typeof(Line)).ToEntityArray(Unity.Collections.Allocator.TempJob)[0];
-    //        var buffer = m_EntityManager.GetBuffer<LinePositionBufferElement>(m_Entity);
-    //        for (int i = 0; i <  buffer.Length-1; i++)
-    //        {
-    //            Debug.DrawLine(buffer[i].Value, buffer[i + 1].Value,Color.red);
-    //        }
-    //    }
-    //}
 }
--- Ported/Metro/Assets/src/LineDebugDrawSystem.cs
+++ Ported/Metro/Assets/src/LineDebugDrawSystem.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Entities;
+﻿using src;
+using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -15,19 +16,37 @@ public class LineDebugDrawSystem : ComponentSystem
 
     protected override void OnUpdate()
     {
-        var buffer = EntityManager.GetBuffer<LinePositionBufferElement>(query.GetSingletonEntity());
+        var buffer = EntityManager.GetBuffer<LineSegmentBufferElement>(query.GetSingletonEntity());
 
-        if (buffer.IsCreated)
+        if (!buffer.IsCreated) return;
+        
+        for(int i = 0; i < buffer.Length - 1; ++i)
         {
-            for(int i = 0; i < buffer.Length - 1; ++i)
+            var element = buffer[i];
+            for (int j = 0; j <= 9; j++)
             {
-                float3 pos = buffer[i];
-                float3 nextPos = buffer[i + 1];
-
-                Debug.DrawLine(pos, nextPos, Color.red);
+                var t = j * 0.1f;
+                var p0 = GetPoint(element.p0, element.p1, element.p2, element.p3, t);
+                var p1 = GetPoint(element.p0, element.p1, element.p2, element.p3, t + 0.1f);
+                Debug.DrawLine(p0, p1, Color.red);
             }
+            
         }
 
 
+    }
+
+    private static float3 GetPoint(float3 p0, float3 p1, float3 p2, float3 p3, float t)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
+        float3 p = uuu * p0;
+        p += 3 * uu * t * p1;
+        p += 3 * u * tt * p2;
+        p += ttt * p3;
+        return p;
     }
 }
