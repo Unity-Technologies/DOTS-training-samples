84f313a10780afe435db59880f4e3a0b28233d7f
Kasper Storm Engelstoft <kaspere@unity3d.com>
2019-12-11 13:38:44 +0100

    implement colony instantiation

---
+++ Ported/AntPhermonesDOTS/Assets/Scripts/ColonyAuthoring.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class ColonyAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+    public GameObject prefab;
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+        referencedPrefabs.Add(prefab);
+    }
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new Colony
+        {
+            Prefab = conversionSystem.GetPrimaryEntity(prefab)
+        });
+    }
+}
---
+++ Ported/AntPhermonesDOTS/Assets/Scripts/ColonySystem.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+public class ColonySystem : JobComponentSystem
+{
+    BeginInitializationEntityCommandBufferSystem EntityCommandBufferSystem;
+    
+    protected override void OnCreate()
+    {
+        EntityCommandBufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+    }
+    
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        inputDeps.Complete();
+        
+        var commandBuffer = EntityCommandBufferSystem.CreateCommandBuffer();
+        //Map map = GetSingleton<Map>();
+        Entity entity = GetSingletonEntity<Colony>();
+        Colony colony = GetSingleton<Colony>();
+
+        // Place the colony prefab in the center.
+        var instance = commandBuffer.Instantiate(colony.Prefab);
+        commandBuffer.SetComponent(instance, new Translation {Value = 0.0f});
+        commandBuffer.DestroyEntity(entity);
+
+        return default;
+    }
+}
