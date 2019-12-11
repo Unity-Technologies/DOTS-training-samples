72fe81cc9d0aa9360d3fdaf1aa7bdc793523ec13
MiaoYu-Goh <57358879+MiaoYu-Goh@users.noreply.github.com>
2019-12-11 13:16:01 +0100

    Spawn arms with IdleState, Finger, and FindGrabbableTargetState.

---
+++ Ported/ThrowerArms/Assets/Scripts/ECS_Port/Systems/ReachForTargetSystem.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+namespace ECS_Port.Systems
+{
+    public class ReachForTargetSystem : JobComponentSystem
+    {
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
