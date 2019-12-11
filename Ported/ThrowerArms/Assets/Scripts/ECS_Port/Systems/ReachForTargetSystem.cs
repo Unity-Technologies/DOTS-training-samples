19691554925106887c7c054d0c36648187f24327
Shanee Nishry <shanee.nishry@unity3d.com>
2019-12-11 15:27:44 +0100

    Change ArmIK to use concurrent commander buffer and add grabbing state.

--- Ported/ThrowerArms/Assets/Scripts/ECS_Port/Systems/ReachForTargetSystem.cs
+++ Ported/ThrowerArms/Assets/Scripts/ECS_Port/Systems/ReachForTargetSystem.cs
@@ -2,62 +2,63 @@
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
 
 public class ReachForTargetSystem : JobComponentSystem
-{
-    private const float k_deltaTime = 1 / 50f;
-    
+{   
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
+        float deltaTime = Time.DeltaTime;
         var endSimulationEcbSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
         EntityCommandBuffer ecb = endSimulationEcbSystem.CreateCommandBuffer();
-        
+        EntityCommandBuffer.Concurrent concurrentBuffer = ecb.ToConcurrent();
+
         var translationFromEntityAccessor = GetComponentDataFromEntity<Translation>();
         
-        return Entities.WithName("TryReachForTargetRock")
+        JobHandle reachForRockJob = Entities.WithName("TryReachForTargetRock")
             .ForEach((Entity entity, int entityInQueryIndex, ref Translation translation, ref ReachForTargetState reachingState, ref ArmComponent arm) =>
         {
             float3 desiredRockTranslation = translationFromEntityAccessor[reachingState.TargetEntity].Value;
             float3 delta = desiredRockTranslation - translation.Value;
 
-            bool reachedRockSuccessfully = math.lengthsq(delta) < math.lengthsq(ArmConstants.MaxReach);
-            
-            if (reachedRockSuccessfully)
+            bool rockIsReachable = math.lengthsq(delta) < math.lengthsq(ArmConstants.MaxReach);
+            if (!rockIsReachable)
             {
-                float3 flatDelta = delta;
-                flatDelta.y = 0;
+                concurrentBuffer.AddComponent(entityInQueryIndex, reachingState.TargetEntity, new GrabbableState());
+                concurrentBuffer.AddComponent(entityInQueryIndex, entity, new IdleState());
+                concurrentBuffer.AddComponent(entityInQueryIndex, entity, new FindGrabbableTargetState());
+                concurrentBuffer.RemoveComponent<ReachForTargetState>(entityInQueryIndex, entity);
+                return;
+            }
+
+            float3 flatDelta = delta;
+            flatDelta.y = 0;
             
-                flatDelta = math.normalize(flatDelta);
+            flatDelta = math.normalize(flatDelta);
 
-                reachingState.HandTarget =
-                    desiredRockTranslation + new float3(0, 1, 0) * reachingState.TargetSize * 0.5f - flatDelta * reachingState.TargetSize * 0.5f;
-                reachingState.ReachTimer += k_deltaTime / ArmConstants.ReachDuration;
+            reachingState.HandTarget =
+                desiredRockTranslation + new float3(0, 1, 0) * reachingState.TargetSize * 0.5f - flatDelta * reachingState.TargetSize * 0.5f;
+            reachingState.ReachTimer += deltaTime / ArmConstants.ReachDuration;
 
-                if (reachingState.ReachTimer < 1f)
-                {
-                    return;
-                }
-                
-                ecb.RemoveComponent<ReachForTargetState>(entity);
-                
-                ecb.AddComponent(entity, new LookForThrowTargetState
-                {
-                    GrabbedEntity = reachingState.TargetEntity
-                });
-                ecb.AddComponent(entity, new GrabbingState
-                {
-                    GrabbedEntity = reachingState.TargetEntity
-                });
-            }
-            else
+            if (reachingState.ReachTimer < 1f)
             {
-                ecb.AddComponent(reachingState.TargetEntity, new GrabbableState());
-                
-                ecb.RemoveComponent<ReachForTargetState>(entity);
-                
-                ecb.AddComponent(entity, new IdleState());
-                ecb.AddComponent(entity, new FindGrabbableTargetState());
+                return;
             }
+
+            concurrentBuffer.RemoveComponent<ReachForTargetState>(entityInQueryIndex, entity);
+
+            concurrentBuffer.AddComponent(entityInQueryIndex, entity, new LookForThrowTargetState
+            {
+                GrabbedEntity = reachingState.TargetEntity
+            });
+            concurrentBuffer.AddComponent(entityInQueryIndex, reachingState.TargetEntity, new GrabbedState
+            {
+                GrabbingEntity = entity
+            });
+            concurrentBuffer.AddComponent<GrabbingState>(entityInQueryIndex, entity);
         }).Schedule(inputDeps);
+        endSimulationEcbSystem.AddJobHandleForProducer(reachForRockJob);
+
+        return reachForRockJob;
     }
 }
