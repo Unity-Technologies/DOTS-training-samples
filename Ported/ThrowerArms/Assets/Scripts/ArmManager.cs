103b9746df6f85b37067aa1eaa326f99fbae7df0
Kim Steen Riber <kimsteenriber@gmail.com>
2019-12-11 13:23:47 +0100

    Arm windup

---
+++ Ported/ThrowerArms/Assets/Scripts/ArmManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmManager : MonoBehaviour {
+	public GameObject armPrefab;
+	public int count;
+	public float spacing;
+
+	public static float armRowWidth;
+
+	void Awake () {
+		armRowWidth = (count - 1) * spacing;
+		for (int i=0;i<count;i++) {
+			Instantiate(armPrefab,Vector3.right * i * spacing,Quaternion.identity);
+		}
+	}
+}
--- Ported/ThrowerArms/Assets/Scripts/ECS_Port/Components/States/WindingUpState.cs
+++ Ported/ThrowerArms/Assets/Scripts/ECS_Port/Components/States/WindingUpState.cs
@@ -1,8 +1,10 @@
 ﻿using Unity.Entities;
+using Unity.Mathematics;
 
 public struct WindingUpState : IComponentData
 {
     public float WindupTimer;
+    public float3 HandTarget;
     public Entity AimedTargetEntity;
     public Entity HeldEntity;
 }
