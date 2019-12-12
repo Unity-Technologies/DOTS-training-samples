9e7bfe72882153c6f86ed789144942ed45f92944
rasmusn <rasmusn@unity3d.com>
2019-12-12 10:39:03 +0100

    Add tileCount and tileSize

--- Ported/AntPhermonesDOTS/Assets/Scripts/MapAuthoring.cs
+++ Ported/AntPhermonesDOTS/Assets/Scripts/MapAuthoring.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 
 public class MapAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
+    // TODO: Enforce power of 2 map size.
     public int mapSize = 128;
+    public int tileCount = 8;
+    
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
         dstManager.AddComponentData(entity, new Map
         {
-            Size = mapSize
+            Size = mapSize,
+            TileSize = mapSize / tileCount
         });
     }
 }
