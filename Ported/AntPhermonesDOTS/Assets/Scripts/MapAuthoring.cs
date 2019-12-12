a270b6eec0cdbc83275de7a7e145924f3da3ab7a
rasmusn <rasmusn@unity3d.com>
2019-12-12 16:12:54 +0100

    Set tileCount to 32

--- Ported/AntPhermonesDOTS/Assets/Scripts/MapAuthoring.cs
+++ Ported/AntPhermonesDOTS/Assets/Scripts/MapAuthoring.cs
@@ -5,7 +5,7 @@ public class MapAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
     // TODO: Enforce power of 2 map size.
     public int mapSize = 128;
-    public int tileCount = 8;
+    public int tileCount = 32;
     
     [Range(0f,1f)]
     public float trailDecay = 0.1f;
