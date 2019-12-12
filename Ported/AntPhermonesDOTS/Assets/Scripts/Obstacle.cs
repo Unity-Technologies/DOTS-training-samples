75bac4680dfe3526f74a39fcd44323b830944435
rasmusn <rasmusn@unity3d.com>
2019-12-12 16:04:38 +0100

    Add Obstacle helpers

--- Ported/AntPhermonesDOTS/Assets/Scripts/Obstacle.cs
+++ Ported/AntPhermonesDOTS/Assets/Scripts/Obstacle.cs
@@ -13,3 +13,28 @@ struct Obstacle : IComponentData
     public Entity Prefab;
     public BlobAssetReference<ObstacleBlobAsset> Blob;
 }
+
+public static class ObstacleHelper
+{
+    const int BitFieldSize = 64;
+    
+    public static int2 WorldPosToTilePos(float2 position, int mapSize, int tileSize) {
+        var discretePosition = new int2((position.xy + mapSize * 0.5f) / new float2(tileSize, tileSize));
+        return discretePosition;
+    }
+    
+    public static int TileIndex(int2 tilePos, int tileCount)
+    {
+        return tilePos.x * tileCount + tilePos.y;
+    }
+
+    public static int BitFieldIndex(int tileIndex)
+    {
+        return tileIndex / BitFieldSize;
+    }
+    
+    public static int BitIndex(int tileIndex)
+    {
+        return tileIndex % BitFieldSize;
+    }
+}
