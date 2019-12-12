d71969884f0e293c97af72393bce89f2cdac07c6
rasmusn <rasmusn@unity3d.com>
2019-12-12 10:51:23 +0100

    Move obstacle position generation to conversion/blobification

--- Ported/AntPhermonesDOTS/Assets/Scripts/ObstacleAuthoring.cs
+++ Ported/AntPhermonesDOTS/Assets/Scripts/ObstacleAuthoring.cs
@@ -4,9 +4,10 @@ using Unity.Mathematics;
 using UnityEngine;
 using Unity.Collections;
 using UnityEngine.Serialization;
+using Random = Unity.Mathematics.Random;
 
 [RequiresEntityConversion]
-[ConverterVersion("kasperrasmus", 2)]
+[ConverterVersion("kasperrasmus", 3)]
 public class ObstacleAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
 {
     [FormerlySerializedAs("Prefab")] public GameObject prefab;
@@ -21,20 +22,43 @@ public class ObstacleAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDec
     }
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
+        var mapSize = 128; // TODO: Fetch proper value.
+        
         var builder = new BlobBuilder(Allocator.Temp);
         ref var root = ref builder.ConstructRoot<ObstacleBlobAsset>();
 
         root.Radius = obstacleRadius;
         root.RingCount = obstacleRingCount;
         root.ObstaclesPerRing = obstaclesPerRing;
-        var positionCount = 4;
-        var posArray = builder.Allocate(ref root.Positions, 4);
-        for (var i = 0; i < positionCount; ++i)
+        
+        var RNG = new Random(1);
+        
+        var worldOffset = new float3(1, 1, 0) * ((float)mapSize / -2.0f);
+        var positionList = new List<float3>();
+        for (var i = 1; i < obstacleRingCount; i++)
         {
-            // TODO move the position generation code from ObstacleSystem here.
-            posArray[i] = new float3((float)i, (float)i, (float)i);
+            float ringRadius = (i / (obstacleRingCount+1f)) * (mapSize * .5f);
+            float circumference = ringRadius * 2f * math.PI;
+            int maxCount = (int)math.ceil(circumference / (2f * obstacleRadius) * 2f);
+            int offset = RNG.NextInt(0,maxCount);
+            int holeCount = RNG.NextInt(1,3);
+            for (int j = 0; j < maxCount; j++)
+            {
+                float t = (float)j / maxCount;
+                if ((t * holeCount)%1f < obstaclesPerRing)
+                {
+                    float angle = (j + offset) / (float)maxCount * (2f * math.PI);
+                    positionList.Add(worldOffset + new float3(mapSize * .5f + math.cos(angle) * ringRadius,
+                                     mapSize * .5f + math.sin(angle) * ringRadius, 0f));
+                }
+            }
         }
-
+        
+        var blobPosArray = builder.Allocate(ref root.Positions, positionList.Count);
+        for (var i = 0; i < positionList.Count; ++i)
+        {
+            blobPosArray[i] = positionList[i];
+        }
         var blobReference = builder.CreateBlobAssetReference<ObstacleBlobAsset>(Allocator.Persistent);
         builder.Dispose();
 
--- Ported/AntPhermonesDOTS/Assets/Scripts/ObstacleSystem.cs
+++ Ported/AntPhermonesDOTS/Assets/Scripts/ObstacleSystem.cs
@@ -21,38 +21,19 @@ public class ObstacleSystem : JobComponentSystem
         Map map = GetSingleton<Map>();
         var RNG = new Random(1); // TODO not deterministic across threads?
         
-        // Place the instantiated prefabs in concentric circles with holes.
+        // Place the instantiated prefabs in pre-calculated concentric circles with holes.
         var jobHandle = Entities
             .WithName("ObstacleSystem")
             .WithBurst(FloatMode.Default, FloatPrecision.Standard, true)
             .ForEach((Entity entity, int entityInQueryIndex, in Obstacle obstacle) =>
             {
-                var worldOffset = new float3(1, 1, 0) * ((float)map.Size / -2.0f);
-                
-                var obstacleRingCount = obstacle.Blob.Value.RingCount;
-                var obstacleRadius = obstacle.Blob.Value.Radius;
-                var obstaclesPerRing = obstacle.Blob.Value.ObstaclesPerRing;
-                for (var i = 1; i < obstacleRingCount; i++)
+                var posArray = obstacle.Blob.Value.Positions.ToArray();
+                foreach (var position in posArray)
                 {
-                    float ringRadius = (i / (obstacleRingCount+1f)) * (map.Size * .5f);
-                    float circumference = ringRadius * 2f * math.PI;
-                    int maxCount = (int)math.ceil(circumference / (2f * obstacleRadius) * 2f);
-                    int offset = RNG.NextInt(0,maxCount);
-                    int holeCount = RNG.NextInt(1,3);
-                    for (int j = 0; j < maxCount; j++)
-                    {
-                        float t = (float)j / maxCount;
-                        if ((t * holeCount)%1f < obstaclesPerRing)
-                        {
-                            float angle = (j + offset) / (float)maxCount * (2f * math.PI);
-                            var position = worldOffset + new float3(map.Size * .5f + math.cos(angle) * ringRadius,map.Size * .5f + math.sin(angle) * ringRadius, 0f);
-                            var instance = commandBuffer.Instantiate(entityInQueryIndex, obstacle.Prefab);
-                            commandBuffer.SetComponent(entityInQueryIndex, instance, new Translation {Value = position});
-                        }
-                    }
+                    var instance = commandBuffer.Instantiate(entityInQueryIndex, obstacle.Prefab);
+                    commandBuffer.SetComponent(entityInQueryIndex, instance, new Translation {Value = position});
                 }
-                commandBuffer.DestroyEntity(entityInQueryIndex, entity);
-        }).Schedule(inputDeps);
+            }).Schedule(inputDeps);
 
         EntityCommandBufferSystem.AddJobHandleForProducer(jobHandle);
 
