3e4cb1ff57f9311d48231449211ca63c76533e65
sunek <sunek@unity3d.com>
2019-12-12 13:42:01 +0100

    Bars rendering. Slow but working

--- Ported/Tornado/Assets/Scripts/CalculateMatricesSystem.cs
+++ Ported/Tornado/Assets/Scripts/CalculateMatricesSystem.cs
@@ -1,6 +1,8 @@
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
+using UnityEngine;
 
 [UpdateInGroup(typeof(PresentationSystemGroup))]
 [UpdateBefore(typeof(PushToRenderSystem))]
@@ -8,21 +10,49 @@ public class CalculateMatricesSystem : JobComponentSystem
 {
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        Entities.ForEach( (Entity entity, ref DynamicBuffer<ConstrainedPointEntry> points, ref DynamicBuffer<RenderMatrixEntry> matricies) =>
+//        // Cubes
+//        Entities.ForEach( (Entity entity, ref DynamicBuffer<ConstrainedPointEntry> points, ref DynamicBuffer<RenderMatrixEntry> matricies) =>
+//        {
+//            if (matricies.Length != points.Length)
+//            {
+//                matricies.ResizeUninitialized(points.Length);
+//            }
+//
+//            for (var i = 0; i < points.Length; i++)
+//            {
+//                matricies[i] = new RenderMatrixEntry {Value = float4x4.Translate(points[i].Value.position)};
+//            }
+//
+//        }).Run();
+
+
+        // Bars
+        var pointsEntity = GetSingletonEntity<ConstrainedPointEntry>();
+        var points = EntityManager.GetBuffer<ConstrainedPointEntry>(pointsEntity).AsNativeArray();
+        
+        Entities.ForEach( (Entity entity, ref DynamicBuffer<BarEntry> bars, ref DynamicBuffer<RenderMatrixEntry> matricies) =>
         {
-            // TODO: Cache the length of the points and only if different change the render matrix buffer to match
-
-            if (matricies.Length != points.Length)
+            if (matricies.Length != bars.Length)
             {
-                matricies.ResizeUninitialized(points.Length);
+                matricies.ResizeUninitialized(bars.Length);
             }
 
-            for (var i = 0; i < points.Length; i++)
+            for (var i = 0; i < bars.Length; i++)
             {
-                matricies[i] = new RenderMatrixEntry {Value = float4x4.Translate(points[i].Value.position)};
+                var bar = bars[i].Value;
+                var point1 = points[bar.p1].Value.position;
+                var point2 = points[bar.p2].Value.position;
+                var newPoint = (point1 + point2) * 0.5f;
+                var dd = point2 - point1;
+                
+                var matrix = Matrix4x4.TRS(newPoint,
+                    Quaternion.LookRotation(dd),
+                    new Vector3(bar.thickness,bar.thickness,bar.length));
+
+                matricies[i] = new RenderMatrixEntry {Value = matrix};
             }
 
-        }).Run();
+        }).Run();      
         
         return inputDeps;
     }
