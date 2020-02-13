437267838ebae5251d97dd7b1044008fcb0724ab
Paul Melamed <paulm@unity3d.com>
2020-02-13 13:07:47 -0800

    added templates for train positioning system and authoring tool

---
+++ Ported/Metro/Assets/Scripts/PathAuthoringComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class PathAuthoringComponent : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        
+    }
+}
