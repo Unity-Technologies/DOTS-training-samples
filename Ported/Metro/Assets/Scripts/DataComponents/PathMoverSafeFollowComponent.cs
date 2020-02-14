80f25a52690bb6a907ed46ebac98925cf1e336db
pastasfuture <pastasfuture@gmail.com>
2020-02-14 15:50:49 -0800

    InstantiatePathMoversOnLoopPaths()

---
+++ Ported/Metro/Assets/Scripts/DataComponents/PathMoverSafeFollowComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+[GenerateAuthoringComponent]
+public struct PathMoverSafeFollowComponent : IComponentData
+{
+	public Entity m_FollowEntity;
+}
