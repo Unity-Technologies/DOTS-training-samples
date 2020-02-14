f2462831049ee8a7383238779993712581ae314c
Alex Kerfoot <alexk@unity3d.com>
2020-02-14 15:34:44 -0800

    Add Platform and queue authoring and components

---
+++ Ported/Metro/Assets/Scripts/Commuting/PlatformNavPoints.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct PlatformNavPoints : IComponentData
+{
+    public float3 backDown;
+    public float3 backUp;
+    public float3 frontDown;
+    public float3 frontUp;
+}
---
+++ Ported/Metro/Assets/Scripts/Commuting/QueuePosition.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct QueuePosition : IBufferElementData
+{
+    public float3 Value;
+    
+    public static implicit operator float3(QueuePosition e)
+    {
+        return e.Value;
+    }
+    public static implicit operator QueuePosition(float3 e)
+    {
+        return new QueuePosition {Value = e};
+    }
+}
