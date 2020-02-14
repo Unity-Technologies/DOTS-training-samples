80aa544ca77767439c710a4c96a315eb87dd4576
Paul Melamed <paulm@unity3d.com>
2020-02-13 16:56:18 -0800

    create looping for paths

--- Ported/Metro/Assets/Scripts/PathAuthoringComponent.cs
+++ Ported/Metro/Assets/Scripts/PathAuthoringComponent.cs
@@ -7,29 +7,17 @@ using UnityEngine;
 
 public class PathAuthoringComponent : MonoBehaviour, IConvertGameObjectToEntity
 {
-    public List<GameObject> m_PathParents;
+    public List<GameObject> m_ParentsLoopedPaths;
+    public List<GameObject> m_ParentsSinglePaths;
+    public float m_LoopPathOffset = 1;
     private TrainPositioningSystem m_TrainPositioningSytem;
-    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
-    {
-        int totalPositionsCount = 0;
-        for(int i = 0; i < m_PathParents.Count; i++)
+
+    void CreatPathPositionData(ref int currIndex, TrainPositioningSystem trainPositioningSystem, List<GameObject> pathParents, bool isLooped)
+    {     
+        for(int i = 0; i < pathParents.Count; i++)
         {
-            totalPositionsCount += m_PathParents[i].GetComponentsInChildren<RailMarker>().Length;
-        }
-        Debug.Assert(totalPositionsCount > 0);
-        World world = dstManager.World;
-        TrainPositioningSystem trainPositioningSystem = world.GetExistingSystem<TrainPositioningSystem>();
-        Debug.Assert(trainPositioningSystem != null);
-        trainPositioningSystem.m_PathPositions = new NativeArray<float3>(totalPositionsCount, Allocator.Persistent);
-        trainPositioningSystem.m_PathCount = (uint)m_PathParents.Count;
-        trainPositioningSystem.m_StartEndPositionIndicies = new NativeArray<int2>(m_PathParents.Count, Allocator.Persistent);
-        trainPositioningSystem.m_PathStopBits.Allocate(totalPositionsCount, Allocator.Persistent);
-        trainPositioningSystem.m_PathStopBits.ClearAllBits();
-        int currIndex = 0;
-        for(int i = 0; i < m_PathParents.Count; i++)
-        {
-            RailMarker[] railMarkers = m_PathParents[i].GetComponentsInChildren<RailMarker>();
             int2 startEnd;
+            RailMarker[] railMarkers = pathParents[i].GetComponentsInChildren<RailMarker>();            
             startEnd.x = currIndex;
             for (int railMarkerIndex = 0; railMarkerIndex < railMarkers.Length; railMarkerIndex++)
             {
@@ -40,12 +28,59 @@ public class PathAuthoringComponent : MonoBehaviour, IConvertGameObjectToEntity
                 {
                     trainPositioningSystem.m_PathStopBits.SetBit(currIndex);
                 }
-
                 currIndex++;
+            }
+            
+            if(isLooped)
+            {
+                float3 prevTan;
+                float3 currTan;
+                int railMarkerIndex = startEnd.x + railMarkers.Length - 1;                
+                prevTan = math.normalize(math.cross(math.normalize(trainPositioningSystem.m_PathPositions[railMarkerIndex] - trainPositioningSystem.m_PathPositions[railMarkerIndex - 1]), Vector3.up));
+                for (; railMarkerIndex >= startEnd.x; railMarkerIndex--)
+                {
+                    currTan = railMarkerIndex == startEnd.x ? prevTan : math.normalize(math.cross(math.normalize(trainPositioningSystem.m_PathPositions[railMarkerIndex] - trainPositioningSystem.m_PathPositions[railMarkerIndex - 1]), Vector3.up));
+                    float3 offset = math.normalize(currTan + prevTan);
+                    trainPositioningSystem.m_PathPositions[currIndex] = trainPositioningSystem.m_PathPositions[railMarkerIndex] + offset * m_LoopPathOffset;
+                    if(trainPositioningSystem.m_PathStopBits.IsBitSet(railMarkerIndex + 1))
+                        trainPositioningSystem.m_PathStopBits.SetBit(currIndex);
+                    prevTan = currTan;                   
+                    currIndex++;
+                }
             }
             startEnd.y = currIndex;
             trainPositioningSystem.m_StartEndPositionIndicies[i] = startEnd;
         }
+    }
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        int totalPositionsCount = 0;
+        for(int i = 0; i < m_ParentsLoopedPaths.Count; i++)
+        {
+            totalPositionsCount += m_ParentsLoopedPaths[i].GetComponentsInChildren<RailMarker>().Length;
+        }
+        totalPositionsCount *= 2;
+        for(int i = 0; i < m_ParentsSinglePaths.Count; i++)
+        {
+            totalPositionsCount += m_ParentsSinglePaths[i].GetComponentsInChildren<RailMarker>().Length;
+        }
+
+        Debug.Assert(totalPositionsCount > 0);
+        World world = dstManager.World;
+        TrainPositioningSystem trainPositioningSystem = world.GetExistingSystem<TrainPositioningSystem>();
+
+        Debug.Assert(trainPositioningSystem != null);
+        trainPositioningSystem.m_PathPositions = new NativeArray<float3>(totalPositionsCount, Allocator.Persistent);
+        trainPositioningSystem.m_PathCount = m_ParentsLoopedPaths.Count + m_ParentsSinglePaths.Count;
+        trainPositioningSystem.m_SinglePathStartIndex = m_ParentsLoopedPaths.Count;
+        trainPositioningSystem.m_StartEndPositionIndicies = new NativeArray<int2>(trainPositioningSystem.m_PathCount, Allocator.Persistent);
+        trainPositioningSystem.m_PathStopBits.Allocate(totalPositionsCount, Allocator.Persistent);
+        trainPositioningSystem.m_PathStopBits.ClearAllBits();
+
+        int currIndex = 0;
+        CreatPathPositionData(ref currIndex, trainPositioningSystem, m_ParentsLoopedPaths, true);
+        CreatPathPositionData(ref currIndex, trainPositioningSystem, m_ParentsSinglePaths, false);
 
         m_TrainPositioningSytem = trainPositioningSystem;
     }
@@ -57,19 +92,17 @@ public class PathAuthoringComponent : MonoBehaviour, IConvertGameObjectToEntity
             Debug.Log("Not converted");
             return;
         }
-        Debug.Log("totalPositionsCount = " + m_TrainPositioningSytem.m_PathCount);
-
-        //for (int i = 0, iLen = m_TrainPositioningSytem.m_PathPositions.Length - 1; i < iLen; ++i)
-        //for (int i = 0, iLen = m_TrainPositioningSytem.m_StartEndPositionIndicies.Length; i < iLen; ++i)
+        Debug.Log("totalPositionsCount = " + m_TrainPositioningSytem.m_PathCount);     
         for (int i = 0; i < m_TrainPositioningSytem.m_StartEndPositionIndicies.Length; ++i)
         {
             int2 startEndPosition = m_TrainPositioningSytem.m_StartEndPositionIndicies[i];
             
-            for (int j = startEndPosition.x; j < (startEndPosition.y - 1); ++j)
+            int pathSize = startEndPosition.y - startEndPosition.x;
+            for (int j = 0; j < pathSize; ++j)
             {
-                float3 positionStart = m_TrainPositioningSytem.m_PathPositions[j + 0];
-                float3 positionEnd = m_TrainPositioningSytem.m_PathPositions[j + 1];
-                bool isStop = m_TrainPositioningSytem.m_PathStopBits.IsBitSet(j + 1);
+                float3 positionStart = m_TrainPositioningSytem.m_PathPositions[startEndPosition.x + j];
+                float3 positionEnd = m_TrainPositioningSytem.m_PathPositions[startEndPosition.x + ((j + 1) % pathSize)];
+                bool isStop = m_TrainPositioningSytem.m_PathStopBits.IsBitSet(startEndPosition.x + ((j + 1) % pathSize));
                 
                 Gizmos.color = Metro.INSTANCE().LineColours[i];
                 Gizmos.color *= isStop ? new Color(0.25f, 0.25f, 0.25f, 1.0f) : new Color(1.0f, 1.0f, 1.0f, 1.0f);
