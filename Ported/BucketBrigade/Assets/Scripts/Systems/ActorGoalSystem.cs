d25947d481075a06a889ea7a4e567d5538cc2062
Gerald Orban <gerald.orban@unity3d.com>
2020-04-15 12:32:54 -0700

    Add actor goal and movement systems

---
+++ Ported/BucketBrigade/Assets/Scripts/Systems/ActorGoalSystem.cs
@@ -0,0 +1,40 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class ActorGoalSystem : SystemBase
+{
+    private Random mRand;
+    protected override void OnCreate()
+    {
+        GetEntityQuery(ComponentType.ReadWrite<Actor>(), ComponentType.Exclude<Destination>());
+        mRand = new Random(1337);
+        base.OnCreate();
+    }
+
+    protected override void OnUpdate()
+    {
+        var ecb = new EntityCommandBuffer(Allocator.TempJob);
+
+        var rand = mRand;
+        Entities
+            .WithName("Actor_Goals")
+            .WithAll<Actor>()
+            .WithNone<Destination>()
+            .ForEach((Entity actor, in Translation currentPos) =>
+            {
+                var randomCircle = (rand.NextFloat2() - 1) * 2;
+                randomCircle *= rand.NextFloat() * 5;
+                Destination dest = new Destination()
+                {
+                    position = currentPos.Value + new float3(randomCircle.x, 0f, randomCircle.y)
+                };
+
+                ecb.AddComponent(actor, dest);
+            }).Run();
+
+        mRand = rand;
+        ecb.Playback(EntityManager);
+    }
+}
---
+++ Ported/BucketBrigade/Assets/Scripts/Systems/ActorMovementSystem.cs
@@ -0,0 +1,45 @@
+﻿using Unity.Collections;
+using  Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class ActorMovementSystem : SystemBase
+{
+    private EntityQuery mActorsToMove;
+
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        mActorsToMove = GetEntityQuery(ComponentType.ReadOnly<Actor>(), ComponentType.ReadWrite<Translation>(), ComponentType.ReadWrite<Destination>());
+    }
+
+    protected override void OnUpdate()
+    {
+        var tuningData = GetSingleton<TuningData>();
+        var deltaTime = Time.DeltaTime;
+
+        var ecb = new EntityCommandBuffer(Allocator.TempJob);
+        Entities
+            .WithName("Actor_Movement")
+            .WithAll<Actor>()
+            .ForEach((Entity actorEntity, ref Translation translation, in Destination dest) =>
+            {
+                float3 delta = dest.position - translation.Value;
+
+
+                float maxTravelDistance = deltaTime * tuningData.ActorSpeed;
+                float distance = deltaTime * math.length(delta);
+                distance = math.min(distance, maxTravelDistance);
+
+                translation.Value = distance * math.normalize(delta) + translation.Value;
+                if (distance < 1e-2)
+                {;
+                    ecb.RemoveComponent<Destination>(actorEntity);
+                }
+            }).Run();
+
+
+        ecb.Playback(EntityManager);
+    }
+}
