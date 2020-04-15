2f54f870d84f3f7f3059f9e811f7171ef633d967
Bill Ramsour <bill@unity3d.com>
2020-04-15 16:24:23 -0500

    fire again

---
+++ Ported/BucketBrigade/Assets/Scripts/Systems/FireVisualSystem.cs
@@ -0,0 +1,24 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class FireVisualSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+        
+        Entities.WithAll<Fire>().
+            ForEach((ref Translation translation, in ValueComponent val) =>
+        {
+            translation.Value.y = val.Value / 255f;
+        }).Run();
+
+        ecb.Playback(EntityManager);
+    }
+}
