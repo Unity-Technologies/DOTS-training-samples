efa8e132a60955d642b3d66fe1ea5f5aa773c0de
Anthony <vincismurf@gmail.com>
2020-04-15 15:49:35 -0500

    Fire Commit
    Added Fire material to prefab and stub of system( not functional)

---
+++ Ported/BucketBrigade/Assets/Scripts/Components/FireMaterialComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+[GenerateAuthoringComponent]
+public struct FireMaterialComponent : IComponentData
+{
+    public Color FireColor;
+    public Color GrassColor;
+    public float Amount;
+}
