2aa1875f607046bae08db5a928b702d007ba2306
Gerald Orban <gerald.orban@unity3d.com>
2020-04-15 14:18:15 -0700

    commit actor movement and bucket fetch goal.

--- Ported/BucketBrigade/Assets/Scripts/Systems/ActorGoalSystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/Systems/ActorGoalSystem.cs
@@ -1,29 +1,109 @@
 ﻿using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 
 public class ActorGoalSystem : SystemBase
 {
     private Random mRand;
+    private EntityQuery mAllBucketsNotBeingHeld;
+    private EntityQuery mAllActorsWithoutDestination;
+    private EndSimulationEntityCommandBufferSystem mEndSimCommandBufferSystem;
+
     protected override void OnCreate()
     {
-        GetEntityQuery(ComponentType.ReadWrite<Actor>(), ComponentType.Exclude<Destination>());
+        mAllActorsWithoutDestination = GetEntityQuery(ComponentType.ReadWrite<Actor>(), ComponentType.Exclude<Destination>());
+        mAllBucketsNotBeingHeld = GetEntityQuery(ComponentType.ReadWrite<Bucket>(), ComponentType.Exclude<HeldBy>());
+        mEndSimCommandBufferSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+
         mRand = new Random(1337);
         base.OnCreate();
     }
 
     protected override void OnUpdate()
     {
-        var ecb = new EntityCommandBuffer(Allocator.TempJob);
+        var actorPerformActionBuffer = new EntityCommandBuffer(Allocator.TempJob);
+        var actorCreateGoalBuffer = mEndSimCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
 
         var rand = mRand;
+
+        var getBucketComponent = GetComponentDataFromEntity<Bucket>();
+        var getRiverComponent = GetComponentDataFromEntity<River>();
+
         Entities
-            .WithName("Actor_Goals")
+            .WithName("Actor_Perform_Action")
             .WithAll<Actor>()
             .WithNone<Destination>()
-            .ForEach((Entity actor, in Translation currentPos) =>
+            .ForEach((Entity actor, in TargetEntity target) => {
+
+                //TODO: If target is a river, get water, if fire dump water, if another actor, send them a bucket
+
+                if (getBucketComponent.Exists(target.target))
+                {
+
+                }
+
+                actorPerformActionBuffer.RemoveComponent<TargetEntity>(actor);
+            }).Run();
+
+        actorPerformActionBuffer.Playback(EntityManager);
+        actorPerformActionBuffer.Dispose();
+
+        int numAvailableBuckets = mAllBucketsNotBeingHeld.CalculateEntityCount();
+        if (numAvailableBuckets > 0)
+        {
+            var bucketEntities = mAllBucketsNotBeingHeld.ToEntityArrayAsync(Allocator.TempJob, out var getBucketEntitiesHandle);
+            var countOfActorsWithoutDestination = mAllActorsWithoutDestination.CalculateEntityCount();
+            var getBucketPositon = GetComponentDataFromEntity<Translation>(true);
+
+            Dependency = JobHandle.CombineDependencies(Dependency, getBucketEntitiesHandle);
+
+            Entities
+                .WithName("Actor_Find_Bucket")
+                .WithAll<Actor>()
+                .WithNone<Destination, HoldingBucket>()
+                .WithReadOnly(getBucketPositon)
+                .WithDeallocateOnJobCompletion(bucketEntities)
+                .ForEach((int entityInQueryIndex, Entity actor, in Translation currentPos) =>
+                {
+                    if (entityInQueryIndex >= bucketEntities.Length)
+                    {
+                        return;
+                    }
+
+                    actorCreateGoalBuffer.AddComponent(entityInQueryIndex, actor, new TargetEntity { target = bucketEntities[entityInQueryIndex] } );
+                    actorCreateGoalBuffer.AddComponent(entityInQueryIndex, bucketEntities[entityInQueryIndex], new HeldBy() {holder = actor});
+                    actorCreateGoalBuffer.AddComponent(entityInQueryIndex, actor, new Destination() {position = getBucketPositon[bucketEntities[entityInQueryIndex]].Value});
+                }).ScheduleParallel();
+        }
+        else
+        {
+            Entities
+                .WithName("Actor_Wander")
+                .WithAll<Actor>()
+                .WithNone<Destination, HoldingBucket>()
+                .ForEach((int nativeThreadIndex, Entity actor, in Translation currentPos) =>
+                {
+                    //TODO: Find bucket
+                    var randomCircle = (rand.NextFloat2() - 1) * 2;
+                    randomCircle *= rand.NextFloat() * 5;
+                    Destination dest = new Destination()
+                    {
+                        position = currentPos.Value + new float3(randomCircle.x, 0f, randomCircle.y)
+                    };
+
+                    actorCreateGoalBuffer.AddComponent(nativeThreadIndex, actor, dest);
+                }).ScheduleParallel();
+        }
+
+        Entities
+            .WithName("Actor_Find_Water")
+            .WithAll<Actor, HoldingBucket>()
+            .WithNone<Destination>()
+            .ForEach((int nativeThreadIndex, Entity actor, in Translation currentPos) =>
             {
+                //TODO: Find water destination
                 var randomCircle = (rand.NextFloat2() - 1) * 2;
                 randomCircle *= rand.NextFloat() * 5;
                 Destination dest = new Destination()
@@ -31,12 +111,27 @@ public class ActorGoalSystem : SystemBase
                     position = currentPos.Value + new float3(randomCircle.x, 0f, randomCircle.y)
                 };
 
-                ecb.AddComponent(actor, dest);
-            }).Run();
+                actorCreateGoalBuffer.AddComponent(nativeThreadIndex, actor, dest);
+            }).ScheduleParallel();
+
+        Entities
+            .WithName("Actor_Find_Fire")
+            .WithAll<Actor, HoldingBucket>()
+            .WithNone<Destination>()
+            .ForEach((int nativeThreadIndex, Entity actor, in Translation currentPos) =>
+            {
+                //TODO: Find fire destination
+                var randomCircle = (rand.NextFloat2() - 1) * 2;
+                randomCircle *= rand.NextFloat() * 5;
+                Destination dest = new Destination()
+                {
+                    position = currentPos.Value + new float3(randomCircle.x, 0f, randomCircle.y)
+                };
+
+                actorCreateGoalBuffer.AddComponent(nativeThreadIndex, actor, dest);
+            }).ScheduleParallel();
 
         mRand = rand;
-        ecb.Playback(EntityManager);
-
-        ecb.Dispose();
+        mEndSimCommandBufferSystem.AddJobHandleForProducer(Dependency);
     }
 }
--- Ported/BucketBrigade/Assets/Scripts/Systems/ActorMovementSystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/Systems/ActorMovementSystem.cs
@@ -26,19 +26,25 @@ public class ActorMovementSystem : SystemBase
             .ForEach((Entity actorEntity, ref Translation translation, in Destination dest) =>
             {
                 float3 delta = dest.position - translation.Value;
-
-
                 float maxTravelDistance = deltaTime * tuningData.ActorSpeed;
                 float distance = deltaTime * math.length(delta);
                 distance = math.min(distance, maxTravelDistance);
 
                 translation.Value = distance * math.normalize(delta) + translation.Value;
                 if (distance < 1e-2)
-                {;
+                {
                     ecb.RemoveComponent<Destination>(actorEntity);
                 }
             }).Run();
 
+        var getActorPosition = GetComponentDataFromEntity<Translation>(true);
+        Entities
+            .WithAll<Bucket>()
+            .ForEach((Entity Bucket, ref Translation bucketTranslation, in HeldBy heldBy) =>
+            {
+                var actorPosition = getActorPosition[heldBy.holder];
+                bucketTranslation.Value = actorPosition.Value = math.up() * .5f;
+            }).Run();
 
         ecb.Playback(EntityManager);
         ecb.Dispose();
