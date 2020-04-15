36ebef0f2f45ec01ee5c8aa5ae52778b9ae54863
Keenan Patterson <keenan@unity3d.com>
2020-04-15 13:10:00 -0700

    speed system first pass

---
+++ Ported/HighwayRacers/Assets/Code/SpeedSystem.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class SpeedSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Speed speed, in BlockSpeed blockSpeed) =>
+        {
+            speed.Value = blockSpeed.Value;
+        }).ScheduleParallel();
+        
+        Entities.ForEach((ref Speed speed, in TargetSpeed targetSpeed, in OvertakeTag overtakeTag, in OvertakeSpeedIncrement overtakeSpeedIncrement) =>
+        {
+            speed.Value = targetSpeed.Value + overtakeSpeedIncrement.Value;
+        }).ScheduleParallel();
+        
+        Entities.WithNone<BlockSpeed, OvertakeTag>().ForEach((ref Speed speed, in TargetSpeed targetSpeed) =>
+        {
+            speed.Value = targetSpeed.Value;
+        }).ScheduleParallel();
+    }
+}
