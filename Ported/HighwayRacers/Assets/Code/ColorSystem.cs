127530f9b0828b72fa95dee83dd45de6d52d435f
Joel Barba <joel.barba@unity3d.com>
2020-04-16 16:38:59 -0500

    V1 of the Color system added.

---
+++ Ported/HighwayRacers/Assets/Code/ColorSystem.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+
+public class ColorSystem : SystemBase
+{
+    private static readonly float4 NormalColor = new float4(0.5f, 0.5f, 0.5f, 1f);
+
+    private static readonly float4 BlockedColor = new float4(1, 0, 0, 1f);
+
+    private static readonly float4 OvertakeColor = new float4(0,1, 0, 1f);
+
+    protected override void OnUpdate()
+    {
+        Entities
+            .WithNone<OvertakeTag, BlockSpeed>()
+            .WithAll<Speed>()
+            .ForEach((ref URPMaterialPropertyBaseColor baseColor) =>
+        {
+            baseColor.Value = NormalColor;
+        }).ScheduleParallel();
+
+        Entities
+            .WithAll<BlockSpeed>()
+            .ForEach((ref URPMaterialPropertyBaseColor baseColor) =>
+        {
+            baseColor.Value = BlockedColor;
+        }).ScheduleParallel();
+
+        Entities
+            .WithAll<OvertakeTag>()
+            .ForEach((ref URPMaterialPropertyBaseColor baseColor) =>
+        {
+            baseColor.Value = OvertakeColor;
+        }).ScheduleParallel();
+    }
+}
