d093576bde2d5d72279012fa65eac5e958e45ed8
Keenan Patterson <keenan@unity3d.com>
2020-04-16 09:20:56 -0700

    removed unused code, simplified

--- Ported/HighwayRacers/Assets/Code/PercentCompleteSystem.cs
+++ Ported/HighwayRacers/Assets/Code/PercentCompleteSystem.cs
@@ -1,37 +1,15 @@
-﻿using System.Diagnostics;
-using Unity.Collections;
-using Unity.Entities;
-using Unity.Jobs;
-using Unity.Mathematics;
+﻿using Unity.Entities;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 public class PercentCompleteSystem : SystemBase
 {
-    protected override void OnCreate()
-    {
-        RequireForUpdate(EntityManager.CreateEntityQuery(typeof(RoadInfo)));
-    }
-
     protected override void OnUpdate()
     {
-        var ecb = new EntityCommandBuffer(Allocator.Temp);
-        RoadInfo laneInfo = GetSingleton<RoadInfo>();
-        float totalDistance = math.abs(laneInfo.EndXZ.y - laneInfo.StartXZ.y);
         float deltaTime = Time.DeltaTime;
 
-        //may not need road length
         Entities.ForEach((ref PercentComplete percentComplete, in Speed speed) =>
         {
-            //float relativeStartPosition = totalDistance * percentComplete.Value;
-            var distance = speed.Value * deltaTime;
-            //float newPercentComplete = (relativeStartPosition + distance) / totalDistance;
-            //if (newPercentComplete > 1.0f)
-                //newPercentComplete = 0.0f;
-            percentComplete.Value = (percentComplete.Value + distance) % 1f;
-
+            percentComplete.Value += speed.Value * deltaTime - (int)percentComplete.Value;
         }).Schedule();
-
-        ecb.Playback(EntityManager);
     }
 }
