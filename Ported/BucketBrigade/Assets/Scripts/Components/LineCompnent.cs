f0e632617d66cff75429d7564587952dfc978b89
Bill Ramsour <bill@unity3d.com>
2020-04-16 11:10:38 -0500

    adding brigadeTag and line component

---
+++ Ported/BucketBrigade/Assets/Scripts/Components/LineCompnent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+[GenerateAuthoringComponent]
+public struct LineComponent : IComponentData
+{
+    public Entity start;
+    public Entity end;
+}
