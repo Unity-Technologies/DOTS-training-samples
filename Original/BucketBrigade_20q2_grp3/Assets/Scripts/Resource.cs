f8f9b0bc3a03db7e1508fd2bffb5ed333ee4d79a
Paul Burslem <paulburslem@gmail.com>
2020-04-16 11:04:14 -0500

    added water resource

---
+++ Original/BucketBrigade_20q2_grp3/Assets/Scripts/Resource.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public class Resource : IComponentData
+{
+    [Range(3f, 10000f)]
+    public float Capacity = 100;
+}
