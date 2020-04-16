3bfea441b07a81bbdf73f3dc23f2c3afa7be5f2d
Kirsten Pilla <kirsten.pilla@unity3d.com>
2020-04-16 12:50:50 -0500

    update render position system

---
+++ Ported/HighwayRacers/Assets/Code/RoadCreationAuthoring.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class RoadCreationAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public float2 StartXZ;
+    public float2 EndXZ;
+    public int MaxLanes;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        float laneWidth = math.abs(EndXZ.x - StartXZ.x) / MaxLanes;
+        dstManager.AddComponentData(entity, new RoadInfo()
+        {
+            StartXZ = StartXZ,
+            EndXZ = EndXZ,
+            LaneWidth = laneWidth,
+            MaxLanes = MaxLanes
+        });
+
+        DynamicBuffer<LaneInfoElement> dynamicBuffer = dstManager.AddBuffer<LaneInfoElement>(entity);
+        float currentLanePos = laneWidth;
+        for (int i = 0; i < MaxLanes; ++i)
+        {
+            LaneInfo info = new LaneInfo { Pivot = currentLanePos };
+            dynamicBuffer.Add(new LaneInfoElement { Value = info });
+            currentLanePos += laneWidth;
+        }
+    }
+}
