f7237ab799425f016534a6f9d19ab6fbfa46e4e7
Joel Barba <joel.barba@unity3d.com>
2020-04-16 18:20:51 -0500

    Updated the Car prefab to have color components and added the ColorSystem to control the colors its using

--- Ported/HighwayRacers/Assets/Code/ColorSystem.cs
+++ Ported/HighwayRacers/Assets/Code/ColorSystem.cs
@@ -1,6 +1,7 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Rendering;
+using Unity.Transforms;
 
 public class ColorSystem : SystemBase
 {
@@ -13,25 +14,51 @@ public class ColorSystem : SystemBase
     protected override void OnUpdate()
     {
         Entities
+            // Get the entities that are not overtaking or blocking, but have speed; the agents that are moving normally. 
             .WithNone<OvertakeTag, BlockSpeed>()
             .WithAll<Speed>()
-            .ForEach((ref URPMaterialPropertyBaseColor baseColor) =>
-        {
-            baseColor.Value = NormalColor;
-        }).ScheduleParallel();
+            .ForEach((ref MaterialColor baseColor, in Parent parent) =>
+            {
+                // Update the color to normal.
+                baseColor.Value = NormalColor;
+
+            }).ScheduleParallel();
 
         Entities
+            // Get the entities that are blocking
             .WithAll<BlockSpeed>()
-            .ForEach((ref URPMaterialPropertyBaseColor baseColor) =>
-        {
-            baseColor.Value = BlockedColor;
-        }).ScheduleParallel();
+            .ForEach((ref MaterialColor baseColor) =>
+            {
+                // Update the color to blocked.
+                baseColor.Value = BlockedColor;
+            })
+            .ScheduleParallel();
 
         Entities
+            // Get the entities that are overtaking
             .WithAll<OvertakeTag>()
-            .ForEach((ref URPMaterialPropertyBaseColor baseColor) =>
-        {
-            baseColor.Value = OvertakeColor;
-        }).ScheduleParallel();
+            .ForEach((ref MaterialColor baseColor) =>
+            {
+                // Update the color to overtake.
+                baseColor.Value = OvertakeColor;
+            })
+            .ScheduleParallel();
+
+        // This system iterates through all the child objects that have renderers.
+        Entities
+            .ForEach((Entity entity, ref URPMaterialPropertyBaseColor baseColor, in Parent parent) =>
+            {
+                // Get the parent entity, which should be the agent.
+                Entity parentEntity = parent.Value;
+
+                // Get the color component of the agent.
+                MaterialColor carColor = EntityManager.GetComponentData<MaterialColor>(parentEntity);
+
+                // Copy the value over so that it affects the material color.
+                baseColor.Value = carColor.Value;
+
+            })
+            .WithoutBurst()
+            .Run();
     }
 }
