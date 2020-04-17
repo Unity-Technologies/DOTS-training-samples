e4ba3c3f2a0391bafd63c983ce22c0f6430d95ae
Paul Burslem <paulburslem@gmail.com>
2020-04-17 10:32:43 -0500

    fixed read access error

--- Original/BucketBrigade_20q2_grp3/Assets/Scripts/ResourceAmount.cs
+++ Original/BucketBrigade_20q2_grp3/Assets/Scripts/ResourceAmount.cs
@@ -8,6 +8,13 @@ public struct ResourceAmount : IComponentData
 {
     [Range(1f, 1000f)]
     public int Value;
+    [HideInInspector]
+    public double NextSpawnTime;
+}
+
+public struct ResourceNextSpawnTime : IComponentData
+{
+    public double Value;
 }
 
 public struct ResourceClaimed : IComponentData
