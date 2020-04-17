a0e6ef5ef086e864bd65a917dd0152fb1090d7fd
Keenan Patterson <keenan@unity3d.com>
2020-04-17 08:57:22 -0700

    components/systems for road segments

---
+++ Ported/HighwayRacers/Assets/Code/SegmentAssignmentSystem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class SegmentAssignmentSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+    }
+}
