a4c8165d950145969ea3409de5cd436231e7bc6a
Keenan Patterson <keenan@unity3d.com>
2020-04-17 08:57:22 -0700

    components/systems for road segments

---
+++ Ported/HighwayRacers/Assets/Code/CurvedSegment.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class CurvedSegment : IComponentData
+{
+    public float Radius;
+}
---
+++ Ported/HighwayRacers/Assets/Code/SegmentInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public struct SegmentInfo : IComponentData
+{
+    public float Length;
+    public float2 StartXZ;
+    public float2 EndXZ;
+    public int Order;
+    public float2 PercentageRange;
+}
+
+public struct SegmentInfoElement : IBufferElementData
+{
+    // These implicit conversions are optional, but can help reduce typing.
+    public static implicit operator SegmentInfo(SegmentInfoElement e) { return e.Value; }
+    public static implicit operator SegmentInfoElement(SegmentInfo e) { return new SegmentInfoElement { Value = e }; }
+    
+    public SegmentInfo Value;
+}
