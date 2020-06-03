03848ece462db4a215cca43a2cf4598efea0c4ea
Victor Lui SC <victorl@unity3d.com>
2020-06-03 13:11:35 +0100

    car spawning system

--- Ported/HighwayRacers/Assets/Scripts/Components/CarSpawner.cs
+++ Ported/HighwayRacers/Assets/Scripts/Components/CarSpawner.cs
@@ -3,5 +3,12 @@
 [GenerateAuthoringComponent]
 public struct CarSpawner : IComponentData
 {
+    public int InstancesToSpawn;
     public Entity CarPrefab;
+
+    public float MaxDefaultSpeed;
+    public float MaxOvertakeSpeed;
+    public float MaxDistanceToCarBeforeOvertaking;
+    public float MaxOvertakeEagerness;
+    public float MaxMergeSpace;
 }
---
+++ Ported/HighwayRacers/Assets/Scripts/Systems/CarSpawningSystem.cs
@@ -0,0 +1,60 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class CarSpawningSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var barrier = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+        var commandBuffer = barrier.CreateCommandBuffer();
+
+        var random = new Unity.Mathematics.Random(1);
+        var jobHandle = Entities.ForEach((in Entity entity, in CarSpawner spawner) => 
+        {
+            var numberToBeSpawned = spawner.InstancesToSpawn;
+            var carToBeSpawned = spawner.CarPrefab;
+
+            for (int i = 0; i < numberToBeSpawned; ++i)
+            {
+                var newCar = commandBuffer.Instantiate(carToBeSpawned);
+                commandBuffer.SetComponent(newCar, new CarProperties
+                {
+                    DefaultSpeed = random.NextFloat(spawner.MaxDefaultSpeed),
+                    OvertakeSpeed = random.NextFloat(spawner.MaxOvertakeSpeed),
+                    DistanceToCarBeforeOvertaking = random.NextFloat(spawner.MaxDistanceToCarBeforeOvertaking),
+                    OvertakeEagerness = random.NextFloat(spawner.MaxOvertakeEagerness),
+                    MergeSpace = random.NextFloat(spawner.MaxMergeSpace),
+                });
+
+                commandBuffer.SetComponent(newCar, new Speed
+                {
+                    Value = 0
+                });
+
+                var pointOnInterval = (float)i / (float)numberToBeSpawned;
+                UniformDistributionOverTrack(pointOnInterval, 4, out var targetLane, out var targetTrackProgress);
+                commandBuffer.SetComponent(newCar, new TrackPosition
+                {
+                    Lane = (float)targetLane,
+                    TrackProgress = targetTrackProgress,
+                });
+
+                commandBuffer.DestroyEntity(entity);
+            }
+        }).Schedule(Dependency);
+
+        Dependency = jobHandle;
+
+        barrier.AddJobHandleForProducer(jobHandle);
+    }
+
+    static void UniformDistributionOverTrack(float value, int numLanes, out int lane, out float trackProgress)
+    {
+        lane = (int)math.floor(value * numLanes);
+        trackProgress = value * numLanes - lane;
+    }
+}
