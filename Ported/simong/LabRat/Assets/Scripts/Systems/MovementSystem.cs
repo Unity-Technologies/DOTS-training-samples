728d5847d154511dcbcb348c561b24f62d4c3c8d
Andy Bastable <andy.bastable@unity3d.com>
2020-06-03 11:24:06 +0100

    Rough placeholder Movement System

---
+++ Ported/simong/LabRat/Assets/Scripts/Systems/MovementSystem.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+
+class MovementSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entities
+            .WithAll<MoveableTag>()
+            .ForEach((ref Position2D pos, ref Rotation2D rot, ref Direction2D dir) =>
+            {
+
+            })
+            .WithName("UpdateMovables")
+            .ScheduleParallel();
+    }
+}
