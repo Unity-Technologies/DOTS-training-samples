500c99a79f00f9971e8a3a06cd51ab5328dead72
simong <simong@unity3d.com>
2020-06-03 10:57:53 +0100

    Move authoring components to dedicated files

---
+++ Ported/simong/LabRat/Assets/Scripts/Components/Direction2D.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public enum GridDirection
+{
+    NORTH = (1 << 0),
+    EAST = (1 << 1),
+    SOUTH = (1 << 2),
+    WEST = (1 << 3)
+}
+
+[GenerateAuthoringComponent]
+public struct Direction2D : IComponentData
+{
+    public GridDirection Value;
+}
+
---
+++ Ported/simong/LabRat/Assets/Scripts/Components/Position2D.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public struct Rotation2D : IComponentData
+{
+    public float Value;
+}
