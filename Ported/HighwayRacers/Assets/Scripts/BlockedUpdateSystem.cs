0a9e02a8406e9a25ec17b8a44d7e2501643229ac
Sebastian Hein <sbstn.hn@gmail.com>
2020-06-03 16:26:55 +0200

    added BlockedUpdateSystem, extracted blocking code from SpeedUpdateSystem

---
+++ Ported/HighwayRacers/Assets/Scripts/BlockedUpdateSystem.cs
@@ -0,0 +1,67 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class BlockedUpdateSystem : SystemBase
+{
+    EntityCommandBufferSystem entityCommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        entityCommandBufferSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>(); 
+    }
+    
+
+    protected override void OnUpdate()
+    {
+        TrackProperties trackProperties = GetSingleton<TrackProperties>();
+        float dtime = Time.DeltaTime;
+
+        var ecb = entityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+
+        Entities
+            .WithNone<BlockedState>()
+            .ForEach((int entityInQueryIndex, Entity carEntity, 
+                ref TrackPosition trackPosition, 
+                in CarInFront carInFront, in Speed speed, in CarProperties carProperties) =>
+        {
+            bool blocked = CheckBlock(speed.Value, carInFront.Speed, carProperties.Acceleration, 
+                trackPosition.TrackProgress, carInFront.TrackProgressCarInFront,trackProperties.TrackLength);
+
+           if (blocked) {
+                ecb.AddComponent<BlockedState>(entityInQueryIndex, carEntity);
+           }
+        }).ScheduleParallel();
+    
+        Entities
+            .WithAll<BlockedState>()
+            .ForEach((int entityInQueryIndex, Entity carEntity, 
+                ref TrackPosition trackPosition, 
+                in CarInFront carInFront, in Speed speed, in CarProperties carProperties) =>
+        {
+            bool blocked = CheckBlock(speed.Value, carInFront.Speed, carProperties.Acceleration, 
+                trackPosition.TrackProgress, carInFront.TrackProgressCarInFront,trackProperties.TrackLength);
+
+            if (!blocked) {
+                ecb.RemoveComponent<BlockedState>(entityInQueryIndex, carEntity);        
+            } 
+        }).ScheduleParallel();
+
+        entityCommandBufferSystem.AddJobHandleForProducer(Dependency);
+    }
+
+    static bool CheckBlock(float velocity, float velocityOfCarInFront, float acceleration, float trackProgress, float trackProgressCarInFront, float trackLength) {
+        float timeToSlowDown = (velocityOfCarInFront-velocity) / acceleration;
+        float spaceToSlowDown = velocity * timeToSlowDown + 0.5f * acceleration * timeToSlowDown * timeToSlowDown;
+    
+        float threshold = 1.25f;
+
+        float distanceBetweenCars = trackProgressCarInFront - trackProgress;
+        distanceBetweenCars -= threshold; // XXX temp. replace
+
+        // get distance and take track length into account
+        distanceBetweenCars = (distanceBetweenCars + trackLength) % trackLength;
+
+        return !(velocityOfCarInFront > velocity || distanceBetweenCars > spaceToSlowDown);
+    }
+}
--- Ported/HighwayRacers/Assets/Scripts/SpeedUpdateSystem.cs
+++ Ported/HighwayRacers/Assets/Scripts/SpeedUpdateSystem.cs
@@ -9,43 +9,22 @@ public class SpeedUpdateSystem : SystemBase
         TrackProperties trackProperties = GetSingleton<TrackProperties>();
         float dtime = Time.DeltaTime;
 
-        Entities.ForEach((ref Speed speed, 
+        // deccelerate in case the car is blocked
+        Entities.WithAll<BlockedState>().ForEach((ref Speed speed, 
             in TrackPosition trackPosition, in CarProperties carProperties, in CarInFront carInFront) =>
         {
-            // 1. if carInFront is faster 
-                // => accelerate
-            // 2. else 
-                // t = (V-V0) / a -- time to slow down
+            speed.Value -= carProperties.Acceleration * dtime; 
+            speed.Value = math.max(carInFront.Speed, math.max(speed.Value, 0));
+        }).ScheduleParallel();
 
-             // s = v0 * t + 1/2 * a * t^2 
+        // Accelerate in case the car is not blocked
+        Entities.WithNone<BlockedState>().ForEach((ref Speed speed, 
+            in TrackPosition trackPosition, in CarProperties carProperties, in CarInFront carInFront) =>
+        {
+            speed.Value += carProperties.Acceleration * dtime; 
 
-       //     { // blocked state check
-                float v = carInFront.Speed; 
-                float v0 = speed.Value;
-
-                float timeToSlowDown = (v-v0) / carProperties.Acceleration;
-                float spaceToSlowDown = v0 * timeToSlowDown + 0.5f * carProperties.Acceleration * timeToSlowDown * timeToSlowDown;
-            
-                float threshold = 1.25f;
-
-                float distanceBetweenCars = carInFront.TrackProgressCarInFront - trackPosition.TrackProgress;
-                distanceBetweenCars -= threshold; // XXX temp. replace
-
-                // get distance and take track length into account
-                distanceBetweenCars = (distanceBetweenCars + trackProperties.TrackLength) % trackProperties.TrackLength;
-       //     }
-
-            if (v > v0 || distanceBetweenCars > spaceToSlowDown) {
-                // simple accelerate first
-                speed.Value += carProperties.Acceleration * dtime; 
-
-                float maxSpeed = carProperties.DefaultSpeed;
-                speed.Value = math.min(v, math.min(speed.Value, maxSpeed));
-            } else {
-            
-                speed.Value -= carProperties.Acceleration * dtime; 
-                speed.Value = math.max(v, math.max(speed.Value, 0));
-            }
+            float maxSpeed = carProperties.DefaultSpeed;
+            speed.Value = math.min(carInFront.Speed, math.min(speed.Value, maxSpeed));
         }).ScheduleParallel();
     }
 }
