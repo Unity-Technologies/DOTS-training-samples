a85dfa75567b6504e4ec94617e82d5a2069d5499
Marco Trivellato <marcot@unity3d.com>
2020-06-03 16:11:23 +0100

    CarStatusRenderSystem placeholder

---
+++ Ported/HighwayRacers/Assets/Scripts/CarStatusDisplayManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarStatusDisplayManager : MonoBehaviour
+{
+    static public CarStatusDisplayManager Instance;
+    
+    public Material BlockedStatusMaterial;
+    public Material DefaultSpeedStatusMaterial;
+    public Material AccelerationStatusMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Ported/HighwayRacers/Assets/Scripts/Systems/CarStatusRenderSystem.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+
+[DisableAutoCreation]
+public class CarStatusRenderSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var blockedStatusMaterial = CarStatusDisplayManager.Instance.BlockedStatusMaterial;
+        var defaultSpeedStatusMaterial = CarStatusDisplayManager.Instance.DefaultSpeedStatusMaterial;
+        var accelerationStatusMaterial = CarStatusDisplayManager.Instance.AccelerationStatusMaterial;
+
+        Entities.WithoutBurst()
+            .ForEach((RenderMesh renderMesh, in CarProperties translation, in Speed speed) =>
+            {
+                renderMesh.material = accelerationStatusMaterial;
+            }).Run();
+    }
+}
