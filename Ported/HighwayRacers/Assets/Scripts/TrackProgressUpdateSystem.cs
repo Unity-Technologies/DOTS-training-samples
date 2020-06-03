639f3880f3a0df47c7c777b7497306a67e8a6bb5
Sebastian Hein <sbstn.hn@gmail.com>
2020-06-03 12:47:04 +0200

    added TrackProgressUpdateSystem

---
+++ Ported/HighwayRacers/Assets/Scripts/TrackProgressUpdateSystem.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class TrackProgressUpdateSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        TrackProperties trackProperties = GetSingleton<TrackProperties>();
+        float dtime = Time.DeltaTime;
+
+        Entities.ForEach((ref TrackPosition trackPosition, in Speed speed) =>
+        {
+            trackPosition.TrackProgress += speed.Value * dtime;
+            trackPosition.TrackProgress %= trackProperties.TrackLength;
+        }).ScheduleParallel();
+    }
+}
