fcd70986df7a535435284c2946ee4bb57557665c
simong <simong@unity3d.com>
2020-06-03 11:23:55 +0100

    Add custome Position2D authoring component to remove default Translation/Rotation

---
+++ Ported/simong/LabRat/Assets/Scripts/Components/Position2DAuthoring.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class Position2DAuthoring : UnityEngine.MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new Position2D { Value = ((float3)transform.position).xy });
+        dstManager.RemoveComponent<Translation>(entity);
+        dstManager.RemoveComponent<Rotation>(entity);
+    }
+}
