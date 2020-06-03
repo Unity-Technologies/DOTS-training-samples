8b13703be4359afcd0da98474d7a7995fbdba9b3
Andy Bastable <andy.bastable@unity3d.com>
2020-06-03 16:12:05 +0100

    Restore IsHole checks

--- Ported/simong/LabRat/Assets/Scripts/Systems/MovementSystem.cs
+++ Ported/simong/LabRat/Assets/Scripts/Systems/MovementSystem.cs
@@ -84,18 +84,18 @@ class MovementSystem : SystemBase
                     var cellIndex = (cellCoord.y * rows) + cellCoord.x;
                     var cell = cells[cellIndex];
 
-                    //if (cell.IsHole())
-                    //{
-                    //    // add falling tag
-                    //    ecb.AddComponent<FallingTag>(entityInQueryIndex, entity);
+                    if (cell.IsHole())
+                    {
+                        // add falling tag
+                        ecb.AddComponent<FallingTag>(entityInQueryIndex, entity);
 
-                    //}
-                    //else if (cell.IsBase())
-                    //{
-                    //    // remove entity and score
-                    //    //ecb.AddComponent<ReachedBase>(entityInQueryIndex, entity);
-                    //}
-                    //else
+                    }
+                    else if (cell.IsBase())
+                    {
+                        // remove entity and score
+                        //ecb.AddComponent<ReachedBase>(entityInQueryIndex, entity);
+                    }
+                    else
                     {
                         var newDirection = dir.Value;
 
