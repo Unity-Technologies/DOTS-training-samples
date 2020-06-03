a83dc9b6bafa883e521c32e614fddbc9f3c0e107
Brian Will <brian.will@unity3d.com>
2020-06-03 15:25:39 -0700

    lane assignment no longer shared

--- Ported/HighwayRacers/Assets/Code/RenderPositionSystem.cs
+++ Ported/HighwayRacers/Assets/Code/RenderPositionSystem.cs
@@ -19,37 +19,14 @@ public class RenderPositionSystem : SystemBase
         var laneInfo = GetSingleton<RoadInfo>();
         var entity = GetSingletonEntity<RoadInfo>();
         var laneInfoElements = EntityManager.GetBuffer<LaneInfoElement>(entity).AsNativeArray();
-
-        List<LaneAssignment> lanes = new List<LaneAssignment>();
-        EntityManager.GetAllUniqueSharedComponentData<LaneAssignment>(lanes);
         
-        //JobHandle combined = new JobHandle();
-        foreach (LaneAssignment lane in lanes)
-        {
-            float xPos = laneInfoElements[lane.Value].Value.Pivot;
-            
-            // TODO Translations somehow causes conflict when these jobs overlap 
-            
-            // var temp = Entities.WithSharedComponentFilter(lane).ForEach(
-            //     (ref Translation translation, in PercentComplete percentComplete) =>
-            //     {
-            //         float yPos = laneInfo.StartXZ.y + (laneInfo.EndXZ.y - laneInfo.StartXZ.y) * percentComplete.Value;
-            //         translation.Value.x = xPos;
-            //         translation.Value.z = yPos;
-            //     }).Schedule(Dependency);
-
-            Entities.WithSharedComponentFilter(lane).ForEach(
-                (ref Translation translation, in PercentComplete percentComplete) =>
-                {
-                    float yPos = laneInfo.StartXZ.y + (laneInfo.EndXZ.y - laneInfo.StartXZ.y) * percentComplete.Value;
-                    translation.Value.x = xPos;
-                    translation.Value.z = yPos;
-                }).Schedule();
-
-            // TODO each lane is small enough to be done in single job, but try profile with ScheduleParallel 
-            //combined = JobHandle.CombineDependencies(combined, temp);
-        }
-
-        //Dependency = combined;
+        Entities.ForEach(
+            (ref Translation translation, in PercentComplete percentComplete, in LaneAssignment lane) =>
+            {
+                float xPos = laneInfoElements[lane.Value].Value.Pivot;
+                float yPos = laneInfo.StartXZ.y + (laneInfo.EndXZ.y - laneInfo.StartXZ.y) * percentComplete.Value;
+                translation.Value.x = xPos;
+                translation.Value.z = yPos;
+            }).Schedule();
     }
 }
