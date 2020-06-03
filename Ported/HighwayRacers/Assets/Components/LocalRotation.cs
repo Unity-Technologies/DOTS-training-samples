53ef5e968b126aa8a86373a141f2ef16db33cd37
Brian Will <@brianwill>
2020-06-03 15:02:10 -0700

    copied over UI from original

---
+++ Ported/HighwayRacers/Assets/Components/LocalRotation.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public struct LocalRotation : IComponentData
+{
+    public float Value;
+}
---
+++ Ported/HighwayRacers/Assets/Components/SegmentPercentagesInitializedTag.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct SegmentPercentagesInitializedTag : IComponentData
+{
+}
