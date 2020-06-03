a326e19e98ee0103feb5f7769bba2a80e2f194a4
Sebastian Hein <sbstn.hn@gmail.com>
2020-06-03 12:28:20 +0200

    added simple DisplayModelSystem

---
+++ Ported/HighwayRacers/Assets/Scripts/DisplayModelSystem.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+[UpdateInGroup(typeof(TransformSystemGroup))]
+public class DisplayModelSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        TrackProperties trackProperties = GetSingleton<TrackProperties>();
+
+        Entities.ForEach((ref LocalToWorld localToWorld, 
+            in TrackPosition trackPosition) =>
+        {
+            float2 carPosition = trackProperties.TrackStartingPoint + 
+                new float2(trackPosition.TrackProgress, trackPosition.Lane * trackProperties.LaneSeparation);
+
+            var trans = float4x4.Translate(new float3(carPosition.x, 0, carPosition.y));
+            localToWorld.Value = trans;
+
+        }).ScheduleParallel();
+    }
+}
