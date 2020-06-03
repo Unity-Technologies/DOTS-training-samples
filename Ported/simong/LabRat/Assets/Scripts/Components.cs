443ed241596f9277c402731699c9f20c974b5b07
Peter Hall <peter.hall@unity3d.com>
2020-06-03 10:55:41 +0100

    LabRat Scene added.  ConstantData moved out to a new file.  ConstantData added to camera, Instance variable setup on component.

--- Ported/simong/LabRat/Assets/Scripts/Components.cs
+++ Ported/simong/LabRat/Assets/Scripts/Components.cs
@@ -12,24 +12,6 @@ public enum CharacterType
     MOUSE
 }
 
-public class ConstantData : MonoBehaviour
-{
-    public static ConstantData Instance;
-    
-    public float[] Speed = {1f, 1f};
-    public float[] Radius = {1f, 1f};
-    
-    public float RoundLength = 60f;
-    public Vector2Int BoardDimensions = new Vector2Int(30, 30);
-    public Vector2 CellSize = new Vector2(1f, 1f);
-
-    public float ArrowLifeTime = 30f;
-    public int MaxArrows = 3;
-
-    // assume player 0 is human, others AI
-    public int NumPlayers = 4;
-}
-
 /*[GenerateAuthoringComponent]
 public struct Position2D : IComponentData
 {
---
+++ Ported/simong/LabRat/Assets/Scripts/ConstantData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantData : MonoBehaviour
+{
+    public static ConstantData Instance;
+    
+    public float[] Speed = {1f, 1f};
+    public float[] Radius = {1f, 1f};
+    
+    public float RoundLength = 60f;
+    public Vector2Int BoardDimensions = new Vector2Int(30, 30);
+    public Vector2 CellSize = new Vector2(1f, 1f);
+
+    public float ArrowLifeTime = 30f;
+    public int MaxArrows = 3;
+
+    // assume player 0 is human, others AI
+    public int NumPlayers = 4;
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+}
+
