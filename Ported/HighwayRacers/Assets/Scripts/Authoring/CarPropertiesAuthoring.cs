741acc309e2204e33462a5c2bd31073af6304dcc
Victor Lui SC <victorl@unity3d.com>
2020-06-03 15:24:40 +0100

    create car property authoring component and make custom conversion system for that - while filling car property DOTS component also removes translation/rotation... make track spawner component remove translation/rotation... when spawning the track and set a LocalToWorld only

---
+++ Ported/HighwayRacers/Assets/Scripts/Authoring/CarPropertiesAuthoring.cs
@@ -0,0 +1,41 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class CarPropertiesAuthoring : MonoBehaviour
+{
+    public float DefaultSpeed;
+    public float OvertakeSpeed;
+    public float DistanceToCarBeforeOvertaking;
+    public float OvertakeEagerness;
+    public float MergeSpace;
+    public float Acceleration;
+}
+
+[UpdateInGroup(typeof(GameObjectAfterConversionGroup))]
+public class CarConversionSystem : GameObjectConversionSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((CarPropertiesAuthoring car) =>
+        {
+            var entity = GetPrimaryEntity(car);
+
+            DstEntityManager.AddComponentData(entity,
+                new CarProperties
+                {
+                    DefaultSpeed = car.DefaultSpeed,
+                    OvertakeSpeed = car.OvertakeSpeed,
+                    DistanceToCarBeforeOvertaking = car.DistanceToCarBeforeOvertaking,
+                    OvertakeEagerness = car.OvertakeEagerness,
+                    MergeSpace = car.MergeSpace,
+                    Acceleration = car.Acceleration
+                });
+
+            DstEntityManager.RemoveComponent<Translation>(entity);
+            DstEntityManager.RemoveComponent<Rotation>(entity);
+            DstEntityManager.RemoveComponent<Scale>(entity);
+            DstEntityManager.RemoveComponent<NonUniformScale>(entity);
+        });
+    }
+}
