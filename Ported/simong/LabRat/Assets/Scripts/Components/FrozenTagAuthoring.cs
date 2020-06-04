f79b09671475131efd669f1e6f27678ffb7b49b0
simong <simong@unity3d.com>
2020-06-04 17:08:58 +0100

    Add frozen tag authoring component

---
+++ Ported/simong/LabRat/Assets/Scripts/Components/FrozenTagAuthoring.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Rendering;
+using UnityEngine;
+
+public class FrozenTagAuthoring : UnityEngine.MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddSharedComponentData(entity, new FrozenRenderSceneTag { SceneGUID = new Unity.Entities.Hash128(1, 1, 1, 1), SectionIndex = 0, HasStreamedLOD = 0 });
+    }
+}
