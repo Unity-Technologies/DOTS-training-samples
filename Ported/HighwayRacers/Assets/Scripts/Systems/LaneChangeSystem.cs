615084934ed2f7ed038040d360bab3a6fc48b828
Wessam Ibrahim <wessami@unity3d.com>
2020-06-04 12:24:14 +0200

    added overtake eagerness, also consider lane blocked if a car behind us is too close

--- Ported/HighwayRacers/Assets/Scripts/Systems/LaneChangeSystem.cs
+++ Ported/HighwayRacers/Assets/Scripts/Systems/LaneChangeSystem.cs
@@ -22,57 +22,65 @@ public class LaneChangeSystem : SystemBase
 
         var commandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
 
-        // overtakeeagerness
-
         Entities
             .WithAll<BlockedState>()
             .WithNone<TargetLane>()
-            .ForEach((Entity entity, int entityInQueryIndex, in TrackPosition car, in CarProperties properties) =>
+            .ForEach((Entity entity, int entityInQueryIndex, in TrackPosition car, in CarProperties properties, in CarInFront carInFront) =>
             {
-                // 1. get our lane
-                int lane = (int)car.Lane;
-                float progress = car.TrackProgress;
-
-                // 2. consider neighboring lanes and find empty one
-                // sort through others and find closest (in front) in neighboring lane(s)
-                // at lane 0, consider lane 1
-                // 1, 2 -> consider 0/2 or 1/3
-                // 3 -> consider 2
-                NativeArray<float> lanesToConsider = new NativeArray<float>(trackProperties.NumberOfLanes, Allocator.Temp);
-
-                for (int carIndex = 0; carIndex < otherCars.Length; carIndex++)
+                // overtake eagerness based on how slow is the car in front,
+                // compared to how fast I should be going
+                if (properties.OvertakeEagerness > carInFront.Speed / properties.DefaultSpeed)
                 {
-                    float otherProgress = otherCars[carIndex].TrackProgress;
-                    float otherLane = otherCars[carIndex].Lane;
+                    // 1. get our lane
+                    int lane = (int)car.Lane;
+                    float progress = car.TrackProgress;
 
-                    // calculate actual index for lane (taking lane switching threshold into account)
-                    int actualLane = (int)otherLane;
+                    // 2. consider neighboring lanes and find empty one
+                    // sort through others and find closest (in front) in neighboring lane(s)
+                    // at lane 0, consider lane 1
+                    // 1, 2 -> consider 0/2 or 1/3
+                    // 3 -> consider 2
+                    NativeArray<float> lanesToConsider = new NativeArray<float>(trackProperties.NumberOfLanes, Allocator.Temp);
 
-                    if (otherLane - actualLane > Threshold)
+                    for (int carIndex = 0; carIndex < otherCars.Length; carIndex++)
                     {
-                        ++actualLane;
-                    }
+                        float otherProgress = otherCars[carIndex].TrackProgress;
+                        float otherLane = otherCars[carIndex].Lane;
 
-                    // if the lane is not already blocked
-                    if (lanesToConsider[actualLane] != -1)
-                    {
-                        // check if the closest car in front is closer than allowed merge space
-                        if (otherProgress > progress && math.abs(otherProgress - progress) < properties.MergeSpace)
+                        // calculate actual index for lane (taking lane switching threshold into account)
+                        int actualLane = (int)otherLane;
+
+                        if (otherLane - actualLane > Threshold)
                         {
-                            // mark lane blocked and move on
-                            lanesToConsider[actualLane] = -1;
+                            ++actualLane;
+                        }
+
+                        // if the lane is not already blocked
+                        if (lanesToConsider[actualLane] != -1)
+                        {
+                            float distanceBetweenCars = otherProgress - progress;
+                            float reverseDistanceBetweenCars = -distanceBetweenCars;
+                            distanceBetweenCars = (distanceBetweenCars + trackProperties.TrackLength) % trackProperties.TrackLength;
+                            reverseDistanceBetweenCars = (reverseDistanceBetweenCars + trackProperties.TrackLength) % trackProperties.TrackLength;
+
+                            // check if the closest car in front is closer than allowed merge space
+                            if (distanceBetweenCars < properties.MergeSpace || reverseDistanceBetweenCars < properties.MergeSpace)
+                            {
+                                // mark lane blocked and move on
+                                lanesToConsider[actualLane] = -1;
+                            }
                         }
                     }
-                }
 
-                // start changing lanes
-                for (int i = 0; i < lanesToConsider.Length; i++)
-                {
-                    // only look at nonblocked, neighboring lanes (difference == 1)
-                    if (lanesToConsider[i] != -1 && math.abs(lane - i) == 1)
+                    // start changing lanes
+                    for (int i = 0; i < lanesToConsider.Length; i++)
                     {
-                        // add it to entity
-                        commandBuffer.AddComponent(entityInQueryIndex, entity, new TargetLane { Value = i });
+                        // only look at nonblocked, neighboring lanes (difference == 1)
+                        if (lanesToConsider[i] != -1 && math.abs(lane - i) == 1)
+                        {
+                            // add it to entity
+                            commandBuffer.AddComponent(entityInQueryIndex, entity, new TargetLane { Value = i });
+                        }
                     }
                 }
             })
