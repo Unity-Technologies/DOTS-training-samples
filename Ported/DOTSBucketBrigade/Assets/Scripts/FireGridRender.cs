09f5151f8a3ed28624b0f4a201863914ffbfcf62
Steve McGreal <stephenm@unity3d.com>
2020-06-04 15:22:36 +0100

    Coloured fire! (sort of)

--- Ported/DOTSBucketBrigade/Assets/Scripts/FireGridRender.cs
+++ Ported/DOTSBucketBrigade/Assets/Scripts/FireGridRender.cs
@@ -24,19 +24,28 @@ public class FireGridRender : SystemBase
         float flashpoint = config.Flashpoint;
 
         float time = (float)Time.ElapsedTime;
+        float4 neutralColor = new float4(config.ColorNeutral.r, config.ColorNeutral.g, config.ColorNeutral.b, 1);
+        float4 coolColor = new float4(config.ColorCool.r, config.ColorCool.g, config.ColorCool.b, 1);
+        float4 hotColor = new float4(config.ColorHot.r, config.ColorHot.g, config.ColorHot.b, 1);
 
-        Entities.WithNativeDisableContainerSafetyRestriction(array).ForEach((ref NonUniformScale Scale, in GridCellIndex Index) =>
+        Entities
+            .WithNativeDisableContainerSafetyRestriction(array)
+            .ForEach((ref NonUniformScale Scale, ref CubeColor cubeColor, in GridCellIndex Index) =>
         {
             FireGridCell cell = array[Index.Index];
             float height = kGroundHeight;
+            float4 color = neutralColor;
 
             if (cell.Temperature > flashpoint)
             {
                 height = math.max(cell.Temperature, kGroundHeight);
                 height += (config.FlickerRange * 0.5f) + UnityEngine.Mathf.PerlinNoise((time - Index.Index) * config.FlickerRate - cell.Temperature, cell.Temperature) * config.FlickerRange;
+                color = math.lerp(coolColor, hotColor, cell.Temperature);
             }
 
             Scale.Value.y = height;
+
+            cubeColor.Color = color;
 
         }).ScheduleParallel();
     }
