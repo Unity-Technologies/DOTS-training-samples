e8eec5ffa74d369f353279940d99ad2827be3995
Peter Hall <peter.hall@unity3d.com>
2020-06-04 10:27:30 +0100

    Player coloured base prefabs.

--- Ported/simong/LabRat/Assets/Scripts/Components/PrefabReferenceComponent.cs
+++ Ported/simong/LabRat/Assets/Scripts/Components/PrefabReferenceComponent.cs
@@ -7,6 +7,12 @@ public struct PrefabReferenceComponent : IComponentData
     public Entity ArrowPrefab1;
     public Entity ArrowPrefab2;
     public Entity ArrowPrefab3;
+    
+    public Entity BasePrefab0;
+    public Entity BasePrefab1;
+    public Entity BasePrefab2;
+    public Entity BasePrefab3;
+    
     public Entity PreviewArrowPrefab;
     public Entity CellPrefab;
     public Entity SpawnerPrefab;
