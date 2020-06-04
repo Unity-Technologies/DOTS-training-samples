36aacb126cba4033b770d9756aacc092b5020b22
Claire Hall <claire.hall@unity3d.com>
2020-06-04 14:30:36 +0100

    Add random walls

--- Ported/simong/LabRat/Assets/Scripts/Components/Direction2D.cs
+++ Ported/simong/LabRat/Assets/Scripts/Components/Direction2D.cs
@@ -9,7 +9,8 @@ public enum GridDirection
     NORTH = (1 << 0),
     EAST = (1 << 1),
     SOUTH = (1 << 2),
-    WEST = (1 << 3)
+    WEST = (1 << 3),
+    ALL = NORTH | EAST | SOUTH | WEST
 }
 
 [GenerateAuthoringComponent]
