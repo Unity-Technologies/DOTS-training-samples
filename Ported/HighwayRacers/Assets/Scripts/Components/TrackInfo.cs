4ea2ffa2f4564eaf82ef5c67659419291992a3e7
Wessam Ibrahim <wessami@unity3d.com>
2020-06-05 12:18:19 +0200

    data structure to represent car progress and speeds across track lanes

---
+++ Ported/HighwayRacers/Assets/Scripts/Components/TrackInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Collections;
+
+public class TrackInfo : IComponentData
+{
+    //TODO: implement functions correctly
+    public NativeArray<float> Progresses;
+    public NativeArray<float> Speeds;
+    public NativeArray<float> Lanes;
+}
