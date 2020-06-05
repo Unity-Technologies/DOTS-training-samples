147b7fc80f8a7de2947243ea1cbe20df3c7a03f1
Andrew Glass <andrew.glass@unity3d.com>
2020-06-05 12:10:03 +0100

    StaticOptimisationRequest to reduce hierarchy transforms

---
+++ Ported/simong/LabRat/Assets/Scripts/Components/StaticOptimisationRequestAuthoring.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class StaticOptimisationRequestAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponent<StaticOptimisationRequest>(entity);
+    }
+}
---
+++ Ported/simong/LabRat/Assets/Scripts/Systems/StaticOptimisationSystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Entities.Streaming;
+using Unity.Transforms;
+
+public struct StaticOptimisationRequest : IComponentData
+{}
+
+public class StaticOptimisationSystem : SystemBase
+{
+    private EntityQuery m_requestQuery;
+
+    protected override void OnCreate()
+    {
+        m_requestQuery = GetEntityQuery(new EntityQueryDesc()
+        {
+            All = new[] {ComponentType.ReadWrite<StaticOptimisationRequest>()},
+        });
+
+        RequireForUpdate(m_requestQuery);
+    }
+
+    protected override void OnUpdate()
+    {
+        var ecb = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>().CreateCommandBuffer();
+        ecb.RemoveComponent<Child>(m_requestQuery);
+        ecb.RemoveComponent<StaticOptimisationRequest>(m_requestQuery);
+    }
+}
