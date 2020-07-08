63db900c9ea5c9e6db938a95c4a6e01f7b35942c
Julien Fryer <julienf@unity3d.com>
2020-07-08 14:58:49 +0200

    Fire spread

--- Ported/BucketBrigadeDOTS/Assets/src/dots/FireRenderSystem.cs
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FireRenderSystem.cs
@@ -10,10 +10,16 @@ public class FireRenderSystem : SystemBase
         GetEntityQuery(typeof(FireCell));
     }
 
+    private static float Noise(float t, float amplitude, float frequency, float phase)
+    {
+        return amplitude * math.sin(frequency * t + phase);
+    }
+
     protected override void OnUpdate()
     {
         var fireGridEntity = GetSingletonEntity<FireGridSettings>();
         var bufferRef = EntityManager.GetBuffer<FireCell>(fireGridEntity);
+        float t = (float)Time.ElapsedTime;
 
         Entities
         .WithAll<FireRendererTag>()
@@ -22,12 +28,21 @@ public class FireRenderSystem : SystemBase
         {
             // Grab the target cell this renderer should be matching
             FireCell currentCell = bufferRef[entityInQueryIndex];
+
+            float actualTemperature = currentCell.FireTemperature < 0.5f ? 0.001f : math.min(currentCell.FireTemperature, 1.0f);
+
+            float noise = actualTemperature < 0.5f ? 0.0f : 
+                Noise(t, 0.02f, 2.0f, entityInQueryIndex * 0.8f) + 
+                Noise(t, 0.03f, 3.0f, entityInQueryIndex * 0.9f) + 
+                Noise(t, 0.04f, 4.0f, entityInQueryIndex * 1.0f);
+            float actualTemperatureWithNoise = actualTemperature + noise;
+
             // Update its scale
-            scale.Value.y = math.clamp(currentCell.FireTemperature, 0.001f, 1.0f);
+            scale.Value.y = actualTemperatureWithNoise;
             // Update its translation
             translation.Value.y = scale.Value.y * 0.5f;
             // Update its color
-            fireColor.Value = math.lerp(new float4(0.0f, 1.0f, 0.0f, 1.0f), new float4(1.0f, 0.0f, 0.0f, 1.0f), currentCell.FireTemperature);
+            fireColor.Value = actualTemperature < 0.5f ? new float4(0.0f, 1.0f, 0.0f, 1.0f) : math.lerp(new float4(1.0f, 1.0f, 0.0f, 1.0f), new float4(1.0f, 0.0f, 0.0f, 1.0f), (actualTemperature * 2.0f) - 1.0f);
         }).ScheduleParallel();
     }
 }
--- Ported/BucketBrigadeDOTS/Assets/src/dots/FireSpreadSystem.cs
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FireSpreadSystem.cs
@@ -16,6 +16,7 @@ public class FireSpreadSystem : SystemBase
         var fireGridEntity = GetSingletonEntity<FireGridSettings>();
         var fireGridSetting = GetComponent<FireGridSettings>(fireGridEntity);
         var bufferPrev = EntityManager.GetBuffer<FireCellHistory>(fireGridEntity);
+        float dt = Time.DeltaTime;
 
         Entities
         .WithReadOnly(bufferPrev)
@@ -23,8 +24,22 @@ public class FireSpreadSystem : SystemBase
         {
             for (int i = 0; i < buffer.Length; ++i)
             {
+                int x = i % (int)fireGridSetting.FireGridResolution.x;
+                int y = i / (int)fireGridSetting.FireGridResolution.x;
+
+                int kernelRadius = 1;
+                int2 rangeX = math.clamp(new int2(x - kernelRadius, x + kernelRadius), 0, (int)fireGridSetting.FireGridResolution.x - 1);
+                int2 rangeY = math.clamp(new int2(y - kernelRadius, y + kernelRadius), 0, (int)fireGridSetting.FireGridResolution.y - 1);
+
                 FireCell cell = buffer[i];
-                cell.FireTemperature = bufferPrev[i].FireTemperaturePrev + 0.01f;
+                cell.FireTemperature = bufferPrev[i].FireTemperaturePrev;
+                for (int v = rangeY.x; v <= rangeY.y; ++v)
+                    for (int u = rangeX.x; u <= rangeX.y; ++u)
+                    {
+                        cell.FireTemperature += bufferPrev[v * (int)fireGridSetting.FireGridResolution.x + u].FireTemperaturePrev * 0.03f * dt;
+                    }
+
+                cell.FireTemperature = math.min(cell.FireTemperature, 1.0f);
                 buffer[i] = cell;
             }
         }).Schedule();
