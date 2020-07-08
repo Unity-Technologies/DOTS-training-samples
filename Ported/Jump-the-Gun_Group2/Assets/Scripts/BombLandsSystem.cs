94e99f747a7dcb83fcb56f1360c16d7fc5521b2e
ThomH-Unity <52419672+ThomH-Unity@users.noreply.github.com>
2020-07-08 09:32:51 +0100

    Added bomblands system

--- Ported/Jump-the-Gun_Group2/Assets/Scripts/BombLandsSystem.cs
+++ Ported/Jump-the-Gun_Group2/Assets/Scripts/BombLandsSystem.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class BombLandsSystem : SystemBase
 {
+
+    private EntityCommandBufferSystem cbs;
+
+    private EntityQuery needsSpawn;
+
+    protected override void OnCreate() {
+        cbs = World.GetExistingSystem<EndInitializationEntityCommandBufferSystem>();
+    }
+
     protected override void OnUpdate()
-    { }
+    {
+        var ecb = cbs.CreateCommandBuffer().ToConcurrent();
+
+        Entities
+           .WithNone<PlayerTag>()
+           .ForEach((int entityInQueryIndex, Entity e, ref NormalisedMoveTime n, in MovementParabola p) => {
+
+               //if the bomb has finished moving
+               if(n.Value > 1) {
+                   //destroy the bomb
+                   ecb.DestroyEntity(entityInQueryIndex, e);
+                   //reduce the height of the terrain at the position of the target
+                   float3 target = p.Target;
+                   //TODO reduce the height of the terrain
+               }
+
+           }).ScheduleParallel();
+
+        cbs.AddJobHandleForProducer(Dependency);
+    }
 }
