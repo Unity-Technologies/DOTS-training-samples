a5f5a371a2a1b82a077569f18a2cbefaf571f788
Bart Pieters <bart.pieters@unity3d.com>
2020-07-08 12:46:11 +0200

    Plant growing system first test

---
+++ Ported/AutoFarmers/Assets/Scripts/Data/FullyGrownPlant_Tag.cs
@@ -0,0 +1,6 @@
+﻿using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct FullyGrownPlant_Tag : IComponentData
+{
+}
---
+++ Ported/AutoFarmers/Assets/Scripts/Systems/PlantGrowingSystem.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public class PlantGrowingSystem : SystemBase
+{
+    private EntityCommandBufferSystem m_CommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        m_CommandBufferSystem = World.GetExistingSystem<EndInitializationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var ecb = m_CommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+        
+        Entities.WithNone<FullyGrownPlant_Tag>().ForEach((int entityInQueryIndex, Entity entity, ref Plant plant) =>
+        {
+            plant.Age += 1;
+            if (plant.Age >= FarmConstants.PlantMaturityAge)
+            {
+                ecb.AddComponent<FullyGrownPlant_Tag>(entityInQueryIndex, entity, new FullyGrownPlant_Tag());
+            }
+        }).ScheduleParallel();
+        
+        m_CommandBufferSystem.AddJobHandleForProducer(Dependency);
+    }
+}
+
+public struct FarmConstants
+{
+    public static int PlantMaturityAge = 40;
+}
