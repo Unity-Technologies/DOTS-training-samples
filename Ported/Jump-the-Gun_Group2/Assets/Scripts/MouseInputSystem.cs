a7f29a9e144e2ccd0ae99a64b5bd3a56def9fc72
FrancescoC-Unity <francescoc@unity3d.com>
2020-07-08 11:33:10 +0200

    Fixed directions

--- Ported/Jump-the-Gun_Group2/Assets/Scripts/MouseInputSystem.cs
+++ Ported/Jump-the-Gun_Group2/Assets/Scripts/MouseInputSystem.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class MouseInputSystem : SystemBase
 {
+    EntityQuery m_PlayerQuery;
+
+    protected override void OnCreate()
+    {
+        // To be used for having input relative to player
+        m_PlayerQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[]
+            {
+                ComponentType.ReadOnly<PlayerTag>(),
+                ComponentType.ReadOnly<Direction>(),
+                ComponentType.ReadOnly<Position>()
+            }
+        });
+    }
+
     protected override void OnUpdate()
     {
         float2 mousePos = ((float3)UnityEngine.Input.mousePosition).xy;
@@ -13,9 +29,16 @@ public class MouseInputSystem : SystemBase
 
         mousePos -= 0.5f;
         mousePos *= 2;
+        // 10 pixel away from center indicates a direction shift.
+        float2 threshold =  1.0f * math.rcp(math.float2(Screen.width, Screen.height));
 
-        Entities.ForEach((ref Direction d) => {
-            d.Value = mousePos;
+        Entities.ForEach((ref Direction d) => 
+        {
+            if (mousePos.x > threshold.x) d.Value.x = 1.0f;
+            if (mousePos.x < threshold.x) d.Value.x = -1.0f;
+            if (mousePos.y > threshold.y) d.Value.y = 1.0f;
+            if (mousePos.y < threshold.y) d.Value.y = -1.0f;
+
         }).Schedule();
     }
 }
