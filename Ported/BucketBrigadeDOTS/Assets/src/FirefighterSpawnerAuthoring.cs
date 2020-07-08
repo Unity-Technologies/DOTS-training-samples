4a279453f8128bc4fa873ac8864e327666837091
Kuba Cupisz <kubacupisz@gmail.com>
2020-07-08 10:26:06 +0200

    Firefighter spawning.

---
+++ Ported/BucketBrigadeDOTS/Assets/src/FirefighterSpawnerAuthoring.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+ 
+[GenerateAuthoringComponent]
+public struct FirefighterSpawner : IComponentData
+{
+    public Entity Prefab;
+    public int CountX;
+    public int CountZ;
+}
