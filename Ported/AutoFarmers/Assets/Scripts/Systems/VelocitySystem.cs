948f51155f827adfda609c0ff37ba1a7deea01db
Chris <chris.mcmath@unity3d.com>
2020-07-08 13:18:54 +0100

    Targetting in Velocity System

--- Ported/AutoFarmers/Assets/Scripts/Systems/VelocitySystem.cs
+++ Ported/AutoFarmers/Assets/Scripts/Systems/VelocitySystem.cs
@@ -1,22 +1,30 @@
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 
-[UpdateInGroup(typeof(SimulationSystemGroup))]
-public class VelocitySystem : SystemBase
+namespace AutoFarmers
 {
-    protected override void OnUpdate()
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    public class VelocitySystem : SystemBase
     {
-        float delta = UnityEngine.Time.deltaTime;
-        
-        Entities
-            .ForEach((Entity entity, ref Velocity velocity, in Target target) =>
-            {
-                //velocity.Value = GetVelocity();
-            }).ScheduleParallel();
-    }
+        protected override void OnUpdate()
+        {
+            float delta = UnityEngine.Time.deltaTime;
+            float speed = 1f;
+            
+            Entities
+                .ForEach((Entity entity, ref Velocity velocity, in Translation translation, in Target target) =>
+                {
+                    Translation targetPosition = GetComponent<Translation>(target.Value);
+                    float3 direction = math.normalize(targetPosition.Value - translation.Value);
+                    float3 targetVelocity = direction * speed;
+                    velocity.Value = math.lerp(velocity.Value, targetVelocity, 0.5f);
+                }).ScheduleParallel();
+        }
 
-    private float2 GetVelocity()
-    {
-        throw new System.NotImplementedException();
+        private float2 GetVelocity()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
