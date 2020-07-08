b2ee331e1323cc40065d90c29c7909173d5ead14
Graphine Charles <charles@graphinesoftware.com>
2020-07-08 15:51:47 +0200

    Namepaces, rocks no longer overlap

--- Ported/AutoFarmers/Assets/Scripts/Data/CellSize.cs
+++ Ported/AutoFarmers/Assets/Scripts/Data/CellSize.cs
@@ -1,8 +1,11 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
 
-[GenerateAuthoringComponent]
-public struct CellSize : IComponentData
+namespace AutoFarmers
 {
-    public int2 Value;
+    [GenerateAuthoringComponent]
+    public struct CellSize : IComponentData
+    {
+        public int2 Value;
+    }
 }
--- Ported/AutoFarmers/Assets/Scripts/Systems/RockSpawningSystem.cs
+++ Ported/AutoFarmers/Assets/Scripts/Systems/RockSpawningSystem.cs
@@ -2,52 +2,105 @@
 using Unity.Mathematics;
 using Unity.Transforms;
 
-[UpdateInGroup(typeof(InitializationSystemGroup))]
-public class RockSpawningSystem : SystemBase
+namespace AutoFarmers
 {
-    private EntityCommandBufferSystem m_CommandBufferSystem;
-    private Random m_Random;
-
-    // Set on spawners to make them go to sleep after init
-    public struct RocksAreInitalizedTag : IComponentData
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    [UpdateAfter(typeof(GridInitializationSystem))]
+    public class RockSpawningSystem : SystemBase
     {
-    };
+        private EntityCommandBufferSystem m_CommandBufferSystem;
+        private Random m_Random;
 
-    protected override void OnCreate()
-    {
-        m_Random = new Random(0x1234567);
-        m_CommandBufferSystem = World.GetExistingSystem<EndInitializationEntityCommandBufferSystem>();
-    }
-
-    protected override void OnUpdate()
-    {
-        var ecb = m_CommandBufferSystem.CreateCommandBuffer();
-        var random = m_Random;
-
-        Entities
-        .WithNone<RocksAreInitalizedTag>()
-        .ForEach((Entity rockSpawner, in RockSpawner spawner) =>
+        // Set on spawners to make them go to sleep after init
+        public struct RocksAreInitalizedTag : IComponentData
         {
-            for (int n=0; n<spawner.NumRocks; n++)
+        };
+
+        protected override void OnCreate()
+        {
+            m_Random = new Random(0x1234567);
+            m_CommandBufferSystem = World.GetExistingSystem<EndInitializationEntityCommandBufferSystem>();
+        }
+
+        static bool IsAreaFree(DynamicBuffer<CellTypeElement> buffer, int2 gridSize, int2 position, int2 areaSize)
+        {
+            for (int j = 0; j < areaSize.y; j++)
             {
-                int2 size = random.NextInt2(spawner.RandomSizeMin, spawner.RandomSizeMax);
-                int2 position = random.NextInt2(new int2(0, 0), new int2(64, 64));
-                var instance = ecb.Instantiate(spawner.RockPrefab);
-                var translation = new float3(position.x, 0.0f, position.y);
-                var scale = new float3(size.x, 1.0f, size.y);
-                translation += scale * 0.5f;
-                ecb.SetComponent(instance, new Translation { Value = translation });
-                ecb.AddComponent(instance, new NonUniformScale { Value = scale });
-                ecb.AddComponent(instance, new CellPosition { Value = position });
-                ecb.AddComponent(instance, new CellSize { Value = size });
-                ecb.AddComponent(instance, new Rock_Tag());
-
-                float health = random.NextFloat(spawner.minHeight, spawner.maxHeight);
-                ecb.AddComponent(instance, new Health { Value = health });
+                for (int i = 0; i < areaSize.x; i++)
+                {
+                    int idx = (position.x + i) + (position.y + j) * gridSize.x;
+                    if ( buffer[idx].Value != CellType.Raw)
+                    {
+                        return false;
+                    }
+                }
             }
-            ecb.AddComponent(rockSpawner, new RocksAreInitalizedTag());
-        }).Schedule();
 
-        m_CommandBufferSystem.AddJobHandleForProducer(Dependency);
+            return true;
+        }
+
+        protected override void OnUpdate()
+        {
+            var ecb = m_CommandBufferSystem.CreateCommandBuffer();
+            var random = m_Random;
+
+            var entity = GetSingletonEntity<Grid>();
+            var typeBuffer = EntityManager.GetBuffer<CellTypeElement>(entity);
+            var entityBuffer = EntityManager.GetBuffer<CellEntityElement>(entity);
+            int2 gridSize = GetSingleton<Grid>().Size;
+
+            Entities
+            .WithNone<RocksAreInitalizedTag>()
+            .ForEach((Entity rockSpawner, in RockSpawner spawner) =>
+            {
+                for (int n = 0; n < spawner.NumRocks; n++)
+                {
+                    int2 size = new int2();
+                    int2 position = new int2();
+
+                    for (int tries =  0; tries < 100; tries++)
+                    {
+                        size = random.NextInt2(spawner.RandomSizeMin, spawner.RandomSizeMax);
+                        position = random.NextInt2(new int2(0, 0), gridSize - size);
+
+                        if ( IsAreaFree(typeBuffer, gridSize, position, size))
+                        {
+                            break;
+                        }
+                    }
+
+                    // Box position account for the position being the minimum but the model we use having the pivot at the center
+                    var translation = new float3(position.x, 0.0f, position.y);
+                    var scale = new float3(size.x, 1.0f, size.y);
+                    translation += scale * 0.5f;
+
+                    // Inset the box
+                    scale.x -= 0.2f;
+                    scale.z -= 0.2f;
+
+                    var instance = ecb.Instantiate(spawner.RockPrefab);
+                    ecb.SetComponent(instance, new Translation { Value = translation });
+                    ecb.AddComponent(instance, new NonUniformScale { Value = scale });
+                    ecb.AddComponent(instance, new CellPosition { Value = position });
+                    ecb.AddComponent(instance, new CellSize { Value = size });
+                    ecb.AddComponent(instance, new Rock_Tag());
+
+                    float health = random.NextFloat(spawner.minHeight, spawner.maxHeight);
+                    ecb.AddComponent(instance, new Health { Value = health });
+   
+                    for (int j = 0; j < size.y; j++)
+                    {
+                        for (int i = 0; i < size.x; i++)
+                        {
+                            int idx = (position.x + i) + (position.y + j) * gridSize.x;
+                            typeBuffer[idx] = new CellTypeElement { Value = CellType.Rock };
+                        }
+                    }
+                }
+                ecb.AddComponent(rockSpawner, new RocksAreInitalizedTag());
+            }).Run();
+
+            m_CommandBufferSystem.AddJobHandleForProducer(Dependency);
+        }
     }
 }
