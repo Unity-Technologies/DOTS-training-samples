e6eb36ef2ae87c751dc67ee87cb6e439cc7fd4ae
Sven Santema <svens@unity3d.com>
2020-07-08 15:32:18 +0200

    Add system to update GridHeight dynamic buffer.

---
+++ Ported/Jump-the-Gun_Group2/Assets/Scripts/UpdateGridStateSystem.cs
@@ -0,0 +1,35 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public class UpdateGridStateSystem : SystemBase
+{
+    EntityQuery m_Query;
+
+    protected override void OnCreate()
+    {
+        m_Query = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[]
+            {
+                ComponentType.ReadOnly<Height>()
+            }
+        });
+        m_Query.AddChangedVersionFilter(new ComponentType(typeof(Height)));
+
+        RequireSingletonForUpdate<GameParams>();
+        RequireSingletonForUpdate<GridTag>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var e = GetSingletonEntity<GridTag>();
+        var gameParams = GetSingleton<GameParams>();
+        var buffer = EntityManager.GetBuffer<GridHeight>(e);
+
+        Entities.ForEach((in Height height, in Position position) =>
+        {
+            var pos = (int2)position.Value.xz;
+            buffer[pos.y * gameParams.TerrainDimensions.x + pos.x] = new GridHeight { Height = height.Value };
+        }).Schedule();
+    }
+}
