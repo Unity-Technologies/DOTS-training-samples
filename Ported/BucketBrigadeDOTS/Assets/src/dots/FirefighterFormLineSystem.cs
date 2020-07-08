cec0c25793ddf0066372dabdb6c1fe452db9e96f
Kuba Cupisz <kubacupisz@gmail.com>
2020-07-08 14:50:31 +0200

    Curved line of firefighters.

--- Ported/BucketBrigadeDOTS/Assets/src/dots/FirefighterFormLineSystem.cs
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FirefighterFormLineSystem.cs
@@ -16,12 +16,15 @@ public class FirefighterFormLineSystem : SystemBase
     {
         float2 src = new float2(0.0f, 0.0f);
         float2 dst = new float2(10.0f, 10.0f);
+        float2 fromTo = (dst - src);
+        float2 normal = new float2(-fromTo.y, fromTo.x);
         
         var ecb = m_ECBSystem.CreateCommandBuffer().ToConcurrent();
 
         Entities.WithNone<Target>().ForEach((int entityInQueryIndex, Entity entity, Firefighter firefighter, FirefighterPositionInLine positionInLine, in Translation2D translation) =>
         {
-            float2 pos = (dst - src) * positionInLine.Value + src;
+            float offset = 0.4f * positionInLine.Value * (1.0f - positionInLine.Value);
+            float2 pos = fromTo * positionInLine.Value + src + offset * normal;
             ecb.AddComponent<Target>(entityInQueryIndex, entity, new Target{ Value = pos });
         }).ScheduleParallel();
 
