d530c11197f0e5f3436928fee12c9aeb6b1900e3
sebastienlagarde <sebastien@unity3d.com>
2020-07-08 14:22:43 +0200

    update parabola math

--- Ported/Jump-the-Gun_Group2/Assets/Scripts/CannonFireSystem.cs
+++ Ported/Jump-the-Gun_Group2/Assets/Scripts/CannonFireSystem.cs
@@ -26,14 +26,17 @@ public class CannonFireSystem : SystemBase
             if (coolDown.Value  < 0.0f)
             {
                 var instance = ecb.Instantiate(entityInQueryIndex, gameParams.CannonBallPrefab);
-                ecb.AddComponent(entityInQueryIndex, instance, new MovementParabola { Origin = position.Value, Target = playerLocation.Value, Parabola = new float3(0.0f, 0.0f, 0.0f) });
+                MovementParabola movementParabole = new MovementParabola { Origin = position.Value, Target = playerLocation.Value, Parabola = new float3(0.0f, 0.0f, 0.0f) };
+                ParabolaMath.Create(position.Value.y, 10.0f, playerLocation.Value.y, out movementParabole.Parabola.x, out movementParabole.Parabola.y, out movementParabole.Parabola.z);
+
+                ecb.AddComponent(entityInQueryIndex, instance, movementParabole);
                 ecb.AddComponent(entityInQueryIndex, instance, new NormalisedMoveTime { Value = 0.0f });
 
                 coolDown.Value = 1.0f;
             }
             else
             {
-                coolDown.Value  -= 0.1f;
+                coolDown.Value  -= 0.05f;
             }
         }).ScheduleParallel();
 
