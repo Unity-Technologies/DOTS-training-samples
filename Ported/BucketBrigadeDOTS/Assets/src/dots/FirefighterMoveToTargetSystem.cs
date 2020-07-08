54924e1af54bd1066eecd22d537e1d3d6021f972
Robert Cupisz <robertcupisz@gmail.com>
2020-07-08 12:24:45 +0200

    Firefighters moving to random targets

--- Ported/BucketBrigadeDOTS/Assets/src/dots/FirefighterMoveToTargetSystem.cs
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FirefighterMoveToTargetSystem.cs
@@ -1,28 +1,25 @@
-﻿using Unity.Entities;
+﻿using System.ComponentModel.Design.Serialization;
+using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
-using Unity.Collections;
 
-public class FirefighterRandomTargetSystem : SystemBase
+public class FirefighterMoveToTargetSystem : SystemBase
 {
-    private Random m_Random;
     private EntityCommandBufferSystem m_ECBSystem;
 
     protected override void OnCreate()
     {
-        m_Random = new Random(0x1234567);
         m_ECBSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
     }
 
     protected override void OnUpdate()
     {
         var ecb = m_ECBSystem.CreateCommandBuffer();
-        var random = m_Random;
 
-        Entities.WithNone<Target>().ForEach((Entity entity, Firefighter firefighter) =>
+        Entities.ForEach((Entity entity, ref Translation2D translation, in Target target) =>
         {
-            float2 pos = random.NextFloat2() * 10;
-            ecb.AddComponent<Target>(entity, new Target{ Value = pos });
+            translation.Value += math.normalize(target.Value - translation.Value) * 0.01f;
+            // ecb.AddComponent<Target>(entity, new Target{ Value = pos });
         }).Schedule();
 
         m_ECBSystem.AddJobHandleForProducer(Dependency);
