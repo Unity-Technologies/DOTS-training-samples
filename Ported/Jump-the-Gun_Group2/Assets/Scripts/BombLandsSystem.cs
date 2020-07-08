9c3f0dc44383fecf4b79693f0c7de6b2a0c91a5a
ThomH-Unity <52419672+ThomH-Unity@users.noreply.github.com>
2020-07-08 10:06:57 +0100

    Added scale to the LTW system and terrainheight modification to the bomb lands system

--- Ported/Jump-the-Gun_Group2/Assets/Scripts/BombLandsSystem.cs
+++ Ported/Jump-the-Gun_Group2/Assets/Scripts/BombLandsSystem.cs
@@ -1,21 +1,54 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
-using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
 
 public class BombLandsSystem : SystemBase
 {
 
     private EntityCommandBufferSystem cbs;
 
-    private EntityQuery needsSpawn;
+    private EntityQuery bombQuery;
 
     protected override void OnCreate() {
+
+        bombQuery = GetEntityQuery(new EntityQueryDesc {
+            All = new[]
+            {
+                ComponentType.ReadOnly<MovementParabola>(),
+            },
+            None = new[]
+            {
+                ComponentType.ReadOnly<PlayerTag>(),
+            }
+        });
+
         cbs = World.GetExistingSystem<EndInitializationEntityCommandBufferSystem>();
     }
 
     protected override void OnUpdate()
     {
         var ecb = cbs.CreateCommandBuffer().ToConcurrent();
+
+        var bombPosition = bombQuery.ToComponentDataArrayAsync<Position>(Allocator.TempJob, out var bombPositionHandle);
+        var bombTime = bombQuery.ToComponentDataArrayAsync<NormalisedMoveTime>(Allocator.TempJob, out var bombTimeHandle);
+
+        Dependency = JobHandle.CombineDependencies(Dependency, bombPositionHandle);
+        Dependency = JobHandle.CombineDependencies(Dependency, bombTimeHandle);
+
+        Entities
+           .WithDeallocateOnJobCompletion(bombPosition)
+           .WithDeallocateOnJobCompletion(bombTime)
+           //for all the tiles
+           .ForEach((ref Height height, in Position position, in Color c) => {
+               //for all the bombs
+               for (int i = 0; i < bombPosition.Length; ++i) {
+                   if (bombTime[i].Value > 1 && math.distance(bombPosition[i].Value, position.Value) < 0.01f) {
+                       height.Value -= 0.3f;
+                   }
+               }
+
+           }).ScheduleParallel();
 
         Entities
            .WithNone<PlayerTag>()
@@ -25,9 +58,6 @@ public class BombLandsSystem : SystemBase
                if(n.Value > 1) {
                    //destroy the bomb
                    ecb.DestroyEntity(entityInQueryIndex, e);
-                   //reduce the height of the terrain at the position of the target
-                   float3 target = p.Target;
-                   //TODO reduce the height of the terrain
                }
 
            }).ScheduleParallel();
