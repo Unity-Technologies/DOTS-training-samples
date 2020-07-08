51484e276ffb1c37260a8d7fead39aeb69aa4d5d
Kuba Cupisz <kubacupisz@gmail.com>
2020-07-08 14:10:42 +0200

    Added FirefighterFormLineSystem and disabled the random one.

--- Ported/BucketBrigadeDOTS/Assets/src/dots/FirefighterFormLineSystem.cs
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FirefighterFormLineSystem.cs
@@ -3,7 +3,7 @@ using Unity.Mathematics;
 using Unity.Transforms;
 using Unity.Collections;
 
-public class FirefighterRandomTargetSystem : SystemBase
+public class FirefighterFormLineSystem : SystemBase
 {
     private EntityCommandBufferSystem m_ECBSystem;
 
@@ -14,13 +14,16 @@ public class FirefighterRandomTargetSystem : SystemBase
 
     protected override void OnUpdate()
     {
-        return;
+        float2 src = new float2(0.0f, 0.0f);
+        float2 dst = new float2(10.0f, 10.0f);
+        
         var ecb = m_ECBSystem.CreateCommandBuffer().ToConcurrent();
 
         Entities.WithNone<Target>().ForEach((int entityInQueryIndex, Entity entity, Firefighter firefighter, in Translation2D translation) =>
         {
             var rand = new Random((uint)((translation.Value.x * 10000 + translation.Value.y * 100 + 1 )));
-            float2 pos = rand.NextFloat2(0, 10) - 5;
+            float normalizedPosOnLine = rand.NextFloat();
+            float2 pos = (dst - src) * normalizedPosOnLine + src;
             ecb.AddComponent<Target>(entityInQueryIndex, entity, new Target{ Value = pos });
         }).ScheduleParallel();
 
