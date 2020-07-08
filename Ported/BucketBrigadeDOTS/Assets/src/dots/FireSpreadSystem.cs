ec03d663d4dac4f921d9afa037a8fdf9bdb8c716
Anis <anis@unity3d.com>
2020-07-08 12:29:10 +0200

    empty fire spread system

---
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FireSpreadSystem.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class FireSpreadSystem : SystemBase
+{
+    private EntityCommandBufferSystem m_CommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        m_CommandBufferSystem = World.GetExistingSystem<EndInitializationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        //var fireGridEntity = GetSingletonEntity<FireGridSettings>();
+        //var bufferRef = EntityManager.GetBuffer<FireCell>(fireGridEntity);
+        /*
+        Entities
+        .ForEach((int entityInQueryIndex) =>
+        {
+
+        }).Schedule();
+        */
+        //m_CommandBufferSystem.AddJobHandleForProducer(Dependency);
+    }
+}
