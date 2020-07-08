ca0d7b686d43297bf1412324fffdbd845e2c4236
FrancescoC-Unity <francescoc@unity3d.com>
2020-07-08 11:06:23 +0200

    Simple Movement system

--- Ported/Jump-the-Gun_Group2/Assets/Scripts/MovementSystem.cs
+++ Ported/Jump-the-Gun_Group2/Assets/Scripts/MovementSystem.cs
@@ -5,5 +5,13 @@ using UnityEngine;
 public class MovementSystem : SystemBase
 {
     protected override void OnUpdate()
-    { }
+    {
+        float deltaTime = Time.DeltaTime;
+        Entities.ForEach((ref Position pos, ref NormalisedMoveTime normalisedMoveTime, in MovementParabola movementParabola) =>
+        {
+            pos.Value = math.lerp(movementParabola.Origin, movementParabola.Target, math.saturate(normalisedMoveTime.Value));
+            normalisedMoveTime.Value += deltaTime;
+
+        }).ScheduleParallel();
+    }
 }
