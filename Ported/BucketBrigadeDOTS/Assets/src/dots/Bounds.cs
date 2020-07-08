ad7780131897fadc2a330ee99c14e6ef84ac7db4
Julien Fryer <julienf@unity3d.com>
2020-07-08 10:36:55 +0200

    FireGrid data description

---
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/Bounds.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct Bounds : IComponentData
+{
+    public float2 BoundsCenter;
+    public float2 BoundsExtent;
+}
---
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FireSpawnSystem.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+
+public class FireSpawnSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+        var fireGridSetting = GetSingleton<FireGridSettings>();
+        var fireGridEntity = GetSingletonEntity<FireGridSettings>();
+        var fireGrid = EntityManager.AddBuffer<FireCell>(fireGridEntity);
+        fireGrid.Capacity = fireGridSetting.FireGridResolution.x * fireGridSetting.FireGridResolution.y;
+    }
+
+    protected override void OnUpdate()
+    {
+             
+    }
+}
