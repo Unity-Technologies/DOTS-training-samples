338927a6d597f2a5ce84c8c8471cba9d3210df1d
Sven Santema <svens@unity3d.com>
2020-07-08 13:57:45 +0200

    Added player to scene. Added camera lookat player.

--- Ported/Jump-the-Gun_Group2/Assets/Scripts/CannonLookAtSystem.cs
+++ Ported/Jump-the-Gun_Group2/Assets/Scripts/CannonLookAtSystem.cs
@@ -1,9 +1,19 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
-using UnityEngine;
 
 public class CannonLookAtSystem : SystemBase
 {
     protected override void OnUpdate()
-    { }
+    {
+        var playerEntity = GetSingletonEntity<PlayerTag>();
+        var playerLocation = EntityManager.GetComponentData<Position>(playerEntity).Value.xz;
+
+        Entities
+            .ForEach((ref Rotation rotation, in Position position) => 
+            {
+                var direction = playerLocation - position.Value.xz;
+                var rot = direction.y < 0 ? math.PI : 0f;
+                rotation.Value = rot + math.atan(direction.x / direction.y);
+            }).ScheduleParallel();
+    }
 }
