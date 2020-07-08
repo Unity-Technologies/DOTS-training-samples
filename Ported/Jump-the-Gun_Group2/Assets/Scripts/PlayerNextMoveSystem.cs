bc7f09dbbeadff5c2655ee73626c4f9c5c13bd30
FrancescoC-Unity <francescoc@unity3d.com>
2020-07-08 10:14:12 +0200

    Player movement system (untested as we don't have Movement yet)

--- Ported/Jump-the-Gun_Group2/Assets/Scripts/PlayerNextMoveSystem.cs
+++ Ported/Jump-the-Gun_Group2/Assets/Scripts/PlayerNextMoveSystem.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class PlayerNextMoveSystem : SystemBase
 {
+    EntityQuery m_PlayerQuery;
+
+    protected override void OnCreate()
+    {
+        m_PlayerQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[]
+            {
+                ComponentType.ReadOnly<PlayerTag>(),
+                ComponentType.ReadOnly<Direction>()
+            }
+        });
+    }
+
     protected override void OnUpdate()
-    { }
+    {
+        // Assume it is scaled later
+        float2 cubeSize = math.float2(1, 1);
+
+        Entities.ForEach((ref MovementParabola movement, ref NormalisedMoveTime normalisedMoveTime, in PlayerTag player, in Direction direction, in Position pos) =>
+        {
+            if(normalisedMoveTime.Value >= 1.0f)
+            {
+                movement.Origin = pos.Value;
+                movement.Target = pos.Value + math.float3(cubeSize * direction.Value, 0);
+                normalisedMoveTime.Value = 0.0f;
+            }
+        }).ScheduleParallel();
+
+    }
 }
