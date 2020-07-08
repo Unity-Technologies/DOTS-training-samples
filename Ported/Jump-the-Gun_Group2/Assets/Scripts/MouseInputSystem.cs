9e6e7ac9eecf824bb5055df814a8dfcb9d8fe0e3
ThomH-Unity <52419672+ThomH-Unity@users.noreply.github.com>
2020-07-08 09:13:55 +0100

    Adde LTW system

--- Ported/Jump-the-Gun_Group2/Assets/Scripts/MouseInputSystem.cs
+++ Ported/Jump-the-Gun_Group2/Assets/Scripts/MouseInputSystem.cs
@@ -6,10 +6,16 @@ public class MouseInputSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        float2 mousePos2 = ((float3)UnityEngine.Input.mousePosition).xy;
+        float2 mousePos = ((float3)UnityEngine.Input.mousePosition).xy;
+
+        mousePos.x /= Screen.width;
+        mousePos.y /= Screen.height;
+
+        mousePos -= 0.5f;
+        mousePos *= 2;
 
         Entities.ForEach((ref Direction d) => {
-            d.Value = mousePos2;
+            d.Value = mousePos;
         }).Schedule();
     }
 }
