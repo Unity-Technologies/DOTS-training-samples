715bca9ad8fb09dd5ef66c0c3972d0934ba2dc6e
Sven Santema <svens@unity3d.com>
2020-07-08 09:41:17 +0200

    Bundled Origin, Target and Parabola into single component.

---
+++ Ported/Jump-the-Gun_Group2/Assets/Scripts/Components/MovementParabola.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct MovementParabola : IComponentData
+{
+    public float3 Origin;
+    public float3 Target;
+    public float3 Parabola;
+}
--- Ported/Jump-the-Gun_Group2/Assets/Scripts/Components/Target.cs
+++
@@ -1,7 +0,0 @@
-﻿using Unity.Entities;
-using Unity.Mathematics;
-
-public struct Target : IComponentData
-{
-    public float3 Value;
-}
