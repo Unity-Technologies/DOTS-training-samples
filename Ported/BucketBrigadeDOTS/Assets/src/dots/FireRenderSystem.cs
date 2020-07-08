f74a9229671336dfc7e9575e6d4d472377ed306d
Anis <anis@unity3d.com>
2020-07-08 12:19:44 +0200

    Adding the color to the fire drawing

---
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FireRenderSystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class FireRenderSystem : SystemBase
+{
+    private EntityCommandBufferSystem m_CommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        m_CommandBufferSystem = World.GetExistingSystem<EndInitializationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var fireGridEntity = GetSingletonEntity<FireGridSettings>();
+        var bufferRef = EntityManager.GetBuffer<FireCell>(fireGridEntity);
+        Entities
+        .WithAll<FireRendererTag>()
+        .ForEach((int entityInQueryIndex, ref NonUniformScale scale, ref Translation translation, ref FireColor fireColor) =>
+        {
+            FireCell currentCell = bufferRef[entityInQueryIndex];
+            scale.Value.y = math.clamp(currentCell.FireTemperature, 0.001f, 1.0f);
+            translation.Value.y = currentCell.FireTemperature * 0.5f;
+            fireColor.Value = math.lerp(new float4(0.0f, 1.0f, 0.0f, 1.0f), new float4(1.0f, 0.0f, 0.0f, 1.0f), currentCell.FireTemperature);
+        }).Schedule();
+        m_CommandBufferSystem.AddJobHandleForProducer(Dependency);
+    }
+}
---
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FireRendererTag.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+// Fire simulation grid settings
+[GenerateAuthoringComponent]
+public struct FireRendererTag : IComponentData
+{
+}
