2db3c9b15fd188eb36d6de299aa44a678dbe2ed7
Anis <anis@unity3d.com>
2020-07-08 12:22:49 +0200

    Add comments

--- Ported/BucketBrigadeDOTS/Assets/src/dots/FireRenderSystem.cs
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FireRenderSystem.cs
@@ -19,9 +19,13 @@ public class FireRenderSystem : SystemBase
         .WithAll<FireRendererTag>()
         .ForEach((int entityInQueryIndex, ref NonUniformScale scale, ref Translation translation, ref FireColor fireColor) =>
         {
+            // Grab the target cell this renderer should be matching
             FireCell currentCell = bufferRef[entityInQueryIndex];
+            // Update its scale
             scale.Value.y = math.clamp(currentCell.FireTemperature, 0.001f, 1.0f);
+            // Update its translation
             translation.Value.y = currentCell.FireTemperature * 0.5f;
+            // Update its color
             fireColor.Value = math.lerp(new float4(0.0f, 1.0f, 0.0f, 1.0f), new float4(1.0f, 0.0f, 0.0f, 1.0f), currentCell.FireTemperature);
         }).Schedule();
         m_CommandBufferSystem.AddJobHandleForProducer(Dependency);
