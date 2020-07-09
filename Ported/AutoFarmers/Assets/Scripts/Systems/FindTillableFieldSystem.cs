99975ab0b4670e90e2cfd97565524433d1e373e3
Jonas El Sayeh Khalil <jonas.khalil@unity3d.com>
2020-07-09 09:03:39 +0200

    Generate fields to till in parallel

--- Ported/AutoFarmers/Assets/Scripts/Systems/FindTillableFieldSystem.cs
+++ Ported/AutoFarmers/Assets/Scripts/Systems/FindTillableFieldSystem.cs
@@ -8,43 +8,54 @@ namespace AutoFarmers
 {
     public class FindTillableFieldSystem : SystemBase
     {
+        private EntityCommandBufferSystem _entityCommandBufferSystem;
+
+        protected override void OnCreate()
+        {
+            _entityCommandBufferSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+        }
+
         protected override void OnUpdate()
         {
+            EntityCommandBuffer.Concurrent ecb = _entityCommandBufferSystem.CreateCommandBuffer().ToConcurrent();
+            int2 GridSize = GetSingleton<Grid>().Size;
             Entities
                 .WithAll<TillField_Intent>()
                 .WithNone<TillRect>()
-                .WithStructuralChanges()
-                .ForEach((Entity entity) =>
+                .ForEach((int entityInQueryIndex, Entity entity, ref RandomSeed randomSeed) =>
                 {
-                    int2 GridSize = GetSingleton<Grid>().Size;
                     int2 fieldPos, fieldDimensions;
 
                     // Find a suitable area to till on the farm
                     {
                         // For now, just any random rectangle that fits on the farm
                         // Without considering rocks, shops, or anything that's already tilled
+                        Random random = new Random(randomSeed.Value);
+                        fieldPos = random.NextInt2(new int2(0,0), GridSize);
+                        fieldDimensions = random.NextInt2(new int2(2, 2), new int2(5, 5));
+                        randomSeed.Value = random.state;
 
-                        fieldPos.x = UnityEngine.Random.Range(0, GridSize.x);
+                        /*fieldPos.x = UnityEngine.Random.Range(0, GridSize.x);
                         fieldPos.y = UnityEngine.Random.Range(0, GridSize.y);
 
                         fieldDimensions.x = UnityEngine.Random.Range(2, 5);
-                        fieldDimensions.y = UnityEngine.Random.Range(2, 5);
+                        fieldDimensions.y = UnityEngine.Random.Range(2, 5);*/
 
                         fieldDimensions.x = math.min(fieldDimensions.x, GridSize.x - fieldPos.x);
                         fieldDimensions.y = math.min(fieldDimensions.y, GridSize.y - fieldPos.y);
                     }
 
-                    EntityManager.RemoveComponent<PathFindingTargetReached_Tag>(entity);
-                    EntityManager.AddComponent<TillRect>(entity);
-                    SetComponent(entity, new TillRect
+                    ecb.RemoveComponent<PathFindingTargetReached_Tag>(entityInQueryIndex, entity);
+                    ecb.AddComponent<TillRect>(entityInQueryIndex, entity, new TillRect
                     {
                         X = fieldPos.x,
                         Y = fieldPos.y,
                         Width = fieldDimensions.x,
                         Height = fieldDimensions.y
                     });
-                    
-                }).Run();
+                }).ScheduleParallel();
+
+            _entityCommandBufferSystem.AddJobHandleForProducer(Dependency);
         }
     }
 }
