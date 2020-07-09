c5ead5dbb5fdf291ef775d7169ce88c51dc4d861
Graphine Charles <charles@graphinesoftware.com>
2020-07-09 13:43:22 +0200

    Add conversion to/from Translation

--- Ported/AutoFarmers/Assets/Scripts/Data/CellPosition.cs
+++ Ported/AutoFarmers/Assets/Scripts/Data/CellPosition.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 
 namespace AutoFarmers
 {
@@ -7,5 +8,26 @@ namespace AutoFarmers
     public struct CellPosition : IComponentData
     {
         public int2 Value;
+
+        public static float3 ToTranslation(int2 Value)
+        {
+            return new float3(Value.x, 0.0f, Value.y) + new float3(0.5f, 0.0f, 0.5f);
+        }
+
+        public Translation ToTranslation()
+        {
+            return new Translation { Value = ToTranslation(Value) };
+        }
+
+        public static int2 FromTranslation(float3 t)
+        {
+            var bias = t - new float3(0.5f, 0.0f, 0.5f);
+            return new int2((int)bias.x, (int)bias.z);
+        }
+
+        public void FromTranslation(Translation t)
+        {
+            Value = FromTranslation(t.Value);
+        }
     }
 }
