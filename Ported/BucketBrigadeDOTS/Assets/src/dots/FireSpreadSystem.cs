5bac1a4c24857667494403a53a811bb3803cd10a
Julien Fryer <julienf@unity3d.com>
2020-07-09 10:25:07 +0200

    Jobify fire sim

--- Ported/BucketBrigadeDOTS/Assets/src/dots/FireSpreadSystem.cs
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FireSpreadSystem.cs
@@ -1,4 +1,7 @@
-﻿using Unity.Entities;
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 
@@ -8,38 +11,56 @@ public class FireSpreadSystem : SystemBase
     {
     }
 
+    [BurstCompile]
+    struct SpreadFireJob : IJobParallelFor
+    {
+        public uint2 resolution;
+        [ReadOnly]
+        public NativeArray<FireCellHistory> bufferSrc; // RO
+        public NativeArray<FireCell> bufferDst;
+        public float timeStep;
+
+        public void Execute(int chunkIndex)
+        { 
+            int i = chunkIndex;
+
+            int x = i % (int)resolution.x;
+            int y = i / (int)resolution.x;
+
+            int kernelRadius = 1;
+            int2 rangeX = math.clamp(new int2(x - kernelRadius, x + kernelRadius), 0, (int)resolution.x - 1);
+            int2 rangeY = math.clamp(new int2(y - kernelRadius, y + kernelRadius), 0, (int)resolution.y - 1);
+
+            FireCell cell = bufferDst[i];
+            cell.FireTemperature = bufferSrc[i].FireTemperaturePrev;
+            for (int v = rangeY.x; v <= rangeY.y; ++v)
+                for (int u = rangeX.x; u <= rangeX.y; ++u)
+                {
+                    float targetHistoryTemp = bufferSrc[v * (int)resolution.x + u].FireTemperaturePrev;
+                    cell.FireTemperature += targetHistoryTemp > 0.2f ? targetHistoryTemp * 0.03f * timeStep : 0.0f;
+                }
+
+            cell.FireTemperature = math.min(cell.FireTemperature, 1.0f);
+            bufferDst[i] = cell;
+        }
+    }
+
     protected override void OnUpdate()
     {
         var fireGridEntity = GetSingletonEntity<FireGridSettings>();
         var fireGridSetting = GetComponent<FireGridSettings>(fireGridEntity);
-        var bufferPrev = EntityManager.GetBuffer<FireCellHistory>(fireGridEntity);
-        float dt = Time.DeltaTime;
+        var bufferSrc = EntityManager.GetBuffer<FireCellHistory>(fireGridEntity);
+        var bufferDst = EntityManager.GetBuffer<FireCell>(fireGridEntity);
 
-        Entities
-        .WithReadOnly(bufferPrev)
-        .ForEach((DynamicBuffer<FireCell> buffer) =>
+        var fireSim = new SpreadFireJob
         {
-            for (int i = 0; i < buffer.Length; ++i)
-            {
-                int x = i % (int)fireGridSetting.FireGridResolution.x;
-                int y = i / (int)fireGridSetting.FireGridResolution.x;
+            resolution = fireGridSetting.FireGridResolution,
+            bufferSrc = bufferSrc.AsNativeArray(),
+            bufferDst = bufferDst.AsNativeArray(),
+            timeStep = Time.DeltaTime,
+        };
 
-                int kernelRadius = 1;
-                int2 rangeX = math.clamp(new int2(x - kernelRadius, x + kernelRadius), 0, (int)fireGridSetting.FireGridResolution.x - 1);
-                int2 rangeY = math.clamp(new int2(y - kernelRadius, y + kernelRadius), 0, (int)fireGridSetting.FireGridResolution.y - 1);
-
-                FireCell cell = buffer[i];
-                cell.FireTemperature = bufferPrev[i].FireTemperaturePrev;
-                for (int v = rangeY.x; v <= rangeY.y; ++v)
-                    for (int u = rangeX.x; u <= rangeX.y; ++u)
-                    {
-                        float targetHistoryTemp = bufferPrev[v * (int)fireGridSetting.FireGridResolution.x + u].FireTemperaturePrev;
-                        cell.FireTemperature += targetHistoryTemp > 0.2f ? targetHistoryTemp * 0.03f * dt : 0.0f;
-                    }
-
-                cell.FireTemperature = math.min(cell.FireTemperature, 1.0f);
-                buffer[i] = cell;
-            }
-        }).Schedule();
+        JobHandle jobHandle = fireSim.Schedule(bufferDst.Length, 256);
+        jobHandle.Complete();
     }
 }
