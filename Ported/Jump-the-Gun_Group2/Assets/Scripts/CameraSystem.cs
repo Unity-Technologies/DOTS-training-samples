93ee4afccada6e4964ea83cc47520f9ff6460791
ThomH-Unity <52419672+ThomH-Unity@users.noreply.github.com>
2020-07-09 09:17:06 +0100

    added camera follow system, set camera origin position in main scene

---
+++ Ported/Jump-the-Gun_Group2/Assets/Scripts/CameraSystem.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public class CameraSystem : SystemBase {
+
+    float3 offset;
+
+    protected override void OnCreate() {
+        RequireSingletonForUpdate<PlayerTag>();
+        offset = UnityEngine.Camera.main.transform.position;
+    }
+
+    protected override void OnUpdate() {
+        var e = GetSingletonEntity<PlayerTag>();
+        var pos = GetComponent<Position>(e).Value;
+        pos.y = 0;
+        UnityEngine.Camera.main.transform.position = pos + offset;
+    }
+}
