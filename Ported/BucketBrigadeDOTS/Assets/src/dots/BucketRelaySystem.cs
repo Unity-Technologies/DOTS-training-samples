962f7bd5da5e1c71ad3c5ee581b18e6bb5e3ec2b
Kuba Cupisz <kubacupisz@gmail.com>
2020-07-09 10:25:48 +0200

    A stub for the bucket relay system.

---
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/BucketRelaySystem.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Collections;
+
+public class BucketRelaySystem : SystemBase
+{
+    private EntityCommandBufferSystem m_ECBSystem;
+
+    protected override void OnCreate()
+    {
+        m_ECBSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var ecb = m_ECBSystem.CreateCommandBuffer().ToConcurrent();
+        
+        // Entities
+        //     .ForEach()
+
+        m_ECBSystem.AddJobHandleForProducer(Dependency);
+    }
+}
