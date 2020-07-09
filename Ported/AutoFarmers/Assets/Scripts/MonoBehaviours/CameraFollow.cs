b19a5fa2cfae19f1468da6dd2f4da8afc11e95c6
Chris McMath <chris.mcmath@unity3d.com>
2020-07-09 14:29:51 +0100

    Camera following (as per original)

--- Ported/AutoFarmers/Assets/Scripts/MonoBehaviours/CameraFollow.cs
+++ Ported/AutoFarmers/Assets/Scripts/MonoBehaviours/CameraFollow.cs
@@ -10,12 +10,14 @@ public class CameraFollow : MonoBehaviour
 	public float mouseSensitivity;
 	
     private EntityManager _manager;
-    private EntityQuery _query;
+    private EntityQuery _cameraFollowQuery;
+    private EntityQuery _farmerQuery;
 
     private void Awake()
     {
         _manager = World.DefaultGameObjectInjectionWorld.EntityManager;
-        _query = _manager.CreateEntityQuery(ComponentType.ReadOnly<CameraFollow_Tag>());
+        _cameraFollowQuery = _manager.CreateEntityQuery(ComponentType.ReadOnly<CameraFollow_Tag>());
+        _farmerQuery = _manager.CreateEntityQuery(ComponentType.ReadWrite<Farmer_Tag>());
     }
     
 	private void Start () 
@@ -25,26 +27,33 @@ public class CameraFollow : MonoBehaviour
     
     private void LateUpdate()
     {
-        var entities = _query.ToEntityArray(Allocator.TempJob);
+        NativeArray<Entity> entities = _cameraFollowQuery.ToEntityArray(Allocator.TempJob);
 
         if (entities.Length < 1)
         {
 	        entities.Dispose();
-	        return;
+	        
+			NativeArray<Entity> farmerEntities = _farmerQuery.ToEntityArray(Allocator.TempJob);
+
+			if (farmerEntities.Length > 0)
+			{
+				_manager.AddComponent<CameraFollow_Tag>(farmerEntities[0]);
+			}
+			
+			farmerEntities.Dispose();
+			return;
         }
 
         Translation translation = _manager.GetComponentData<Translation>(entities[0]);
         UpdateCameraPosition(translation.Value);
 
-        transform.position = translation.Value;
-        
         entities.Dispose();
     }
 	
 	void UpdateCameraPosition(Vector3 pos)
 	{
-		viewAngles.x += Input.GetAxis("Horizontal") * mouseSensitivity/Screen.height;
-		viewAngles.y -= Input.GetAxis("Vertical") * mouseSensitivity/Screen.height;
+		viewAngles.x += Input.GetAxis("X") * mouseSensitivity/Screen.height;
+		viewAngles.y -= Input.GetAxis("Y") * mouseSensitivity/Screen.height;
 		viewAngles.y = Mathf.Clamp(viewAngles.y,7f,80f);
 		viewAngles.x -= Mathf.Floor(viewAngles.x / 360f) * 360f;
 		transform.rotation = Quaternion.Euler(viewAngles.y,viewAngles.x,0f);
