66e7d6deda907b3ffbef2724802f0f883abc0404
Kuba Cupisz <kubacupisz@gmail.com>
2020-07-09 11:19:51 +0200

    A non-working version of adding a bucket to the first firefighter. Some issue with structural changes.

--- Ported/BucketBrigadeDOTS/Assets/src/dots/BucketSpawnSystem.cs
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/BucketSpawnSystem.cs
@@ -8,14 +8,43 @@ using UnityEngine;
 public class BucketSpawnSystem : SystemBase
 {
     private EntityQuery m_WaterBucketSpawnerQuery;
+    private EntityQuery m_WaterBucketQuery;
+    private EntityQuery m_FirefighterFullTagQuery;
 
     protected override void OnCreate()
     {
         RequireForUpdate(m_WaterBucketSpawnerQuery);
+        
+        m_WaterBucketQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new ComponentType[]
+            {
+                typeof(WaterBucketTag),
+            }
+        });
+        
+        m_FirefighterFullTagQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new ComponentType[]
+            {
+                typeof(FirefighterFullTag),
+            }
+        });
     }
     
     protected override void OnUpdate()
     {
+        Debug.Log("BucketSpawnSystem 0");
+        
+        Entity firstFirefighter = Entity.Null;
+        using (var firefighterEntities = m_FirefighterFullTagQuery.ToEntityArray(Allocator.TempJob))
+        {
+            if (firefighterEntities.Length == 0)
+                return;
+
+            firstFirefighter = firefighterEntities[0];
+        }
+
         Entities.WithStructuralChanges()
             .WithStoreEntityQueryInField(ref m_WaterBucketSpawnerQuery)
             .ForEach((Entity entity, in WaterBucketSpawner spawner, in LocalToWorld ltw) =>
@@ -26,9 +55,22 @@ public class BucketSpawnSystem : SystemBase
                     SetComponent<Translation2D>(instance, new Translation2D { Value = 0 });
                     EntityManager.AddComponent<WaterBucketTag>(instance);
                 }
-                
+
                 EntityManager.DestroyEntity(entity);
             }).Run();
+
+        Debug.Log("BucketSpawnSystem 1");
+        
+        Entity firstWaterBucket = Entity.Null;
+        using (var waterBucketEntities = m_WaterBucketQuery.ToEntityArray(Allocator.TempJob))
+        {
+            if (waterBucketEntities.Length == 0)
+                return;
+        
+            firstWaterBucket = waterBucketEntities[0];
+        
+            //EntityManager.AddComponentData(firstFirefighter, new WaterBucketID { Value = firstWaterBucket });
+        }
 
         Entities
             .WithStructuralChanges()
@@ -36,7 +78,12 @@ public class BucketSpawnSystem : SystemBase
             .ForEach((int entityInQueryIndex, Entity entity) =>
             {
                 if (entityInQueryIndex == 0)
+                {
                     EntityManager.AddComponent<WaterBucketID>(entity);
+                    SetComponent(entity, new WaterBucketID { Value = firstWaterBucket });
+                }
             }).Run();
+        
+        Debug.Log("BucketSpawnSystem 2");
     }
 }
