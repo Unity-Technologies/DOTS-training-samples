61781d4dccffddb200080f9333b83eece8432fb2
Robert Cupisz <robertcupisz@gmail.com>
2020-07-09 16:46:29 +0200

    Move the bucket along

---
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/BucketMoveSystem.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public class BucketMoveSystem : SystemBase
+{
+    private EntityCommandBufferSystem m_ECBSystem;
+
+    protected override void OnCreate()
+    {
+        m_ECBSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var ecb = m_ECBSystem.CreateCommandBuffer().ToConcurrent();
+
+        Entities.ForEach((int entityInQueryIndex, Entity entity, in WaterBucketID waterBucketID, in Translation2D translation) =>
+        {
+            ecb.SetComponent(entityInQueryIndex, waterBucketID.Value, translation);
+        }).ScheduleParallel();
+
+        m_ECBSystem.AddJobHandleForProducer(Dependency);
+    }
+}
--- Ported/BucketBrigadeDOTS/Assets/src/dots/TransformSystem2D.cs
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/TransformSystem2D.cs
@@ -13,11 +13,26 @@ public class TransformSystem2D : SystemBase
         // so this system will effectively be presentation layer logic and will need a different name
         
         Entities
+            .WithAll<Firefighter>()
             .WithChangeFilter<Translation2D>()
             .ForEach((ref LocalToWorld localToWorld, in Translation2D translation2D) =>
         {
             var trans = float4x4.Translate(new float3(translation2D.Value.x, 0, translation2D.Value.y));
             var scale = float4x4.Scale(firefighterScale);
+            localToWorld.Value = math.mul(trans, scale);
+        }).ScheduleParallel();
+
+
+        float raisedBucketHeight = 0.5f;
+        float3 bucketScale = new float3(0.3f, 0.3f, 0.3f);
+
+        Entities
+            .WithAll<WaterBucketTag>()
+            .WithChangeFilter<Translation2D>()
+            .ForEach((ref LocalToWorld localToWorld, in Translation2D translation2D) =>
+        {
+            var trans = float4x4.Translate(new float3(translation2D.Value.x, raisedBucketHeight, translation2D.Value.y));
+            var scale = float4x4.Scale(bucketScale);
             localToWorld.Value = math.mul(trans, scale);
         }).ScheduleParallel();
     }
