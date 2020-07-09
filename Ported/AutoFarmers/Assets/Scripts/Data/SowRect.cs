98bd2008f3f10bf74f06a2ad95b859dedc44b801
Bart Pieters <bart.pieters@unity3d.com>
2020-07-09 10:54:52 +0200

    First steps in sowing system

---
+++ Ported/AutoFarmers/Assets/Scripts/Data/SowRect.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+
+// This is basicaly just a rect, so maybe we should rename and merge it later with the TillRect
+public struct SowRect : IComponentData
+{
+    public int X;
+    public int Y;
+    public int Width;
+    public int Height;
+}
