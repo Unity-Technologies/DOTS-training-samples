297457c6e9c53f68c73644d3f89c49cabf377c4e
Anis <anis@unity3d.com>
2020-07-10 13:19:07 +0200

    Adding some comments

--- Ported/BucketBrigadeDOTS/Assets/src/dots/FireGrid.cs
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FireGrid.cs
@@ -2,10 +2,13 @@
 using Unity.Mathematics;
 
 // Fire simulation grid settings
+
 [GenerateAuthoringComponent]
 public struct FireGridSettings : IComponentData
 {
+	// The resolution of the grid
     public uint2 FireGridResolution;
+    // Current mip to visualize (-1 -> numMips -1)
     public int MipDebugIndex;
     public float FlashPoint;
     public float HeatTransferRate;
@@ -20,7 +23,7 @@ public struct FireCell : IBufferElementData
     public float FireTemperature;
 }
 
-// TODO Bad
+// Not as bad as we thought
 public struct FireCellHistory : IBufferElementData
 {
     public float FireTemperaturePrev;
@@ -33,8 +36,15 @@ public struct FireCellFlag : IBufferElementData
 
 public struct FireGridMipLevelData : IBufferElementData
 {
+	// Resolution of the mip level
     public uint2 dimensions;
+    // Offset of the mip data in the buffer
     public uint offset;
+    // World space cell size
     public float2 cellSize;
+    // The position of the center of 
+    // bottom left corner of the grid
     public float2 minCellPosition;
 }
+
+
--- Ported/BucketBrigadeDOTS/Assets/src/dots/FireGridSpawner.cs
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/FireGridSpawner.cs
@@ -5,9 +5,14 @@ using Unity.Mathematics;
 [GenerateAuthoringComponent]
 public struct FireGridSpawner : IComponentData
 {
+	// Prefab for the fire cells
     public Entity FirePrefab;
+    // Number of fire points at start
     public int StartingFireCount;
+    // Prefab for the wanter puddles
     public Entity WaterPrefab;
+    // Distance from the grid to the river
     public float DistanceToGrid;
+    // Number of water cells per side
     public int ElementsPerSide;
 }
