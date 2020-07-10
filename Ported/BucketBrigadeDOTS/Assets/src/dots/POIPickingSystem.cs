ce275a869b962a6cc5c5cd1c526443352fc50384
Kuba Cupisz <kubacupisz@gmail.com>
2020-07-10 13:10:11 +0200

    POIPickingSystem needs to run after FireMipChainBuild (not after FireSpreadSystem, that one doesn't matter).

--- Ported/BucketBrigadeDOTS/Assets/src/dots/POIPickingSystem.cs
+++ Ported/BucketBrigadeDOTS/Assets/src/dots/POIPickingSystem.cs
@@ -6,7 +6,7 @@ using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 
-[UpdateAfter(typeof(FireSpreadSystem))]
+[UpdateAfter(typeof(FireMipChainBuild))]
 public class POIPickingSystem : SystemBase
 {
     private EntityCommandBufferSystem m_CommandBufferSystem;
