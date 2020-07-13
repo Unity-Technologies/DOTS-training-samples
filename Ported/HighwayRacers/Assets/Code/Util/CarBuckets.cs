2840505b1366540d50957f3a2391e0cc55c46cad
Brian Will <@brianwill>
2020-07-13 04:09:49 -0700

    rewrite: finished audit; still no testing

--- Ported/HighwayRacers/Assets/Code/Util/CarBuckets.cs
+++ Ported/HighwayRacers/Assets/Code/Util/CarBuckets.cs
@@ -16,7 +16,8 @@ namespace HighwayRacer
 
         private NativeArray<UnsafeList.ParallelWriter> writers;
         private NativeArray<UnsafeList> lists;
-        private NativeArray<int> indexMoveAllAfterToNext; // for each list, the index of first car that should be moved to next list
+        
+        private NativeArray<int> idxMoveMeAndAllAfter; // for each list, the index of first car that should be moved to next list
         private UnsafeList<Car> tempList;
 
         public CarBuckets(int nSegments, int nCarsPerSegment)
@@ -26,7 +27,7 @@ namespace HighwayRacer
 
             writers = new NativeArray<UnsafeList.ParallelWriter>(nSegments, Allocator.Persistent);
             lists = new NativeArray<UnsafeList>(nSegments, Allocator.Persistent);
-            indexMoveAllAfterToNext = new NativeArray<int>(nSegments, Allocator.Persistent);
+            idxMoveMeAndAllAfter = new NativeArray<int>(nSegments, Allocator.Persistent);
 
             var ptr = (UnsafeList*) lists.GetUnsafePtr();
 
@@ -58,6 +59,8 @@ namespace HighwayRacer
             bucket.AddNoResize(car);
         }
 
+        // 1. set new pos given speed
+        // 2. moves cars between buckets when they move past end
         public void AdvanceCars(NativeArray<float> segmentLengths)
         {
             // update car pos based on speed. Also record idx at-and-past which cars in the bucket should move to next bucket
@@ -66,7 +69,7 @@ namespace HighwayRacer
                 var segmentLength = segmentLengths[segmentIdx];
                 var cars = GetCars(segmentIdx);
 
-                indexMoveAllAfterToNext[segmentIdx] = cars.Length;
+                idxMoveMeAndAllAfter[segmentIdx] = cars.Length;
                 for (var i = 0; i < cars.Length; i++)
                 {
                     var car = cars[i];
@@ -74,46 +77,82 @@ namespace HighwayRacer
                     if (car.Pos > segmentLength)
                     {
                         car.Pos -= segmentLength;
-                        if (i < indexMoveAllAfterToNext[segmentIdx])
+                        if (i < idxMoveMeAndAllAfter[segmentIdx])
                         {
-                            indexMoveAllAfterToNext[segmentIdx] = i;
+                            idxMoveMeAndAllAfter[segmentIdx] = i;
                         }
                     }
+                    cars[i] = car;
                 }
             }
 
             // cache cars to move from last bucket, then actually remove them from that bucket
-            var lastBucket = GetCars(lists.Length - 1);
-            var index = indexMoveAllAfterToNext[lists.Length - 1];
-            tempList.Clear();
-            moveCarsNext(lastBucket, index, tempList);
+            pushCarsToCache(idxMoveMeAndAllAfter[lists.Length - 1], lists.Length - 1);
 
             // from each bucket, move all cars at or past index to next bucket
-            var nextBucket = lastBucket;
-            for (int i = lists.Length - 2; i >= 0; i--)
+            for (int bucketIdx = lists.Length - 2; bucketIdx >= 0; bucketIdx--)
             {
-                var bucket = GetCars(i);
-                moveCarsNext(bucket, indexMoveAllAfterToNext[i], nextBucket);
-                nextBucket = bucket;
+                moveCarsNext(idxMoveMeAndAllAfter[bucketIdx], bucketIdx, bucketIdx + 1);
             }
 
-            // move cached cars into first bucket
-            moveCarsNext(tempList, 0, nextBucket);
+            // append cached cars to end of first bucket 
+            popCarsFromCache(0);
 
             Sort(); // sorts all the buckets
         }
 
-        private void moveCarsNext(UnsafeList<Car> srcBucket, int indexMoveAllAfter, UnsafeList<Car> dstBucket)
+        private void moveCarsNext(int idxMoveMeAndAllAfter, int srcBucketIdx, int dstBucketIdx)
         {
-            int i = 0;
-            for (; i < srcBucket.Length; i++)
+            var ptr = (UnsafeList*) lists.GetUnsafePtr();
+            var srcBucketPtr = ptr + srcBucketIdx;
+            var srcBucket = GetCars(srcBucketIdx);
+            
+            var dstBucketPtr = ptr + dstBucketIdx;
+            var dstBucket = GetCars(dstBucketIdx);
+            
+            var count = srcBucket.Length - idxMoveMeAndAllAfter;
+            for (int i = 0; i < count; i++)
             {
-                srcBucket[indexMoveAllAfter + i] = dstBucket[dstBucket.Length + 1];
+                 dstBucket[dstBucket.Length + i] = srcBucket[idxMoveMeAndAllAfter + i];
             }
 
-            dstBucket.Length += i + 1;
-            srcBucket.Length = indexMoveAllAfter;
+            dstBucketPtr->Length += count;
+            srcBucketPtr->Length = idxMoveMeAndAllAfter;
         }
+        
+        
+        private void pushCarsToCache(int idxMoveMeAndAllAfter, int srcBucketIdx)
+        {
+            tempList.Clear();
+            
+            var ptr = (UnsafeList*) lists.GetUnsafePtr();
+            var srcBucketPtr = ptr + srcBucketIdx;
+            var srcBucket = GetCars(srcBucketIdx);
+
+            var count = srcBucket.Length - idxMoveMeAndAllAfter;
+            for (int i = 0; i < count; i++)
+            {
+                tempList[i] = srcBucket[idxMoveMeAndAllAfter + i];
+            }
+            
+            tempList.Length += count;
+            srcBucketPtr->Length = idxMoveMeAndAllAfter;
+        }
+        
+        private void popCarsFromCache(int dstBucketIdx)
+        {
+            var ptr = (UnsafeList*) lists.GetUnsafePtr();
+            var dstBucketPtr = ptr + dstBucketIdx;
+            var dstBucket = GetCars(dstBucketIdx);
+            
+            for (int i = 0; i < tempList.Length; i++)
+            {
+                dstBucket[dstBucket.Length + i] = tempList[i];
+            }
+            
+            dstBucketPtr->Length = tempList.Length;
+        }
+        
 
         // updates cars in all ways except advancing their position
         // 1. update lane offset of merging cars; cars that complete merge leave merge state
@@ -160,7 +199,26 @@ namespace HighwayRacer
             for (int i = 0; i < lists.Length; i++)
             {
                 var list = lists[i];
-                list.Sort<Car, CarCompare>(new CarCompare());
+                InsertionSort<Car, CarCompare>(list.Ptr, 0, list.Length, new CarCompare());
+            }
+        }
+        
+        // copied from Collections
+        void InsertionSort<T, U>(void* array, int lo, int hi, U comp) where T : struct where U : IComparer<T>
+        {
+            int i, j;
+            T t;
+            for (i = lo; i < hi; i++)
+            {
+                j = i;
+                t = UnsafeUtility.ReadArrayElement<T>(array, i + 1);
+                while (j >= lo && comp.Compare(t, UnsafeUtility.ReadArrayElement<T>(array, j)) < 0)
+                {
+                    UnsafeUtility.WriteArrayElement<T>(array, j + 1, UnsafeUtility.ReadArrayElement<T>(array, j));
+                    j--;
+                }
+
+                UnsafeUtility.WriteArrayElement<T>(array, j + 1, t);
             }
         }
 
@@ -175,7 +233,7 @@ namespace HighwayRacer
 
                 writers.Dispose();
                 lists.Dispose();
-                indexMoveAllAfterToNext.Dispose();
+                idxMoveMeAndAllAfter.Dispose();
                 tempList.Dispose();
             }
         }
@@ -294,25 +352,6 @@ namespace HighwayRacer
         {
             var list = Lists[index];
             list.Sort<Car, CarCompare>(new CarCompare());
-        }
-
-        // copied from 
-        unsafe static void InsertionSort<T, U>(void* array, int lo, int hi, U comp) where T : struct where U : IComparer<T>
-        {
-            int i, j;
-            T t;
-            for (i = lo; i < hi; i++)
-            {
-                j = i;
-                t = UnsafeUtility.ReadArrayElement<T>(array, i + 1);
-                while (j >= lo && comp.Compare(t, UnsafeUtility.ReadArrayElement<T>(array, j)) < 0)
-                {
-                    UnsafeUtility.WriteArrayElement<T>(array, j + 1, UnsafeUtility.ReadArrayElement<T>(array, j));
-                    j--;
-                }
-
-                UnsafeUtility.WriteArrayElement<T>(array, j + 1, t);
-            }
         }
     }
 }
