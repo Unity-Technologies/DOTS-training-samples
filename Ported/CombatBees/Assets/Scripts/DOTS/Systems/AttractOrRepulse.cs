7e7933698edcf072a2b18a7f33225cf705dbf612
John Wheeler <jwheeler@unity3d.com>
2020-07-15 15:18:20 -0700

    Team attraction

--- Ported/CombatBees/Assets/Scripts/DOTS/Systems/AttractOrRepulse.cs
+++ Ported/CombatBees/Assets/Scripts/DOTS/Systems/AttractOrRepulse.cs
@@ -7,51 +7,93 @@ using Unity.Transforms;
 
 public class AttractOrRepulse : SystemBase
 {
-    EntityQuery m_FriendsQuery;
-    EntityQuery m_EnemiesQuery;
+    EntityQuery m_TeamOneQuery;
+    EntityQuery m_TeamTwoQuery;
+
+    Random m_Random;
+
+    private EntityCommandBufferSystem m_ECBSystem;
 
     protected override void OnCreate()
     {
-        m_FriendsQuery = GetEntityQuery(new EntityQueryDesc
+        m_TeamOneQuery = GetEntityQuery(new EntityQueryDesc
         {
             All = new[]
             {
-                ComponentType.ReadOnly<TeamOne>()
+                ComponentType.ReadOnly<TeamOne>(),
+                ComponentType.ReadOnly<Size>()
             }
         });
 
-        m_EnemiesQuery = GetEntityQuery(new EntityQueryDesc
+        m_TeamTwoQuery = GetEntityQuery(new EntityQueryDesc
         {
             All = new[]
             {
-                ComponentType.ReadOnly<TeamTwo>()
+                ComponentType.ReadOnly<TeamTwo>(),
+                ComponentType.ReadOnly<Size>()
             }
         });
+
+        m_Random = new Random(0x5716318);
+
+        m_ECBSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
     }
 
     protected override void OnUpdate()
     {
-        // Assign values to local variables captured in your job here, so that it has
-        // everything it needs to do its work when it runs later.
-        // For example,
-        //     float deltaTime = Time.DeltaTime;
+        // List<Bee> allies = teamsOfBees[bee.team];
+        // Bee attractiveFriend = allies[Random.Range(0,allies.Count)];
+        // Vector3 delta = attractiveFriend.position - bee.position;
+        // float dist = Mathf.Sqrt(delta.x * delta.x + delta.y * delta.y + delta.z * delta.z);
+        // if (dist > 0f) {
+        //     bee.velocity += delta * (teamAttraction * deltaTime / dist);
+        // }
+        //
+        // Bee repellentFriend = allies[Random.Range(0,allies.Count)];
+        // delta = attractiveFriend.position - bee.position;
+        // dist = Mathf.Sqrt(delta.x * delta.x + delta.y * delta.y + delta.z * delta.z);
+        // if (dist > 0f) {
+        //     bee.velocity -= delta * (teamRepulsion * deltaTime / dist);
+        // }
 
-        // This declares a new kind of job, which is a unit of work to do.
-        // The job is declared as an Entities.ForEach with the target components as parameters,
-        // meaning it will process all entities in the world that have both
-        // Translation and Rotation components. Change it to process the component
-        // types you want.
+        var random = m_Random;
+        var deltaTime = Time.DeltaTime;
+        var teamOneEntities = m_TeamOneQuery.ToEntityArrayAsync(Allocator.TempJob, out var teamOneEntitiesHandle);
+        var teamTwoEntities = m_TeamTwoQuery.ToEntityArrayAsync(Allocator.TempJob, out var teamTwoEntitiesHandle);
 
-        Entities.ForEach((ref Translation translation, in Rotation rotation) =>
-        {
-            // Implement the work to perform for each entity here.
-            // You should only access data that is local or that is a
-            // field on this job. Note that the 'rotation' parameter is
-            // marked as 'in', which means it cannot be modified,
-            // but allows this job to run in parallel with other jobs
-            // that want to read Rotation component data.
-            // For example,
-            //     translation.Value += math.mul(rotation.Value, new float3(0, 0, 1)) * deltaTime;
-        }).Schedule();
+        Dependency = JobHandle.CombineDependencies(Dependency, teamOneEntitiesHandle);
+        Dependency = JobHandle.CombineDependencies(Dependency, teamTwoEntitiesHandle);
+
+        Entities
+            .WithDeallocateOnJobCompletion(teamOneEntities)
+            .ForEach((ref Velocity velocity, in TeamOne team, in Translation translation) =>
+            {
+                var attractiveFriend = teamOneEntities[random.NextInt(0, teamOneEntities.Length - 1)];
+                var friendPos = GetComponent<Translation>(attractiveFriend);
+                var delta = friendPos.Value - translation.Value;
+                var distSq = math.distancesq(friendPos.Value, translation.Value);
+                if (distSq > float.Epsilon)
+                {
+                    velocity.Value += delta * ((BeeManager.Instance.teamAttraction - BeeManager.Instance.teamRepulsion) * deltaTime / math.sqrt(distSq));
+                }
+            }).Schedule();
+
+        Entities
+            .WithDeallocateOnJobCompletion(teamTwoEntities)
+            .ForEach((ref Velocity velocity, in TeamTwo team, in Translation translation) =>
+            {
+                var attractiveFriend = teamTwoEntities[random.NextInt(0, teamTwoEntities.Length - 1)];
+                var friendPos = GetComponent<Translation>(attractiveFriend);
+                var delta = friendPos.Value - translation.Value;
+                var distSq = math.distancesq(friendPos.Value, translation.Value);
+                if (distSq > float.Epsilon)
+                {
+                    velocity.Value += delta * ((BeeManager.Instance.teamAttraction - BeeManager.Instance.teamRepulsion) * deltaTime / math.sqrt(distSq));
+                }
+            }).Schedule();
+
+        m_ECBSystem.AddJobHandleForProducer(Dependency);
+
+        m_Random = random;
     }
 }
