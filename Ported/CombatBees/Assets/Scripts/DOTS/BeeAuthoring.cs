933a77cb0d3c63964e6becb3dd58aa907926c6e5
John Wheeler <jwheeler@unity3d.com>
2020-07-15 11:40:44 -0700

    Initial values

--- Ported/CombatBees/Assets/Scripts/DOTS/BeeAuthoring.cs
+++ Ported/CombatBees/Assets/Scripts/DOTS/BeeAuthoring.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 [RequiresEntityConversion]
 public class BeeAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
+    public int Team;
+    public float3 InitialVelocity;
+    public Color Color;
+
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
-        dstManager.AddComponentData(entity, new Velocity() { Value = new float3(0f) });
-        dstManager.AddComponentData(entity, new Team() { Value = 0 });
+        dstManager.AddComponentData(entity, new Velocity() { Value = InitialVelocity });
+        dstManager.AddComponentData(entity, new Team() { Value = Team });
         dstManager.AddComponentData(entity, new Size() { Value = 1.0f });
-        dstManager.AddComponentData(entity, new BeeColor() { Value = new float4(0f) });
+        dstManager.AddComponentData(entity, new BeeColor() { Value = new float4(Color.r, Color.g, Color.b, Color.a) });
         dstManager.AddComponentData(entity, new NonUniformScale() { Value = new float3(1f) });
     }
 }
