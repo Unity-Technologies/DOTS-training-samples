7556547545c4bf80aa61325c9de8d5d52f1ec433
Amy DiGiovanni <amyd@unity3d.com>
2020-07-15 13:13:39 -0700

    * Remove PlatformWaypoint component * Add blob asset usage example for platforms graph

---
+++ Ported/07-2020-Team3/Assets/Scripts/PlatformAuthoring.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public struct Platform : IComponentData
+{
+    public float3 HeadStairsBottom;
+    public float3 HeadStairsTop;
+    public float3 FootStairsBottom;
+    public float3 FootStairsTop;
+    public Entity NextPlatform;
+}
--- Ported/07-2020-Team3/Assets/Scripts/PlatformWaypointAuthoring.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Unity.Entities;
-using UnityEngine;
-
-[GenerateAuthoringComponent]
-public struct PlatformWaypoint : IComponentData
-{
-}
