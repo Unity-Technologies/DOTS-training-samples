1c263c26f25f1884d864b9fbd7d4f43f2d127cec
Karl N <karln@unity3d.com>
2020-07-15 14:49:22 -0700

    Better spawning again.

--- Ported/CombatBees/Assets/Scripts/DOTS/Authoring/FieldInfoAuthoring.cs
+++ Ported/CombatBees/Assets/Scripts/DOTS/Authoring/FieldInfoAuthoring.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using Unity.Entities;
-using Unity.Mathematics; 
+using Unity.Mathematics;
 
 [RequiresEntityConversion]
 public class FieldInfoAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
     public float Height = 10;
+    public Color TeamColor;
 
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
-        var t = GetComponent<Transform>(); 
+        var t = GetComponent<Transform>();
         var pos = t.position;
         var scale = t.lossyScale;
         var halfHeight = Height * 0.5f;
@@ -20,7 +21,8 @@ public class FieldInfoAuthoring : MonoBehaviour, IConvertGameObjectToEntity
             {
                 Center = new float3(pos.x, pos.y + halfHeight, pos.z),
                 Extents = new float3(scale.x * 0.5f, halfHeight, scale.z * 0.5f)
-            }
+            },
+            TeamColor = TeamColor
         });
     }
 }
@@ -28,5 +30,6 @@ public class FieldInfoAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 public struct FieldInfo : IComponentData
 {
     public Bounds Bounds;
+    public Color TeamColor;
 }
 
--- Ported/CombatBees/Assets/Scripts/DOTS/Systems/BeeSpawnerSystem.cs
+++ Ported/CombatBees/Assets/Scripts/DOTS/Systems/BeeSpawnerSystem.cs
@@ -7,22 +7,41 @@ using UnityEngine;
 
 public class BeeSpawnerSystem : SystemBase
 {
+    private EntityQuery m_baseQuery;
+    Unity.Mathematics.Random m_Random = new Unity.Mathematics.Random(0x5716318);
+    protected override void OnCreate()
+    {
+        m_baseQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[]
+            {
+                ComponentType.ReadOnly<FieldInfo>()
+            },
+            Any = new[]
+            {
+                ComponentType.ReadOnly<TeamOne>(),
+                ComponentType.ReadOnly<TeamTwo>()
+            }
+        });
+    }
     protected override void OnUpdate()
     {
         //var currentScore = GetSingleton<BeeManager.Instance>();
+        var mainField = m_baseQuery.ToComponentDataArray<FieldInfo>(Unity.Collections.Allocator.Temp);
 
         Entities.WithStructuralChanges()
             .ForEach((Entity entity, in BeeSpawner spawner, in LocalToWorld ltw) =>
             {
-                for (int i = 0; i < BeeManager.Instance.teamColors.Length; i++)
+                for (int i = 0; i < mainField.Length; i++)
                 {
                     for (int x = 0; x < spawner.BeeCount; ++x)
                     {
+                        //var xVal = m_Random.NextInt(mainField[i].Bounds.Extents.)
                         var instance = EntityManager.Instantiate(spawner.Prefab);
                         EntityManager.SetComponentData(instance,
                             new Translation
                             {
-                                Value = ltw.Position + new float3(BeeManager.Instance.baseLocations[i].x, math.sin(x) * x, math.cos(x) * x)
+                                Value = new float3(mainField[i].Bounds.Center.x, math.sin(x) * x, math.cos(x) * x)
                             });
 
                         if (i == 0)
@@ -37,7 +56,7 @@ public class BeeSpawnerSystem : SystemBase
                         EntityManager.SetComponentData(instance,
                             new BeeColor
                             {
-                                Value = new float4(BeeManager.Instance.teamColors[i].r, BeeManager.Instance.teamColors[i].g, BeeManager.Instance.teamColors[i].b, BeeManager.Instance.teamColors[i].a)
+                                Value = new float4(mainField[i].TeamColor.r, mainField[i].TeamColor.g, mainField[i].TeamColor.b, mainField[i].TeamColor.a)
                             });
                     }
                 }
