eb1cb8b932e3605f376fa75208347b0636b00130
Karl N <karln@unity3d.com>
2020-07-15 09:45:11 -0700

    beesbees bees!

--- Ported/CombatBees/Assets/Scripts/BeeSpawnerAuthoring.cs
+++ Ported/CombatBees/Assets/Scripts/BeeSpawnerAuthoring.cs
@@ -1,18 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using Unity.Entities;
 
-public class BeeSpawnerAuthoring : MonoBehaviour
+[GenerateAuthoringComponent]
+public struct BeeSpawner : IComponentData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Entity Prefab;
+    public int BeeCount;
 }
--- Ported/CombatBees/Assets/Scripts/BeeSpawnerSystem.cs
+++ Ported/CombatBees/Assets/Scripts/BeeSpawnerSystem.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
-public class BeeSpawnerSystem : MonoBehaviour
+public class BeeSpawnerSystem : SystemBase
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void OnUpdate()
     {
-        
-    }
+        Entities.WithStructuralChanges()
+    .ForEach((Entity entity, in BeeSpawner spawner, in LocalToWorld ltw) =>
+    {
+        for (int x = 0; x < spawner.BeeCount; ++x)
+        {
+            //var posX = 2 * (x - (spawner.CountX - 1) / 2);
+            //var posZ = 2 * (z - (spawner.CountZ - 1) / 2);
+            var instance = EntityManager.Instantiate(spawner.Prefab);
+            SetComponent(instance, new Translation
+            {
+                Value = ltw.Position + new float3(x, 0, 0)
+            });
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        EntityManager.DestroyEntity(entity);
+    }).Run();
     }
 }
