d69d22de04178c67322f75320d96f2bfb3c350aa
Eric <eric.provencher@unity3d.com>
2020-07-15 19:08:03 -0400

    WIP adding initial fires

--- Ported/Buckets/Assets/Scripts/Fire/FireGridSpawnerSystem.cs
+++ Ported/Buckets/Assets/Scripts/Fire/FireGridSpawnerSystem.cs
@@ -3,6 +3,8 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
+using Random = Unity.Mathematics.Random;
 
 namespace Fire
 {
@@ -10,9 +12,16 @@ namespace Fire
     public struct FireGridSpawner : IComponentData
     {
         public Entity Prefab;
+
         public int CountX;
         public int CountZ;
         public float3 Origin;
+
+        public int MinInitialFires;
+        public int MaxInitialFires;
+
+        public float StartFireAmount;
+        public float StartFireVelocity;
     }
 
     public struct Initialized : IComponentData { };
@@ -24,10 +33,17 @@ namespace Fire
         struct SpawnerInfo
         {
             public Entity Prefab;
+
             public int CountX;
             public int CountZ;
             public int TotalCount;
             public float3 Center;
+
+            public int MinInitialFires;
+            public int MaxInitialFires;
+
+            public float StartFireAmount;
+            public float StartFireVelocity;
         }
 
         protected override void OnCreate()
@@ -55,7 +71,11 @@ namespace Fire
                         CountX = spawner.CountX,
                         CountZ = spawner.CountZ,
                         TotalCount = spawner.CountX * spawner.CountZ,
-                        Center = spawner.Origin
+                        Center = spawner.Origin,
+                        StartFireAmount = spawner.StartFireAmount,
+                        StartFireVelocity = spawner.StartFireVelocity,
+                        MinInitialFires = spawner.MinInitialFires,
+                        MaxInitialFires = spawner.MaxInitialFires
                     };
                 }).Run();
 
@@ -84,10 +104,24 @@ namespace Fire
             gridBuffer.ResizeUninitialized(spanwerInfoInstance.TotalCount);
             var gridArray = gridBuffer.AsNativeArray();
 
-            Random m_Random = new Random(0x1234567);
+            Random random = new Random(0x1234567);
+
+            // Start initial fires
+            int numberOfFires = random.NextInt(spanwerInfoInstance.MinInitialFires, spanwerInfoInstance.MaxInitialFires);
+            int[] fireIndiciesArr = new int[numberOfFires];
+
+            // Pick indicies of fires that should be started, at random
+            for (int i = 0; i < numberOfFires; i++)
+            {
+                int fireIndex = random.NextInt(0, spanwerInfoInstance.TotalCount - 1);
+                fireIndiciesArr[i] = fireIndex;
+            }
+
             Entities
                 .WithDeallocateOnJobCompletion(spawners)
-                .ForEach((Entity fireEntity, int entityInQueryIndex, ref Translation translation, in BoundsComponent bounds) =>
+                .ForEach((Entity fireEntity, int entityInQueryIndex, ref Translation translation,
+                    /*ref TemperatureComponent temperature,*/
+                    in BoundsComponent bounds) =>
                 {
                     for (int i = 0; i < spawners.Length; ++i)
                     {
@@ -99,16 +133,36 @@ namespace Fire
                             var posX = bounds.SizeXZ * (x - (spawner.CountX - 1) / 2);
                             var posZ = bounds.SizeXZ * (z - (spawner.CountZ - 1) / 2);
                             // Add random offset on y to debug that the grid spacing is correct
-                            var posY = m_Random.NextFloat(-0.01f, 0.01f);
+                            var posY = random.NextFloat(-0.01f, 0.01f);
                             translation.Value = spawner.Center + new float3(posX, posY, posZ);
-
-                            gridArray[entityInQueryIndex] = new FireBufferElement { FireEntity = fireEntity };
+                            /*
+                            // If we should start a fire, start it
+                            if (fireIndiciesArr.Contains(entityInQueryIndex))
+                            {
+                                temperature.Value = spanwerInfoInstance.StartFireAmount;
+                                // TODO rethink how to add fire velocity component
+                            }
+                            */
+                            gridArray[entityInQueryIndex] = new FireBufferElement {FireEntity = fireEntity};
                             break;
                         }
                         entityInQueryIndex -= spawner.TotalCount;
                     }
                 }).ScheduleParallel();
         }
+
+        /*
+        void StartFire(Entity fireEntity, float startFireAmount, float startFireVelocity)
+        {
+            // Set Starting fire amount
+            var amount = math.clamp(startFireAmount, 0f, 1f);
+            EntityManager.SetComponentData(fireEntity, new TemperatureComponent {Value = amount });
+
+            // Set Starting fire velocity
+            var velocity = math.clamp(startFireVelocity, 0f, 1f);
+            EntityManager.AddComponentData(fireEntity, new TemperatureVelocity { Value = velocity });
+        }
+         */
     }
 }
 
