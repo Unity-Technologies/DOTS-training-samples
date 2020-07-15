216d45d53eee8ec3b7709f191d6ce7b73957cb27
John Wheeler <jwheeler@unity3d.com>
2020-07-15 12:50:43 -0700

    Hopefully move to target working... but not tested because we can't set a target.

--- Ported/CombatBees/Assets/Scripts/DOTS/Systems/BeeSpawnerSystem.cs
+++ Ported/CombatBees/Assets/Scripts/DOTS/Systems/BeeSpawnerSystem.cs
@@ -12,29 +12,34 @@ public class BeeSpawnerSystem : SystemBase
         //var currentScore = GetSingleton<BeeManager.Instance>();
 
         Entities.WithStructuralChanges()
-    .ForEach((Entity entity, in BeeSpawner spawner, in LocalToWorld ltw) =>
-    {
-        for (int i = 0; i < BeeManager.Instance.teamColors.Length; i++)
-        {
-            for (int x = 0; x < spawner.BeeCount; ++x)
+            .ForEach((Entity entity, in BeeSpawner spawner, in LocalToWorld ltw) =>
             {
-                var instance = EntityManager.Instantiate(spawner.Prefab);
-                SetComponent(instance, new Translation
+                for (int i = 0; i < BeeManager.Instance.teamColors.Length; i++)
                 {
-                    Value = ltw.Position + new float3(x, i * 2, 0)
-                });
-                SetComponent(instance, new Team
-                {
-                    Value = i
-                });
-                SetComponent(instance, new BeeColor
-                {
-                    Value = new float4(BeeManager.Instance.teamColors[i].r, BeeManager.Instance.teamColors[i].g, BeeManager.Instance.teamColors[i].b, BeeManager.Instance.teamColors[i].a)
-                });
-            }
-        }
+                    for (int x = 0; x < spawner.BeeCount; ++x)
+                    {
+                        var instance = EntityManager.Instantiate(spawner.Prefab);
+                        SetComponent(instance,
+                            new Translation
+                            {
+                                Value = ltw.Position + new float3(x, i * 2, 0)
+                            });
 
-        EntityManager.DestroyEntity(entity);
-    }).Run();
+                        SetComponent(instance,
+                            new Team
+                            {
+                                Value = i
+                            });
+
+                        SetComponent(instance,
+                            new BeeColor
+                            {
+                                Value = new float4(BeeManager.Instance.teamColors[i].r, BeeManager.Instance.teamColors[i].g, BeeManager.Instance.teamColors[i].b, BeeManager.Instance.teamColors[i].a)
+                            });
+                    }
+                }
+
+                EntityManager.DestroyEntity(entity);
+            }).Run();
     }
 }
