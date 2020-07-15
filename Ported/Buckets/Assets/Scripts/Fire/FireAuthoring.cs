c47dc72e4a80ec78bc88e4c945163375492fc447
Eric <eric.provencher@unity3d.com>
2020-07-15 14:10:04 -0400

    WIP Fire grid setup

---
+++ Ported/Buckets/Assets/Scripts/Fire/FireAuthoring.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Fire
+{
+    public class FireAuthoring : UnityEngine.MonoBehaviour, IConvertGameObjectToEntity
+    {
+        public float SizeXZ = 1;
+        public float SizeY = 5;
+
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            dstManager.AddComponentData(entity, new Translation { Value = new float3() });
+            dstManager.AddComponentData(entity, new TemperatureComponent { Value = 0f });
+            dstManager.AddComponentData(entity, new BoundsComponent { Size = SizeXZ, Height = SizeY});
+        }
+    }
+
+    public struct TemperatureComponent : IComponentData
+    {
+        public float Value;
+    }
+
+    // TODO make bounds readonly once initialized
+    public struct BoundsComponent : IComponentData
+    {
+        public float Size;
+        public float Height;
+    }
+
+    // Note: added at runtime - used to signify "On Fire" state
+    public struct TemperatureVelocity : IComponentData
+    {
+        public float Value;
+    }
+}
---
+++ Ported/Buckets/Assets/Scripts/Fire/FireGridSpawnerSystem.cs
@@ -0,0 +1,92 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Fire.Authoring
+{
+    [GenerateAuthoringComponent]
+    public struct FireGridSpawner : IComponentData
+    {
+        public Entity Prefab;
+        public int CountX;
+        public int CountZ;
+    }
+
+    public struct Initialised : IComponentData { };
+
+    public class FireGridSpawnerSystem : SystemBase
+    {
+        private EntityQuery UninitializedSpawners;
+
+        struct SpawnerInfo
+        {
+            public Entity Prefab;
+            public int CountX;
+            public int CountZ;
+            public int TotalCount;
+            public float3 Center;
+        }
+
+        protected override void OnCreate()
+        {
+            RequireForUpdate(UninitializedSpawners);
+        }
+
+        protected override void OnUpdate()
+        {
+            var spawners = new NativeArray<SpawnerInfo>(UninitializedSpawners.CalculateEntityCount(), Allocator.TempJob);
+
+            Entities.WithStoreEntityQueryInField(ref UninitializedSpawners).WithNone<Initialised>()
+                .ForEach((int entityInQueryIndex, ref FireGridSpawner spawner, in LocalToWorld ltw) =>
+                {
+                    spawners[entityInQueryIndex] = new SpawnerInfo
+                    {
+                        Prefab = spawner.Prefab,
+                        CountX = spawner.CountX,
+                        CountZ = spawner.CountZ,
+                        TotalCount = spawner.CountX * spawner.CountZ,
+                        Center = ltw.Position
+                    };
+                }).Run();
+
+            foreach (var spawner in spawners)
+            {
+                EntityManager.Instantiate(spawner.Prefab, spawner.TotalCount, Allocator.Temp);
+            }
+
+            EntityManager.AddComponent<Initialised>(UninitializedSpawners);
+
+            Entities.WithDeallocateOnJobCompletion(spawners)
+                .ForEach((int entityInQueryIndex, ref Translation translation, in BoundsComponent bounds) =>
+                {
+
+                    // TODO write
+                    for (int i = 0; i < spawners.Length; ++i)
+                    {
+                        var spawner = spawners[i];
+                        if (entityInQueryIndex < spawner.TotalCount)
+                        {
+                            int x = entityInQueryIndex % spawner.CountX;
+                            int z = entityInQueryIndex / spawner.CountX;
+                            var posX = bounds.Size / 2 * (x - (spawner.CountX - 1) / 2);
+                            var posZ = bounds.Size / 2 * (z - (spawner.CountZ - 1) / 2);
+                            translation.Value = spawner.Center + new float3(posX, 0, posZ);
+                            break;
+                        }
+                        entityInQueryIndex -= spawner.TotalCount;
+                    }
+                }).ScheduleParallel();
+        }
+    }
+}
+
+/*
+ * Entity for all fires (Grid storage)
+ * On that entity we can add a dynamic buffer component (native list, attached to an entity)
+ * new struct IBufferElementData (entity fire)
+ *
+ * As we initialize this list - make sure all the entity references are correct
+ * - Can access the singleton reference
+ * - Or we can pass the native array into jobs to work with
+*/
