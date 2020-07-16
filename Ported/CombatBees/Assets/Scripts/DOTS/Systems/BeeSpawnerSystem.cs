69df44689aa6da3c81688bbe08d353f610282eab
Pete Thompson <pete@digitalmonarchmedia.com>
2020-07-16 09:37:31 -0700

    Convert BeeSpawnerSystem to use ecb instead

--- Ported/CombatBees/Assets/Scripts/DOTS/Systems/BeeSpawnerSystem.cs
+++ Ported/CombatBees/Assets/Scripts/DOTS/Systems/BeeSpawnerSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
@@ -9,6 +10,8 @@ public class BeeSpawnerSystem : SystemBase
 {
     private EntityQuery m_baseQuery;
     Unity.Mathematics.Random m_Random = new Unity.Mathematics.Random(0x5716318);
+    private EntityCommandBufferSystem m_ECBSystem;
+
 
     protected override void OnCreate()
     {
@@ -24,61 +27,75 @@ public class BeeSpawnerSystem : SystemBase
                 ComponentType.ReadOnly<TeamTwo>()
             }
         });
+
+        m_ECBSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
     }
 
     protected override void OnUpdate()
     {
-        var mainField = m_baseQuery.ToComponentDataArray<FieldInfo>(Unity.Collections.Allocator.Temp);
+        var baseFields = m_baseQuery.ToComponentDataArrayAsync<FieldInfo>(Unity.Collections.Allocator.TempJob, out var baseHandle);
 
-        Entities.WithStructuralChanges()
-            .ForEach((Entity entity, in BeeSpawner spawner, in LocalToWorld ltw) =>
+        Dependency = JobHandle.CombineDependencies(Dependency, baseHandle); 
+
+        var ecb = m_ECBSystem.CreateCommandBuffer().ToConcurrent();
+        var random = m_Random;
+
+        var maxSpawnSpeed = BeeManager.Instance.maxSpawnSpeed;
+        var minBeeSize = BeeManager.Instance.minBeeSize;
+        var maxBeeSize = BeeManager.Instance.maxBeeSize;
+
+        Entities
+            .WithDeallocateOnJobCompletion(baseFields)
+            .ForEach((int entityInQueryIndex, Entity entity, in BeeSpawner spawner, in LocalToWorld ltw) =>
             {
-                for (int i = 0; i < mainField.Length; i++)
+                for (int i = 0; i < baseFields.Length; i++)
                 {
-                    float xCenter = mainField[i].Bounds.Center.x;
-                    float xExtents = mainField[i].Bounds.Extents.x;
-                    float yCenter = mainField[i].Bounds.Center.y;
-                    float yExtents = mainField[i].Bounds.Extents.y;
-                    float zCenter = mainField[i].Bounds.Center.z;
-                    float zExtents = mainField[i].Bounds.Extents.z;
+                    var baseField = baseFields[i]; 
+
+                    float xCenter = baseField.Bounds.Center.x;
+                    float xExtents = baseField.Bounds.Extents.x;
+                    float yCenter = baseField.Bounds.Center.y;
+                    float yExtents = baseField.Bounds.Extents.y;
+                    float zCenter = baseField.Bounds.Center.z;
+                    float zExtents = baseField.Bounds.Extents.z;
+
                     for (int x = 0; x < spawner.BeeCount; ++x)
                     {
-                        var xVal = m_Random.NextFloat(xCenter - xExtents, xCenter + xExtents);
-                        var yVal = m_Random.NextFloat(yCenter - yExtents, yCenter + yExtents);
-                        var zVal = m_Random.NextFloat(zCenter - zExtents, zCenter + zExtents);
-                        var instance = EntityManager.Instantiate(spawner.Prefab);
-                        EntityManager.SetComponentData(instance,
-                            new Translation
-                            {
-                                Value = new float3(xVal, yVal, zVal)
-                            });
+                        var xVal = random.NextFloat(xCenter - xExtents, xCenter + xExtents);
+                        var yVal = random.NextFloat(yCenter - yExtents, yCenter + yExtents);
+                        var zVal = random.NextFloat(zCenter - zExtents, zCenter + zExtents);
 
-                        EntityManager.AddComponentData(instance, new Target());
+                        var instance = ecb.Instantiate(entityInQueryIndex, spawner.Prefab);
+
+                        ecb.SetComponent(entityInQueryIndex, instance, new Translation
+                        {
+                            Value = new float3(xVal, yVal, zVal)
+                        });
+
+                        ecb.AddComponent(entityInQueryIndex, instance, new Target());
 
                         if (i == 0)
                         {
-                            EntityManager.AddComponentData(instance, new TeamOne());
+                            ecb.AddComponent(entityInQueryIndex, instance, new TeamOne());
                         }
                         else
                         {
-                            EntityManager.AddComponentData(instance, new TeamTwo());
+                            ecb.AddComponent(entityInQueryIndex, instance, new TeamTwo());
                         }
 
-                        EntityManager.SetComponentData(instance,
-                            new BeeColor
-                            {
-                                Value = new float4(mainField[i].TeamColor.r, mainField[i].TeamColor.g, mainField[i].TeamColor.b, mainField[i].TeamColor.a)
-                            });
+                        ecb.SetComponent(entityInQueryIndex, instance, new BeeColor { Value = new float4(baseField.TeamColor.r, baseField.TeamColor.g, baseField.TeamColor.b, baseField.TeamColor.a) });
 
                         // Random start velocity
-                        EntityManager.SetComponentData(instance, new Velocity() { Value = m_Random.NextFloat3Direction() * BeeManager.Instance.maxSpawnSpeed });
+                        ecb.SetComponent(entityInQueryIndex, instance, new Velocity() { Value = random.NextFloat3Direction() * maxSpawnSpeed });
 
                         // Random size
-                        EntityManager.SetComponentData(instance, new Size() { Value = m_Random.NextFloat(BeeManager.Instance.minBeeSize, BeeManager.Instance.maxBeeSize) });
+                        ecb.SetComponent(entityInQueryIndex, instance, new Size() { Value = random.NextFloat(minBeeSize, maxBeeSize) });
                     }
                 }
 
-                EntityManager.DestroyEntity(entity);
-            }).Run();
+                ecb.DestroyEntity(entityInQueryIndex, entity);
+            }).Schedule();
+
+        m_ECBSystem.AddJobHandleForProducer(Dependency);
     }
 }
