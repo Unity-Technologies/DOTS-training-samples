cc46f305a689ce5933c77a19dcabc3f51caa5cd4
Karl N <karln@unity3d.com>
2020-07-16 08:53:17 -0700

    Changes to how resources are tagged.  Fixes for the allocation of targets.

--- Ported/CombatBees/Assets/Scripts/DOTS/Systems/AssignTargetSystem.cs
+++ Ported/CombatBees/Assets/Scripts/DOTS/Systems/AssignTargetSystem.cs
@@ -21,7 +21,7 @@ public class AssignTargetSystem : SystemBase
             All = new[]
             {
                 ComponentType.ReadOnly<TeamOne>(),
-                ComponentType.ReadOnly<Target>()
+                ComponentType.ReadWrite<Target>()
             }
         });
 
@@ -30,7 +30,7 @@ public class AssignTargetSystem : SystemBase
             All = new[]
             {
                 ComponentType.ReadOnly<TeamTwo>(),
-                ComponentType.ReadOnly<Target>()
+                ComponentType.ReadWrite<Target>()
             }
         });
 
@@ -38,7 +38,7 @@ public class AssignTargetSystem : SystemBase
         {
             All = new[]
             {
-                ComponentType.ReadOnly<Gravity>()
+                ComponentType.ReadOnly<ResourceEntity>()
             }
         });
 
@@ -50,80 +50,94 @@ public class AssignTargetSystem : SystemBase
     protected override void OnUpdate()
     {
         var random = m_Random;
-        var resourceEntities = m_ResourceQuery.ToEntityArray(Allocator.TempJob);
-        var teamOneEntities = m_TeamOneQuery.ToEntityArray(Allocator.TempJob);
-        var teamTwoEntities = m_TeamTwoQuery.ToEntityArray(Allocator.TempJob);
+        //var resourceEntities = m_ResourceQuery.ToEntityArray(Allocator.TempJob);
+        //var teamOneEntities = m_TeamOneQuery.ToEntityArray(Allocator.TempJob);
+        //var teamTwoEntities = m_TeamTwoQuery.ToEntityArray(Allocator.TempJob);
 
-        foreach (Entity e in teamOneEntities)
-        {
-            ComponentDataFromEntity<Target> myTypeFromEntity = GetComponentDataFromEntity<Target>(true);
-            Target target = myTypeFromEntity[e];
-            if (target.EnemyTarget == Entity.Null || target.ResourceTarget == Entity.Null)
-            {
+        //foreach (Entity e in teamOneEntities)
+        //{
+        //    ComponentDataFromEntity<Target> myTypeFromEntity = GetComponentDataFromEntity<Target>(true);
+        //    Target target = myTypeFromEntity[e];
+        //    if (target.EnemyTarget == Entity.Null || target.ResourceTarget == Entity.Null)
+        //    {
 
-                var aggression = random.NextFloat(0, 1);
-                if (aggression < 0.5f)
-                {
-                    target.EnemyTarget = teamTwoEntities[random.NextInt(0, teamTwoEntities.Length - 1)];
-                }
-                else
-                {
-                    //var resourceEntities = m_ResourceQuery.ToEntityArray(Allocator.TempJob);
-                    target.ResourceTarget = resourceEntities[random.NextInt(0, resourceEntities.Length - 1)];
-                }
-            }
-            else if (target.EnemyTarget != null)
-            {
+        //        var aggression = random.NextFloat(0, 1);
+        //        if (aggression < 0.5f)
+        //        {
+        //            target.EnemyTarget = teamTwoEntities[random.NextInt(0, teamTwoEntities.Length - 1)];
+        //        }
+        //        else
+        //        {
+        //            if (resourceEntities.Length > 0)
+        //                target.ResourceTarget = resourceEntities[random.NextInt(0, resourceEntities.Length - 1)];
+        //        }
+        //    }
+        //    else if (target.EnemyTarget != null)
+        //    {
 
-            }
-        }
+        //    }
+        //}
         //var random = m_Random;
         //var deltaTime = Time.DeltaTime;
-        //var teamOneEntities = m_TeamOneQuery.ToEntityArrayAsync(Allocator.TempJob, out var teamOneEntitiesHandle);
-        //var teamTwoEntities = m_TeamTwoQuery.ToEntityArrayAsync(Allocator.TempJob, out var teamTwoEntitiesHandle);
-        //var resourceEntities = m_ResourceQuery.ToEntityArrayAsync(Allocator.TempJob, out var resourceEntitiesHandle);
+        var teamOneEntities = m_TeamOneQuery.ToEntityArrayAsync(Allocator.TempJob, out var teamOneEntitiesHandle);
+        var teamTwoEntities = m_TeamTwoQuery.ToEntityArrayAsync(Allocator.TempJob, out var teamTwoEntitiesHandle);
+        var resourceEntities = m_ResourceQuery.ToEntityArrayAsync(Allocator.TempJob, out var resourceEntitiesHandle);
 
-        //Dependency = JobHandle.CombineDependencies(Dependency, teamOneEntitiesHandle);
-        //Dependency = JobHandle.CombineDependencies(Dependency, teamTwoEntitiesHandle);
-        //Dependency = JobHandle.CombineDependencies(Dependency, resourceEntitiesHandle);
+        Dependency = JobHandle.CombineDependencies(Dependency, teamOneEntitiesHandle);
+        Dependency = JobHandle.CombineDependencies(Dependency, teamTwoEntitiesHandle);
+        Dependency = JobHandle.CombineDependencies(Dependency, resourceEntitiesHandle);
 
-        //Entities
-        //    .WithDeallocateOnJobCompletion(teamOneEntities)
-        //    .ForEach((ref Target target, in TeamOne team) =>
-        //    {
-        //        if (target.EnemyTarget == Entity.Null || target.ResourceTarget == Entity.Null)
-        //        {
+        Entities
+            .ForEach((ref Target target, in TeamOne team) =>
+            {
+                if (target.EnemyTarget == Entity.Null || target.ResourceTarget == Entity.Null)
+                {
+                    var aggression = random.NextFloat(0, 1);
+                    if (aggression < 0.5f)
+                    {
+                        target.EnemyTarget = teamTwoEntities[random.NextInt(0, teamOneEntities.Length - 1)];
+                    }
+                    else
+                    {
+                        if (resourceEntities.Length > 0)
+                            target.ResourceTarget = resourceEntities[random.NextInt(0, resourceEntities.Length - 1)];
+                    }
+                }
+                else if (target.EnemyTarget != null)
+                {
 
-        //            var aggression = random.NextFloat(0, 1);
-        //            if (aggression < 0.5f)
-        //            {
-        //                target.EnemyTarget = teamTwoEntities[random.NextInt(0, teamOneEntities.Length - 1)];
-        //            }
-        //            else
-        //            {
-        //                target.ResourceTarget = resourceEntities[random.NextInt(0, resourceEntities.Length - 1)];
-        //            }
-        //        }
-        //        else if (target.EnemyTarget != null)
-        //        {
+                }
 
-        //        }
+            }).Schedule();
 
-        //    }).Schedule();
+        Entities
+            .WithDeallocateOnJobCompletion(teamOneEntities)
+            .WithDeallocateOnJobCompletion(teamTwoEntities)
+            .WithDeallocateOnJobCompletion(resourceEntities)
+            .ForEach((ref Target target, in TeamTwo team) =>
+            {
+                if (target.EnemyTarget == Entity.Null || target.ResourceTarget == Entity.Null)
+                {
+                    var aggression = random.NextFloat(0, 1);
+                    if (aggression < 0.5f)
+                    {
+                        target.EnemyTarget = teamTwoEntities[random.NextInt(0, teamOneEntities.Length - 1)];
+                    }
+                    else
+                    {
+                        if (resourceEntities.Length > 0)
+                            target.ResourceTarget = resourceEntities[random.NextInt(0, resourceEntities.Length - 1)];
+                    }
+                }
+                else if (target.EnemyTarget != null)
+                {
 
-        ////Entities
-        ////    .WithDeallocateOnJobCompletion(teamTwoEntities)
-        ////    .ForEach((ref Velocity velocity, in TeamTwo team, in Target target) =>
-        ////    {
-        ////        if (target.EnemyTarget == null || target.ResourceTarget == null)
-        ////        {
-        ////            var randomAttack = teamOneEntities[random.NextInt(0, teamTwoEntities.Length - 1)];
-        ////        }
+                }
 
-        ////    }).Schedule();
+            }).Schedule();
 
-        //m_ECBSystem.AddJobHandleForProducer(Dependency);
+        m_ECBSystem.AddJobHandleForProducer(Dependency);
 
-        //m_Random = random;
+        m_Random = random;
     }
 }
