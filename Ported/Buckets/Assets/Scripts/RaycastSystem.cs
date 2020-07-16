145a59e0280cd1a5296ba110d412ed14445223a5
Barrington C <barrington.campbell@unity3d.com>
2020-07-16 15:06:39 -0400

    Fire/Mouse Interaction

--- Ported/Buckets/Assets/Scripts/RaycastSystem.cs
+++ Ported/Buckets/Assets/Scripts/RaycastSystem.cs
@@ -2,23 +2,15 @@
 using Fire;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Rendering;
 using Unity.Transforms;
 
 namespace Water
 {
     public class RaycastSystem : SystemBase
     {
-        //private EndSimulationEntityCommandBufferSystem ecbSystem;
-
-        //protected override void OnCreate()
-        //{
-        //ecbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-        //}
-
         protected override void OnUpdate()
         {
-            //var ecb = ecbSystem.CreateCommandBuffer().ToConcurrent();
-            //EntityManager manager = Unity.Entities.World.DefaultGameObjectInjectionWorld.EntityManager;
             var camera = UnityEngine.Camera.main;
             if (camera == null)
                 return;
@@ -28,43 +20,44 @@ namespace Water
             var hit = (float3) ray.GetPoint(enter);
 
             var deltaTime = Time.DeltaTime;
-            var mouseDown = UnityEngine.Input.GetMouseButton(0);
-
-            var fireBufferEntity = GetSingletonEntity<Fire.FireBuffer>();
+            var mouseLeftDown = UnityEngine.Input.GetMouseButton(0);
+            var mouseRightDown = UnityEngine.Input.GetMouseButton(1);
+            float splashRadius = .05f;
+            float splashEffect = 2;
+            //var fireBufferEntity = GetSingletonEntity<Fire.FireBuffer>();
             //var fireSystem = GetSingletonEntity<Fire.FireGridSpawnerSystem>();
             
             // Grab Fire buffer
-            var gridBufferLookup = GetBufferFromEntity<Fire.FireBufferElement>();
-            var gridBuffer = gridBufferLookup[fireBufferEntity];
-            var gridArray = gridBuffer.AsNativeArray();
+            //var gridBufferLookup = GetBufferFromEntity<Fire.FireBufferElement>();
+            //var gridBuffer = gridBufferLookup[fireBufferEntity];
+            //var gridArray = gridBuffer.AsNativeArray();
 
-            Entities.ForEach((int entityInQueryIndex, ref Entity entity, ref TemperatureComponent temperature, ref Color color, in LocalToWorld ltw) =>
+            Entities.ForEach((ref TemperatureComponent temperature, in Translation trans, in WorldRenderBounds bounds, in LocalToWorld ltw) =>
             {
-                var dist = mouseDown ? math.distancesq(ltw.Position, hit) / 10 : 1;
+                //Extinguish Flames
+                var extinguishDistance = mouseLeftDown ? math.distancesq(ltw.Position, hit) / 10 : temperature.Value;
 
-                if (dist < .4)
-                {
-                    //ecb.SetComponent(entityInQueryIndex, gridArray[entityInQueryIndex].FireEntity, new TemperatureComponent { Value = 1 });
-                    temperature = new TemperatureComponent{Value = 1 };
-                   // SetComponent(entity, new TemperatureComponent { Value = 1 });
-                    var clamped = math.clamp(1 - dist, 0, 1);
-                    color.Value = new float4(1, 0, 0, 1);
+                if (extinguishDistance < splashRadius) 
+                { 
+                    temperature = new TemperatureComponent{Value = temperature.Value - ((splashRadius - extinguishDistance ) * splashEffect)};
                 }
 
+                //Create Fire
+                var lightFireDistance = mouseRightDown ? math.distancesq(ltw.Position, hit) / 10 : 1;
 
+                var topBound = bounds.Value.Center.z + (bounds.Value.Extents.z);
+                var bottomBound = bounds.Value.Center.z - (bounds.Value.Extents.z);
+                var leftBound = bounds.Value.Center.x + (bounds.Value.Extents.x);
+                var rightBound = bounds.Value.Center.x - (bounds.Value.Extents.x);
 
-                //var dist = mouseDown ? math.distancesq(ltw.Position, hit) / 10 : 1;
+                if(mouseRightDown && (leftBound > hit.x && hit.x > rightBound)
+                && (topBound > hit.z && hit.z > bottomBound))
+                {
+                    temperature = new TemperatureComponent{Value = 1};
+                }
 
-                //if(dist < .5)
-                //{
-                //    var clamped = math.clamp(1 - dist, 0, 1);
-                //    color.Value = new float4(1,0,0,1);
-                //}
-
-                //ecb.AddComponent(entityInQueryIndex, entity, new BucketActiveTag { });
             }).ScheduleParallel();
 
-            //ecbSystem.AddJobHandleForProducer(Dependency);
         }
     }
 }
