92a404000619c662d3f7551cae798aaf663f93a0
Thomas Key <thomas.key@unity3d.com>
2020-07-16 10:51:05 -0700

    Added Authoring For Track and began authoring for other parts

---
+++ Ported/07-2020-Team3/Assets/Scripts/Authoring/TrainAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class TrainAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+    [SerializeField] CartAuthoring m_CartPrefab;
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        
+    }
+
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+        referencedPrefabs.Add(m_CartPrefab.gameObject);
+    }
+}
