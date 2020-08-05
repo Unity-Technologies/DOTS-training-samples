66445e26538cb28b89f7d4056ff2e70059d9caa2
Arthur Dufay <arthur.dufay@unity3d.com>
2020-08-05 10:49:56 +0200

    create some systems

---
+++ Ported/DotsAnts/Assets/Scripts/PheromoneSteeringSystem.cs
@@ -0,0 +1,11 @@
+using Unity.Entities;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class PheromoneSteeringSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+    }
+}
---
+++ Ported/DotsAnts/Assets/Scripts/WallSteerinSystem.cs
@@ -0,0 +1,11 @@
+using Unity.Entities;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class WallSteeringSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+    }
+}
