54bedc1e62e479ddaf6e3636fe6958fef54defaa
MingWai <cmwdes@gmail.com>
2020-08-05 11:25:53 +0200

    fix car init system

--- Ported/StackInterchange/Assets/Scripts/CarInitializeSystem.cs
+++ Ported/StackInterchange/Assets/Scripts/CarInitializeSystem.cs
@@ -4,6 +4,7 @@ using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
+using Random = Unity.Mathematics.Random;
 
 [UpdateInGroup(typeof(SimulationSystemGroup))]
 public class CarInitializeSystem : SystemBase
@@ -19,20 +20,28 @@ public class CarInitializeSystem : SystemBase
     {
         var commandBuffer = m_EntityCommandBufferSystem.CreateCommandBuffer().AsParallelWriter();
 
+        var random = new Random( (uint) Time.ElapsedTime + 18564584);
+
         Entities
             .WithName("CarInitSystem")
             .WithAll<Disabled>()
             .ForEach((Entity entity, int entityInQueryIndex, 
-                ref OffsetAuthoring offset,
-                ref SizeAuthoring size,
-                ref SpeedAuthoring speed,
-                ref BelongToSplineAuthoring belongToSpline,
-                ref CurrentSegmentAuthoring currentSegment,
-                ref ProgressAuthoring progress
+                ref Offset offset,
+                ref Size size,
+                ref Speed speed,
+                ref BelongToSpline belongToSpline,
+                ref CurrentSegment currentSegment,
+                ref Progress progress
             ) =>
             {
                 //Initializing car data
-
+                offset.Value = random.NextFloat(-1.0F, 1.0F);
+                var newSize = new float3(1f,1f,1f);
+                newSize.x = random.NextFloat(1.0F, 2.0F);
+                newSize.y = random.NextFloat(1.0F, 2.0F);
+                newSize.z = random.NextFloat(1.0F, 2.0F);
+                size.Value = newSize;
+                speed.Value = random.NextFloat(1.0F, 2.0F);
                 progress.Value = 0f;
 
                 //Enable the car
