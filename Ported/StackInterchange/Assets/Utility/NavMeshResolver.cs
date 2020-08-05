cd0091daac6842d0c5a7edb3e8ed6b0c16926096
Paul Demeulenaere <pauld@unity3d.com>
2020-08-05 22:12:50 +0200

    *WIP test utility to compute dynamically path based on navmesh

---
+++ Ported/StackInterchange/Assets/Utility/NavMeshResolver.cs
@@ -0,0 +1,229 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CustomEditor(typeof(NavMeshResolver))]
+public class NavMeshResolverEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (GUILayout.Button("Compute !!!"))
+        {
+            ((NavMeshResolver)target).Compute();
+        }
+    }
+}
+
+[ExecuteInEditMode]
+public class NavMeshResolver : MonoBehaviour
+{
+    public struct PathToResolve
+    {
+        public Transform From;
+        public Transform To;
+    }
+
+    public float m_maxSegmentLength = 15.0f;
+    public float m_mergeRadius = 1.0f;
+    public GameObject m_root;
+
+    private List<Transform> m_computeWayPoint = new List<Transform>();
+
+    private PathToResolve[] GeneratePathToResolve()
+    {
+        var pathToResolve = new PathToResolve[16];
+
+        var blueBegin = GameObject.Find("Blue_Begin").transform;
+        var blueEnd = GameObject.Find("Blue_End").transform;
+
+        var purpleBegin = GameObject.Find("Purple_Begin").transform;
+        var purpleEnd = GameObject.Find("Purple_End").transform;
+
+        var pinkBegin = GameObject.Find("Pink_Begin").transform;
+        var pinkEnd = GameObject.Find("Pink_End").transform;
+
+        var redBegin = GameObject.Find("Red_Begin").transform;
+        var redEnd = GameObject.Find("Red_End").transform;
+
+        //Going forward
+        /*pathToResolve[0] = new PathToResolve()
+        {
+            From = blueBegin,
+            To = blueEnd
+        };
+
+        pathToResolve[1] = new PathToResolve()
+        {
+            From = purpleBegin,
+            To = purpleEnd
+        };*/
+        
+        pathToResolve[2] = new PathToResolve()
+        {
+            From = pinkBegin,
+            To = pinkEnd
+        };
+        /*
+
+        pathToResolve[3] = new PathToResolve()
+        {
+            From = redBegin,
+            To = redEnd
+        };
+
+        //Turn right
+        pathToResolve[4] = new PathToResolve()
+        {
+            From = blueBegin,
+            To = redEnd
+        };
+
+        pathToResolve[5] = new PathToResolve()
+        {
+            From = pinkBegin,
+            To = blueEnd
+        };
+
+        pathToResolve[6] = new PathToResolve()
+        {
+            From = purpleBegin,
+            To = pinkEnd
+        };
+
+        pathToResolve[7] = new PathToResolve()
+        {
+            From = redBegin,
+            To = purpleEnd
+        };
+
+        //Turn left
+        pathToResolve[8] = new PathToResolve()
+        {
+            From = blueBegin,
+            To = pinkEnd
+        };
+
+        pathToResolve[9] = new PathToResolve()
+        {
+            From = pinkBegin,
+            To = purpleEnd
+        };
+
+        pathToResolve[10] = new PathToResolve()
+        {
+            From = purpleBegin,
+            To = redEnd
+        };
+
+        pathToResolve[11] = new PathToResolve()
+        {
+            From = redBegin,
+            To = blueEnd
+        };*/
+
+        return pathToResolve;
+    }
+
+    Transform GetTransform(Vector3 position)
+    {
+        var allTransform = m_root.GetComponentsInChildren<Transform>();
+        return allTransform.FirstOrDefault(o => (o.position - position).sqrMagnitude < m_mergeRadius * m_mergeRadius);
+    }
+
+    Transform GetOrAddTransform(Vector3 position)
+    {
+        var transform = GetTransform(position);
+        if (transform == null)
+        {
+            var newWayPoint = new GameObject("waypoint");
+            newWayPoint.transform.position = position;
+            newWayPoint.transform.SetParent(m_root.transform, true);
+            transform = newWayPoint.transform;
+        }
+        return transform;
+    }
+
+    public void Compute()
+    {
+        var pathToResolve = GeneratePathToResolve();
+
+        m_computeWayPoint.Clear();
+
+        var allPath = new List<GameObject>();
+        foreach (var path in pathToResolve)
+        {
+            if (path.From == null || path.To == null)
+                continue;
+
+            var navMeshPath = new NavMeshPath();
+            var result = new GameObject(path.From.gameObject.name + "_" + path.To.gameObject.name);
+            result.transform.position = new Vector3(0, 100.0f, 0);
+            var pathResult = result.AddComponent<PathResult>();
+
+            var currentWayPoint = new List<Transform>();
+            var distanceTraveled = float.MaxValue;
+
+            Vector3 currentPosition = path.From.position;
+            int maxIteration = 1024;
+            while ((currentPosition - path.To.position).sqrMagnitude > m_mergeRadius * m_mergeRadius)
+            {
+                if (!NavMesh.CalculatePath(currentPosition, path.To.position, NavMesh.AllAreas, navMeshPath))
+                {
+                    break;
+                }
+
+                if (navMeshPath.corners.Length < 2)
+                {
+                    break;
+                }
+
+                if (maxIteration-- < 0)
+                {
+                    break;
+                }
+
+                bool addPoint = false;
+                if (distanceTraveled > m_maxSegmentLength)
+                {
+                    addPoint = true;
+                }
+
+                var direction = navMeshPath.corners[1] - currentPosition;
+                if (!addPoint && direction.sqrMagnitude < m_mergeRadius * m_mergeRadius)
+                {
+                    addPoint = true;
+                    currentPosition = navMeshPath.corners[1];
+                }
+
+                if (!addPoint)
+                {
+                    var closeTransform = GetTransform(currentPosition);
+                    if (closeTransform != null && !currentWayPoint.Contains(closeTransform))
+                    {
+                        addPoint = true;
+                        currentPosition = closeTransform.transform.position;
+                    }
+                }
+
+                if (addPoint)
+                {
+                    currentWayPoint.Add(GetOrAddTransform(currentPosition));
+                    distanceTraveled = 0.0f;
+                }
+                else
+                {
+                    var dt = m_mergeRadius * 0.5f;
+                    currentPosition += direction.normalized * dt;
+                    distanceTraveled += dt;
+                }
+            }
+
+            pathResult.m_Path = currentWayPoint.ToArray();
+            result.transform.SetParent(m_root.transform, true);
+        }
+    }
+}
---
+++ Ported/StackInterchange/Assets/Utility/PathResult.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PathResult : MonoBehaviour
+{
+    public Transform[] m_Path;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (m_Path == null)
+            return;
+
+        for (int i = 0; i < m_Path.Length - 1; i++)
+        {
+            Debug.DrawLine(m_Path[i].transform.position, m_Path[i + 1].transform.position, Color.red);
+        }
+
+        for (int i = 0; i < m_Path.Length; i++)
+        {
+            Debug.DrawLine(m_Path[i].transform.position, m_Path[i].transform.position + Vector3.up, Color.green);
+        }
+    }
+}
