aecb4be9080c693949779a1c706dddb85de15b37
Arthur Dufay <arthur.dufay@unity3d.com>
2020-08-05 15:43:27 +0200

    Auto stash before merge of "2020-08-cest-grp1-antpheromones" and "origin/2020-08-cest-grp1-antpheromones"

--- Ported/DotsAnts/Assets/Scripts/PheromoneSteeringSystem.cs
+++ Ported/DotsAnts/Assets/Scripts/PheromoneSteeringSystem.cs
@@ -1,11 +1,61 @@
 using Unity.Entities;
-using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
+
 
 public class PheromoneSteeringSystem : SystemBase
 {
+	static Texture2D texture;
+    static int mapSize;
+    
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        AntDefaults defaults = Camera.main.GetComponent<AntDefaults>();
+        texture = defaults.pheromoneMap;
+        mapSize = defaults.mapSize;
+    }
+
+    static int PheromoneIndex(int x, int y, int mapSize)
+    {
+		return x + y * mapSize;
+	}
+
+	static float PheromoneSteering(float2 position, float facingAngle, float distance, int mapSize)
+	{
+		float output = 0;
+
+		for (int i=-1;i<=1;i+=2) {
+			float angle = facingAngle + i * Mathf.PI*.25f;
+			float testX = position.x + Mathf.Cos(angle) * distance;
+			float testY = position.y + Mathf.Sin(angle) * distance;
+
+			if (testX <0 || testY<0 || testX>=mapSize || testY>=mapSize) {
+
+			} 
+			else {
+				//int index = PheromoneIndex((int)testX,(int)testY,mapSize);
+				float value = texture.GetPixel((int)testX,(int)testY).r;
+				output += value*i;
+			}
+		}
+		return Mathf.Sign(output);
+	}
+
     protected override void OnUpdate()
     {
+    	//var mapResolution = GetSingleton<MapResolution>();
+    	//Debug.Log("test map =" + mapResolution.value);
+
+        //AntDefaults defaults = GameObject.Find("MainCamera").GetComponent<AntDefaults>();
+        //Debug.Log("test map 2 =" + defaults.mapSize);
+  
+
+		Entities.WithAll<Ant>().ForEach((in Position pos, in DirectionAngle directionAngle) => {
+
+			float pheromoneSteering = PheromoneSteering(pos.value, directionAngle.value, 3f, mapSize);
+
+		}).ScheduleParallel();
     }
 }
--- Ported/DotsAnts/Assets/Scripts/WallSteeringSystem.cs
+++ Ported/DotsAnts/Assets/Scripts/WallSteeringSystem.cs
@@ -1,11 +1,57 @@
 using Unity.Entities;
-using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
+
 
 public class WallSteeringSystem : SystemBase
 {
+	static private AntDefaults defaults;
+    
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        defaults = GameObject.Find("MainCamera").GetComponent<AntDefaults>();
+    }
+
+/*
+    Obstacle[] emptyBucket = new Obstacle[0];
+	Obstacle[] GetObstacleBucket(Vector2 pos) {
+		return GetObstacleBucket(pos.x,pos.y);
+	}
+	Obstacle[] GetObstacleBucket(float posX, float posY) {
+		int x = (int)(posX / mapSize * bucketResolution);
+		int y = (int)(posY / mapSize * bucketResolution);
+		if (x<0 || y<0 || x>=bucketResolution || y>=bucketResolution) {
+			return emptyBucket;
+		} else {
+			return obstacleBuckets[x,y];
+		}
+	}
+*/
+
+	static int WallSteering(float2 position, float facingAngle, float distance, int mapSize) {
+		int output = 0;
+
+		for (int i = -1; i <= 1; i+=2) {
+			float angle = facingAngle + i * Mathf.PI*.25f;
+			float testX = position.x + Mathf.Cos(angle) * distance;
+			float testY = position.y + Mathf.Sin(angle) * distance;
+
+			if (testX < 0 || testY < 0 || testX >= mapSize || testY >= mapSize) {
+
+			} else {
+				int value = 0; //GetObstacleBucket(testX,testY).Length;
+				if (value > 0) {
+					output -= i;
+				}
+			}
+		}
+		return output;
+	}
+
     protected override void OnUpdate()
     {
+    	//WallSteering(ant,1.5f);
     }
 }
