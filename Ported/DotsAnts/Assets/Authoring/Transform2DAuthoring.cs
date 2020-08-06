a412b26e9bde9c465079e770cdf1075dcb38476e
Benjamin John Spencer <ben.spencer@unity3d.com>
2020-08-06 10:49:59 +0100

    Added ant generator system and authoring components

---
+++ Ported/DotsAnts/Assets/Authoring/Transform2DAuthoring.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class Transform2DAuthoring : UnityEngine.MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new Position { value = ((float3)transform.position).xy });
+        dstManager.AddComponentData(entity, new Scale { value = ((float3)transform.localScale).xy });
+
+        dstManager.RemoveComponent<Translation>(entity);
+        dstManager.RemoveComponent<Rotation>(entity);
+        dstManager.RemoveComponent<Scale>(entity);
+        dstManager.RemoveComponent<NonUniformScale>(entity);
+    }
+}
+
+[WriteGroup(typeof(LocalToWorld))]
+public struct Scale : IComponentData
+{
+    public float2 value;
+}
---
+++ Ported/DotsAnts/Assets/Scripts/AntGenerationSystem.cs
@@ -0,0 +1,49 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public class AntGenerationSystem : SystemBase
+{
+    private EntityCommandBufferSystem m_CommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        m_CommandBufferSystem = World.GetExistingSystem<EndInitializationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        AntDefaults antDefaults = (AntDefaults)Camera.main.GetComponent(typeof(AntDefaults));
+        int mapSize = antDefaults.mapSize;
+        int antCount = antDefaults.antCount;
+        float2 colonyLocation = new float2();
+        bool successfullySpawned = false;
+       
+        var ecb = m_CommandBufferSystem.CreateCommandBuffer();
+        Entities.ForEach((Entity spawnerEntity, in AntGeneration spawner, in Position spawnerPosition) =>
+        {
+            colonyLocation = spawnerPosition.value + new float2(mapSize, mapSize) * 0.5f;
+
+            Unity.Mathematics.Random rng = new Unity.Mathematics.Random(89729364);
+            
+            for (int i = 0; i < antCount; i++)
+            {   
+                var instance = ecb.Instantiate(spawner.AntPrefab);
+                ecb.SetComponent(instance, new Position { value = colonyLocation });
+                ecb.SetComponent(instance, new DirectionAngle { value = math.PI * rng.NextFloat() });
+                ecb.SetComponent(instance, new CarryingFood { value = false });
+            }
+            ecb.DestroyEntity(spawnerEntity);
+            successfullySpawned = true;
+
+        }).Run();
+
+        if(successfullySpawned)
+        {
+            //EntityManager.CreateEntity(ComponentType.ReadOnly<ColonyLocation>());
+            SetSingleton(new ColonyLocation { value = colonyLocation });
+        }
+    }
+}
