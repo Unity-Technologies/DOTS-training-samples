2c19dfde525616855374617fc44fd262e346df2f
Aleksi Jokinen <aleksi.jokinen@gmail.com>
2020-08-06 10:54:05 +0300

    getting correct colony location

--- Ported/DotsAnts/Assets/Scripts/AntPositionSystem.cs
+++ Ported/DotsAnts/Assets/Scripts/AntPositionSystem.cs
@@ -68,6 +68,8 @@ public class AntPositionSystem : SystemBase
         Unity.Mathematics.Random mathRandom = new Unity.Mathematics.Random(12345678);
         var defaultValues = defaults;
         
+        float2 colonyPosition = GetSingleton<ColonyLocation>().value;
+        
         Entities.WithAll<Ant, LocalToWorld>().ForEach((ref Translation translation, ref Rotation rotation, ref Position position, ref DirectionAngle angle, ref Speed speed, in SteeringAngle steeringAngle, in CarryingFood carryingFood) =>
                 {
                     float targetSpeed = speed.value;
@@ -114,7 +116,7 @@ public class AntPositionSystem : SystemBase
                         pushRadius = defaultValues.mapSize;
                     }
 
-                    var colonyPosition = new float2(defaultValues.mapSize / 2.0f, defaultValues.mapSize / 2.0f);
+                    
                     
                     dx = colonyPosition.x - position.value.x;
                     dy = colonyPosition.y - position.value.y;
