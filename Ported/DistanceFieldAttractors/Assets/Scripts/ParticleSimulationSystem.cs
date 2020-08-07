83e5605bbea1217bbfe7c8d647e9337fa27cd193
Elizabeth Baumel <ebaumel@unity3d.com>
2020-08-07 15:28:45 -0700

    Use correct up vector, make velocity in parameter, delete unused deltaTime

--- Ported/DistanceFieldAttractors/Assets/Scripts/ParticleSimulationSystem.cs
+++ Ported/DistanceFieldAttractors/Assets/Scripts/ParticleSimulationSystem.cs
@@ -6,14 +6,13 @@ public class ParticleSimulationSystem : SystemBase
     protected override void OnUpdate()
     {
         var emitterSettings = GetSingleton<ParticleEmitterSettings>();
-        var deltaTime = Time.DeltaTime;
 
         Entities
             .WithName("ParticleVariableRateSimulation")
-            .ForEach((ref LocalToWorld localToWorld, ref Velocity velocity, in Translation translation) =>
+            .ForEach((ref LocalToWorld localToWorld, in Velocity velocity, in Translation translation) =>
         {
             var scale = new float3(.1f, .01f, math.max(.1f, math.length(velocity.Value) * emitterSettings.SpeedStretch));
-            var rotation = quaternion.LookRotationSafe(velocity.Value, scale);
+            var rotation = quaternion.LookRotationSafe(velocity.Value, new float3(0f, 1f, 0f));
             localToWorld.Value = float4x4.TRS(translation.Value, rotation, scale);
         }).ScheduleParallel();
     }
