18eb794d84b43e844428ce04c2218338113e7989
Milo <milo.verbowski@unity3d.com>
2020-08-12 15:54:34 -0700

    basic start for colourupdater

---
+++ Ported/BucketBrigadePort/Assets/Scripts/Systems/ColorUpdaterSystem.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+
+public class ColorUpdaterSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+        // Create queries and static data only here.
+
+    }
+    protected override void OnUpdate()
+    {
+        Entities
+           .WithStructuralChanges()
+           .ForEach((Entity entity, ref Color color, in Temperature temperature, in Tile tile) =>
+           {
+
+           }).Run();
+    }
+}
