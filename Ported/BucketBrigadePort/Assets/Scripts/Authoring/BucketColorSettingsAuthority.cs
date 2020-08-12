d0309875307613d38f47ca1824513fcac1e32e2f
Milo <milo.verbowski@unity3d.com>
2020-08-12 16:55:35 -0700

    bucket colour

---
+++ Ported/BucketBrigadePort/Assets/Scripts/Authoring/BucketColorSettingsAuthority.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+// [GenerateAuthoringComponent]
+
+public struct BucketColorSettings : IComponentData
+{
+    public float4 Empty;
+    public float4 Full;
+}
+
+
+// TODO: This is not showing the colorpicker in the editor. To fix.
+public class BucketColorSettingsAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public UnityEngine.Color BucketEmpty; // = new UnityEngine.Color(255, 105, 117, 1);
+    public UnityEngine.Color BucketFull; // = new UnityEngine.Color(0, 250, 255, 1);
+
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new BucketColorSettings
+        {
+            Empty = new float4(BucketEmpty.r, BucketEmpty.g, BucketEmpty.b, BucketEmpty.a),
+            Full = new float4(BucketFull.r, BucketFull.g, BucketFull.b, BucketFull.a)
+        });
+    }
+}
--- Ported/BucketBrigadePort/Assets/Scripts/Authoring/ColorAuthoring.cs
+++ Ported/BucketBrigadePort/Assets/Scripts/Authoring/ColorAuthoring.cs
@@ -1,7 +1,9 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Rendering;
 
 [GenerateAuthoringComponent]
+[MaterialProperty("_BaseColor", MaterialPropertyFormat.Float4)]
 public struct Color : IComponentData
 {
     public float4 Value;
