169a52d05a20fee2e18174aa8b2509fb4f9d4c3a
Jonathan Brodsky <jonathan.brodsky@unity3d.com>
2020-08-12 10:05:39 -0700

    brigade intialization
    passer types only

---
+++ Ported/BucketBrigade/Assets/Scripts/BrigadeColorAuthoring.cs
@@ -0,0 +1,40 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using UnityEngine;
+
+public struct BrigadeColor : IComponentData
+{
+    public float4 tossColor;
+    public float4 scoopColor;
+    public float4 emptyColor;
+    public float4 fullColor;
+}
+
+[MaterialProperty("_BaseColor", MaterialPropertyFormat.Float4)]
+public struct BotColor : IComponentData
+{
+    public float4 Value;
+}
+
+public class BrigadeColorAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public UnityEngine.Color tossColor;
+    public UnityEngine.Color scoopColor;
+    public UnityEngine.Color emptyColor;
+    public UnityEngine.Color fullColor;
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new BrigadeColor()
+        {
+            tossColor = ColorToFloat4(tossColor),
+            scoopColor = ColorToFloat4(scoopColor),
+            emptyColor = ColorToFloat4(emptyColor),
+            fullColor = ColorToFloat4(fullColor)
+        });
+    }
+    public float4 ColorToFloat4(UnityEngine.Color c)
+    {
+        return new float4(c.r, c.g, c.b, c.a);
+    }
+}
---
+++ Ported/BucketBrigade/Assets/Scripts/BrigadeInitializationSystem.cs
@@ -0,0 +1,70 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public struct BrigadeId : ISharedComponentData
+{
+    public int Value;
+}
+public class BrigadeInitializationSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entities
+            .WithStructuralChanges()
+            .ForEach((in Entity e, in BrigadeInitialization init, in BrigadeColor colors) =>
+            {
+                for (int i = 0; i < init.brigadeCount; i++)
+                {
+                    for (int j = 0; j < init.emptyPassers; j++)
+                    {
+                        var instance = EntityManager.Instantiate(init.bot);
+                        SetComponent(instance, new Translation()
+                        {
+                            Value = GetChainPosition(j, init.emptyPassers, float3.zero, new float3(10, 0, 0))    
+                        });
+                        EntityManager.AddComponentData(instance, new BotColor()
+                        {
+                            Value = colors.emptyColor
+                        });
+                        EntityManager.AddSharedComponentData(instance, new BrigadeId()
+                        {
+                            Value = i
+                        });
+                    }
+                    for (int j = 0; j < init.fullPassers; j++)
+                    {
+                        var instance = EntityManager.Instantiate(init.bot);
+                        SetComponent(instance, new Translation()
+                        {
+                            Value = GetChainPosition(j, init.fullPassers,new float3(10, 0, 0), float3.zero)    
+                        });
+                        EntityManager.AddComponentData(instance, new BotColor()
+                        {
+                            Value = colors.fullColor
+                        });
+                        EntityManager.AddSharedComponentData(instance, new BrigadeId()
+                        {
+                            Value = i
+                        });
+                    }
+                }
+                // remove the setup data
+                EntityManager.DestroyEntity(e);
+            }).Run();
+    }
+    static float3 GetChainPosition(int _index, int _chainLength, float3 _startPos, float3 _endPos){
+        // adds two to pad between the SCOOPER AND THROWER
+        float progress = (float) _index / _chainLength;
+        float curveOffset = math.sin(progress * math.PI) * 1f;
+
+        // get Vec2 data
+        float2 heading = new float2(_startPos.x, _startPos.z) -  new float2(_endPos.x, _endPos.y);
+        float distance = math.length(heading);
+        float2 direction = heading / distance;
+        float2 perpendicular = new float2(direction.y, -direction.x);
+
+        //Debug.Log("chain progress: " + progress + ",  curveOffset: " + curveOffset);
+        return math.lerp(_startPos, _endPos, (float)_index / (float)_chainLength) + (new float3(perpendicular.x, 0f, perpendicular.y) * curveOffset);
+    }
+}
