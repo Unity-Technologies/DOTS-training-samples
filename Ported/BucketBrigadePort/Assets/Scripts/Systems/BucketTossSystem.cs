059c699d9f83f724f8a7016fb1b8eb1961aa6c15
Barbara Wang <barbara.wang@unity3d.com>
2020-08-13 17:01:38 -0400

    Update fileTile temperature after tossing a bucket

--- Ported/BucketBrigadePort/Assets/Scripts/Systems/BucketTossSystem.cs
+++ Ported/BucketBrigadePort/Assets/Scripts/Systems/BucketTossSystem.cs
@@ -18,9 +18,9 @@ public class BucketTossSystem : SystemBase
         {
             All = new[]
             {
-                ComponentType.ReadOnly<Translation>(),
+                ComponentType.ReadOnly<Tile>(),
                 typeof(Temperature)
-            }
+    }
         });
 
         m_tosserQuery = GetEntityQuery(new EntityQueryDesc
@@ -28,55 +28,161 @@ public class BucketTossSystem : SystemBase
             All = new[]
             {
                 ComponentType.ReadOnly<Translation>(),
-                ComponentType.ReadOnly<BucketTosser>(),
+                ComponentType.ReadOnly<Bot>(),
+                ComponentType.ReadOnly<BucketTosser>()
             }
-        });        
+        });
+
+        m_bucketQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[]
+            {
+                ComponentType.ReadOnly<Translation>(),
+                ComponentType.ReadOnly<WaterAmount>()
+            },
+
+            None = new[]
+            {
+                ComponentType.ReadOnly<WaterRefill>()
+            }
+        });
     }
 
     protected override void OnUpdate()
     {
-        // hardcoded water amount for a bucketWater 1
-        // hardcoded range of a lake 5
+        // hardcode settings
         var bucketWater = 1.0f;
         var fireRange = 1.0f;
+        var bucketDistRange = 1.0f;
 
         var bucketEmptyColor = GetSingleton<BucketColorSettings>().Empty;
+        var flashPoint = GetSingleton<flashpoint>().flashpoint;
 
-        var fireCellTranslations =
-            m_fireCellQuery.ToComponentDataArray<Translation>(Allocator.TempJob);
+        var tileSpawner = GetSingleton<TileSpawner>();
+        int columns = tileSpawner.XSize;
+        int rows = tileSpawner.YSize;
+
+        var fireCellTiles =
+            m_fireCellQuery.ToComponentDataArray<Tile>(Allocator.TempJob);
+        var fireCellTemperatures =
+            m_fireCellQuery.ToComponentDataArray<Temperature>(Allocator.TempJob);
         var tosserTranslations =
             m_tosserQuery.ToComponentDataArray<Translation>(Allocator.TempJob);
-        
+        var bucketTranslations =
+            m_bucketQuery.ToComponentDataArray<Translation>(Allocator.TempJob);
+        var bucketWaterAmounts =
+            m_bucketQuery.ToComponentDataArray<WaterAmount>(Allocator.TempJob);
+
+        // Handle fileCell being tossed at
+        Entities
+        .WithName("bucket_toss_fileCell")
+        .WithNone<Temperature>()
+        .WithDisposeOnCompletion(bucketTranslations)
+        .WithDisposeOnCompletion(bucketWaterAmounts)
+        .ForEach((ref Temperature fireCellTemperature, in Translation position) =>
+        {
+            // fireCell is flashed
+            if (fireCellTemperatures[j].Value >= flashPoint)
+            {
+                for (int i = 0; i < tosserTranslations.Length; i++)
+                {
+                    var tosserFireDist = Vector3.Distance(tosserTranslations[i].Value, fireCellTiles[j].Value);
+
+                    // tosser is not close to the firecell
+                    if (tosserFireDist > fireRange)
+                    {
+                        continue;
+                    }
+
+                    for (int j = 0; j < bucketTranslations.Length; j++)
+                    {
+                        // bucket is not full
+                        if (bucketWaterAmounts[j].Value < bucketWater)
+                        {
+                            continue;
+                        }
+
+                        var tosserBucketDist = Vector3.Distance(position.Value, tosserTranslations[i].Value);
+
+                        // full bucket is not with the tosser
+                        if (tosserBucketDist > bucketDistRange)
+                        {
+                            continue;
+                        }
+
+                        //fireCellTemperature
+
+                    for (int j = 0; j < fireCellTiles.Length; j++)
+                    {
+                        var tosserFireDist = Vector3.Distance(tosserTranslations[i].Value, fireCellTiles[j].Value);
+
+                        // tosser is not close to a firecell
+                        if (tosserFireDist > fireRange)
+                        {
+                            continue;
+                        }
+
+                        // Toss the bucket
+                        bucketWaterAmount.Value -= bucketWater;
+                        if (bucketWaterAmount.Value < 0)
+                        {
+                            bucketWaterAmount.Value = 0;
+                        }
+                        bucketColor.Value = bucketEmptyColor;
+                    }
+                }
+            }
+
+        }).ScheduleParallel();
+
+        // Handle the bucket being tossed
         Entities
         .WithName("bucket_toss_buckets")
         .WithAll<WaterAmount>()
         .WithNone<WaterRefill>()
-        .WithDisposeOnCompletion(fireCellTranslations)
+        .WithDisposeOnCompletion(fireCellTiles)
         .WithDisposeOnCompletion(tosserTranslations)
         .ForEach((ref WaterAmount bucketWaterAmount, ref Color bucketColor, in Translation position) =>
         {
-            if (bucketWaterAmount.Value > 0)
+            for (int i = 0; i < tosserTranslations.Length; i++)
             {
-                for (int i = 0; i < fireCellTranslations.Length; i++)
+                // bucket is not full
+                if (bucketWaterAmount.Value < bucketWater)
                 {
-                    for (int j = 0; j < tosserTranslations.Length; j++)
-                    {
-                        var tosserFireDist = Vector3.Distance(fireCellTranslations[i].Value, tosserTranslations[j].Value);
-                        if (tosserFireDist <= fireRange)
-                        {
-                            var tosserBucketDist = Vector3.Distance(position.Value, tosserTranslations[j].Value);
+                    break;
+                }
 
-                            if (tosserFireDist <= fireRange)
-                            {
-                                bucketWaterAmount.Value -= bucketWater;
-                                if(bucketWaterAmount.Value < 0)
-                                {
-                                    bucketWaterAmount.Value = 0;
-                                }
-                                bucketColor.Value = bucketEmptyColor;
-                            }
-                        }
+                var tosserBucketDist = Vector3.Distance(position.Value, tosserTranslations[i].Value);
+
+                // full bucket is not with the tosser
+                if (tosserBucketDist > bucketDistRange)
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < fireCellTranslations.Length; j++)
+                {
+                    // fireCell not flashed
+                    if (fireCellTemperatures.Value < flashPoint)
+                    {
+                        continue;
                     }
+
+                    var tosserFireDist = Vector3.Distance(tosserTranslations[i].Value, fireCellTranslations[j].Value);
+
+                    // tosser is not close to a firecell
+                    if (tosserFireDist > fireRange)
+                    {
+                        continue;
+                    }
+
+                    // Toss the bucket
+                    bucketWaterAmount.Value -= bucketWater;
+                    if (bucketWaterAmount.Value < 0)
+                    {
+                        bucketWaterAmount.Value = 0;
+                    }
+                    bucketColor.Value = bucketEmptyColor;
                 }
             }
 
