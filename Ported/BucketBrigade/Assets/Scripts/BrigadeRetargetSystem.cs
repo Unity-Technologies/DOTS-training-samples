77690dd1598a87e656a881f9f0888e64378629a2
Boris Bauer <boris.bauer@unity3d.com>
2020-08-13 15:11:57 -0400

    Fixed brigade's fire targeting

--- Ported/BucketBrigade/Assets/Scripts/BrigadeRetargetSystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/BrigadeRetargetSystem.cs
@@ -10,7 +10,7 @@ using Random = Unity.Mathematics.Random;
 public class BrigadeRetargetSystem : SystemBase
 {
     private float updateSpeed = 10.0f;
-    private float nextUpdate = 0;
+    private float nextUpdate = 0.1f;
     private Random random;
 
     private EntityQuery temperatureGroup;
@@ -31,6 +31,9 @@ public class BrigadeRetargetSystem : SystemBase
         }
         nextUpdate = updateSpeed;
 
+        var fireConfigEntity = GetSingletonEntity<FireConfiguration>();
+        var fireConfig = EntityManager.GetComponentData<FireConfiguration>(fireConfigEntity);
+
         // build the brigade lookup
         // this should really be done once
         var BrigadeDataLookup = new NativeHashMap<Entity, Brigade>(16, Allocator.TempJob);
@@ -45,6 +48,7 @@ public class BrigadeRetargetSystem : SystemBase
         var riverTranslation = GetComponentDataFromEntity<Translation>(true);
         // just randomly updating the targets
         // will need to be replaced by the search
+        
         var handle1 = Entities.WithAll<Brigade>()
             .WithReadOnly(temperatures)
             .WithReadOnly(riverTranslation)
@@ -52,45 +56,44 @@ public class BrigadeRetargetSystem : SystemBase
             .WithReadOnly(translations)
             .ForEach((ref Brigade brigade) =>
             {
-
                 float3 waterTarget = riverTranslation[brigade.waterEntity].Value;
-            
-            // Find closest fire
-            float closestDistSq = 0;
-            float3 closestFirePosition = float3.zero;
-            Entity closestFire = Entity.Null;
-            bool foundFire = false;
-            for (int i = 0; i < temperatureArray.Length; ++i)
-            {
-                Entity e = temperatureArray[i];
-                Temperature theirTemp = temperatures[e];
-                Translation translation = translations[e];
 
-                float3 diff = waterTarget - translation.Value;
-                float distSq = diff.x * diff.x + diff.z * diff.z;
-                if (!foundFire || distSq < closestDistSq)
+                // Find closest fire
+                float closestDistSq = 0;
+                float3 closestFirePosition = float3.zero;
+                Entity closestFire = Entity.Null;
+                bool foundFire = false;
+                for (int i = 0; i < temperatureArray.Length; ++i)
                 {
-                    foundFire = true;
-                    closestDistSq = distSq;
-                    closestFirePosition = translation.Value;
-                    closestFirePosition.y = 0;
-                    closestFire = e;
+                    Entity e = temperatureArray[i];
+                    Temperature theirTemp = temperatures[e];
+                    Translation translation = translations[e];
+
+                    float3 diff = waterTarget - translation.Value;
+                    float distSq = diff.x * diff.x + diff.z * diff.z;
+                    if (theirTemp.Value > fireConfig.FlashPoint && (!foundFire || distSq < closestDistSq))
+                    {
+                        foundFire = true;
+                        closestDistSq = distSq;
+                        closestFirePosition = translation.Value;
+                        closestFirePosition.y = 0;
+                        closestFire = e;
+                    }
                 }
-            }
 
-            if (foundFire)
-            {
-                brigade.fireTarget = closestFirePosition;
-                brigade.fireEntity = closestFire;
-            }   
-            else
-            {
-                brigade.fireTarget = brigade.random.NextFloat3() * 10.0f;
-                brigade.fireEntity = Entity.Null;
-            }
+                if (foundFire)
+                {
+                    brigade.fireTarget = closestFirePosition;
+                    brigade.fireEntity = closestFire;
+                }
+                else
+                {
+                    brigade.fireTarget = brigade.random.NextFloat3() * 10.0f;
+                    brigade.fireEntity = Entity.Null;
+                }
 
-            brigade.fireTarget.y = 0;
-        }).Schedule(Dependency);
+                brigade.fireTarget.y = 0;
+            }).Schedule(Dependency);
         
         var handle2 = Entities
             .WithReadOnly(riverTranslation)
