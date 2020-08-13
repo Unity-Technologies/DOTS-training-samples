9c2a167aa1f6a98787a27c85dc92e103e2d95f53
Scott Peterson <scott.peterson@unity3d.com>
2020-08-13 15:28:42 -0700

    Remove stuff I had added to BotAuthoring to prep for merge.

--- Ported/BucketBrigadePort/Assets/Scripts/Authoring/BotAuthoring.cs
+++ Ported/BucketBrigadePort/Assets/Scripts/Authoring/BotAuthoring.cs
@@ -5,17 +5,6 @@ using Unity.Transforms;
 [GenerateAuthoringComponent]
 public struct Bot : IComponentData
 {
-    public Translation targetTranslation;
 }
 
-public struct LineId : IComponentData
-{
-    public int Value;
-}
 
-public struct BucketTosser : IComponentData
-{
-    public Entity BucketFiller;
-}
-
-public struct BucketFiller : IComponentData { }
