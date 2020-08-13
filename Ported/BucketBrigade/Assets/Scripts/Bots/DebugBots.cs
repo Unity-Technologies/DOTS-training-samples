ff63b034b2d9beee048a588df767cca8f38088eb
Jonathan Brodsky <jonathan.brodsky@unity3d.com>
2020-08-13 14:20:38 -0700

    bucket colors

--- Ported/BucketBrigade/Assets/Scripts/Bots/DebugBots.cs
+++ Ported/BucketBrigade/Assets/Scripts/Bots/DebugBots.cs
@@ -18,13 +18,13 @@ public class DebugBots : SystemBase
                         Translation t = EntityManager.GetComponentData<Translation>(nextBot.Value);
                         var start = translation.Value;
                         var end = translation.Value + (t.Value - translation.Value) * 0.8f;
-                        Debug.DrawLine(start, end, Color.yellow, 0.1f);
+                        Debug.DrawLine(start, end, UnityEngine.Color.yellow, 0.1f);
                     }
                     else
                     {
                         var start = translation.Value;
                         var end = translation.Value + new float3(0, 2, 0); 
-                        Debug.DrawLine(start, end, Color.red, 0.1f);
+                        Debug.DrawLine(start, end, UnityEngine.Color.red, 0.1f);
                     }
                 }
             ).Run();
--- Ported/BucketBrigade/Assets/Scripts/Bucket/BucketSystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/Bucket/BucketSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine.Rendering;
 
 public struct Owner : IComponentData
@@ -9,6 +10,20 @@ public class BucketSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-                
+        Entities
+            .WithAll<Bucket>()
+            .WithNone<Color>()
+            .WithStructuralChanges()
+            .ForEach((Entity e) =>
+            {
+                EntityManager.AddComponent<Color>(e);
+            }).Run();
+        // update the color of the bucket based on the fill amount
+        Entities
+            .ForEach((Entity e, ref Color c, in Water w, in Bucket b) =>
+            {
+                c.Value = math.lerp(b.emptyColor, b.fullColor, w.volume / w.capacity);
+            }).Schedule();
+        
     }
 }
