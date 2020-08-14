0d581ea2ab69a3902a06b1e2256220ea3347fef2
Scott Peterson <scott.peterson@unity3d.com>
2020-08-13 23:05:44 -0700

    Added system to move bots into lines.

---
+++ Ported/BucketBrigadePort/Assets/Scripts/Systems/LineMoveSystem.cs
@@ -0,0 +1,96 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+using UnityEngine;
+
+// [UpdateInGroup(typeof(SimulationSystemGroup))]
+// [UpdateAfter(typeof(LineUpdateSystem))]
+public class LineMoveSystem : SystemBase
+{
+    private EntityQuery m_LineModifyQuery;
+    private EntityCommandBufferSystem m_ECBSystem;
+
+    protected override void OnCreate()
+    {
+        m_LineModifyQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[]
+            {
+                ComponentType.ReadOnly<LineModify>(),
+            }
+        });
+        m_ECBSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    private static float3 GetLinePosition(float t, float3 startPos, float3 endPos)
+    {
+        // Compute a position along a line defined by a start and end position.
+        // Returns positions that are curved from the center by a perpendicular offset.
+        // Positive t means offset to one side of the line, and negative means the other.
+        float signT = math.sign(t);
+        t = math.saturate(math.abs(t));
+        float curveOffset = math.sin(t * math.PI);
+        float2 direction = new float2(startPos.x, startPos.z) - new float2(endPos.x, endPos.y);
+        direction = math.normalize(direction);
+        // Switch sides based on the pasitive or negative of t
+        float3 perpendicular = signT * new float3(direction.y, 0, -direction.x);
+
+        return math.lerp(startPos, endPos, t) + perpendicular * curveOffset;
+    } 
+    
+    protected override void OnUpdate()
+    {
+        // Get the lines.
+        var lineModifiers =
+            m_LineModifyQuery.ToComponentDataArrayAsync<LineModify>(Allocator.TempJob, out var lineModifiersHandle);
+
+        Dependency = JobHandle.CombineDependencies(Dependency, lineModifiersHandle);
+        
+        var ecb = m_ECBSystem.CreateCommandBuffer();
+
+        Entities
+            .WithName("LineAssignBot")
+            .WithDisposeOnCompletion(lineModifiers)
+            .ForEach((
+                Entity botEntity,
+                ref BotRootPosition rootPosition,
+                in LineId lineId,
+                in BotLineLocationId lineLocation) =>
+            {
+                for (int i = 0; i < lineModifiers.Length; i++)
+                {
+                    if (lineId.Value == lineModifiers[i].lineId)
+                    {
+                        float3 linePosition = GetLinePosition(lineLocation.Value,
+                            lineModifiers[i].fillTranslation, lineModifiers[i].tossTranslation);
+                        rootPosition.Value = linePosition;
+                        // Now make the bot walk to the linePosition
+                        // TODO: Check that AddComponent will overwrite an existing component.
+                        ecb.AddComponent<TargetPosition>(botEntity, new TargetPosition
+                        {
+                            Value = linePosition
+                        });
+                    }
+                }
+            }).Schedule();
+
+        // Destroy all of the LineModifier Entities (because they are processed only once).
+        // Note that the entityCommandBuffer dependency is already set up to run after the job.
+        
+        // DestroyEntity doesn't work, because it destroys them before lineModifiers is set.
+        // Even when I set the order of the system: [UpdateAfter(typeof(LineUpdateSystem))].
+        // ecb.DestroyEntity(m_LineModifyQuery);
+        
+        Entities
+            .WithName("DeleteLineModify")
+            .ForEach((Entity lineModifyEntity, in LineModify lineModify) =>
+            {
+                ecb.DestroyEntity(lineModifyEntity);
+            }).Schedule();
+
+        // Register a dependency for the EntityCommandBufferSystem.
+        m_ECBSystem.AddJobHandleForProducer(Dependency);
+    }
+}
---
+++ Ported/BucketBrigadePort/Assets/Scripts/Systems/TosserSeekFireSystem.cs
@@ -0,0 +1,96 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class TosserSeekFireSystem : SystemBase
+{
+    private float m_AssesLineTimer = 0f;
+    private const float m_AssesLineRate = 2f;
+    private EntityQuery m_FireTranslationsQuery;
+    private EntityCommandBufferSystem m_ECBSystem;
+
+    protected override void OnCreate()
+    {
+        m_FireTranslationsQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[]
+            {
+                ComponentType.ReadOnly<Translation>(),
+                ComponentType.ReadOnly<OnFire>(),
+            }
+        });
+        m_ECBSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.DeltaTime;
+        m_AssesLineTimer -= deltaTime;
+        if (m_AssesLineTimer <= 0)
+        {
+            m_AssesLineTimer = m_AssesLineRate;
+            
+            // Get the positions of all of the fire.
+            var fireTranslations =
+                m_FireTranslationsQuery.ToComponentDataArrayAsync<Translation>(Allocator.TempJob,
+                    out var fireTranslationsHandle);
+            
+            Dependency = JobHandle.CombineDependencies(Dependency, fireTranslationsHandle);
+            var ecb = m_ECBSystem.CreateCommandBuffer().AsParallelWriter();
+            
+            // System updates End Point every 2s, sets to closest fire to the Line Beginning Point
+            // IF the line end point changes, then the system needs to set new target positions of all of the 
+            // bats on the line.
+            // Inputs: entities with bucket tosser tag, target fire position
+
+            Entities
+                .WithName("TosserSeekFire")
+                .WithDisposeOnCompletion(fireTranslations)
+                .ForEach((
+                    int entityInQueryIndex,
+                    Entity tosserEntity, 
+                    ref BotRoleTosser tosser, 
+                    in Translation translation,
+                    in LineId lineId) =>
+                {
+                    // Find the closest fire point.
+                    float3 minPosition = float3.zero;
+                    float minDistance = 999999999f;
+                    bool fireFound = false;
+                    for (int fireIndex = 0; fireIndex < fireTranslations.Length; fireIndex++)
+                    {
+                        float distance = math.length(translation.Value.xz - fireTranslations[fireIndex].Value.xz);
+                        if (distance < minDistance)
+                        {
+                            minDistance = distance;
+                            minPosition = fireTranslations[fireIndex].Value;
+                            fireFound = true;
+                        }
+                    }
+
+                    // If the closest fire point is different than the current fire point, then set a new 
+                    // targetPosition.
+                    // TODO: This is an optimization for later.
+                    
+                    if (fireFound && tosser.BotFiller != Entity.Null)
+                    {
+                        var fillerTranslation = GetComponent<Translation>(tosser.BotFiller);
+                        var eventEntity = ecb.CreateEntity(entityInQueryIndex);
+                        ecb.AddComponent(entityInQueryIndex, eventEntity, component: new LineModify
+                        {
+                            lineId = lineId.Value,
+                            tossTranslation = minPosition,
+                            fillTranslation = fillerTranslation.Value
+                        });
+                    }
+
+                }).ScheduleParallel();
+            
+            // Register a dependency for the EntityCommandBufferSystem.
+            m_ECBSystem.AddJobHandleForProducer(Dependency);
+        }
+    }
+}
