bd91da2c7a1b61f152deb0d4f437ff85ce194325
Milo <milo.verbowski@unity3d.com>
2020-08-13 17:05:13 -0700

    Added dependents to botRoles so they don't need to look them up

--- Ported/BucketBrigadePort/Assets/Scripts/Authoring/BotRoleAuthoring.cs
+++ Ported/BucketBrigadePort/Assets/Scripts/Authoring/BotRoleAuthoring.cs
@@ -2,18 +2,23 @@
 
 public struct BotRoleFinder : IComponentData
 {
+    Entity Dependent;
 }
 public struct BotRoleTosser : IComponentData
 {
+    Entity Dependent;
 }
 public struct BotRoleFiller : IComponentData
 {
+    Entity Dependent;
 }
 public struct BotRolePasserFull : IComponentData
 {
+    Entity Dependent;
 }
 public struct BotRolePasserEmpty : IComponentData
 {
+    Entity Dependent;
 }
 public struct BotRoleOmni : IComponentData
 {
