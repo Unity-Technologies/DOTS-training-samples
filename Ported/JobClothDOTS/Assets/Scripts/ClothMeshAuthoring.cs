31a7275c3821e3282cd5a2303764ce287650d0eb
Lasse Jon Fuglsang Pedersen <volmer@gmail.com>
2020-09-08 17:50:43 +0200

    add skeleton

---
+++ Ported/JobClothDOTS/Assets/Scripts/ClothMeshAuthoring.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+public struct ClothMesh : ISharedComponentData, IEquatable<ClothMesh>
+{
+	public Mesh mesh;
+	public NativeArray<float3> vertexPosition;
+	public NativeArray<float> vertexMass;
+
+	public bool Equals(ClothMesh other)
+	{
+		return (
+			mesh == other.mesh &&
+			vertexPosition == other.vertexPosition &&
+			vertexMass == other.vertexMass
+		);
+	}
+
+	public override int GetHashCode()
+	{
+		int hash = 0;
+		{
+			hash ^= ReferenceEquals(mesh, null) ? 0 : mesh.GetHashCode();
+			hash ^= vertexMass.GetHashCode();
+			hash ^= vertexPosition.GetHashCode();
+		}
+		return hash;
+	}
+}
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class ClothMeshAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+		var mf = GetComponent<MeshFilter>();
+		if (mf == null)
+			return;
+
+		var meshInstance = GetComponent<MeshFilter>().mesh;
+		if (meshInstance == null)
+			return;
+
+		// initialize position
+		var bufferPosition = new NativeArray<float3>(meshInstance.vertexCount, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+		{
+			using (var meshData = Mesh.AcquireReadOnlyMeshData(meshInstance))
+			{
+				meshData[0].GetVertices(bufferPosition.Reinterpret<Vector3>());
+			}
+		}
+
+		// initialize mass
+		var bufferMass = new NativeArray<float>(meshInstance.vertexCount, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+		{
+			for (int i = 0; i != meshInstance.vertexCount; i++)
+			{
+				bufferMass[i] = 1.0f;
+				// TODO pinned vertices
+			}
+		}
+
+		dstManager.AddSharedComponentData(entity, new ClothMesh
+		{
+			mesh = meshInstance,
+			vertexPosition = bufferPosition,
+			vertexMass = bufferMass,
+		});
+
+		meshInstance.MarkDynamic();
+	}
+}
