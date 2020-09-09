02360d0ee1acda79902f940a2f2fd3cbca08513f
Colm Vize <colm@artomatix.com>
2020-09-09 12:28:52 +0100

    updated transform system

--- Ported/LabRat/Assets/Scripts/Systems/TransformSystem.cs
+++ Ported/LabRat/Assets/Scripts/Systems/TransformSystem.cs
@@ -5,11 +5,26 @@ using Unity.Transforms;
 [UpdateInGroup(typeof(TransformSystemGroup))]
 public class TransformSystem : SystemBase
 {
+    private EntityCommandBufferSystem ecbSystem;
+
+    protected override void OnCreate()
+    {
+        this.ecbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
     protected override void OnUpdate()
     {
+        var ecb = this.ecbSystem.CreateCommandBuffer();
+
         Entities.WithName("L2WFromPositionXZ")
+            .WithStructuralChanges()
             .WithChangeFilter<PositionXZ>()
-            .ForEach((ref LocalToWorld l2w, in PositionXZ pos) => l2w.Value = float4x4.Translate(new float3(pos.Value.x, 0f, pos.Value.y)))
-            .ScheduleParallel();
+            .ForEach((Entity entity, in LocalToWorld l2w, in PositionXZ pos) =>
+            {
+                ecb.SetComponent(entity, new Translation
+                {
+                    Value = l2w.Position + new float3(pos.Value.x, 0, pos.Value.y)
+                });
+            }).Run();
     }
 }
