19fa0596d7ffda8beaab2fea2494c1348c578566
Andy Milsom <andrewmi@unity3d.com>
2020-09-09 09:43:26 +0100

    InitSetup

---
+++ Ported/CombatBees/Assets/Scripts/Components/BattleFieldAuthoring.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct BattleField : IComponentData
+{
+	public float3 Bounds;
+	public float HiveDistance;
+}
