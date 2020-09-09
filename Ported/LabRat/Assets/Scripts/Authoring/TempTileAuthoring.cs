bdc8c18309965755139b98ec43527f8e132b4b47
Torbjorn Laedre <271210+tlaedre@users.noreply.github.com>
2020-09-09 23:26:25 +0200

    Rename temp authoring field.

--- Ported/LabRat/Assets/Scripts/Authoring/TempTileAuthoring.cs
+++ Ported/LabRat/Assets/Scripts/Authoring/TempTileAuthoring.cs
@@ -1,15 +1,15 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 class TempTileAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
-    
-    [SerializeField] Tile.Attributes walls;
+    [FormerlySerializedAs("walls")] [SerializeField] Tile.Attributes attributes;
 
     public void Convert(Entity entity, EntityManager mgr, GameObjectConversionSystem conversionSystem)
     {
-        mgr.AddComponentData(entity, new Tile { Value = walls});
+        mgr.AddComponentData(entity, new Tile { Value = attributes});
         mgr.AddComponentData(entity, new Size { Value = transform.localScale.x });
         mgr.AddComponentData(entity, new PositionXZ {Value = new float2(transform.position.x, transform.position.z)});
     }
