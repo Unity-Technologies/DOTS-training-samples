70d81dbfde80a9fe3063ad231f8f8ada15428cfc
Torbjorn Laedre <271210+tlaedre@users.noreply.github.com>
2020-09-09 11:22:54 +0200

    Move tile attributes

--- Ported/LabRat/Assets/Scripts/Authoring/TempTileAuthoring.cs
+++ Ported/LabRat/Assets/Scripts/Authoring/TempTileAuthoring.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 class TempTileAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
-    [System.Flags] enum Wall { None, West = 1, North = 2, East = 4, South = 8 }
-    [SerializeField] Wall walls;
+    
+    [SerializeField] Tile.Attributes walls;
 
     public void Convert(Entity entity, EntityManager mgr, GameObjectConversionSystem conversionSystem)
     {
-        mgr.AddComponentData(entity, new Tile {Value = (byte) walls});
+        mgr.AddComponentData(entity, new Tile { Value = walls});
         mgr.AddComponentData(entity, new Size { Value = transform.localScale.x });
         mgr.AddComponentData(entity, new PositionXZ {Value = new float2(transform.position.x, transform.position.z)});
     }
--- Ported/LabRat/Assets/Scripts/Components/Tile.cs
+++ Ported/LabRat/Assets/Scripts/Components/Tile.cs
@@ -2,5 +2,20 @@
 
 struct Tile : IComponentData
 {
-    public byte Value;
+    [System.Flags]
+    public enum Attributes : byte
+    {
+        None,
+        
+        Left = 1,
+        Up = 2,
+        Right = 4,
+        Down = 8,
+        
+        Hole = 16,
+        Goal = 32,
+        Arrow = 64,
+    }
+
+    public Attributes Value;
 }
