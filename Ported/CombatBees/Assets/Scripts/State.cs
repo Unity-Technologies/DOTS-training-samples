cd8db7404cd3f84d91c97412c61cec03ae661d2d
Remi Chapelain <remi.chapelain@unity3d.com>
2020-09-09 10:02:59 +0200

    State and Team Tag scripts

---
+++ Ported/CombatBees/Assets/Scripts/State.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Entities;
+
+public struct Idle : IComponentData
+{
+
+}
+
+public struct Attack : IComponentData
+{
+
+}
+
+public struct Collecting : IComponentData
+{
+
+}
+
+public struct Carrying : IComponentData
+{
+
+}
+
+public struct Dying : IComponentData
+{
+
+}
+
+public struct Wounded : IComponentData
+{
+
+}
+
+
+public struct Spawning : IComponentData
+{
+
+}
---
+++ Ported/CombatBees/Assets/Scripts/Team.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+public struct TeamA : IComponentData
+{
+
+}
+
+public struct TeamB : IComponentData
+{
+
+}
