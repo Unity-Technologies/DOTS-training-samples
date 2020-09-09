ee072be849623eea6008fcef7347e5445298b0b5
Sean Puller <sean.puller@unity3d.com>
2020-09-09 16:03:31 +0200

    Adding gravity and bounds to scene

---
+++ Ported/CombatBees/Assets/Scripts/Systems/GravitySystem.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+// initial resources falling
+// death
+// resources falling
+
+public class GravitySystem : SystemBase
+{
+
+    protected override void OnUpdate()
+    {
+        var deltaTime = Time.DeltaTime;
+        var b = GetSingleton<BattleField>();
+
+        Entities.WithNone<Parent>().ForEach((ref Translation translation, ref Velocity velocity) =>
+                {
+                    velocity.Value.y -= deltaTime * 9.8f;
+                    translation.Value += velocity.Value * deltaTime;
+                    if (translation.Value.y < -b.Bounds.y / 2)
+                    {
+                        velocity.Value.y *= -1;
+                        velocity.Value.y *= 0.3f;
+                    }
+                    translation.Value.y = math.clamp(translation.Value.y, -b.Bounds.y / 2, b.Bounds.y / 2);
+
+                }).Schedule();
+     }
+
+
+}
