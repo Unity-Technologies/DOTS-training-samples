a396098ca91396b3c0941e38296699d13024c89c
Lasse Jon Fuglsang Pedersen <volmer@gmail.com>
2020-09-10 13:10:23 +0200

    cleanup

--- Ported/JobClothDOTS/Assets/Scripts/DOTS/ClothApplyConstraintsSystem.cs
+++ Ported/JobClothDOTS/Assets/Scripts/DOTS/ClothApplyConstraintsSystem.cs
@@ -1,5 +1,4 @@
-﻿//#define AVOID_NESTED_FOREACH
-//#define RUN_ON_MAIN
+﻿//#define USE_CHUNK_ITERATION
 
 using Unity.Entities;
 using Unity.Jobs;
@@ -11,8 +10,42 @@ public class ClothApplyConstraintsSystem : SystemBase
 {
 	protected override void OnUpdate()
 	{
-#if AVOID_NESTED_FOREACH
+#if !USE_CHUNK_ITERATION
 
+		Entities.ForEach((ref ClothMeshToken clothMeshToken, in ClothMesh clothMesh) =>
+		{
+			var vertexPosition = clothMesh.vertexPosition;
+			var vertexInvMass = clothMesh.vertexInvMass;
+
+			clothMeshToken.jobHandle = Entities.WithSharedComponentFilter(clothMesh).ForEach((in ClothEdge edge) =>
+			{
+				int index0 = edge.IndexPair.x;
+				int index1 = edge.IndexPair.y;
+
+				var p0 = vertexPosition[index0];
+				var p1 = vertexPosition[index1];
+				var w0 = vertexInvMass[index0];
+				var w1 = vertexInvMass[index1];
+
+				float3 r = p1 - p0;
+				float rd = math.length(r);
+
+				float delta = 1.0f - edge.Length / rd;
+				float W_inv = delta / (w0 + w1);
+
+				vertexPosition[index0] += r * (w0 * W_inv);
+				vertexPosition[index1] -= r * (w1 * W_inv);
+			}
+			).Schedule(clothMeshToken.jobHandle);
+
+			//TODO: ScheduleParallel
+
+			Dependency = JobHandle.CombineDependencies(Dependency, clothMeshToken.jobHandle);
+		}
+		).WithoutBurst().Run();
+
+#else
+		
 		var entityQuery = GetEntityQuery(
 			ComponentType.ReadWrite<ClothMeshToken>(),
 			ComponentType.ReadOnly<ClothMesh>()
@@ -61,50 +94,9 @@ public class ClothApplyConstraintsSystem : SystemBase
 			).Schedule(clothMeshToken.jobHandle);
 
 			EntityManager.SetComponentData(clothMeshTokenEntity, clothMeshToken);
-		}
-
-#else
-
-		Entities.ForEach((ref ClothMeshToken clothMeshToken, in ClothMesh clothMesh) =>
-		{
-			var vertexPosition = clothMesh.vertexPosition;
-			var vertexInvMass = clothMesh.vertexInvMass;
-
-#if RUN_ON_MAIN
-			clothMeshToken.jobHandle.Complete();
-			Entities.WithSharedComponentFilter(clothMesh).ForEach((in ClothEdge edge) =>
-#else
-			clothMeshToken.jobHandle = Entities.WithSharedComponentFilter(clothMesh).ForEach((in ClothEdge edge) =>
-#endif
-			{
-				int index0 = edge.IndexPair.x;
-				int index1 = edge.IndexPair.y;
-
-				var p0 = vertexPosition[index0];
-				var p1 = vertexPosition[index1];
-				var w0 = vertexInvMass[index0];
-				var w1 = vertexInvMass[index1];
-
-				float3 r = p1 - p0;
-				float rd = math.length(r);
-
-				float delta = 1.0f - edge.Length / rd;
-				float W_inv = delta / (w0 + w1);
-
-				vertexPosition[index0] += r * (w0 * W_inv);
-				vertexPosition[index1] -= r * (w1 * W_inv);
-			}
-#if RUN_ON_MAIN
-			).Run();
-#else
-			).Schedule(clothMeshToken.jobHandle);
-#endif
-
-			//TODO: ScheduleParallel
 
 			Dependency = JobHandle.CombineDependencies(Dependency, clothMeshToken.jobHandle);
 		}
-		).WithoutBurst().Run();
 
 #endif
 	}
