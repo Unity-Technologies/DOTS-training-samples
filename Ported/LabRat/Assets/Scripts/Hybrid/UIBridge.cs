23d07d72a38fd15b1e6343255c1df478db18a5db
Torbjorn Laedre <271210+tlaedre@users.noreply.github.com>
2020-09-10 15:04:48 +0200

    Game controller stuff mk.I

---
+++ Ported/LabRat/Assets/Scripts/Hybrid/UIBridge.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBridge : MonoBehaviour
+{
+#pragma warning disable 0649
+    [SerializeField] float tweenTime = 1f;
+    [SerializeField] float tweenScale = 1.25f;
+    
+    [SerializeField] RectTransform readyTextTransform;
+    [SerializeField] RectTransform setTextTransform;
+    [SerializeField] RectTransform goTextTransform;
+
+    [SerializeField] GameObject gameOverOverlay;
+    [SerializeField] Text gameOverText;
+
+    [SerializeField] Text timerText;
+    [SerializeField] Text[] playerScoreText;
+#pragma warning restore
+
+    System.Collections.IEnumerator TweenElement(RectTransform transform, System.Action onCompleted)
+    {
+        transform.gameObject.SetActive(true);
+
+        for (var elapsed = 0f; elapsed < tweenTime; elapsed += Time.deltaTime)
+        {
+            transform.localScale = Vector3.one * Mathf.Lerp(1f, tweenScale, elapsed / tweenTime);
+            yield return null;
+        }
+        
+        transform.gameObject.SetActive(false);
+        onCompleted?.Invoke();
+    }
+
+    public void ShowReady(System.Action onCompleted = null) => StartCoroutine(TweenElement(readyTextTransform, onCompleted));
+    public void ShowSet(System.Action onCompleted = null) => StartCoroutine(TweenElement(setTextTransform, onCompleted));
+    public void ShowGo(System.Action onCompleted = null) => StartCoroutine(TweenElement(goTextTransform, onCompleted));
+
+    public void SetTimer(float timeRemaining)
+    {
+        timerText.text = System.TimeSpan.FromSeconds(timeRemaining).ToString(@"mm\:ss");
+        timerText.color = timeRemaining < 6f ? UnityEngine.Color.red : UnityEngine.Color.white;
+    }
+
+    public void SetScore(int playerIndex, int score) => playerScoreText[playerIndex].text = score.ToString();
+
+    public void ShowGameOver(string winnerTeam, Color winnerColor)
+    {
+        gameOverOverlay.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        gameOverText.text = $"{winnerTeam} Wins!";
+        gameOverText.color = new UnityEngine.Color(winnerColor.Value.x, winnerColor.Value.y, winnerColor.Value.z, winnerColor.Value.w);
+    }
+
+    public void ResetGUI()
+    {
+        gameOverOverlay.SetActive(false);
+        gameOverText.gameObject.SetActive(false);
+        for(var i = 0; i < playerScoreText.Length; ++i)
+            SetScore(i, 0);
+    }
+}
---
+++ Ported/LabRat/Assets/Scripts/Systems/GameController.cs
@@ -0,0 +1,153 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+struct WantsGameStateTransitions : IComponentData {}
+
+struct GameStateInitialize : IComponentData {}
+struct GameStateStart : IComponentData {}
+struct GameStateRunning : IComponentData {}
+struct GameStateEnd : IComponentData {}
+
+public class GameController : SystemBase
+{
+    const float GameDuration = 10;
+    const float GameRestartDelay = 10;
+
+    enum GameState { None, ApplicationStarting, GameInitializing, GameStarting, GameStarted, GameRunning, GameEnding, GameRestarting }
+
+    EntityCommandBufferSystem m_EntityCommandBufferSystem;
+    Entity m_GameControllerEntity;
+    
+    UIBridge m_UIBridge;
+    
+    GameState m_GameState;
+    float m_TimeAccumulator;
+
+    protected override void OnCreate()
+    {
+        // We'll sync our command buffers on the beginning of next frame's init group
+        m_EntityCommandBufferSystem = World.GetExistingSystem<BeginInitializationEntityCommandBufferSystem>();
+
+        // Create an entity so we can hook ourselves to game states
+        m_GameControllerEntity = EntityManager.CreateEntity(typeof(WantsGameStateTransitions));
+        EntityManager.SetName(m_GameControllerEntity, "GameController");
+
+        // Bind to hybrid UI
+        m_UIBridge = UnityEngine.Object.FindObjectOfType<UIBridge>();
+
+        // Set the starting state
+        m_GameState = GameState.ApplicationStarting;
+    }
+
+    protected override void OnUpdate()
+    {
+        switch (m_GameState)
+        {
+            case GameState.ApplicationStarting:
+            {
+                var ecb = m_EntityCommandBufferSystem.CreateCommandBuffer();
+                Entities.WithName("EnterState_GameInit")
+                    .WithAll<WantsGameStateTransitions>()
+                    .ForEach((Entity entity) => ecb.AddComponent<GameStateInitialize>(entity))
+                    .Schedule();
+                m_EntityCommandBufferSystem.AddJobHandleForProducer(Dependency);
+
+                m_GameState = GameState.GameInitializing;
+                break;
+            }
+
+            case GameState.GameInitializing:
+            {
+                var ecb = m_EntityCommandBufferSystem.CreateCommandBuffer();
+                Entities.WithName("LeaveState_GameInit")
+                    .WithAll<WantsGameStateTransitions, GameStateInitialize>()
+                    .ForEach((Entity entity) => ecb.RemoveComponent<GameStateInitialize>(entity))
+                    .Schedule();
+                m_EntityCommandBufferSystem.AddJobHandleForProducer(Dependency);
+
+                m_TimeAccumulator = 0f;
+                
+                m_UIBridge.ShowReady(() =>
+                {
+                    m_UIBridge.ShowSet(() =>
+                    {
+                        m_UIBridge.ShowGo();
+
+                        m_GameState = GameState.GameStarting;
+                    });
+                });
+                
+                m_GameState = GameState.None;
+                break;
+            }
+
+            case GameState.GameStarting:
+            {
+                var ecb = m_EntityCommandBufferSystem.CreateCommandBuffer();
+                Entities.WithName("EnterState_GameStart")
+                    .WithAll<WantsGameStateTransitions>()
+                    .ForEach((Entity entity) => ecb.AddComponent<GameStateStart>(entity))
+                    .Schedule();
+                m_EntityCommandBufferSystem.AddJobHandleForProducer(Dependency);
+                
+                m_GameState = GameState.GameStarted;
+                break;
+            }
+
+            case GameState.GameStarted:
+            {
+                var ecb = m_EntityCommandBufferSystem.CreateCommandBuffer();
+                Entities.WithName("LeaveState_GameStart_EnterState_GameRunning")
+                    .WithAll<WantsGameStateTransitions>()
+                    .ForEach((Entity entity) =>
+                    {
+                        ecb.RemoveComponent<GameStateStart>(entity);
+                        ecb.AddComponent<GameStateRunning> (entity);
+                    })
+                    .Schedule();
+                m_EntityCommandBufferSystem.AddJobHandleForProducer(Dependency);
+                
+                m_GameState = GameState.GameStarted;
+                goto case GameState.GameRunning;
+            }
+
+            case GameState.GameRunning:
+            {
+                m_TimeAccumulator += Time.DeltaTime;
+                var gameTimeRemaining = math.max(0f, GameDuration - m_TimeAccumulator);
+        
+                m_UIBridge.SetTimer(gameTimeRemaining);
+                
+                for (int i = 0; i < 4; i++)
+                    m_UIBridge.SetScore(i, UnityEngine.Random.Range(1, 100));
+
+                if (gameTimeRemaining == 0f)
+                    m_GameState = GameState.GameEnding;
+                
+                break;
+            }
+
+            case GameState.GameEnding:
+            {
+                m_UIBridge.ShowGameOver("some winner", new Color { Value = new float4(1f, 1f, 0f, 1f)});
+                m_TimeAccumulator = 0f;
+                
+                m_GameState = GameState.GameRestarting;
+                break;
+            }
+            
+            case GameState.GameRestarting:
+            {
+                m_TimeAccumulator += Time.DeltaTime;
+
+                if (m_TimeAccumulator >= GameRestartDelay)
+                {
+                    m_UIBridge.ResetGUI();
+                    m_GameState = GameState.GameInitializing;
+                }
+                
+                break;
+            }
+        }
+    }
+}
