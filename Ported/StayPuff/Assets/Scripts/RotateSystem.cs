c3f77d62890c0f2cd97de93e711bf5678e8fa8bd
Ben Laidlaw <58746245+LaidlawFXatUnity@users.noreply.github.com>
2020-09-14 10:05:34 -0700

    Tutorial cloned to Ported Project
    Clonging the tutorial project to the Ported project for tabla rosa to begin conversion.

---
+++ Ported/StayPuff/Assets/Scripts/RotateSystem.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class RotateSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var deltaTime = Time.DeltaTime;
+        Entities.ForEach((ref Rotation rotation, in Rotate rotate) =>
+        {
+            var delta = quaternion.RotateY(deltaTime * rotate.RotationSpeed);
+            rotation.Value = math.normalize(math.mul(rotation.Value, delta));
+        }).ScheduleParallel();
+    }
+}
---
+++ Ported/StayPuff/Assets/Scripts/SpawnerAuthoring.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct Spawner : IComponentData
+{
+    public Entity Prefab;
+    public int CountX;
+    public int CountZ;
+}
