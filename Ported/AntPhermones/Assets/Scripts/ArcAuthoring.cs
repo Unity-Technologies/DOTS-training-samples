1563afb095a35a3d2e466bf129320d5d7fc6fb4f
George Ing <george.ing@unity3d.com>
2020-09-15 16:20:32 -0400

    Arc Authoring.

---
+++ Ported/AntPhermones/Assets/Scripts/ArcAuthoring.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public class Arc : IComponentData
+{
+    public float Radius;
+    public float StartAngle;
+    public float EndAngle;
+}
