91163135f85daea88a9da41acaaa9d9a6a779f9a
Jeremy Cowles <jeremy.cowles@gmail.com>
2020-09-15 15:00:17 -0700

    PheromoneMap: Initial setup

---
+++ Ported/AntPhermones/Assets/Scripts/PheromonePlacementSystem.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class PheromonePlacementSystem : SystemBase {
+
+    protected override void OnUpdate() {
+        var mapEntity = GetSingletonEntity<PheromoneMap>();
+        var map = EntityManager.GetComponentData<PheromoneMap>(mapEntity);
+        var pheromones = EntityManager.GetBuffer<PheromoneStrength>(mapEntity);
+
+        Entities.WithAll<AntTag>().ForEach((in Translation translation) => {
+            int2 gridPos = PheromoneMap.WorldToGridPos(map, translation.Value);
+            int index = PheromoneMap.GridPosToIndex(map, gridPos);
+            pheromones[index] += map.AntPheremoneStrength;
+        }).Run();
+    }
+}
---
+++ Ported/AntPhermones/Assets/Scripts/PheromoneVisualizationSystem.cs
@@ -0,0 +1,6 @@
+﻿using Unity.Entities;
+
+public class PheromoneVisualizationSystem : SystemBase {
+    protected override void OnUpdate() {
+    }
+}
