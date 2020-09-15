b8a014ab71bc178945f17a96fc296c6dc09621aa
Jeremy Cowles <jeremy.cowles@gmail.com>
2020-09-15 13:26:51 -0700

    Spawner: fix class vs. struct

--- Ported/AntPhermones/Assets/Scripts/SpawnerAuthoring.cs
+++ Ported/AntPhermones/Assets/Scripts/SpawnerAuthoring.cs
@@ -1,8 +1,7 @@
 ﻿using Unity.Entities;
-using UnityEngine;
 
 [GenerateAuthoringComponent]
-public class Spawner : IComponentData
+public struct Spawner : IComponentData
 {
     public Entity Ant;
     public int NumberOfAnts;
