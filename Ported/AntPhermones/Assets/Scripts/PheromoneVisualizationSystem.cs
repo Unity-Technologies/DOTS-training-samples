7c1b7b955db89716e9a31e72bea3e593eeb03db6
Jeremy Cowles <jeremy.cowles@gmail.com>
2020-09-15 16:07:41 -0700

    Vis: First crack at visualization

--- Ported/AntPhermones/Assets/Scripts/PheromoneVisualizationSystem.cs
+++ Ported/AntPhermones/Assets/Scripts/PheromoneVisualizationSystem.cs
@@ -2,5 +2,12 @@
 
 public class PheromoneVisualizationSystem : SystemBase {
     protected override void OnUpdate() {
+        var pv = UnityEngine.Component.FindObjectOfType<PheremoneVisualization>();
+
+        var mapEntity = GetSingletonEntity<PheromoneMap>();
+        var map = EntityManager.GetComponentData<PheromoneMap>(mapEntity);
+        var pheromones = EntityManager.GetBuffer<PheromoneStrength>(mapEntity);
+
+        pv.SetMap(map, pheromones);
     }
 }
