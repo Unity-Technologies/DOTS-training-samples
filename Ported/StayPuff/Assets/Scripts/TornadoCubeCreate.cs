996ef8d554b3a8724a86ba7b6e36859847612fb1
Ben Laidlaw <58746245+LaidlawFXatUnity@users.noreply.github.com>
2020-09-15 14:50:23 -0700

    Tornado Cubes
    Defaults not set for Tornado Cubes 45,45,45, -45,.5,-45 and prefab set to cube

---
+++ Ported/StayPuff/Assets/Scripts/TornadoCubeCreate.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class TornadoCubeCreate : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entities.WithStructuralChanges()
+            .ForEach((Entity entity, in TornadoCubeData cube, in LocalToWorld ltw) =>
+            {
+                for (int x = 0; x < cube.CountX; ++x)
+                    for (int z = 0; z < cube.CountZ; ++z)
+                    {
+                        var posX = UnityEngine.Random.Range(cube.BoundsMin.x, cube.BoundsMax.x);
+                        var posY = UnityEngine.Random.Range(cube.BoundsMin.y, cube.BoundsMax.y);
+                        var posZ = UnityEngine.Random.Range(cube.BoundsMin.z, cube.BoundsMax.z);
+                        var instance = EntityManager.Instantiate(cube.Prefab);
+                        SetComponent(instance, new Translation
+                        {
+                            Value = ltw.Position + new float3(posX, posY, posZ)
+                        });
+                    }
+
+                EntityManager.DestroyEntity(entity);
+            }).Run();
+    }
+}
---
+++ Ported/StayPuff/Assets/Scripts/TornadoCubeData.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public struct TornadoCubeData : IComponentData
+{
+    public Entity Prefab;
+    public int CountX;
+    public int CountZ;
+    public Vector3 BoundsMax;
+    public Vector3 BoundsMin;
+}
