bba15d454795c4a611ebf971ac8fdaf65ecdcbe6
Jack Potter <jack.potter@ubisoft.com>
2020-09-15 15:24:24 -0400

    Fix math in movement system

--- Ported/AntPhermones/Assets/Scripts/MovementSystem.cs
+++ Ported/AntPhermones/Assets/Scripts/MovementSystem.cs
@@ -11,10 +11,11 @@ public class MovementSystem : SystemBase
     protected override void OnUpdate()
     {
         float deltaTime = Time.DeltaTime;
+        float moveSpeed = 1.0f; // temp
         Entities.WithAll<AntTag>().ForEach((ref Translation translation, in Rotation rotation) =>
         {
             float3 forward = new float3(0.0f, 0.0f, 1.0f);
-            translation.Value += forward * math.mul(rotation.Value, forward) * deltaTime;
+            translation.Value += math.mul(rotation.Value, forward) * deltaTime * moveSpeed;
         }).Run();
     }
 }
