eaaf98b0700a91bdee2898642f3c0e649c139cc5
George Ing <george.ing@unity3d.com>
2020-09-16 18:50:04 -0400

    Teleporting fix.

--- Ported/AntPhermones/Assets/Scripts/AntTagAuthoring.cs
+++ Ported/AntPhermones/Assets/Scripts/AntTagAuthoring.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 [GenerateAuthoringComponent]
 public struct AntTag : IComponentData
 {
-    public const float Size = 1.5f;
+    public const float Size = 0.5f;
     public bool HasFood;
 }
