a31186c6874a66a86851f1adf07e8232655eda16
Jeremy Cowles <jeremy.cowles@gmail.com>
2020-09-16 15:04:11 -0700

    GUI: Overlay first commit

---
+++ Ported/AntPhermones/Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class KeyboardInput : MonoBehaviour {
+	Text text;
+
+	static bool showText=true;
+
+	void Start () {
+		text = GetComponent<Text>();
+		text.enabled = showText;
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.H)) {
+			showText = !showText;
+			text.enabled = showText;
+		}
+		if (Input.GetKeyDown(KeyCode.R)) {
+			Time.timeScale = 1f;
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+}
