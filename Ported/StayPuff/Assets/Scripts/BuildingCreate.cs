3ca9ac555a6d95a5345068fd3098a3cad02da2b4
Ben Laidlaw <58746245+LaidlawFXatUnity@users.noreply.github.com>
2020-09-16 09:28:05 -0700

    Joint Scripts from Physic Scample

---
+++ Ported/StayPuff/Assets/Scripts/BuildingCreate.cs
@@ -0,0 +1,64 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class BuildingCreate : SystemBase
+{
+    protected override void OnUpdate()
+    {
+      
+        
+        
+        Entities.ForEach((ref Translation translation, in Rotation rotation) => {
+
+        }).Schedule();
+    }
+}
+//for (int i = 0; i < 35; i++)
+//{
+//	int height = Random.Range(4, 12);
+//	Vector3 pos = new Vector3(Random.Range(-45f, 45f), 0f, Random.Range(-45f, 45f));
+//	float spacing = 2f;
+//	for (int j = 0; j < height; j++)
+//	{
+//		Point point = new Point();
+//		point.x = pos.x + spacing;
+//		point.y = j * spacing;
+//		point.z = pos.z - spacing;
+//		point.oldX = point.x;
+//		point.oldY = point.y;
+//		point.oldZ = point.z;
+//		if (j == 0)
+//		{
+//			point.anchor = true;
+//		}
+//		pointsList.Add(point);
+//		point = new Point();
+//		point.x = pos.x - spacing;
+//		point.y = j * spacing;
+//		point.z = pos.z - spacing;
+//		point.oldX = point.x;
+//		point.oldY = point.y;
+//		point.oldZ = point.z;
+//		if (j == 0)
+//		{
+//			point.anchor = true;
+//		}
+//		pointsList.Add(point);
+//		point = new Point();
+//		point.x = pos.x + 0f;
+//		point.y = j * spacing;
+//		point.z = pos.z + spacing;
+//		point.oldX = point.x;
+//		point.oldY = point.y;
+//		point.oldZ = point.z;
+//		if (j == 0)
+//		{
+//			point.anchor = true;
+//		}
+//		pointsList.Add(point);
+//	}
+//}
---
+++ Ported/StayPuff/Assets/Scripts/CityCreate.cs
@@ -0,0 +1,73 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class CityCreate : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        
+        
+        Entities.ForEach((ref Translation translation, in Rotation rotation) => {
+
+        }).Schedule();
+    }
+}
+
+//// ground details
+//for (int i = 0; i < 600; i++)
+//{
+//	Vector3 pos = new Vector3(Random.Range(-55f, 55f), 0f, Random.Range(-55f, 55f));
+//	Point point = new Point();
+//	point.x = pos.x + Random.Range(-.2f, -.1f);
+//	point.y = pos.y + Random.Range(0f, 3f);
+//	point.z = pos.z + Random.Range(.1f, .2f);
+//	point.oldX = point.x;
+//	point.oldY = point.y;
+//	point.oldZ = point.z;
+//	pointsList.Add(point);
+
+//	point = new Point();
+//	point.x = pos.x + Random.Range(.2f, .1f);
+//	point.y = pos.y + Random.Range(0f, .2f);
+//	point.z = pos.z + Random.Range(-.1f, -.2f);
+//	point.oldX = point.x;
+//	point.oldY = point.y;
+//	point.oldZ = point.z;
+//	if (Random.value < .1f)
+//	{
+//		point.anchor = true;
+//	}
+//	pointsList.Add(point);
+//}
+
+//int batch = 0;
+
+//for (int i = 0; i < pointsList.Count; i++)
+//{
+//	for (int j = i + 1; j < pointsList.Count; j++)
+//	{
+//		Bar bar = new Bar();
+//		bar.AssignPoints(pointsList[i], pointsList[j]);
+//		if (bar.length < 5f && bar.length > .2f)
+//		{
+//			bar.point1.neighborCount++;
+//			bar.point2.neighborCount++;
+
+//			barsList.Add(bar);
+//			matricesList[batch].Add(bar.matrix);
+//			if (matricesList[batch].Count == instancesPerBatch)
+//			{
+//				batch++;
+//				matricesList.Add(new List<Matrix4x4>());
+//			}
+//			if (barsList.Count % 500 == 0)
+//			{
+//				yield return null;
+//			}
+//		}
+//	}
+//}
