161e4e47849d877a5874019cf9f55ede8f6838a9
Audrey Paiement <audreyp@unity3d.com>
2020-09-17 15:19:35 -0400

    Arrow placement preview

--- Ported/LabRat/Assets/Scripts/Systems/PlayerInputSystem.cs
+++ Ported/LabRat/Assets/Scripts/Systems/PlayerInputSystem.cs
@@ -8,6 +8,7 @@ public class PlayerInputSystem : SystemBase
 {
     Plane plane;
     Camera camera;
+    int2 lastPos;
 
     protected override void OnCreate()
     {
@@ -21,76 +22,108 @@ public class PlayerInputSystem : SystemBase
         if (camera == null)
             camera = Camera.main;
 
-        if (Input.GetMouseButtonDown(0))
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        float enter;
+
+        if (!plane.Raycast(ray, out enter))
+            return;
+
+        var worldPos = ray.GetPoint(enter);
+
+        int2 pos = new int2(Mathf.RoundToInt(worldPos.x), Mathf.RoundToInt(worldPos.z));
+
+        var boardSize = GetSingleton<BoardSize>();
+
+        var cellDataEntity = GetSingletonEntity<CellData>();
+        var cellData = EntityManager.GetComponentObject<CellData>(cellDataEntity);
+
+        if (pos.x >= 0 && pos.x < boardSize.Value.x && pos.y >= 0 && pos.y < boardSize.Value.y)
         {
-            var ray = camera.ScreenPointToRay(Input.mousePosition);
-
-            float enter;
-
-            if (!plane.Raycast(ray, out enter))
-			    return;
-
-            var worldPos = ray.GetPoint(enter);
-
-            int2 pos = new int2(Mathf.RoundToInt(worldPos.x), Mathf.RoundToInt(worldPos.z));
-
-            var boardSize = GetSingleton<BoardSize>();
-
-            if (pos.x >= 0 && pos.x < boardSize.Value.x && pos.y >= 0 && pos.y < boardSize.Value.y)
+            if (pos.x != lastPos.x || pos.y != lastPos.y)
             {
-                var cellDataEntity = GetSingletonEntity<CellData>();
-                var cellData = EntityManager.GetComponentObject<CellData>(cellDataEntity);
+                var lastCellEntity = cellData.cells[lastPos.y * boardSize.Value.x + lastPos.x];
 
-                var cellEntity = cellData.cells[pos.y * boardSize.Value.x + pos.x];
-
-                if (!EntityManager.HasComponent<DisableRendering>(cellEntity))
+                if (!EntityManager.HasComponent<ArrowTag>(lastCellEntity))
                 {
-                    var cellLinks = EntityManager.GetComponentData<CellComponentLink>(cellEntity);
+                    var lastCellLinks = EntityManager.GetComponentData<CellComponentLink>(lastCellEntity);
+                    EntityManager.SetComponentData(lastCellLinks.arrow, new Color { Value = float4.zero });
+                    EntityManager.SetComponentData(lastCellLinks.arrowOutline, new Color { Value = float4.zero });
+                }
+            }
 
-                    if (EntityManager.HasComponent<ArrowTag>(cellEntity))
+            var cellEntity = cellData.cells[pos.y * boardSize.Value.x + pos.x];
+
+            if (!EntityManager.HasComponent<DisableRendering>(cellEntity))
+            {
+                var cellLinks = EntityManager.GetComponentData<CellComponentLink>(cellEntity);
+
+                if (EntityManager.HasComponent<ArrowTag>(cellEntity))
+                {
+                    if (Input.GetMouseButtonDown(0))
                     {
                         EntityManager.RemoveComponent<ArrowTag>(cellEntity);
                         EntityManager.SetComponentData(cellLinks.arrow, new Color { Value = float4.zero });
                         EntityManager.SetComponentData(cellLinks.arrowOutline, new Color { Value = float4.zero });
                     }
-                    else
+                }
+                else
+                {
+                    float2 cellOffset = new float2(worldPos.x - pos.x, worldPos.z - pos.y);
+
+                    DirectionEnum arrowDirection = math.abs(cellOffset.y) > math.abs(cellOffset.x)
+                        ? (cellOffset.y > 0 ? DirectionEnum.North : DirectionEnum.South)
+                        : (cellOffset.x > 0 ? DirectionEnum.East : DirectionEnum.West);
+
+                    if (Input.GetMouseButtonDown(0))
                     {
-                        float2 cellOffset = new float2(worldPos.x - pos.x, worldPos.z - pos.y);
-
-                        DirectionEnum arrowDirection = math.abs(cellOffset.y) > math.abs(cellOffset.x)
-                            ? (cellOffset.y > 0 ? DirectionEnum.North : DirectionEnum.South)
-                            : (cellOffset.x > 0 ? DirectionEnum.East : DirectionEnum.West);
-
                         EntityManager.AddComponent<ArrowTag>(cellEntity);
                         EntityManager.SetComponentData(cellEntity, new Direction { Value = arrowDirection });
-
                         EntityManager.SetComponentData(cellLinks.arrow, new Color { Value = new float4(1, 1, 1, 1) });
                         EntityManager.SetComponentData(cellLinks.arrowOutline, new Color { Value = new float4(0, 0, 0, 1) });
-
-                        Quaternion rot;
-
-                        switch (arrowDirection)
-                        {
-                            case DirectionEnum.North:
-                                rot = quaternion.EulerXYZ(math.PI / 2f, 0, 0);
-                                break;
-                            case DirectionEnum.South:
-                                rot = quaternion.EulerXYZ(math.PI / 2f, math.PI, 0);
-                                break;
-                            case DirectionEnum.East:
-                                rot = quaternion.EulerXYZ(math.PI / 2f, math.PI / 2f, 0);
-                                break;
-                            case DirectionEnum.West:
-                                rot = quaternion.EulerXYZ(math.PI / 2f, -math.PI / 2f, 0);
-                                break;
-                            default:
-                                rot = quaternion.EulerXYZ(math.PI / 2f, 0, 0);
-                                break;
-                        }
-
-                        EntityManager.SetComponentData(cellLinks.arrow, new Rotation { Value = rot });
                     }
+                    else
+                    {
+                        EntityManager.SetComponentData(cellLinks.arrow, new Color { Value = new float4(0.5f, 0.5f, 0.5f, 1) });
+                        EntityManager.SetComponentData(cellLinks.arrowOutline, new Color { Value = new float4(0, 0, 0, 0) });
+                    }
+
+                    Quaternion rot;
+
+                    switch (arrowDirection)
+                    {
+                        case DirectionEnum.North:
+                            rot = quaternion.EulerXYZ(math.PI / 2f, 0, 0);
+                            break;
+                        case DirectionEnum.South:
+                            rot = quaternion.EulerXYZ(math.PI / 2f, math.PI, 0);
+                            break;
+                        case DirectionEnum.East:
+                            rot = quaternion.EulerXYZ(math.PI / 2f, math.PI / 2f, 0);
+                            break;
+                        case DirectionEnum.West:
+                            rot = quaternion.EulerXYZ(math.PI / 2f, -math.PI / 2f, 0);
+                            break;
+                        default:
+                            rot = quaternion.EulerXYZ(math.PI / 2f, 0, 0);
+                            break;
+                    }
+
+                    EntityManager.SetComponentData(cellLinks.arrow, new Rotation { Value = rot });
                 }
+            }
+
+            lastPos = pos;
+        }
+        else if (lastPos.x >= 0 && lastPos.x < boardSize.Value.x && lastPos.y >= 0 && lastPos.y < boardSize.Value.y)
+        {
+            var lastCellEntity = cellData.cells[lastPos.y * boardSize.Value.x + lastPos.x];
+
+            if (!EntityManager.HasComponent<ArrowTag>(lastCellEntity))
+            {
+                var lastCellLinks = EntityManager.GetComponentData<CellComponentLink>(lastCellEntity);
+                EntityManager.SetComponentData(lastCellLinks.arrow, new Color { Value = float4.zero });
+                EntityManager.SetComponentData(lastCellLinks.arrowOutline, new Color { Value = float4.zero });
             }
         }
     }
