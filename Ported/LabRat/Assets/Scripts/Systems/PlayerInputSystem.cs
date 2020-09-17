4f280c11c203052f624935ddd16b85507961d39d
Audrey Paiement <audreyp@unity3d.com>
2020-09-17 13:45:01 -0400

    Placed arrows now visually show up on the board

--- Ported/LabRat/Assets/Scripts/Systems/PlayerInputSystem.cs
+++ Ported/LabRat/Assets/Scripts/Systems/PlayerInputSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Rendering;
 using Unity.Transforms;
 using UnityEngine;
 
@@ -35,27 +36,60 @@ public class PlayerInputSystem : SystemBase
 
             var boardSize = GetSingleton<BoardSize>();
 
-            if (pos.x > 0 && pos.x < boardSize.Value.x && pos.y > 0 && pos.y < boardSize.Value.y)
+            if (pos.x >= 0 && pos.x < boardSize.Value.x && pos.y >= 0 && pos.y < boardSize.Value.y)
             {
                 var cellDataEntity = GetSingletonEntity<CellData>();
                 var cellData = EntityManager.GetComponentObject<CellData>(cellDataEntity);
 
                 var cellEntity = cellData.cells[pos.y * boardSize.Value.x + pos.x];
 
-                if (EntityManager.HasComponent<ArrowTag>(cellEntity))
+                if (!EntityManager.HasComponent<DisableRendering>(cellEntity))
                 {
-                    EntityManager.RemoveComponent<ArrowTag>(cellEntity);
-                }
-                else
-                {
-                    float2 cellOffset = new float2(worldPos.x - pos.x, worldPos.z - pos.y);
+                    var cellLinks = EntityManager.GetComponentData<CellComponentLink>(cellEntity);
 
-                    DirectionEnum arrowDirection = math.abs(cellOffset.y) > math.abs(cellOffset.x)
-                        ? (cellOffset.y > 0 ? DirectionEnum.North : DirectionEnum.South)
-                        : (cellOffset.x > 0 ? DirectionEnum.East : DirectionEnum.West);
+                    if (EntityManager.HasComponent<ArrowTag>(cellEntity))
+                    {
+                        EntityManager.RemoveComponent<ArrowTag>(cellEntity);
+                        EntityManager.SetComponentData(cellLinks.arrow, new Color { Value = float4.zero });
+                        EntityManager.SetComponentData(cellLinks.arrowOutline, new Color { Value = float4.zero });
+                    }
+                    else
+                    {
+                        float2 cellOffset = new float2(worldPos.x - pos.x, worldPos.z - pos.y);
 
-                    EntityManager.AddComponent<ArrowTag>(cellEntity);
-                    EntityManager.SetComponentData(cellEntity, new Direction { Value = arrowDirection });
+                        DirectionEnum arrowDirection = math.abs(cellOffset.y) > math.abs(cellOffset.x)
+                            ? (cellOffset.y > 0 ? DirectionEnum.North : DirectionEnum.South)
+                            : (cellOffset.x > 0 ? DirectionEnum.East : DirectionEnum.West);
+
+                        EntityManager.AddComponent<ArrowTag>(cellEntity);
+                        EntityManager.SetComponentData(cellEntity, new Direction { Value = arrowDirection });
+
+                        EntityManager.SetComponentData(cellLinks.arrow, new Color { Value = new float4(1, 1, 1, 1) });
+                        EntityManager.SetComponentData(cellLinks.arrowOutline, new Color { Value = new float4(0, 0, 0, 1) });
+
+                        Quaternion rot;
+
+                        switch (arrowDirection)
+                        {
+                            case DirectionEnum.North:
+                                rot = quaternion.EulerXYZ(math.PI / 2f, 0, 0);
+                                break;
+                            case DirectionEnum.South:
+                                rot = quaternion.EulerXYZ(math.PI / 2f, math.PI, 0);
+                                break;
+                            case DirectionEnum.East:
+                                rot = quaternion.EulerXYZ(math.PI / 2f, math.PI / 2f, 0);
+                                break;
+                            case DirectionEnum.West:
+                                rot = quaternion.EulerXYZ(math.PI / 2f, -math.PI / 2f, 0);
+                                break;
+                            default:
+                                rot = quaternion.EulerXYZ(math.PI / 2f, 0, 0);
+                                break;
+                        }
+
+                        EntityManager.SetComponentData(cellLinks.arrow, new Rotation { Value = rot });
+                    }
                 }
             }
         }
