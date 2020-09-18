83ec564025d04cbfe1be32191fe4fea12ae8a12c
Audrey Paiement <audreyp@unity3d.com>
2020-09-18 11:43:31 -0400

    Added camera controls

---
+++ Ported/LabRat/Assets/CameraMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Camera camera;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.Self);
+        }
+
+        camera.orthographicSize -= Input.mouseScrollDelta.y;
+        if (camera.orthographicSize < 0f)
+            camera.orthographicSize = 0f;
+    }
+}
