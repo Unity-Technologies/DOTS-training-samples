c8736efea3bdb1847c9df940f4462cb35abfc777
Adam Dickin <adam.dickin@unity3d.com>
2020-10-05 16:40:58 -0600

    checking in dots sample setup

---
+++ Ported/Assets/Scripts/RotateAuthoring.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public struct Rotate : IComponentData
+{
+    public float RotationSpeed;
+}
+
+public class RotateAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public float RotationSpeedDegrees;
+
+    public void Convert(Entity entity, EntityManager dstManager,
+        GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new Rotate
+        {
+            RotationSpeed = math.radians(RotationSpeedDegrees)
+        });
+    }
+}
