77e93905d267329aa9f51301740d2d51ba19da0f
Maxime Plantady <maxime.plantady@unity3d.com>
2020-10-06 12:02:50 -0400

    Cell Spawner and fix rendering

---
+++ Ported/BucketBrigade/Assets/Scripts/BoardSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public struct BoardSpawner : IComponentData
+{
+	public Entity Prefab;
+	public int SizeX;
+	public int SizeZ;
+	public float RandomYOffset;
+}
---
+++ Ported/BucketBrigade/Assets/Scripts/BoardSpawnerSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardSpawnerSystem : SystemBase
+{
+	protected override void OnUpdate()
+	{
+		Entities.WithStructuralChanges()
+			.ForEach((Entity entity, in BoardSpawner spawner, in LocalToWorld world) =>
+		{
+			for (int x = 0; x < spawner.SizeX; ++x)
+			{
+				for (int z = 0; z < spawner.SizeZ; ++z)
+				{
+					float offsetY = Random.Range(0, spawner.RandomYOffset);
+					
+					var instance = EntityManager.Instantiate(spawner.Prefab);
+					SetComponent(instance, new Translation
+					{
+						Value = world.Position + new float3(x, offsetY, z)
+					});
+				}
+			}
+			
+			EntityManager.DestroyEntity(entity);
+		}).Run();
+	}
+}
