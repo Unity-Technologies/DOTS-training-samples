b57673327a2f5b8be0d68c84872989112498fb8b
Lucille Caillaud <lucille.caillaud@unity3d.com>
2020-10-06 17:56:07 -0400

    BoardInitialization: place bases, holes and walls (naive approach)

--- Ported/LabRat/Assets/Scripts/BoardAuthoring.cs
+++ Ported/LabRat/Assets/Scripts/BoardAuthoring.cs
@@ -1,12 +1,16 @@
 ﻿using Unity.Entities;
+using UnityEngine;
 
 [GenerateAuthoringComponent]
 public struct Board : IComponentData
 {
     public Entity tilePrefab;
     public Entity wallPrefab;
-    public int size;
+    public Entity homeBasePrefab;
+
+    [Min(2)] public int size;
     public float yNoise;
+
     public int wallCount;
     public int holeCount;
 }
