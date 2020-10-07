8123b540fdb1357cdd856c5946b2f24fe0e68fb9
Gustavo Castro <gustavo@unity3d.com>
2020-10-07 09:27:15 -0700

    Linked-List style Road Nodes Spawn points are starting Nodes

--- Ported/StackInterchange/Assets/Scripts/Components/RoadNode.cs
+++ Ported/StackInterchange/Assets/Scripts/Components/RoadNode.cs
@@ -4,6 +4,9 @@ using Unity.Transforms;
 [GenerateAuthoringComponent]
 public struct RoadNode : IComponentData
 {
+     public uint index;
      public Entity nodePosition;
      public int colorBits;
+     
+     public Entity nextNode;
 }
