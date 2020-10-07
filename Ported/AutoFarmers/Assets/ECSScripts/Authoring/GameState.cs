5040a73f8de387159c84cbfcbbbc264a11dc951e
Andrew Theisen <andrew.theisen@unity3d.com>
2020-10-07 13:33:03 +0200

    -missed merged changes

--- Ported/AutoFarmers/Assets/ECSScripts/Authoring/GameState.cs
+++ Ported/AutoFarmers/Assets/ECSScripts/Authoring/GameState.cs
@@ -9,10 +9,6 @@ public struct GameState : IComponentData
     public Entity WaterPrefab;
     public Entity PlantPrefab;
     public float WaterProbability;
-<<<<<<< HEAD
     public float SimulationSpeed;
-=======
-    
     public Entity FarmerPrefab;
->>>>>>> ca02060284fc3a0fbb1fea2da6b34dfc55d99388
 }
