b6a42b93437e5f4b5a38607d23b6b13baf182c43
Sean Robertshaw <sean.robertshaw@unity3d.com>
2020-10-07 11:09:05 -0600

    Road Traverse: First cut

---
+++ Ported/StackInterchange/Assets/Scripts/BlobData/ColorMask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Unity.Mathematics;
+
+namespace Assets.Scripts.BlobData
+{
+    class ColorMask
+    {
+        [Flags]
+        public enum Masks
+        {
+            None = 0,
+            ColorA = 1,
+            ColorB = 2,
+            ColorC = 3,
+            ColorD = 4, 
+            ColorE = 5, 
+            ColorF = 6, 
+        }
+        
+        public static (float4, Masks) ColorA = (new float4(0, 0, 0, 1), Masks.ColorA);
+        public static (float4, Masks) ColorB = (new float4(1, 1, 1, 1), Masks.ColorB);
+        public static (float4, Masks) ColorC = (new float4(0, 0, 1, 1), Masks.ColorC); 
+        public static (float4, Masks) ColorD = (new float4(0, 1, 0, 1), Masks.ColorD);
+        public static (float4, Masks) ColorE = (new float4(1, 0, 0, 1), Masks.ColorE);
+        public static (float4, Masks) ColorF = (new float4(1, 0, 1, 1), Masks.ColorF);
+
+        private static readonly (float4, Masks)[] _allMasks = { ColorA, ColorB, ColorC, ColorD, ColorE, ColorF };
+
+        public static Masks GetMask(float4 color)
+        {
+            foreach((float4, Masks) mask in _allMasks)
+            {
+                if (mask.Item1.Equals(color))
+                    return mask.Item2;
+            }
+            return Masks.None;
+        }
+    }
+}
---
+++ Ported/StackInterchange/Assets/Scripts/BlobData/Node.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace Assets.Scripts.BlobData
+{
+    class Node
+    {
+        public Translation translation;
+        public float4 Color;
+        public ColorMask.Masks PathBitField;
+
+        public List<Node> Childern = new List<Node>();
+        public long index;
+
+        public Node GetNext(float4 carColor)
+        {
+            ColorMask.Masks mask = ColorMask.GetMask(carColor);
+
+            Node next = null;
+            foreach(var child in Childern)
+            {
+                // Default stay on the same road.
+                if (next == null && child.Color.Equals(Color))
+                    next = child;
+
+                if ((child.PathBitField & mask) != ColorMask.Masks.None)
+                    next = child;
+            }
+            return next;
+        }
+    }
+}
