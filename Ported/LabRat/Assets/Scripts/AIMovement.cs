b3c007dc6f77ac1a24f98fcb2c8b7e32a20f1fbc
Bastien Humeau <bastienh@unity3d.com>
2020-10-07 14:01:26 -0400

    Diminished wall quantity + added pause timer on AIMovement

--- Ported/LabRat/Assets/Scripts/AIMovement.cs
+++ Ported/LabRat/Assets/Scripts/AIMovement.cs
@@ -3,14 +3,29 @@ using Unity.Mathematics;
 
 public class AIMovement : SystemBase
 {
-    static readonly float AISpeed = 3f;
-    
+    static readonly float AISpeed = 10f;
+    EntityCommandBufferSystem m_ECBSystem;
+
+    protected override void OnCreate()
+    {
+        m_ECBSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
     protected override void OnUpdate()
     {
         var deltaTime = Time.DeltaTime;
-        var elapsedTime = (uint)System.DateTime.Now.Ticks;
+        var randomSeed = (uint)System.DateTime.Now.Ticks;
         var boardSize = GetSingleton<GameInfo>().boardSize;
-        Entities.ForEach((int entityInQueryIndex, ref AICursor cursor, ref Position position) =>
+        
+        var ecb = m_ECBSystem.CreateCommandBuffer().AsParallelWriter();
+
+        Entities.WithAll<Timer, AICursor>().ForEach((Entity e, int entityInQueryIndex, in Timer timer) =>
+        {
+            if(timer.Value <= 0f)
+                ecb.RemoveComponent<Timer>(entityInQueryIndex, e);
+        }).ScheduleParallel();
+        
+        Entities.WithNone<Timer>().ForEach((Entity e, int entityInQueryIndex, ref AICursor cursor, ref Position position) =>
         {
             var direction = cursor.Destination - position.Value;
             var distance = math.lengthsq(direction);
@@ -20,9 +35,10 @@ public class AIMovement : SystemBase
             if (distance == 0 || movementLength >= distance)
             {
                 position.Value = cursor.Destination;
-                var random = new Random((uint)(elapsedTime + entityInQueryIndex));
+                var random = new Random((uint)(randomSeed + entityInQueryIndex));
                 cursor.Destination = new int2(random.NextInt(0, boardSize.x), random.NextInt(0, boardSize.y));
                 // TODO: click and wait before moving again maybe?
+                ecb.AddComponent(entityInQueryIndex, e, new Timer(){Value = random.NextFloat(1f,2f)});
             }
             else
             {
@@ -30,5 +46,7 @@ public class AIMovement : SystemBase
             }
 
         }).ScheduleParallel();
+        
+        m_ECBSystem.AddJobHandleForProducer(Dependency);
     }
 }
