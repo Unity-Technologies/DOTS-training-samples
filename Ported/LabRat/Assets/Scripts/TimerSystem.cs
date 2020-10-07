ccd16a615dc4b4d420bad000f866f4233d0ee7aa
Bronson Zgeb <bronson.zgeb@unity3d.com>
2020-10-07 11:39:54 -0400

    Added a timer system

---
+++ Ported/LabRat/Assets/Scripts/TimerSystem.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public class TimerSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var deltaTime = Time.DeltaTime;
+        Entities.ForEach(
+            (ref Timer timer) =>
+            {
+                timer.Value -= deltaTime;
+                timer.Value = math.max(timer.Value, 0f);
+            }).ScheduleParallel();
+    }
+}
