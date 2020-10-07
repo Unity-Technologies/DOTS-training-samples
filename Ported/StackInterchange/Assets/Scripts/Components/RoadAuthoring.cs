192c774a0195d8f0fab82dcf85914a81501f90c0
Gustavo Castro <gustavo@unity3d.com>
2020-10-07 08:35:17 -0700

    Saving Road Work - mostly scaffolding

---
+++ Ported/StackInterchange/Assets/Scripts/Components/RoadAuthoring.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct Road : IComponentData
+{
+}
+
+public class RoadAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public List<GameObject> nodeList;
+    
+    public void Convert(Entity entity, EntityManager dstManager,
+        GameObjectConversionSystem conversionSystem)
+    {
+    }
+}
--- Ported/StackInterchange/Assets/Scripts/Components/RoadNode.cs
+++ Ported/StackInterchange/Assets/Scripts/Components/RoadNode.cs
@@ -1,7 +1,9 @@
 using Unity.Entities;
+using Unity.Transforms;
 
 [GenerateAuthoringComponent]
 public struct RoadNode : IComponentData
 {
-
+     public Entity nodePosition;
+     public int colorBits;
 }
