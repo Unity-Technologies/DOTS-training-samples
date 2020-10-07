51b63c77b89483a299fb6ccf95e394eeebfbf802
kaveh.kamali <kaveh.kamali@unity3d.com>
2020-10-07 10:06:13 -0400

    Collision System

---
+++ Ported/LabRat/Assets/DOTSPorted/DOTSScripts/CollisionSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[UpdateAfter(typeof(SpawningSystem))]
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+public class CollisionSystem : SystemBase
+{
+    private EntityQuery m_Query;
+    protected override void OnCreate()
+    {
+        m_Query = GetEntityQuery(typeof(Cat), typeof(Position));
+        RequireForUpdate(m_Query);
+    }
+    
+    protected override void OnUpdate()
+    {
+        var catPositionArray = m_Query.ToComponentDataArray<Position>(Allocator.TempJob);
+        var sys = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+        var ecb = sys.CreateCommandBuffer();
+        var collisionRadius = GetSingleton<GameState>().collisionRadius;
+        Entities
+            .WithAll<Mouse>()
+            .WithDisposeOnCompletion(catPositionArray)
+            .ForEach((ref Entity entity, in Position translation) =>
+        {
+            for (var i=0; i<catPositionArray.Length; i++)
+            {
+                var dist = math.distance(translation.position, catPositionArray[i].position);
+                if (dist < collisionRadius)
+                {
+                    ecb.DestroyEntity(entity);
+                }
+            }
+        }).Schedule();
+        sys.AddJobHandleForProducer(Dependency);
+    }
+}
