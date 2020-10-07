4991f86b17e638bb5be425c08b7aa59432baf805
Gustavo Castro <gustavo@unity3d.com>
2020-10-07 11:03:53 -0700

    Changes to “Next Nodes”
    NextNode => forward Node
    ExitNode => towards exits

--- Ported/StackInterchange/Assets/Scripts/Components/RoadNode.cs
+++ Ported/StackInterchange/Assets/Scripts/Components/RoadNode.cs
@@ -4,9 +4,8 @@ using Unity.Transforms;
 [GenerateAuthoringComponent]
 public struct RoadNode : IComponentData
 {
-     public uint index;
-     public Entity nodePosition;
      public int colorBits;
-     
+
      public Entity nextNode;
+     public Entity exitNode;
 }
