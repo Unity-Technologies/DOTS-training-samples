9192827a6b180586c05108a2e35cb0be7af06c12
Alan Hughes <alan.hughes@unity3d.com>
2020-10-07 13:16:04 -0700

    Fix cell spawning
    - Fix initial fire intensity values
    - Add CellTag to make sure we only process cells

--- Ported/BucketBrigade/Assets/Scripts/CellDisplaySystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/CellDisplaySystem.cs
@@ -22,7 +22,7 @@ public class CellDisplaySystem : SystemBase
 		float cellDisplayRange = cellDisplay.FireValue - cellDisplay.CoolValue;
 
 		Entities
-			.ForEach((Entity entity, ref Translation translation, ref NonUniformScale scale, ref Color color, in Intensity intensity, in RootTranslation rootTranslation) =>
+			.ForEach((ref Translation translation, ref NonUniformScale scale, ref Color color, in CellTag cell, in Intensity intensity, in RootTranslation rootTranslation) =>
 			{
 				float t = math.clamp((intensity.Value - cellDisplay.CoolValue) / cellDisplayRange, 0.0f, 1.0f);
 				float top = math.lerp(cellDisplay.CoolHeight, cellDisplay.FireHeight, t) + rootTranslation.Value.y;
