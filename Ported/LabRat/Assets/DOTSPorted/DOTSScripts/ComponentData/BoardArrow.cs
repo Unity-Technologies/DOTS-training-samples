f5b846477cb2f4927263a7aefb70bec6ca01b7c0
Zoe Bare <zoe.bare@unity3d.com>
2020-10-08 18:14:21 -0500

    Added a shrink function for board arrows.

--- Ported/LabRat/Assets/DOTSPorted/DOTSScripts/ComponentData/BoardArrow.cs
+++ Ported/LabRat/Assets/DOTSPorted/DOTSScripts/ComponentData/BoardArrow.cs
@@ -1,10 +1,21 @@
 
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 
 [GenerateAuthoringComponent]
 public struct BoardArrow : IComponentData
 {
+    public static void Shrink(EntityManager entityManager, Entity arrowEntity)
+    {
+        var scale = entityManager.GetComponentData<NonUniformScale>(arrowEntity);
+
+        entityManager.SetComponentData(arrowEntity, new NonUniformScale()
+        {
+            Value = new float3(scale.Value.x * 0.5f, scale.Value.y * 0.5f, scale.Value.z * 0.5f),
+        });
+    }
+
     public Entity targetEntity;
     public EntityDirection direction;
     public int2 gridPosition;
