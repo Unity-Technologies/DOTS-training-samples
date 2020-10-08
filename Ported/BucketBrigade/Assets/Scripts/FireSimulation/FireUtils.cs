91abb38e5379e79c031954efd577bd3c87c34daf
Julien Ignace <julien@unity3d.com>
2020-10-08 10:32:23 +0200

    Added propagation (runs on main thread for now).

---
+++ Ported/BucketBrigade/Assets/Scripts/FireSimulation/FireUtils.cs
@@ -0,0 +1,13 @@
+﻿class FireUtils
+{
+    public static int GridToArrayCoord(int row, int column, int rowCount)
+    {
+        return column * rowCount + row;
+    }
+
+    public static void ArrayToGridCoord(int index, int rowCount, out int row, out int column)
+    {
+        row = index % rowCount;
+        column = UnityEngine.Mathf.FloorToInt(index / rowCount);
+    }
+}
