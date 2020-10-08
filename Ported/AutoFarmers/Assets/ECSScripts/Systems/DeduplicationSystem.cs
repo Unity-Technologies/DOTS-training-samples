f018bee3fc0156e618ddf78e66b5dfad45300911
Apoorva Joshi <apoorva.joshi@unity3d.com>
2020-10-08 14:45:01 +0200

    Add deduplication system

---
+++ Ported/AutoFarmers/Assets/ECSScripts/Systems/DeduplicationSystem.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class DeduplicationSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+    }
+    protected override void OnUpdate()
+    {
+        NativeList<Entity> lockedEntities = new NativeList<Entity>(Allocator.TempJob);
+        Entities.
+            WithStructuralChanges().
+            WithAll<NeedsDeduplication>().
+            ForEach(
+            (Entity farmerEntity, int entityInQueryIndex, in TargetEntity targetEntity) =>
+            {
+                EntityManager.RemoveComponent<NeedsDeduplication>(farmerEntity);
+
+                if (lockedEntities.Contains(targetEntity.target))
+                {
+                    // Unassign this entity because it is already taken by another farmer
+                    EntityManager.RemoveComponent<TargetEntity>(farmerEntity);
+                    EntityManager.RemoveComponent<PickUpCropTask>(farmerEntity);
+                }
+                else
+                {
+                    lockedEntities.Add(targetEntity.target);
+                    EntityManager.AddComponent<Assigned>(targetEntity.target);
+                }
+            }).Run();
+
+        lockedEntities.Dispose();
+    }
+
+}
