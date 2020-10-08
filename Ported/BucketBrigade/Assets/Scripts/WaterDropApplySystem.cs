04901f4a8561fff118234833045dbe919674c09d
Maxime Plantady <maxime.plantady@unity3d.com>
2020-10-08 15:13:47 -0400

    Avoid updating water drop system when query is empty

--- Ported/BucketBrigade/Assets/Scripts/WaterDropApplySystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/WaterDropApplySystem.cs
@@ -3,13 +3,19 @@ using Unity.Entities;
 
 public class WaterDropApplySystem : SystemBase
 {
+	EntityQuery m_ForEachQuery;
+	
+	protected override void OnCreate() {
+		RequireForUpdate(m_ForEachQuery);
+	}
+	
 	protected override void OnUpdate()
 	{
 		var heatMapEntity = GetSingletonEntity<HeatMap>();
 		var heatMap = EntityManager.GetComponentData<HeatMap>(heatMapEntity);
-		var heatMapBuffer = EntityManager.GetBuffer<HeatMapElement>(heatMapEntity).AsNativeArray();
+		var heatMapBuffer = EntityManager.GetBuffer<HeatMapElement>(heatMapEntity);
 
-		Entities.WithStructuralChanges().ForEach((Entity entity, ref WaterDrop drop) =>
+		Entities.WithStoreEntityQueryInField(ref m_ForEachQuery).WithStructuralChanges().ForEach((Entity entity, ref WaterDrop drop) =>
 		{
 			for (int offsetX = -drop.Range; offsetX <= drop.Range; offsetX++)
 			{
