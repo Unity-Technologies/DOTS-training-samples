0ea329875501b7f815ff2cc3ebb6ac19f28cd223
Bronson Zgeb <bronson.zgeb@unity3d.com>
2020-10-08 14:34:26 -0400

    Added the Cat and Rat tags to their respective prefabs. Increased the minimum speed on spawn.

--- Ported/LabRat/Assets/Scripts/SpawningSystem.cs
+++ Ported/LabRat/Assets/Scripts/SpawningSystem.cs
@@ -3,7 +3,7 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 
-public class SpawningSystem: SystemBase
+public class SpawningSystem : SystemBase
 {
     EntityCommandBufferSystem m_ECBSystem;
 
@@ -18,10 +18,10 @@ public class SpawningSystem: SystemBase
 
         Entities.WithAll<Timer, SpawnPoint>().ForEach((Entity entity, int entityInQueryIndex, in Timer timer) =>
         {
-            if(timer.Value <= 0f)
+            if (timer.Value <= 0f)
                 ecb.RemoveComponent<Timer>(entityInQueryIndex, entity);
         }).ScheduleParallel();
-        
+
         Entities
             .WithAll<SpawnPoint, Translation>()
             .WithNone<Timer>()
@@ -33,16 +33,17 @@ public class SpawningSystem: SystemBase
                 {
                     var instance = ecb.Instantiate(entityInQueryIndex, spawnPoint.spawnPrefab);
 
-                    ecb.AddComponent(entityInQueryIndex, instance, new Position { Value = new float2(translation.Value.x, translation.Value.z)});
-                    ecb.AddComponent(entityInQueryIndex, instance, new Speed { Value = rand.NextFloat(0.4f, 20f) });
-                    ecb.AddComponent(entityInQueryIndex, instance, new Direction { Value = spawnPoint.direction});
-                    
-                    ecb.AddComponent(entityInQueryIndex, entity, new Timer() { Value = spawnPoint.spawnFrequency });
+                    ecb.AddComponent(entityInQueryIndex, instance,
+                        new Position {Value = new float2(translation.Value.x, translation.Value.z)});
+                    ecb.AddComponent(entityInQueryIndex, instance, new Speed {Value = rand.NextFloat(2f, 20f)});
+                    ecb.AddComponent(entityInQueryIndex, instance, new Direction {Value = spawnPoint.direction});
+
+                    ecb.AddComponent(entityInQueryIndex, entity, new Timer() {Value = spawnPoint.spawnFrequency});
 
                     spawnPoint.spawnCount -= 1;
                 }
             }).ScheduleParallel();
-        
+
         m_ECBSystem.AddJobHandleForProducer(Dependency);
     }
 }
