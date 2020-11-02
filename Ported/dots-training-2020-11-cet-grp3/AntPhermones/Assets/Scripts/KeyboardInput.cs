7cf1143d2ce7a4bc17f1d81294a73966b3844041
Pavlos Mavridis <pavlos.mavridis@unity3d.com>
2020-11-02 15:29:21 +0100

    First commit: copy of the original project to the ported folder

---
+++ Ported/dots-training-2020-11-cet-grp3/AntPhermones/Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class KeyboardInput : MonoBehaviour {
+	Text text;
+
+	static bool showText=true;
+
+	void Start () {
+		text = GetComponent<Text>();
+		text.enabled = showText;
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.H)) {
+			showText = !showText;
+			text.enabled = showText;
+		}
+		if (Input.GetKeyDown(KeyCode.R)) {
+			Time.timeScale = 1f;
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+}
