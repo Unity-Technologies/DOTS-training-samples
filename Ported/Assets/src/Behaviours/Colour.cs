af5812da45389c82fe2b5e80aeb99623e431be56
Soufiane KHIAT <soufiane.khiat@unity3d.com>
2020-11-02 16:35:56 +0100

    Add first Ported

---
+++ Ported/Assets/src/Behaviours/Colour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colour {
+
+	public static void Set(ref Material _material, Color _newColour)
+	{
+		_material.color = _newColour;
+	}
+
+	public static void RecolourChildren(Transform _target, Color _newColour)
+	{
+		Renderer[] renderers = _target.GetComponentsInChildren<Renderer>();
+		for (int i = 0; i < renderers.Length; i++)
+		{
+			renderers[i].material.color = _newColour;
+		}
+	}
+}
---
+++ Ported/Assets/src/EditorTools/CommuterNavPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CommuterNavPoint : MonoBehaviour
+{
+
+    private Transform t;
+    
+    private void Start()
+    {
+        t = transform;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(t.position, Vector3.one * 0.1f);
+    }
+}
