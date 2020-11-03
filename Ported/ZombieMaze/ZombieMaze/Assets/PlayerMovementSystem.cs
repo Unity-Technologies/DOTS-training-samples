0a1067af83361cfb0e13597bc5bc131d02372bf9
Justin Graham <justing@unity3d.com>
2020-11-03 12:00:48 -0800

    adding player movement system

---
+++ Ported/ZombieMaze/ZombieMaze/Assets/PlayerMovementSystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public struct Position : IComponentData
+{
+    public float2 Value;
+}
+
+public struct Direction : IComponentData
+{
+    public float2 Value;
+}
+
+public class PlayerMovementSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+
+    }
+
+    protected override void OnUpdate()
+    {
+   
+    }
+}
