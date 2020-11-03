e1d057981f46632a1cb990349c77aa7dd4f22aad
Justin Graham <justing@unity3d.com>
2020-11-03 10:12:11 -0800

    add player input system

---
+++ Ported/ZombieMaze/ZombieMaze/Assets/PlayerInputSystem.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+using UnityEngine;
+
+public struct PlayerInput : IComponentData
+{
+    public float2 Direction;
+    public float  ZoomDelta;
+}
+
+public class PlayerInputSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+        var e = EntityManager.CreateEntity(typeof(PlayerInput));
+#if UNITY_EDITOR
+        EntityManager.SetName(e, "PlayerInput");
+#endif
+    }
+
+    protected override void OnUpdate()
+    {
+        var playerInputEntity = GetSingletonEntity<PlayerInput>();
+        PlayerInput playerInput;
+        playerInput.Direction = new float2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        playerInput.ZoomDelta = Input.mouseScrollDelta.y;
+        EntityManager.SetComponentData(playerInputEntity, playerInput);
+    }
+}
