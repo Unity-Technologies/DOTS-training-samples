8982a55815c70d6cf1a70dd363da9d9be3f6df69
Vincent <vincent.breysse99@gmail.com>
2020-11-04 10:58:10 +0100

    Setup TestRayCast scene. Looks like it's working great

---
+++ Ported/AntPhermones/Assets/ECS/TestRayCast/TestRayCastSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class TestRayCastSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+        TestRayCast.EntityManager = EntityManager;
+    }
+
+    protected override void OnUpdate()
+    {
+
+    }
+}
