66d5e4fed13d5e99642ccb2ae1801d6cbbe0890a
Soufiane KHIAT <soufiane.khiat@unity3d.com>
2020-11-04 16:21:57 +0100

    Local Change

--- Ported/Metro/Assets/Ported/Scripts/Authoring/PathDataAuthoring.cs
+++ Ported/Metro/Assets/Ported/Scripts/Authoring/PathDataAuthoring.cs
@@ -13,89 +13,73 @@ public class PathDataAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
         dstManager.AddSharedComponentData(entity, new ID {Value = transform.GetSiblingIndex()});
-        
+
         var builder = new BlobBuilder(Allocator.Temp);
         ref var pathData = ref builder.ConstructRoot<PathData>();
 
         var halfMarkerCount = transform.childCount;
         var totalMarkerCount = halfMarkerCount * 2;
-        
-        // Initialise Arrays
-        var positions = builder.Allocate(ref pathData.Positions, totalMarkerCount);
-        var markerTypes = builder.Allocate(ref pathData.MarkerTypes, totalMarkerCount);
-        var handlesIn = builder.Allocate(ref pathData.HandlesIn, totalMarkerCount);
-        var handlesOut = builder.Allocate(ref pathData.HandlesOut, totalMarkerCount);
-        var distances = builder.Allocate(ref pathData.Distances, totalMarkerCount);
-        float totalDistance = 0.0f;
+
+        // Initialize Arrays
+        float3[] positions = new float3[totalMarkerCount];
+        var markerTypesB = builder.Allocate(ref pathData.MarkerTypes, totalMarkerCount);
+        float3[] handlesIn = new float3[totalMarkerCount];
+        float3[] handlesOut = new float3[totalMarkerCount];
+        float[] distances = new float[totalMarkerCount];
 
         // Outbound Positions
         for (var c = 0; c < transform.childCount; c++)
             positions[c] = transform.GetChild(c).transform.position;
 
-        // Outbound Handles
-        for (var p = 0; p < halfMarkerCount; p++)
-        {
-            var previousPosition = positions[p == 0 ? halfMarkerCount - 1 : p - 1];
-            var currentPosition = positions[p];
-            var nextPosition = positions[(p + 1) % halfMarkerCount];
+        RebuildHandle(positions, ref handlesIn, ref handlesOut, halfMarkerCount);
 
-            var offsetPosition = nextPosition - previousPosition;
-            var handleIn = BezierHelpers.GetHandleIn(currentPosition, offsetPosition);
-            var handleOut = BezierHelpers.GetHandleOut(currentPosition, offsetPosition);
-
-            handlesIn[p] = handleIn;
-            handlesOut[p] = handleOut;
-        }
-
-        float halfDistance = BezierHelpers.MeasurePath(positions, handlesIn, handlesOut, out var tempDistances);
+        float halfDistance = BezierHelpers.MeasurePath(positions, handlesIn, handlesOut, halfMarkerCount, out var tempDistances);
         for (var p = 0; p < halfMarkerCount; p++)
         {
             distances[p] = tempDistances[p];
         }
 
-        // Return Positions
-        for (int p = 0; p < halfMarkerCount; p++)
+        for (int p = halfMarkerCount - 1; p >= 0; p--)
         {
             var position = positions[p];
             var distance = distances[p];
             var perpPosition = BezierHelpers.GetPointPerpendicularOffset(position, distance, positions, handlesIn,
                 handlesOut, distances, halfDistance, Globals.BEZIER_PLATFORM_OFFSET);
 
-            positions[halfMarkerCount + p] = perpPosition;
+            positions[halfMarkerCount + p - halfMarkerCount] = perpPosition;
         }
-        
-        // Outbound Handles
-        for (var p = halfMarkerCount; p < totalMarkerCount; p++)
-        {
-            var previousPosition = positions[p == 0 ? halfMarkerCount - 1 : p - 1];
-            var currentPosition = positions[p];
-            var nextPosition = positions[(p + 1) % halfMarkerCount];
-            
-            var offsetPosition = nextPosition - previousPosition;
-            var handleIn = BezierHelpers.GetHandleIn(currentPosition, offsetPosition);
-            var handleOut = BezierHelpers.GetHandleOut(currentPosition, offsetPosition);
-            
-            handlesIn[p] = handleIn;
-            handlesOut[p] = handleOut;
-        }
-        
+
+        RebuildHandle(positions, ref handlesIn, ref handlesOut, totalMarkerCount);
+
         // Marker types
         for (var m = 0; m < halfMarkerCount; m++)
         {
             // Outbound
-            markerTypes[m] = (int) railMarkerTypes[m];
-            
+            markerTypesB[m] = (int) railMarkerTypes[m];
+
             // Return
-            markerTypes[halfMarkerCount + m] = (int)railMarkerTypes[halfMarkerCount - 1 - m];
+            markerTypesB[halfMarkerCount + m] = (int)railMarkerTypes[halfMarkerCount - 1 - m];
         }
 
         // Total path distance
-        totalDistance = BezierHelpers.MeasurePath(positions, handlesIn, handlesOut, out tempDistances);
-        pathData.TotalDistance = totalDistance;
+        pathData.TotalDistance = BezierHelpers.MeasurePath(positions, handlesIn, handlesOut, totalMarkerCount, out tempDistances);
 
         // Marker distances
         for (var d = 0; d < totalMarkerCount; d++)
             distances[d] = tempDistances[d];
+
+        var positionsB = builder.Allocate(ref pathData.Positions, totalMarkerCount);
+        var handlesInB = builder.Allocate(ref pathData.HandlesIn, totalMarkerCount);
+        var handlesOutB = builder.Allocate(ref pathData.HandlesOut, totalMarkerCount);
+        var distancesB = builder.Allocate(ref pathData.Distances, totalMarkerCount);
+
+        for (var p = 0; p < halfMarkerCount; p++)
+        {
+            positionsB[p] = positions[p];
+            handlesInB[p] = handlesIn[p];
+            handlesOutB[p] = handlesOut[p];
+            distancesB[p] = distances[p];
+        }
 
         // Path colour
         pathData.Colour = new float3(pathColour.r, pathColour.g, pathColour.b);
@@ -106,10 +90,55 @@ public class PathDataAuthoring : MonoBehaviour, IConvertGameObjectToEntity
         });
     }
 
+    private void RebuildHandle(float3[] positions, ref float3[] handlesIn, ref float3[] handlesOut, int size)
+    {
+        // Outbound Handles
+        for (var p = 1; p < size - 1; p++)
+        {
+            var currentPosition = positions[p];
+            var previousPosition = positions[p - 1];
+            var nextPosition = positions[p + 1];
+
+            var offsetPosition = nextPosition - previousPosition;
+            var handleIn = BezierHelpers.GetHandleIn(currentPosition, offsetPosition);
+            var handleOut = BezierHelpers.GetHandleOut(currentPosition, offsetPosition);
+
+            handlesIn[p] = handleIn;
+            handlesOut[p] = handleOut;
+        }
+        // Point 0
+        {
+            var currentPosition = positions[0];
+            var ptA = positions[1];
+            var ptB = positions[0];
+
+            var offsetPosition = ptA - ptB;
+            var handleIn = BezierHelpers.GetHandleIn(currentPosition, offsetPosition);
+            var handleOut = BezierHelpers.GetHandleOut(currentPosition, offsetPosition);
+
+            handlesIn[0] = handleIn;
+            handlesOut[0] = handleOut;
+        }
+        // Last Point
+        {
+            int last = size - 1;
+            var currentPosition = positions[last];
+            var ptA = positions[last];
+            var ptB = positions[last - 1];
+
+            var offsetPosition = ptA - ptB;
+            var handleIn = BezierHelpers.GetHandleIn(currentPosition, offsetPosition);
+            var handleOut = BezierHelpers.GetHandleOut(currentPosition, offsetPosition);
+
+            handlesIn[last] = handleIn;
+            handlesOut[last] = handleOut;
+        }
+    }
+
     private void OnDrawGizmos()
     {
         Debug.Log("Gizmos");
-        
+
         Gizmos.color = pathColour;
         for (var c = 0; c < transform.childCount; c++)
         {
--- Ported/Metro/Assets/Ported/Scripts/Systems/RailGeneration.cs
+++ Ported/Metro/Assets/Ported/Scripts/Systems/RailGeneration.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.Collections;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
 
 public class RailGeneration : SystemBase
@@ -9,19 +10,54 @@ public class RailGeneration : SystemBase
         var railPrefab = GetSingleton<MetroData>().RailPrefab;
         var ecb = new EntityCommandBuffer(Allocator.Temp);
 
-        Entities.ForEach((in PathRef pathdata) =>
-        {
+        Entities.WithoutBurst().ForEach((in PathRef pathdata) =>
+        { 
             ref var positions = ref pathdata.Data.Value.Positions;
-            for (var i = 0; i < positions.Length; i++)
+            ref var handlesIn = ref pathdata.Data.Value.HandlesIn;
+            ref var handlesOut = ref pathdata.Data.Value.HandlesOut;
+            ref var distances = ref pathdata.Data.Value.Distances;
+            float totalAbsoluteDistance = pathdata.Data.Value.TotalDistance;
+            float absoluteDistance = 0.0f;
+
+            int count = positions.Length;
+
+            float3[] pos = positions.ToArray();
+            float3[] hIn = handlesIn.ToArray();
+            float3[] hOut = handlesOut.ToArray();
+            float[] dist = distances.ToArray();
+
+            //float3[] pos = new float3[count];
+            //float3[] hIn = new float3[count];
+            //float3[] hOut = new float3[count];
+            //float[] dist = new float[count];
+            //for (int i = 0; i < count; ++i)
+            //{
+            //    pos[i]  = distances[i];
+            //    hIn[i]  = handlesIn[i];
+            //    hOut[i] = handlesOut[i];
+            //    dist[i] = distances[i];
+            //}
+
+            while (absoluteDistance < totalAbsoluteDistance)
             {
+                float coef = absoluteDistance / totalAbsoluteDistance;
+
+                float3 railPos = BezierHelpers.GetPosition(pos, hIn, hOut, dist, totalAbsoluteDistance, coef);
+                float3 railRot = BezierHelpers.GetNormalAtPosition(pos, hIn, hOut, dist, totalAbsoluteDistance, coef);
+
                 var railEntity = ecb.Instantiate(railPrefab);
-                var railTranslation = new Translation {Value = positions[i]};
+                var railTranslation = new Translation { Value = railPos };
+                var railRotation = new Rotation {Value = quaternion.LookRotation(railRot, new float3(0, 1, 0)) };
+
                 ecb.SetComponent(railEntity, railTranslation);
+                ecb.SetComponent(railEntity, railRotation);
+
+                absoluteDistance += Globals.RAIL_SPACING;
             }
         }).Run();
 
         ecb.Playback(EntityManager);
-        
+
         Enabled = false;
     }
 }
