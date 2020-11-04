45069950c0646573b82ff495f800e77cccad3a7a
Jono Forbes <jono@unity3d.com>
2020-11-04 15:40:28 -0500

    CarPosToWorld now ScheduleParallel

--- Ported/MagneticRoads/Assets/MagnetoRoads/Source/System/CarPositionToWorldSystem.cs
+++ Ported/MagneticRoads/Assets/MagnetoRoads/Source/System/CarPositionToWorldSystem.cs
@@ -7,7 +7,10 @@ public class CarPositionToWorldSystem : SystemBase
 {
     protected override void OnUpdate()
     {
+        ComponentDataFromEntity<Translation> translationAccessor = GetComponentDataFromEntity<Translation>();
+        
         Entities
+            .WithNativeDisableContainerSafetyRestriction(translationAccessor)
             .ForEach((Entity entity, ref Spline spline, ref Lane lane, ref DynamicBuffer<CarBufferElement> buffer) =>
             {
                 foreach (Entity car in buffer.Reinterpret<Entity>())
@@ -15,10 +18,10 @@ public class CarPositionToWorldSystem : SystemBase
                     CarPosition carPosition = GetComponent<CarPosition>(car);
                     float splineRatio = carPosition.Value / lane.Length;
                     float3 pos = math.lerp(spline.startPos, spline.endPos, splineRatio);
-                    Translation translation = new Translation {Value = pos}; 
-                    SetComponent(car, translation);
+                    Translation translation = new Translation {Value = pos};
+                    translationAccessor[car] = translation;
                 }
                 
-            }).Run();
+            }).ScheduleParallel();
     }
 }
