f73146bb5a0d1c02ac25eddebedbfa87b0b296cd
Justin Graham <justing@unity3d.com>
2020-11-04 13:59:50 -0800

    wip for dijkstra map generation, and components for map data:

--- Ported/ZombieMaze/ZombieMaze/Assets/CommonComponents.cs
+++ Ported/ZombieMaze/ZombieMaze/Assets/CommonComponents.cs
@@ -5,6 +5,8 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 
+using System;
+
 public struct Speed : IComponentData
 {
     public float Value;
@@ -43,4 +45,37 @@ public struct TileSpawner : IComponentData
 {
     public float2 TileSize;
     public Entity TilePrefab;
+}
+
+[Flags]
+public enum WallBits : byte
+{
+    Left   = (1 << 0),
+    Right  = (1 << 1),
+    Top    = (1 << 2),
+    Bottom = (1 << 3)
+}
+
+public struct TagDijkstraGenerateMap : IComponentData {};
+
+public struct MapCell : IBufferElementData
+{
+    public byte Value;
+}
+
+public struct DistCell : IBufferElementData
+{
+    public int Value;
+}
+
+public struct DijkstraMap : IComponentData
+{
+    public int Width;
+    public int Height;
+
+    public DijkstraMap(int width, int height)
+    {
+        Width  = width;
+        Height = height;
+    }
 }
