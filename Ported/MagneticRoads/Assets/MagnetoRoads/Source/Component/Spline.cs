9f09c956814812ac40e3d22001505bb98c795a74
Jono Forbes <jono@unity3d.com>
2020-11-04 14:12:03 -0500

    Offset lanes; stub code for bezier eval

--- Ported/MagneticRoads/Assets/MagnetoRoads/Source/Component/Spline.cs
+++ Ported/MagneticRoads/Assets/MagnetoRoads/Source/Component/Spline.cs
@@ -5,7 +5,9 @@ using Unity.Mathematics;
 public struct Spline : IComponentData
 {
     public float3 startPos;
+    public float3 startTangent;
     public float3 endPos;
+    public float3 endTangent;
     public quaternion startRot;
     public quaternion endRot;
 }
