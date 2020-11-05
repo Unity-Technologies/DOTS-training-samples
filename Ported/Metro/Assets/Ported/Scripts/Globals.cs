bf9ab75646ebd5cc425d3caef7901ddaf16ae79a
Tim Volossevich <tim.volossevich@unity3d.com>
2020-11-05 13:58:16 +0000

    Updated global parameters to match original

--- Ported/Metro/Assets/Ported/Scripts/Globals.cs
+++ Ported/Metro/Assets/Ported/Scripts/Globals.cs
@@ -1,7 +1,7 @@
 ﻿public class Globals
 {
-    public const float BEZIER_HANDLE_REACH = 0.1f;
-    public const float BEZIER_PLATFORM_OFFSET = 3f;
+    public const float BEZIER_HANDLE_REACH = 0.15f;
+    public const float BEZIER_PLATFORM_OFFSET = 8f;
     public const float PLATFORM_ADJACENCY_LIMIT = 12f;
     public const int BEZIER_MEASUREMENT_SUBDIVISIONS = 2;
     public const float PLATFORM_ARRIVAL_THRESHOLD = 0.975f;
