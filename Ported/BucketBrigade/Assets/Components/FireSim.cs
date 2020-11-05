d780f8fb8f310490bdb887a0bb130606ccafabe7
Nigel John Williams <nigel.williams@unity3d.com>
2020-11-05 17:28:55 +0100

    Started passing bucket system

--- Ported/BucketBrigade/Assets/Components/FireSim.cs
+++ Ported/BucketBrigade/Assets/Components/FireSim.cs
@@ -58,4 +58,23 @@ public struct FireSim : IComponentData
         }
         return index;
     }
+
+    public static Entity GetClosestEntity2(float3 position, NativeArray<Entity> emptyEntities, NativeArray<Translation> translations)
+    {
+        float distance = float.MaxValue;
+        Entity closestEntity = Entity.Null;
+        for (int i = 0; i < emptyEntities.Length; i++)
+        {
+            var entity = emptyEntities[i];
+            var translation = translations[i].Value;
+            var currentDistance = math.distance(position, translation);
+
+            if (currentDistance < distance)
+            {
+                distance = currentDistance;
+                closestEntity = entity;
+            }
+        }
+        return closestEntity;
+    }
 }
--- Ported/BucketBrigade/Assets/Systems/PassingBucketSystem.cs
+++ Ported/BucketBrigade/Assets/Systems/PassingBucketSystem.cs
@@ -1,14 +1,59 @@
-﻿using Unity.Entities;
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
 public class PassingBucketSystem : SystemBase
 {
+    public static EntityQuery buckets;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        buckets = GetEntityQuery(ComponentType.ReadOnly<EmptyBucket>(), ComponentType.ReadOnly<Translation>());
+    }
+
     protected override void OnUpdate()
     {
-        var ecb = new EntityCommandBuffer()
-        /*Entities
-            .ForEach((Entity entity, in PasserBot bot, in HoldingBucket bucket) =>
+
+        Entities
+            .ForEach((Entity entity, ref Translation translation, in FillerBot fillerBot) =>
             {
-                
-            }).Schedule();*/
+                var fillerPosition = translation;
+
+                //if (filler)
+            }).ScheduleParallel();
+
+
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+        var cdfe = GetComponentDataFromEntity<Translation>();
+
+        Entities
+            .ForEach((Entity entity, ref Translation translation, in PasserBot bot, in HoldingBucket bucket) =>
+            {
+                var passerPosition = translation;
+                var pickupPosition = bot.DropoffPosition;
+
+                var distanceLeft = passerPosition.Value - pickupPosition;
+                var length = math.length(distanceLeft);
+
+                var threshold = 2.0f;
+                var speed = 5.0f;
+                var deltaTime = 0.5f;
+
+                if (length <= threshold)
+                {
+                    ecb.RemoveComponent<HoldingBucket>(entity);
+                }
+                else
+                {
+                    var direction = distanceLeft / length;
+                    var newPosition = passerPosition.Value + speed * deltaTime * direction;
+                    cdfe[entity] = new Translation() { Value = newPosition };
+                    cdfe[bucket.Target] = new Translation() { Value = newPosition };
+                }
+
+            }).ScheduleParallel();
     }
 }
 
