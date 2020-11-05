58aea7e24a839dbea185cdc1800dfe2756b12918
Justin Graham <justing@unity3d.com>
2020-11-05 08:47:37 -0800

    updated the map generation, still happens every frame for now

--- Ported/ZombieMaze/ZombieMaze/Assets/DijkstraGeneratorSystem.cs
+++ Ported/ZombieMaze/ZombieMaze/Assets/DijkstraGeneratorSystem.cs
@@ -1,13 +1,16 @@
 using Unity.Burst;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
 
+using UnityEngine;
+
 public class DijkstraGeneratorSystem : SystemBase
 {   
-    const int kWidth  = 10;
-    const int kHeight = 10;
+    const int kWidth  = 1000;
+    const int kHeight = 1000;
  
     protected override void OnCreate()
     {
@@ -17,27 +20,37 @@ public class DijkstraGeneratorSystem : SystemBase
 #endif
         EntityManager.AddBuffer<MapCell>(e);
         EntityManager.AddBuffer<DistCell>(e);
-        EntityManager.SetComponentData(e, new DijkstraMap(kWidth, kHeight));
+        var map = new DijkstraMap(kWidth, kHeight);
+        map.SetOrigin(0, 0);
+        EntityManager.SetComponentData(e, map);
     }
 
-    protected override void OnUpdate()
+    unsafe protected override void OnUpdate()
     {
-        int2 position = new int2(0, 0);
-
         Entities.ForEach((Entity e, ref DijkstraMap map, ref DynamicBuffer<MapCell> wallMap, ref DynamicBuffer<DistCell> distMap) => 
         {
             var length = map.Width * map.Height;
             if (distMap.Length != length)
                 distMap.ResizeUninitialized(length);
 
+// Temp code to init the wall data to no walls.
+if (wallMap.Length != length)
+{
+    wallMap.ResizeUninitialized(length);
+    for (var i = 0; i < length; ++i)
+        wallMap[i] = new MapCell(){ Value = 0 };  
+}
+
             var positions = new NativeList<int2>(length, Allocator.Temp);
 
+            // Init all distances to max distance. This can be parallelized.
+            DistCell* pDistCell = (DistCell*)distMap.GetUnsafePtr();
             for (var i = 0; i < distMap.Length; ++i)
-                distMap[i] = new DistCell(){ Value = int.MaxValue };
+                pDistCell->Value = int.MaxValue;
 
-            var sourceIndex = position.y * map.Width + position.x;
-            distMap[sourceIndex] = new DistCell(){ Value = 0 };
-            positions.AddNoResize(position);
+            var sourceIndex = map.Origin.y * map.Width + map.Origin.x;
+            pDistCell[sourceIndex].Value = 0;
+            positions.AddNoResize(map.Origin);
 
             while (positions.Length > 0)
             {
@@ -48,28 +61,38 @@ public class DijkstraGeneratorSystem : SystemBase
                 var index = current.y * map.Width + current.x;
                 var dist = 1 + distMap[index].Value;
 
-                VisitNeighbor(current.x - 1, current.y - 1, dist, ref map, ref distMap, ref positions);
-				VisitNeighbor(current.x - 1, current.y + 0, dist, ref map, ref distMap, ref positions);
-				VisitNeighbor(current.x - 1, current.y + 1, dist, ref map, ref distMap, ref positions);
-				VisitNeighbor(current.x,     current.y - 1, dist, ref map, ref distMap, ref positions);
-				VisitNeighbor(current.x,     current.y + 1, dist, ref map, ref distMap, ref positions);
-				VisitNeighbor(current.x + 1, current.y - 1, dist, ref map, ref distMap, ref positions);
-				VisitNeighbor(current.x + 1, current.y + 0, dist, ref map, ref distMap, ref positions);
-				VisitNeighbor(current.x + 1, current.y + 1, dist, ref map, ref distMap, ref positions);
-            }
+         		VisitNeighbor(current, -1,  0, WallBits.Left,   dist, ref map, ref wallMap, ref distMap, ref positions);
+				VisitNeighbor(current,  0, -1, WallBits.Bottom, dist, ref map, ref wallMap, ref distMap, ref positions);
+				VisitNeighbor(current,  0, +1, WallBits.Top,    dist, ref map, ref wallMap, ref distMap, ref positions);
+				VisitNeighbor(current, +1,  0, WallBits.Right,  dist, ref map, ref wallMap, ref distMap, ref positions);
+		    }
         })
         .Schedule();
     }
 
-    static void VisitNeighbor(int x, int y, int distance, ref DijkstraMap map, ref DynamicBuffer<DistCell> distMap, ref NativeList<int2> positions)
+    unsafe static void VisitNeighbor(int2 current, int dx, int dy, WallBits wall, int distance, ref DijkstraMap map, ref DynamicBuffer<MapCell> wallMap, ref DynamicBuffer<DistCell> distMap, ref NativeList<int2> positions)
     {
-        if (x >= 0 && x < map.Width && y >= 0 && y < map.Height)
+        DistCell* pDistCell = (DistCell*)distMap.GetUnsafePtr();
+
+        var index = current.y * map.Width + current.x;
+
+        // If passable in the desired direction
+        var walls = (WallBits)wallMap[index].Value;
+        if ((walls & wall) == 0)
         {
-            var index = y * map.Width + x;
-            if (distMap[index].Value == int.MaxValue)
+            int2 next;
+            next.x = current.x + dx;
+            next.y = current.y + dy;
+
+            // If the next position is valid, and hasn't been visited yet.
+            if (next.x >= 0 && next.x < map.Width && next.y >= 0 && next.y < map.Height)
             {
-                distMap[index] = new DistCell(){ Value = distance };
-                positions.AddNoResize(new int2(x, y));
+                index = next.y * map.Width + next.x;
+                if (pDistCell[index].Value == int.MaxValue)
+                {
+                    pDistCell[index].Value = distance;
+                    positions.AddNoResize(next);
+                }
             }
         }
     }
