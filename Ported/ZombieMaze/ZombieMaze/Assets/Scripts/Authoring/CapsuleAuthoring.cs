1de16914cf7afe4c047daace9ddd2eaeeb997b72
Matt Roper <mattr@unity3d.com>
2020-11-05 13:49:08 -0800

    Capsules spinning and stuff

--- Ported/ZombieMaze/ZombieMaze/Assets/Scripts/Authoring/CapsuleAuthoring.cs
+++ Ported/ZombieMaze/ZombieMaze/Assets/Scripts/Authoring/CapsuleAuthoring.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 [DisallowMultipleComponent]
@@ -8,5 +9,8 @@ public class CapsuleAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
         dstManager.AddComponent<CapsuleRotation>(entity);
+        dstManager.AddComponentData(entity, new Position());
+        //dstManager.AddComponentData(entity, new Rotation());
+        dstManager.AddComponentData(entity, new Random((uint)UnityEngine.Random.Range(0, int.MaxValue)));
     }
 }
