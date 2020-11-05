3d3feaf847d52233f4c588349af3d9b87579e16b
Jono Forbes <jono@unity3d.com>
2020-11-05 14:40:00 -0500

    Single and Double intersection spline setup systems

--- Ported/MagneticRoads/Assets/MagnetoRoads/Source/System/IntersectionSystems/DoubleIntersectionSystem.cs
+++ Ported/MagneticRoads/Assets/MagnetoRoads/Source/System/IntersectionSystems/DoubleIntersectionSystem.cs
@@ -1,6 +1,6 @@
 using Unity.Collections;
 using Unity.Entities;
-using UnityEngine;
+using Unity.Transforms;
 
 [UpdateAfter(typeof(SimpleIntersectionSystem))]
 public class DoubleIntersectionSystem : SystemBase
@@ -10,11 +10,12 @@ public class DoubleIntersectionSystem : SystemBase
         float deltaTime = Time.DeltaTime;
 
         Entities
-            .ForEach((Entity entity, ref DoubleIntersection doubleIntersection) =>
+            .ForEach((Entity entity, ref DoubleIntersection doubleIntersection, in Spline spline0, in Spline spline1) =>
             {
                 NativeArray<Entity> laneIns = new NativeArray<Entity>(2, Allocator.Temp);
                 NativeArray<Entity> laneOuts = new NativeArray<Entity>(2, Allocator.Temp);
                 NativeArray<Entity> cars = new NativeArray<Entity>(2, Allocator.Temp);
+                NativeArray<Spline> splines = new NativeArray<Spline>(2, Allocator.Temp);
 
                 laneIns[0] = doubleIntersection.laneIn0;
                 laneIns[1] = doubleIntersection.laneIn1;
@@ -24,6 +25,9 @@ public class DoubleIntersectionSystem : SystemBase
 
                 cars[0] = doubleIntersection.car0;
                 cars[1] = doubleIntersection.car1;
+
+                splines[0] = spline0;
+                splines[1] = spline1;
 
                 for (int i = 0; i < laneIns.Length; i++)
                 {
@@ -65,19 +69,37 @@ public class DoubleIntersectionSystem : SystemBase
                             else
                                 doubleIntersection.car1 = reachedEndOfLaneCar;
 
+                            SetComponent(reachedEndOfLaneCar, new CarPosition {Value = 0});
                         }
                     }
                     else
                     {
-                        // TODO: MBRIAU: Still make that car accelerate but cap the normalized speed to 0.7f while in an intersection (Look at Car.cs)
-                        // TODO: MBRIAU: We also need to make the first car of each input lane slowdown since the intersection is busy
-                        DynamicBuffer<CarBufferElement> laneOutCars = GetBuffer<CarBufferElement>(laneOuts[i]);
-                        laneOutCars.Add(cars[i]);
-                        SetComponent(cars[i], new CarPosition {Value = 0});
-                        if (i == 0)
-                            doubleIntersection.car0 = Entity.Null;
-                        else
-                            doubleIntersection.car1 = Entity.Null;
+                        if (cars[i] != Entity.Null)
+                        {
+                            var car0Pos = GetComponent<CarPosition>(cars[i]);
+                            if (car0Pos.Value < 1)
+                            {
+                                var carSpeed = GetComponent<CarSpeed>(cars[i]);
+                                float newPosition = car0Pos.Value + carSpeed.NormalizedValue * CarSpeed.MAX_SPEED * deltaTime;
+                                SetComponent(cars[i], new CarPosition {Value = newPosition});
+                                var eval = BezierUtility.EvaluateBezier(splines[i].startPos, splines[i].anchor1, splines[i].anchor2,
+                                    splines[i].endPos, newPosition);
+                                SetComponent(cars[i], new Translation {Value = eval});
+                            }
+                            else
+                            {
+                                // TODO: MBRIAU: Still make that car accelerate but cap the normalized speed to 0.7f while in an intersection (Look at Car.cs)
+                                // TODO: MBRIAU: We also need to make the first car of each input lane slowdown since the intersection is busy
+                                DynamicBuffer<CarBufferElement> laneOutCars = GetBuffer<CarBufferElement>(laneOuts[i]);
+                                laneOutCars.Add(cars[i]);
+                                SetComponent(cars[i], new CarPosition {Value = 0});
+                                
+                                if (i == 0)
+                                    doubleIntersection.car0 = Entity.Null;
+                                else
+                                    doubleIntersection.car1 = Entity.Null;
+                            }
+                        }
                     }
                 }
 
---
+++ Ported/MagneticRoads/Assets/MagnetoRoads/Source/System/IntersectionSystems/Init/SimpleIntersectionSplineSetupSystem.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+
+public class SimpleIntersectionSplineSetupSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var ecb = new EntityCommandBuffer(Allocator.TempJob);
+        
+        Entities
+            .WithNone<Spline>()
+            .ForEach((Entity entity, ref SimpleIntersection simpleIntersection) =>
+        {
+            var splineIn = GetComponent<Spline>(simpleIntersection.laneIn0);
+            var inPos = splineIn.endPos;
+            var splineOut = GetComponent<Spline>(simpleIntersection.laneOut0);
+            var outPos = splineOut.startPos;
+
+            var inToOut = outPos - inPos;
+            
+            var spline = new Spline
+            {
+                startPos = inPos,
+                anchor1 = inPos + inToOut * 0.33f,
+                anchor2 = inPos + inToOut * 0.66f,
+                endPos = outPos
+            };
+
+            ecb.AddComponent(entity, spline);
+            
+        }).Run();
+        
+        ecb.Playback(EntityManager);
+        ecb.Dispose();
+    }
+}
