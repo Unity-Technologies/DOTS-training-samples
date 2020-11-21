643bec0be520cb3c60fe8e4414e75aa8983f66dd
Barbara Wang <barbara.wang@unity3d.com>
2020-11-20 19:48:20 -0500

    Backup Field Container

---
+++ Ported/CombatBees/Assets/Scripts/Authoring/FieldAuthoring.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct FieldAuthoring : IComponentData
+{
+    public float3 size;
+    public float gravity;
+}
---
+++ Ported/CombatBees/Assets/Scripts/Systems/CarMovementSystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+
+public class CarMovementSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        // Time is a field of SystemBase, and SystemBase is a class. This prevents
+        // using it in a job, so we have to fetch ElapsedTime and store it in a local
+        // variable. This local variable can then be used in the job.
+        var time = Time.ElapsedTime;
+
+        // Entities.ForEach is a job generator, the lambda it contains will be turned
+        // into a proper IJob by IL post processing.
+        Entities
+            .ForEach((ref Translation translation) =>
+            {
+                translation.Value.x = (float)time;
+            }).ScheduleParallel();
+    }
+}
