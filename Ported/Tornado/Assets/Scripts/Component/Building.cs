df1b3006d635d62e5426017b4b72108243040fae
Coleen <coleen.turquin@unity3d.com>
2020-12-08 09:56:01 +0100

    Added Building component

---
+++ Ported/Tornado/Assets/Scripts/Component/Building.cs
@@ -0,0 +1,7 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+
+public struct Building : IComponentData
+{
+    public NativeArray<Constraint> Constraints;
+}
--- Ported/Tornado/Assets/Scripts/Component/Node.cs
+++ Ported/Tornado/Assets/Scripts/Component/Node.cs
@@ -1,5 +1,4 @@
 ﻿using Unity.Entities;
-using Unity.Mathematics;
 
 public struct Node : IComponentData
 {
