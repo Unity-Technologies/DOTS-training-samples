4e2c83e66aa26c3bd54e188c3569b117559bf7b5
Remy Maetz <remym@unity3d.com>
2020-12-08 15:06:43 +0100

    Add the base code for bar spawning

---
+++ Ported/Tornado/Assets/Scripts/Components/BarSpawner.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public class BarSpawner : IComponentData
+{
+    public Entity barPrefab;
+}
---
+++ Ported/Tornado/Assets/Scripts/Systems/BarSpawningSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class BarSpawningSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var ecb = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
+
+        //Entities.ForEach(Entity entity, in BarSpawner spawner ) => { }.Run();
+    }
+}
