8318cbf5593867831e088d4a55a3a967d43d2dc2
TomasKiniulis <tomas.kiniulis@unity3d.com>
2020-12-08 11:50:24 +0200

    tornado particle spawning

---
+++ Ported/Tornado/Assets/Scripts/Systems/ParticleSpawnerAuthoring.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSpawnerAuthoring : MonoBehaviour
+{
+	public Mesh particleMesh;
+	public Material particleMaterial;
+	public float spinRate;
+	public float upwardSpeed;
+	Vector3[] points;
+	Matrix4x4[] matrices;
+	MaterialPropertyBlock matProps;
+	float[] radiusMults;
+
+	private void Start()
+	{
+		points = new Vector3[1000];
+		matrices = new Matrix4x4[1000];
+		radiusMults = new float[1000];
+		Vector4[] colors = new Vector4[1000];
+
+		for (int i = 0; i < points.Length; i++)
+		{
+			Vector3 pos = new Vector3(Random.Range(-50f, 50f), Random.Range(0f, 50f), Random.Range(-50f, 50f));
+			points[i] = pos;
+			matrices[i] = Matrix4x4.TRS(points[i], Quaternion.identity, Vector3.one * Random.Range(.2f, .7f));
+			radiusMults[i] = Random.value;
+			colors[i] = Color.white * Random.Range(.3f, .7f);
+		}
+		matProps = new MaterialPropertyBlock();
+		matProps.SetVectorArray("_Color", colors);
+	}
+}
---
+++ Ported/Tornado/Assets/Scripts/Systems/ParticleSpawning.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class ParticleSpawning : SystemBase
+{
+
+    protected override void OnUpdate()
+    {
+        Random random = new Random(1234);
+
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+
+        Entities.ForEach((Entity entity, in ParticleSpawner particleSpawner) =>
+        {
+            for (int i = 0; i < particleSpawner.ParticleCount; i++)
+            {
+                var instance = ecb.Instantiate(particleSpawner.ParticlePrefab);
+                var translation = new Translation { Value = new float3(random.NextFloat(-50f, 50f), random.NextFloat(0, 50f), random.NextFloat(-50f, 50f)) };
+                ecb.AddComponent(instance, typeof(Particle));
+                ecb.SetComponent(instance, translation);
+            }
+        }).Run();
+
+        ecb.Playback(EntityManager);
+    }
+}
