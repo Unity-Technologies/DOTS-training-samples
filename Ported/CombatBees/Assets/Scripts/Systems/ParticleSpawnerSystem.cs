b059fd880a152490491fc94ca07e950d98225490
Barbara Wang <barbara.wang@unity3d.com>
2020-12-10 18:56:02 -0500

    Add ParticelSpawnerSystem.

--- Ported/CombatBees/Assets/Scripts/Systems/ParticleSpawnerSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/ParticleSpawnerSystem.cs
@@ -5,14 +5,25 @@ using Unity.Rendering;
 using Unity.Transforms;
 using UnityEngine;
 
-#if COMMENT
+/*
+public struct ParticleSpawner : IComponentData
+{
+    //public Entity bloodPrefab;
+    //public Entity flashPrefab;
+    public int count;
+    public ParticleType.Type type;
+    public float3 velocity;
+    public float velocityJitter;
+}
+*/
+
 public class ParticleSpawnerSystem : SystemBase
 {
     protected override void OnUpdate()
     {
+        var particleParams = GetSingleton<ParticleParams>();
         var random = new Unity.Mathematics.Random(1234789);
 
-        /*
         var ecb = new EntityCommandBuffer(Allocator.TempJob);
         Entities
             .WithName("Particle_Spawner")
@@ -23,12 +34,12 @@ public class ParticleSpawnerSystem : SystemBase
                     Entity particle;
                     float3 velocity;
                     float lifeDuration;
-                    float3 size;
+                    float size;
                     float4 baseColor;
 
                     if (particleSpawner.type == ParticleType.Type.Blood)
                     {
-                        particle = ecb.Instantiate(particleSpawner.bloodPrefab);
+                        particle = ecb.Instantiate(particleParams.bloodPrefab);
 
                         velocity = particleSpawner.velocity + random.NextFloat3() * particleSpawner.velocityJitter;
                         ecb.AddComponent(particle, new Velocity { vel = velocity });
@@ -36,10 +47,8 @@ public class ParticleSpawnerSystem : SystemBase
                         lifeDuration = random.NextFloat(3f, 5f);
                         ecb.AddComponent(particle, new LifeDuration { vel = lifeDuration });
 
-                        size = new float3(random.NextFloat(.1f, .2f), 
-                                            random.NextFloat(.1f, .2f), 
-                                            random.NextFloat(.1f, .2f));
-                        ecb.AddComponent(particle, new NonUniformScale { Value = size });
+                        size = random.NextFloat(.1f, .2f);
+                        ecb.AddComponent(particle, new NonUniformScale { Value = new float3(size, size, size) });
 
                         Color color = UnityEngine.Random.ColorHSV(-.05f, .05f, .75f, 1f, .3f, .8f);
                         baseColor.x = color.r;
@@ -50,7 +59,7 @@ public class ParticleSpawnerSystem : SystemBase
                     }
                     else
                     {
-                        particle = ecb.Instantiate(particleSpawner.flashPrefab);
+                        particle = ecb.Instantiate(particleParams.flashPrefab);
 
                         velocity = random.NextFloat3() * 5f;
                         ecb.AddComponent(particle, new Velocity { vel = velocity });
@@ -58,10 +67,8 @@ public class ParticleSpawnerSystem : SystemBase
                         lifeDuration = random.NextFloat(.25f, .5f);
                         ecb.AddComponent(particle, new LifeDuration { vel = lifeDuration });
 
-                        size = new float3(random.NextFloat(1f, 2f),
-                                            random.NextFloat(1f, 2f),
-                                            random.NextFloat(1f, 2f));
-                        ecb.AddComponent(particle, new NonUniformScale { Value = size });
+                        size = random.NextFloat(1f, 2f);
+                        ecb.AddComponent(particle, new NonUniformScale { Value = new float3(size, size, size) });
 
                         baseColor = new float4(1f, 1f, 1f, 1f);
                         ecb.AddComponent(particle, new URPMaterialPropertyBaseColor { Value = baseColor });
@@ -73,10 +80,9 @@ public class ParticleSpawnerSystem : SystemBase
             }).Run();
         ecb.Playback(EntityManager);
         ecb.Dispose();
-        */
+        
 
     }
 
 }
-#endif
 
