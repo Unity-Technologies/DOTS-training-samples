0697eb8811f4997e96522f2f6e96f85eae4dd7a1
John Parsaie <johnpa@unity3d.com>
2020-12-15 14:33:58 -0500

    Initial farmer spawning

---
+++ Ported/AutoFarmers/Assets/Scripts/Components/FarmerSpawner.cs
@@ -0,0 +1,7 @@
+﻿using Unity.Entities;
+
+public struct FarmerSpawner : IComponentData
+{
+    public Entity FarmerPrefab;
+    public int FarmerCounter;
+}
---
+++ Ported/AutoFarmers/Assets/Scripts/Systems/FarmerSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using Unity.Entities;
+
+public class FarmerSpawnerSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+
+        Entities
+            .ForEach((Entity entity, ref FarmerSpawner spawner) =>
+            {
+                Debug.Log("In Entities.ForEach");
+                for (int i = 0; i < spawner.FarmerCounter; ++i)
+                {
+                    Debug.Log("In Farmer Spawn Loop");
+                    var instance = ecb.Instantiate(spawner.FarmerPrefab);
+                }
+
+                spawner.FarmerCounter = 0;
+            }).Run();
+        
+        ecb.Playback(EntityManager);
+    }
+}
