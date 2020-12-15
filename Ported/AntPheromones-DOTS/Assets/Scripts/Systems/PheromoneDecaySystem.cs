39532f6a815319e54ce743d29294372ad6b569de
John Futscher <john.futscher@unity3d.com>
2020-12-15 18:20:17 -0500

    pheromones updating in grid

---
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Systems/PheromoneDecaySystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class PheromoneDecaySystem : SystemBase
+{
+    public struct BufferJobExample: IJob
+    {
+        public BufferTypeHandle<Pheromones> pheromonesType;
+
+        [NativeDisableContainerSafetyRestriction]
+        public DynamicBuffer<Pheromones> pheromoneGrid;
+        
+        public void Execute()
+        {
+            for (int i = 0; i < pheromoneGrid.Length; i++)
+            {
+                float currentStrength = pheromoneGrid[i].pheromoneStrength;
+                
+                pheromoneGrid[i] = new Pheromones{pheromoneStrength = currentStrength - 100f};
+            }
+        }
+    }
+
+    protected override void OnUpdate()
+    {
+        Entity pheromoneEntity = GetSingletonEntity<Pheromones>();
+        DynamicBuffer<Pheromones> pheromoneGrid = EntityManager.GetBuffer<Pheromones>(pheromoneEntity);
+        
+        BufferTypeHandle<Pheromones> pheromoneBufferType = GetBufferTypeHandle<Pheromones>();
+
+        BufferJobExample job = new BufferJobExample
+        {
+            pheromonesType = pheromoneBufferType,
+            pheromoneGrid = pheromoneGrid,
+
+        };
+
+        Dependency = job.Schedule(Dependency);
+    }
+}
--- Ported/AntPheromones-DOTS/Assets/Scripts/Systems/PheromoneTrackingSystem.cs
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Systems/PheromoneTrackingSystem.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 public class PheromoneTrackingSystem : SystemBase
 {
-    protected override void OnCreate()
+    public struct BufferJobExample: IJobEntityBatch
     {
+        public BufferTypeHandle<Pheromones> pheromonesType;
+        [ReadOnly]
+        public ComponentTypeHandle<Translation> translationType;
+        [NativeDisableContainerSafetyRestriction]
+        public DynamicBuffer<Pheromones> pheromoneGrid;
         
-        
+        public void Execute(ArchetypeChunk batchInChunk, int batchIndex)
+        {
+            var translations = batchInChunk.GetNativeArray(translationType);
+            
+            /*for (int i = 0; i < batchInChunk.Count; i++)
+            {
+                var translation = translations[i];
+                float currentStrength = pheromoneGrid[i].pheromoneStrength;
+                
+                pheromoneGrid[0] = new Pheromones{pheromoneStrength = currentStrength + 10f};
+            }*/
+            
+            float currentStrength = pheromoneGrid[0].pheromoneStrength;
+                
+            pheromoneGrid[0] = new Pheromones{pheromoneStrength = currentStrength + 10f};
+        }
     }
 
     protected override void OnUpdate()
     {
-        
+        Entity pheromoneEntity = GetSingletonEntity<Pheromones>();
+        DynamicBuffer<Pheromones> pheromoneGrid = EntityManager.GetBuffer<Pheromones>(pheromoneEntity);
+
+        EntityQuery query = GetEntityQuery(typeof(Translation), typeof(Ant));
+        BufferTypeHandle<Pheromones> pheromoneBufferType = GetBufferTypeHandle<Pheromones>();
+        ComponentTypeHandle<Translation> translationType = GetComponentTypeHandle<Translation>(true);
+
+        BufferJobExample job = new BufferJobExample
+        {
+            pheromonesType = pheromoneBufferType,
+            pheromoneGrid = pheromoneGrid,
+            translationType = translationType
+        };
+
+        Dependency = job.Schedule(query, Dependency);
     }
 }
