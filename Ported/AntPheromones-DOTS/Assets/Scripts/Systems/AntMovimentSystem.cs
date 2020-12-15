32b512d2297374c9f05087419da5d9e5d24e955a
John Futscher <john.futscher@unity3d.com>
2020-12-15 15:42:22 -0500

    Time scaling updated

--- Ported/AntPheromones-DOTS/Assets/Scripts/Systems/AntMovimentSystem.cs
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Systems/AntMovimentSystem.cs
@@ -12,12 +12,17 @@ public class AntMovimentSystem : SystemBase
     protected override void OnUpdate()
     {
         float time = Time.DeltaTime;
-        var random = new Random(244);
+
+        float timeMultiplier = GetSingleton<TimeMultiplier>().SimulationSpeed;
+
+        float scaledTime = time * timeMultiplier;
         
         Entities.WithAll<Ant>().ForEach((ref Translation translation, ref Rotation rotation, in Heading heading) =>
         {
-            translation.Value.x += heading.heading.x * time;
-            translation.Value.y += heading.heading.y * time;
+            rotation.Value = quaternion.RotateZ(math.atan2(heading.heading.y, heading.heading.x));
+            
+            translation.Value.x += heading.heading.x * scaledTime;
+            translation.Value.y += heading.heading.y * scaledTime;
         }).ScheduleParallel();
     }
 }
