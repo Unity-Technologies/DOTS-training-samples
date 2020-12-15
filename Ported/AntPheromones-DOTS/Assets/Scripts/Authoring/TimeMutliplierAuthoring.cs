e356fd95a3194fca09edf20ea88490f9706860e8
jfutscher <jellyfutch@gmail.com>
2020-12-15 11:48:52 -0500

    Initial Time setup authoring

---
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Authoring/TimeMutliplierAuthoring.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class TimeMutliplierAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public float StartingSimulationSpeed;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var timeMulitplier = new TimeMultiplier{SimulationSpeed = StartingSimulationSpeed};
+
+        dstManager.AddComponentData(entity, timeMulitplier);
+    }
+}
---
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Systems/TimeManagementSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class TimeManagementSystem : SystemBase
+{
+
+    protected override void OnUpdate()
+    {
+        var systemSpeed = GetSingletonEntity<TimeMultiplier>();
+        var timeMultiplier = GetComponent<TimeMultiplier>(systemSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) timeMultiplier.SimulationSpeed = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) timeMultiplier.SimulationSpeed = 2;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) timeMultiplier.SimulationSpeed = 3;
+        if (Input.GetKeyDown(KeyCode.Alpha4)) timeMultiplier.SimulationSpeed = 4;
+        if (Input.GetKeyDown(KeyCode.Alpha5)) timeMultiplier.SimulationSpeed = 5;
+        if (Input.GetKeyDown(KeyCode.Alpha6)) timeMultiplier.SimulationSpeed = 6;
+        if (Input.GetKeyDown(KeyCode.Alpha7)) timeMultiplier.SimulationSpeed = 7;
+        if (Input.GetKeyDown(KeyCode.Alpha8)) timeMultiplier.SimulationSpeed = 8;
+        if (Input.GetKeyDown(KeyCode.Alpha9)) timeMultiplier.SimulationSpeed = 9;
+
+        SetComponent(systemSpeed, timeMultiplier);
+    }
+}
