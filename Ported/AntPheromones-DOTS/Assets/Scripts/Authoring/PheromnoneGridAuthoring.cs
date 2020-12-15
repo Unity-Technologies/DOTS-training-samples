b04161b9150aa33f200a2e6497f1344be5388ef3
John Futscher <john.futscher@unity3d.com>
2020-12-15 16:51:31 -0500

    pheromone grid build

---
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Authoring/PheromnoneGridAuthoring.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PheromnoneGridAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public int gridX;
+    public int gridY;
+    
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var pheromoneBuffer = dstManager.AddBuffer<Pheromones>(entity);
+        pheromoneBuffer.EnsureCapacity(gridX * gridY);
+        for (int i = 0; i < gridX; i++)
+        {
+            for (int j = 0; j < gridY; j++)
+            {
+                pheromoneBuffer.Add(new Pheromones {pheromoneStrength = 0});
+            }
+        }
+    }
+}
---
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Systems/PheromoneTrackingSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class PheromoneTrackingSystem : SystemBase
+{
+    protected override void OnCreate()
+    {
+        
+        
+    }
+
+    protected override void OnUpdate()
+    {
+        
+    }
+}
