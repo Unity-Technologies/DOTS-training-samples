1d1b359d025121cb90e80ccd0d32d7541104b967
Adam Blahuta <adam.blahuta@unity3d.com>
2020-12-15 10:42:07 -0500

    First set of known Components

---
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Components/FloatGrid.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+public struct FloatGrid : IComponentData
+{
+	public DynamicBuffer<float> cells;
+	public int width;
+
+	public float this[int x, int y] => cells[y * width + x];
+}
---
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Components/Heading.cs
@@ -0,0 +1,7 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct Heading : IComponentData
+{
+	public float2 heading;
+}
