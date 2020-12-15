37a2c1beb1f70894465d8c73f5cddf9b974dbb87
Andrew Parsons <mrandypuppy@gameparents.com>
2020-12-15 08:56:25 -0800

    Initial Simulation settings

---
+++ Ported/DOTSBucketBrigade/Assets/src/SimController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimController : MonoBehaviour
+{
+    [Header("BOARD")]
+    public int rows = 50;
+    public int columns = 50;
+
+    [Header("COLORS")]
+    public Color color_ground;
+    public Color color_fire_low;
+    public Color color_fire_high;
+    public Color color_bucket_empty;
+    public Color color_bucket_full;
+    public Color color_role_scooper;
+    public Color color_role_thrower;
+    public Color color_role_passer_full;
+    public Color color_role_passer_empty;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
