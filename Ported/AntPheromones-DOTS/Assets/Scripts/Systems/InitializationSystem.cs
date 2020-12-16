12ebde1070fb230e7c0842bed660fc105b1058c7
Rohit Moni <rohit.moni@unity3d.com>
2020-12-16 12:17:18 -0500

    Tweaking Init system, adding Reset system

--- Ported/AntPheromones-DOTS/Assets/Scripts/Systems/InitializationSystem.cs
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Systems/InitializationSystem.cs
@@ -1,29 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections;
+﻿using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
-using UnityEngine;
 using Random = Unity.Mathematics.Random;
 
 public class InitializationSystem : SystemBase
 {
-    
-    
     protected override void OnUpdate()
     {
         var ecb = new EntityCommandBuffer(Allocator.Temp);
         
         var random = new Random(6541);
+        var center = new Translation{ Value = new float3(64, 64, 0) };
+        var minRange = new float2(-1,-1);
+        var maxRange = new float2(1,1);
         
         Entities
             .ForEach((Entity entity, in Init init) =>
             {
-                
                 ecb.DestroyEntity(entity);
 
-                for (int i = 0; i < init.antCount; i++)
+                // Create Board
+                
+                // Create Ants
+                for (var i = 0; i < init.antCount; i++)
                 {
                     var ant = ecb.Instantiate(init.antPrefab);
                     var translation = new Translation{Value = new float3(64,64, 0)};
@@ -31,11 +31,21 @@ public class InitializationSystem : SystemBase
                     
                     ecb.SetComponent(ant, new Heading
                     {
-                        heading = math.normalize(new float2( random.NextFloat(-1,1), random.NextFloat(-1, 1)))
+                        heading = math.normalize(random.NextFloat2(minRange, maxRange))
                     });
                 }
+                
+                // Create Home
+                
+                
+                // Create Food
+                
+                // Create Obstacles
+                
             }).Run();
-        
+
         ecb.Playback(EntityManager);
+        
+        ecb.Dispose();
     }
 }
