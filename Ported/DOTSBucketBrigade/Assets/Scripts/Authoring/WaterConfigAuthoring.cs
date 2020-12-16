eacfeae8f8d2a4eddb9a53aa8ea7bdcc19ff857f
Andrew Parsons <mrandypuppy@gameparents.com>
2020-12-16 09:47:40 -0800

    Basic River/Lake sources

---
+++ Ported/DOTSBucketBrigade/Assets/Scripts/Authoring/WaterConfigAuthoring.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class WaterConfigAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+    public GameObject m_WaterPrefab;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity,
+            new WaterSpawner
+            {
+                Prefab = conversionSystem.GetPrimaryEntity(m_WaterPrefab)
+            });
+    }
+
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+        referencedPrefabs.Add(m_WaterPrefab);
+    }
+}
---
+++ Ported/DOTSBucketBrigade/Assets/Scripts/Components/Water.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+
+    public float volume = 10.0f;
+    public float capacity = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        capacity = volume; 
+    }
+
+    private void Update()
+    {
+        if (volume < 0.5f)
+            DepleteWaterSource();
+    }
+    public bool ScoopWater(float scoopAmount)
+    {
+        if (scoopAmount > volume)
+            return false;
+
+        volume -= scoopAmount;
+        return true;
+    }
+
+    private void DepleteWaterSource()
+    {
+        Destroy(this.gameObject);
+    }
+}
