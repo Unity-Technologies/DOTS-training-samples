f8c2ff973296b5f9310160bd0bb56c75f4eb0bac
Diego Ossa <diego.ossa@unity3d.com>
2020-12-16 16:57:08 -0500

    SmashingRocks

--- Ported/AutoFarmers/Assets/Scripts/Systems/RockDestroying.cs
+++ Ported/AutoFarmers/Assets/Scripts/Systems/RockDestroying.cs
@@ -8,19 +8,25 @@ public class RockDestroying : SystemBase
     protected override void OnUpdate()
     {
         var ecb = new EntityCommandBuffer(Allocator.Temp);
+        var settingsEntity = GetSingletonEntity<Settings>();
         var tileBufferAccessor = GetBufferFromEntity<TileState>();
+        var settings = GetComponentDataFromEntity<Settings>()[settingsEntity];
         
         Entities.ForEach((Entity entity, ref Rock rock, ref NonUniformScale nonUniformScale) =>
             {
+                var tileBuffer = tileBufferAccessor[settingsEntity];
+                
                 nonUniformScale.Value = new float3(nonUniformScale.Value.x, rock.Health, nonUniformScale.Value.z);
                 if (rock.Health <= 0)
                 {
                     ecb.DestroyEntity(entity);
-                    for (int i = rock.Position.x; i < rock.Position.x + rock.Size.x; i++)
+                    for (int x = rock.Position.x; x < rock.Position.x + rock.Size.x; x++)
                     {
-                        for (int j = rock.Position.y; j < rock.Position.y + rock.Size.y; j++)
+                        for (int y = rock.Position.y; y < rock.Position.y + rock.Size.y; y++)
                         {
-                            //tileBufferAccessor[]
+                            var linearIndex = x + y * settings.GridSize.x;
+                            tileBuffer[linearIndex] = new TileState { Value = TileStates.Empty};
+                            //var state = tileBuffer[linearIndex].Value;
                         }
                     }
                 }
--- Ported/AutoFarmers/Assets/Scripts/Systems/SmashRocksSystem.cs
+++ Ported/AutoFarmers/Assets/Scripts/Systems/SmashRocksSystem.cs
@@ -1,17 +1,45 @@
-﻿using Unity.Entities;
+﻿using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
 
 [UpdateAfter(typeof(FarmerIntentionSystem))]
 public class SmashRocksSystem : SystemBase
 {
     protected override void OnUpdate()
     {
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
         var deltaTime = Time.DeltaTime;
 
-        /*Entities.WithAll<Farmer>().WithNone<Searching>()
-            .ForEach((Entity entity, ref SmashRocks smashRocks) =>
+        //Testing
+        var rocks = GetEntityQuery(typeof(Rock)).ToEntityArray(Allocator.Temp);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("SmashRock added");
+            Entities.WithAll<Farmer>()
+                .ForEach((Entity entity, ref SmashRocks smashRocks) => { smashRocks.TargetRock = rocks[0]; })
+                .WithoutBurst().Run();
+        }
+
+        var entityManager = EntityManager;
+
+        Entities.WithAll<Farmer>().WithNone<Searching>()
+            .ForEach((Entity entity, in SmashRocks smashRocks) =>
             {
-                var rock = EntityManager.GetComponentData<Rock>(smashRocks.TargetRock);
-                rock.Health -= 0.1f * deltaTime;
-            }).WithoutBurst().Run();*/
+                if (entityManager.Exists(smashRocks.TargetRock))
+                {
+                    var rock = GetComponent<Rock>(smashRocks.TargetRock);
+                    // var rock = entityManager.GetComponentData<Rock>(smashRocks.TargetRock);
+                    var health = rock.Health - (0.5f * deltaTime);
+                    ecb.SetComponent(smashRocks.TargetRock,
+                        new Rock {Health = health, Position = rock.Position, Size = rock.Size});
+    
+                    if (rock.Health <= 0)
+                    {
+                        ecb.RemoveComponent<SmashRocks>(entity);
+                    }
+                }
+            }).Run();
+
+        ecb.Playback(EntityManager);
     }
 }
