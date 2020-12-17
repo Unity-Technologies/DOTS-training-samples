87610d54b82dc43740bf3050f83a2f97c83054ea
Artur Vidal <artur.vidalmaia@unity3d.com>
2020-12-17 09:08:25 -0500

    Removed debug.log

--- Ported/AutoFarmers/Assets/Scripts/Systems/InputSystem.cs
+++ Ported/AutoFarmers/Assets/Scripts/Systems/InputSystem.cs
@@ -22,22 +22,10 @@ public class InputSystem : SystemBase
                var farmerPosition = new int2((int)math.floor(translation.Value.x), 
                    (int)math.floor(translation.Value.z));
 
-               if (pathNodes.Length <= 1)
+               if (pathNodes.Length == 0)
                {
-                   Debug.Log("Assigning new path");
                   var result = pathMovement.FindNearbyRock(farmerPosition.x, farmerPosition.y, 3600, tileBuffer, pathNodes);
-                   Debug.Log(result);
                }
-               /*var targetPosition = farmerPosition;
-               if (Input.GetKeyDown(KeyCode.UpArrow)) targetPosition.y += 1;
-               if (Input.GetKeyDown(KeyCode.DownArrow)) targetPosition.y -= 1;
-               if (Input.GetKeyDown(KeyCode.RightArrow)) targetPosition.x += 1;
-               if (Input.GetKeyDown(KeyCode.LeftArrow)) targetPosition.x -= 1;
-
-               targetPosition = math.clamp(targetPosition, 0, settings.GridSize);
-
-               if (math.length(farmerPosition) != math.length(targetPosition)) 
-                   pathNodes.Insert(0,new PathNode { Value = targetPosition });*/
            }).WithoutBurst().Run();
     }
 }
