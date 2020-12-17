6c74af2befa8bbe7ca35cdd128633b17588fd569
John Parsaie <johnpa@unity3d.com>
2020-12-17 11:14:42 -0500

    Snap camera to farmer on first update

--- Ported/AutoFarmers/Assets/Scripts/Systems/CameraSystem.cs
+++ Ported/AutoFarmers/Assets/Scripts/Systems/CameraSystem.cs
@@ -5,6 +5,8 @@ using Unity.Transforms;
 [UpdateAfter(typeof(FarmInitializeSystem))]
 public class CameraSystem : SystemBase
 {
+    private static bool k_SnapCameraOnFirstUpdate = true;
+    
     protected override void OnUpdate()
     {
         var settings = GetSingleton<CommonSettings>();
@@ -14,8 +16,16 @@ public class CameraSystem : SystemBase
         
         var camera = this.GetSingleton<GameObjectRefs>().Camera;
         
-        var cameraPosition = camera.transform.position;
-        camera.transform.position = math.lerp(cameraPosition, targetTranslation.Value + settings.CameraOffset, settings.CameraDamping);
-        camera.transform.LookAt(math.lerp(cameraPosition, targetTranslation.Value, settings.CameraDamping));
+        var cameraPosition = new float3(camera.transform.position);
+        
+        var damping = settings.CameraDamping;
+        if (k_SnapCameraOnFirstUpdate)
+        {
+            k_SnapCameraOnFirstUpdate = false;
+            damping = 1f;
+        }
+
+        camera.transform.position = math.lerp(cameraPosition, targetTranslation.Value + settings.CameraOffset, damping);
+        camera.transform.LookAt(math.lerp(cameraPosition, targetTranslation.Value, damping));
     }
 }
