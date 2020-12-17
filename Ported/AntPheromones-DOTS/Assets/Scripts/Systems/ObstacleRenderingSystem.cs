ca362217af7924afa2fee3cc8f91088d7175de13
Buthaina Mahmoud <buthaina.mahmoud@unity3d.com>
2020-12-17 10:48:25 -0800

    Add Obstacle Drawing to texture (for debugging)

---
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Systems/ObstacleRenderingSystem.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ObstacleRenderingSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entity obstacleEntity = GetSingletonEntity<ObstacleBufferElement>();
+        DynamicBuffer<ObstacleBufferElement> obstacleGrid = EntityManager.GetBuffer<ObstacleBufferElement>(obstacleEntity);
+        
+        Entity trailTextureEntity = GetSingletonEntity<TrailTexture>();
+        TrailTexture trailTexture = EntityManager.GetComponentData<TrailTexture>(trailTextureEntity);
+        
+        Color[] pixels = trailTexture.texture.GetPixels();
+        
+        for (int i = 0; i < obstacleGrid.Length; i++)
+        {
+            if (!obstacleGrid[i].present)
+                continue;
+            pixels[i] = new Color(0f, 0f, 0f);
+        }
+        
+        trailTexture.texture.SetPixels(pixels);
+        trailTexture.texture.Apply();
+    }
+}
