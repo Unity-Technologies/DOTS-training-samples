bb622fb4bcae0fe1197195a417da507d3e1de7e1
Artur Vidal <artur.vidalmaia@unity3d.com>
2020-12-16 22:46:28 -0500

    Added pathing to the PathMovement system

--- Ported/AutoFarmers/Assets/Scripts/Systems/InputSystem.cs
+++ Ported/AutoFarmers/Assets/Scripts/Systems/InputSystem.cs
@@ -12,7 +12,8 @@ public class InputSystem : SystemBase
         
         var tileBuffer = GetBufferFromEntity<TileState>()[data];
         var pathBuffers = this.GetBufferFromEntity<PathNode>();
-        
+        var pathMovement = World.GetExistingSystem<PathMovement>();
+
         Entities.WithAll<Farmer>()
            .ForEach((Entity entity, ref Velocity speed, in Translation translation) =>
            {
@@ -21,8 +22,12 @@ public class InputSystem : SystemBase
                var farmerPosition = new int2((int)math.floor(translation.Value.x), 
                    (int)math.floor(translation.Value.z));
 
-               if (pathNodes.Length == 0)
-                   Pathing.FindNearbyRock(pathNodes, tileBuffer, farmerPosition, 100, settings);
+               if (pathNodes.Length <= 1)
+               {
+                   Debug.Log("Assigning new path");
+                  var result = pathMovement.FindNearbyRock(farmerPosition.x, farmerPosition.y, 3600, tileBuffer, pathNodes);
+                   Debug.Log(result);
+               }
                /*var targetPosition = farmerPosition;
                if (Input.GetKeyDown(KeyCode.UpArrow)) targetPosition.y += 1;
                if (Input.GetKeyDown(KeyCode.DownArrow)) targetPosition.y -= 1;
@@ -33,6 +38,6 @@ public class InputSystem : SystemBase
 
                if (math.length(farmerPosition) != math.length(targetPosition)) 
                    pathNodes.Insert(0,new PathNode { Value = targetPosition });*/
-           }).Run();
+           }).WithoutBurst().Run();
     }
 }
