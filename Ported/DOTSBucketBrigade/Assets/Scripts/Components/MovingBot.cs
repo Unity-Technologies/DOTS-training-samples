d21f33b639bcd6f040274dcc4506ac38c734f174
jed.lang <jed.lang@unity3d.com>
2020-12-17 14:24:49 -0700

    Added MovingBot component

---
+++ Ported/DOTSBucketBrigade/Assets/Scripts/Components/MovingBot.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct MovingBot : IComponentData
+{
+    public float2 StartPosition;
+    public float2 TargetPosition;
+    public float StartTime;
+}
