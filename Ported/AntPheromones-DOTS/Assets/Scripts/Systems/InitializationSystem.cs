82ec3993e999d0bcb425cc62dd1777d36bc47b92
Buthaina Mahmoud <buthaina.mahmoud@unity3d.com>
2020-12-17 14:58:48 -0800

    Add LineOfSight Init

--- Ported/AntPheromones-DOTS/Assets/Scripts/Systems/InitializationSystem.cs
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Systems/InitializationSystem.cs
@@ -11,22 +11,26 @@ public class InitializationSystem : SystemBase
     protected override void OnUpdate()
     {
         var ecb = new EntityCommandBuffer(Allocator.Temp);
-        
+
         var random = new Random(2465);
-        var center = new Translation{ Value = new float3(64, 64, 0) };
-        var minRange = new float2(-1,-1);
-        var maxRange = new float2(1,1);
+        var center = new Translation {Value = new float3(64, 64, 0)};
+        var minRange = new float2(-1, -1);
+        var maxRange = new float2(1, 1);
         var bottomLeftFood = new Translation {Value = new float3(10, 10, 0)};
         var topLeftFood = new Translation {Value = new float3(10, 118, 0)};
         var bottomRightFood = new Translation {Value = new float3(118, 10, 0)};
         var topRightFood = new Translation {Value = new float3(118, 118, 0)};
-        
+
         Entity obstacleEntity = GetSingletonEntity<ObstacleBufferElement>();
-        DynamicBuffer<ObstacleBufferElement> obstacleGrid = EntityManager.GetBuffer<ObstacleBufferElement>(obstacleEntity);
-        
-        
+        DynamicBuffer<ObstacleBufferElement> obstacleGrid =
+            EntityManager.GetBuffer<ObstacleBufferElement>(obstacleEntity);
+
+        Entity lineOfSightEntity = GetSingletonEntity<LineOfSightBufferElement>();
+        DynamicBuffer<LineOfSightBufferElement> lineOfSightGrid =
+            EntityManager.GetBuffer<LineOfSightBufferElement>(lineOfSightEntity);
+
         Entities
-            // .WithoutBurst()
+            .WithoutBurst()
             .ForEach((Entity entity, in Init init) =>
             {
                 ecb.DestroyEntity(entity);
@@ -34,25 +38,25 @@ public class InitializationSystem : SystemBase
                 // Create Board
                 var board = ecb.Instantiate(init.boardPrefab);
                 ecb.SetComponent(board, center);
-                
+
                 // Create Ants
                 for (var i = 0; i < init.antCount; i++)
                 {
                     var ant = ecb.Instantiate(init.antPrefab);
                     ecb.SetComponent(ant, center);
-                    
+
                     ecb.SetComponent(ant, new Heading
                     {
                         heading = math.normalize(random.NextFloat2(minRange, maxRange))
                     });
                 }
-                
+
                 // Create Home
                 var home = ecb.Instantiate(init.homePrefab);
                 ecb.SetComponent(home, center);
 
                 // Create Food
-                var randomFoodPosIndex = random.NextInt(0, 4);
+                var randomFoodPosIndex = 0;// random.NextInt(0, 4);
                 var food = ecb.Instantiate(init.goalPrefab);
                 switch (randomFoodPosIndex)
                 {
@@ -93,23 +97,23 @@ public class InitializationSystem : SystemBase
                             Value = new float3(64f + math.cos(angle) * ringRadius,
                                 64f + math.sin(angle) * ringRadius, 0)
                         };
-                        
+
                         int indexInObstacleGrid = (((int) translation.Value.y) * 128) + ((int) translation.Value.x);
-                        
+
                         obstacleGrid[indexInObstacleGrid] = new ObstacleBufferElement {present = true};
                         int boardWidth = 128;
-                            
+
                         int up = indexInObstacleGrid + boardWidth;
                         obstacleGrid[up] = new ObstacleBufferElement {present = true};
                         int down = indexInObstacleGrid - boardWidth;
                         obstacleGrid[down] = new ObstacleBufferElement {present = true};
-                        int upRight = indexInObstacleGrid + boardWidth +1;
+                        int upRight = indexInObstacleGrid + boardWidth + 1;
                         obstacleGrid[upRight] = new ObstacleBufferElement {present = true};
-                        int upLeft = indexInObstacleGrid + boardWidth -1;
+                        int upLeft = indexInObstacleGrid + boardWidth - 1;
                         obstacleGrid[upLeft] = new ObstacleBufferElement {present = true};
-                        int downRight = indexInObstacleGrid - boardWidth +1;
+                        int downRight = indexInObstacleGrid - boardWidth + 1;
                         obstacleGrid[downRight] = new ObstacleBufferElement {present = true};
-                        int downLeft = indexInObstacleGrid - boardWidth -1;
+                        int downLeft = indexInObstacleGrid - boardWidth - 1;
                         obstacleGrid[downLeft] = new ObstacleBufferElement {present = true};
                         int right = indexInObstacleGrid + 1;
                         obstacleGrid[right] = new ObstacleBufferElement {present = true};
@@ -117,20 +121,48 @@ public class InitializationSystem : SystemBase
                         obstacleGrid[left] = new ObstacleBufferElement {present = true};
 
                         ecb.SetComponent(obstacle, translation);
-                        ecb.SetComponent(obstacle, new Radius{radius = 4});
-                        
+                        ecb.SetComponent(obstacle, new Radius {radius = 4});
                     }
-                    
                 }
                 
-                // create a camera that sees only the obstacles, 
-                // the output texture should match the grid
-                
+                // create LineOfSight grid
+                UpdateLineInDirection(bottomLeftFood, obstacleGrid, lineOfSightGrid, 128);
+                // int indexInLineOfSightGrid = (((int) bottomLeftFood.Value.y) * 128) + ((int) bottomLeftFood.Value.x);
+                // foreach (int index in GetLineInDirection(0, obstacleGrid, 128, indexInLineOfSightGrid))
+                //     lineOfSightGrid[index] = new LineOfSightBufferElement {present = true};
+
 
             }).Run();
 
         ecb.Playback(EntityManager);
-        
+
         ecb.Dispose();
     }
+
+    
+    
+    public static void UpdateLineInDirection(Translation start, DynamicBuffer<ObstacleBufferElement> obstacleGrid,
+        DynamicBuffer<LineOfSightBufferElement> lineOfSightGrid, int boardWidth)
+    {
+        int[] lineIndices = new int[boardWidth];
+        for (int i = 0; i < 360; i++)
+        {
+            float2 angle = new float2( math.cos(i),  math.sin(i));
+            for (int dist = 1; dist < boardWidth; dist++)
+            {
+                float2 posToCheck = new float2(start.Value.x, start.Value.y) + (angle * dist);
+                int indexInLineOfSightGrid = (((int) posToCheck.y) * 128) + ((int) posToCheck.x);
+                if (obstacleGrid[indexInLineOfSightGrid].present) 
+                    break;
+                lineOfSightGrid[indexInLineOfSightGrid] = new LineOfSightBufferElement {present = true};
+            }
+        }
+       
+        
+            
+    }
 }
+
+
+
+
