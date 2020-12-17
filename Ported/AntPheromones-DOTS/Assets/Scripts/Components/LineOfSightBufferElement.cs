34b571f6fbc5dbe844370b6d5dd534b5ebf0ee82
Buthaina Mahmoud <buthaina.mahmoud@unity3d.com>
2020-12-17 14:41:07 -0800

    Add LineOfSight (WIP)

---
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Components/LineOfSightBufferElement.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct LineOfSightBufferElement : IBufferElementData
+{
+    public bool present;
+}
---
+++ Ported/AntPheromones-DOTS/Assets/Scripts/Systems/LineOfSightRenderingSystem.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class LineOfSightRenderingSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        // Entity obstacleEntity = GetSingletonEntity<ObstacleBufferElement>();
+        // DynamicBuffer<ObstacleBufferElement> obstacleGrid = EntityManager.GetBuffer<ObstacleBufferElement>(obstacleEntity);
+        //
+        Entity lineOfSightEntity = GetSingletonEntity<LineOfSightBufferElement>();
+        DynamicBuffer<LineOfSightBufferElement> lineOfSightGrid =
+            EntityManager.GetBuffer<LineOfSightBufferElement>(lineOfSightEntity);
+        
+        Entity trailTextureEntity = GetSingletonEntity<TrailTexture>();
+        TrailTexture trailTexture = EntityManager.GetComponentData<TrailTexture>(trailTextureEntity);
+        
+        Color[] pixels = trailTexture.texture.GetPixels();
+        
+        for (int i = 0; i < lineOfSightGrid.Length; i++)
+        {
+            if (!lineOfSightGrid[i].present)
+                continue;
+            pixels[i] = new Color(0f, 1f, 0f);
+        }
+        
+        trailTexture.texture.SetPixels(pixels);
+        trailTexture.texture.Apply();
+    }
+}
