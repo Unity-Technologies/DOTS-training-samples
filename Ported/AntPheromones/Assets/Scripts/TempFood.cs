a8f337df60e84a53f3ed4a1c35191a707730a8aa
LP-389\Dave Forshaw <dave.forshaw@unity3d.com>
2021-01-27 16:37:15 -0500

    Pheromone following

--- Ported/AntPheromones/Assets/Scripts/TempFood.cs
+++ Ported/AntPheromones/Assets/Scripts/TempFood.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public struct TempFood
 {
-	public const float FOODPOSX = 15.0f;
-	public const float FOODPOSY = 15.0f;
-	public const float FOOD_LOS_Y = 5.0f;
+	public const float FOODPOSX = 50.0f;
+	public const float FOODPOSY = 50.0f;
+	public const float FOOD_LOS_Y = 40.0f;
 	public const float HOME_LOS_Y = 25.0f;
 	public const float GOALDIST = 1f;
 }
