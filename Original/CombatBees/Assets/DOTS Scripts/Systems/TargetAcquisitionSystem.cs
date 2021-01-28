37139e1acf0e65b728efe684ec7924ff7f772927
Arnaud Paré-Vogt <arnaud.parevogt@unity3d.com>
2021-01-28 11:40:42 -0500

    Bees target other bees

--- Original/CombatBees/Assets/DOTS Scripts/Systems/TargetAcquisitionSystem.cs
+++ Original/CombatBees/Assets/DOTS Scripts/Systems/TargetAcquisitionSystem.cs
@@ -1,5 +1,7 @@
-﻿using Unity.Collections;
+﻿using Packages.Rider.Editor.UnitTesting;
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Mathematics;
 using Random = Unity.Mathematics.Random;
 
 /// <summary>
@@ -9,6 +11,8 @@ using Random = Unity.Mathematics.Random;
 public class TargetAcquisitionSystem : SystemBase
 {
     EntityQuery m_AvailableFoodQuery;
+    EntityQuery m_Team1BeesQuery;
+    EntityQuery m_Team2BeesQuery;
     Random m_Random;
 
     protected override void OnCreate()
@@ -27,6 +31,8 @@ public class TargetAcquisitionSystem : SystemBase
         };
 
         m_AvailableFoodQuery = GetEntityQuery(availableFoodQueryDescription);
+        m_Team1BeesQuery = GetEntityQuery(typeof(BeeTag), typeof(Team1));
+        m_Team2BeesQuery = GetEntityQuery(typeof(BeeTag), typeof(Team2));
         m_Random = new Random(1234);
     }
 
@@ -34,29 +40,59 @@ public class TargetAcquisitionSystem : SystemBase
     {
         // Let's get all resources that are not currently carried
         var availableFood = m_AvailableFoodQuery.ToEntityArray(Allocator.Temp);
+        var team1Bees = m_Team1BeesQuery.ToEntityArray(Allocator.Temp);
+        var team2Bees = m_Team2BeesQuery.ToEntityArray(Allocator.Temp);
         
         var ecb = new EntityCommandBuffer(Allocator.Temp);
         var random = m_Random;
-        Entities
-            .WithNone<CarriedFood, MoveTarget, TargetPosition>()
-            .WithAll<BeeTag>()
-            .WithDisposeOnCompletion(availableFood)
-            .ForEach((Entity e) =>
-            {
-                if (availableFood.Length != 0)
+        if (availableFood.Length > 0 || team2Bees.Length > 0)
+        {
+            Entities
+                .WithNone<CarriedFood, MoveTarget, TargetPosition>()
+                .WithAll<BeeTag, Team1>()
+                .ForEach((Entity e) =>
                 {
-                    var targetIndex = random.NextInt(availableFood.Length);
-                    var target = availableFood[targetIndex];
-                    ecb.AddComponent(e, new MoveTarget
-                    {
-                        Value = target,
-                    });
-                    ecb.AddComponent<TargetPosition>(e);
-                }
-
-            }).Run();
+                    AcquireTarget(e, ecb, random, availableFood, team2Bees);
+                }).Run();
+        }
+        
+        if (availableFood.Length > 0 || team1Bees.Length > 0)
+        {
+            Entities
+                .WithNone<CarriedFood, MoveTarget, TargetPosition>()
+                .WithAll<BeeTag, Team2>()
+                .ForEach((Entity e) =>
+                {
+                    AcquireTarget(e, ecb, random, availableFood, team1Bees);
+                }).Run();
+        }
 
         ecb.Playback(EntityManager);
         ecb.Dispose();
+        team1Bees.Dispose();
+        team2Bees.Dispose();
+        availableFood.Dispose();
+    }
+
+    static void AcquireTarget(Entity bee, EntityCommandBuffer ecb, Random random, NativeArray<Entity> availableFood, NativeArray<Entity> targetBees)
+    {
+        var targetIndex = random.NextInt(availableFood.Length + targetBees.Length);
+        Entity target;
+        if (targetIndex < availableFood.Length)
+        {
+            target = availableFood[targetIndex];
+            ecb.AddComponent<FetchingFodTag>(bee);
+        }
+        else
+        {
+            target = targetBees[targetIndex - availableFood.Length];
+            ecb.AddComponent<AttackingBeeTag>(bee);
+        }
+        
+        ecb.AddComponent(bee, new MoveTarget
+        {
+            Value = target,
+        });
+        ecb.AddComponent<TargetPosition>(bee);
     }
 }
