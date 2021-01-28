6cd69b30de9d5aa4489e6d855f4def1539b0feb7
JuanRMarquez-Ortiz <juranr.marquezortiz@unity3d.com>
2021-01-28 12:25:00 -0500

    Bees attack other bees!

---
+++ Original/CombatBees/Assets/DOTS Scripts/Systems/AttackSystem.cs
@@ -0,0 +1,42 @@
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+[UpdateAfter(typeof(BeeMovementSystem))]
+public class AttackSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var ecb = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
+
+        // Entities.ForEach is a job generator, the lambda it contains will be turned
+        // into a proper IJob by IL post processing.
+        Entities
+            .WithName("Attack")
+            .WithAll<BeeTag, AttackingBeeTag>()
+            .ForEach((Entity e, ref Translation translation, ref TargetPosition t, in MoveTarget moveTarget) =>
+            {
+                if (MathUtil.IsWithinDistance(0.1f, t.Value, translation.Value))
+                {
+                    //We're close enough to the bee we want to attack. So destroy it. 
+                    ecb.DestroyEntity(moveTarget.Value);
+
+                    //Keep in mind you have to also have the targeted bee drop whatever food it's carrying. Or rather,
+                    //have the food recognize it's not being picked up anymore.
+                    if (HasComponent<CarriedFood>(moveTarget.Value))
+                    {
+                        var carriedFood = GetComponent<CarriedFood>(moveTarget.Value).Value;
+                        ecb.RemoveComponent<CarrierBee>(carriedFood);
+                    }
+
+                    //Remove targeting info from bee so that target acquisition system picks up this bee. 
+                    ecb.RemoveComponent<AttackingBeeTag>(e);
+                    ecb.RemoveComponent<MoveTarget>(e);
+                    ecb.RemoveComponent<TargetPosition>(e);
+                }
+            }).Run();
+
+        ecb.Playback(EntityManager);
+        ecb.Dispose();
+    }
+}
