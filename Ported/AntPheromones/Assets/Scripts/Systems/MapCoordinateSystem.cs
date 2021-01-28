ff86018a4de4f0f5e4d4048327adf371d5e96c69
Julian Halliday <julian@fingerfoodstudios.com>
2021-01-28 08:37:27 -0700

    Made a static function to convert ant positions to the pheromone dynamic buffer array index.

---
+++ Ported/AntPheromones/Assets/Scripts/Systems/MapCoordinateSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public class MapCoordinateSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+    }
+
+    public static int PositionToIndex(float2 positionXY, Tuning tuning)
+    {
+        int xIndex = (int)math.floor(((positionXY.x / tuning.WorldSize) + tuning.WorldOffset.x));
+        int yIndex = (int)math.ceil(((positionXY.y / tuning.WorldSize) + tuning.WorldOffset.y));
+        int index = (int)math.clamp((yIndex * tuning.Resolution) + xIndex, 0, (tuning.Resolution * tuning.Resolution) - 1);
+
+        return index;
+    }
+}
