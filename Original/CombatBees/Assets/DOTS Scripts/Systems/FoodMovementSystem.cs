739fa458f248af33b6b5a77f546d7cdf41bc3804
JuanRMarquez-Ortiz <juranr.marquezortiz@unity3d.com>
2021-01-28 16:33:56 -0500

    In FoodMovementSystem.cs:
    Removed unused variables.
    Re-parallelized.
    Food is now clearly visible under the bees.

--- Original/CombatBees/Assets/DOTS Scripts/Systems/FoodMovementSystem.cs
+++ Original/CombatBees/Assets/DOTS Scripts/Systems/FoodMovementSystem.cs
@@ -9,21 +9,19 @@ public class FoodMovementSystem : SystemBase
     protected override void OnUpdate()
     {
         var allTranslations = GetComponentDataFromEntity<Translation>();
-        var deltaTime = Time.DeltaTime;
-        var gravity = new float3(0f, -30f, 0f);
 
         Entities
             .WithName("CarriedFoodMovementSystem")
-            //.WithNativeDisableParallelForRestriction(allTranslations)
+            .WithNativeDisableParallelForRestriction(allTranslations)
             .WithAll<FoodTag, CarrierBee>()
             .ForEach((Entity e, ref PhysicsData physicsData, in CarrierBee b) =>
             {
                 var beePosition = allTranslations[b.Value];
                 var foodPos = allTranslations[e];
-                foodPos.Value = beePosition.Value + new float3(0f, -0.5f, 0f);
+                foodPos.Value = beePosition.Value + new float3(0f, -1f, 0f);
                 physicsData.v = 0;
                 allTranslations[e] = foodPos;
 
-            }).Run();
+            }).ScheduleParallel();
     }
 }
