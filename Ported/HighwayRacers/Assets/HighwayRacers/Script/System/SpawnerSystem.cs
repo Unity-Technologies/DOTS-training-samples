2bc776690ba458e6c1d450592ac652f1bbf24237
Steve Theodore <steve.theodore@unity3d.com>
2021-01-29 09:29:23 -0800

    interactively change driver count

--- Ported/HighwayRacers/Assets/HighwayRacers/Script/System/SpawnerSystem.cs
+++ Ported/HighwayRacers/Assets/HighwayRacers/Script/System/SpawnerSystem.cs
@@ -1,82 +1,19 @@
-﻿using Unity.Collections;
+﻿using NUnit.Framework.Constraints;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Rendering;
 using Unity.Transforms;
 
+
 public class SpawnerSystem : SystemBase
 {
-
-  static float3 MapToRoundedCorners(float t, float radius)
-  {
-      float R = 0.29f;
-        float straight = 1.0f - 2.0f * R;
-        float curved = (2.0f * math.PI * R) * 0.25f;
-        float total = straight + curved;
-        float tls = math.saturate(straight/total);
-        float tlr = math.saturate(curved/total);
-
-        int q = (int)(t * 4.0f);
-
-        float x = 0;
-        float y = 0;
-        float a = 0;
-
-        if(q == 0)
-        {
-            float n = t * 4.0f;
-            x = R;
-            y = math.lerp(R, 1.0f - R, math.saturate(n/tls));
-
-            a = 0.5f * math.PI * math.saturate((n - tls)/tlr);
-            x -= math.cos(a) * R;
-            y += math.sin(a) * R;
-        }
-        else if(q == 1)
-        {
-            float n = (t - 0.25f) * 4.0f;
-            y = 1.0f - R;
-            x = math.lerp(R, 1.0f - R, math.saturate(n/tls));
-
-            a = 0.5f * math.PI * math.saturate((n - tls)/tlr);
-            y += math.cos(a) * R;
-            x += math.sin(a) * R;
-            a += math.PI/2.0f;
-        }
-        else if(q == 2)
-        {
-            float n = (t - 0.5f) * 4.0f;
-            x = 1.0f - R;
-            y = math.lerp(1.0f - R, R, math.saturate(n/tls));
-
-            a = 0.5f * math.PI * math.saturate((n - tls)/tlr);
-            x += math.cos(a) * R;
-            y -= math.sin(a) * R;
-            a -= math.PI;
-        }
-        else
-        {
-            float n = (t - 0.75f) * 4.0f;
-            y = R;
-            x = math.lerp(1.0f - R, R, math.saturate(n/tls));
-
-            a = 0.5f * math.PI * math.saturate((n - tls)/tlr);
-            y -= math.cos(a) * R;
-            x -= math.sin(a) * R;
-            a -= math.PI/2.0f;
-        }
-
-        x -= 0.5f;
-        y -= 0.5f;
-        x *= radius;
-        y *= radius;
-        return new float3(x,y,a);
-    }
-
     public static readonly float MinimumVelocity = 0.035f;
     private EntityQuery RequirePropagation;
     private TrackOccupancySystem m_TrackOccupancySystem;
+    private TrackSettings CurrentTrackSettings;
+    private int CurrentSpawnCount;
 
     protected override void OnCreate()
     {
@@ -85,100 +22,6 @@ public class SpawnerSystem : SystemBase
 
     protected override void OnUpdate()
     {
-        var ecb = new EntityCommandBuffer(Allocator.Temp);
-
-        // The PRNG (pseudorandom number generator) from Unity.Mathematics is a struct
-        // and can be used in jobs. For simplicity and debuggability in development,
-        // we'll initialize it with a constant. (In release, we'd want a seed that
-        // randomly varies, such as the time from the user's system clock.)
-        var random = new Random(1234);
-        uint laneCount = m_TrackOccupancySystem.LaneCount;
-        uint tilesPerLane = TrackOccupancySystem.TilesPerLane;
-        float minimumVelocity = MinimumVelocity;
-
-//tile debugging
-// if (false)
-// {
-//         Entities
-//             .ForEach((Entity entity, in Spawner spawner) =>
-//             {
-//                 for (uint j = 0; j < laneCount; ++j)
-//                 {
-//                     for (uint i = 0; i < tilesPerLane; ++i)
-//                     {
-//                         var tile = ecb.Instantiate(spawner.TilePrefab);
-//
-//                         float laneRadius = (CarMovementSystem.TrackRadius + (j * CarMovementSystem.LaneWidth));
-//
-//                         float t = (float)i/(float)tilesPerLane;
-//                         float3 spawnPosition = MapToRoundedCorners(t, laneRadius);
-//
-//                         spawnPosition.x += (CarMovementSystem.TrackRadius)/2.0f + 2.75f;
-//                         spawnPosition.y += (CarMovementSystem.TrackRadius)/4.0f - 6.0f;
-//
-//
-//                         var translation = new Translation {Value = new float3(spawnPosition.x, 0, spawnPosition.y)};
-//                         ecb.SetComponent(tile, translation);
-//
-//                         ecb.SetComponent(tile, new URPMaterialPropertyBaseColor
-//                         {
-//                             Value = new float4(0.5f, 0.5f, 0.5f, 1.0f)
-//                         });
-//
-//                         ecb.SetComponent(tile, new TileDebugColor
-//                         {
-//                             laneId = j,
-//                             tileId = i
-//                         });
-//
-//                     }
-//                 }
-//             }).Run();
-// }
-
-        Entities
-            .ForEach((Entity entity, in Spawner spawner) =>
-            {
-                // Destroying the current entity is a classic ECS pattern,
-                // when something should only be processed once then forgotten.
-                ecb.DestroyEntity(entity);
-
-                for (uint i = 0; i < spawner.CarCount; ++i)
-                {
-                    var vehicle = ecb.Instantiate(spawner.CarPrefab);
-                    var translation = new Translation {Value = new float3(0, 0, 0)};
-                    ecb.SetComponent(vehicle, translation);
-
-                    ecb.SetComponent(vehicle, new URPMaterialPropertyBaseColor
-                    {
-                        Value = random.NextFloat4()
-                    });
-
-                    // If the driver profile is set to random, pick one of
-                    // the profiles for the new car.
-                    DriverProfile profile = spawner.DriverProfile;
-                    if (profile == DriverProfile.Random)
-                    {
-                        profile = random.NextBool() ? DriverProfile.American : DriverProfile.European;
-                    }
-
-                    uint currentLane = i % laneCount;
-                    ecb.SetComponent(vehicle, new CarMovement
-                    {
-                        // todo here we ar enot smart enough. Two cars might end up in the same tile.
-                        // This means they can drive through each other.
-                        Offset = (float)i / spawner.CarCount,
-                        Lane = currentLane,
-                        LaneOffset = (float)currentLane,
-                        Velocity = random.NextFloat(minimumVelocity, 0.075f),
-                        LaneSwitchCounter = 0,
-                        Profile = profile
-                    });
-                }
-            }).Run();
-
-        ecb.Playback(EntityManager);
-        
         // Propagate color from parent to child entities
         // We do this every frame because we change the color of the cars
 
@@ -202,5 +45,77 @@ public class SpawnerSystem : SystemBase
                     cdfe[group[i].Value] = color;
                 }
             }).ScheduleParallel();
+
+
+        // The code below only runs when the UI updates
+
+
+        if (UIValues.GetModified() == CurrentTrackSettings.Iteration && CurrentTrackSettings.Iteration > 0)
+        {
+            return;
+        }
+
+        CurrentTrackSettings = UIValues.GetTrackSettings();
+        if (CurrentSpawnCount == CurrentTrackSettings.CarCount)
+        {
+            return;
+        }
+
+
+        var random = new Random(1234);
+        uint laneCount = m_TrackOccupancySystem.LaneCount;
+        uint tilesPerLane = TrackOccupancySystem.TilesPerLane;
+        float minimumVelocity = MinimumVelocity;
+
+        int carCount = CurrentTrackSettings.CarCount;
+
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+
+
+        Entities.ForEach((Entity ent, in PropagateColor c) => { ecb.DestroyEntity(ent); }
+        ).Run();
+
+        Entities
+            .ForEach((Entity entity, in Spawner spawner) =>
+            {
+                // Destroying the current entity is a classic ECS pattern,
+                // when something should only be processed once then forgotten.
+
+
+                for (uint i = 0; i < carCount; ++i)
+                {
+                    var vehicle = ecb.Instantiate(spawner.CarPrefab);
+                    var translation = new Translation {Value = new float3(0, 0, 0)};
+                    ecb.SetComponent(vehicle, translation);
+
+                    ecb.SetComponent(vehicle, new URPMaterialPropertyBaseColor
+                    {
+                        Value = random.NextFloat4()
+                    });
+
+                    // If the driver profile is set to random, pick one of
+                    // the profiles for the new car.
+                    DriverProfile profile = spawner.DriverProfile;
+                    if (profile == DriverProfile.Random)
+                    {
+                        profile = random.NextBool() ? DriverProfile.American : DriverProfile.European;
+                    }
+
+                    uint currentLane = i % laneCount;
+                    ecb.SetComponent(vehicle, new CarMovement
+                    {
+                        // todo here we ar enot smart enough. Two cars might end up in the same tile.
+                        // This means they can drive through each other.
+                        Offset = (float) i / spawner.CarCount,
+                        Lane = currentLane,
+                        LaneOffset = (float) currentLane,
+                        Velocity = random.NextFloat(minimumVelocity, 0.075f),
+                        LaneSwitchCounter = 0,
+                        Profile = profile
+                    });
+                }
+            }).Run();
+
+        ecb.Playback(EntityManager);
     }
 }
