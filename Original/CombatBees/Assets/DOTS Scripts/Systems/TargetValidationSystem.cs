5df783aa1f5cb6713f17cdbd8d68168613204643
JuanRMarquez-Ortiz <juranr.marquezortiz@unity3d.com>
2021-01-29 01:10:05 -0500

    Turns out we had a situation.
    When we made TargetValidationSystem.cs, attacking bees wasn't a thing yet. This means that, once we could attack bees, we weren't considering that possibility. This means that, whenever a bee targeted another, validation didn't recognize it and scrapped the targeting, forcing reacquisition.
    
    No more.
    
    Also slowed down the bees when attacking. It makes things a tad too crazy. Need to work on dampening the speeds faster if we're gonna make them go faster so they don't slide all over the place.

--- Original/CombatBees/Assets/DOTS Scripts/Systems/TargetValidationSystem.cs
+++ Original/CombatBees/Assets/DOTS Scripts/Systems/TargetValidationSystem.cs
@@ -3,7 +3,8 @@ using Unity.Entities;
 
 [UpdateAfter(typeof(ClickSpawnSystem))]
 public class TargetingValidationSystem : SystemBase
-{    
+{
+    private int beeModulusToCheck = 0;
     protected override void OnUpdate()
     {
         var ecb = new EntityCommandBuffer(Allocator.TempJob);
@@ -13,15 +14,29 @@ public class TargetingValidationSystem : SystemBase
         Entities
             .WithName("TargetValidation")
             .WithAll<MoveTarget>()
-            .ForEach((Entity e, int entityInQueryIndex, ref MoveTarget targetFood) => 
+            .ForEach((Entity e, int entityInQueryIndex, ref MoveTarget target) => 
             {
-                Entity targetEntity = targetFood.Value;
-                
-                if(!HasComponent<FoodTag>(targetEntity) || // if previously destroyed
-                    HasComponent<CarrierBee>(targetEntity)) // if carried by another bee
+                Entity targetEntity = target.Value;
+                if (HasComponent<FetchingFoodTag>(e))
                 {
-                    pEcb.AddComponent<BeeWithInvalidtarget>(entityInQueryIndex, e);
+                    //We're looking for food; we can do these checks
+                    if(!HasComponent<FoodTag>(targetEntity) || // if previously destroyed
+                       HasComponent<CarrierBee>(targetEntity)) // if carried by another bee
+                    {
+                        pEcb.AddComponent<BeeWithInvalidtarget>(entityInQueryIndex, e);
+                    }
                 }
+                else if (HasComponent<AttackingBeeTag>(e))
+                {
+                    //We're attacking a bee
+                    if (!HasComponent<BeeTag>(targetEntity))
+                    {
+                        //bee we were going for is dead; invalid.
+                        pEcb.AddComponent<BeeWithInvalidtarget>(entityInQueryIndex, e);
+                    }
+                }
+                
+                
             }).ScheduleParallel();
         
         Dependency.Complete();
