f14b02f73e7373f5761a346c5cc5125e31dc2f8e
JuanRMarquez-Ortiz <juranr.marquezortiz@unity3d.com>
2021-01-29 01:26:42 -0500

    Blood spatter systems have been parallelized. Additionally shoved the structure changes in the end simulation system; they're not time-critical.

--- Original/CombatBees/Assets/DOTS Scripts/Systems/BloodSplatSystem.cs
+++ Original/CombatBees/Assets/DOTS Scripts/Systems/BloodSplatSystem.cs
@@ -6,36 +6,43 @@ using UnityEngine;
 
 public class BloodSplatSystem: SystemBase
 {
+    private EntityCommandBufferSystem ecbs;
+    protected override void OnCreate()
+    {
+        ecbs = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
     protected override void OnUpdate()
     {
         var time = Time.ElapsedTime;
         float floorHeight = GetSingleton<SpawnZones>().LevelBounds.Min.y;
 
-        var ecb = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
+        var ecb = ecbs.CreateCommandBuffer().AsParallelWriter();
         Entities
+            .WithName("BloodLifeSystem")
             .WithAll<BloodTag>()
             .WithNone<GroundedTime>()
-            .ForEach((Entity e, ref RandomComponent rng, in PhysicsData data, in Translation translation) =>
+            .ForEach((Entity e, int entityInQueryIndex, ref RandomComponent rng, in PhysicsData data, in Translation translation) =>
             {
                 if (translation.Value.y <= floorHeight + 0.001f)
                 {
-                    ecb.AddComponent(e, Lifetime.FromTimeRemaining(rng.Value.NextFloat(2f,4f)));
+                    ecb.AddComponent(entityInQueryIndex, e, Lifetime.FromTimeRemaining(rng.Value.NextFloat(2f,4f)));
 
-                    ecb.AddComponent(e, new GroundedTime
+                    ecb.AddComponent(entityInQueryIndex, e, new GroundedTime
                     {
                         Time = time,
                     });
                 }
-            }).Run();
-        ecb.Playback(EntityManager);
-        ecb.Dispose();
+            }).ScheduleParallel();
+        
 
 
         Entities
+            .WithName("BloodFadeSystem")
             .WithAll<BloodTag>()
             .ForEach((ref URPPropertyLifetime shaderPropLifetime, in Lifetime actualLifetime) =>
             {
                 shaderPropLifetime.Value = actualLifetime.NormalizedTimeRemaining;
-            }).Run();
+            }).ScheduleParallel();
     }
 }
