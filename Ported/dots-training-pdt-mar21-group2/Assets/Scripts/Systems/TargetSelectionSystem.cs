b419d6ac97a4ca7cc26dd1dc2690dd6ed89a2b01
Sandy Carter <sandy.carter@unity3d.com>
2021-03-16 09:25:13 -0400

    Add stub system classes

---
+++ Ported/dots-training-pdt-mar21-group2/Assets/Scripts/Systems/TargetSelectionSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public class TargetSelectionSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        
+    }
+}
