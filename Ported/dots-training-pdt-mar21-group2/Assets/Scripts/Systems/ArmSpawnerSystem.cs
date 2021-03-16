078c325d7e48a5dd4499f14faca5c2fc3b3a9072
Sandy Carter <sandy.carter@unity3d.com>
2021-03-16 09:59:33 -0400

    Add arm spawner

---
+++ Ported/dots-training-pdt-mar21-group2/Assets/Scripts/Systems/ArmSpawnerSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public class SpawnerSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+
+        Entities
+            .ForEach((Entity entity, in Spawner spawner) =>
+            {
+                // Destroying the current entity is a classic ECS pattern,
+                // when something should only be processed once then forgotten.
+                ecb.DestroyEntity(entity);
+
+                for (int i = 0; i < spawner.ArmCount; ++i)
+                {
+                    var instance = ecb.Instantiate(spawner.ArmPrefab);
+                    var translation = new Translation {Value = new float3(i, 0, 0)};
+                    ecb.SetComponent(instance, translation);
+                }
+            }).Run();
+
+        ecb.Playback(EntityManager);
+        ecb.Dispose();
+    }
+}
