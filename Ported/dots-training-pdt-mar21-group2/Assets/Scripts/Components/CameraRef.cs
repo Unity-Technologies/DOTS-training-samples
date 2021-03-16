7c1a2920e84c351d4981062e3441275daa1a6de9
Raman Grewal <raman.grewal@unity3d.com>
2021-03-16 10:33:03 -0400

    Added rest of components Created camera ref so it's can be accessed in DOTS

---
+++ Ported/dots-training-pdt-mar21-group2/Assets/Scripts/Components/CameraRef.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+// Way to get managed Camera
+public class CameraRef : IComponentData
+{
+    public Camera Camera;
+}
---
+++ Ported/dots-training-pdt-mar21-group2/Assets/Scripts/Components/Velocity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+public struct Velocity : IComponentData
+{
+    public float3 Value;
+}
