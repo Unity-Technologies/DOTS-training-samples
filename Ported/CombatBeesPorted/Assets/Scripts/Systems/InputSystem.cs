8bbc382ddafaf78f1d5e4c4d4ff8f5e1efd88ca9
Jans Margevics <jans.margevics@unity3d.com>
2021-03-17 17:24:00 +0200

    Generate food from a mouse click

---
+++ Ported/CombatBeesPorted/Assets/Scripts/Systems/InputSystem.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public class InputSystem : SystemBase
+{
+    private EntityQuery boundsQuery;
+    protected override void OnCreate()
+    {
+        
+        var boundsQueryDesc = new EntityQueryDesc
+        {
+            All = new ComponentType[] { typeof(SpawnBounds) },
+        };
+        boundsQuery = GetEntityQuery(boundsQueryDesc);
+    }
+
+    protected override void OnUpdate()
+    {
+        var spawnBoundsArray = boundsQuery.ToEntityArray(Allocator.Temp);
+        var gameConfig = GetSingleton<GameConfiguration>();
+        var commandBuffer = new EntityCommandBuffer(Allocator.Temp);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var boundsComponent = GetComponent<SpawnBounds>(spawnBoundsArray[0]);
+
+            
+            float3 planeFloor = new float3(0, boundsComponent.Center.y - boundsComponent.Extents.y, 0);
+            if (new Plane(Vector3.up, planeFloor + 1f).Raycast(ray, out var hit))
+            {
+                var foodEntity = commandBuffer.Instantiate(gameConfig.FoodPrefab);
+                if (GetComponent<SpawnBounds>(spawnBoundsArray[0]).Contains( ray.GetPoint(hit)))
+                {                    
+                    commandBuffer.SetComponent(foodEntity, new Translation() { Value = ray.GetPoint(hit) });
+                    commandBuffer.AddComponent(foodEntity, new Force() { });
+                    commandBuffer.AddComponent(foodEntity, new Velocity() { });
+
+                }
+                else if (GetComponent<SpawnBounds>(spawnBoundsArray[1]).Contains(ray.GetPoint(hit)))
+                {
+                    commandBuffer.SetComponent(foodEntity, new Translation() { Value = ray.GetPoint(hit) });
+                    commandBuffer.AddComponent(foodEntity, new Force() { });
+                    commandBuffer.AddComponent(foodEntity, new Velocity() { });
+                }
+
+                commandBuffer.Playback(EntityManager);
+                commandBuffer.Dispose();
+
+            }
+           
+        }
+    }
+}
