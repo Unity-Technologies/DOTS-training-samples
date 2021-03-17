48acdbf6f43c53b0ac9e108c93e29a6cff117edb
Raman Grewal <raman.grewal@unity3d.com>
2021-03-17 15:04:21 -0400

    Moved rock throwing into different system. Added TargetCan to prefab

--- Ported/dots-training-pdt-mar21-group2/Assets/Scripts/Components/TargetCan.cs
+++ Ported/dots-training-pdt-mar21-group2/Assets/Scripts/Components/TargetCan.cs
@@ -3,7 +3,7 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 
-[Serializable]
+[GenerateAuthoringComponent, Serializable]
 // For hand to mark which hand it will target
 public struct TargetCan : IComponentData
 {
