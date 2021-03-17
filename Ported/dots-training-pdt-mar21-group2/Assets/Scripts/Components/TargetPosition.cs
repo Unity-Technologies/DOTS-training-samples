7b4a99e123c75645a0f22ef578c63023ab5b519b
Francois Fournel <francois.fournel@unity3d.com>
2021-03-17 10:24:28 -0400

    Add TargetPosition & Timer components to arm. Make arm bigger

--- Ported/dots-training-pdt-mar21-group2/Assets/Scripts/Components/TargetPosition.cs
+++ Ported/dots-training-pdt-mar21-group2/Assets/Scripts/Components/TargetPosition.cs
@@ -3,7 +3,7 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 
-[Serializable]
+[GenerateAuthoringComponent, Serializable]
 // Used for knowing position of a target ie Can
 public struct TargetPosition : IComponentData
 {
