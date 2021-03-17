89fd6d640296ecddd83ad0e1ad13b45c4a7031a4
JulienH <julienh@unity3d.com>
2021-03-17 14:39:22 +0100

    VelocityRotationSystem base

---
+++ Ported/CombatBeesPorted/Assets/Scripts/Systems/VelocityRotationSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class VelocityRotationSystem : SystemBase
+{
+
+    protected override void OnUpdate()
+    {
+        Entities
+            .ForEach((Entity entity,ref Rotation Rotation, in Translation translation, in Velocity velocity, in Bee bee) =>
+            {
+                //float3 lookDirection = math.normalize(translation.Value + velocity.Value);
+                Quaternion targetRotation=Quaternion.LookRotation(math.normalize(velocity.Value),float3.zero );
+
+                Rotation.Value = targetRotation;
+
+            })
+            .ScheduleParallel();
+    }
+}
