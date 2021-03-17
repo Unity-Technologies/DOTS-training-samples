4e46d5ce210da05e3fbd2a19ad1f9bb071ef079c
Pavel Laurs <pavell@unity3d.com>
2021-03-17 14:59:26 +0200

    add partitions to nearest bee

--- Ported/CombatBeesPorted/Assets/Scripts/Systems/RepulsionSystem.cs
+++ Ported/CombatBeesPorted/Assets/Scripts/Systems/RepulsionSystem.cs
@@ -4,6 +4,7 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Rendering;
 using Unity.Transforms;
+using UnityEngine;
 
 public class RepulsionSystem: SystemBase
 {
@@ -24,31 +25,60 @@ public class RepulsionSystem: SystemBase
 
         EntityQuery allBeesQuery = GetEntityQuery(allBeesQueryDesc);
         NativeArray<Entity> allBees = allBeesQuery.ToEntityArray(Allocator.Temp);
+        const float maxRepulsionDistance = 10.0f;
+
+        NativeMultiHashMap<int,float3> partitions =  new NativeMultiHashMap<int, float3>(allBees.Length, Allocator.TempJob);
+        Entities
+            .WithNone<Attacking>()
+            .WithAll<Bee>()
+            .ForEach((in Translation translation) =>
+            {
+                var partIndex = translation.Value / maxRepulsionDistance / 2.0f;
+                int index =  (int)partIndex.x + ((int)partIndex.y)*1024;
+                partitions.Add(index, translation.Value);
+            
+            }).Run();
+
+        /*NativeArray<int> allKeys = partitions.GetUniqueKeyArray(Allocator.Temp).Item1;
+        foreach( var key in allKeys)
+            Debug.Log(key);*/
 
         Entities
             .WithNone<Attacking>()
+            .WithoutBurst()
             .ForEach((Entity entity, ref Force beeForce, in Bee bee, in Translation translation) =>
             {
                 float3 sumForce = float3.zero;
-                int count = 0;
+                int count = 1;
+                var partIndex = translation.Value / maxRepulsionDistance / 2.0f;
+                
+                for( int indexX=((int)partIndex.x)-1; indexX<=((int)partIndex.x)+1; indexX++) {
+                    for( int indexY=((int)partIndex.y)-1; indexY<=((int)partIndex.y)+1; indexY++) {
 
-                foreach(Entity anotherBee in allBees) {
-                    if(entity == anotherBee) continue;
-                    if( HasComponent<Attacking>(anotherBee) ) continue;
-                    
-                    float3 vec = translation.Value - GetComponent<Translation>(anotherBee).Value;
-                    float distance = math.length(vec);
-                    float amout = 0;
+                        int index =  indexX + indexY*1024;
+                        var iterator = partitions.GetValuesForKey(index);
+                        while( iterator.MoveNext()) {
+                            var anotherBeePos = iterator.Current;
 
-                    amout = 1.0f - (math.clamp(distance, 0.0f, 10.0f) / 10.0f);
-                    sumForce +=  math.normalize(vec) * amout;
-                    count += 1;
-                }
+                            if( math.all(translation.Value == anotherBeePos)) continue;
+                            
+                            float3 vec = translation.Value - anotherBeePos;
+
+                            float distance = math.length(vec);                            
+                            if(distance < maxRepulsionDistance) {
+                                float amout = 1.0f - distance / maxRepulsionDistance;
+                                sumForce +=  math.normalize(vec) * amout;
+                                count += 1;
+                                //Debug.DrawLine(translation.Value,anotherBeePos);   
+                            }
+                        }
+                    }   
+                }             
 
                 beeForce.Value += sumForce *3 / count;
             }).Run();
         
-        
+        partitions.Dispose();
         commandBuffer.Playback(EntityManager);
         commandBuffer.Dispose();
     }
