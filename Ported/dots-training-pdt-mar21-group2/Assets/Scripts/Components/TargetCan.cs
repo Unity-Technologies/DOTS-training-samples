84939e000967d53a80a7d15d3cbfc4fd699095ac
Raman Grewal <raman.grewal@unity3d.com>
2021-03-17 10:32:25 -0400

    Added component to target a specific can for a hand

---
+++ Ported/dots-training-pdt-mar21-group2/Assets/Scripts/Components/TargetCan.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+// For hand to mark which hand it will target
+public struct TargetCan : IComponentData
+{
+    public Entity Value;
+}
