0460e30961506a3b8f5661090958cd5517902f7b
Julien Heijmans <julienh@unity3d.com>
2021-03-18 14:17:18 +0100

    Increase velocityScale

--- Ported/CombatBeesPorted/Assets/Scripts/Systems/VelocityScaleSystem.cs
+++ Ported/CombatBeesPorted/Assets/Scripts/Systems/VelocityScaleSystem.cs
@@ -10,18 +10,14 @@ public class VelocityScaleSystem: SystemBase
     
     protected override void OnUpdate()
     {
-        // var commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
         
         Entities
             .WithAll<VelocityScale>()
             .ForEach((Entity entity, ref NonUniformScale nonUniformScale, in Velocity velocity, in InitialScale initialScale) =>
             {
-                float yScale = math.lerp(initialScale.Value.y, initialScale.Value.y * 4f, math.clamp((math.length(velocity.Value) / 40f) - 0.5f,0,1)); // Expose these numbers?
+                float yScale = math.lerp(initialScale.Value.y, initialScale.Value.y * 4f, math.clamp((math.length(velocity.Value) / 30f) - 0.5f,0,1)); // Expose these numbers?
                 nonUniformScale.Value = new float3( initialScale.Value.x, yScale, initialScale.Value.y);
             }).Run();
-        
-        // commandBuffer.Playback(EntityManager);
-        // commandBuffer.Dispose();
     }
     
 }
