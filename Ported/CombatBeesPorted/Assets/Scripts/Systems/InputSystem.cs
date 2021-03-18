fc075d9ef359adcef13186d2bd495a20ad36c9f2
Jans Margevics <jans.margevics@unity3d.com>
2021-03-18 15:40:08 +0200

    Create food anywhere in area, support floor and ceiling

--- Ported/CombatBeesPorted/Assets/Scripts/Systems/InputSystem.cs
+++ Ported/CombatBeesPorted/Assets/Scripts/Systems/InputSystem.cs
@@ -10,6 +10,8 @@ public class InputSystem : SystemBase
 {
     private EntityQuery boundsQuery;
     private EntityQuery allWithForceQuery;
+    private EntityQuery arenaQuery;
+
     protected override void OnCreate()
     { 
         var boundsQueryDesc = new EntityQueryDesc
@@ -22,15 +24,23 @@ public class InputSystem : SystemBase
         {
             All = new ComponentType[] { typeof(Force) },
         };
-        allWithForceQuery = GetEntityQuery(allWithForce);                 
+        allWithForceQuery = GetEntityQuery(allWithForce);
+
+        var getArena = new EntityQueryDesc
+        {
+            All = new ComponentType[] { typeof(GameConfiguration), typeof(WorldRenderBounds)}
+        };
+        arenaQuery = GetEntityQuery(getArena);
     }
 
     protected override void OnUpdate()
     {
         var spawnBoundsArray = boundsQuery.ToEntityArray(Allocator.Temp);
+        var arena = arenaQuery.ToEntityArray(Allocator.Temp);
         var gameConfig = GetSingleton<GameConfiguration>();
         var commandBuffer = new EntityCommandBuffer(Allocator.Temp);
         var boundsComponent = GetComponent<SpawnBounds>(spawnBoundsArray[0]);
+        var arenaBounds = GetComponent<WorldRenderBounds>(arena[0]);
     
         if (Input.GetKeyDown(KeyCode.R))
         {
@@ -45,19 +55,31 @@ public class InputSystem : SystemBase
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);      
             float3 planeFloor = new float3(0, boundsComponent.Center.y - boundsComponent.Extents.y, 0);
+            float3 planeCeil= new float3(0, boundsComponent.Center.y + boundsComponent.Extents.y, 0);
+
+            var foodEntity = commandBuffer.Instantiate(gameConfig.FoodPrefab);
             if (new Plane(Vector3.up, planeFloor + 1f).Raycast(ray, out var hit))
             {
-                var foodEntity = commandBuffer.Instantiate(gameConfig.FoodPrefab);
-                if (GetComponent<SpawnBounds>(spawnBoundsArray[0]).ContainsPoint( ray.GetPoint(hit)) ||
-                    GetComponent<SpawnBounds>(spawnBoundsArray[1]).ContainsPoint(ray.GetPoint(hit)))
+                if (Vector3.Dot(ray.direction, Vector3.up) < 0 && arenaBounds.Value.Contains(ray.GetPoint(hit)))
                 {                    
                     commandBuffer.SetComponent(foodEntity, new Translation() { Value = ray.GetPoint(hit) });
                     commandBuffer.AddComponent(foodEntity, new Force() { });
                     commandBuffer.AddComponent(foodEntity, new Velocity() { });
                     commandBuffer.Playback(EntityManager);
                     commandBuffer.Dispose();
-                } 
-            }   
+                }
+            }
+            if (new Plane(Vector3.up, planeCeil - 1f).Raycast(ray, out var hit2))
+            {
+                if (Vector3.Dot(ray.direction, Vector3.down) < 0 && arenaBounds.Value.Contains(ray.GetPoint(hit2)))
+                {
+                    commandBuffer.SetComponent(foodEntity, new Translation() { Value = ray.GetPoint(hit2) });
+                    commandBuffer.AddComponent(foodEntity, new Force() { });
+                    commandBuffer.AddComponent(foodEntity, new Velocity() { });
+                    commandBuffer.Playback(EntityManager);
+                    commandBuffer.Dispose();
+                }
+            }
         }   
     }
 }
