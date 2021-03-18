2d3ae263070331c25505bdbb9a0958685ef40c8c
Pavel Laurs <pavell@unity3d.com>
2021-03-18 13:57:38 +0200

    optimize repulsion

--- Ported/CombatBeesPorted/Assets/Scripts/Systems/RepulsionSystem.cs
+++ Ported/CombatBeesPorted/Assets/Scripts/Systems/RepulsionSystem.cs
@@ -23,63 +23,50 @@ public class RepulsionSystem: SystemBase
 
         var commandBuffer = new EntityCommandBuffer(Allocator.Temp);
 
-        EntityQuery allBeesQuery = GetEntityQuery(allBeesQueryDesc);
-        NativeArray<Entity> allBees = allBeesQuery.ToEntityArray(Allocator.Temp);
-        const float maxRepulsionDistance = 10.0f;
+        EntityQuery allBeesQuery = GetEntityQuery(allBeesQueryDesc);        
+        const float maxRepulsionDistance = 5.0f;
 
-        NativeMultiHashMap<int,float3> partitions =  new NativeMultiHashMap<int, float3>(allBees.Length, Allocator.TempJob);
+        NativeMultiHashMap<int,float3> partitions =  new NativeMultiHashMap<int, float3>(allBeesQuery.CalculateEntityCount(), Allocator.TempJob);
         Entities
             .WithNone<Attacking>()
             .WithAll<Bee>()
             .ForEach((in Translation translation) =>
             {
                 var partIndex = translation.Value / maxRepulsionDistance / 2.0f;
-                int index =  (int)partIndex.x + ((int)partIndex.y)*1024;
+                int index =  (int)partIndex.x;// + ((int)partIndex.y)*1024;// + ((int)partIndex.z)*1024*1024;
                 partitions.Add(index, translation.Value);
             
             }).Run();
 
-        /*NativeArray<int> allKeys = partitions.GetUniqueKeyArray(Allocator.Temp).Item1;
-        foreach( var key in allKeys)
-            Debug.Log(key);*/
-
         Entities
             .WithNone<Attacking>()
-            .WithoutBurst()
-            .ForEach((Entity entity, ref Force beeForce, in Bee bee, in Translation translation) =>
+            .WithAll<Bee>()
+            .WithDisposeOnCompletion(partitions)
+            .ForEach((ref Force beeForce, in Translation translation) =>
             {
                 float3 sumForce = float3.zero;
                 int count = 1;
                 var partIndex = translation.Value / maxRepulsionDistance / 2.0f;
                 
-                for( int indexX=((int)partIndex.x)-1; indexX<=((int)partIndex.x)+1; indexX++) {
-                    for( int indexY=((int)partIndex.y)-1; indexY<=((int)partIndex.y)+1; indexY++) {
+                for( int index=((int)partIndex.x)-1; index<=((int)partIndex.x)+1; index++) {
+                    var iterator = partitions.GetValuesForKey(index);
+                    while( iterator.MoveNext()) {
+                        var anotherBeePos = iterator.Current;
 
-                        int index =  indexX + indexY*1024;
-                        var iterator = partitions.GetValuesForKey(index);
-                        while( iterator.MoveNext()) {
-                            var anotherBeePos = iterator.Current;
+                        //if( math.all(translation.Value == anotherBeePos)) continue;
+                        
+                        float3 vec = translation.Value - anotherBeePos;
 
-                            if( math.all(translation.Value == anotherBeePos)) continue;
-                            
-                            float3 vec = translation.Value - anotherBeePos;
-
-                            float distance = math.length(vec);                            
-                            if(distance < maxRepulsionDistance) {
-                                float amout = 1.0f - distance / maxRepulsionDistance;
-                                sumForce +=  math.normalize(vec) * amout;
-                                count += 1;
-                                //Debug.DrawLine(translation.Value,anotherBeePos);   
-                            }
+                        float distance = math.length(vec);                            
+                        if(distance < maxRepulsionDistance && distance != 0) {
+                            float amout = 1.0f - distance / maxRepulsionDistance;
+                            sumForce +=  math.normalize(vec) * amout;
+                            count += 1;
                         }
-                    }   
+                    }
                 }             
 
                 beeForce.Value += sumForce *3 / count;
             }).Run();
-        
-        partitions.Dispose();
-        commandBuffer.Playback(EntityManager);
-        commandBuffer.Dispose();
     }
 }
