6e48dcd24caf7b851f7d42f54de8aed86e648c4d
Jans Margevics <jans.margevics@unity3d.com>
2021-03-18 17:32:33 +0200

    All walls are clickable now

--- Ported/CombatBeesPorted/Assets/Scripts/Systems/InputSystem.cs
+++ Ported/CombatBeesPorted/Assets/Scripts/Systems/InputSystem.cs
@@ -55,7 +55,13 @@ public class InputSystem : SystemBase
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);      
             float3 planeFloor = new float3(0, boundsComponent.Center.y - boundsComponent.Extents.y, 0);
-            float3 planeCeil= new float3(0, boundsComponent.Center.y + boundsComponent.Extents.y, 0);
+            float3 planeCeil = new float3(0, boundsComponent.Center.y + boundsComponent.Extents.y, 0);
+
+            float3 lateraltWall1 = new float3(0, 0, boundsComponent.Center.z - boundsComponent.Extents.z);
+            float3 lateraltWall2 = new float3(0, 0, boundsComponent.Center.z + boundsComponent.Extents.z);
+
+            float3 capWall1 = new float3(arenaBounds.Value.Center.x - arenaBounds.Value.Extents.x, 0, 0);
+            float3 capWall2 = new float3(boundsComponent.Center.x + boundsComponent.Extents.x, 0, 0);
 
             var foodEntity = commandBuffer.Instantiate(gameConfig.FoodPrefab);
             if (new Plane(Vector3.up, planeFloor + 1f).Raycast(ray, out var hit))
@@ -74,6 +80,50 @@ public class InputSystem : SystemBase
                 if (Vector3.Dot(ray.direction, Vector3.down) < 0 && arenaBounds.Value.Contains(ray.GetPoint(hit2)))
                 {
                     commandBuffer.SetComponent(foodEntity, new Translation() { Value = ray.GetPoint(hit2) });
+                    commandBuffer.AddComponent(foodEntity, new Force() { });
+                    commandBuffer.AddComponent(foodEntity, new Velocity() { });
+                    commandBuffer.Playback(EntityManager);
+                    commandBuffer.Dispose();
+                }
+            }
+            if (new Plane(new float3(0, 0, 1), lateraltWall1 + 1f).Raycast(ray, out var hit3))
+            {
+                if (Vector3.Dot(ray.direction, new float3(0, 0, 1)) < 0 && arenaBounds.Value.Contains(ray.GetPoint(hit3)))
+                {
+                    commandBuffer.SetComponent(foodEntity, new Translation() { Value = ray.GetPoint(hit3) });
+                    commandBuffer.AddComponent(foodEntity, new Force() { });
+                    commandBuffer.AddComponent(foodEntity, new Velocity() { });
+                    commandBuffer.Playback(EntityManager);
+                    commandBuffer.Dispose();
+                }
+            }
+            if (new Plane(new float3(0, 0, -1), lateraltWall2 - 1f).Raycast(ray, out var hit4))
+            {
+                if (Vector3.Dot(ray.direction, new float3(0, 0, -1)) < 0 && arenaBounds.Value.Contains(ray.GetPoint(hit4)))
+                {
+                    commandBuffer.SetComponent(foodEntity, new Translation() { Value = ray.GetPoint(hit4) });
+                    commandBuffer.AddComponent(foodEntity, new Force() { });
+                    commandBuffer.AddComponent(foodEntity, new Velocity() { });
+                    commandBuffer.Playback(EntityManager);
+                    commandBuffer.Dispose();
+                }
+            }
+            if (new Plane(new float3(1, 0, 0), capWall1 + 1f).Raycast(ray, out var hit5))
+            {
+                if (Vector3.Dot(ray.direction, new float3(1, 0, 0)) < 0 && arenaBounds.Value.Contains(ray.GetPoint(hit5)))
+                {
+                    commandBuffer.SetComponent(foodEntity, new Translation() { Value = ray.GetPoint(hit5) });
+                    commandBuffer.AddComponent(foodEntity, new Force() { });
+                    commandBuffer.AddComponent(foodEntity, new Velocity() { });
+                    commandBuffer.Playback(EntityManager);
+                    commandBuffer.Dispose();
+                }
+            }
+            if (new Plane(new float3(-1, 0, 0), capWall2 - 1f).Raycast(ray, out var hit6))
+            {
+                if (Vector3.Dot(ray.direction, new float3(-1, 0, 0)) < 0 && arenaBounds.Value.Contains(ray.GetPoint(hit6)))
+                {
+                    commandBuffer.SetComponent(foodEntity, new Translation() { Value = ray.GetPoint(hit6) });
                     commandBuffer.AddComponent(foodEntity, new Force() { });
                     commandBuffer.AddComponent(foodEntity, new Velocity() { });
                     commandBuffer.Playback(EntityManager);
