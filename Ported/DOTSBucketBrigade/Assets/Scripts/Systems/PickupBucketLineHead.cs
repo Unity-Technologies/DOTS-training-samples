1e63c3f898de4fbe6fd3a6416c11d0f042877d38
Krista Capps <krista.capps@unity3d.com>
2021-03-19 09:07:00 -0500

    added pickup bucket for first in line

---
+++ Ported/DOTSBucketBrigade/Assets/Scripts/Systems/PickupBucketLineHead.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PickupBucketLineHead : SystemBase
+{
+    private EntityQuery bucketQuery;
+    private EndSimulationEntityCommandBufferSystem sys;
+
+    protected override void OnCreate()
+    {
+        bucketQuery = GetEntityQuery(
+            typeof(Bucket),
+            typeof(Volume),
+            typeof(Translation));
+        sys = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var ecb = sys.CreateCommandBuffer();
+        var bucketPositions = bucketQuery.ToComponentDataArray<Translation>(Allocator.TempJob);
+        var bucketVolumes = bucketQuery.ToComponentDataArray<Volume>(Allocator.TempJob);
+        var bucketIDs = bucketQuery.ToEntityArray(Allocator.TempJob);
+        
+        Dependency = Entities
+            .WithAll<Line>()
+            .WithDisposeOnCompletion(bucketIDs)
+            .WithDisposeOnCompletion(bucketVolumes)
+            .WithDisposeOnCompletion(bucketPositions)
+            .ForEach((Entity entity, in Line line) =>
+            {
+                Translation position = GetComponent<Translation>(line.FullHead);
+                Radius radius = GetComponent<Radius>(line.FullHead);
+                Entity fullBucket;
+                for (int i = 0; i < bucketPositions.Length; i++)
+                {
+                    if (bucketVolumes[i].Value >= 1.0f)
+                    {
+                        if (math.distance(bucketPositions[i].Value, position.Value) <= 50)//radius.Value)
+                        {
+                            ecb.AddComponent<CarryingBucket>(line.FullHead);
+                            ecb.SetComponent(line.FullHead,new BucketID(){ Value = bucketIDs[i] });
+                            break;
+                        }
+                    }
+                }
+            }).Schedule(Dependency);
+        sys.AddJobHandleForProducer(Dependency);
+    }
+}
--- Ported/DOTSBucketBrigade/Assets/Scripts/Systems/PickupBucketLineSystem.cs
+++ Ported/DOTSBucketBrigade/Assets/Scripts/Systems/PickupBucketLineSystem.cs
@@ -8,12 +8,17 @@ using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class PickupBucketSystem : SystemBase
+public class PickupBucketLineSystem : SystemBase
 {
+    private EntityQuery bucketQuery;
     private EndSimulationEntityCommandBufferSystem sys;
 
     protected override void OnCreate()
     {
+        bucketQuery = GetEntityQuery(
+            typeof(Bucket),
+            typeof(Volume),
+            typeof(Translation));
         sys = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
     }
 
@@ -22,16 +27,17 @@ public class PickupBucketSystem : SystemBase
         var ecb = sys.CreateCommandBuffer();
         
         Dependency = Entities
-            .WithAll<BucketFetcher>()
+            .WithAll<EmptyBucketer>()
+            .WithAll<FullBucketer>()
             .WithNone<CarryingBucket>()
-            .ForEach((Entity entity, ref TargetPosition targetPosition, ref BucketID bucketId, in Translation position) =>
+            .ForEach((Entity entity, ref TargetPosition targetPosition, ref BucketID bucketId, in Translation position, in Radius radius) =>
             {
                 if (bucketId.Value != Entity.Null)
                 {
                     var targetBucketPos = GetComponent<Translation>(bucketId.Value);
                     var distx = math.distance(targetBucketPos.Value.x, position.Value.x);
                     var distz = math.distance(targetBucketPos.Value.z, position.Value.z);
-                    if (distx < 0.1f && distz < 0.1f)
+                    if (distx < radius.Value || distz < radius.Value)
                     {
                         ecb.AddComponent<CarryingBucket>(entity);
                     }
