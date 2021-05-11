ad5acb9e662afd16d8d4ba384f8bad5333827302
Kleber Garcia <kleber.garcia@unity3d.com>
2021-05-11 14:48:55 -0400

    Alternate parabola function

--- Ported/JumpTheGun/Assets/Code/Util/ParabolaUtil.cs
+++ Ported/JumpTheGun/Assets/Code/Util/ParabolaUtil.cs
@@ -18,6 +18,18 @@ public static class ParabolaUtil
     }
 
     /// <summary>
+    /// Creates a parabola with a collision point at time value q and certain height:
+    /// - Contains the points (0, startY) and (1, endY)
+    /// - Reaches the given height at time q.
+    /// </summary>
+    public static void CreateParabolaOverPoint(float startY, float q, float height, float endY, out float a, out float b, out float c)
+    {
+        c = startY;
+        a = height - q * endY + q * c - c / (q * q - q);
+        b = endY - startY - a;
+    }
+
+    /// <summary>
     /// Solves a parabola (in the form y = a*t*t + b*t + c) for y.
     /// </summary>
     public static float Solve(float a, float b, float c, float t)
