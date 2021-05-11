c813b4b64e00de4210febc7ed8f3591816c4cd1d
Alain Lafleur <alain.lafleur@unity3d.com>
2021-05-11 10:32:12 -0400

    First batch of component data

---
+++ Ported/LabCats/Assets/Scripts/Components/CellOffset.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public struct CellOffset : IComponentData
+{
+    public float Value;
+}
---
+++ Ported/LabCats/Assets/Scripts/Components/GridCellSize.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+public struct GridCellSize : IComponentData
+{
+    public float X;
+    public float Y;
+}
