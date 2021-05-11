f344268f3f3cbb3a9e4e3c47095e8b56bf6fea4a
Andre Bastien <andre.bastien@unity3d.com>
2021-05-11 15:10:26 -0400

    add aim turret system

---
+++ Ported/JumpTheGun/Assets/Code/Authoring/Turret.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Generic;
+
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class TurretAuthoring : MonoBehaviour
+    , IConvertGameObjectToEntity
+    , IDeclareReferencedPrefabs
+{
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+    }
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new Turret { });
+
+        dstManager.AddComponent<Translation>(entity);
+        dstManager.AddComponent<Rotation>(entity);
+    }
+}
---
+++ Ported/JumpTheGun/Assets/Code/Systems/TurretAimSystem.cs
@@ -0,0 +1,26 @@
+﻿
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class TankSystemSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+        
+        Entities.
+            WithAll<Turret, Translation, Rotation>()
+            .ForEach((ref Rotation rotation, in Translation turretPosition) =>
+            {
+                Entity player = GetSingletonEntity<Player>();
+                float3 targetPosition = GetComponent<Translation>(player).Value;
+
+                rotation.Value = quaternion.LookRotation(turretPosition.Value, targetPosition);
+            }).Run();
+
+        ecb.Playback(EntityManager);
+        ecb.Dispose();
+    }
+}
