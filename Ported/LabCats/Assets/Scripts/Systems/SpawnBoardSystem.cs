ded6b4f8cff6055435f9c1202bef58a2ca221a85
jeffreyfillingham-unity <71677909+jeffreyfillingham-unity@users.noreply.github.com>
2021-05-11 18:19:48 -0300

    Moving James's code inside the SpawnBoardSystem's Foreach

--- Ported/LabCats/Assets/Scripts/Systems/SpawnBoardSystem.cs
+++ Ported/LabCats/Assets/Scripts/Systems/SpawnBoardSystem.cs
@@ -12,56 +12,17 @@ public class SpawnBoardSystem : SystemBase
 
         var random = new Unity.Mathematics.Random(1234);
 
-        var gameObjectRefs = this.GetSingleton<GameObjectRefs>();
-
-        // set reference to MainCamera gameObject - james
-        var camera = this.GetSingleton<GameObjectRefs>().Camera;
-
-        // orthographic default state for camera - james
-        camera.orthographic = true;
-
-        // adjustment to orthograhpic angle - james
-        var overheadFactor = 1.5f;
-
-        var maxSize = Mathf.Max(NumberRows, NumberColumns);
-        var maxCellSize = Mathf.Max(CellSize.x, CellSize.y);
-
-        // set the orthographic size based on board and cell dimensions - james
-        camera.orthographicSize = maxSize * maxCellSize * .65f;
-
-        // scale based on board dimensions - james
-        var posXZ = Vector2.Scale(new Vector2(NumberRows, NumberColumns) * 0.5f, CellSize);
-
-        // hold position value adjusted by dimensions of board - james
-        float3 camPosition = new Vector3(0, maxSize * maxCellSize * overheadFactor, 0);
-
-        // set camera position to modified value - james
-        camera.transform.position = camPosition;
-
-        // set camera to look at board center - james
-        camera.transform.LookAt(new Vector3(posXZ.x, 0f, posXZ.y));
-
         Entities
             .WithNone<BoardInitializedTag>()
-            .ForEach((Entity entity, ref GameData gameData) =>
+            .WithoutBurst()
+            .ForEach((Entity entity, ref GameData gameData, in BoardDefinition boardDefinition) =>
             {
-                var boardEntity = ecb.CreateEntity(); // ???
-
-                var boardDefinition = new BoardDefinition
-                {
-                    // TODO: Get the following from authoring:
-                    CellSize = 1,
-                    NumberColumns = 13,
-                    NumberRows = 13,
-                };
-                ecb.AddComponent(boardEntity, boardDefinition);
-
                 var firstCellPosition = new FirstCellPosition
                 {
                     // TODO: Also get the following from authoring:
                     Value = new float3(0, 0, 0)
                 };
-                ecb.AddComponent(boardEntity, firstCellPosition);
+                ecb.AddComponent(entity, firstCellPosition);
 
                 // TODO: Add time?
 
@@ -71,6 +32,27 @@ public class SpawnBoardSystem : SystemBase
                 // TODO: Set up holes
                 // ...
                 // TODO: Set up Game Data
+
+                // Setup camera
+                var gameObjectRefs = this.GetSingleton<GameObjectRefs>();
+                var camera = gameObjectRefs.Camera;
+                camera.orthographic = true;
+                var overheadFactor = 1.5f;
+
+                var maxSize = Mathf.Max(boardDefinition.NumberRows, boardDefinition.NumberColumns);
+                var maxCellSize = boardDefinition.CellSize;
+                camera.orthographicSize = maxSize * maxCellSize * .65f;
+
+                // scale based on board dimensions - james
+                var posXZ = Vector2.Scale(new Vector2(boardDefinition.NumberRows, boardDefinition.NumberColumns) * 0.5f, new Vector2(boardDefinition.CellSize, boardDefinition.CellSize));
+
+                // hold position value adjusted by dimensions of board
+                float3 camPosition = new Vector3(0, maxSize * maxCellSize * overheadFactor, 0);
+                camera.transform.position = camPosition;
+
+                // set camera to look at board center
+                camera.transform.LookAt(new Vector3(posXZ.x, 0f, posXZ.y));
+
 
                 // Only run on first frame the BoardInitializedTag is not found. Add it so we don't run again
                 ecb.AddComponent(entity, new BoardInitializedTag());
