148d06ec77c5b869be740f4949ac1ccc9887c76f
Simon Bouvier-Zappa <simonbz@unity3d.com>
2021-05-11 14:41:41 -0400

    Updated materials

--- Ported/JumpTheGun/Assets/Code/Systems/BulletMovementSystem.cs
+++ Ported/JumpTheGun/Assets/Code/Systems/BulletMovementSystem.cs
@@ -9,12 +9,12 @@ public class BulletMovementSystem : SystemBase
     {
         Entities
             .WithAll<Bullet, Arc, Time>()
-            .WithAll<Position, TargetPosition, TimeOffset>().ForEach(
-            (ref Translation translation, in Time t, in Position pos, in TargetPosition targetPos, in Arc arc, in TimeOffset timeOffset) =>
+            .WithAll<Translation, TargetPosition, TimeOffset>().ForEach(
+            (ref Translation translation, in Time t, in TargetPosition targetPos, in Arc arc, in TimeOffset timeOffset) =>
             {
                 var timeInParabola =  System.Math.Min(t.Value - timeOffset.Value, 0.0f);
                 float yInParabola = ParabolaUtil.Solve(arc.Value.x, arc.Value.y, arc.Value.z, timeInParabola);
-                float3 position = math.lerp(pos.Value, targetPos.Value, timeInParabola);
+                float3 position = math.lerp(translation.Value, targetPos.Value, timeInParabola);
                 position.y = yInParabola;
                 translation.Value = position;
             }).ScheduleParallel();
