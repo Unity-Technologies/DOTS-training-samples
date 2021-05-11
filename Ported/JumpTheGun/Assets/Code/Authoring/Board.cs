9f5d39a3b0458283ba64d76c91d3cbe329293870
Andre Bastien <andre.bastien@unity3d.com>
2021-05-11 16:01:48 -0400

    add platform color update

--- Ported/JumpTheGun/Assets/Code/Authoring/Board.cs
+++ Ported/JumpTheGun/Assets/Code/Authoring/Board.cs
@@ -1,19 +1,32 @@
 
 using Unity.Entities;
+using UnityEngine;
 
 [GenerateAuthoringComponent]
 public struct Board : IComponentData
 {
+    [Range(1, 1000)]
     public int SizeX;
+
+    [Range(1, 1000)]
     public int SizeY;
 
+    [Range(0.5F, 20F)]
     public float MinHeight;
+
+    [Range(0.5F, 20F)]
     public float MaxHeight;
 
+    [Range(0.1F, 20F)]
     public float ReloadTime;
+
+    [Range(0.1F, 20F)]
     public float Radius;
 
+    [Range(1, 10000)]
     public int NumberOfTanks;
+
+    [Range(0.1F, 5F)]
     public float HitStrength;
     
     public Entity PlaformPrefab;
---
+++ Ported/JumpTheGun/Assets/Code/Util/Colorize.cs
@@ -0,0 +1,20 @@
+﻿
+using Unity.Mathematics;
+
+public static class Colorize
+{
+    public static float4 MIN_HEIGHT_COLOR = new float4(0F, 1F, 0F, 1F);
+    public static float4 MAX_HEIGHT_COLOR = new float4(0.388F, 0.184F, 0F, 1F);
+
+    public static float4 Platform(float height, float minHeight, float maxHeight)
+    {
+        if (height - 0.0001 <= minHeight)
+        {
+            return MIN_HEIGHT_COLOR;
+        }
+        else
+        {
+            return math.lerp(MIN_HEIGHT_COLOR, MAX_HEIGHT_COLOR, (height - minHeight) / (maxHeight - minHeight));
+        }
+    }
+}
