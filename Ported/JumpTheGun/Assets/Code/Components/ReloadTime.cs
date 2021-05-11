6a4c062766b369035993b700f934845b9149c10d
Andre Bastien <andre.bastien@unity3d.com>
2021-05-11 11:29:08 -0400

    add tank authoring

--- Ported/JumpTheGun/Assets/Code/Components/ReloadTime.cs
+++ Ported/JumpTheGun/Assets/Code/Components/ReloadTime.cs
@@ -2,5 +2,5 @@ using Unity.Entities;
 
 struct ReloadTimes : IComponentData
 {
-    float Value;
+    public float Value;
 }
--- Ported/JumpTheGun/Assets/Code/Components/Tank.cs
+++ Ported/JumpTheGun/Assets/Code/Components/Tank.cs
@@ -2,4 +2,5 @@ using Unity.Entities;
 
 public struct Tank : IComponentData
 {
+    public Entity Prefab;
 }
