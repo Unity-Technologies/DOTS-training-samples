b9cf533871e02e2ace9d040cb7900809c400614f
Simon Bouvier-Zappa <simonbz@unity3d.com>
2021-05-13 12:19:37 -0400

    Quick fix for hit detection.

--- Ported/JumpTheGun/Assets/Code/Util/CoordUtils.cs
+++ Ported/JumpTheGun/Assets/Code/Util/CoordUtils.cs
@@ -46,8 +46,8 @@ public static class CoordUtils
     public static int2 WorldToBoardPosition(in float3 position, in BoardSize boardSize, in float3 boardWorldOffset)
     {
         float3 localPos = position - boardWorldOffset;
-        float boardX = Mathf.Floor(localPos.x / (float)boardSize.Value.x);
-        float boardY = Mathf.Floor(localPos.y / (float)boardSize.Value.y);
+        float boardX = Mathf.Floor(localPos.x);
+        float boardY = Mathf.Floor(localPos.y);
         return new int2((int)boardX, (int)boardY);
     }
 }
