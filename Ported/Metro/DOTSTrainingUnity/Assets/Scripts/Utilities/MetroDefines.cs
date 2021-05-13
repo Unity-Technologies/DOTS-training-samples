9e3a750aa5ee671694d8a05bcb0567c6c2d0ccde
ColeIngram <cole.ingram@unity3d.com>
2021-05-13 14:29:09 -0700

    widen train lines

--- Ported/Metro/DOTSTrainingUnity/Assets/Scripts/Utilities/MetroDefines.cs
+++ Ported/Metro/DOTSTrainingUnity/Assets/Scripts/Utilities/MetroDefines.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MetroDefines {
     public static float CUSTOMER_SATISFACTION = 1f;
     public static float BEZIER_HANDLE_REACH = 0.1f;
-    public static float BEZIER_PLATFORM_OFFSET = 3f;
+    public static float BEZIER_PLATFORM_OFFSET = 12f;
     public static float PLATFORM_ADJACENCY_LIMIT = 12f;
     public const int BEZIER_MEASUREMENT_SUBDIVISIONS = 2;
     public const float PLATFORM_ARRIVAL_THRESHOLD = 0.975f;
