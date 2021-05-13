79954a0652bc056c701f4d44227dd8b6f0d3d3d5
Jody McAdams <jody.mcadams@unity3d.com>
2021-05-13 09:04:07 -0700

    Trains have doors and they open/close

--- Ported/Metro/DOTSTrainingUnity/Assets/Scripts/Authoring/TrainCarAuthoring.cs
+++ Ported/Metro/DOTSTrainingUnity/Assets/Scripts/Authoring/TrainCarAuthoring.cs
@@ -9,7 +9,9 @@ public class TrainCarAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     public int trainCarIndex = 0;
     public UnityEngine.Color color;
     public GameObject theTrainEngine;
-    
+    public GameObject doorLeft;
+    public GameObject doorRight;
+
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
         dstManager.AddComponents(entity, new ComponentTypes(typeof(TrainCarIndex), typeof(TrainEngineRef), typeof(Color)));
@@ -19,5 +21,6 @@ public class TrainCarAuthoring : MonoBehaviour, IConvertGameObjectToEntity
         dstManager.SetComponentData(entity, new TrainEngineRef(){value = trainEntity});
         
         dstManager.SetComponentData(entity, new Color() {value = new float4(color.r, color.g, color.b, color.a)});
+        dstManager.AddComponentData(entity, new DoorsRef() { doorEntLeft = conversionSystem.GetPrimaryEntity(doorLeft), doorEntRight = conversionSystem.GetPrimaryEntity(doorRight)});
     }
 }
