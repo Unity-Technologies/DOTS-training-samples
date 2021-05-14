05454fa9b24281c2806bb6337d451aa02ea4b9e0
ColeIngram <cole.ingram@unity3d.com>
2021-05-13 17:40:07 -0700

    Added Distance table and applied it to the track itself (not the trains yet, got to copy it to native arrays)

--- Ported/Metro/DOTSTrainingUnity/Assets/Scripts/Utilities/RailGeneration.cs
+++ Ported/Metro/DOTSTrainingUnity/Assets/Scripts/Utilities/RailGeneration.cs
@@ -73,7 +73,7 @@ public struct Line
         for (int i = total_outboundPoints - 1; i >= 0; i--)
         {
             BezierPoint point = bezierPath.points[i];
-            Vector3 _targetLocation = bezierPath.GetPoint_PerpendicularOffset(ref point, platformOffset);
+            Vector3 _targetLocation = bezierPath.GetPoint_PerpendicularOffset(ref point, platformOffset, false);
             bezierPath.points[i] = point;
             bezierPath.AddPoint(_targetLocation);
         }
@@ -152,9 +152,9 @@ public class RailGeneration : MonoBehaviour
                     // convert distance value to 0 - 1 range
                     float distAsInterpolant = dist / _newLine.bezierPath.GetPathDistance();
 
-                    Vector3 railPosition = _newLine.bezierPath.Get_Position(distAsInterpolant);
+                    Vector3 railPosition = _newLine.bezierPath.Get_Position(distAsInterpolant, true);
 
-                    Vector3 normalAtRailPosition = _newLine.bezierPath.Get_NormalAtPosition(distAsInterpolant);
+                    Vector3 normalAtRailPosition = _newLine.bezierPath.Get_NormalAtPosition(distAsInterpolant, true);
                     
                     // rail that has been created
                     GameObject _rail = GameObject.Instantiate(prefabRail);
