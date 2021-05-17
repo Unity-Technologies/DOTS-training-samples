ed5670f20a186ae873d80093b5fd3c3829792051
Anthony Sturdy <anthonysturdy123@gmail.com>
2021-05-17 15:59:09 +0100

    Added WallSpawner, Changed PlayerInput Reset to Add Respawn component

--- Ported/Assets/Components/SimulationSpeed.cs
+++ Ported/Assets/Components/SimulationSpeed.cs
@@ -1,6 +1,7 @@
 using Unity.Entities;
 using Unity.Mathematics;
 
+[GenerateAuthoringComponent]
 public struct SimulationSpeed : IComponentData
 {
     public float Value;
--- Ported/Assets/Systems/PlayerInput.cs
+++ Ported/Assets/Systems/PlayerInput.cs
@@ -1,7 +1,9 @@
 using Unity.Collections;
 using Unity.Entities;
+using UnityEditor.SceneManagement;
 using UnityInput = UnityEngine.Input;
 using UnityKeyCode = UnityEngine.KeyCode;
+using UnityDebug = UnityEngine.Debug;
 
 public class PlayerInput : SystemBase
 {
@@ -14,13 +16,13 @@ public class PlayerInput : SystemBase
         // Loop through numeric keycodes and assign Simulation Speed upon match
         for( int i = (int) UnityKeyCode.Alpha1 ; i < (int) UnityKeyCode.Alpha9 ; ++i )
         {
+            
             if(UnityInput.GetKeyDown((UnityKeyCode)i))
             {
                 SimulationSpeed newSimSpeed = new SimulationSpeed
                 {
                     Value = i
                 };
-                
                 SetComponent(simSpeedEntity, newSimSpeed);
             }
         }
@@ -30,6 +32,7 @@ public class PlayerInput : SystemBase
         var ecb = new EntityCommandBuffer(Allocator.Temp);
         if (UnityInput.GetKeyDown(UnityKeyCode.R))
         {
+            // Create group query
             var antQuery = new EntityQueryDesc
             {
                 All = new ComponentType[] { typeof(Position), typeof(Direction) }
@@ -41,11 +44,20 @@ public class PlayerInput : SystemBase
 
             antWallQueryGroup = GetEntityQuery(new EntityQueryDesc[] {antQuery, wallQuery});
             
+            // Destroy all Walls and Ants
             Entities
                 .WithStoreEntityQueryInField(ref antWallQueryGroup)
                 .ForEach((Entity entity) =>
                 {
                     ecb.DestroyEntity(entity);
+                }).Run();
+            
+            // Add Respawn component to AntSpawner
+            Entities
+                .WithAny<AntSpawner, WallSpawner>()
+                .ForEach((Entity entity) =>
+                {
+                    ecb.AddComponent<Respawn>(entity);
                 }).Run();
         }
         ecb.Playback(EntityManager);
