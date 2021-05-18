919a1f5c5418159870b7e1692160e17568fe79ab
Niklas Höglund <niklas.hoglund@unity3d.com>
2021-05-18 13:55:16 +0100

    Add food source and change ant colour to yellow when they reach the food source

---
+++ Ported/Assets/Components/FoodSource.cs
@@ -0,0 +1,6 @@
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct FoodSource : IComponentData
+{
+}
--- Ported/Assets/Systems/AntMovementSystem.cs
+++ Ported/Assets/Systems/AntMovementSystem.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Rendering;
 using Unity.Transforms;
@@ -12,16 +13,24 @@ using Random = Unity.Mathematics.Random;
 
 public class AntMovementSystem : SystemBase
 {
+    private EntityCommandBufferSystem m_ECBSystem;
+
+    protected override void OnCreate()
+    {
+        m_ECBSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
     protected override void OnUpdate()
     {
         const float maxDirectionChangePerSecond = 5.0f;
-
+        const float foodSourceRadius = 2.5f;
+        
         var random = new Random((uint)(Time.ElapsedTime * 1000f + 1f));
         var deltaTime = Time.DeltaTime;
         var simulationSpeedEntity = GetSingletonEntity<SimulationSpeed>();
         var simulationSpeed = GetComponent<SimulationSpeed>(simulationSpeedEntity).Value;
 
-        Entities
+        Dependency = Entities
             .WithAll<Ant>()
             .ForEach((Entity entity, ref Translation translation, ref Direction direction, ref Rotation rotation) =>
             {
@@ -32,6 +41,30 @@ public class AntMovementSystem : SystemBase
                                                        maxDirectionChangePerSecond * deltaTime * simulationSpeed);
                 
                 rotation = new Rotation {Value = quaternion.RotateZ(direction.Radians)};
-            }).Schedule();
+            }).ScheduleParallel(Dependency);
+
+        Dependency.Complete();
+        
+        var foodSource = GetSingletonEntity<FoodSource>();
+        var foodSourceTranslation = GetComponent<Translation>(foodSource);
+        var foodPos = new Vector2(foodSourceTranslation.Value.x, foodSourceTranslation.Value.y);
+        
+        var ecb = m_ECBSystem.CreateCommandBuffer().AsParallelWriter();
+        
+        Dependency = Entities
+            .WithAll<Ant>()
+            .WithNone<CarryingFood>()
+            .ForEach((Entity entity, int entityInQueryIndex, ref URPMaterialPropertyBaseColor color, in Translation translation) =>
+            {
+                var pos = new Vector2(translation.Value.x, translation.Value.y);
+                if (Vector2.Distance(pos, foodPos) < foodSourceRadius)
+                {
+                    color.Value = new float4(1, 1, 0, 0);
+                    
+                    ecb.AddComponent<CarryingFood>(entityInQueryIndex, entity);
+                }
+            }).ScheduleParallel(Dependency);
+        
+        Dependency.Complete();
     }
 }
