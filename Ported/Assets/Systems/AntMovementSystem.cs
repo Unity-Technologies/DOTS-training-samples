cbc368006aa9520f73c2aa0b43d7fc4aff1fb768
Niklas Höglund <niklas.hoglund@unity3d.com>
2021-05-18 12:13:57 +0100

    Make the ants turn in random directions

--- Ported/Assets/Systems/AntMovementSystem.cs
+++ Ported/Assets/Systems/AntMovementSystem.cs
@@ -14,17 +14,24 @@ public class AntMovementSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        var random = new Random(1234);
+        const float maxDirectionChangePerSecond = 5.0f;
+
+        var random = new Random((uint)(Time.ElapsedTime * 1000f + 1f));
         var deltaTime = Time.DeltaTime;
         var simulationSpeedEntity = GetSingletonEntity<SimulationSpeed>();
         var simulationSpeed = GetComponent<SimulationSpeed>(simulationSpeedEntity).Value;
 
         Entities
             .WithAll<Ant>()
-            .ForEach((Entity entity, ref Translation translation, in Direction direction) =>
+            .ForEach((Entity entity, ref Translation translation, ref Direction direction, ref Rotation rotation) =>
             {
                 var delta = new float3(Mathf.Cos(direction.Radians), Mathf.Sin(direction.Radians), 0);
                 translation.Value += delta * deltaTime * simulationSpeed;
+
+                direction.Radians += random.NextFloat(-maxDirectionChangePerSecond * deltaTime * simulationSpeed,
+                                                       maxDirectionChangePerSecond * deltaTime * simulationSpeed);
+                
+                rotation = new Rotation {Value = quaternion.RotateZ(direction.Radians)};
             }).Schedule();
     }
 }
