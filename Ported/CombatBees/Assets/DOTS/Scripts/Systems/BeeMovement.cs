0a4a1e027b3adeb2a18e7a9c4bbde2e2bc0564b7
Bianca Marina Stana <bianca.stana@unity3d.com>
2021-05-20 10:00:48 +0300

    Refactored code

--- Ported/CombatBees/Assets/DOTS/Scripts/Systems/BeeMovement.cs
+++ Ported/CombatBees/Assets/DOTS/Scripts/Systems/BeeMovement.cs
@@ -15,7 +15,7 @@ using UnityRangeAttribute = UnityEngine.RangeAttribute;
 [UpdateAfter(typeof(BeePerception))]
 public class BeeMovement : SystemBase
 {
-    const float maxRotation =180f;
+    const float maxRotation = 180f;
     const float distanceMaxSpeed = 4f;
     
     [ReadOnly]
