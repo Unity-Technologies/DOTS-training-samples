f0015e4fe361f0df75ef2adc8d3e7e6d87ffa30c
Arttu Peltonen <arttu.peltonen@unity3d.com>
2021-05-20 12:38:37 +0300

    Allow deleting your own arrows.

--- Ported/dots-training-2021-05-eu-group1_Labsrat/Assets/Scripts/Systems/ArrowSpawnerSystem.cs
+++ Ported/dots-training-2021-05-eu-group1_Labsrat/Assets/Scripts/Systems/ArrowSpawnerSystem.cs
@@ -19,6 +19,19 @@ public class ArrowSpawnerSystem : SystemBase
     {
         m_EcbSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
     }
+
+    static void RemoveArrow(ref DynamicBuffer<CreatedArrowData> arrowBuffer, ref EntityCommandBuffer ecb)
+    {
+        // Destroy arrow entity
+        Entity arrowToDestroy = arrowBuffer[0].CreatedArrow;
+        ecb.DestroyEntity(arrowToDestroy);
+                    
+        // Reset tile forced direction
+        Entity arrowTile = arrowBuffer[0].TileEntity;
+        ecb.SetComponent(arrowTile, new ForcedDirection());
+                    
+        arrowBuffer.RemoveAt(0);
+    }
     
     protected override void OnUpdate()
     {
@@ -39,8 +52,18 @@ public class ArrowSpawnerSystem : SystemBase
                 Entity cellEntity = cellArray[playerInput.TileIndex];
                 var cellTranslation = translationData[cellEntity];
 
-                // Don't allow duplicate arrows
-                if (forcedDirectionData[cellEntity].Value != Cardinals.None)
+                bool foundOwnArrow = false;
+                foreach (var alreadyCreatedArrows in ArrowBuffer)
+                {
+                    if (alreadyCreatedArrows.TileEntity == cellEntity)
+                    {
+                        RemoveArrow(ref ArrowBuffer, ref ecb);
+                        foundOwnArrow = true;
+                    }
+                }
+
+                if (foundOwnArrow || // Removed our own arrow
+                    !foundOwnArrow && forcedDirectionData[cellEntity].Value != Cardinals.None) // Don't allow clearing other players arrows
                     return;
 
                 Entity arrowEntity = ecb.Instantiate(gameConfig.ArrowPrefab);
@@ -63,15 +86,7 @@ public class ArrowSpawnerSystem : SystemBase
 
                 if (ArrowBuffer.Length > gameConfig.MaximumArrows-1)
                 {
-                    // Destroy arrow entity
-                    Entity arrowToDestroy = ArrowBuffer[0].CreatedArrow;
-                    ecb.DestroyEntity(arrowToDestroy);
-                    
-                    // Reset tile forced direction
-                    Entity arrowTile = ArrowBuffer[0].TileEntity;
-                    ecb.SetComponent(arrowTile, new ForcedDirection());
-                    
-                    ArrowBuffer.RemoveAt(0);
+                    RemoveArrow(ref ArrowBuffer, ref ecb);
                 }
 
                 switch (direction)
