ad63a784f2d094448895e81f43452fed3f3b373f
philcossou <philippec@unity3d.com>
2021-07-27 14:16:28 -0400

    Add Ant Components

---
+++ Ported/AntPheromones/Assets/Scripts/Components/Brightness.cs
@@ -0,0 +1,6 @@
+using Unity.Entities;
+
+public struct Brightness : IComponentData
+{
+    private float Value;
+}
---
+++ Ported/AntPheromones/Assets/Scripts/Components/FacingAngle.cs
@@ -0,0 +1,6 @@
+using Unity.Entities;
+
+public struct FacingAngle : IComponentData
+{
+    private float Value;
+}
