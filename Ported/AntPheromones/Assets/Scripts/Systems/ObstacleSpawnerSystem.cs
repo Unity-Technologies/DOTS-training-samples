4a64b215c7f8a5ca7e457cd1922ac94186bb5355
Marc-Olivier Belisle <marcolivier.belisle@unity3d.com>
2021-07-27 13:38:24 -0400

    Add Obstacle Spawner System issue with rendering

--- Ported/AntPheromones/Assets/Scripts/Systems/ObstacleSpawnerSystem.cs
+++ Ported/AntPheromones/Assets/Scripts/Systems/ObstacleSpawnerSystem.cs
@@ -6,14 +6,51 @@ using Unity.Transforms;
 
 public class ObstacleSpawnerSystem : SystemBase
 {
+    const int mapSize = 128;
+
     protected override void OnUpdate()
     {
-        var ecb = new EntityCommandBuffer(Allocator.Temp);
+        using (var ecb = new EntityCommandBuffer(Allocator.Temp))
+        {
+            Random rand = new Random(1234);
 
-        Entities
-            .ForEach((Entity entity, in ObstacleSpawner map) =>
-            {
-                // Add logic for creating the obstacle rings
-            }).Run();
+            Entities
+                .ForEach((Entity entity, in ObstacleSpawner spawner) =>
+                {
+                    // Add logic for creating the obstacle rings
+                    ecb.DestroyEntity(entity);
+
+                    for (int i = 0; i < spawner.obstacleRingCount; i++)
+                    {
+                        float ringRadius = (i / (spawner.obstacleRingCount + 1f)) * (mapSize * .5f);
+                        float circumference = ringRadius * 2f * math.PI;
+                        int maxCount = (int)math.ceil(circumference / (2f * spawner.ObstacleRadius) * 2f);
+                        int offset = rand.NextInt(0, maxCount);
+                        int holeCount = rand.NextInt(1, 3);
+                        for (int j = 0; j < maxCount; j++)
+                        {
+                            float t = (float)j / maxCount;
+                            if ((t * holeCount) % 1f < spawner.ObstaclesPerRing)
+                            {
+                                float angle = (j + offset) / (float)maxCount * (2f * math.PI);
+
+                                var obstacleEntity = ecb.Instantiate(spawner.ObstaclePrefab);
+                                ecb.SetComponent(obstacleEntity, new Translation
+                                {
+                                    Value = new float3(mapSize * .5f + math.cos(angle) * ringRadius, mapSize * .5f + math.sin(angle) * ringRadius, 0)
+                                });
+                                //ecb.AddComponent(obstacleEntity, new Scale
+                                //{
+                                //    Value = spawner.ObstacleRadius
+                                //});
+                            }
+                        }
+                    }
+
+                }).Run();
+
+
+            ecb.Playback(EntityManager);
+        }
     }
 }
