530598e334a40c150bbdc4d85593626940fa7af9
Nicolas Lebedenco <nicolas@lebedenco.net>
2021-07-27 04:55:25 -0400

    .

--- Ported/BucketBrigade/Assets/src/Systems/OmniWorkerSystem.cs
+++ Ported/BucketBrigade/Assets/src/Systems/OmniWorkerSystem.cs
@@ -15,34 +15,11 @@ public class OmniWorkerSystem: SystemBase
             .WithNone<WorkerIsHoldingBucket>()            
             .ForEach((ref Translation translation, ref Position position) =>
             {
-                var query = GetEntityQuery(new EntityQueryDesc {
-                    All = new ComponentType[] { typeof(src.Components.Bucket) }
-                });
-                try
-                {
-                    var chunks = query.CreateArchetypeChunkArray(Allocator.Persistent);
-                    try
-                    {
-                        var bucketTypeHandle = GetComponentTypeHandle<src.Components.Bucket>(false);
-                        for (int i = 0; i < chunks.Length; ++i)
-                        {
-                            var buckets = chunks[i].GetNativeArray(bucketTypeHandle);
-                            // Get nearest empty bucket: O(n)
-                            // If close enough to the bucket
-                            //     pick it up
-                            // else
-                            //     move towards the nearest bucket
-                        }
-                    }
-                    finally
-                    {
-                        chunks.Dispose();
-                    }
-                }
-                finally
-                {
-                    query.Dispose();
-                }
+                // Get nearest empty bucket
+                // If close enough to the bucket
+                //     pick it up
+                // else
+                //     move towards the nearest bucket
             }).ScheduleParallel(Dependency);
 
         var updateWorkerWithBucket = Entities
@@ -52,43 +29,14 @@ public class OmniWorkerSystem: SystemBase
             {
                 // Get bucket reference
                 // If bucket is full
-                if (true)
-                {
-                    //     Get nearest fire spot
-                    //     If not close enough to the fire spot 
-                    //         move towards the fire spot                   
-                }
-                else
-                {
-                    var query = GetEntityQuery(new EntityQueryDesc
-                    {
-                        All = new ComponentType[] { typeof(src.Components.WaterTag) }
-                    });
-                    try
-                    {
-                        var chunks = query.CreateArchetypeChunkArray(Allocator.Persistent);
-                        try
-                        {
-                            var waterTypeHandle = GetComponentTypeHandle<src.Components.Bucket>(false);
-                            for (int i = 0; i < chunks.Length; ++i)
-                            {
-                                var waterSources = chunks[i].GetNativeArray(waterTypeHandle);
-                                // Get nearest water spot
-                                // If not close enough to the spot 
-                                //      move towards the water spot                
-                            }
-                        }
-                        finally
-                        {
-                            chunks.Dispose();
-                        }
-                    }
-                    finally
-                    {
-                        query.Dispose();
-                    }
-                }                             
-            }).ScheduleParallel(Dependency);
+                //     Get nearest fire spot
+                //     If not close enough to the fire spot 
+                //         move towards the fire spot                   
+                // Else
+                // Get nearest water spot
+                // If not close enough to the spot 
+                //      move towards the water spot                
+            }).ScheduleParallel(updateWorkersWithoutBucket);
 
         Dependency = updateWorkerWithBucket;
     }
