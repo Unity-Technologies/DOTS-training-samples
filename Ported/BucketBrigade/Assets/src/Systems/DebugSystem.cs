4e4a25994f27e3ae441828d804b3c488c0c1d6b7
Tomas Dirvanauskas <tomas@unity3d.com>
2021-07-27 16:23:16 +0300

    Add Debug code

---
+++ Ported/BucketBrigade/Assets/src/Systems/DebugSystem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using src.Components;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace src.Systems
+{
+    [UpdateInGroup(typeof(SimulationSystemGroup))]
+    [AlwaysUpdateSystem]
+    public class DebugSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities
+                .WithName("DrawFetchersNoBuckets")
+                .WithAll<BucketFetcherWorkerTag>()
+                .WithNone<WorkerIsHoldingBucket>()
+                .ForEach((in Position pos) =>
+            {
+                Debug.DrawLine(new Vector3(pos.Value.x, 0.0f, pos.Value.y ),
+                    new Vector3(pos.Value.x, 10.0f, pos.Value.y), Color.red);
+            }).Run();
+
+            Entities
+                .WithName("DrawFetchersWithBuckets")
+                .WithAll<BucketFetcherWorkerTag, WorkerIsHoldingBucket>()
+                .ForEach((in Position pos) =>
+                {
+                    Debug.DrawLine(new Vector3(pos.Value.x, 0.0f, pos.Value.y),
+                        new Vector3(pos.Value.x, 10.0f, pos.Value.y), Color.yellow);
+                }).Run();
+
+            Entities
+                .WithoutBurst()
+                .WithName("DrawBuckets")
+                .WithAll<EcsBucket>()
+                .ForEach((in Position pos) =>
+                {
+                    DrawCross(new Vector3(pos.Value.x, 5.0f, pos.Value.y), 2, Color.blue);
+                }).Run();
+
+
+        }
+
+
+        private void DrawCross(Vector3 position, float size, Color color)
+        {
+            Debug.DrawLine(position - Vector3.up * size, position + Vector3.up * size, color);
+            Debug.DrawLine(position - Vector3.right * size, position + Vector3.right * size, color);
+            Debug.DrawLine(position - Vector3.forward * size, position + Vector3.forward * size, color);
+        }
+    }
+}
