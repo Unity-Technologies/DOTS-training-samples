60018d057c9145a0859cbe94c30df489c27af42b
Anthony-Unity <anthony.davis@unity3d.com>
2021-07-28 16:44:36 -0400

    Update CannonballBoxCollisionSystem.cs

--- Ported/JumpTheGunPorted/Assets/Scripts/Systems/CannonballBoxCollisionSystem.cs
+++ Ported/JumpTheGunPorted/Assets/Scripts/Systems/CannonballBoxCollisionSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Unity.Entities;
+using Unity.Collections;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
@@ -9,45 +10,27 @@ public class CannonballBoxCollisionSystem : SystemBase
 
     protected override void OnCreate()
     {
-        var query = GetEntityQuery(typeof(Cannonball), typeof(Translation), typeof(NonUniformScale));
+        var query = GetEntityQuery(typeof(Cannonball));
         RequireForUpdate(query);
-
-        m_ECBSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
     }
 
     protected override void OnUpdate()
     {
-        /* TODO: this is copied from breakout for reference
-        var balls = GetEntitys<Cannonball>();
-        var ballPos = GetComponent<Translation>(ball);
-        var ballScale = GetComponent<NonUniformScale>(ball);
-
-        // TODO:
-        //var ballAabb = new AABB2D { Center = ballPos.Value.xy, Extents = ballScale.Value.xy / 2 };
-
-        var ecb = m_ECBSystem.CreateCommandBuffer().AsParallelWriter();
+        var ecb = new EntityCommandBuffer(Allocator.TempJob);
 
         Entities
-            .WithName("cannonball_box_collision_test")
-            .WithAll<Box>()
-            .ForEach((
-                Entity brickEntity,
-                int entityInQueryIndex,
-                in Translation boxTranslation,
-                in NonUniformScale boxScale) =>
+            .WithoutBurst()
+            .WithAll<Cannonball>()
+            .ForEach((Entity entity, in ParabolaTValue tValue) =>
             {
-                // TODO:
-                var brickAabb = new AABB2D { Center = boxTranslation.Value.xy, Extents = boxScale.Value.xy / 2 };
-                var axisFlip = new float2();
-
-                if (brickAabb.Intersects(ballAabb, ref axisFlip))
+                UnityEngine.Debug.Log(tValue.Value);
+                if (tValue.Value >= 0.97f)
                 {
-                    ecb.SetComponent(entityInQueryIndex, ball, new Velocity2D { Value = ballVelocity.Value * axisFlip });
-                    ecb.DestroyEntity(entityInQueryIndex, brickEntity);
+                    ecb.DestroyEntity(entity);
                 }
-            }).ScheduleParallel();
-
-        m_ECBSystem.AddJobHandleForProducer(Dependency);
-        */
+            }).Schedule();
+        Dependency.Complete();
+        ecb.Playback(EntityManager);
+        ecb.Dispose();
     }
 }
