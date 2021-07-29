f1985e8bb5295bed764ccd372bb1f34497db5fc2
Mod Rimkus <mod.rimkus@unity3d.com>
2021-07-29 15:04:46 -0400

    Improved AI's arrow direction picking logic

--- Ported/DOTSLabRat/Assets/Scripts/Utility/Utils.cs
+++ Ported/DOTSLabRat/Assets/Scripts/Utility/Utils.cs
@@ -56,5 +56,37 @@ namespace DOTSRATS
             return new Vector2((viewportPosition.x * canvasRect.sizeDelta.x) - (canvasRect.sizeDelta.x * 0.5f),
                 (viewportPosition.y * canvasRect.sizeDelta.y) - (canvasRect.sizeDelta.y * 0.5f));
         }
+        
+        public static int2 PlayerNumberToGoalCoord(int playerNumber, int boardSize)
+        {
+            var halfSize = boardSize / 2;
+            var midCoord = halfSize + (boardSize % 2 == 0 ? 1 : 2);
+            var offsetFromCenter = halfSize / 6;
+
+            var goalCoords = new int4(halfSize - offsetFromCenter - 1, midCoord + offsetFromCenter - 1,
+                                       halfSize - offsetFromCenter - 1, midCoord + offsetFromCenter - 1);
+
+            var playerGoal = new int2(playerNumber % 2 == 0 ? goalCoords[0] : goalCoords[1],
+                                      playerNumber < 2 ? goalCoords[2] : goalCoords[3]);
+            return playerGoal;
+        }
+
+        public static Direction GetCardinalDirectionFromIntDirection(int2 direction)
+        {
+            if (math.abs(direction.x) > math.abs(direction.y))
+            {
+                if (direction.x < 0)
+                    return Direction.West;
+                else
+                    return Direction.East;
+            }
+            else
+            {
+                if (direction.y < 0)
+                    return Direction.South;
+                else
+                    return Direction.North;
+            }
+        }
     }
 }
