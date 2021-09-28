ef07d4fc63fae2413e4c44b4f04465a2f01776a9
Rasmus Selsmark <rasmus@unity3d.com>
2021-09-28 16:49:58 +0200

    Simple implementation of slowing down when approaching platform

--- Original/Metro/Assets/dots_src/Systems/TrainMovementSystem.cs
+++ Original/Metro/Assets/dots_src/Systems/TrainMovementSystem.cs
@@ -10,20 +10,46 @@ public partial class TrainMovementSystem : SystemBase
     {
         var splineData = GetSingleton<SplineDataReference>().BlobAssetReference;
         
-        Entities.WithAll<TrainMovement>().ForEach((ref Translation translation, ref Rotation rotation, ref TrainMovement movement, in LineIndex lineIndex) =>
+        Entities.ForEach((ref Translation translation, ref Rotation rotation, ref TrainMovement movement, in LineIndex lineIndex) =>
         {
             float speed = 0.05f; //from global settings singleton
 
-            movement.position += speed;
+            ref var points = ref splineData.Value.splineBlobAssets[lineIndex.Index].points;
+            ref var platformPositions = ref splineData.Value.splineBlobAssets[lineIndex.Index].platformPositions;
+            
+            // TODO: platforms are positioned 0..1 range (relative to total track length). currently our train
+            // position is absolute in terms of track length, e.g. 0..25 
+            var trackRelativePosition = movement.position / points.Length;
+            if (IsApproachingPlatform(trackRelativePosition, ref platformPositions))
+            {
+                Debug.Log($"Train on line #{lineIndex.Index} is approaching platform, slowing down!");
+                speed = 0.005f;
+            }
 
-            if (movement.position > splineData.Value.splineBlobAssets[lineIndex.Index].points.Length - 1)
+            movement.position += speed;
+            if (movement.position > points.Length - 1)
             {
                 movement.position = 0;
             }
 
-            (float3 lerpedPosition, _) = TrackPositionToWorldPosition(movement.position, ref splineData.Value.splineBlobAssets[lineIndex.Index].points);
+            (float3 lerpedPosition, _) = TrackPositionToWorldPosition(movement.position, ref points);
             translation.Value = lerpedPosition;
         }).ScheduleParallel();
+    }
+
+    static bool IsApproachingPlatform(float position, ref BlobArray<float> platformPositions)
+    {
+        // platformPositions contains ends of platforms, in range 0..1 of whole track
+        for (int i = 0; i < platformPositions.Length; i++)
+        {
+            var platformPosition = platformPositions[i];
+            if (position > platformPosition - 0.05 && position < platformPosition)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static (float3, Quaternion) TrackPositionToWorldPosition(float trackPosition, ref BlobArray<float3> points)
