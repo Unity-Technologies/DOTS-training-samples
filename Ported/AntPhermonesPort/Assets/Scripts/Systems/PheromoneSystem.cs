3e277a429278f238b2ac18c35498f93ae3a238f5
James McCafferty <james.mccafferty@unity3d.com>
2021-09-28 16:50:37 -0400

    group 2 changes, texture update

--- Ported/AntPhermonesPort/Assets/Scripts/Systems/PheromoneSystem.cs
+++ Ported/AntPhermonesPort/Assets/Scripts/Systems/PheromoneSystem.cs
@@ -1,9 +1,52 @@
 ﻿using Unity.Entities;
+using Unity.Rendering;
 
 public partial class PheromoneSystem : SystemBase
 {
+    private UnityEngine.Texture2D texture;
+
+    protected override void OnStartRunning()
+    {
+        //var mapEntity = GetSingletonEntity<MapSetting>();
+        //var mapSetting = GetComponent<MapSetting>(mapEntity);
+
+        //if (texture != null && texture.width == mapSetting.Size && texture.height == mapSetting.Size)
+        //    return;
+
+        //if (texture != null)
+        //    UnityEngine.GameObject.Destroy(texture);
+
+        //texture = new UnityEngine.Texture2D(500, 500, UnityEngine.TextureFormat.RGBAFloat, false);
+        //texture.hideFlags = UnityEngine.HideFlags.HideAndDontSave;
+        //texture.filterMode = UnityEngine.FilterMode.Point;
+
+        //for(int x = 0; x < texture.width/2; x++)
+        //{
+        //    for(int y = 0; y<texture.height/2; ++y)
+        //    {
+        //        texture.SetPixel(x, y, new UnityEngine.Color(10000, 0, 0, 1));
+        //    }
+        //}
+
+        //var buffer = GetBuffer<Pheromone>(mapEntity).Reinterpret<float4>();
+
+        //texture.LoadRawTextureData(buffer.AsNativeArray());
+        //texture.Apply();
+
+        //var pheromoneMapEntity = GetSingletonEntity<CellMap>();
+        //var renderMesh = EntityManager.GetSharedComponentData<RenderMesh>(pheromoneMapEntity);
+
+        //renderMesh.material = new UnityEngine.Material(renderMesh.material);
+        //renderMesh.material.mainTexture = texture;
+    }
+
     protected override void OnUpdate()
     {
-        
+        // In the update
+        //var pheromoneMapEntity = GetSingletonEntity<CellMap>();
+        //var pheromoneMapBuffer = GetBuffer<Pheromone>(pheromoneMapEntity).Reinterpret<float4>();
+
+        //texture.LoadRawTextureData(pheromoneMapBuffer.AsNativeArray());
+        //texture.Apply();
     }
 }
