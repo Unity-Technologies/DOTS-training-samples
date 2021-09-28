68f3d1713dc6435d0cb0cfc0f1fba1fed8d4610e
Martin Barrette <martin.barrette@unity3d.com>
2021-09-28 14:06:54 -0400

    ensure physics movement stop at ground

--- Ported/CombatBees/Assets/Scripts/Systems/PhysicsMovementSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/PhysicsMovementSystem.cs
@@ -1,21 +1,28 @@
 using Unity.Entities;
+using Unity.Collections;
 using Unity.Transforms;
 
 public partial class PhysicsMovementSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        var time = Time.ElapsedTime;
-        float multiplier = 5;
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+        float deltaTime = Time.DeltaTime;
+        float multiplier = 15f;
+        float floorY = 0.5f;
         
         Entities
             .WithAll<Food>()
-            .ForEach((ref Translation translation) =>
+            .WithNone<Grounded>()
+            .ForEach((Entity food, ref Translation translation) =>
             {
 
-                translation.Value.y = - (float) time * multiplier;
-
-                
-            }).ScheduleParallel();
+                translation.Value.y -= deltaTime * multiplier;
+                if (translation.Value.y < floorY)
+                {
+                    translation.Value.y = floorY;// this assume 0 is at the basis of the cylinder
+                    ecb.AddComponent(food, new Grounded{});
+                }
+            }).Run();
     }
 }
