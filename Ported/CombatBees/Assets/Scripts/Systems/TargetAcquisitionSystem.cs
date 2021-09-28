d11b70e704e2c6910322f4fe6f305fda78b5780a
Marc Schraffenberger <m.schraffenberger@unity3d.com>
2021-09-28 16:33:21 -0400

    fixing materials on bees and fixing target code bug

--- Ported/CombatBees/Assets/Scripts/Systems/TargetAcquisitionSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/TargetAcquisitionSystem.cs
@@ -10,13 +10,13 @@ public partial class TargetAcquisitionSystem : SystemBase
         var random = new Random(1234);
 
         EntityQuery foodQuery = GetEntityQuery(typeof(Food));
-        NativeArray<ArchetypeChunk> foodChunks = foodQuery.CreateArchetypeChunkArray(Allocator.Temp);
+        NativeArray<ArchetypeChunk> foodChunks = foodQuery.CreateArchetypeChunkArray(Allocator.TempJob);
         
         EntityQuery teamRedQuery = GetEntityQuery(typeof(TeamRed));
-        NativeArray<ArchetypeChunk> teamRedChunks = teamRedQuery.CreateArchetypeChunkArray(Allocator.Temp);
+        NativeArray<ArchetypeChunk> teamRedChunks = teamRedQuery.CreateArchetypeChunkArray(Allocator.TempJob);
         
         EntityQuery teamBlueQuery = GetEntityQuery(typeof(TeamBlue));
-        NativeArray<ArchetypeChunk> teamBlueChunks = teamBlueQuery.CreateArchetypeChunkArray(Allocator.Temp);        
+        NativeArray<ArchetypeChunk> teamBlueChunks = teamBlueQuery.CreateArchetypeChunkArray(Allocator.TempJob);        
         
         EntityTypeHandle entityHandles = GetEntityTypeHandle();
 
@@ -42,7 +42,7 @@ public partial class TargetAcquisitionSystem : SystemBase
                         target.Value = entities[random.NextInt(0, entities.Length)];               
                     }
                 }
-            }).ScheduleParallel();
+            }).Run();
         
         Entities
             .WithReadOnly(foodChunks)
@@ -66,32 +66,6 @@ public partial class TargetAcquisitionSystem : SystemBase
                         target.Value = entities[random.NextInt(0, entities.Length)];               
                     }
                 }
-            }).ScheduleParallel();
-
-        /*
-        var random = new Random(1234);
-        
-        EntityQuery query = GetEntityQuery(typeof(Food));
-        var entityChunks = query.CreateArchetypeChunkArray(Allocator.Temp);
-        var randomChunk = entityChunks [random.NextInt(0, entityChunks.Length)];
-
-        EntityTypeHandle entityHandles = GetEntityTypeHandle();
-        var entityContainerFinally = randomChunk.GetNativeArray(entityHandles);
-        targets.Food = entityContainerFinally[random.NextInt(0, entityContainerFinally.Length)];
-        
-        EntityQuery teamRedQuery = GetEntityQuery(typeof(TeamRed));
-        var teamRedChunks = teamRedQuery.CreateArchetypeChunkArray(Allocator.Temp);
-        var randomTeamRedChunk = teamRedChunks [random.NextInt(0, teamRedChunks.Length)];
-        var teamRedEntityContainerFinally = randomTeamRedChunk.GetNativeArray(entityHandles);
-        targets.TeamBlue = teamRedEntityContainerFinally[random.NextInt(0, teamRedEntityContainerFinally.Length)];
-        
-        EntityQuery teamBlueQuery = GetEntityQuery(typeof(TeamBlue));
-        var teamBlueChunks = teamBlueQuery.CreateArchetypeChunkArray(Allocator.Temp);
-        var randomTeamBlueChunk = teamBlueChunks [random.NextInt(0, teamBlueChunks.Length)];
-        var teamBlueEntityContainerFinally = randomTeamBlueChunk.GetNativeArray(entityHandles);
-        targets.TeamRed = teamBlueEntityContainerFinally[random.NextInt(0, teamBlueEntityContainerFinally.Length)];
-        
-        SetSingleton<TargetCandidates>(targets);
-        */
+            }).Run();
     }
 }
