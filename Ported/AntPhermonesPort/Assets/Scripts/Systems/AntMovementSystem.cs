a54d3e9b3388e25d515b9143a799e2e6348fa34b
ryantwarner <ryan.warner@unity3d.com>
2021-09-28 11:08:48 -0400

    Ants marching right

--- Ported/AntPhermonesPort/Assets/Scripts/Systems/AntMovementSystem.cs
+++ Ported/AntPhermonesPort/Assets/Scripts/Systems/AntMovementSystem.cs
@@ -7,11 +7,12 @@ public partial class AntMovementSystem : SystemBase
 {
     protected override void OnUpdate()
     {
+        var time = Time.ElapsedTime;
         var ecb = new EntityCommandBuffer(Allocator.Temp);
         Entities
             .ForEach((ref Translation translation, in AntMovement ant) =>
             {
-                //translation.Value += 1.0f;
+                translation.Value.x = (float)((time + 2.0f) % 100) - 50f;
             }).Run();
 
         ecb.Playback(EntityManager);
