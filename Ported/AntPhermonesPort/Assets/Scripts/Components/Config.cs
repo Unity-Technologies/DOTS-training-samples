f368633fde9eeb1639ca3be3941cd03461bce46d
Andrew Skowronski <andrew.skowronski@unity3d.com>
2021-09-28 09:50:14 -0400

    Extra config content

--- Ported/AntPhermonesPort/Assets/Scripts/Components/Config.cs
+++ Ported/AntPhermonesPort/Assets/Scripts/Components/Config.cs
@@ -4,5 +4,13 @@ using Unity.Transforms;
 
 public struct Config : IComponentData
 {
-    public int Speed;
+    public int Speed = 1; // 1-9
+	
+	// Simulation Constants
+	public const int AntCount = 100;
+
+	// GreenDotDistance
+	// SphereRadius
+	// WallDistanceFromCenter
+	// Total simulation size CellMapResolution 
 }
