27458f61c366b7ebc3cba0a847d315fa1f128426
Rasmus Selsmark <rasmus@unity3d.com>
2021-09-28 16:05:54 +0200

    Code formatting for CarriageMovementSystem

--- Original/Metro/Assets/dots_src/Systems/CarriageMovementSystem.cs
+++ Original/Metro/Assets/dots_src/Systems/CarriageMovementSystem.cs
@@ -10,21 +10,23 @@ public partial class CarriageMovementSystem : SystemBase
     protected override void OnUpdate()
     {
         var splineData = GetSingleton<SplineDataReference>().BlobAssetReference;
-        Entities.ForEach((ref Translation translation, ref Rotation rotation, in TrainReference TrainReference) =>
+        Entities.ForEach((ref Translation translation, ref Rotation rotation, in TrainReference trainReference) =>
         {
             float carriageSizeWithMargins = 0.2f;
             
-            TrainMovement trainMovement = EntityManager.GetComponentData<TrainMovement>(TrainReference.Train);
-            LineIndex lineIndex = EntityManager.GetComponentData<LineIndex>(TrainReference.Train);
-            float offsetPosition = trainMovement.position - TrainReference.Index * carriageSizeWithMargins;
+            TrainMovement trainMovement = EntityManager.GetComponentData<TrainMovement>(trainReference.Train);
+            LineIndex lineIndex = EntityManager.GetComponentData<LineIndex>(trainReference.Train);
+
+            float offsetPosition = trainMovement.position - trainReference.Index * carriageSizeWithMargins;
             if (offsetPosition < 0)
             {
                 var pointsLength = splineData.Value.splineBlobAssets[lineIndex.Index].points.Length;
                 offsetPosition = pointsLength + offsetPosition - 1;
             }
-            (float3 lerpedPosition, Quaternion newRotation) = TrainMovementSystem.TrackPositionToWorldPosition(offsetPosition, ref splineData.Value.splineBlobAssets[lineIndex.Index].points);
-            translation.Value = lerpedPosition;
-            rotation.Value = newRotation;
-        }).WithoutBurst().Run();
+
+            (translation.Value, rotation.Value) = TrainMovementSystem.TrackPositionToWorldPosition(
+                offsetPosition,
+                ref splineData.Value.splineBlobAssets[lineIndex.Index].points);
+        }).WithoutBurst().Run(); // TODO: `.WithoutBurst().Run()` likely makes this slower, can we use `.ScheduleParallel()`? 
     }
 }
