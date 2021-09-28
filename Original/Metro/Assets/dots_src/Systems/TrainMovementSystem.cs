37e1d9628cf61513c4c14b462235967ed16be8ca
Jonasmortensen <Jonasmortensen3@gmail.com>
2021-09-28 09:37:44 +0200

    First iteration of train movement system

--- Original/Metro/Assets/dots_src/Systems/TrainMovementSystem.cs
+++ Original/Metro/Assets/dots_src/Systems/TrainMovementSystem.cs
@@ -1,6 +1,7 @@
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
 
 public partial class TrainMovementSystem : SystemBase
 {
@@ -8,11 +9,16 @@ public partial class TrainMovementSystem : SystemBase
     {
         var splineData = GetSingleton<SplineDataReference>().BlobAssetReference;
         
-        Entities.WithAll<TrainMovement>().ForEach((ref Translation translation, ref TrainMovement movement) =>
+        Entities.WithAll<TrainMovement>().ForEach((ref Translation translation, ref Rotation rotation, ref TrainMovement movement) =>
         {
-            float speed = 0.01f; //from global settings singleton
+            float speed = 0.05f; //from global settings singleton
 
             movement.position += speed;
+
+            if (movement.position > splineData.Value.points.Length - 1)
+            {
+                movement.position = 0;
+            }
 
             float3 from = splineData.Value.points[(int)math.floor(movement.position)];
             float3 to = splineData.Value.points[(int)math.ceil(movement.position)];
@@ -22,6 +28,10 @@ public partial class TrainMovementSystem : SystemBase
             float3 lerpedPosition = math.lerp(from, to, t); 
 
             translation.Value = lerpedPosition;
+
+            float3 trainDirection = math.normalize(from - to);
+
+            rotation.Value = Quaternion.LookRotation(new Vector3(trainDirection.x, trainDirection.y, trainDirection.z));
 
         }).ScheduleParallel();
     }
