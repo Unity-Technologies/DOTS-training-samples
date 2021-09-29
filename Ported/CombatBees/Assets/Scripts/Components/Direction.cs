d914b9b9d73dec550d37c6d47b7af1669e167f94
Marc Schraffenberger <m.schraffenberger@unity3d.com>
2021-09-29 09:52:06 -0400

    removing unneeded direction and position components

--- Ported/CombatBees/Assets/Scripts/Components/Direction.cs
+++
@@ -1,8 +0,0 @@
-using Unity.Entities;
-using Unity.Mathematics;
-
-[GenerateAuthoringComponent]
-public struct Direction : IComponentData
-{
-    public float3 Value;
-}
--- Ported/CombatBees/Assets/Scripts/Components/Position.cs
+++
@@ -1,8 +0,0 @@
-using Unity.Entities;
-using Unity.Mathematics;
-
-[GenerateAuthoringComponent]
-public struct Position : IComponentData
-{
-    public float3 Value;
-}
