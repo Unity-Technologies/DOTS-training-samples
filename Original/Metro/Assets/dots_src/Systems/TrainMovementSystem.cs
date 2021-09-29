ed99bd10607c7a9d07936a0a7dc9f05ae2ba89e7
Rasmus Selsmark <rasmus@unity3d.com>
2021-09-29 16:12:08 +0200

    Stopping at platforms (and added few comments on units)

--- Original/Metro/Assets/dots_src/Systems/TrainMovementSystem.cs
+++ Original/Metro/Assets/dots_src/Systems/TrainMovementSystem.cs
@@ -16,43 +16,33 @@ public partial class TrainMovementSystem : SystemBase
         Entities.ForEach((ref Translation translation, ref Rotation rotation, ref TrainMovement movement, in LineIndex lineIndex) =>
         {
             float lineLength = splineData.Value.splineBlobAssets[lineIndex.Index].length;
-            float maxSpeed = settings.MaxSpeed * deltaTime / lineLength * 1000;
-
             float breakingPoint = 0.05f;
 
             ref var points = ref splineData.Value.splineBlobAssets[lineIndex.Index].points;
             ref var platformPositions = ref splineData.Value.splineBlobAssets[lineIndex.Index].platformPositions;
-
-            // TODO: platforms are positioned 0..1 range (relative to total track length). currently our train
-            // position is absolute in terms of track length, e.g. 0..25
-            
             var trackRelativePosition = movement.position / points.Length;
-            if (movement.state == TrainMovemementStates.Running
-                && IsApproachingPlatform(trackRelativePosition, ref platformPositions))
-            {
-                // Debug.Log($"Train on line #{lineIndex.Index} is approaching platform, slowing down!");
-                movement.state = TrainMovemementStates.Stopping;
-            }
 
             switch (movement.state)
             {
                 case TrainMovemementStates.Starting:
-                    movement.speed += maxSpeed / 100 * 5;
-                    if (movement.speed >= maxSpeed)
+                    movement.speed += settings.MaxSpeed / 100 * 5;
+                    if (movement.speed >= settings.MaxSpeed)
                     {
                         movement.state = TrainMovemementStates.Running;
                     }
                     break;
                 
                 case TrainMovemementStates.Stopping:
-                    float distanceTraveledLastFrame = (movement.speed / lineLength) * deltaTime;
+                    // following are in meters 
+                    float distanceTraveledLastFrame = movement.speed * deltaTime;
+                    var distanceToPlatform = DistanceToClosestPlatform(trackRelativePosition, ref platformPositions) * lineLength;
+                    float framesUntilStop = distanceToPlatform/ distanceTraveledLastFrame;
 
-                    float framesUntilStop = (distanceToClosestPlatform(trackRelativePosition, ref platformPositions) /
-                                           distanceTraveledLastFrame);
+                    Debug.Log(distanceToPlatform);
+                    var reduceSpeedWith = math.max(0.1f * deltaTime, movement.speed / framesUntilStop); 
+                    movement.speed -= reduceSpeedWith;
 
-                    //movement.speed -= movement.speed / framesUntilStop;
-                    movement.speed = (distanceTraveledLastFrame / 0.05f) * (maxSpeed);
-                    if (movement.speed <= 0.0f)
+                    if (movement.speed <= 0.0f || distanceToPlatform < 1f)
                     {
                         movement.speed = 0.0f;
                         movement.state = TrainMovemementStates.Stopped;
@@ -74,11 +64,23 @@ public partial class TrainMovementSystem : SystemBase
                     break;
 
                 case TrainMovemementStates.Running:
-                    movement.speed = maxSpeed;
+                    movement.speed = settings.MaxSpeed;
+
+                    // are we approaching platform?
+                    var relativeDistanceToPlatform = DistanceToClosestPlatform(trackRelativePosition, ref platformPositions);
+                    var distanceToPlatformInMeters = relativeDistanceToPlatform * lineLength;
+
+                    if (distanceToPlatformInMeters < 50)
+                    {
+                        movement.state = TrainMovemementStates.Stopping;
+                    }
+
                     break;
             }
 
-            movement.position += movement.speed;
+            // convert speed (m/s) to track positions/second
+            var trackPositionsSpeed = movement.speed / lineLength * points.Length;
+            movement.position += trackPositionsSpeed * deltaTime;
             if (movement.position > points.Length - 1)
             {
                 movement.position = 0;
@@ -86,26 +88,14 @@ public partial class TrainMovementSystem : SystemBase
 
             (float3 lerpedPosition, _) = TrackPositionToWorldPosition(movement.position, ref points);
             translation.Value = lerpedPosition;
-        // }).WithoutBurst().Run(); // for debugging
-        }).ScheduleParallel();
+        }).WithoutBurst().Run(); // for debugging
+        // }).ScheduleParallel();
     }
 
-    static bool IsApproachingPlatform(float position, ref BlobArray<float> platformPositions)
-    {
-        // platformPositions contains ends of platforms, in range 0..1 of whole track
-        for (int i = 0; i < platformPositions.Length; i++)
-        {
-            var platformPosition = platformPositions[i];
-            if (position > platformPosition - 0.05 && position < platformPosition)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    static float distanceToClosestPlatform(float position, ref BlobArray<float> platformPositions)
+    /// <summary>
+    /// Returns distance to next platform in relative points, i.e. range [0..1[ 
+    /// </summary>
+    static float DistanceToClosestPlatform(float position, ref BlobArray<float> platformPositions)
     {
         for (int i = 0; i < platformPositions.Length; i++)
         {
@@ -123,20 +113,6 @@ public partial class TrainMovementSystem : SystemBase
     {
         var floor = (int)math.floor(trackPosition);
         var ceil = (int)math.ceil(trackPosition);
-        
-        
-        /*
-        if (floor == ceil)
-        {
-            ceil += 1;
-            
-            // check for overflow
-            if (ceil >= points.Length)
-            {
-                ceil = 0;
-            }
-        }
-        */
 
         float3 from = points[floor];
         float3 to = points[ceil];
