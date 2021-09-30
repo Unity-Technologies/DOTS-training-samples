47a2e7cf4e94c2c273b0f89fd2d807529f29a1da
Marc Schraffenberger <m.schraffenberger@unity3d.com>
2021-09-30 16:16:01 -0400

    blood hitting wall

--- Ported/CombatBees/Assets/Scripts/Systems/ParticleMovementSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/ParticleMovementSystem.cs
@@ -19,11 +19,19 @@ public partial class ParticleMovementSystem : SystemBase
         Entities
             .WithAll<Particle>()
             .WithNone<Grounded>()
-            .ForEach((Entity entity, int entityInQueryIndex, ref Translation translation, ref Velocity velocity) =>
+            .ForEach((Entity entity, int entityInQueryIndex, ref Translation translation, ref Rotation rotation, ref Velocity velocity) =>
             {
                 velocity.Value = velocity.Value + (Gravity * deltaTime);
                 
                 float3 position = translation.Value + (velocity.Value * deltaTime);
+
+                if (position.z <= bounds.AABB.Min.z + 0.1f || position.z >= bounds.AABB.Max.z - 0.1f)
+                {
+                    rotation.Value = math.mul(quaternion.RotateX(math.PI * 0.5f), rotation.Value);
+                    ecb.AddComponent(entityInQueryIndex, entity, new Grounded{});
+                    return;
+                }
+                
                 translation.Value = WorldUtils.ClampToWorldBounds(bounds, position, 0.1f);
 
                 if (translation.Value.y <= 0.1f)
