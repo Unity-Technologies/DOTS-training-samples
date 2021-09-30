c157ab173f66cfa5f759edeff9c9e91ed8ebec2e
Martin Barrette <martin.barrette@unity3d.com>
2021-09-30 16:36:04 -0400

    non optimized stacking

--- Ported/CombatBees/Assets/Scripts/Systems/PhysicsMovementSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/PhysicsMovementSystem.cs
@@ -5,6 +5,9 @@ using Unity.Mathematics;
 
 public partial class PhysicsMovementSystem : SystemBase
 {
+
+    private bool[,] GridOccupancy = null;
+
     protected override void OnUpdate()
     {
         var system = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
@@ -12,29 +15,69 @@ public partial class PhysicsMovementSystem : SystemBase
 
         var worldBoundsEntity = GetSingletonEntity<WorldBounds>();
         var bounds = GetComponent<WorldBounds>(worldBoundsEntity);
-        var hiveBluePosition = bounds.AABB.Min +
-                               new float3(bounds.HiveOffset / 2.0f, (bounds.AABB.Max.y - bounds.AABB.Min.y)/2.0f, (bounds.AABB.Max.z - bounds.AABB.Min.z)/2.0f);
-        var hiveRedPosition = bounds.AABB.Max -
-                              new float3(bounds.HiveOffset / 2.0f, (bounds.AABB.Max.y - bounds.AABB.Min.y)/2.0f, (bounds.AABB.Max.z - bounds.AABB.Min.z)/2.0f);
+
+        int MaxX = ((int) (bounds.AABB.Max.x - bounds.AABB.Min.x) +1) * (int) (bounds.AABB.Max.z - bounds.AABB.Min.z);
+        int MaxY = (int) (bounds.AABB.Max.y - bounds.AABB.Min.y);
+        if (GridOccupancy == null)
+        {
+            GridOccupancy = new bool[MaxX, MaxY];
+        }
+
+        for (int i = 0; i < MaxX; ++i)
+        {
+            for (int j = 0; j < MaxY; ++j)
+                GridOccupancy[i, j] = false;
+        }
         
         float deltaTime = Time.DeltaTime;
         float multiplier = 9.8f;
         float floorY = 0.5f;
-        
+
+        var grid = GridOccupancy;
+
         Entities
+            .WithoutBurst()
+            .WithAny<Food>()
+            .ForEach((Entity entity, int entityInQueryIndex, in Translation translation) =>
+            {
+                grid[(int) (translation.Value.x - bounds.AABB.Min.x) * (int) (bounds.AABB.Max.z - bounds.AABB.Min.z) + 
+                     (int)(translation.Value.z - bounds.AABB.Min.z),
+                    (int) (translation.Value.y - bounds.AABB.Min.y)] = true;
+            }).Run();
+        
+
+        Entities
+            .WithoutBurst()
             .WithAny<Food>()
             .WithNone<Grounded>()
             .ForEach((Entity entity, int entityInQueryIndex, ref Translation translation) =>
             {
-                translation.Value.y -= deltaTime * multiplier;
-                
+                int GridX = (int)(translation.Value.x - bounds.AABB.Min.x) *
+                            (int)(bounds.AABB.Max.z - bounds.AABB.Min.z) + 
+                            (int) (translation.Value.z - bounds.AABB.Min.z);
+                int GridY = (int)(translation.Value.y - bounds.AABB.Min.y);
+
+                float delta = deltaTime * multiplier;
+
+                int newGridY = (int)((translation.Value.y - delta) - 0.5f - bounds.AABB.Min.y);
+                if (newGridY < GridY)
+                {
+                    grid[GridX, GridY] = false;
+                    if (grid[GridX, newGridY] == false)
+                        translation.Value.y -= delta;
+                }
+                else
+                {
+                    translation.Value.y -= delta;
+                }
+
                 if (translation.Value.y < floorY)
                 {
                     translation.Value.y = floorY;// this assume 0 is at the basis of the cylinder
                     ecb.AddComponent(entityInQueryIndex, entity, new Grounded{});
                 }
-            }).ScheduleParallel();
-        
+            }).Run();
+
         system.AddJobHandleForProducer(Dependency);
     }
 }
