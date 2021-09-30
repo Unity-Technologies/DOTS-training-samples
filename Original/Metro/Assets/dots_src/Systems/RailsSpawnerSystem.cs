acf562301b5a9d5e4c1183d74a48ec6fd18e437a
Ludovic Theobald <ludovic.theobald@unity3d.com>
2021-09-30 10:28:00 +0200

    Aligned platforms

--- Original/Metro/Assets/dots_src/Systems/RailsSpawnerSystem.cs
+++ Original/Metro/Assets/dots_src/Systems/RailsSpawnerSystem.cs
@@ -13,6 +13,10 @@ public partial class RailsSpawnerSystem : SystemBase
         var ecb = new EntityCommandBuffer(Allocator.Temp);
         var splineDataArrayRef = GetSingleton<SplineDataReference>().BlobAssetReference;
 
+        const float platformSize = 30.0f;
+        const float platformWidth = 10.0f;
+        float3 returnPlatformOffset = new float3(20, 0, -8);
+        
         Entities.
             ForEach((Entity entity, in RailsSpawner spawner) =>
             {
@@ -38,21 +42,30 @@ public partial class RailsSpawnerSystem : SystemBase
                     int nbPlatforms = splineData.platformPositions.Length;
                     int halfPlatforms = nbPlatforms / 2;
                     NativeArray<Rotation> outBoundsRotations = new NativeArray<Rotation>(halfPlatforms, Allocator.Temp);
+                    NativeArray<float3> outBoundsTranslations = new NativeArray<float3>(halfPlatforms, Allocator.Temp);
                     for (int i = 0; i < nbPlatforms; i++)
                     {
                         var instance = ecb.Instantiate(spawner.PlatformPrefab);
                         int pointIndex = Mathf.FloorToInt(splineData.platformPositions[i] * splineData.points.Length);
+                        int centerPlatformIndex = Mathf.FloorToInt((splineData.platformPositions[i] - platformSize/(2 * splineData.length))  * splineData.points.Length );
+                        var centerPos = splineData.points[centerPlatformIndex];
+                        var centerNextPos = splineData.points[centerPlatformIndex + 1];
                         Translation translation;
                         if (i < halfPlatforms)
                         {
                             (translation ,rotation) = GetStationTransform(splineData.points[pointIndex],
-                                splineData.points[pointIndex + 1]);
+                                splineData.points[centerPlatformIndex],
+                                splineData.points[centerPlatformIndex + 1]);
                             outBoundsRotations[i] = rotation;
+                            outBoundsTranslations[i] = translation.Value;
                         }
                         else
                         {
-                            translation = new Translation {Value = splineData.points[pointIndex]};
                             var outBoundQuaternion = outBoundsRotations[nbPlatforms - i - 1].Value;
+                            var outBoundTranslation = outBoundsTranslations[nbPlatforms - i - 1];
+                            var returnTranslation = math.mul(outBoundQuaternion, returnPlatformOffset) + outBoundTranslation;
+                            //var returnTranslation = outBoundTranslation;
+                            translation = new Translation() {Value = returnTranslation};
                             rotation = new Rotation() {Value = math.mul(quaternion.RotateY(math.PI), outBoundQuaternion)};
                         }
 
@@ -77,9 +90,9 @@ public partial class RailsSpawnerSystem : SystemBase
         return rotation;
     }
 
-    static (Translation, Rotation) GetStationTransform(float3 curPos, float3 nextPos)
+    static (Translation, Rotation) GetStationTransform(float3 curPos, float3 centerPos, float3 centerNextPos)
     {
-        Vector3 backTrackDir =  - Vector3.Normalize(nextPos - curPos);
+        Vector3 backTrackDir =  - Vector3.Normalize(centerNextPos - centerPos);
         Vector3 forwardPlatformDir = Vector3.Cross(backTrackDir, Vector3.up);
         Rotation rotation = new Rotation()
         {
