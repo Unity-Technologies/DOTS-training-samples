31594dd22a2c1f3ff3d30758e88ef9b1e718970d
AlainLaferriere <alain.laferriere@unity3d.com>
2021-10-01 11:29:37 -0400

    Parallelized the pheromone decay system

--- Ported/AntPhermonesPort/Assets/Scripts/Systems/PheromoneDecaySystem.cs
+++ Ported/AntPhermonesPort/Assets/Scripts/Systems/PheromoneDecaySystem.cs
@@ -1,6 +1,23 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Rendering;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Burst;
+
+[BurstCompile]
+struct DecayJob : IJobParallelFor
+{
+    [NativeDisableParallelForRestriction]
+    public DynamicBuffer<PheromoneMap> pheromoneMap;
+    public float decayRate;
+
+    public void Execute(int index)
+    {
+        ref PheromoneMap item = ref pheromoneMap.ElementAt(index);
+        item.intensity.x *= decayRate;
+    }
+}
 
 [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
 public partial class PheromoneDecaySystem : SystemBase
@@ -14,6 +31,11 @@ public partial class PheromoneDecaySystem : SystemBase
         Entity pheromoneMapEntity = GetSingletonEntity<PheromoneMap>();
         PheromoneMapHelper helper = new PheromoneMapHelper(EntityManager.GetBuffer<PheromoneMap>(pheromoneMapEntity), config.CellMapResolution, config.WorldSize);
 
-        helper.DecrementIntensity(config.PheromoneDecayRate);
+        DecayJob job = new DecayJob { pheromoneMap = helper.pheromoneMap, decayRate = config.PheromoneDecayRate };
+
+        Dependency = job.Schedule(helper.pheromoneMap.Length, config.CellMapResolution, Dependency);
+
+// TODO: cleanup/remove
+  //      helper.DecrementIntensity(config.PheromoneDecayRate);
     }
 }
