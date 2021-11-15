26e8d3f5cfffd04815cbf8e36802352012c69d32
Jonathan Schmidt <jonathans@unity3d.com>
2021-11-15 13:37:46 -0500

    Tweak project settings

--- Ported/Tornado/Assets/Original/Scripts/ParticleManager.cs
+++ Ported/Tornado/Assets/Original/Scripts/ParticleManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ParticleManager : MonoBehaviour
 {
@@ -18,12 +16,11 @@ public class ParticleManager : MonoBehaviour
         points = new Vector3[1000];
         matrices = new Matrix4x4[1000];
         radiusMults = new float[1000];
-        Vector4[] colors = new Vector4[1000];
+        var colors = new Vector4[1000];
 
         for (int i = 0; i < points.Length; i++)
         {
-            Vector3 pos = new Vector3(Random.Range(-50f, 50f), Random.Range(0f, 50f), Random.Range(-50f, 50f));
-            points[i] = pos;
+            points[i] = new Vector3(Random.Range(-50f, 50f), Random.Range(0f, 50f), Random.Range(-50f, 50f));
             matrices[i] = Matrix4x4.TRS(points[i], Quaternion.identity, Vector3.one * Random.Range(.2f, .7f));
             radiusMults[i] = Random.value;
             colors[i] = Color.white * Random.Range(.3f, .7f);
@@ -36,16 +33,15 @@ public class ParticleManager : MonoBehaviour
     {
         for (int i = 0; i < points.Length; i++)
         {
-            Vector3 tornadoPos = new Vector3(PointManager.tornadoX + PointManager.TornadoSway(points[i].y), points[i].y, PointManager.tornadoZ);
-            Vector3 delta = (tornadoPos - points[i]);
-            float dist = delta.magnitude;
-            delta /= dist;
+            var tornadoPos = new Vector3(PointManager.tornadoX + PointManager.TornadoSway(points[i].y), points[i].y, PointManager.tornadoZ);
+            var delta = tornadoPos - points[i];
+            var dist = delta.magnitude;
             float inForce = dist - Mathf.Clamp01(tornadoPos.y / 50f) * 30f * radiusMults[i] + 2f;
+            
+            delta /= dist;
             points[i] += new Vector3(-delta.z * spinRate + delta.x * inForce, upwardSpeed, delta.x * spinRate + delta.z * inForce) * Time.deltaTime;
             if (points[i].y > 50f)
-            {
                 points[i] = new Vector3(points[i].x, 0f, points[i].z);
-            }
 
             Matrix4x4 matrix = matrices[i];
             matrix.m03 = points[i].x;
