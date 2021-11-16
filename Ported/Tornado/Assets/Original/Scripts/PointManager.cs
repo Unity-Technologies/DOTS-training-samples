2e11cd9fea38958126caf356881700f92d9b3e80
Marc-Andre Loyer <marcandre.loyer@unity3d.com>
2021-11-16 09:16:10 -0500

    Making the original scene display with URP

--- Ported/Tornado/Assets/Original/Scripts/PointManager.cs
+++ Ported/Tornado/Assets/Original/Scripts/PointManager.cs
@@ -167,6 +167,9 @@ public class PointManager : MonoBehaviour
             colors[i % instancesPerBatch] = barsList[i].color;
             if ((i + 1) % instancesPerBatch == 0 || i == barsList.Count - 1)
             {
+                
+                // Warning, MaterialPropertyBlock is not supported in URP...
+                // beams color will not work, but keeping the code here for now
                 MaterialPropertyBlock block = new MaterialPropertyBlock();
                 block.SetVectorArray("_Color", colors);
                 matProps[i / instancesPerBatch] = block;
