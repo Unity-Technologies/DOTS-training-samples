8958b9ed5de194ab3764ba4d25a4873c425e1275
yvyeung <77370514+yvyeung@users.noreply.github.com>
2021-11-16 10:39:29 -0800

    BeeSpawner progress
    Spawned bees in hive
    Colored bees.
    Fixed spawning bees every frame

--- Ported/CombatBees/Assets/Scripts/Components/Velocity.cs
+++ Ported/CombatBees/Assets/Scripts/Components/Velocity.cs
@@ -3,6 +3,8 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 
+
+[GenerateAuthoringComponent] // 
 [Serializable]
 public struct Velocity : IComponentData
 {
--- Ported/CombatBees/Assets/Scripts/Systems/BeeSpawner.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/BeeSpawner.cs
@@ -4,11 +4,18 @@ using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
+using Unity.Rendering;
 
 public partial class BeeSpawner : SystemBase
 {
     public int totalBees = 100;
-
+    
+    protected override void OnCreate()
+    {
+        this.RequireSingletonForUpdate<Spawner>();
+    }
+   
+    // May run before scene is loaded
     protected override void OnUpdate()
     {
         var spawner = GetSingletonEntity<Spawner>();
@@ -19,15 +26,25 @@ public partial class BeeSpawner : SystemBase
         Entities
             .WithAll<HiveTag>()
             .WithStructuralChanges()
-            .ForEach((in AABB aabb, in TeamID teamID) => 
+            .ForEach((in AABB aabb, in TeamID teamID, in URPMaterialPropertyBaseColor urpColor) => 
         {
             for (int i = 0; i < totalBees; ++i)
             {
                 var entity = EntityManager.Instantiate(spawnerComponent.BeePrefab);
 
                 var vel = math.normalize(random.NextFloat3Direction());
-                EntityManager.AddComponentData<Velocity>(entity, new Velocity { Value = vel });
+                // Optimize by Setting the velocity instead of adding.
+                EntityManager.SetComponentData<Velocity>(entity, new Velocity { Value = vel });
+                // Move bee to hive location
+                EntityManager.SetComponentData<Translation>(entity, new Translation { Value = aabb.center });
+                // Set bee color based off the hive
+                URPMaterialPropertyBaseColor color = urpColor;
+                color.Value.w = 1;
+                EntityManager.SetComponentData<URPMaterialPropertyBaseColor>(entity, color);
+
             }
         }).Run();
+
+        this.Enabled = false;
     }
 }
