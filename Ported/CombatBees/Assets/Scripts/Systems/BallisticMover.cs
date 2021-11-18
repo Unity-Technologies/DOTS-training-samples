c08e569ce527fc4af3fcfad0bb50ac300482f21f
John Paquin <john.paquin@unity3d.com>
2021-11-18 14:59:20 -0800

    keep ballistic stuff contained in the world

--- Ported/CombatBees/Assets/Scripts/Systems/BallisticMover.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/BallisticMover.cs
@@ -25,41 +25,55 @@ public partial class BallisticMover : SystemBase
         float3 gravityVector = new float3(0, -2, 0);
 
         var ecb = ecbs.CreateCommandBuffer();
-        
-        Entities
-            .WithAll<Ballistic>()
-            .ForEach((ref Velocity velocity) =>
-        {
-            velocity.Value += gravityVector * time;
-        }).Schedule();
 
         Entities
             .WithAll<Ballistic>()
-            .ForEach((ref Translation translation, in Velocity velocity) => 
-        {
-             translation.Value = translation.Value + velocity.Value * time;
-        }).Schedule();
+            .ForEach((ref Velocity velocity) => { velocity.Value += gravityVector * time; }).Schedule();
+
+        Entities
+            .WithAll<Ballistic>()
+            .ForEach((ref Translation translation, in Velocity velocity) =>
+            {
+                translation.Value = translation.Value + velocity.Value * time;
+            }).Schedule();
 
         Entities
             .WithAll<Ballistic>()
             .WithNone<Decay>()
-            .ForEach((Entity entity, ref Translation translation, in AABB aabb) =>
+            .ForEach((Entity entity, ref Translation translation, ref Velocity velocity, in AABB aabb) =>
             {
-                var height = translation.Value.y + aabb.center.y - aabb.halfSize.y;
-            
-            if (height < globalData.BoundsMin.y)
-            {
-                translation.Value.y = globalData.BoundsMin.y + aabb.halfSize.y - aabb.center.y;
-                ecb.RemoveComponent<Ballistic>(entity);
-                if (HasComponent<Food>(entity))
+                var abspos = math.abs(translation.Value + aabb.center)+aabb.halfSize;
+                if (abspos.x > globalData.BoundsMax.x)
                 {
-                    // if food is in hive, spawn new bees
+                    if (velocity.Value.x * translation.Value.x > 0.0f)
+                    {
+                        velocity.Value.x *= -0.5f;
+                    }
                 }
-                else
-                    ecb.AddComponent(entity, new Decay());
-            }
-        }).Schedule();
-        
+                
+                if (abspos.z > globalData.BoundsMax.z)
+                {
+                    if (velocity.Value.z * translation.Value.z > 0.0f)
+                    {
+                        velocity.Value.z *= -0.5f;
+                    }
+                }
+                
+                var height = translation.Value.y + aabb.center.y - aabb.halfSize.y;
+
+                if (height < globalData.BoundsMin.y)
+                {
+                    translation.Value.y = globalData.BoundsMin.y + aabb.halfSize.y - aabb.center.y;
+                    ecb.RemoveComponent<Ballistic>(entity);
+                    if (HasComponent<Food>(entity))
+                    {
+                        // if food is in hive, spawn new bees
+                    }
+                    else
+                        ecb.AddComponent(entity, new Decay());
+                }
+            }).Schedule();
+
         ecbs.AddJobHandleForProducer(Dependency);
     }
 }
