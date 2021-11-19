b2b976e2efb6b2b8abe40271c6b5dbedec625e91
Sherry <sharareh.faramarz@unity3d.com>
2021-11-19 10:57:17 -0800

    resolved merge conflict

--- Ported/CombatBees/Assets/Scripts/Systems/BallisticMover.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/BallisticMover.cs
@@ -20,7 +20,7 @@ public partial class BallisticMover : SystemBase
             .GetOrCreateSystem<BeginSimulationEntityCommandBufferSystem>();
         this.RequireSingletonForUpdate<GlobalData>();
     }
-
+  
 
     protected override void OnUpdate()
     {
@@ -48,10 +48,9 @@ public partial class BallisticMover : SystemBase
         Entities
             .WithAll<Ballistic>()
             .WithNone<Decay, InHive>()
-            .ForEach((Entity entity, int entityInQueryIndex, ref Translation translation, ref Velocity velocity,
-                in AABB aabb) =>
+            .ForEach((Entity entity, int entityInQueryIndex, ref Translation translation, ref Velocity velocity, in AABB aabb) =>
             {
-                var abspos = math.abs(translation.Value + aabb.center) + aabb.halfSize;
+                var abspos = math.abs(translation.Value + aabb.center)+aabb.halfSize;
                 if (abspos.x > globalData.BoundsMax.x)
                 {
                     if (velocity.Value.x * translation.Value.x > 0.0f)
@@ -59,7 +58,7 @@ public partial class BallisticMover : SystemBase
                         velocity.Value.x *= -0.5f;
                     }
                 }
-
+                
                 if (abspos.z > globalData.BoundsMax.z)
                 {
                     if (velocity.Value.z * translation.Value.z > 0.0f)
@@ -67,8 +66,17 @@ public partial class BallisticMover : SystemBase
                         velocity.Value.z *= -0.5f;
                     }
                 }
-
+                
                 var height = translation.Value.y + aabb.center.y - aabb.halfSize.y;
+            
+            if (height < globalData.BoundsMin.y)
+            {
+                translation.Value.y = globalData.BoundsMin.y + aabb.halfSize.y - aabb.center.y;
+                ecb.RemoveComponent<Ballistic>(entityInQueryIndex, entity);
+                if (!HasComponent<Food>(entity))
+                    ecb.AddComponent(entityInQueryIndex, entity, new Decay());
+            }
+        }).ScheduleParallel();
 
         Entities
         .WithAll<Ballistic, Food>()
@@ -78,21 +86,22 @@ public partial class BallisticMover : SystemBase
             var random = Random.CreateFromIndex((uint)(entityInQueryIndex + frameNumber));
             var height = translation.Value.y + aabb.center.y - aabb.halfSize.y;
 
-        Entities
-            .WithAll<Ballistic, Food>()
-            .WithNone<Decay>()
-            .ForEach((Entity entity, int entityInQueryIndex, ref Translation translation, in AABB aabb,
-                in TargetedBy targetedby) =>
+            if (height < globalData.BoundsMin.y)
             {
-                var height = translation.Value.y + aabb.center.y - aabb.halfSize.y;
+                // Despawn the food object
+                ecb.DestroyEntity(entityInQueryIndex, entity);
 
-                if (height < globalData.BoundsMin.y)
+                var explosion = becb.Instantiate(entityInQueryIndex, globalData.ExplosionPrefab);
+                becb.SetComponent<Translation>(entityInQueryIndex, explosion, translation);
+
+                for (int i = 0; i < globalData.BeeExplosionCount; ++i)
                 {
                     var bee = becb.Instantiate(entityInQueryIndex, globalData.BeePrefab);
                     var vel = BeeSpawner.SetBees(bee, entityInQueryIndex, becb, GetComponent<TeamID>(entity), ref random);
                     becb.SetComponent<Translation>(entityInQueryIndex, bee, new Translation {Value = translation.Value + vel*.25f});
                 }
-            }).ScheduleParallel();
+            }
+        }).ScheduleParallel();
 
         ecbs.AddJobHandleForProducer(Dependency);
         becbs.AddJobHandleForProducer(Dependency);
