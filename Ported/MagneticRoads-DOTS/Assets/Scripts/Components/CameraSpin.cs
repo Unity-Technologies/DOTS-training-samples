ec2685b7fe6d59fce7d67055c994dc9a4e1c4bfb
David Rogers <davidro@unity3d.com>
2022-01-24 13:36:21 -0600

    Road Generation

---
+++ Ported/MagneticRoads-DOTS/Assets/Scripts/Components/CameraSpin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSpin : MonoBehaviour {
+	public Vector3 center;
+	public float distance;
+	public float tilt;
+	public float spinSpeed;
+
+	float spinAngle=0f;
+	void Start () {
+		
+	}
+	
+	void Update () {
+		spinAngle += spinSpeed*Time.deltaTime;
+		transform.rotation = Quaternion.Euler(tilt,spinAngle,0f);
+		transform.position = center - transform.forward * distance;
+	}
+}
