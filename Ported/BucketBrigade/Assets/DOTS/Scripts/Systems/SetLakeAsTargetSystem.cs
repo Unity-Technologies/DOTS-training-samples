755f5994f4876600fd76b41e1b2c7efdad312f5e
Antoine Serre <antoine.serre@unity3d.com>
2022-01-25 16:12:00 +0100

    New Goto Lake system implementation

--- Ported/BucketBrigade/Assets/DOTS/Scripts/Systems/SetLakeAsTargetSystem.cs
+++ Ported/BucketBrigade/Assets/DOTS/Scripts/Systems/SetLakeAsTargetSystem.cs
@@ -9,8 +9,11 @@ public partial class SetLakeAsTargetSystem : SystemBase
 {
     private EntityCommandBufferSystem CommandBufferSystem;
 
+    private EntityQuery LakeQuery;
+
     protected override void OnCreate()
     {
+        LakeQuery = GetEntityQuery(ComponentType.ReadOnly<Lake>(), ComponentType.ReadOnly<Translation>());
         CommandBufferSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
     }
 
@@ -19,19 +22,39 @@ public partial class SetLakeAsTargetSystem : SystemBase
         var config = GetSingleton<GameConstants>();
         var ecb = CommandBufferSystem.CreateCommandBuffer();
 
+        var lakeTranslations = LakeQuery.ToComponentDataArray<Translation>(Allocator.TempJob);
+
+        var readonlyLakeTranslations = lakeTranslations.AsReadOnly();
+
+        CompleteDependency();
+
         // TODO: if there are no flames don't do anything
         Entities
-            //.WithReadOnly(field)
             .WithAll<HoldsEmptyBucket>()
             .WithNone<TargetDestination>()
-            .ForEach((Entity e, in Translation translation) => {
+            .WithDisposeOnCompletion(readonlyLakeTranslations)
+            .ForEach((Entity e, in Translation translation) =>
+            {
                 // HACK: We assume that a flame exists here...
                 var closest = new float2(10000000, 100000); // This is bad HACK
+                var bestDistance = float.MaxValue;
                 // HACK: We are mixing types, this is awful.
+
+                for (int i = 0; i < readonlyLakeTranslations.Length; i++)
+                {
+                    var dist = math.distance(readonlyLakeTranslations[i].Value, translation.Value);
+
+                    if (dist < bestDistance)
+                    {
+                        bestDistance = dist;
+                        closest = readonlyLakeTranslations[i].Value.xz;
+                    }
+                }
 
                 ecb.AddComponent(e, new TargetDestination { Value = closest });
 
             }).Schedule();
+
 
         CommandBufferSystem.AddJobHandleForProducer(Dependency);
     }
