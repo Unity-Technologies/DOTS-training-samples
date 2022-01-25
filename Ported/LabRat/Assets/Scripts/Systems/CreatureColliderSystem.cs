944d8e2a2e186f686b3e932781e82cc29952c1a3
Tiago Rezende <tiago.rezende@unity3d.com>
2022-01-25 15:37:05 +0100

    Plugging leak on CreatureColliderSystem

--- Ported/LabRat/Assets/Scripts/Systems/CreatureColliderSystem.cs
+++ Ported/LabRat/Assets/Scripts/Systems/CreatureColliderSystem.cs
@@ -44,7 +44,7 @@ public partial class CreatureColliderSystem : SystemBase
                         ecb.DestroyEntity(entityInQueryIndex, entity);
                     }
                 }
-            }).ScheduleParallel();
+            }).WithDisposeOnCompletion(cattiles).ScheduleParallel();
 
         mECBSystem.AddJobHandleForProducer(Dependency);
     }
@@ -75,7 +75,7 @@ public partial class CreatureColliderSystem : SystemBase
                         ecb.DestroyEntity(entityInQueryIndex, entity);
                     }
                 }
-            }).ScheduleParallel();
+            }).WithDisposeOnCompletion(catTranslations).ScheduleParallel();
 
         mECBSystem.AddJobHandleForProducer(Dependency);
     }
