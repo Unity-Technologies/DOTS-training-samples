951f871ab879de2f0febe11b05b05ed406921fc3
Marina Joffrineau <marina.joffrineau@unity3d.com>
2022-01-25 15:52:29 +0100

    fire extinguish

---
+++ Ported/BucketBrigade/Assets/DOTS/Scripts/Systems/ExtinguishFire.cs
@@ -0,0 +1,46 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+[UpdateAfter(typeof(SetupGameSystem))]
+[UpdateAfter(typeof(PropagateFireSystem))]
+public partial class ExtinguishFire : SystemBase
+{
+    private EntityCommandBufferSystem CommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        CommandBufferSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var gameConstants = GetSingleton<GameConstants>();
+        var field = GetBuffer<FireHeat>(GetSingletonEntity<FireField>());
+        var ecb = CommandBufferSystem.CreateCommandBuffer();
+
+        Entities
+            .WithAll<DroppedBucket>()
+            .WithNone<EmptyBucket>()
+            .ForEach((Entity e, ref Bucket bucket, in Translation translation) =>
+            {
+                for (int outerY = 0; outerY < gameConstants.FieldSize.y; outerY++)
+                {
+                    for (int outerX = 0; outerX < gameConstants.FieldSize.x; outerX++)
+                    {
+                        float3 cellPos = new float3(outerX, 0, outerY) * 0.3f;
+                        var distance = math.distance(translation.Value, cellPos);
+
+                        if (distance < gameConstants.BucketExtinguishRadius && field[outerX + outerY * gameConstants.FieldSize.x] > 0.2f)
+                        {
+                            field[outerX + outerY * gameConstants.FieldSize.x] -= gameConstants.BucketCoolingRate;
+                            bucket.Volume = 0;
+                            ecb.AddComponent<EmptyBucket>(e);
+                        }
+                    }
+                }
+            }).Schedule();
+
+        CommandBufferSystem.AddJobHandleForProducer(Dependency);
+    }
+}
---
+++ Ported/BucketBrigade/Assets/Editor/ReplaceWithPrefab.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEditor;
+
+public class ReplaceWithPrefab : EditorWindow
+{
+    [SerializeField] private GameObject prefab;
+
+    [MenuItem("Tools/Replace With Prefab")]
+    static void CreateReplaceWithPrefab()
+    {
+        EditorWindow.GetWindow<ReplaceWithPrefab>();
+    }
+
+    private void OnGUI()
+    {
+        prefab = (GameObject)EditorGUILayout.ObjectField("Prefab", prefab, typeof(GameObject), false);
+
+        if (GUILayout.Button("Replace"))
+        {
+            var selection = Selection.gameObjects;
+
+            for (var i = selection.Length - 1; i >= 0; --i)
+            {
+                var selected = selection[i];
+                var prefabType = PrefabUtility.GetPrefabType(prefab);
+                GameObject newObject;
+
+                if (prefabType == PrefabType.Prefab)
+                {
+                    newObject = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+                }
+                else
+                {
+                    newObject = Instantiate(prefab);
+                    newObject.name = prefab.name;
+                }
+
+                if (newObject == null)
+                {
+                    Debug.LogError("Error instantiating prefab");
+                    break;
+                }
+
+                Undo.RegisterCreatedObjectUndo(newObject, "Replace With Prefabs");
+                newObject.transform.parent = selected.transform.parent;
+                newObject.transform.localPosition = selected.transform.localPosition;
+                newObject.transform.localRotation = selected.transform.localRotation;
+                newObject.transform.localScale = selected.transform.localScale;
+                newObject.transform.SetSiblingIndex(selected.transform.GetSiblingIndex());
+                Undo.DestroyObjectImmediate(selected);
+            }
+        }
+
+        GUI.enabled = false;
+        EditorGUILayout.LabelField("Selection count: " + Selection.objects.Length);
+    }
+}
