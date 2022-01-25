7558a5f0c4dd90e01c166142befcb85ebdd90018
Krisztian G. Birkas <gaborkb@unity3d.com>
2022-01-25 00:26:38 -0500

    Value/Range tweaks

--- Ported/Tornado/TornadoDOTS/Assets/Scripts/Authoring/CityAuthoring.cs
+++ Ported/Tornado/TornadoDOTS/Assets/Scripts/Authoring/CityAuthoring.cs
@@ -7,12 +7,12 @@ using UnityMonoBehaviour = UnityEngine.MonoBehaviour;
 public class CityAuthoring : UnityMonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
 {
     public UnityGameObject BarPrefab;
-    [UnityRange(1, 1000)] public int NumberOfTowers;
+    [UnityRange(1, 10000)] public int NumberOfTowers;
     [UnityRange(1, 40)] public int TowerMinHeight;
     [UnityRange(1, 40)] public int TowerMaxHeight;
     
-    [UnityRange(20, 100)] public int CityWidth;
-    [UnityRange(20, 100)] public int CityLength;
+    [UnityRange(20, 5000)] public int CityWidth;
+    [UnityRange(20, 5000)] public int CityLength;
     
     public void DeclareReferencedPrefabs(List<UnityGameObject> referencedPrefabs)
     {
