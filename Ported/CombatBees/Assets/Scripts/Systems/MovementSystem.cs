3735c0740d19d901a06fb453bf464539f117f994
TenCatsAtUnity <alexander.hogan@unity3d.com>
2022-01-25 10:01:18 -0800

    Fixed a bug - actually writing to food translation now

--- Ported/CombatBees/Assets/Scripts/Systems/MovementSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/MovementSystem.cs
@@ -7,11 +7,13 @@ public partial class MovementSystem : SystemBase
 {
     protected override void OnUpdate()
     {
+        var tAdd = Time.DeltaTime;
+
         //bees and bits and blood
         var beeHandle = Entities.ForEach((ref Translation translation, ref PP_Movement ppMovement, in BeeTag beeTag, in Velocity velocity) =>
         {
             // do bee movement
-            ppMovement.t += Time.DeltaTime;
+            ppMovement.t += tAdd;
             translation.Value = math.lerp(ppMovement.startLocation, ppMovement.endLocation, math.smoothstep(0, 1, ppMovement.t));
         }).Schedule(Dependency);
 
@@ -21,9 +23,10 @@ public partial class MovementSystem : SystemBase
             .ForEach((Entity e, ref Translation translation, ref PP_Movement ppMovement, in Velocity velocity) =>
             {
                 // calculate bee movement
-                ppMovement.t += Time.DeltaTime;
+                ppMovement.t += tAdd;
                 var beePosition = math.lerp(ppMovement.startLocation, ppMovement.endLocation, math.smoothstep(0, 1, ppMovement.t));
                 beePosition -= new float3(0, -1, 0); // food dangles below
+                translation.Value = beePosition;
 
                 // do orientation later
 
