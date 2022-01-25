4a357221c180945280394cf4f845bac3141edcc5
RLHWORKALIEN\rlh <richard.harrington@unity3d.com>
2022-01-25 11:47:41 -0800

    Added Bee Bits prefab

--- Ported/CombatBees/Assets/Scripts/Components/Spawner.cs
+++ Ported/CombatBees/Assets/Scripts/Components/Spawner.cs
@@ -6,5 +6,6 @@ public struct Spawner : IComponentData
     public Entity YellowBeePrefab;
     public Entity BlueBeePrefab;
     public Entity BloodPrefab;
+    public Entity BeeBitsPrefab;
     public Entity ResourcePrefab;
 }
