8a0a46a7f996d0a7ba1fc4dba99e4a13b2fb658c
Tiago Rezende <tiago.rezende@unity3d.com>
2022-01-25 10:30:40 +0100

    PropagateColor now works as intended 😅

--- Ported/LabRat/Assets/Scripts/Authoring/ExitAuthoring.cs
+++ Ported/LabRat/Assets/Scripts/Authoring/ExitAuthoring.cs
@@ -1,3 +1,4 @@
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Rendering;
 using UnityEngine;
@@ -6,7 +7,18 @@ public class ExitAuthoring : MonoBehaviour, IConvertGameObjectToEntity
 {
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
-        dstManager.AddComponent<URPMaterialPropertyBaseColor>(entity);
+        
+        var allRenderers = transform.GetComponentsInChildren<MeshRenderer>();
+        var needBaseColor = new NativeArray<Entity>(allRenderers.Length, Allocator.Temp);
+
+        for (int i = 0; i < allRenderers.Length; i++)
+        {
+            var meshRenderer = allRenderers[i];
+            needBaseColor[i] = conversionSystem.GetPrimaryEntity(meshRenderer.gameObject);
+        }
+
+        dstManager.AddComponent<URPMaterialPropertyBaseColor>(needBaseColor);
+
         dstManager.AddComponent<PropagateColor>(entity);
         dstManager.AddComponent<Tile>(entity);
         dstManager.AddComponent<Exit>(entity);
