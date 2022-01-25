7400a29e4048d4564997607ca8268af2dac3afd8
Gabriel Dechichi <gabriel.dechichi@unity3d.com>
2022-01-25 08:50:09 -0800

    create BarAssignColorSystem

---
+++ Ported/Tornado/TornadoDOTS/Assets/Scripts/Systems/BarAssignColorSystem.cs
@@ -0,0 +1,40 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using Random = Unity.Mathematics.Random;
+
+public partial class BarAssignColorSystem : SystemBase
+{
+    private EntityQuery barAssignColorEntitiesQuery;
+    private EntityCommandBufferSystem commandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        commandBufferSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var ecb = commandBufferSystem.CreateCommandBuffer();
+        
+        ecb.RemoveComponentForEntityQuery<BarAssignColor>(barAssignColorEntitiesQuery);
+
+        var gcfe = GetComponentDataFromEntity<Translation>();
+        var rnd = new Random(1234);
+        Entities
+            .WithNativeDisableContainerSafetyRestriction(gcfe)
+            .WithStoreEntityQueryInField(ref barAssignColorEntitiesQuery)
+            .WithAll<BarAssignColor>()
+            .ForEach((ref URPMaterialPropertyBaseColor color, in BarConnection connection) =>
+            {
+                var join1Pos = gcfe[connection.Joint1].Value;
+                var join2Pos = gcfe[connection.Joint2].Value;
+                var forward = math.normalize(join2Pos - join1Pos);
+
+                var upDot = math.acos(math.abs(math.dot(forward, new float3(0, 1, 0))))/math.PI;
+                color.Value = new float4(1) * rnd.NextFloat(0.7f, 1.0f) * upDot;
+            }).ScheduleParallel();
+    }
+}
