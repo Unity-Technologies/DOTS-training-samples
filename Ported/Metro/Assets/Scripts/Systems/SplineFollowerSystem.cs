fa8ac4fb5a44fbf137eb5671f8775b495df59e53
Michel Salvado <michel.salvado@unity3d.com>
2022-01-25 18:26:15 -0500

    Fixed blob access errors
    Replace all parameters from Spline to SplineData, and put refs everywhere, including in the 'var' at the root of the function calls hierarchy. This is what was mainly creating the issue, as 'var' only was making a copy explitely.

--- Ported/Metro/Assets/Scripts/Systems/SplineFollowerSystem.cs
+++ Ported/Metro/Assets/Scripts/Systems/SplineFollowerSystem.cs
@@ -1,4 +1,3 @@
-#if false
 using Onboarding.BezierPath;
 using Unity.Entities;
 using Unity.Mathematics;
@@ -14,26 +13,27 @@ public partial class SplineFollowerSystem : SystemBase
             .WithAll<SplineFollower>()
             .ForEach((ref Translation translation,
                       ref Rotation rotation,
-                      in Spline spline) =>
+                      ref Spline spline) =>
             {
+                ref var splineData = ref spline.splinePath.Value;
+                
                 int dummySegment = 0;
-                InterpolatePosition(spline, ref dummySegment, distance, out translation.Value);
+                InterpolatePosition(ref splineData, ref dummySegment, distance, out translation.Value);
             }).ScheduleParallel();
     }
 
-    public static void InterpolatePosition(Spline data, ref int currentSegment, float distanceFromOrigin, out float3 value) {
-        var splineData = data.splinePath.Value;
+    public static void InterpolatePosition(ref SplineData splineData, ref int currentSegment, float distanceFromOrigin, out float3 value) 
+    {
         float parametricT = FindSegmentFromDistance(ref splineData, ref currentSegment, distanceFromOrigin);
-
         InterpolatePosition(ref splineData, splineData.distanceToParametric[currentSegment].bezierIndex, parametricT, out value);
     }
 
-    public static void InterpolatePosition(ref SplineData splineData, int splineIndex, float _t, out float3 value) {
+    public static void InterpolatePosition(ref SplineData data, int splineIndex, float _t, out float3 value) {
         int offset = splineIndex * 3;
-        float3 p0 = splineData.bezierControlPoints[offset + 0];
-        float3 p1 = splineData.bezierControlPoints[offset + 1];
-        float3 p2 = splineData.bezierControlPoints[offset + 2];
-        float3 p3 = splineData.bezierControlPoints[offset + 3];
+        float3 p0 = data.bezierControlPoints[offset + 0];
+        float3 p1 = data.bezierControlPoints[offset + 1];
+        float3 p2 = data.bezierControlPoints[offset + 2];
+        float3 p3 = data.bezierControlPoints[offset + 3];
         double t = _t;
         double t2 = t * t;
         double t3 = t2 * t;
@@ -45,22 +45,22 @@ public partial class SplineFollowerSystem : SystemBase
                             (float)(p0.z * _1_t_3 + 3 * p1.z * _1_t_2 * t + 3 * p2.z * _1_t * t2 + p3.z * t3));
     }
 
-    private static float FindSegmentFromDistance(ref SplineData data, ref int currentSegment, float distanceFromOrigin) {
-        float distance = math.clamp(distanceFromOrigin, 0, data.pathLength);
+    private static float FindSegmentFromDistance(ref SplineData splineData, ref int currentSegment, float distanceFromOrigin) {
+        float distance = math.clamp(distanceFromOrigin, 0, splineData.pathLength);
         int increment = 1;
 
         // If the user teleports around, the cache will not work properly, as it's assuming we are going at a reasonable speed
         // in the same direction.
         // To alleviate the issue, we assume we always more forward, except if we pass a distance that was supposed to already have
         // been treated, and would never be found in our segment list if we were going forward through the list
-        if (distance < data.distanceToParametric[currentSegment].start)
+        if (distance < splineData.distanceToParametric[currentSegment].start)
             increment = -1;
 
         float parametricT = -1;
-        for (int i = currentSegment; i >= 0 && i < data.distanceToParametric.Length; i += increment) {
-            if (distance >= data.distanceToParametric[i].start && distance <= data.distanceToParametric[i].end) {
-                float ratio = (distance - data.distanceToParametric[i].start) / (data.distanceToParametric[i].end - data.distanceToParametric[i].start);
-                parametricT = InterpolateBezier1D(data.distanceToParametric[i], ratio);
+        for (int i = currentSegment; i >= 0 && i < splineData.distanceToParametric.Length; i += increment) {
+            if (distance >= splineData.distanceToParametric[i].start && distance <= splineData.distanceToParametric[i].end) {
+                float ratio = (distance - splineData.distanceToParametric[i].start) / (splineData.distanceToParametric[i].end - splineData.distanceToParametric[i].start);
+                parametricT = InterpolateBezier1D(ref splineData.distanceToParametric[i], ratio);
                 currentSegment = i;
                 break;
             }
@@ -70,9 +70,8 @@ public partial class SplineFollowerSystem : SystemBase
         return parametricT;
     }
 
-    public static float InterpolateBezier1D(ApproximatedCurveSegment data, float t) {
+    public static float InterpolateBezier1D(ref ApproximatedCurveSegment data, float t) {
         // This is assuming the coefficients are DEVELOPED at baking time
         return data.p0 + t * (data.p1 + t * (data.p2 + data.p3 * t));
     }
 }
-#endif
