3abe3690f0bd7f8fdf6612edf5320ded191c49c1
TenCatsAtUnity <alexander.hogan@unity3d.com>
2022-01-25 12:15:44 -0800

    Movement system updates - making food dangle only if it's not moving

--- Ported/CombatBees/Assets/Scripts/Systems/MovementSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/MovementSystem.cs
@@ -9,6 +9,39 @@ public partial class MovementSystem : SystemBase
     {
         var tAdd = Time.DeltaTime;
 
+        /*
+        //bits
+        Entities
+            .WithAll<BeeBitsTag>()
+            .ForEach((Entity e, ref Translation translation) =>
+            {
+                // calculate bits falling movement - straight down for now
+                if (translation.Value.y > 0)
+                {
+                    translation.Value = translation.Value * -9.8f * tAdd;
+                }
+                else
+                {
+                    //create an ECB, kill this Entity, Spawn a Blood here
+                }
+            }).Schedule();
+
+        //blood
+        Entities
+            .WithAll<FoodTag>()
+            .ForEach((Entity e, ref Translation translation, ref PP_Movement ppMovement, in Velocity velocity) =>
+            {
+                // calculate bee movement
+                ppMovement.t += tAdd;
+                var beePosition = math.lerp(ppMovement.startLocation, ppMovement.endLocation, math.smoothstep(0, 1, ppMovement.t));
+                beePosition -= new float3(0, -1, 0); // food dangles below
+                translation.Value = beePosition;
+
+                // do orientation later
+
+            }).Schedule();
+*/
+
         //bees
         Dependency = Entities.ForEach((ref Translation translation, ref PP_Movement ppMovement, in BeeTag beeTag, in Velocity velocity) =>
         {
@@ -24,9 +57,14 @@ public partial class MovementSystem : SystemBase
             {
                 // calculate bee movement
                 ppMovement.t += tAdd;
-                var beePosition = math.lerp(ppMovement.startLocation, ppMovement.endLocation, math.smoothstep(0, 1, ppMovement.t));
-                beePosition -= new float3(0, -1, 0); // food dangles below
-                translation.Value = beePosition;
+
+                if (!ppMovement.startLocation.Equals(ppMovement.endLocation))
+                {
+                    var beePosition = math.lerp(ppMovement.startLocation, ppMovement.endLocation,
+                        math.smoothstep(0, 1, ppMovement.t));
+                    beePosition -= new float3(0, 1, 0); // food dangles below
+                    translation.Value = beePosition;
+                }
 
                 // do orientation later
 
