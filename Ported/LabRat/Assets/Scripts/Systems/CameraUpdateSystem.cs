4133285b38759410ea038b47a50e52c88bf826a2
Alexander Ederer <alexander.ederer@unity3d.com>
2022-01-26 17:08:43 +0100

    System to update camera to map size

---
+++ Ported/LabRat/Assets/Scripts/Systems/CameraUpdateSystem.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public partial class CameraUpdateSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Config conf = GetSingleton<Config>();
+
+        Camera cam = Camera.main;
+
+        int size = math.max(conf.MapWidth, conf.MapHeight);
+        cam.orthographicSize = 0.75f * size;
+        float factor = size / 20.0f;
+        cam.transform.position = new Vector3(18 * factor, 25 * factor, 18 * factor);
+    }
+}
