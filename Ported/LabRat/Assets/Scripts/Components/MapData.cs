df796f85c512a5b41a90bf27d22640207d623ed2
Nicolaj Zøllner <nicolaj.zollner@unity3d.com>
2022-01-26 16:22:35 +0100

    Arrows can no longer be placed on holes

--- Ported/LabRat/Assets/Scripts/Components/MapData.cs
+++ Ported/LabRat/Assets/Scripts/Components/MapData.cs
@@ -8,4 +8,22 @@ public struct TileData : IBufferElementData
 
 public struct MapData: IComponentData
 {
+    public static DirectionEnum GetTileWalls(Config config, DynamicBuffer<TileData> mapTiles, int2 coord)
+    {
+        if (coord.x < 0 || coord.y < 0 || coord.x >= config.MapWidth || coord.y >= config.MapHeight)
+        {
+            return DirectionEnum.North | DirectionEnum.East | DirectionEnum.South | DirectionEnum.West;
+        }
+    
+        int index = coord.y * config.MapWidth + coord.x;
+        return mapTiles[index].Walls.Value;
+    }
+    
+    public static bool HasHole(Config config, DynamicBuffer<TileData> mapTiles, int2 current)
+    {
+        var hole = GetTileWalls(config, mapTiles, current);
+    
+        return hole.Impassable();
+    }
 }
+
