f3d7e15e4f819849f267af34eaab2cbace8a794f
Jacob Kjær <jacob.kjaer@unity3d.com>
2022-01-26 15:41:13 +0100

    Smooth movement

--- Ported/BucketBrigade/Assets/DOTS/Scripts/Systems/Team/PickLinePositionsForTeamSystem.cs
+++ Ported/BucketBrigade/Assets/DOTS/Scripts/Systems/Team/PickLinePositionsForTeamSystem.cs
@@ -8,11 +8,14 @@ using Unity.Transforms;
 [UpdateAfter(typeof(MoveToTargetLocationSystem))]
 public partial class PickLinePositionsForTeamSystem : SystemBase
 {
+    private EntityCommandBufferSystem CommandBufferSystem;
+
     private EntityQuery LakeQuery;
     private EntityQuery FireQuery;
 
     protected override void OnCreate()
     {
+        CommandBufferSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
         LakeQuery = GetEntityQuery(ComponentType.ReadOnly<Lake>(), ComponentType.ReadOnly<Translation>());
         FireQuery = GetEntityQuery(ComponentType.ReadOnly<FireRenderer>(), ComponentType.ReadOnly<Translation>());
     }
@@ -39,6 +42,7 @@ public partial class PickLinePositionsForTeamSystem : SystemBase
     protected override void OnUpdate()
     {
         var lakeTranslations = LakeQuery.ToComponentDataArray<Translation>(Allocator.TempJob);
+        var ecb = CommandBufferSystem.CreateCommandBuffer();
 
         Entities
             .WithReadOnly(lakeTranslations)
@@ -84,16 +88,31 @@ public partial class PickLinePositionsForTeamSystem : SystemBase
                     var target = (math.lerp(lineLakePosition.Value, lineFirePosition.Value, t) + math.sin(t * math.PI) * offset + offset);
 
                     // DON'T REMOVE THIS LINE
-                    TeamWorkers entity = workersBuffer[x];
+                    TeamWorkers workerEntity = workersBuffer[x];
 
-                    SetComponent(entity, new Translation() { Value = target });
+                    if (HasComponent<TargetDestination>(workerEntity))
+                        SetComponent<TargetDestination>(workerEntity, target.xz);
+                    else
+                        ecb.AddComponent<TargetDestination>(workerEntity, target.xz);
                 }
 
                 // Backward Line
+                for (int x = 0; x < gameConstants.WorkersPerLine; x++)
                 {
+                    float t = (float)(x + 1) / (gameConstants.WorkersPerLine + 2);
+                    var target = (math.lerp(lineLakePosition.Value, lineFirePosition.Value, t) - math.sin(t * math.PI) * offset + offset);
 
+                    // DON'T REMOVE THIS LINE
+                    TeamWorkers workerEntity = workersBuffer[x + gameConstants.WorkersPerLine];
+
+                    if (HasComponent<TargetDestination>(workerEntity))
+                        SetComponent<TargetDestination>(workerEntity, target.xz);
+                    else
+                        ecb.AddComponent<TargetDestination>(workerEntity, target.xz);
                 }
             })
             .Schedule();
+
+        CommandBufferSystem.AddJobHandleForProducer(Dependency);
     }
 }
