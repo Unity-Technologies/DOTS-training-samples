a792265f1c73278c638d778fa66f6038a67ab0fb
Riccardo Lasagno <riccardo.lasagno@unity3d.com>
2022-01-27 08:24:19 +0000

    remove obstacle debug code

--- Ported/Assets/Scripts/Systems/PheromoneTextureUpdateSystem.cs
+++ Ported/Assets/Scripts/Systems/PheromoneTextureUpdateSystem.cs
@@ -5,6 +5,25 @@ public partial class PheromoneTextureUpdateSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        
+        Entities.ForEach((Entity entity, in Ground ground) =>
+        {
+            var gridEntity = GetSingletonEntity<Grid2D>();
+            var grid = GetComponent<Grid2D>(gridEntity);
+            var obstacles = GetBuffer<ObstaclePositionAndRadius>(gridEntity);
+            var texture = ground.Texture;
+            var data = texture.GetRawTextureData<Color32>();
+
+            for (int i = 0; i < grid.columnLength; ++i)
+            {
+                for (int j = 0; j < grid.rowLength; ++j)
+                {
+                    var index = j + i * grid.rowLength;
+                    var indexTex = i + j * grid.columnLength;
+                    data[indexTex] = obstacles[index].IsValid ? new Color(0, obstacles[index].position.x + 0.5f, obstacles[index].position.y + 0.5f, 1f) : Color.black;
+                }
+            }
+
+            texture.Apply();
+        }).WithoutBurst().Run();
     }
 }
