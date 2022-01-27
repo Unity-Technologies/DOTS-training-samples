606c261334acba4e47a5829ef9d683089f30f491
Jacob Kjær <jacob.kjaer@unity3d.com>
2022-01-27 17:01:22 +0100

    Clean up SetLakeAsTarget system

--- Ported/BucketBrigade/Assets/DOTS/Scripts/Systems/SetLakeAsTargetSystem.cs
+++ Ported/BucketBrigade/Assets/DOTS/Scripts/Systems/SetLakeAsTargetSystem.cs
@@ -10,7 +10,6 @@ public partial class SetLakeAsTargetSystem : SystemBase
 {
     private EntityCommandBufferSystem CommandBufferSystem;
 
-    private EntityQuery LakeQuery;
     private EntityQuery BucketQuery;
 
     protected override void OnCreate()
@@ -25,8 +24,6 @@ public partial class SetLakeAsTargetSystem : SystemBase
         var buckets = BucketQuery.ToComponentDataArray<Bucket>(Allocator.TempJob);
         var bucketEntities = BucketQuery.ToEntityArray(Allocator.TempJob);
 
-
-        // TODO: if there are no flames don't do anything
         Entities
             .WithReadOnly(buckets)
             .WithDisposeOnCompletion(buckets)
@@ -66,42 +63,6 @@ public partial class SetLakeAsTargetSystem : SystemBase
                 {
                     targetDestination = bucketFetcher.LakePosition.xz;
                 }
-
-                /*
-                if (lakeTranslations.Length == 0)
-                    return;
-
-                // HACK: We assume that a flame exists here...
-                var closestIndex = -1;
-                var closest = new float2(10000000, 100000); // This is bad HACK
-                var bestDistance = float.MaxValue;
-                // HACK: We are mixing types, this is awful.
-
-                for (int i = 0; i < lakeTranslations.Length; i++)
-                {
-                    var dist = math.distance(lakeTranslations[i].Value, translation.Value);
-
-                    if (dist < bestDistance)
-                    {
-                        bestDistance = dist;
-                        closest = lakeTranslations[i].Value.xz;
-                        closestIndex = i;
-                    }
-                }
-
-                // TODO: If distance is close enough to fill bucket, set a "filling bucket" tag instead
-                if (bestDistance < 0.1f)
-                {
-                    ecb.RemoveComponent<HoldsEmptyBucket>(entityInQueryIndex, e);
-                    ecb.AddComponent<HoldsBucketBeingFilled>(entityInQueryIndex, e);
-                    ecb.RemoveComponent<EmptyBucket>(entityInQueryIndex, holdingBucket.HeldBucket);
-                // BUcket volume == hack
-                    ecb.AppendToBuffer(entityInQueryIndex, lakeEntities[closestIndex], new BucketFillAction { Bucket = holdingBucket.HeldBucket, FireFighter = e, BucketVolume = 0f , Position = translation.Value });
-                }
-                else
-                {
-                    ecb.AddComponent(entityInQueryIndex, e, new TargetDestination { Value = closest });
-                }*/
             }).ScheduleParallel();
 
 
