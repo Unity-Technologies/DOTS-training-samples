173d3077f6c29f2b7f71d9dc4c5ddcc2171f4f64
TenCatsAtUnity <alexander.hogan@unity3d.com>
2022-01-26 18:02:30 -0800

    adding utils class

---
+++ Ported/CombatBees/Assets/Scripts/utils.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Mathematics;
+using UnityEditor.UIElements;
+
+namespace Utils
+{
+    public static class utilityExtensions
+    {
+        public static float3 Floored(this float3 f)
+        {
+            return new float3(f.x, 0, f.z);
+        }
+    }
+}
