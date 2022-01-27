d3b6323e87dea4c6e2a8e3648762d10c99ee7436
Charles Yeh <charles.yeh@unity3d.com>
2022-01-26 16:45:53 -0800

    Add Android build editor script to do one click Android settings Add keystore and DOTS specific build settings

---
+++ Ported/MagneticRoads-DOTS/Assets/Editor/AutoBuild.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Ext
+{
+
+    public class AutoBuild
+    {
+
+        [MenuItem("Tools/Quick Build Android Development Build/Build and Run on connected Android device")]
+        public static void BuildAndRunAndroidAPK()
+        {
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, "BUILD_INTERNAL");
+            EditorUserBuildSettings.buildAppBundle = false;
+            PlayerSettings.SetStackTraceLogType(LogType.Log, StackTraceLogType.ScriptOnly);
+            PlayerSettings.SetStackTraceLogType(LogType.Assert, StackTraceLogType.ScriptOnly);
+            PlayerSettings.SetStackTraceLogType(LogType.Error, StackTraceLogType.ScriptOnly);
+            PlayerSettings.SetStackTraceLogType(LogType.Exception, StackTraceLogType.ScriptOnly);
+            PlayerSettings.SetStackTraceLogType(LogType.Warning, StackTraceLogType.ScriptOnly);
+            PerformBuildAndroid(true);
+        }
+
+        public static void PerformBuildAndroid(bool bRun = false)
+        {
+
+            //select enabled scene to scenes list
+            string[] scenes = EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+
+            AutoBuildPlayerSetting.SetupAndroid();
+
+            return;
+
+            /*BuildPlayerOptions bpo = new BuildPlayerOptions();
+            bpo.scenes = scenes;
+            bpo.target = BuildTarget.Android;
+            bpo.locationPathName = "./group2.apk";
+
+            //check if that file exist
+            if (File.Exists(bpo.locationPathName) == true)
+            {
+                Debug.Log("<AutoBuild.PerformBuildAndroid> file " + bpo.locationPathName + " exist. Remove it ...");
+                FileAttributes attr = File.GetAttributes(bpo.locationPathName);
+                if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                    Directory.Delete(bpo.locationPathName);
+                else
+                    File.Delete(bpo.locationPathName);
+            }
+
+            bpo.options = BuildOptions.None;
+            if (bRun == true)
+            {
+                bpo.options |= BuildOptions.AutoRunPlayer;
+            }
+            bpo.options |= BuildOptions.Development;
+            bpo.options |= BuildOptions.ConnectWithProfiler;
+
+            bpo.targetGroup = BuildTargetGroup.Android;
+            BuildReport report = BuildPipeline.BuildPlayer(bpo);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("<AutoBuild.PerformBuild> Build succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.LogError("<AutoBuild.PerformBuild> Build failed #" + summary.totalErrors);
+            }*/
+        }
+
+        public static void AutoBuildAndroid()
+        {
+            Debug.Log("<AutoBuild.AutoBuildAndroid> start Android build automation...");
+            EditorUserBuildSettings.buildAppBundle = false;
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, "BUILD_INTERNAL");
+            PlayerSettings.SetStackTraceLogType(LogType.Log, StackTraceLogType.ScriptOnly);
+            PerformBuildAndroid(false);
+        }
+
+    }
+
+}
---
+++ Ported/MagneticRoads-DOTS/Assets/Editor/AutoBuildPlayerSetting.cs
@@ -0,0 +1,72 @@
+﻿/// AutoBuildPlayerSetting.cs - automatically setup player settings
+///
+
+
+using UnityEngine;
+using UnityEditor;
+
+namespace Ext {
+
+    public class AutoBuildPlayerSetting
+    {
+
+        private static void SetupCommonSettings()
+        {
+            PlayerSettings.graphicsJobMode = GraphicsJobMode.Native;
+            PlayerSettings.graphicsJobs = false;
+
+            PlayerSettings.muteOtherAudioSources = false;
+
+            PlayerSettings.colorSpace = ColorSpace.Gamma;
+            PlayerSettings.companyName = "Unity Technologies";
+            PlayerSettings.defaultInterfaceOrientation = UIOrientation.LandscapeRight;
+            PlayerSettings.MTRendering = true;
+            PlayerSettings.productName = "DotsTrainingGroup2";
+            PlayerSettings.allowedAutorotateToPortrait = PlayerSettings.allowedAutorotateToPortraitUpsideDown = false;
+        }
+
+        public static void SetupAndroid()
+        {
+            SetupCommonSettings();
+
+            PlayerSettings.Android.minifyDebug = false;
+            PlayerSettings.Android.minifyRelease = false;
+
+            EditorUserBuildSettings.allowDebugging = true;
+            EditorUserBuildSettings.androidBuildSystem = AndroidBuildSystem.Gradle;
+
+            PlayerSettings.gpuSkinning = false;
+
+            PlayerSettings.Android.androidIsGame = false;
+            PlayerSettings.Android.androidTVCompatibility = false;
+            PlayerSettings.Android.ARCoreEnabled = false;
+            PlayerSettings.Android.blitType = AndroidBlitType.Auto;
+            PlayerSettings.Android.buildApkPerCpuArchitecture = false;
+            PlayerSettings.Android.disableDepthAndStencilBuffers = false;
+            PlayerSettings.Android.forceInternetPermission = true;
+            PlayerSettings.Android.forceSDCardPermission = true;
+            PlayerSettings.Android.keystoreName = "group2.keystore";
+            PlayerSettings.Android.minSdkVersion = AndroidSdkVersions.AndroidApiLevel22;
+            PlayerSettings.Android.preferredInstallLocation = AndroidPreferredInstallLocation.Auto;
+            PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARMv7 | AndroidArchitecture.ARM64;
+            PlayerSettings.Android.targetSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
+            PlayerSettings.Android.useCustomKeystore = true;
+
+            PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.unity.dotstraining.group2");
+
+            PlayerSettings.Android.keyaliasName = "group2";
+            PlayerSettings.Android.keyaliasPass = "group2";
+            PlayerSettings.Android.keystoreName = "./Credentials/group2.keystore";
+            PlayerSettings.Android.keystorePass = "group2";
+
+            PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.Android, ApiCompatibilityLevel.NET_4_6);
+            PlayerSettings.SetIl2CppCompilerConfiguration(BuildTargetGroup.Android, Il2CppCompilerConfiguration.Release);
+            PlayerSettings.SetManagedStrippingLevel(BuildTargetGroup.Android, ManagedStrippingLevel.Low);
+            PlayerSettings.SetMobileMTRendering(BuildTargetGroup.Android, false);
+            PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.Android, true);
+        }
+
+    }
+
+}
