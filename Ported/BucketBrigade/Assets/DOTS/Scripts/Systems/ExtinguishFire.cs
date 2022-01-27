3025534f0c02c44608ada6c137008adf305f75a6
Marina Joffrineau <marina.joffrineau@unity3d.com>
2022-01-27 10:43:05 +0100

    fix bucket filling

--- Ported/BucketBrigade/Assets/DOTS/Scripts/Systems/ExtinguishFire.cs
+++ Ported/BucketBrigade/Assets/DOTS/Scripts/Systems/ExtinguishFire.cs
@@ -20,7 +20,6 @@ public partial class ExtinguishFire : SystemBase
         var ecb = CommandBufferSystem.CreateCommandBuffer();
 
         Entities
-            .WithAll<ThrownBucket>()
             .WithNone<EmptyBucket>()
             .ForEach((Entity e, ref Bucket bucket, in Translation translation) =>
             {
