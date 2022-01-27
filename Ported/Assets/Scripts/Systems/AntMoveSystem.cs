eed6a1d9ea54384d59725166a44959c411f18bc8
Bertrand Guay-Paquet <bertrand.guaypaquet@unity3d.com>
2022-01-27 14:15:42 +0100

    Add AntMoveSystem

---
+++ Ported/Assets/Scripts/Systems/AntMoveSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Profiling;
+using Unity.Rendering;
+using Unity.Transforms;
+using UnityEngine;
+
+[UpdateAfter(typeof(SteeringSystem))]
+public partial class AntMoveSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var deltaTime = Time.DeltaTime;
+        Entities.WithAll<AntTag>().ForEach((Entity entity, ref Translation translation, ref Rotation rotation, in Velocity velocity) =>
+        {
+            var deltaPosition = velocity.Direction * velocity.Speed * deltaTime;
+
+            var newPosition = translation.Value.xz + deltaPosition;
+            translation.Value = new float3(newPosition, 0);
+            
+            rotation.Value = quaternion.LookRotation(new float3(velocity.Direction, 0), new float3(0, 0, 1));
+        }).Schedule();
+    }
+}
