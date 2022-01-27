dd079ea64796772266d9586e4fee8d324e272707
Alexander Ederer <alexander.ederer@unity3d.com>
2022-01-27 11:01:38 +0100

    Set camera clip planes

--- Ported/LabRat/Assets/Scripts/Systems/CameraUpdateSystem.cs
+++ Ported/LabRat/Assets/Scripts/Systems/CameraUpdateSystem.cs
@@ -6,13 +6,17 @@ public partial class CameraUpdateSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        Config conf = GetSingleton<Config>();
-
-        Camera cam = Camera.main;
+        var conf = GetSingleton<Config>();
+        var goRefs = this.GetSingleton<GameObjectRefs>();
+        Camera cam = goRefs.Camera;
 
         int size = math.max(conf.MapWidth, conf.MapHeight);
         cam.orthographicSize = 0.75f * size;
         float factor = size / 20.0f;
         cam.transform.position = new Vector3(18 * factor, 25 * factor, 18 * factor);
+
+        // Set clip planes to increase shadow quality
+        cam.nearClipPlane = size;
+        cam.farClipPlane = size * 2;
     }
 }
