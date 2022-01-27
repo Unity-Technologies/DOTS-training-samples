8ec16490ca04373a76e788b2989553860b0a73e8
Nicolaj Zøllner <nicolaj.zollner@unity3d.com>
2022-01-27 11:24:37 +0100

    Hover arrow no longer uses gameobject but dots

---
+++ Ported/LabRat/Assets/Scripts/Authoring/ArrowHoverAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using MonoBehaviour = UnityEngine.MonoBehaviour;
+
+public class ArrowHoverAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public UnityEngine.Color color;
+    public bool UserControlled;
+    public int Index;
+    public uint AISeed;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponent<Player>(entity);
+        dstManager.AddComponentData(entity, new CursorPosition { Value = float2.zero });
+    }
+}
