8c9c1ef7cac01a021772091b999c6578d0f876bb
Elfi <elfi.kuhndorf@unity3d.com>
2022-02-01 17:15:44 +0100

    Adjusted change of scale values and reformatted code

--- Ported/Combat Bees - DOTS/Assets/Scripts/Systems/BeeStretching.cs
+++ Ported/Combat Bees - DOTS/Assets/Scripts/Systems/BeeStretching.cs
@@ -8,23 +8,33 @@ public partial class BeeStretching : SystemBase
     {
         float speedStretch = 0.1f;
         float deltaTime = World.Time.DeltaTime;
-            
-        Entities.WithAll<BeeTag>().ForEach((ref NonUniformScale nonUniformScale, in BeeDead beeDead, in Velocity velocity) =>
-        {
-            if (!beeDead.Value)
+
+        Entities.WithAll<BeeTag>().ForEach(
+            (ref NonUniformScale nonUniformScale, in BeeDead beeDead, in Velocity velocity) =>
             {
-                float magnitude = math.sqrt(velocity.Value.x * velocity.Value.x + velocity.Value.y * velocity.Value.y + velocity.Value.z + velocity.Value.z);
-                float stretch = math.max(1f, magnitude * speedStretch);
-                nonUniformScale.Value.z = stretch;
-                nonUniformScale.Value.x = 1f / stretch;
-                nonUniformScale.Value.y = 1f / stretch;
-            }
-            else
-            {
-                if (nonUniformScale.Value.x > 0.01f){
-                    nonUniformScale.Value = nonUniformScale.Value - new float3(0.1f, 0.1f, 0.1f) * new float3(deltaTime, deltaTime, deltaTime);
+                if (!beeDead.Value)
+                {
+                    float magnitude = math.sqrt(velocity.Value.x * velocity.Value.x +
+                                                velocity.Value.y * velocity.Value.y + velocity.Value.z +
+                                                velocity.Value.z);
+                    float stretch = math.max(1f, magnitude * speedStretch);
+                    nonUniformScale.Value.z = stretch;
+                    nonUniformScale.Value.x = 2f / stretch;
+                    nonUniformScale.Value.y = 2f / stretch;
                 }
-            }
-        }).ScheduleParallel();
+                else if (nonUniformScale.Value.z > 1f)
+                {
+                    nonUniformScale.Value = new float3(1f, 1f, 1f);
+                }
+                else
+                {
+                    if (nonUniformScale.Value.x > 0.01f)
+                    {
+                        nonUniformScale.Value = nonUniformScale.Value -
+                                                new float3(0.1f, 0.1f, 0.1f) *
+                                                new float3(deltaTime, deltaTime, deltaTime);
+                    }
+                }
+            }).ScheduleParallel();
     }
 }
