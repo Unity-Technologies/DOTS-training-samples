9c1b4b834741042e17339221088b9438de968c9e
Raymond Luong <raymond.luong@unity3d.com>
2022-03-29 13:16:27 -0700

    Align resource grid with Battlefield

--- Ported/CombatBees/Assets/Scripts/Systems/ResourceSpawnerSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/ResourceSpawnerSystem.cs
@@ -15,12 +15,18 @@ public partial class ResourceSpawnerSystem : SystemBase
 
             var random = new Random(1234);
 
+            var minX = (int) -Field.size.x/2 + 20;
+            var maxX = (int) Field.size.x/2 - 20;
+            var minZ = (int) -Field.size.z/2;
+            var maxZ = (int) Field.size.z/2;
+            var minY = (int) -Field.size.y/2 + 1;
+
             for (int i = 0; i < spawner.StartingResourceCount; ++i)
             {
                 var instance = ecb.Instantiate(spawner.ResourcePrefab);
-                var xCoord = random.NextInt(0, (int) Field.size.x);
-                var zCoord = random.NextInt(0, (int) Field.size.z);
-                var translation = new Translation { Value = new float3(xCoord, 0, zCoord)};
+                var xCoord = random.NextInt(minX, maxX);
+                var zCoord = random.NextInt(minZ, maxZ);
+                var translation = new Translation { Value = new float3(xCoord, minY, zCoord)};
                 ecb.SetComponent(instance, translation);
             }
         }).Run();
