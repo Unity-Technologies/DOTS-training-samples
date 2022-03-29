81e1cbd2beb5e13c08bec3ed7a3503712a1ad580
Brian Elgaard Bennett <belgaard@users.noreply.github.com>
2022-03-29 16:15:46 +0200

    Extracted Create and Solve into statics

--- Ported/JumpTheGun/Assets/Scripts/Components/Parabola.cs
+++ Ported/JumpTheGun/Assets/Scripts/Components/Parabola.cs
@@ -1,34 +1,39 @@
 using Unity.Entities;
 using Unity.Mathematics;
 
-public struct Parabola : IComponentData
+public struct ParabolaData : IComponentData
 {
-    private float a;
-    private float b;
-    private float c;
+    public float a;
+    public float b;
+    public float c;
+}
 
+public struct Parabola
+{
     /// <summary>
     /// Creates a parabola (in the form y = a*t*t + b*t + c) that matches the following conditions:
     /// - Contains the points (0, startY) and (1, endY)
     /// - Reaches the given height.
     /// </summary>
-    public void Create(float startY, float height, float endY)
+    public static ParabolaData Create(float startY, float height, float endY)
     {
+        ParabolaData data = new ParabolaData();
 
-        c = startY;
+        data.c = startY;
 
         float k = math.sqrt(math.abs(startY - height)) / (math.sqrt(math.abs(startY - height)) + math.sqrt(math.abs(endY - height)));
-        a = (height - startY - k * (endY - startY)) / (k * k - k);
+        data.a = (height - startY - k * (endY - startY)) / (k * k - k);
 
-        b = endY - startY - a;
+        data.b = endY - startY - data.a;
+
+        return data;
     }
 
     /// <summary>
     /// Solves a parabola (in the form y = a*t*t + b*t + c) for y.
     /// </summary>
-    public float Solve(float t)
+    public static float Solve(in ParabolaData data, float t)
     {
-        return a * t * t + b * t + c;
+        return data.a * t * t + data.b * t + data.c;
     }
-
 }
