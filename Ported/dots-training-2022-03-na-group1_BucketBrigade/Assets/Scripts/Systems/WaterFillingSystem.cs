92cfb8da9dc123ff98c0a3be963e82cd5520c7ac
UnityPix <doug.bruvall@unity3d.com>
2022-03-28 23:39:36 -0700

    Adding stub systems from spreadsheet.

---
+++ Ported/dots-training-2022-03-na-group1_BucketBrigade/Assets/Scripts/Systems/WaterFillingSystem.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+
+public partial class WaterFillingSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var time = Time.ElapsedTime;
+/*
+        Entities
+            .ForEach((ref Translation translation, in CarMovement movement) =>
+            {
+                translation.Value.x = (float) ((time + movement.Offset) % 100) - 50f;
+            }).ScheduleParallel();
+            */
+    }
+}
