f965ee48200db92ab447b0a3cab2bb3a03f5eca6
RonanTumelty <99658345+RonanTumelty@users.noreply.github.com>
2022-03-30 09:25:24 +0100

    Used GetComponent to access Train entity data

--- Ported/Metro/Assets/Scripts/Systems/CarriageMovementSystem.cs
+++ Ported/Metro/Assets/Scripts/Systems/CarriageMovementSystem.cs
@@ -8,12 +8,9 @@ public partial class CarriageMovementSystem : SystemBase
     
     protected override void OnUpdate()
     {
-        ComponentDataFromEntity<TrackPositionComponent> trackPositions =
-            GetComponentDataFromEntity<TrackPositionComponent>();
-        
         Entities.ForEach((ref CarriageComponent carriage, ref Translation translation) =>
         {
-            float trainPosition = trackPositions[carriage.Train].Value;
+            float trainPosition = GetComponent<TrackPositionComponent>(carriage.Train).Value;
             
             float3 start = new float3(0, 0, 0); // Replace with curve sampling
             float3 destination = new float3(50, 0, 0);
