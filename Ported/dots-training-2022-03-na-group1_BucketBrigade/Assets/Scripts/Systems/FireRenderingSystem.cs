620984b80891480dc27047fd19ed67070c9fee17
alexis-sotelo <alexis.sotelo@unity3d.com>
2022-03-30 13:59:04 -0500

    Added FireRenderingSystem.cs

---
+++ Ported/dots-training-2022-03-na-group1_BucketBrigade/Assets/Scripts/Systems/FireRenderingSystem.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+
+//Reads the Heatmap Buffer to Update all Flame entities visuals (Color and 'Scale')
+public partial class FireRenderingSystem : SystemBase
+{
+  static float flickerRate = 2f;
+  static float flickerRange = 0.1f;
+  protected override void OnUpdate()
+  {
+    var heatmapData = GetSingleton<HeatMapData>(); 
+    var heatmap = GetSingletonEntity<HeatMapTemperature>();
+    
+    float time = (float) Time.ElapsedTime;
+    float tileHalfHeight = heatmapData.maxTileHeight * 0.5f;
+        
+    var heatmapBufferReadonly = EntityManager.GetBuffer<HeatMapTemperature>(heatmap, true);
+
+    Entities.WithAll<FireIndex>()
+      .WithReadOnly(heatmapBufferReadonly)
+      .ForEach( (ref URPMaterialPropertyBaseColor colorComponent, ref Translation translation, in FireIndex fireIndex) =>
+      {
+        float intensity = heatmapBufferReadonly[fireIndex.index];
+
+        if (intensity < FirePropagationSystem.HEAT_THRESHOLD)
+        {
+          colorComponent.Value = heatmapData.colorNeutral;
+        }
+        else
+        {
+          colorComponent.Value = math.lerp(heatmapData.colorCool, heatmapData.colorHot, intensity);
+          translation.Value.y = math.lerp(0, tileHalfHeight, intensity);
+                     
+          //Apply flicker noise
+          float2 sample = new float2((time - fireIndex.index) * flickerRate - intensity, intensity);
+          translation.Value.y += flickerRange * 0.5f + (noise.cnoise(sample) * flickerRange);
+        }
+
+      })
+      .ScheduleParallel();
+  }
+}
