569c52edaf62d4996557dd87b9e0d3cebc3bb36d
Raymond Luong <raymond.luong@unity3d.com>
2022-04-01 07:23:44 -0700

    Drop initial resources (still no stacking)

--- Ported/CombatBees/Assets/Scripts/Systems/ResourceSpawnerSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/ResourceSpawnerSystem.cs
@@ -1,4 +1,3 @@
-using System.Security.Cryptography;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
@@ -21,14 +20,16 @@ public partial class ResourceSpawnerSystem : SystemBase
             var maxX = (int) Field.size.x/2 - (int) (Field.size.x - Field.startingResourceZoneWidth)/2;
             var minZ = (int) -Field.size.z/2;
             var maxZ = (int) Field.size.z/2;
-            var minY = (int) -Field.size.y/2 + 1;
+            var minY = (int) -Field.size.y/2;
+            var maxY = (int) Field.size.y/2;
 
             for (int i = 0; i < spawner.StartingResourceCount; ++i)
             {
                 var instance = ecb.Instantiate(spawner.ResourcePrefab);
                 var xCoord = random.NextInt(minX, maxX);
                 var zCoord = random.NextInt(minZ, maxZ);
-                var translation = new Translation { Value = new float3(xCoord, minY, zCoord)};
+                var yCoord = random.NextInt(minY, maxY);
+                var translation = new Translation { Value = new float3(xCoord, yCoord, zCoord)};
                 ecb.SetComponent(instance, translation);
 				ecb.SetComponent(instance, new VelocityComponent { Value = 0f });;
 		    }
