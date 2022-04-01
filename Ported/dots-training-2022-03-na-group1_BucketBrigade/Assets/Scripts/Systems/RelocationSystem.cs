cc21088611c0ab5a42930b83de1c806d4070ccbf
UnityPix <doug.bruvall@unity3d.com>
2022-04-01 12:22:53 -0700

    Cancel all relocations every second.

--- Ported/dots-training-2022-03-na-group1_BucketBrigade/Assets/Scripts/Systems/RelocationSystem.cs
+++ Ported/dots-training-2022-03-na-group1_BucketBrigade/Assets/Scripts/Systems/RelocationSystem.cs
@@ -6,38 +6,53 @@ using Unity.Mathematics;
 [UpdateBefore(typeof(FirePropagationSystem))]
 public partial class RelocationSystem : SystemBase
 {
+    public const int ResetAllRelocationCount = 60;
+    
     protected override void OnUpdate()
     {
         var deltaTime = (float)Time.DeltaTime;
 
-
-        Entities
-            .ForEach((ref Position movement, ref MyWorkerState state, in RelocatePosition destination, in Speed speed ) =>
-            {
-                if (state.Value == WorkerState.Repositioning)
+        if ((BucketBrigadeUtility.GetCurrentFrame() % ResetAllRelocationCount) == 0)
+        {
+            Entities
+                .ForEach((ref MyWorkerState state, ref RelocatePosition destination, in Position movement, in Speed speed ) =>
                 {
-                    var movementThisFrame = deltaTime * speed.Value;
-
-                    var remainingDisplacement = destination.Value - movement.Value;
-
-                    var remainingDistance = math.length(remainingDisplacement);
-
-                    if (movementThisFrame > remainingDistance)
+                    if (state.Value == WorkerState.Repositioning)
                     {
-                        movement.Value = destination.Value;
+                        destination.Value = movement.Value;
                         state.Value = WorkerState.Idle;
                     }
-                    else
-                    {
-                        var t = movementThisFrame / remainingDistance;
-                        movement.Value = movement.Value + new float2(remainingDisplacement.x * t, remainingDisplacement.y * t);
-                    }
-                }
-                else if (math.distancesq(movement.Value, destination.Value) > 0.01)
+                }).ScheduleParallel();
+        }
+        else
+        {
+            Entities
+                .ForEach((ref Position movement, ref MyWorkerState state, in RelocatePosition destination, in Speed speed ) =>
                 {
-                    state.Value = WorkerState.Repositioning;
-                }
-            }).ScheduleParallel();
-        
+                    if (state.Value == WorkerState.Repositioning)
+                    {
+                        var movementThisFrame = deltaTime * speed.Value;
+
+                        var remainingDisplacement = destination.Value - movement.Value;
+
+                        var remainingDistance = math.length(remainingDisplacement);
+
+                        if (movementThisFrame > remainingDistance)
+                        {
+                            movement.Value = destination.Value;
+                            state.Value = WorkerState.Idle;
+                        }
+                        else
+                        {
+                            var t = movementThisFrame / remainingDistance;
+                            movement.Value = movement.Value + new float2(remainingDisplacement.x * t, remainingDisplacement.y * t);
+                        }
+                    }
+                    else if (math.distancesq(movement.Value, destination.Value) > 0.01)
+                    {
+                        state.Value = WorkerState.Repositioning;
+                    }
+                }).ScheduleParallel();
+        }
     }
 }
