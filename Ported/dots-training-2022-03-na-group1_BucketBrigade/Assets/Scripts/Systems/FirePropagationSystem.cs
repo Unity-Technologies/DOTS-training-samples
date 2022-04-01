688c4614531422b685e4498abe243493dc3c816d
alexis-sotelo <alexis.sotelo@unity3d.com>
2022-03-31 19:24:42 -0500

    Changed HeatJobImplementation

--- Ported/dots-training-2022-03-na-group1_BucketBrigade/Assets/Scripts/Systems/FirePropagationSystem.cs
+++ Ported/dots-training-2022-03-na-group1_BucketBrigade/Assets/Scripts/Systems/FirePropagationSystem.cs
@@ -7,57 +7,92 @@ using Unity.Jobs;
 public partial class FirePropagationSystem : SystemBase
 {
     public const float HEAT_THRESHOLD = 0.2f;
-    
-    int firePropagationRadius = 2;
+    public const int firePropagationRadius = 2;
 
-    NativeArray<int2> checkAdjacents;
+    private NativeArray<float> heatIncrementBuffer;
+    private int bufferLength;
+    
     protected override void OnCreate()
     {
-        GridUtility.CreateAdjacentTileArray(ref checkAdjacents,firePropagationRadius);
-        
     }
     protected override void OnDestroy()
     {
-        checkAdjacents.Dispose();
+        heatIncrementBuffer.Dispose();
     }
 
     protected override void OnUpdate()
     {
-        var localCheckAdjacents = checkAdjacents;
-
         var heatmapData = GetSingleton<HeatMapData>();
         var heatmapBuffer = BucketBrigadeUtility.GetHeatmapBuffer(this);
-        
-        float deltaHeat = Time.DeltaTime * heatmapData.heatPropagationSpeed;
 
-        var job = new HeatJob()
+        if (bufferLength == 0)
+        {
+            bufferLength = heatmapData.mapSideLength * heatmapData.mapSideLength;
+            heatIncrementBuffer = new NativeArray<float>( bufferLength, Allocator.Persistent);
+        }
+
+        float deltaHeat = Time.DeltaTime * heatmapData.heatPropagationSpeed;
+        
+        var jobFirst = new HeatIncrementJob()
         {
             width = heatmapData.mapSideLength,
-            adjacentOffsets = localCheckAdjacents,
             deltaHeat = deltaHeat,
-            heatmapBuffer = heatmapBuffer
+            heatmapBuffer = heatmapBuffer,
+            heatIncrementArray = heatIncrementBuffer,
+        };
+        
+        var jobSecond = new HeatUpdateJob()
+        {
+            heatmapBuffer = heatmapBuffer,
+            heatIncrementArray = heatIncrementBuffer
         };
 
-        var handle = job.Schedule(heatmapBuffer.Length, 32);
+        var handle = jobFirst.Schedule(heatmapBuffer.Length, 32);
         handle.Complete();
-        
+
+        var secondHandle = jobSecond.Schedule(heatmapBuffer.Length, 32, handle);
+        secondHandle.Complete();
     }
     
     [BurstCompile]
-    struct HeatJob : IJobParallelFor
+    struct HeatIncrementJob : IJobParallelFor
     {
-        [ReadOnly] public NativeArray<int2> adjacentOffsets;
+        [ReadOnly] public DynamicBuffer<HeatMapTemperature> heatmapBuffer;
         
-        [NativeDisableParallelForRestriction]
-        public DynamicBuffer<HeatMapTemperature> heatmapBuffer;
+        public NativeArray<float> heatIncrementArray;
         
         public float deltaHeat;
         public int width;
+        
         public void Execute(int tileIndex)
         {
+            heatIncrementArray[tileIndex] = 0f;
+            int2 tileCoord = GridUtility.GetTileCoordinate(tileIndex, width);
+
+            for (int rowIndex = -firePropagationRadius; rowIndex <= firePropagationRadius; rowIndex++)
+            {
+                int currentRow = tileCoord.x - rowIndex;
+                if (currentRow >= 0 && currentRow < width)
+                {
+                    for (int columnIndex = -firePropagationRadius; columnIndex <= firePropagationRadius; columnIndex++)
+                    {
+                        int currentColumn = tileCoord.y + columnIndex;
+                        if (currentColumn >= 0 && currentColumn < width)
+                        {
+                            float _neighbourHeat = heatmapBuffer[(currentRow * width) + currentColumn];
+                            if (_neighbourHeat >= HEAT_THRESHOLD)
+                            {
+                                heatIncrementArray[tileIndex] += deltaHeat;
+                            }
+                        }
+                    }
+                }
+            }
+
+            /*
             if (heatmapBuffer[tileIndex] >= HEAT_THRESHOLD)
             {
-                heatmapBuffer[tileIndex] += deltaHeat;
+                heatIncrementArray[tileIndex] += deltaHeat;
 
                 //heat adjacents
                 for (int iAdjacent = 0; iAdjacent < adjacentOffsets.Length; iAdjacent++)
@@ -73,14 +108,30 @@ public partial class FirePropagationSystem : SystemBase
                         int adjacentIndex = GridUtility.GetTileIndex(x, z, width);
 
                         if (heatmapBuffer[adjacentIndex] < HEAT_THRESHOLD)
-                            heatmapBuffer[adjacentIndex] += deltaHeat;
+                        {
+                            heatIncrementArray[adjacentIndex] += deltaHeat;
+                        }
+                            
                     }
                 }
-            }
-
-            if (heatmapBuffer[tileIndex] >= 1f)
-                heatmapBuffer[tileIndex] = 1f;
+            }*/
         }
     }
 
+    [BurstCompile]
+    struct HeatUpdateJob : IJobParallelFor
+    {
+        [ReadOnly] public NativeArray<float> heatIncrementArray;
+        
+        [NativeDisableParallelForRestriction]
+        public DynamicBuffer<HeatMapTemperature> heatmapBuffer;
+
+        public void Execute(int tileIndex)
+        {
+            heatmapBuffer[tileIndex] += heatIncrementArray[tileIndex];
+            
+            if (heatmapBuffer[tileIndex] >= 1f)
+                heatmapBuffer[tileIndex] = 1f;
+        }   
+    }
 }
