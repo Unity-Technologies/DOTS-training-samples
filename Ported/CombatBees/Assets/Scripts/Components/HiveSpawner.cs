d443512ae4c77c60a6a736b7ff005845fb1d938d
Fabrice Lete <fabricel@unity3d.com>
2022-05-16 20:32:41 +0200

    removed CombatBees port that should have been in a branch and not on master

--- Ported/CombatBees/Assets/Scripts/Components/HiveSpawner.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Entities;
-
-public struct HiveSpawner : IComponentData
-{
-    public Entity BeePrefab;
-    public int BeesAmount;
-    public Entity ResourcePrefab;
-    public int ResourceAmount;
-}
