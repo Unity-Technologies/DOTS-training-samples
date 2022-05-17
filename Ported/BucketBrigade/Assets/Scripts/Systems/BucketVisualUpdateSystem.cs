53115672e8b1d2ae174095d630400d149f4825b5
Juan R. Marquez <juan.r.marquez.ortiz@unity3d.com>
2022-05-17 15:22:03 -0400

    BucketVisualUpdateSystem and BucketAspect refined

--- Ported/BucketBrigade/Assets/Scripts/Systems/BucketVisualUpdateSystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/Systems/BucketVisualUpdateSystem.cs
@@ -1,4 +1,32 @@
-public class BucketVisualUpdateSystem {
+
+
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Rendering;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+[BurstCompile]
+partial struct BucketVisualUpdateSystem : ISystem {
     //This system will handle reading the fill level and using that to
     //calculate the bucket's size and color
+
+    public void OnCreate(ref SystemState state) {
+        
+    }
+
+    public void OnDestroy(ref SystemState state) {
+        
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state) {
+        float scale = 1;
+        foreach (var bucket in SystemAPI.Query<BucketAspect>()) {
+            bucket.Scale = 0.5f + (bucket.FillLevel * 2);
+            bucket.Color = Color.Lerp(Color.cyan, Color.blue, bucket.FillLevel);
+            //Adjust this to use math.lerp, using the float4 representations of the colors.
+        }
+    }
 }
