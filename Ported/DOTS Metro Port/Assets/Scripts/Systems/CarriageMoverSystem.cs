c0aa36272f9e546a9f499eaae259e1e9f6b54e39
Lewis Jordan <lewis.jordan@hotmail.co.uk>
2022-05-17 14:29:01 +0100

    Finished carriage moved

--- Ported/DOTS Metro Port/Assets/Scripts/Systems/CarriageMoverSystem.cs
+++ Ported/DOTS Metro Port/Assets/Scripts/Systems/CarriageMoverSystem.cs
@@ -6,9 +6,11 @@ using Unity.Mathematics;
 [BurstCompile]
 partial struct CarriageMover : ISystem
 {
+    private ComponentDataFromEntity<DistanceAlongBezier> _componentFromEntity;
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<Config>();
+        _componentFromEntity = state.GetComponentDataFromEntity<DistanceAlongBezier>(false);
     }
 
     public void OnDestroy(ref SystemState state)
@@ -19,10 +21,11 @@ partial struct CarriageMover : ISystem
     public void OnUpdate(ref SystemState state)
     {
         Config config = SystemAPI.GetSingleton<Config>();
-
+        _componentFromEntity.Update(ref state);
         foreach (var (carriage, position) in SystemAPI.Query<RefRO<Carriage>, RefRW<DistanceAlongBezier>>())
         {
-            var train = SystemAPI.GetComponent<DistanceAlongBezier>(carriage.ValueRO.Train);
+            //var train = SystemAPI.GetComponent<DistanceAlongBezier>(carriage.ValueRO.Train);
+            var train = _componentFromEntity[carriage.ValueRO.Train];
             float carriageDistance = train.Distance - config.TrainOffset - (config.CarriageLength * carriage.ValueRO.CarriageIndex);
             position.ValueRW.Distance = carriageDistance;
 
