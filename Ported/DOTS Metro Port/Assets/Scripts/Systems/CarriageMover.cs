457ecc0d66be6871c2def1792d6bdfa9f2df6e4e
Jo Petty <jo.petty@unity3d.com>
2022-05-17 11:16:34 +0100

    Added carriage mover

---
+++ Ported/DOTS Metro Port/Assets/Scripts/Systems/CarriageMover.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Entities;
+using Unity.Burst;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+[BurstCompile]
+partial struct CarriageMover : ISystem
+{
+    private EntityQuery _carriageQuery;
+
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<Config>();
+
+        _carriageQuery = state.GetEntityQuery(typeof (Carriage));
+    }
+
+    public void OnDestroy(ref SystemState state)
+    {
+        
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        var entities = _carriageQuery.ToEntityArray(state.WorldUnmanaged.UpdateAllocator.ToAllocator);
+
+        foreach (var (carriage, transform) in SystemAPI.Query<RefRO<Carriage>, TransformAspect>())
+        {
+            
+        }
+    }
+}
+
