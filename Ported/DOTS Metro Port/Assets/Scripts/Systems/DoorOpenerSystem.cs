ec3c2f0257c697f7288654cd059991faa0295632
Max Stolze <90327462+MaxiStolze@users.noreply.github.com>
2022-05-18 15:21:01 +0200

    Update DoorOpenerSystem.cs

--- Ported/DOTS Metro Port/Assets/Scripts/Systems/DoorOpenerSystem.cs
+++ Ported/DOTS Metro Port/Assets/Scripts/Systems/DoorOpenerSystem.cs
@@ -26,10 +26,12 @@ public partial struct DoorOpenerSystem : ISystem
 	{
 		//var job = new DoorOpeningJob { ElapsedTime = state.Time.ElapsedTime };
 		//job.ScheduleParallel();
+
+		Config config = SystemAPI.GetSingleton<Config>();
 		m_trainFromEntity.Update(ref state);
 		m_CarriageFromEntity.Update(ref state);
 
-		var job = new ManageDoors { m_trainFromEntity = m_trainFromEntity, m_CarriageFromEntity = m_CarriageFromEntity, time = state.Time.ElapsedTime};
+		var job = new ManageDoors { m_trainFromEntity = m_trainFromEntity, m_CarriageFromEntity = m_CarriageFromEntity, time = state.Time.ElapsedTime, config = config};
 		job.ScheduleParallel();
 	}
 }
@@ -39,17 +41,38 @@ partial struct ManageDoors : IJobEntity
 	[ReadOnly] public ComponentDataFromEntity<Train> m_trainFromEntity;
 	[ReadOnly] public ComponentDataFromEntity<Carriage> m_CarriageFromEntity;
 	public double time;
-
+	public Config config;
+	
 	void Execute(in Door door, TransformAspect transform)
 	{
 		Train train = m_trainFromEntity[m_CarriageFromEntity[door.Carriage].Train];
 		var trainState = train.TrainState;
 
 		float3 desiredPosition = door.StartPosition;
+		
+		float timeToOpenDoor = 1f;
+
+		float startTime = train.DepartureTime - config.TrainWaitTime;
+		float endTime = train.DepartureTime;
+
+		float progress = 0;
+
+		if (time < startTime + timeToOpenDoor)
+		{
+			progress = ((float)time - startTime) / timeToOpenDoor;
+		}
+		else if (time > endTime - timeToOpenDoor)
+		{
+			progress = 1f - ((float)time - (endTime - timeToOpenDoor)) / timeToOpenDoor;
+		}
+		else 
+		{
+			progress = 1f;
+		}
 
 		if (trainState == TrainState.Stopped)
 		{
-			desiredPosition = math.lerp(door.StartPosition, door.EndPosition, 1f);
+			desiredPosition = math.lerp(door.StartPosition, door.EndPosition, progress);
 		}
 		else
 		{
