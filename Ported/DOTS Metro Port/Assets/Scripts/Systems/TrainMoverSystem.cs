9ff92b2ddfff851d001c00c08b81e885c2b13277
Jo Petty <jo.petty@unity3d.com>
2022-05-18 09:35:05 +0100

    Made trains slow down

--- Ported/DOTS Metro Port/Assets/Scripts/Systems/TrainMoverSystem.cs
+++ Ported/DOTS Metro Port/Assets/Scripts/Systems/TrainMoverSystem.cs
@@ -1,14 +1,18 @@
 using Unity.Entities;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Transforms;
 using Unity.Mathematics;
 
 [BurstCompile]
 partial struct TrainMoverSystem : ISystem
 {
+    private BufferFromEntity<Platform> _bufferFromEntity;
+
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<Config>();
+        _bufferFromEntity = state.GetBufferFromEntity<Platform>(true);
     }
 
     public void OnDestroy(ref SystemState state)
@@ -19,10 +23,42 @@ partial struct TrainMoverSystem : ISystem
     public void OnUpdate(ref SystemState state)
     {
         Config config = SystemAPI.GetSingleton<Config>();
-        foreach (var position in SystemAPI.Query<RefRW<DistanceAlongBezier>>().WithAll<Train>())
+        _bufferFromEntity.Update(ref state);
+
+        foreach (var train in SystemAPI.Query<RefRW<DistanceAlongBezier>>().WithAll<Train>())
         {
-            position.ValueRW.Distance += config.MaxTrainSpeed * state.Time.DeltaTime;
+            NativeArray<Platform> platforms = _bufferFromEntity[train.ValueRW.TrackEntity].AsNativeArray();
+
+            if (IsTrainOnPlatform(train, platforms, ref state))
+            {
+                train.ValueRW.Distance += config.MaxTrainSpeed/4 * state.Time.DeltaTime;
+
+            }
+
+            else
+            {
+                train.ValueRW.Distance += config.MaxTrainSpeed * state.Time.DeltaTime;
+
+            }
+
         }
+
+
     }
+
+    private bool IsTrainOnPlatform(RefRW<DistanceAlongBezier> train, NativeArray<Platform> platforms, ref SystemState state)
+    {
+        foreach (Platform platform in platforms)
+        {
+            if (train.ValueRW.Distance >= platform.startPoint && train.ValueRW.Distance <= platform.endPoint)
+            {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
 }
 
