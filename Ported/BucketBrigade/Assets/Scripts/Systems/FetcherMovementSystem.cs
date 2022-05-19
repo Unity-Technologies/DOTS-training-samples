5ef9caa959f6a1dd8f2df924380e385502ef6439
suyun.lin <suyun.lin@unity3d.com>
2022-05-19 13:15:33 -0400

    Description: Expose empty/full Fetcher speed to authoring

--- Ported/BucketBrigade/Assets/Scripts/Systems/FetcherMovementSystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/Systems/FetcherMovementSystem.cs
@@ -176,12 +176,12 @@ partial struct FetcherMovementSystem : ISystem
         if (fetcher.CurrentState == FetcherState.MoveTowardsBucket)
         {
             targetPos = m_TransformFromEntity[fetcher.TargetPickUp].Position;
-            movementSpeed = state.Time.DeltaTime * 3.5f;
+            movementSpeed = state.Time.DeltaTime * fetcher.SpeedEmpty;
         }
         else if (fetcher.CurrentState == FetcherState.MoveTowardsWater)
         {
             targetPos = m_TransformFromEntity[fetcher.TargetDropZone].Position;
-            movementSpeed = state.Time.DeltaTime * 3.0f;
+            movementSpeed = state.Time.DeltaTime * fetcher.SpeedFull;
         }
 
         float arriveThreshold = 0.5f;
