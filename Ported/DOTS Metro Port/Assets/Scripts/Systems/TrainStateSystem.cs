2f6afafa3a4fe9f8af1310efe265384791a5d768
Lewis Jordan <lewis.jordan@hotmail.co.uk>
2022-05-19 12:32:27 +0100

    Made train state management run in parallel

--- Ported/DOTS Metro Port/Assets/Scripts/Systems/TrainStateSystem.cs
+++ Ported/DOTS Metro Port/Assets/Scripts/Systems/TrainStateSystem.cs
@@ -14,71 +14,84 @@ public partial struct TrainStateSystem : ISystem
         _bufferFromEntity = state.GetBufferFromEntity<Platform>(true);
     }
 
-	public void OnDestroy(ref SystemState state)
-	{
-	}
+    public void OnDestroy(ref SystemState state)
+    {
+    }
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
         Config config = SystemAPI.GetSingleton<Config>();
         _bufferFromEntity.Update(ref state);
+        var job = new ManageTrainStateJob { BufferFromEntity = _bufferFromEntity, Config = config, ElapsedTime = (float)state.Time.ElapsedTime };
+        job.ScheduleParallel();
+    }
+}
 
-        foreach (var (trainPosition, train) in SystemAPI.Query<RefRO<DistanceAlongBezier>, RefRW<Train>>())
-		{
-            NativeArray<Platform> platforms = _bufferFromEntity[trainPosition.ValueRO.TrackEntity].AsNativeArray();
-            Platform? trainOnPlatform = IsTrainOnPlatform(trainPosition, platforms, config, ref state);
+[BurstCompile]
+public partial struct ManageTrainStateJob : IJobEntity
+{
+    [ReadOnly]
+    public BufferFromEntity<Platform> BufferFromEntity;
+    public Config Config;
+    public float ElapsedTime;
 
-            switch(train.ValueRO.TrainState)
-			{
-                case TrainState.Moving:
-				{
-                    if(trainOnPlatform.HasValue)
-					{
-                        train.ValueRW.TrainState = TrainState.Stopping;
-					}
-                    else
-					{
-                        train.ValueRW.SpeedPercentage = GetTrainSpeedOffPlatform(train.ValueRO, ref state);
-					}
-                    break;
-				}
-                case TrainState.Stopping:
-				{
-                    if(trainOnPlatform.HasValue)
-					{
-                        train.ValueRW.SpeedPercentage = GetTrainSpeedOnPlatform(
-                            GetPlatformEnterPoint(trainOnPlatform.Value, config),
-                            GetPlatformExitPoint(trainOnPlatform.Value, config),
-                            trainPosition.ValueRO.Distance);
-                    }
-                    else
-					{
-                        train.ValueRW.TrainState = TrainState.Stopped;
-                        train.ValueRW.SpeedPercentage = 0.0f;
-                        train.ValueRW.DepartureTime = (float)state.Time.ElapsedTime + config.TrainWaitTime;
-                    }
-                    break;
-				}
-                case TrainState.Stopped:
-				{
-                    if(train.ValueRO.DepartureTime < (float)state.Time.ElapsedTime)
-					{
-                        train.ValueRW.TrainState = TrainState.Moving;
-					}
-                    break;
-				}
-			}
-		}
+    [BurstCompile]
+    public void Execute(in DistanceAlongBezier trainPosition, ref Train train)
+    {
+        NativeArray<Platform> platforms = BufferFromEntity[trainPosition.TrackEntity].AsNativeArray();
+        Platform? trainOnPlatform = IsTrainOnPlatform(trainPosition, platforms, Config);
+        
+
+        switch (train.TrainState)
+        {
+            case TrainState.Moving:
+            {
+                if (trainOnPlatform.HasValue)
+                {
+                    train.TrainState = TrainState.Stopping;
+                }
+                else
+                {
+                    train.SpeedPercentage = GetTrainSpeedOffPlatform(train, ElapsedTime);
+                }
+                break;
+            }
+            case TrainState.Stopping:
+            {
+                if (trainOnPlatform.HasValue)
+                {
+                    train.SpeedPercentage = GetTrainSpeedOnPlatform(
+                        GetPlatformEnterPoint(trainOnPlatform.Value, Config),
+                        GetPlatformExitPoint(trainOnPlatform.Value, Config),
+                        trainPosition.Distance);
+                }
+                else
+                {
+                    train.TrainState = TrainState.Stopped;
+                    train.SpeedPercentage = 0.0f;
+                    train.DepartureTime = ElapsedTime + Config.TrainWaitTime;
+                }
+                break;
+            }
+            case TrainState.Stopped:
+            {
+                if (train.DepartureTime < ElapsedTime)
+                {
+                    train.TrainState = TrainState.Moving;
+                }
+                break;
+            }
+        }
     }
 
     [BurstCompile]
-    private Platform? IsTrainOnPlatform(RefRO<DistanceAlongBezier> trainPosition, NativeArray<Platform> platforms, Config config, ref SystemState state)
+    private Platform? IsTrainOnPlatform(DistanceAlongBezier trainPosition, NativeArray<Platform> platforms, Config config)
     {
         foreach (Platform platform in platforms)
         {
-            if (trainPosition.ValueRO.Distance >= GetPlatformEnterPoint(platform, config)
-                && trainPosition.ValueRO.Distance <= GetPlatformExitPoint(platform, config))
+            if (trainPosition.Distance >= GetPlatformEnterPoint(platform, config)
+                && trainPosition.Distance <= GetPlatformExitPoint(platform, config))
             {
                 return platform;
             }
@@ -87,25 +100,26 @@ public partial struct TrainStateSystem : ISystem
     }
 
     private float GetPlatformEnterPoint(Platform platform, Config config)
-	{
+    {
         return platform.startPoint - config.TrainOffset * 3;
     }
-    
+
     private float GetPlatformExitPoint(Platform platform, Config config)
-	{
+    {
         return platform.endPoint + config.TrainOffset;
     }
 
     private float GetTrainSpeedOnPlatform(float platformStart, float platformEnd, float trainPosition)
-	{
+    {
         float maxDistance = platformEnd + 5.0f - platformStart;
         float curDistance = platformEnd + 5.0f - trainPosition;
         return curDistance / maxDistance;
-	}
+    }
 
-    private float GetTrainSpeedOffPlatform(Train train, ref SystemState state)
-	{
-        float timeElapsed = (float)state.Time.ElapsedTime - train.DepartureTime;
+    private float GetTrainSpeedOffPlatform(Train train, float elapsedTime)
+    {
+        float timeElapsed = elapsedTime - train.DepartureTime;
         return math.clamp(timeElapsed / 3.0f, 0, 1);
-	}
+    }
 }
+
