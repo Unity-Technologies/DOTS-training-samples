09ebfb433eb251151de8431db4996edb792321b9
Tim Volossevich <tim.volossevich@unity3d.com>
2022-05-19 10:54:37 +0100

    Added bezier parameters to Config

--- Ported/Metro/Assets/ECS/Scripts/Authoring/ConfigAuthoring.cs
+++ Ported/Metro/Assets/ECS/Scripts/Authoring/ConfigAuthoring.cs
@@ -9,7 +9,16 @@ class ConfigAuthoring : MonoBehaviour
     public GameObject CommuterPefab;
     public GameObject RailPrefab;
     
+    [Header("Train data")]
     public int TrainsToSpawn;
+    
+    [Header("Bezier data")]
+    public float BEZIER_HANDLE_REACH = 0.1f;
+    public float BEZIER_PLATFORM_OFFSET = 3f;
+    public float PLATFORM_ADJACENCY_LIMIT = 12f;
+    public int BEZIER_MEASUREMENT_SUBDIVISIONS = 2;
+    public float PLATFORM_ARRIVAL_THRESHOLD = 0.975f;
+    public float RAIL_SPACING = 0.5f;
 }
 
 class ConfigBaker : Baker<ConfigAuthoring>
@@ -24,6 +33,13 @@ class ConfigBaker : Baker<ConfigAuthoring>
             RailPrefab = GetEntity(authoring.RailPrefab),
             
             TrainsToSpawn = authoring.TrainsToSpawn,
+            
+            BEZIER_HANDLE_REACH = authoring.BEZIER_HANDLE_REACH,
+            BEZIER_PLATFORM_OFFSET = authoring.BEZIER_PLATFORM_OFFSET,
+            PLATFORM_ADJACENCY_LIMIT = authoring.PLATFORM_ADJACENCY_LIMIT,
+            BEZIER_MEASUREMENT_SUBDIVISIONS = authoring.BEZIER_MEASUREMENT_SUBDIVISIONS,
+            PLATFORM_ARRIVAL_THRESHOLD = authoring.PLATFORM_ARRIVAL_THRESHOLD,
+            RAIL_SPACING = authoring.RAIL_SPACING
         });
     }
 }
--- Ported/Metro/Assets/ECS/Scripts/Components/Config.cs
+++ Ported/Metro/Assets/ECS/Scripts/Components/Config.cs
@@ -10,4 +10,12 @@ public struct Config : IComponentData
     
     // Trail data
     public int TrainsToSpawn;
+    
+    // Bezier data
+    public float BEZIER_HANDLE_REACH;
+    public float BEZIER_PLATFORM_OFFSET;
+    public float PLATFORM_ADJACENCY_LIMIT;
+    public int BEZIER_MEASUREMENT_SUBDIVISIONS;
+    public float PLATFORM_ARRIVAL_THRESHOLD;
+    public float RAIL_SPACING;
 }
