fe345367d3632cd49d7308272760dc44dc4f4c36
WCampos <16523949+wcampospro@users.noreply.github.com>
2022-07-25 15:58:38 -0700

    Added Config Component with Authoring

---
+++ Ported/CombatBeesPorted/Assets/Scripts/Authoring/ConfigAuthoring.cs
@@ -0,0 +1,42 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+class ConfigAuthoring : UnityEngine.MonoBehaviour
+{
+    public float BloodDuration;
+    public float ExplosionDuration;
+    public float HitDistance;
+    public float GrabDistance;
+    public float HiveDepth;
+    public float3 PlayVolume;
+    public int StartingBeeCount;
+    public int StartingResourceCount;
+    
+    public UnityEngine.GameObject BeePrefab;
+    public UnityEngine.GameObject ResourcePrefab;
+    public UnityEngine.GameObject ExplosionPrefab;
+    public UnityEngine.GameObject BloodPrefab;
+}
+
+class ConfigBaker : Baker<ConfigAuthoring>
+{
+    public override void Bake(ConfigAuthoring authoring)
+    {
+        AddComponent(new ConfigComponent()
+        {
+            BloodDuration = authoring.BloodDuration,
+            ExplosionDuration = authoring.ExplosionDuration,
+            HitDistance = authoring.HitDistance,
+            GrabDistance = authoring.GrabDistance,
+            HiveDepth = authoring.HiveDepth,
+            PlayVolume = authoring.PlayVolume,
+            StartingBeeCount = authoring.StartingBeeCount,
+            StartingResourceCount = authoring.StartingResourceCount,
+            
+            BeePrefab = GetEntity(authoring.BeePrefab),
+            ResourcePrefab = GetEntity(authoring.ResourcePrefab),
+            ExplosionPrefab = GetEntity(authoring.ExplosionPrefab),
+            BloodPrefab = GetEntity(authoring.BloodPrefab)
+        });
+    }
+}
---
+++ Ported/CombatBeesPorted/Assets/Scripts/Components/ConfigComponent.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct ConfigComponent : IComponentData
+{
+    public float BloodDuration;
+    public float ExplosionDuration;
+    public float HitDistance;
+    public float GrabDistance;
+    public float HiveDepth;
+    public float3 PlayVolume;
+    public int StartingBeeCount;
+    public int StartingResourceCount;
+    
+    public Entity BeePrefab;
+    public Entity ResourcePrefab;
+    public Entity ExplosionPrefab;
+    public Entity BloodPrefab;
+}
