6985b2273b5cce746ea1e54debf1d4d2d7509f02
SkipPetty <skip.petty@unity3d.com>
2022-07-25 15:57:43 -0700

    Adding TargetObject and ResourceBelow components

---
+++ Ported/CombatBeesPorted/Assets/Scripts/Components/ResourceBelow.cs
@@ -0,0 +1,6 @@
+using Unity.Entities;
+
+struct ResourceBelow : IComponentData
+{
+    public Entity Value;
+}
---
+++ Ported/CombatBeesPorted/Assets/Scripts/Components/TargetObject.cs
@@ -0,0 +1,6 @@
+using Unity.Entities;
+
+struct TargetObject : IComponentData
+{
+    public Entity Value;
+}
