540fe3cd733355bc3c9dbad72d2e518ce06e508e
WCampos <16523949+wcampospro@users.noreply.github.com>
2022-07-25 15:58:31 -0700

    Added Velocity Component with Authoring

---
+++ Ported/CombatBeesPorted/Assets/Scripts/Authoring/VelocityAuthoring.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+
+class VelocityAuthoring : UnityEngine.MonoBehaviour
+{
+    public float Value;
+}
+
+class VelocityBaker : Baker<VelocityAuthoring>
+{
+    public override void Bake(VelocityAuthoring authoring)
+    {
+        AddComponent(new VelocityComponent
+        {
+            Value = authoring.Value
+        });
+    }
+    
+}
---
+++ Ported/CombatBeesPorted/Assets/Scripts/Components/VelocityComponent.cs
@@ -0,0 +1,7 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+struct VelocityComponent : IComponentData
+{
+    public float3 Value;
+}
