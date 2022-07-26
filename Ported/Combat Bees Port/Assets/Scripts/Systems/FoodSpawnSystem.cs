f78643b1f140354244fce041260a727539c823d0
Jokubask <jokubask@unity3d.com>
2022-07-26 15:05:04 +0300

    food spawn system

---
+++ Ported/Combat Bees Port/Assets/Scripts/Systems/FoodSpawnSystem.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Components;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.VisualScripting;
+using UnityEngine;
+
+partial struct FoodSpawnSystem : ISystem
+{
+    private RaycastHit raycasthit;
+
+    public void OnCreate(ref SystemState state)
+    {
+
+    }
+
+    public void OnDestroy(ref SystemState state)
+    {
+
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        var camera = CameraSingleton.Instance;
+        var ray = camera.ScreenPointToRay(Input.mousePosition);
+        var test = ray.GetPoint(10); 
+        Debug.Log(test);
+
+        var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            var foodSpawnJob = new FoodSpawn
+            {
+                ECB = ecb,
+                direction = test
+            };
+            foodSpawnJob.Schedule();
+        }
+
+    }
+}
+
+partial struct FoodSpawn : IJobEntity
+{
+    public EntityCommandBuffer ECB;
+    public float3 direction;
+
+    private void Execute(in InitialSpawn prefab)
+    {
+        var instance = ECB.Instantiate(prefab.foodPrefab);
+        ECB.SetComponent(instance, new Translation{Value = direction });
+    }
+}
