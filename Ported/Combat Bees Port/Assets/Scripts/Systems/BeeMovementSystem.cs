2049e4a303a26828ae8cb0bb6d332b63d6108725
benassteponavicius <benas.steponavicius@unity3d.com>
2022-07-26 13:37:34 +0300

    Almost fixed the BeeMovementSystem.cs

--- Ported/Combat Bees Port/Assets/Scripts/Systems/BeeMovementSystem.cs
+++ Ported/Combat Bees Port/Assets/Scripts/Systems/BeeMovementSystem.cs
@@ -4,6 +4,7 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
+using Random = Unity.Mathematics.Random;
 
 partial class BeeMovementSystem : SystemBase
 {
@@ -12,23 +13,27 @@ partial class BeeMovementSystem : SystemBase
         var dt = Time.DeltaTime;
         var et = (float)Time.ElapsedTime;
         var perlinOffset = 3f;
-        var speed = 5f;
+        var speed = 0.005f;
+        Random random = new Random(1234);
         
+        //Entities.ForEach((ref Translation translation, ref Rotation rotation, in LocalToWorld transform) =>
                                                                         // CHANGE TO BEE ASPECT
-        Entities.ForEach((ref Translation translation, ref Rotation rotation, in LocalToWorld transform) =>
+        Entities
+            .WithAll<Bee>()
+            .ForEach((ref Translation translation, ref Rotation rotation, in LocalToWorld transform) =>
             {
-                var direction = math.normalizesafe(new float3(30, 0, 0) - translation.Value);
-                rotation.Value = quaternion.LookRotationSafe(direction, new float3(0,1,0));
-                var position = transform.Position;
+                var direction = math.normalizesafe(
+                    new float3(random.NextFloat(-40,40), random.NextFloat(-10,10), random.NextFloat(-10,10)) - translation.Value);
                 
-                //position += direction * speed * dt;
+                rotation.Value = quaternion.LookRotationSafe(direction, new float3(0,1,0));
+               // var position = transform.Position;
 
                 var offset = new float3(
                     noise.cnoise(new float2(et, perlinOffset)) - 0.5f,
                     noise.cnoise(new float2(et, perlinOffset)) - 0.5f,
                     noise.cnoise(new float2(et, perlinOffset)) - 0.5f
                 );
-                translation.Value = position * direction * speed * dt + offset;
+                translation.Value += direction + offset * speed;
 
             }).ScheduleParallel();
     }
