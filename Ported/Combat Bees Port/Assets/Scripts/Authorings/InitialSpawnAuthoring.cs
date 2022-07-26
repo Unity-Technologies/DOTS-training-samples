87e0a5140ab76e335df991c9b29525b96b516ab2
ManfredasŠiurkus <manfredas.surkus@unity3d.com>
2022-07-26 09:59:55 +0300

    adds spawning system

--- Ported/Combat Bees Port/Assets/Scripts/Authorings/InitialSpawnAuthoring.cs
+++ Ported/Combat Bees Port/Assets/Scripts/Authorings/InitialSpawnAuthoring.cs
@@ -1,17 +1,39 @@
 ﻿using Components;
 using Unity.Entities;
+using Unity.Mathematics;
 
 namespace Authorings
 {
     class InitialSpawnAuthoring : UnityEngine.MonoBehaviour
     {
+        public Entity yellowBeePrefab;
+        public Entity blueBeePrefab;
+        public Entity foodPrefab;
+
+
+        public int beeCount;
+        public int foodCount;
+
+        public float3 yellowBase;
+        public float3 blueBase;
+        public float3 mapCenter;
     }
 
     class InitialSpawnBaker : Baker<InitialSpawnAuthoring>
     {
         public override void Bake(InitialSpawnAuthoring authoring)
         {
-            AddComponent<InitialSpawn>();
+            AddComponent(new InitialSpawn
+            {
+                yellowBeePrefab = authoring.yellowBeePrefab,
+                blueBeePrefab = authoring.blueBeePrefab,
+                foodPrefab = authoring.foodPrefab,
+                beeCount = authoring.beeCount,
+                foodCount = authoring.foodCount,
+                yellowBase = authoring.yellowBase,
+                blueBase = authoring.blueBase,
+                mapCenter = authoring.mapCenter
+            });
         }
     }
 }
