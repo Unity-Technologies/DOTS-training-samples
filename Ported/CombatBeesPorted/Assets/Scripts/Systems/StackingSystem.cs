bec79d93b3a2f517146ee231d32a5e6cc707551d
Isaac Seah <isaac.seah@unity3d.com>
2022-07-26 16:50:12 -0400

    stackingSystem, objects don't fall infinitely

---
+++ Ported/CombatBeesPorted/Assets/Scripts/Systems/StackingSystem.cs
@@ -0,0 +1,45 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+[WithAny(typeof(ResourceStateGrabbable), typeof(ResourceStateStacked), typeof(ResourceStateGrabbed))]
+[WithAll(typeof(Gravity))]
+[BurstCompile]
+partial struct StackingJob : IJobEntity
+{
+    public float PlayVolumeFloor;
+    public EntityCommandBuffer Ecb;
+    void Execute(in Entity entity, ref Velocity vel, in Translation trans)
+    {
+        Ecb.SetComponentEnabled<ResourceStateGrabbable>(entity, false);
+        if (trans.Value.y < PlayVolumeFloor)
+        {
+            Ecb.SetComponentEnabled<Gravity>(entity, false);
+            vel.Value.y = 0;
+            
+        }
+    }
+}
+[BurstCompile]
+public partial struct StackingSystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+    }
+
+    public void OnDestroy(ref SystemState state)
+    {
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        var playVolumeFloor = SystemAPI.GetSingleton<Config>().PlayVolume.xy.y;
+        var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+    // TODO: Parallel schedule please. 
+        new StackingJob
+            {
+                PlayVolumeFloor = playVolumeFloor, 
+                Ecb = ecb
+            }.Schedule();
+    }
+} 
