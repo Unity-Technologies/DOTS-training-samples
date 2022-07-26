3c82fbabc3eda55d3c698e2cb305ceccca4df29c
Augustinas <augustinas.zigunis@unity3d.com>
2022-07-26 17:43:29 +0300

    Renaming stuff

--- Ported/AutoFarmers/Assets/FarmerDev/FarmerPrefabCreator.cs
+++ Ported/AutoFarmers/Assets/FarmerDev/FarmerPrefabCreator.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using Unity.Entities;
 using UnityEngine;
 
-public class PrefabCreator : MonoBehaviour
+public class FarmerPrefabCreator : MonoBehaviour
 {
     public GameObject m_prefab;
     // Start is called before the first frame update
 }
 
-public class PrefabCreatorBaker : Baker<PrefabCreator>
+public class PrefabCreatorBaker : Baker<FarmerPrefabCreator>
 {
-    public override void Bake(PrefabCreator authoring)
+    public override void Bake(FarmerPrefabCreator authoring)
     {
         AddComponent(new Ecsprefabcreator
         {
