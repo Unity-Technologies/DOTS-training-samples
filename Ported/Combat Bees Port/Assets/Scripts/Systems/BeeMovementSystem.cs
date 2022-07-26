259e934a489881d7df1e7fb18263201ba9612ae7
Jokubask <jokubask@unity3d.com>
2022-07-26 13:08:24 +0300

    sir pls fix

--- Ported/Combat Bees Port/Assets/Scripts/Systems/BeeMovementSystem.cs
+++ Ported/Combat Bees Port/Assets/Scripts/Systems/BeeMovementSystem.cs
@@ -15,36 +15,21 @@ partial class BeeMovementSystem : SystemBase
         var speed = 5f;
         
                                                                         // CHANGE TO BEE ASPECT
-        Entities.WithAll<BlueTeam>().ForEach((ref Translation translation, ref Rotation rotation, ref Bee bee, in LocalToWorld transform) =>
+        Entities.ForEach((ref Translation translation, ref Rotation rotation, in LocalToWorld transform) =>
             {
                 var direction = math.normalizesafe(new float3(30, 0, 0) - translation.Value);
                 rotation.Value = quaternion.LookRotationSafe(direction, new float3(0,1,0));
                 var position = transform.Position;
                 
-                bee.position += direction * speed * dt;
+                //position += direction * speed * dt;
 
                 var offset = new float3(
                     noise.cnoise(new float2(et, perlinOffset)) - 0.5f,
                     noise.cnoise(new float2(et, perlinOffset)) - 0.5f,
                     noise.cnoise(new float2(et, perlinOffset)) - 0.5f
                 );
-                translation.Value = bee.position + offset;
+                translation.Value = position * direction * speed * dt + offset;
 
             }).ScheduleParallel();
-        
-        Entities.WithAll<YellowTeam>().ForEach((ref Translation translation, ref Rotation rotation, ref Bee bee) =>
-        {
-            var direction = math.normalizesafe(new float3(-30, 0, 0) - translation.Value);
-            rotation.Value = quaternion.LookRotationSafe(direction, new float3(0,1,0));
-            bee.position += direction * speed * dt;
-
-            var offset = new float3(
-                noise.cnoise(new float2(et, perlinOffset)) - 0.5f,
-                noise.cnoise(new float2(et, perlinOffset)) - 0.5f,
-                noise.cnoise(new float2(et, perlinOffset)) - 0.5f
-            );
-            translation.Value = bee.position + offset;
-
-        }).ScheduleParallel();
     }
 }
