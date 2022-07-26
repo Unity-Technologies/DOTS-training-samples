44b4e9497ae9c9beb2cbfe1316919a85a40dffad
benassteponavicius <benas.steponavicius@unity3d.com>
2022-07-26 14:05:40 +0300

    Added collission to BeeMovementSystem.cs

--- Ported/Combat Bees Port/Assets/Scripts/Systems/BeeMovementSystem.cs
+++ Ported/Combat Bees Port/Assets/Scripts/Systems/BeeMovementSystem.cs
@@ -10,16 +10,14 @@ partial class BeeMovementSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        var dt = Time.DeltaTime;
-        var et = (float)Time.ElapsedTime;
-        var perlinOffset = 3f;
-        var speed = 2.5f;
-        Random random = new Random(1234);
-        float3 target = new float3(5, 0, 0);
+            var dt = Time.DeltaTime;
+            var et = (float)Time.ElapsedTime;
+            var perlinOffset = 3f;
+            var speed = 2.5f;
+            Random random = new Random(1234);
+            float3 target = new float3(5, 0, 0);
         
-        //Entities.ForEach((ref Translation translation, ref Rotation rotation, in LocalToWorld transform) =>
-                                                                        // CHANGE TO BEE ASPECT
-        Entities
+            Entities
             .WithAll<Bee>()
             .ForEach((ref Translation translation, ref Rotation rotation, in LocalToWorld transform) =>
             {
@@ -34,10 +32,24 @@ partial class BeeMovementSystem : SystemBase
                 
                 rotation.Value = quaternion.LookRotationSafe(direction, new float3(0,1,0));
                 position +=  (offset +direction) * speed * dt;
+                CheckBounds(ref position);
 
                 
-               translation.Value = position;
+                translation.Value = position;
 
             }).ScheduleParallel();
+            
+            void CheckBounds(ref float3 position)
+            {
+                if (position.x < -49) position.x = -49;
+                if (position.x > 49) position.x = 49;
+                if (position.y < -9) position.y = -9;
+                if (position.y > 9) position.y = 9;
+                if (position.z < -9) position.z = -9;
+                if (position.z > 9) position.z = 9;
+            }
     }
+
+    
+    
 }
