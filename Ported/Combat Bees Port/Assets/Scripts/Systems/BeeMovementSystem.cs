6ce48f7d6a56acef6becf2598e0cc04d020d5cfb
benassteponavicius <benas.steponavicius@unity3d.com>
2022-07-26 12:23:17 +0300

    Updated BeeMovementSystem.cs

--- Ported/Combat Bees Port/Assets/Scripts/Systems/BeeMovementSystem.cs
+++ Ported/Combat Bees Port/Assets/Scripts/Systems/BeeMovementSystem.cs
@@ -12,20 +12,21 @@ partial class BeeMovementSystem : SystemBase
         var dt = Time.DeltaTime;
         var et = (float)Time.ElapsedTime;
         var perlinOffset = 3f;
+        var speed = 5f;
         
                                                                         // CHANGE TO BEE ASPECT
-        Entities
-            .WithAll<Bee>()
-            .ForEach((ref Translation translation, ref Rotation rotation, ref Bee bee) =>
+        Entities.ForEach((ref Translation translation, ref Rotation rotation, ref Bee bee) =>
             {
-                var direction = math.normalizesafe(new float3(0, 0, 0) - translation.Value);
+                var direction = math.normalizesafe(new float3(30, 0, 0) - translation.Value);
                 rotation.Value = quaternion.LookRotationSafe(direction, new float3(0,1,0));
+                bee.position += direction * speed * dt;
 
                 var offset = new float3(
                     noise.cnoise(new float2(et, perlinOffset)) - 0.5f,
                     noise.cnoise(new float2(et, perlinOffset)) - 0.5f,
                     noise.cnoise(new float2(et, perlinOffset)) - 0.5f
                 );
+                translation.Value = bee.position + offset;
 
             }).Run();
     }
