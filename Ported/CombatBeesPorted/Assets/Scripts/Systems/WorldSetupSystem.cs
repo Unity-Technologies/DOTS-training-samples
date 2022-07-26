23fde5956b77705d14e5c85934fb9d8b6eec15f6
SkipPetty <skip.petty@unity3d.com>
2022-07-26 13:58:08 -0700

    Updated Scene name to BeeBattleRoyale
    Updated custom shaders to be supported in a URP DOTS context
    
    Created Bee, Resource, and Particle prefabs
    
    Updated WorldSetupSystem to update resource colors on startup

--- Ported/CombatBeesPorted/Assets/Scripts/Systems/WorldSetupSystem.cs
+++ Ported/CombatBeesPorted/Assets/Scripts/Systems/WorldSetupSystem.cs
@@ -66,11 +66,12 @@ partial struct WorldSetupSystem : ISystem
         Vector2Int gridCounts = Vector2Int.RoundToInt(new Vector2(field.x, field.z) / resourceSize);
         Vector2 gridSize = new Vector2(field.x/gridCounts.x,field.z/gridCounts.y);
         Vector2 minGridPos = new Vector2((gridCounts.x-1f)*-.5f*gridSize.x,(gridCounts.y-1f)*-.5f*gridSize.y);
-
+        var resourceColor = new URPMaterialPropertyBaseColor { Value = (UnityEngine.Vector4)new Color(0.1572f, 0.4191f, 0.0739f, 1.0f) };
         var resources = CollectionHelper.CreateNativeArray<Entity>(config.StartingResourceCount, Allocator.Temp);
         ecb.Instantiate(config.ResourcePrefab, resources);
         foreach (var resource in resources)
         {
+            ecb.SetComponentForLinkedEntityGroup(resource, queryColorMask, resourceColor);
             Vector3 startPos = new Vector3(minGridPos.x * .25f + UnityEngine.Random.value * field.x * .25f, UnityEngine.Random.value * 10f,
                 minGridPos.y + UnityEngine.Random.value * field.z);
             ecb.SetComponentForLinkedEntityGroup(resource, queryTranslationMask, new Translation { Value = startPos});
