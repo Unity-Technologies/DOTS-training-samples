d7dd13e34cd06a172ecb22125e1fa03ef5117fef
Jokubask <jokubask@unity3d.com>
2022-07-26 12:32:18 +0300

    separated bees

--- Ported/Combat Bees Port/Assets/Scripts/Systems/BeeMovementSystem.cs
+++ Ported/Combat Bees Port/Assets/Scripts/Systems/BeeMovementSystem.cs
@@ -15,7 +15,7 @@ partial class BeeMovementSystem : SystemBase
         var speed = 5f;
         
                                                                         // CHANGE TO BEE ASPECT
-        Entities.ForEach((ref Translation translation, ref Rotation rotation, ref Bee bee) =>
+        Entities.WithAll<BlueTeam>().ForEach((ref Translation translation, ref Rotation rotation, ref Bee bee) =>
             {
                 var direction = math.normalizesafe(new float3(30, 0, 0) - translation.Value);
                 rotation.Value = quaternion.LookRotationSafe(direction, new float3(0,1,0));
@@ -28,6 +28,21 @@ partial class BeeMovementSystem : SystemBase
                 );
                 translation.Value = bee.position + offset;
 
-            }).Run();
+            }).ScheduleParallel();
+        
+        Entities.WithAll<YellowTeam>().ForEach((ref Translation translation, ref Rotation rotation, ref Bee bee) =>
+        {
+            var direction = math.normalizesafe(new float3(-30, 0, 0) - translation.Value);
+            rotation.Value = quaternion.LookRotationSafe(direction, new float3(0,1,0));
+            bee.position += direction * speed * dt;
+
+            var offset = new float3(
+                noise.cnoise(new float2(et, perlinOffset)) - 0.5f,
+                noise.cnoise(new float2(et, perlinOffset)) - 0.5f,
+                noise.cnoise(new float2(et, perlinOffset)) - 0.5f
+            );
+            translation.Value = bee.position + offset;
+
+        }).ScheduleParallel();
     }
 }
