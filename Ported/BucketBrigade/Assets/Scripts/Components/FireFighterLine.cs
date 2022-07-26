908f792c2ed372394f11e0079e6ae9ba8152a768
Sebastien Dalgo <sebastien.dalgo@unity3d.com>
2022-07-26 14:15:04 -0400

    Adding Components for FireFighterLine

---
+++ Ported/BucketBrigade/Assets/Scripts/Components/FireFighterLine.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+// Just like we did with the turret, we create a tag component to identify the tank (cube).
+struct FireFighterLine : IComponentData
+{
+    public int LineId;
+    public float2 StartPosition;
+    public float2 EndPosition;
+    
+}
