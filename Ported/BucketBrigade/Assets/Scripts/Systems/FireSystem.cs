bf3ac400b6a49475f629fb2baba2c28a74ac1c19
Mélody Roy <melody.roy@unity3d.com>
2022-07-27 09:27:20 -0400

    add subscene

--- Ported/BucketBrigade/Assets/Scripts/Systems/FireSystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/Systems/FireSystem.cs
@@ -3,10 +3,12 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 using System.Numerics;
+using UnityEngine;
 
 [BurstCompile]
 partial struct FireSystem : ISystem
 {
+
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
@@ -21,8 +23,55 @@ partial struct FireSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        DynamicBuffer<Temperature> buff = SystemAPI.GetSingletonBuffer<Temperature>();
+        DynamicBuffer<Temperature> HeatMap = SystemAPI.GetSingletonBuffer<Temperature>();
+        Config Config = SystemAPI.GetSingleton<Config>();
 
+        int heatRadius = 1;
+        float heatTransferRate = 0.7f;
+        float fireSimUpdateRate = 0.5f;
+
+        float threshold = Config.FireThreshold; //0.5f
+        int gridSize = Config.GridSize; //20
         
+        
+
+        for (int cellIndex =0; cellIndex < HeatMap.Length; cellIndex++)
+        {
+            float tempChange = 0f;
+            float currentTemperature = HeatMap.ElementAt(cellIndex).Value; // currentCell
+
+            int cellRowIndex = Mathf.FloorToInt(cellIndex / gridSize);
+            int cellColumnIndex = cellIndex % gridSize;
+
+            for (int rowIndex = -heatRadius; rowIndex <= heatRadius; rowIndex++)
+            {
+                int currentRow = cellRowIndex - rowIndex;
+                if (currentRow >= 0 && currentRow < gridSize)
+                {
+                    for (int columnIndex = -heatRadius; columnIndex <= heatRadius; columnIndex++)
+                    {
+                        int currentColumn = cellColumnIndex + columnIndex;
+                        if (currentColumn >= 0 && currentColumn < gridSize)
+                        {
+                            float neighbourTemperature = HeatMap.ElementAt((currentRow * gridSize) + currentColumn).Value;
+                            if (neighbourTemperature >= threshold) // neighbour in fire
+                            {
+                                tempChange += neighbourTemperature * heatTransferRate;
+                            }
+                        }
+                    }
+                }
+            }
+
+            float newTemperature = Mathf.Clamp(currentTemperature + tempChange, -1f, 1f);
+            HeatMap.ElementAt(cellIndex).Value = newTemperature;
+
+            if (newTemperature >= threshold) Burn();
+        } 
+    }
+
+    public void Burn()
+    {
+        Debug.Log("Burn!");
     }
 }
