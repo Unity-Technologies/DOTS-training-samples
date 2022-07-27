0108f1579fd3340c61e35f30f19e0c3978463762
Joe Fazzino <joe.fazzino@unity3d.com>
2022-07-27 16:08:15 +0100

    Fix brakes

--- Ported/MagneticRoads/Assets/Scripts/Systems/CarSpeedSystem.cs
+++ Ported/MagneticRoads/Assets/Scripts/Systems/CarSpeedSystem.cs
@@ -1,5 +1,6 @@
 using Components;
 using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
@@ -26,8 +27,7 @@ namespace Systems
         {
             m_CarDataFromEntity.Update(ref state);
 
-            var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
-            var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
 
             // This can probably become a parrelel job
             foreach (var buffer in SystemAPI.Query<DynamicBuffer<CarDynamicBuffer>>())
@@ -39,7 +39,7 @@ namespace Systems
                 {
                     var xPositionOnSpline = m_CarDataFromEntity[entities[x]].T;
                     var xCarComponent = m_CarDataFromEntity[entities[x]];
-                    
+
                     for (int y = 0; y < entities.Length; y++)
                     {
                         if (x == y)
@@ -59,11 +59,13 @@ namespace Systems
                             xCarComponent.Speed = 10f;
                         }
                     }
-                    
-                    // Debug.Log(xCarComponent.T);
-                    // ecb.SetComponent(entities[x], xCarComponent);
+
+                    ecb.SetComponent(entities[x], xCarComponent);
                 }
             }
+
+            ecb.Playback(state.EntityManager);
+            ecb.Dispose();
         }
     }
 }
