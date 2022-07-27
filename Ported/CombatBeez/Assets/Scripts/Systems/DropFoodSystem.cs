4e55c17bf0198036ef87f830fd628b415defdd4e
Christian Deiss <christian.deiss@unity3d.com>
2022-07-27 15:39:14 -0700

    feat: added spawn for new bees when food is dropped in hive

--- Ported/CombatBeez/Assets/Scripts/Systems/DropFoodSystem.cs
+++ Ported/CombatBeez/Assets/Scripts/Systems/DropFoodSystem.cs
@@ -1,12 +1,16 @@
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+
+[BurstCompile]
 partial struct DropFoodSystem : ISystem
 {
     Random rand;
     float timeSinceLastDrop;
 
+    [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<Config>();
@@ -15,13 +19,18 @@ partial struct DropFoodSystem : ISystem
         timeSinceLastDrop = 0.0f;
     }
 
+    [BurstCompile]
     public void OnDestroy(ref SystemState state)
     {
     }
 
+    [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
         var config = SystemAPI.GetSingleton<Config>();
+
+        // Create an EntityCommandBuffer.
+        var ecb = new EntityCommandBuffer(state.WorldUpdateAllocator);
 
         timeSinceLastDrop += state.Time.DeltaTime;
 
@@ -33,7 +42,7 @@ partial struct DropFoodSystem : ISystem
 
         // Component type FoodResource is implicitly defined by the query so no WithAll is needed.
         // WithAll puts ADDITIONAL requirements on the query.
-        foreach (var (foodResource, transform) in SystemAPI.Query<RefRW<FoodResource>, TransformAspect>())
+        foreach (var (entity, foodResource, transform) in SystemAPI.Query<Entity, RefRW<FoodResource>, TransformAspect>())
         {
             switch (foodResource.ValueRW.State)
             {
@@ -46,10 +55,19 @@ partial struct DropFoodSystem : ISystem
                 case FoodState.SETTLED:
                     MarkFoodSettleInHiveAsDelivered(ref state, foodResource, transform);
                     break;
+                case FoodState.DELIVERED:
+                    SpawnNewBeesWhereFoodIsDelivered(ref state, ref config, foodResource, transform);
+                    ecb.DestroyEntity(entity);
+                    break;
             }
         }
+
+        // Playback and dispose the EntityCommandBuffer.
+        ecb.Playback(state.EntityManager);
+        ecb.Dispose();
     }
 
+    [BurstCompile]
     public void RandomlyPlaceDroppedFoodInPlayArea(ref SystemState state, RefRW<FoodResource> foodResource, TransformAspect transform)
     {
         var position = new float3(rand.NextInt(-40, 41), rand.NextInt(7, 13), rand.NextInt(-15, 16));
@@ -58,6 +76,7 @@ partial struct DropFoodSystem : ISystem
         foodResource.ValueRW.State = FoodState.FALLING;
     }
 
+    [BurstCompile]
     public void UpdatePositionOfFallingFood(ref SystemState state, ref Config config, RefRW<FoodResource> foodResource, TransformAspect transform)
     {
         var position = new float3(transform.Position.x, transform.Position.y - config.FallingSpeed, transform.Position.z);
@@ -69,6 +88,7 @@ partial struct DropFoodSystem : ISystem
         transform.Position = position;
     }
 
+    [BurstCompile]
     public void MarkFoodSettleInHiveAsDelivered(ref SystemState state, RefRW<FoodResource> foodResource, TransformAspect transform)
     {
         // if food position is in either hive (x and z)
@@ -77,6 +97,42 @@ partial struct DropFoodSystem : ISystem
         {
             // mark it as delivered
             foodResource.ValueRW.State = FoodState.DELIVERED;
+        }
+    }
+
+    [BurstCompile]
+    public void SpawnNewBeesWhereFoodIsDelivered(ref SystemState state, ref Config config, RefRW<FoodResource> foodResource, TransformAspect foodTransform)
+    {
+        // check which hive the food is in
+        Team beeTeam = Team.BLUE;
+        Entity beePrefab = config.BlueBeePrefab;
+        if (40 <= foodTransform.Position.x && foodTransform.Position.x <= 50)
+        {
+            beeTeam = Team.YELLOW;
+            beePrefab = config.YellowBeePrefab;
+        }
+
+        // spawn bees similar to spawner system
+        NativeArray<Entity> spawnedBees = state.EntityManager.Instantiate(beePrefab, 10, Allocator.Temp);
+
+        // add tag for newly spawned stuff by default and remove it after initialization
+
+        foreach (Entity beeEntity in spawnedBees)
+        {
+            // spawn bee at position where food has been dropped
+            //var beeTransform = SystemAPI.GetComponent<TransformAspect>(beeEntity);
+            var beeTransform = state.EntityManager.GetAspect<TransformAspect>(beeEntity);
+            beeTransform.Position = foodTransform.Position;
+
+            // make team color match hive
+            //var beeComponent = SystemAPI.GetComponent<Bee>(beeEntity);
+            //var beeComponent = state.EntityManager.GetComponent<Bee>(beeEntity);
+            var beeComponent = state.EntityManager.GetComponentData<Bee>(beeEntity);
+            beeComponent.SpawnPoint = foodTransform.Position;
+            beeComponent.beeTeam = beeTeam;
+            //SystemAPI.SetComponent<Bee>(beeEntity, beeComponent);
+            //state.EntityManager.SetComponent<Bee>(beeEntity, beeComponent);
+            state.EntityManager.SetComponentData<Bee>(beeEntity, beeComponent);
         }
     }
 }
