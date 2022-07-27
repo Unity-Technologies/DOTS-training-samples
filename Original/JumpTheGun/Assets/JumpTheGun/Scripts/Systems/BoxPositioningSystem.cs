4c1942c64449de78aed9596ba0f68b25fb344252
suma-amreiz <suma.amreiz@unity3d.com>
2022-07-27 12:08:08 +0100

    Spawning boxes in random positions on the x and z axis

--- Original/JumpTheGun/Assets/JumpTheGun/Scripts/Systems/BoxPositioningSystem.cs
+++ Original/JumpTheGun/Assets/JumpTheGun/Scripts/Systems/BoxPositioningSystem.cs
@@ -4,13 +4,26 @@ using Unity.Transforms;
 
 partial class BoxPositioningSystem : SystemBase
 {
-    protected override void OnCreate()
-    {
-        base.OnCreate();
-    }
-
     protected override void OnUpdate()
     {
-        throw new System.NotImplementedException();
+        Random random;
+
+        random = Random.CreateFromIndex(1234);
+
+        var deltaTime = Time.DeltaTime;
+
+        Entities
+            .WithAll<Boxes>()
+            .ForEach((Entity entity, TransformAspect transform) =>
+            {
+
+                // Notice that this is a lambda being passed as parameter to ForEach.
+                var pos = transform.Position;
+                pos.x = random.NextFloat(0, 10);
+                pos.z = random.NextFloat(0, 10);
+
+                transform.Position = pos;
+
+            }).ScheduleParallel();
     }
 }
