e836843e19b6004f83e79ac2baba5bb1ee636924
Christian Deiss <christian.deiss@unity3d.com>
2022-07-27 08:23:12 -0700

    fix: fixed drop rate for drop food system

--- Ported/CombatBeez/Assets/Scripts/Systems/DropFoodSystem.cs
+++ Ported/CombatBeez/Assets/Scripts/Systems/DropFoodSystem.cs
@@ -5,12 +5,16 @@ using Unity.Transforms;
 partial struct DropFoodSystem : ISystem
 {
     Random rand;
+    float dropRatePerSecond;
+    float timeSinceLastDrop;
 
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<Config>();
 
         rand = new Random(123);
+        dropRatePerSecond = 1.0f;
+        timeSinceLastDrop = 0.0f;
     }
 
     public void OnDestroy(ref SystemState state)
@@ -19,23 +23,27 @@ partial struct DropFoodSystem : ISystem
 
     public void OnUpdate(ref SystemState state)
     {
-        int dropRatePerFrame = 1;
+        timeSinceLastDrop += state.Time.DeltaTime;
 
-        var config = SystemAPI.GetSingleton<Config>();
-
-        NativeArray<Entity> newlyDroppedFoodResources = state.EntityManager.Instantiate(config.FoodResourcePrefab, dropRatePerFrame, Allocator.Temp);
-        // have default tag on newly generated entities
-        foreach (var (foodResource, transform) in SystemAPI.Query<RefRW<FoodResource>, TransformAspect>())
-        // Component type FoodResource is implicitly defined by the query so no WithAll is needed.
-        // WithAll puts ADDITIONAL requirements on the query.
+        if ((1.0f / dropRatePerSecond) <= timeSinceLastDrop)
         {
-            if (foodResource.ValueRW.State == FoodState.NULL)
-            {
-                var position = new float3(rand.NextInt(-40, 41), rand.NextInt(7, 13), rand.NextInt(-15, 16));
-                transform.Position = position;
+            var config = SystemAPI.GetSingleton<Config>();
 
-                foodResource.ValueRW.State = FoodState.FALLING;
+            NativeArray<Entity> newlyDroppedFoodResources = state.EntityManager.Instantiate(config.FoodResourcePrefab, 1, Allocator.Temp);
+            // have default tag on newly generated entities
+            foreach (var (foodResource, transform) in SystemAPI.Query<RefRW<FoodResource>, TransformAspect>())
+            // Component type FoodResource is implicitly defined by the query so no WithAll is needed.
+            // WithAll puts ADDITIONAL requirements on the query.
+            {
+                if (foodResource.ValueRW.State == FoodState.NULL)
+                {
+                    var position = new float3(rand.NextInt(-40, 41), rand.NextInt(7, 13), rand.NextInt(-15, 16));
+                    transform.Position = position;
+
+                    foodResource.ValueRW.State = FoodState.FALLING;
+                }
             }
+            timeSinceLastDrop = 0.0f;
         }
     }
 }
