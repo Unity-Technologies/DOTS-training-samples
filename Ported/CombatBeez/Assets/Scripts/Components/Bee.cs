e9d9bddd9669d97b8414d7052c1af79f91bd8c8e
Kiki Saintonge <matrixtunnel@gmail.com>
2022-07-27 09:38:11 -0700

    Bees can attack each other and die

--- Ported/CombatBeez/Assets/Scripts/Components/Bee.cs
+++ Ported/CombatBeez/Assets/Scripts/Components/Bee.cs
@@ -6,8 +6,9 @@ public struct Bee : IComponentData
     public float3 OcillateOffset;
     public float3 Target;
     public Entity TargetResource;
+    public Entity TargetBee;
     public BEESTATE beeState;
-
+    public Team beeTeam;
     public float3 SpawnPoint;
 
     public enum BEESTATE
--- Ported/CombatBeez/Assets/Scripts/Components/Team.cs
+++ Ported/CombatBeez/Assets/Scripts/Components/Team.cs
@@ -1,5 +1,5 @@
 public enum Team
 {
-    RED,
+    YELLOW,
     BLUE
 }
