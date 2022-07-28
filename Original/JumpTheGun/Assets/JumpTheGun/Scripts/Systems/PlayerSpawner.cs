b0acfeb0323d619c30d5b568211c67f87a18bd47
a.pilmore-bedford <a.pilmore-bedford@unity3d.com>
2022-07-28 11:15:36 +0100

    Update PlayerSpawner.cs

--- Original/JumpTheGun/Assets/JumpTheGun/Scripts/Systems/PlayerSpawner.cs
+++ Original/JumpTheGun/Assets/JumpTheGun/Scripts/Systems/PlayerSpawner.cs
@@ -13,16 +13,13 @@ partial struct PlayerSpawner : ISystem
     EntityQuery playerQuery;
     EntityQuery boxQuery;
     ComponentDataFromEntity<Boxes> boxesFromEntity;
-    //ComponentDataFromEntity<Translation> playerCFromEntity;
 
 
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
-        // This system should not run before the Config singleton has been loaded.
         state.RequireForUpdate<Config>();
         boxesFromEntity = state.GetComponentDataFromEntity<Boxes>(true);
-        //playerCFromEntity= state.GetComponentDataFromEntity<Translation>(true);
         boxQuery = state.GetEntityQuery(typeof(Boxes));
         playerQuery = state.GetEntityQuery(typeof(PlayerComponent));
     }
@@ -36,37 +33,30 @@ partial struct PlayerSpawner : ISystem
     public void OnUpdate(ref SystemState state)
     {
         var config = SystemAPI.GetSingleton<Config>();
-        boxesFromEntity.Update(ref state);
         var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
         var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
         var boxEntities = boxQuery.ToEntityArray(Unity.Collections.Allocator.TempJob);
         var players = CollectionHelper.CreateNativeArray<Entity>(1, Allocator.Temp);
-        ecb.Instantiate(config.playerPrefab, players);
-
-        UnityEngine.Debug.Log("Before the Masking" );
         var queryMask = playerQuery.GetEntityQueryMask();
-        UnityEngine.Debug.Log("Post Masking" );
+
+        ecb.Instantiate(config.playerPrefab, players);
+        boxesFromEntity.Update(ref state);
 
         foreach (var player in players)
         {
             ecb.SetComponentForLinkedEntityGroup(player, queryMask, PlayerProperties(boxesFromEntity, config, boxEntities));
-            UnityEngine.Debug.Log("An endless cycle of life and death" );
         }
-        // This system should only run once at startup. So it disables itself after one update.
         state.Enabled = false;
-        UnityEngine.Debug.Log("I only happen once");
     }
 
     public static PlayerComponent PlayerProperties(ComponentDataFromEntity<Boxes> boxesFromEntity, Config config, NativeArray<Entity> boxes)
     {
-        UnityEngine.Debug.Log("No boxes :(((");
         var pc = new PlayerComponent();
         foreach (Entity box in boxes){
             UnityEngine.Debug.Log("we have a box");
             pc.startBox = box;
             Boxes boxRef = boxesFromEntity[box]; 
             float3 pcPos = Spawn(boxRef.row,boxRef.column, pc , config, boxRef, boxesFromEntity, boxes);
-            //playerCFromEntity[box] = new Translation(0,0,0);
             return pc;
         }
         return pc;
