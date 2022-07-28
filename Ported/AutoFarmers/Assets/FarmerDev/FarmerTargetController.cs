843250ef48aeb2cada48901c372f4fd7be8c31eb
Ehsan Ehrari <101901479+Ehrarie@users.noreply.github.com>
2022-07-28 10:47:57 +0100

    Farmer to Target Movement

--- Ported/AutoFarmers/Assets/FarmerDev/FarmerTargetController.cs
+++ Ported/AutoFarmers/Assets/FarmerDev/FarmerTargetController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 public partial struct FarmerTargetController : ISystem
@@ -13,7 +14,6 @@ public partial struct FarmerTargetController : ISystem
     public void OnDestroy(ref SystemState state)
     {
     }
-
     public void OnUpdate(ref SystemState state)
     {
         FindTargetJob targetJob = new FindTargetJob();
@@ -25,8 +25,19 @@ public partial struct FarmerTargetController : ISystem
 
 public  partial struct FindTargetJob: IJobEntity
 {
-    public void Execute(TargetPosition targetPosition, ref Velocity velocity)
+    public void Execute(TransformAspect transformAspect, TargetPosition targetPosition, ref Velocity velocity,in FarmerSpeed speed)
     {
-        velocity.value = targetPosition.Target;
+        float3 delta = targetPosition.Target - transformAspect.Position; 
+        Debug.Log(delta);
+        float3 targetVelocity = 0.0f;
+        if (math.lengthsq(delta) > 0.0001f)
+        {
+            float3 direction = math.normalize(delta);
+            float distance = math.length(delta);
+            float finalSpeed = speed.MovementSpeed* math.clamp(distance, 0f, 1f);
+            targetVelocity = direction * finalSpeed;
+
+        }
+        velocity.value = math.lerp(velocity.value, targetVelocity, 0.5f);
     }
 }
