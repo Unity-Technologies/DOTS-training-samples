ae931386749da1f30b0397a3c5df1b3a0cbf8f87
Omar Gonzalez <omar.gonzalez@unity3d.com>
2022-07-28 21:03:31 -0400

    Added system to carry bucket to lake and fill up

---
+++ Ported/BucketBrigade/Assets/Scripts/Systems/BucketMovementSystem.cs
@@ -0,0 +1,71 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+[BurstCompile]
+public partial struct BucketMovementSystem : ISystem
+{
+    EntityQuery m_BucketFillerFetcherQuery;
+
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        m_BucketFillerFetcherQuery = state.GetEntityQuery(typeof(BucketFillerFetcher), typeof(Translation), typeof(BucketId));
+    }
+
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state) { }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var bucketMoveJob = new BucketTravel
+        {
+            BucketFetcherTranslations = m_BucketFillerFetcherQuery.ToComponentDataArray<Translation>(Allocator.TempJob),
+            BucketFetchers = m_BucketFillerFetcherQuery.ToComponentDataArray<BucketFillerFetcher>(Allocator.TempJob),
+            BucketIds = m_BucketFillerFetcherQuery.ToComponentDataArray<BucketId>(Allocator.TempJob),
+            Time = Time.deltaTime,
+            FillSpeed = 1f
+        };
+
+        bucketMoveJob.Schedule();
+    }
+
+    [BurstCompile]
+    partial struct BucketTravel : IJobEntity
+    {
+        [ReadOnly]
+        public NativeArray<Translation> BucketFetcherTranslations;
+        [ReadOnly]
+        public NativeArray<BucketFillerFetcher> BucketFetchers;
+        [ReadOnly]
+        public NativeArray<BucketId> BucketIds;
+        public float Time;
+        public float FillSpeed;
+
+        [BurstCompile]
+        void Execute(ref Translation translation, in BucketId bucketId, ref Volume volume)
+        {
+            for (var i = 0; i < BucketFetchers.Length; i++)
+            {
+                if (BucketIds[i].Value != bucketId.Value)
+                {
+                    continue;
+                }
+
+                // fetcher has bucket
+                if (BucketFetchers[i].state == BucketFillerFetcher.BucketFillerFetcherState.GoToLake)
+                {
+                    translation.Value = BucketFetcherTranslations[i].Value + new float3 { y = 1 };
+                }
+                else if (BucketFetchers[i].state == BucketFillerFetcher.BucketFillerFetcherState.FillBucket)
+                {
+                    volume.Value += FillSpeed * Time;
+                }
+            }
+        }
+    }
+}
