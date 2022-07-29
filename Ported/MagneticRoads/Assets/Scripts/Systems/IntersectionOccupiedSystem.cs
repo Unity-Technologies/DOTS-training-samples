3b8d9652dd5a823ffd86ff69e724c33d330c17c0
Delia Hamwood <delia.hamwood@unity3d.com>
2022-07-29 09:27:48 +0100

    updates to road generation - intersection occupied system is currently disabled

--- Ported/MagneticRoads/Assets/Scripts/Systems/IntersectionOccupiedSystem.cs
+++ Ported/MagneticRoads/Assets/Scripts/Systems/IntersectionOccupiedSystem.cs
@@ -17,12 +17,34 @@ namespace Systems
     partial struct IntersectionOccupiedSystem : ISystem
     {
         [BurstCompile]
-        public void OnCreate(ref SystemState state) { }
+        public void OnCreate(ref SystemState state)
+        {
+            //need to get rid of this to work lol
+            state.Enabled = false;
+        }
 
         [BurstCompile]
         public void OnDestroy(ref SystemState state) { }
 
         [BurstCompile]
-        public void OnUpdate(ref SystemState state) { }
+        public void OnUpdate(ref SystemState state)
+        {
+            var ecbSingleton = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
+            var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+
+            foreach (var carAspect in SystemAPI.Query<CarAspect>().WithAll<WaitingAtIntersection>().WithNone<TraversingIntersection>())
+            {
+                var intersection = SystemAPI.GetComponent<Intersection>(carAspect.NextIntersection);
+                if (intersection.IsOccupied)
+                {
+                    ecb.SetComponentEnabled<WaitingAtIntersection>(carAspect.Entity, false);
+                }
+                else
+                {
+                    intersection.IsOccupied = true;
+                }
+            }
+        }
     }
 }
+
