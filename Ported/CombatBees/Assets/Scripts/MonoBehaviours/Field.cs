df75d7baaf058035bd758a3ada00b466fb73d83e
chris-amyot <chris.amyot@unity3d.com>
2022-09-20 12:50:01 -0600

    chore: Scene Setup for Combat Bees dots training exercise

---
+++ Ported/CombatBees/Assets/Scripts/MonoBehaviours/Field.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Field {
+	public static Vector3 size = new Vector3(100f,20f,30f);
+	public static float gravity = -20f;
+}
