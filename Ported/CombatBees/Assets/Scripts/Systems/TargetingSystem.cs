4a3626142377d848bf9fb0707c59f0b65911c598
Dan Lynch <dan.lynch@unity3d.com>
2022-09-21 11:28:29 -0700

    add job interdependencies, add separate resource job, execute parallel

--- Ported/CombatBees/Assets/Scripts/Systems/TargetingSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/TargetingSystem.cs
@@ -1,37 +1,52 @@
 ﻿using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 
 [BurstCompile]
-[WithNone(typeof(TargetId))]
-partial struct TargetingJob : IJobEntity
+partial struct TargetingEnemyJob : IJobEntity
 {
-    public EntityCommandBuffer ECB;
+    public EntityCommandBuffer.ParallelWriter ECB;
 
     public Random random;
     public float aggression;
 
     [ReadOnly] public NativeArray<Entity> enemies;
-    [ReadOnly] public NativeArray<Entity> resources;
 
-    void Execute(Entity bee)
+    void Execute(Entity bee, [EntityInQueryIndex] int idx)
     {
-        if (random.NextFloat() < aggression && enemies.Length > 0)
+        if (enemies.Length == 0)
         {
-            // attacking
-            ECB.AddComponent<TargetId>(bee, new TargetId() { Value = enemies[random.NextInt(0, enemies.Length)] });
+            return;
         }
-        else if (resources.Length > 0)
+        
+        if (random.NextFloat() < aggression)
         {
-            // gathering
-            // TODO for now we are just grabbing a random resource, but the Original sample only sets this target if it is on the top of the stack 
-            ECB.AddComponent<TargetId>(bee, new TargetId() { Value = resources[random.NextInt(0, resources.Length)] });
+            ECB.SetComponentEnabled<TargetId>(idx, bee, true);
+            ECB.SetComponent(idx, bee, new TargetId() { Value = enemies[random.NextInt(0, enemies.Length)] });
         }
     }
 }
 
 [BurstCompile]
+[WithAny(typeof(YellowTeam), typeof(BlueTeam))]
+partial struct TargetingResourceJob : IJobEntity
+{
+    public EntityCommandBuffer.ParallelWriter ECB;
+
+    public Random random;
+
+    [ReadOnly] public NativeArray<Entity> resources;
+
+    void Execute(Entity bee, [EntityInQueryIndex] int idx)
+    {
+        ECB.SetComponentEnabled<TargetId>(idx, bee, true);
+        ECB.SetComponent(idx, bee, new TargetId() { Value = resources[random.NextInt(0, resources.Length)] });
+    }
+}
+
+//[BurstCompile]
 partial struct TargetingSystem : ISystem
 {
     private EntityQuery m_yellowTeamQuery;
@@ -42,8 +57,8 @@ partial struct TargetingSystem : ISystem
     {
         state.RequireForUpdate<BeeConfig>();
 
-        m_yellowTeamQuery = state.GetEntityQuery(typeof(YellowTeam));
-        m_blueTeamQuery = state.GetEntityQuery(typeof(BlueTeam));
+        m_yellowTeamQuery = state.GetEntityQuery(typeof(YellowTeam), ComponentType.Exclude<TargetId>());
+        m_blueTeamQuery = state.GetEntityQuery(typeof(BlueTeam), ComponentType.Exclude<TargetId>());
         m_resourcesQuery = state.GetEntityQuery(typeof(GridPosition));
     }
 
@@ -57,22 +72,31 @@ partial struct TargetingSystem : ISystem
 
         var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
         var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+        var ecbWriter = ecb.AsParallelWriter();
 
-        var blueTeamJob = new TargetingJob()
+        var blueTeamJob = new TargetingEnemyJob()
         {
-            ECB = ecb, random = Random.CreateFromIndex((uint)state.Time.ElapsedTime), aggression = config.aggression,
-            enemies = m_yellowTeamQuery.ToEntityArray(Allocator.TempJob),
-            resources = m_resourcesQuery.ToEntityArray(Allocator.TempJob)
-        };
+            ECB = ecbWriter, random = Random.CreateFromIndex((uint)state.Time.ElapsedTime),
+            aggression = config.aggression,
+            enemies = m_yellowTeamQuery.ToEntityArray(Allocator.TempJob)
+        }.ScheduleParallel(m_blueTeamQuery, state.Dependency);
 
-        var yellowTeamJob = new TargetingJob()
+        var yellowTeamJob = new TargetingEnemyJob()
         {
-            ECB = ecb, random = Random.CreateFromIndex((uint)state.Time.ElapsedTime), aggression = config.aggression,
-            enemies = m_blueTeamQuery.ToEntityArray(Allocator.TempJob),
-            resources = m_resourcesQuery.ToEntityArray(Allocator.TempJob)
-        };
+            ECB = ecbWriter, random = Random.CreateFromIndex((uint)state.Time.ElapsedTime),
+            aggression = config.aggression,
+            enemies = m_blueTeamQuery.ToEntityArray(Allocator.TempJob)
+        }.ScheduleParallel(m_yellowTeamQuery, state.Dependency);
 
-        blueTeamJob.Schedule(m_blueTeamQuery);
-        yellowTeamJob.Schedule(m_yellowTeamQuery);
+        var enemyJob = JobHandle.CombineDependencies(blueTeamJob, yellowTeamJob);
+
+        if (!m_resourcesQuery.IsEmpty)
+        {
+            new TargetingResourceJob()
+            {
+                ECB = ecbWriter, random = Random.CreateFromIndex((uint)state.Time.ElapsedTime),
+                resources = m_resourcesQuery.ToEntityArray(Allocator.TempJob)
+            }.ScheduleParallel(enemyJob);
+        }
     }
 }
