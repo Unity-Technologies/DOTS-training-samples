5b90d493f41eca70b7ad50e4d106d9cc20daca46
Steve Haines <steve.haines@unity3d.com>
2022-09-21 13:52:08 -0700

    snap resources to grid prep for stacks

--- Ported/CombatBees/Assets/Scripts/Systems/ResourceSpawningSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/ResourceSpawningSystem.cs
@@ -11,6 +11,7 @@ partial struct ResourceSpawningSystem : ISystem
     Vector2Int m_GridCounts;
     Vector2 m_GridSize;
     Vector2 m_MinGridPos;
+    float m_ResourceSize;
 
     [BurstCompile]
     public void OnCreate(ref SystemState state)
@@ -36,6 +37,7 @@ partial struct ResourceSpawningSystem : ISystem
         m_GridCounts = Vector2Int.RoundToInt(new Vector2(fieldConfig.FieldScale.x, fieldConfig.FieldScale.z) / resourceConfig.resourceSize);
         m_GridSize = new Vector2(fieldConfig.FieldScale.x / m_GridCounts.x, fieldConfig.FieldScale.z / m_GridCounts.y);
         m_MinGridPos = new Vector2((m_GridCounts.x - 1f) * -.5f * m_GridSize.x, (m_GridCounts.y - 1f) * -.5f * m_GridSize.y);
+        m_ResourceSize = resourceConfig.resourceSize;
 
         var resources = CollectionHelper.CreateNativeArray<Entity>(resourceConfig.startResourceCount, Allocator.Temp);
 
@@ -44,15 +46,49 @@ partial struct ResourceSpawningSystem : ISystem
         foreach (var resource in resources)
         {
             float3 pos = new float3(m_MinGridPos.x * .25f + UnityEngine.Random.value * fieldConfig.FieldScale.x * .25f, UnityEngine.Random.value * 10f, m_MinGridPos.y + UnityEngine.Random.value * fieldConfig.FieldScale.z);
+            pos = NearestSnappedPos(pos);
 
-            ecb.AddComponent(resource, new GridPosition());
+            int gridX, gridY;
+            GetGridIndex(pos, out gridX, out gridY);
+
+            var gridPos = new GridPosition() { Value = new int2 (gridX, gridY) };
+
+            ecb.AddComponent(resource, gridPos);
+
+            var stackIndex = new StackIndex();
+            //TODO: calculate stack index
+            ecb.AddComponent(resource, stackIndex);
+
+            float floorY = GetStackPos(gridX, gridY, stackIndex.Value).y;
+            pos.y = floorY;
 
             var scaleTransform = UniformScaleTransform.FromPosition(pos);
-            scaleTransform.Scale = resourceConfig.resourceSize;
+            scaleTransform.Scale = m_ResourceSize;
 
             ecb.SetComponent(resource, new LocalToWorldTransform() { Value = scaleTransform });
         }
 
         state.Enabled = false;
+    }
+
+    float3 NearestSnappedPos(float3 pos)
+    {
+        int x, y;
+        GetGridIndex(pos, out x, out y);
+        return new float3(m_MinGridPos.x + x * m_GridSize.x, pos.y, m_MinGridPos.y + y * m_GridSize.y);
+    }
+
+    void GetGridIndex(float3 pos, out int gridX, out int gridY)
+    {
+        gridX = Mathf.FloorToInt((pos.x - m_MinGridPos.x + m_GridSize.x * .5f) / m_GridSize.x);
+        gridY = Mathf.FloorToInt((pos.z - m_MinGridPos.y + m_GridSize.y * .5f) / m_GridSize.y);
+
+        gridX = Mathf.Clamp(gridX, 0, m_GridCounts.x - 1);
+        gridY = Mathf.Clamp(gridY, 0, m_GridCounts.y - 1);
+    }
+
+    Vector3 GetStackPos(int x, int y, int height)
+    {
+        return new Vector3(m_MinGridPos.x + x * m_GridSize.x, -Field.size.y * .5f + (height + .5f) * m_ResourceSize, m_MinGridPos.y + y * m_GridSize.y);
     }
 }
