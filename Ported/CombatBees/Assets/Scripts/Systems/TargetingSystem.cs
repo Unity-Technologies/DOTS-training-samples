01f080f4646b7ddb4562f56dd39f58eeaad43398
Dan Lynch <dan.lynch@unity3d.com>
2022-09-21 11:44:31 -0700

    separate idle/non-idle team members

--- Ported/CombatBees/Assets/Scripts/Systems/TargetingSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/TargetingSystem.cs
@@ -31,6 +31,7 @@ partial struct TargetingEnemyJob : IJobEntity
 
 [BurstCompile]
 [WithAny(typeof(YellowTeam), typeof(BlueTeam))]
+[WithNone(typeof(TargetId))]
 partial struct TargetingResourceJob : IJobEntity
 {
     public EntityCommandBuffer.ParallelWriter ECB;
@@ -50,15 +51,19 @@ partial struct TargetingResourceJob : IJobEntity
 partial struct TargetingSystem : ISystem
 {
     private EntityQuery m_yellowTeamQuery;
+    private EntityQuery m_yellowTeamIdleQuery;
     private EntityQuery m_blueTeamQuery;
+    private EntityQuery m_blueTeamIdleQuery;
     private EntityQuery m_resourcesQuery;
 
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<BeeConfig>();
 
-        m_yellowTeamQuery = state.GetEntityQuery(typeof(YellowTeam), ComponentType.Exclude<TargetId>());
-        m_blueTeamQuery = state.GetEntityQuery(typeof(BlueTeam), ComponentType.Exclude<TargetId>());
+        m_yellowTeamQuery = state.GetEntityQuery(typeof(YellowTeam));
+        m_yellowTeamIdleQuery = state.GetEntityQuery(typeof(YellowTeam), ComponentType.Exclude<TargetId>());
+        m_blueTeamQuery = state.GetEntityQuery(typeof(BlueTeam));
+        m_blueTeamIdleQuery = state.GetEntityQuery(typeof(BlueTeam), ComponentType.Exclude<TargetId>());
         m_resourcesQuery = state.GetEntityQuery(typeof(GridPosition));
     }
 
@@ -79,14 +84,14 @@ partial struct TargetingSystem : ISystem
             ECB = ecbWriter, random = Random.CreateFromIndex((uint)state.Time.ElapsedTime),
             aggression = config.aggression,
             enemies = m_yellowTeamQuery.ToEntityArray(Allocator.TempJob)
-        }.ScheduleParallel(m_blueTeamQuery, state.Dependency);
+        }.ScheduleParallel(m_blueTeamIdleQuery, state.Dependency);
 
         var yellowTeamJob = new TargetingEnemyJob()
         {
             ECB = ecbWriter, random = Random.CreateFromIndex((uint)state.Time.ElapsedTime),
             aggression = config.aggression,
             enemies = m_blueTeamQuery.ToEntityArray(Allocator.TempJob)
-        }.ScheduleParallel(m_yellowTeamQuery, state.Dependency);
+        }.ScheduleParallel(m_yellowTeamIdleQuery, state.Dependency);
 
         var enemyJob = JobHandle.CombineDependencies(blueTeamJob, yellowTeamJob);
 
