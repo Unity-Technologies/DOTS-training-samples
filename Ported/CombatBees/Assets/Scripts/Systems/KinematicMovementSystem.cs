58cee07cb44d9589791d581ecc0fa90ee01e9f32
Steve Haines <steve.haines@unity3d.com>
2022-09-22 10:02:04 -0700

    add jitter job

--- Ported/CombatBees/Assets/Scripts/Systems/KinematicMovementSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/KinematicMovementSystem.cs
@@ -1,15 +1,27 @@
 ﻿using Unity.Entities;
+using Unity.Mathematics;
 
 partial struct KinematicMovementSystem : ISystem {
     public void OnCreate(ref SystemState state) {
+        state.RequireForUpdate<BeeConfig>();
     }
 
     public void OnDestroy(ref SystemState state) {
     }
 
     public void OnUpdate(ref SystemState state) {
+        var beeConfig = SystemAPI.GetSingleton<BeeConfig>();
+
         state.Dependency = new MovementJob() {
-            deltaTime = state.Time.DeltaTime
+            deltaTime = state.Time.DeltaTime,
         }.ScheduleParallel(state.Dependency);
+
+        state.Dependency = new JitterJob()
+        {
+            random = Random.CreateFromIndex((uint)state.Time.ElapsedTime * 1000),
+            deltaTime = state.Time.DeltaTime,
+            jitter = beeConfig.flightJitter,
+            damping = beeConfig.damping
+        }.Schedule(state.Dependency);
     }
 }
