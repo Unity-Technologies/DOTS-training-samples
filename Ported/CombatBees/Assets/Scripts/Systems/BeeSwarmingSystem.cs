e1f9457f63598761a073388f11573ded783c0d75
alex.rogovskyi <alex.rogovskyi@unity3d.com>
2022-11-14 15:14:25 -0800

    System adjustments.

--- Ported/CombatBees/Assets/Scripts/Systems/BeeSwarmingSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/BeeSwarmingSystem.cs
@@ -8,7 +8,34 @@ using Random = Unity.Mathematics.Random;
 
 namespace Systems
 {
+    [WithAll(typeof(Bee))]
     [BurstCompile]
+    partial struct SafeZoneJob : IJobEntity
+    {
+        [NativeDisableParallelForRestriction] public ComponentLookup<Bee> Bees;
+        public EntityQuery BeeQuery;
+        public Random Random;
+        public float Dt;
+
+        public float SquaredRadius;
+
+        void Execute(Entity entity, Bee currentBee)
+        {
+            var teamAttraction = SystemAPI.GetSingleton<BeeConfig>().Team1.TeamAttraction;
+            
+            var allBees = BeeQuery.ToEntityArray(Allocator.Temp);
+            var targetEntity = allBees[Random.NextInt(allBees.Length)];
+            Bees.TryGetComponent(targetEntity, out var randomBee);
+            
+            float3 delta = randomBee.Position - currentBee.Position;
+            float dist = Mathf.Sqrt(delta.x * delta.x + delta.y * delta.y + delta.z * delta.z);
+            if (dist > 0f)
+            {
+                currentBee.Velocity += delta * (teamAttraction * Dt / dist);
+            }
+        }
+    }
+    
     public partial class BeeSwarmingSystem : SystemBase
     {
         private EntityQuery _beeQuery;
@@ -25,24 +52,42 @@ namespace Systems
         protected override void OnUpdate()
         {
             var dt = SystemAPI.Time.DeltaTime;
-            var allBees = _beeQuery.ToEntityArray(Allocator.Temp);
-            var teamAttraction = SystemAPI.GetSingleton<BeeConfig>().Team1.TeamAttraction;
+            // var allBees = _beeQuery.ToEntityArray(Allocator.Temp);
+            // var teamAttraction = SystemAPI.GetSingleton<BeeConfig>().Team1.TeamAttraction;
+            //
+            
 
-            Entities
-                .WithAll<Bee>()
-                .ForEach((Entity entity, Bee currentBee) =>
-                {
-                    var targetEntity = allBees[_random.NextInt(allBees.Length)];
-                    _bees.TryGetComponent(targetEntity, out var randomBee);
-                    
-                    float3 delta = randomBee.Position - currentBee.Position;
-                    float dist = Mathf.Sqrt(delta.x * delta.x + delta.y * delta.y + delta.z * delta.z);
-                    if (dist > 0f)
-                    {
-                        currentBee.Velocity += delta * (teamAttraction * dt / dist);
-                    }
-                })
-                .ScheduleParallel(Dependency);
+            // Bee GetRandom()
+            // {
+            //     var targetEntity = allBees[_random.NextInt(allBees.Length)];
+            //     _bees.TryGetComponent(targetEntity, out var randomBee);
+            //     return randomBee;
+            // }
+            //
+            // Entities
+            //     .WithAll<Bee>()
+            //     .ForEach((ref Entity entity, ref Bee currentBee) =>
+            //     {
+            //         var randomBee = GetRandom();
+            //         
+            //         float3 delta = randomBee.Position - currentBee.Position;
+            //         float dist = Mathf.Sqrt(delta.x * delta.x + delta.y * delta.y + delta.z * delta.z);
+            //         if (dist > 0f)
+            //         {
+            //             currentBee.Velocity += delta * (teamAttraction * dt / dist);
+            //         }
+            //     })
+            //     .WithoutBurst()
+            //     .ScheduleParallel(Dependency);
+
+            var job = new SafeZoneJob()
+            {
+                BeeQuery = _beeQuery,
+                Bees = _bees,
+                Dt = dt
+            };
+
+            job.ScheduleParallel();
         }
     }
 }
