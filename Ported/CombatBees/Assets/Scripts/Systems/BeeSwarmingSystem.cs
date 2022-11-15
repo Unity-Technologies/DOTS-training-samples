345cb30e62054ec31dce55493b474701972d1c4c
alex.rogovskyi <alex.rogovskyi@unity3d.com>
2022-11-14 16:02:25 -0800

    System adjustments.

--- Ported/CombatBees/Assets/Scripts/Systems/BeeSwarmingSystem.cs
+++ Ported/CombatBees/Assets/Scripts/Systems/BeeSwarmingSystem.cs
@@ -1,4 +1,5 @@
-﻿using Components;
+﻿using System;
+using Components;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
@@ -8,58 +9,68 @@ using Random = Unity.Mathematics.Random;
 
 namespace Systems
 {
-    [WithAll(typeof(Bee))]
     [BurstCompile]
     partial struct SwarmJob : IJobEntity
     {
-        [NativeDisableParallelForRestriction] public ComponentLookup<Bee> Bees;
-        public EntityQuery BeeQuery;
-        public Random Random;
+        [ReadOnly] public NativeArray<Bee> BeeComponents;
+        public float TeamAttraction;
         public float Dt;
+        public uint Seed;
 
-        void Execute(Entity entity, Bee currentBee)
+        void Execute([EntityInQueryIndex] int index, Entity entity, ref Bee currentBee)
         {
-            var teamAttraction = SystemAPI.GetSingleton<BeeConfig>().Team1.TeamAttraction;
-            
-            var allBees = BeeQuery.ToEntityArray(Allocator.Temp);
-            var targetEntity = allBees[Random.NextInt(allBees.Length)];
-            Bees.TryGetComponent(targetEntity, out var randomBee);
-            
+            var newRandom = Random.CreateFromIndex((uint)index + Seed);
+            var randomBee = BeeComponents[newRandom.NextInt(BeeComponents.Length)];
+
             float3 delta = randomBee.Position - currentBee.Position;
             float dist = Mathf.Sqrt(delta.x * delta.x + delta.y * delta.y + delta.z * delta.z);
             if (dist > 0f)
             {
-                currentBee.Velocity += delta * (teamAttraction * Dt / dist);
+                currentBee.Velocity += delta * (TeamAttraction * Dt / dist);
             }
         }
     }
-    
-    public partial class BeeSwarmingSystem : SystemBase
+
+    public partial struct BeeSwarmingSystem : ISystem
     {
         private EntityQuery _beeQuery;
         private ComponentLookup<Bee> _bees;
         private Random _random;
+        private uint _randomSeed;
 
-        protected override void OnCreate()
+
+        public void OnCreate(ref SystemState state)
         {
             _random = Random.CreateFromIndex(4000);
-            _beeQuery = GetEntityQuery(typeof(Bee));
-            _bees = GetComponentLookup<Bee>();
+            _beeQuery = state.GetEntityQuery(typeof(Bee));
+            _bees = state.GetComponentLookup<Bee>();
         }
 
-        protected override void OnUpdate()
+        public void OnDestroy(ref SystemState state)
+        {
+        }
+
+        public void OnUpdate(ref SystemState state)
         {
             var dt = SystemAPI.Time.DeltaTime;
+            _bees.Update(ref state);
+            
+            var teamAttraction = SystemAPI.GetSingleton<BeeConfig>().Team1.TeamAttraction;
+            //var bees = _beeQuery.ToEntityArray(Allocator.TempJob);
+            var beeComponents = _beeQuery.ToComponentDataArray<Bee>(Allocator.TempJob);
 
             var job = new SwarmJob()
             {
-                BeeQuery = _beeQuery,
-                Bees = _bees,
+                //Entities = bees,
+                BeeComponents = beeComponents,
                 Dt = dt,
-                Random = _random
+                Seed = _random.NextUInt(),
+                TeamAttraction = teamAttraction
             };
 
-            job.ScheduleParallel();
+            job.Schedule();
+
+            beeComponents.Dispose(state.Dependency);
         }
     }
 }
