e1feac609b7a3257f99e86b82dc277ab4d92671e
Matt Brown <matthew.brown@unity3d.com>
2022-11-16 12:33:40 -0600

    clear walls out from open paths

--- Ported/Mazombie/Mazombie/Assets/Scripts/Systems/MazeGeneratorSystem.cs
+++ Ported/Mazombie/Mazombie/Assets/Scripts/Systems/MazeGeneratorSystem.cs
@@ -170,58 +170,61 @@ public partial struct MazeGeneratorSystem : ISystem
             }
         }
 
-        // remove walls around border
-        //for (int y = 0; y < math.min(gameConfig.openStripWidth, size); y++)
-        //{
-        //    for (int x = 0; x < size; x++)
-        //    {
-        //        if (x > 0)
-        //        {
-        //            SetDownWall(x, y, ref grid, size);
-        //        }
-        //        if (y > 0)
-        //        {
-        //            SetLeftWall(x, y, ref grid, size);
-        //        }
-        //    }
-        //}
+        // remove walls at west border running south to north
+        for (int y = 0; y < 0; y++)
+        {
+            for (int x = 0; x < math.min(gameConfig.openStripWidth, size); x++)
+            {
+                if (x > 0)
+                {
+                    RemoveEastWestWall(x, y, ref grid, size);
+                }
+                if (y > 0)
+                {
+                    RemoveNorthSouthWall(x, y, ref grid, size);
+                }
+            }
+        }
 
-        //for (int y = math.max(size - gameConfig.openStripWidth, 0); y < size; y++)
-        //{
-        //    for (int x = 0; x < size; x++)
-        //    {
-        //        if (x > 0)
-        //        {
-        //            SetDownWall(x, y, ref grid, size);
-        //        }
-        //        if (y > 0)
-        //        {
-        //            SetLeftWall(x, y, ref grid, size);
-        //        }
-        //    }
-        //}
+        // remove walls at east border running south to north
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = math.max(size - gameConfig.openStripWidth, 0); x < size; x++)
+            {
+                if (x > 0)
+                {
+                    RemoveEastWestWall(x, y, ref grid, size);
+                }
+                if (y > 0)
+                {
+                    RemoveNorthSouthWall(x, y, ref grid, size);
+                }
+            }
+        }
 
-        //for (int x = 0; x < math.min(gameConfig.openStripWidth, size); x++)
-        //{
-        //    for (int y = 0; y < size; y++)
-        //    {
-        //        if (x > 0)
-        //            SetDownWall(x, y, ref grid, size);
-        //        if (y > 0)
-        //            SetLeftWall(x, y, ref grid, size);
-        //    }
-        //}
+        // remove walls at south border running west to east
+        for (int y = 0; y < math.min(gameConfig.openStripWidth, size); y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                if (x > 0)
+                    RemoveEastWestWall(x, y, ref grid, size);
+                if (y > 0)
+                    RemoveNorthSouthWall(x, y, ref grid, size);
+            }
+        }
 
-        //for (int x = math.max(size - gameConfig.openStripWidth, 0); x < size; x++)
-        //{
-        //    for (int y = 0; y < size; y++)
-        //    {
-        //        if (x > 0)
-        //            SetDownWall(x, y, ref grid, size);
-        //        if (y > 0)
-        //            SetLeftWall(x, y, ref grid, size);
-        //    }
-        //}
+        // remove walls at north border running west to east
+        for (int y = math.max(size - gameConfig.openStripWidth, 0); y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                if (x > 0)
+                    RemoveEastWestWall(x, y, ref grid, size);
+                if (y > 0)
+                    RemoveNorthSouthWall(x, y, ref grid, size);
+            }
+        }
 
         // spawn walls
         for (int y = 0; y < size; y++)
