45f97ed3e2562aa8880a84e1917b2b332f5e71e4
saranyacodes <saranyas@sas.upenn.edu>
2022-11-16 12:02:39 -0500

    colony and resource update

--- Ported/Ants/Assets/Scripts/Systems/ResourceSpawnSystem.cs
+++ Ported/Ants/Assets/Scripts/Systems/ResourceSpawnSystem.cs
@@ -30,7 +30,7 @@ partial struct ResourceSpawnSystem : ISystem
         
         // get resource position 
         Random rand = new Random(123); // todo: figure out how to make this a random number 
-        float resourceAngle = rand.NextFloat(0f, 360f) * 2f * math.PI;
+        float resourceAngle = rand.NextFloat() * 2f * math.PI;
         float3 resourcePosition = new float3(new float2(config.MapSize * 0.5f, config.MapSize * 0.5f) + new float2(math.cos(resourceAngle) * config.MapSize * .475f,
             math.sin(resourceAngle) * config.MapSize * .475f), 0f);
         
