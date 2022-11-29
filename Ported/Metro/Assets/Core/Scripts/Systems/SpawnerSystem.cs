f12cad2fd4754e8dbb5534bb90c83671bc11b6c4
Karlo Eldic <karloeldic@gmail.com>
2022-11-29 11:30:40 +0100

    Add platform prefab to config, test spawn

--- Ported/Metro/Assets/Core/Scripts/Systems/SpawnerSystem.cs
+++ Ported/Metro/Assets/Core/Scripts/Systems/SpawnerSystem.cs
@@ -19,12 +19,13 @@ partial struct SpawnerSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-            var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
-            var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
-            var config = SystemAPI.GetSingleton<Config>();
-            var persons = CollectionHelper.CreateNativeArray<Entity>(config.PersonCount, Allocator.Temp);
-            Debug.Log("persons: " + persons.Length);
-            ecb.Instantiate(config.PersonPrefab, persons);
+        var ecbSingleton = SystemAPI.GetSingleton<BeginInitializationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+        var config = SystemAPI.GetSingleton<Config>();
+        var persons = CollectionHelper.CreateNativeArray<Entity>(config.PersonCount, Allocator.Temp);
+        var platforms = CollectionHelper.CreateNativeArray<Entity>(config.PlatformCountPerStation, Allocator.Temp);
+        ecb.Instantiate(config.PlatformPrefab, platforms);
+        ecb.Instantiate(config.PersonPrefab, persons);
         state.Enabled = false;
     }
 }
