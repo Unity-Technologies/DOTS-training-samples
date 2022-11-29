e160b591f3073759a46c94a321f90e10e0187d3b
Erik Hakala <erik.hakala@unity3d.com>
2022-11-29 13:07:33 +0200

    More work on the pheromone systems.

--- Ported/AntGroup2/Assets/Scripts/Systems/PheromoneDisplaySystem.cs
+++ Ported/AntGroup2/Assets/Scripts/Systems/PheromoneDisplaySystem.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using Unity.Entities;
+using Unity.Mathematics;
+using Random = UnityEngine.Random;
 
-
+//[UpdateAfter(typeof(PheromoneDecaySystem))]
+[UpdateInGroup(typeof(LateSimulationSystemGroup))]
 partial class PheromoneDisplaySystem : SystemBase
 {
+    public const int PheromoneTextureSizeX = 64;
+    public const int PheromoneTextureSizeY = 64;
+        
     public GameObject displayPlane;
     private Material displayMaterial;
     private Texture2D pheromoneTexture;
@@ -14,14 +20,14 @@ partial class PheromoneDisplaySystem : SystemBase
         
         displayPlane = GameObject.Find("DisplayPlane");
         
-        pheromoneTexture = new Texture2D(64, 64);   // TEMP: Hardcoded size
+        pheromoneTexture = new Texture2D(PheromoneTextureSizeX, PheromoneTextureSizeY);   // TEMP: Hardcoded size
         
         if (displayPlane.TryGetComponent<Renderer>(out var displayRenderer))
             displayMaterial = displayRenderer.material;
 
         // TEMP: Dummy Add a single entity.
-        var e = EntityManager.CreateEntity();
-        EntityManager.AddComponent<PheromoneMap>(e);
+        //var e = EntityManager.CreateEntity();
+        //EntityManager.AddComponent<PheromoneMap>(e);
     }
 
     protected override void OnUpdate()
@@ -32,7 +38,9 @@ partial class PheromoneDisplaySystem : SystemBase
             //DynamicBuffer<PheromoneMap> mapBuffer = EntityManager.GetBuffer<PheromoneMap>(ent);
             // TODO...
             
-            Debug.Log("Foo");
+            //Debug.Log("Foo");
+
+            int2 texSize = new int2(pheromoneTexture.width, pheromoneTexture.height);
             
             // Clear
             for (int i = 0; i < 64; i++)
@@ -44,12 +52,23 @@ partial class PheromoneDisplaySystem : SystemBase
             }
             
             // Random fill contents
-            for (int i = 0; i < 64; i++)
+            /*for (int i = 0; i < 64; i++)
             {
                 float x = Random.value;
                 float y = Random.value;
             
-                pheromoneTexture.SetPixel((int)(x*63), (int)(y*63), Color.red);
+                pheromoneTexture.SetPixel((int)(x*(texSize.x-1)), (int)(y*(texSize.x-1)), Color.red);
+            }*/
+            
+            for (int i = 0; i < map.Length; i++)
+            {
+                float amount = map[i].amount;
+                int colorAmount = (int)(math.saturate(amount) * 255);
+
+                int x = i % PheromoneTextureSizeX;
+                int y = i / PheromoneTextureSizeX;
+
+                pheromoneTexture.SetPixel(x, y, new Color(colorAmount, 0, 0, 255));
             }
             pheromoneTexture.Apply();
             
