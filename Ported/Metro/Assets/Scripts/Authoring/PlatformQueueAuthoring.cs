9a96c52297407e496d12b9a861556beaaff57ce4
Daniel Reidler <daniel.reidler@unity3d.com>
2022-11-29 16:32:38 +0100

    Added PlatformSpawner, PlatformQueue and Waypoints

---
+++ Ported/Metro/Assets/Scripts/Authoring/PlatformQueueAuthoring.cs
@@ -0,0 +1,19 @@
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+public class PlatformQueueAuthoring : MonoBehaviour
+{
+    
+}
+
+class PlatformQueueBaker : Baker<PlatformQueueAuthoring>
+{
+    public override void Bake(PlatformQueueAuthoring authoring)
+    {
+        AddComponent(new PlatformQueue
+        {
+            //Passengers = new NativeList<Entity>();
+        });
+    }
+}
---
+++ Ported/Metro/Assets/Scripts/Components/PlatformConfig.cs
@@ -0,0 +1,6 @@
+using Unity.Entities;
+
+public struct PlatformConfig : IComponentData
+{
+    public Entity PlatformPrefab;
+}
