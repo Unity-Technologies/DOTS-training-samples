e6d2041abe859f4f9c2b713ac11854770a300d04
Lieven Van den Audenaeren <lieven.audenaeren@unity3d.com>
2022-11-30 14:39:50 +0100

    WIP commit input

---
+++ Ported/CombatBees/Assets/Scripts/Systems/InputBehaviourSystem.cs
@@ -0,0 +1,69 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+partial struct InputBehaviourSystem : ISystem
+{
+    private static Camera camera;
+    
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<Config>();
+    }
+
+    public void OnDestroy(ref SystemState state)
+    {
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        var config = SystemAPI.GetSingleton<Config>();
+        var timeData = state.WorldUnmanaged.Time;
+        var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+        var halfFieldSize = config.fieldSize * .5f;
+        if (!camera)
+            camera = Camera.main;
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            var mouseRay = camera.ScreenPointToRay(Input.mousePosition);
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = -1; j <= 1; j += 2)
+                {
+                    var wallCenter = new Vector3();
+                    wallCenter[i] = halfFieldSize[i] * j;
+                    var plane = new Plane(-wallCenter, wallCenter);
+                    float hitDistance;
+                    if (Vector3.Dot(plane.normal, mouseRay.direction) < 0f)
+                    {
+                        if (plane.Raycast(mouseRay, out hitDistance))
+                        {
+                            Vector3 hitPoint = mouseRay.GetPoint(hitDistance);
+                            bool insideField = true;
+                            for (int k = 0; k < 3; k++)
+                            {
+                                if (Mathf.Abs(hitPoint[k]) > config.fieldSize[k] * .5f + .01f)
+                                {
+                                    insideField = false;
+                                    break;
+                                }
+                            }
+
+                            if (insideField)
+                            {
+                                SpawnResource();
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void SpawnResource()
+    {
+        
+    }
+}
