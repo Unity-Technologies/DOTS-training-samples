accf567596a54b82fe85b2b6e34d4da8768b64dc
Daniel Reidler <daniel.reidler@unity3d.com>
2022-11-30 15:42:57 +0100

    Added StationId to markers

---
+++ Ported/Metro/Assets/Scripts/Authoring/StationIdAuthoring.cs
@@ -0,0 +1,15 @@
+using Unity.Entities;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class StationIdAuthoring : MonoBehaviour
+{
+    public int Value;
+}
+class StationIdBaker : Baker<StationIdAuthoring>
+{
+    public override void Bake(StationIdAuthoring authoring)
+    {
+        AddComponent(new StationId{ Value = authoring.Value});
+    }
+}
