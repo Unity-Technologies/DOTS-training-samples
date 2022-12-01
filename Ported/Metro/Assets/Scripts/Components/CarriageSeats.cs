8b69140deff16f8c9f5287c7f91090e619b65399
Kirill Titov <kirill.titov@unity3d.com>
2022-12-01 15:02:03 +0100

    Added seats position and array for passengers

---
+++ Ported/Metro/Assets/Scripts/Components/CarriageSeats.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct CarriageSeats : IComponentData
+{
+    public NativeArray<float3> Seats;
+    public NativeArray<Entity> Passengers;
+}
