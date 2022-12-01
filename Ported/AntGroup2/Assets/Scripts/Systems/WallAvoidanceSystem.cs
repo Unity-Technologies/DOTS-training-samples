35c0902e9ce6697e24d9c20115f614d19037706c
RobinDM <robin.demoor@unity3d.com>
2022-12-01 09:32:54 +0100

    Wall tunneling fix

--- Ported/AntGroup2/Assets/Scripts/Systems/WallAvoidanceSystem.cs
+++ Ported/AntGroup2/Assets/Scripts/Systems/WallAvoidanceSystem.cs
@@ -48,54 +48,7 @@ partial struct WallAvoidanceSystem : ISystem
         var walls = wallQuery.ToComponentDataArray<LocalTransform>(state.WorldUpdateAllocator);
         var job = new WallAvoidanceJob { wallPositions = walls };
         job.ScheduleParallel();
-        // foreach (var ant in SystemAPI.Query<DirectionAspect, TransformAspect>().WithAll<Ant>())
-        // {
-        //     //bool lineOfSight = true;
-        //     bool wallBounce = false;
-        //     foreach (var wall in SystemAPI.Query<TransformAspect>().WithAll<Obstacle>())
-        //     {
-        //         //check if wall in nearby line of sight
-        //         float range = 3.0f;
-        //         float3 antStep = ant.Item2.WorldPosition + new float3(math.sin(ant.Item1.CurrentDirection),0, math.cos(ant.Item1.CurrentDirection)) * range;
-        //         if (Intersect(ant.Item2.WorldPosition.xz, antStep.xz, wall.LocalPosition.xz, 1.0f))
-        //         {
-        //             var dx = antStep.x - wall.WorldPosition.x;
-        //             var dy = antStep.z - wall.WorldPosition.z;
-        //             float sqrDist = dx * dx + dy * dy;
-        //
-        //             float dir = 1.0f;
-        //             float2 reflect = math.reflect(new float2(ant.Item2.Forward.x, ant.Item2.Forward.z), new float2(ant.Item2.WorldPosition.x, ant.Item2.WorldPosition.z));
-        //             float target = math.atan2(reflect.x, reflect.y);
-        //
-        //             target -= ant.Item1.CurrentDirection;
-        //             if (target < 0)
-        //                 dir = -1.0f;
-        //             
-        //             ant.Item1.WallDirection = (math.PI / 4.0f) * (math.pow((range*range - sqrDist)/(range*range ), 1.0f)) * dir;
-        //             if (ant.Item1.WallDirection > math.PI)
-        //                 ant.Item1.WallDirection -= (float)(math.PI * 2.0f);
-        //             if (ant.Item1.WallDirection < -math.PI)
-        //                 ant.Item1.WallDirection += (float)(math.PI * 2.0f);
-        //         }
-        //         
-        //         //check if inside wall
-        //         if (ant.Item2.WorldPosition.x - wall.WorldPosition.x <= 2f || ant.Item2.WorldPosition.z - wall.WorldPosition.z <= 2f)
-        //         {
-        //             var dx = ant.Item2.WorldPosition.x - wall.WorldPosition.x;
-        //             var dy = ant.Item2.WorldPosition.z - wall.WorldPosition.z;
-        //             float sqrDist = dx * dx + dy * dy;
-        //             if(sqrDist < /*ObstacleRadius*/ 1.8f * 1.8f)
-        //             {
-        //                 var dist = math.sqrt(sqrDist);
-        //                 dx /= dist;
-        //                 dy /= dist;
-        //                 wallBounce = true;
-        //             }
-        //         }
-        //
-        //         ant.Item1.WallBounce = wallBounce;
-        //     }
-        // }
+        
     }
  
     [BurstCompile]
@@ -156,9 +109,12 @@ partial struct WallAvoidanceSystem : ISystem
                     if (wallDirection.Angle < -math.PI)
                         wallDirection.Angle += (float)(math.PI * 2.0f);
                 }
-                
-                //check if inside wall
-                if (transformAspect.Position.x - wall.Position.x <= 2f || transformAspect.Position.z - wall.Position.z <= 2f)
+
+                if (wallDirection.WallBounceDirection != 0)
+                {
+                    wallBounce = 0;
+                }
+                else if (transformAspect.Position.x - wall.Position.x <= 2f || transformAspect.Position.z - wall.Position.z <= 2f)
                 {
                     var dx = transformAspect.Position.x - wall.Position.x;
                     var dy = transformAspect.Position.z - wall.Position.z;
