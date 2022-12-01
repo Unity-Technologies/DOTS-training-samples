115c959e79ad2fc318a48976c2bb9e763cc24d40
Petter Edblom <petter.edblom@unity3d.com>
2022-12-01 09:50:22 +0100

    Food job

--- Ported/AntGroup2/Assets/Scripts/Systems/FoodSystem.cs
+++ Ported/AntGroup2/Assets/Scripts/Systems/FoodSystem.cs
@@ -24,27 +24,33 @@ partial struct FoodSystem : ISystem
 
     public void OnUpdate(ref SystemState state)
     {
-        //var config = SystemAPI.GetSingleton<Config>();
-
         var colonyLocation = SystemAPI.GetComponent<LocalTransform>(SystemAPI.GetSingletonEntity<Colony>()).Position;
         var foodLocation = SystemAPI.GetComponent<LocalTransform>(SystemAPI.GetSingletonEntity<Food>()).Position;
 
-        //foreach (var foodTransform in SystemAPI.Query<TransformAspect>().WithAll<Food>())
+        var job = new FoodJob { colonyLocation = colonyLocation, foodLocation = foodLocation };
+        job.ScheduleParallel();
+    }
+
+    [BurstCompile]
+    [WithAll(typeof(Ant))]
+    partial struct FoodJob : IJobEntity
+    {
+        public float3 colonyLocation;
+        public float3 foodLocation;
+
+        public void Execute(TransformAspect transform, ref HasResource hasResource, ref URPMaterialPropertyBaseColor color)
         {
-            foreach (var (transform, hasResource, color) in SystemAPI.Query<TransformAspect, RefRW<HasResource>, RefRW<URPMaterialPropertyBaseColor>>().WithAll<Ant>())
+            bool currentlyHasResource = hasResource.Value;
+            float3 targetPosition = currentlyHasResource ? colonyLocation : foodLocation;
+            if (math.distance(transform.LocalPosition, targetPosition) < 1.0f) // TODO Hard coded food radius of 1 m
             {
-                bool currentlyHasResource = hasResource.ValueRO.Value;
-                float3 targetPosition = currentlyHasResource ? colonyLocation : foodLocation;
-                if (math.distance(transform.LocalPosition, targetPosition) < 1.0f) // TODO Hard coded food radius of 1 m
-                {
-                    hasResource.ValueRW.Value = !currentlyHasResource;
-                    hasResource.ValueRW.Trigger = true;
-                    color.ValueRW.Value = currentlyHasResource ? new float4(1.0f, 1.0f, 1.0f, 1.0f) : new float4(0.0f, 1.0f, 1.0f, 1.0f);
-                }
-                else
-                {
-                    hasResource.ValueRW.Trigger = false;
-                }
+                hasResource.Value = !currentlyHasResource;
+                hasResource.Trigger = true;
+                color.Value = currentlyHasResource ? new float4(1.0f, 1.0f, 1.0f, 1.0f) : new float4(0.0f, 1.0f, 1.0f, 1.0f);
+            }
+            else
+            {
+                hasResource.Trigger = false;
             }
         }
     }
