5a4e229006579a74d477b8f41970b40d002fa336
Lina Gudžiūnaitė <ascendingman@gmail.com>
2022-12-01 16:04:33 +0200

    Jobify Resources

--- Ported/CombatBees/Assets/Scripts/Aspects/ResourceAspect.cs
+++ Ported/CombatBees/Assets/Scripts/Aspects/ResourceAspect.cs
@@ -5,16 +5,45 @@ using Unity.Transforms;
 public readonly partial struct ResourceAspect : IAspect
 {
     public readonly Entity Self;
-
-    readonly TransformAspect Transform;
+    public readonly TransformAspect Transform;
     
     readonly RefRW<Resource> resourceComponent;
-    readonly RefRW<ResourceCarried> resourceCarriedState;
-    readonly RefRW<ResourceDropped> resourceDroppedState;
     
     public float3 Position
     {
-        get => Transform.WorldPosition;
-        set => Transform.WorldPosition = value;
+        get => Transform.Position;
+        set => Transform.Position = value;
     }
+    
+    public float3 velocity
+    {
+        get => resourceComponent.ValueRO.velocity;
+        set => resourceComponent.ValueRW.velocity = value;
+    }
+    
+    public bool droppedEnabled
+    {
+        get => resourceComponent.ValueRO.droppedEnabled;
+        set => resourceComponent.ValueRW.droppedEnabled = value;
+    }
+    
+    public bool carriedEnabled
+    {
+        get => resourceComponent.ValueRO.carriedEnabled;
+        set => resourceComponent.ValueRW.carriedEnabled = value;
+    }
+    
+    public float3 ownerVelocity
+    {
+        get => resourceComponent.ValueRO.ownerVelocity;
+        set => resourceComponent.ValueRW.ownerVelocity = value;
+    }
+    
+    public float3 ownerPosition
+    {
+        get => resourceComponent.ValueRO.ownerPosition;
+        set => resourceComponent.ValueRW.ownerPosition = value;
+    }
+    
+    public Entity carriedBee => resourceComponent.ValueRO.ownerBee;
 }
