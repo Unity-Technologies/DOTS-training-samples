196b74555aa639c9141e5b6539a3995808d16b96
Victor Apihtin <victor.apihtin@unity3d.com>
2022-12-01 16:35:49 +0100

    jobified MovementSystem

--- Ported/Metro/Assets/Core/Scripts/Systems/MovementSystem.cs
+++ Ported/Metro/Assets/Core/Scripts/Systems/MovementSystem.cs
@@ -1,4 +1,6 @@
 using Unity.Burst;
+using Unity.Burst.Intrinsics;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -6,9 +8,14 @@ using Unity.Transforms;
 [BurstCompile]
 partial struct MovementSystem : ISystem
 {
+    private EntityQuery myQuery;
+    
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
+        var builder = new EntityQueryBuilder(Allocator.Temp);
+        builder.WithAll<LocalTransform, Speed, TargetPosition, WaypointMovementTag>();
+        myQuery = state.GetEntityQuery(builder);
     }
     
     [BurstCompile]
@@ -19,29 +26,94 @@ partial struct MovementSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (transform, /*position, direction,*/ speed, target, waypointMovement) in 
+        var dt = SystemAPI.Time.DeltaTime;
+        
+        /*
+        foreach (var (transform, speed, target, waypointMovement) in
                  SystemAPI.Query<
                      TransformAspect,
-                     //Direction, 
-                     
-                     //Position,
                      RefRO<Speed>,
                      RefRO<TargetPosition>,
-                     WaypointMovementTag> ())
-        {
-            
-            var dt = SystemAPI.Time.DeltaTime;
+                     WaypointMovementTag>())
+        {       
             var vectorToTarget = target.ValueRO.Value - transform.WorldPosition;
-            
+            var direction = math.normalize(vectorToTarget);
+            var look = new float3(direction.x, 0.0f, direction.y);
+            transform.WorldRotation = quaternion.LookRotation(look, math.up());
+            transform.WorldPosition += direction * speed.ValueRO.Value * dt;
+        }
+        */
+        
+        // entity job version
+        state.Dependency = new MovementEntityJob { DT = dt }.ScheduleParallel(state.Dependency);
+        
+        // chunk job version
+        //state.Dependency = new MovementChunkJob
+        //{
+        //    DT = dt,
+        //    TransformHandle = state.GetComponentTypeHandle<LocalTransform>(),
+        //    SpeedHandle = state.GetComponentTypeHandle<Speed>(true),
+        //    TargetHandle = state.GetComponentTypeHandle<TargetPosition>(true),
+        //}.ScheduleParallel(myQuery, state.Dependency);
+    }
+    
+    [BurstCompile]
+    partial struct MovementEntityJob : IJobEntity
+    {
+        public float DT;
+        
+        [BurstCompile]
+        public void Execute([ChunkIndexInQuery] int chunkIndex, Entity entity, ref TransformAspect transform, in Speed speed, in TargetPosition target)
+        {
+            var vectorToTarget = target.Value - transform.WorldPosition;
+            var direction = math.normalize(vectorToTarget);
+            var look = new float3(direction.x, 0.0f, direction.y);
+            transform.WorldRotation = quaternion.LookRotation(look, math.up());
+            transform.WorldPosition += direction * speed.Value * DT;
+        }
+    }
+    
+    [WithAll(typeof(WaypointMovementTag))]
+    [BurstCompile]
+    public partial struct MovementChunkJob : IJobChunk
+    {
+        public float DT;
+        
+        public ComponentTypeHandle<LocalTransform> TransformHandle;
+        [ReadOnly] public ComponentTypeHandle<Speed> SpeedHandle;
+        [ReadOnly] public ComponentTypeHandle<TargetPosition> TargetHandle;
 
-            //if (math.lengthsq(vectorToTarget) > Utility.kStopDistance)
+        [BurstCompile]
+        public void Execute(in ArchetypeChunk chunk,
+                int unfilteredChunkIndex,
+                bool useEnableMask,
+                in v128 chunkEnabledMask)
+        {
+            var transforms =
+                chunk.GetNativeArray(ref TransformHandle);
+            var speeds =
+                    chunk.GetNativeArray(ref SpeedHandle);
+            var targets =
+                    chunk.GetNativeArray(ref TargetHandle);
+            
+            var enumerator = new ChunkEntityEnumerator(
+                    useEnableMask,
+                    chunkEnabledMask,
+                    chunk.Count);
+            
+            while (enumerator.NextEntityIndex(out var i))
             {
+                var transform = transforms[i];
+                var speed = speeds[i];
+                var target = targets[i];
+                
+                var vectorToTarget = target.Value - transform.Position;
                 var direction = math.normalize(vectorToTarget);
-                
                 var look = new float3(direction.x, 0.0f, direction.y);
-                transform.WorldRotation = quaternion.LookRotation(look, math.up());
+                transform.Rotation = quaternion.LookRotation(look, math.up());
+                transform.Position += direction * speed.Value * DT;
                 
-                transform.WorldPosition += direction * speed.ValueRO.Value * dt;
+                transforms[i] = transform;
             }
         }
     }
