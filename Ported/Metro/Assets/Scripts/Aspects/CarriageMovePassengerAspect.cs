787218fe0cc2c3124a2863f72db3d08a42b5b167
Kirill Titov <kirill.titov@unity3d.com>
2022-12-02 10:29:30 +0100

    Switch carirages to jobs

---
+++ Ported/Metro/Assets/Scripts/Aspects/CarriageMovePassengerAspect.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+
+public readonly partial struct CarriageMovePassengerAspect : IAspect
+{
+    // Aspects can contain other aspects.
+    public readonly RefRO<LocalTransform> Transform;
+
+    // A RefRW field provides read write access to a component. If the aspect is taken as an "in"
+    // parameter, the field will behave as if it was a RefRO and will throw exceptions on write attempts.
+    public readonly RefRO<CarriageSeatsPositions> CarriageSeats;
+
+    public readonly DynamicBuffer<CarriagePassengers> Passengers;
+}
