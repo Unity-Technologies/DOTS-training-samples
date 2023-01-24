2aa7656df3da82396dbf0f0033d78b7b66c5de9c
Dan Wright <dan.wright@unity3d.com>
2023-01-24 13:15:54 -0800

    Scaling the size of straight highway piece based on length

--- Ported/HighwayRacer/Assets/Scripts/Systems/TrackSpawner.cs
+++ Ported/HighwayRacer/Assets/Scripts/Systems/TrackSpawner.cs
@@ -14,29 +14,40 @@ namespace Systems
 
 		public void OnDestroy(ref SystemState state)
 		{
-			
+
 		}
 
 		public void OnUpdate(ref SystemState state)
 		{
 			var config = SystemAPI.GetSingleton<Config>();
-		
+
 			// TODO: actual track authoring and placement. For now, just slap a single straight segment into the scene
-			var track = state.EntityManager.Instantiate(config.StraightTrackPrefab);
-			state.EntityManager.SetComponentData(track,  new Track
-			{
-				Length = 100,
-				SegmentId = 0
-			});
-			state.EntityManager.SetComponentData(track, new LocalTransform
-			{
-				_Position = float3.zero,
-				_Rotation = quaternion.identity,
-				_Scale = 1
-			});
-			
-			
-			state.Enabled = false;
+            var track = InstantiateTrack(ref state, config, 100, 0);
+
+            state.Enabled = false;
 		}
+
+        private Entity InstantiateTrack(ref SystemState state, Config config, float length, int segmentId)
+        {
+            var track = state.EntityManager.Instantiate(config.StraightTrackPrefab);
+            var trackLength = 100;
+
+            state.EntityManager.SetComponentData(track,  new Track
+            {
+                Length = trackLength,
+                SegmentId = 0
+            });
+            state.EntityManager.SetComponentData(track, new LocalTransform
+            {
+                _Position = float3.zero,
+                _Rotation = quaternion.identity,
+                _Scale = 1
+            });
+            state.EntityManager.AddComponentData(track, new PostTransformScale
+            {
+                Value = float3x3.Scale(1, 1, trackLength)
+            });
+            return track;
+        }
 	}
 }
