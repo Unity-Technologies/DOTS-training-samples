e307000e26bda1246b43238fb0f3595488fb05a8
will-mearns <will.mearns@unity3d.com>
2023-01-24 17:02:19 -0600

    Silo spawning Farmers

--- Ported/AutoFarmers/Assets/Scripts/Aspects/SiloAspect.cs
+++ Ported/AutoFarmers/Assets/Scripts/Aspects/SiloAspect.cs
@@ -1,16 +1,27 @@
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Rendering;
+using Unity.Transforms;
 
 // Instead of directly accessing the Silo component, we are creating an aspect.
 // Aspects allows you to provide a customized API for accessing your components.
 readonly partial struct SiloAspect : IAspect
 {
-    readonly RefRO<Config> m_config;
-    readonly RefRO<Silo> m_silo;
+    public readonly RefRW<Silo> m_silo;
+    public readonly TransformAspect Transform;
 
-    public Entity FarmerPrefab => m_config.ValueRO.FarmerPrefab;
-    public Entity DronePrefab => m_config.ValueRO.DronePrefab;
     public Entity FarmerSpawn => m_silo.ValueRO.FarmerSpawn;
     public Entity DroneSpawn => m_silo.ValueRO.DroneSpawn;
+
+    public int Cash
+    {
+        get => m_silo.ValueRW.Cash ;
+        set => m_silo.ValueRW.Cash = value;
+    }
+    public int FarmerCost
+    {
+        get => m_silo.ValueRW.FarmerCost;
+        set => m_silo.ValueRW.FarmerCost = value;
+    }
+
 }
--- Ported/AutoFarmers/Assets/Scripts/Systems/SiloSystem.cs
+++ Ported/AutoFarmers/Assets/Scripts/Systems/SiloSystem.cs
@@ -1,24 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Unity.Entities;
 using Unity.Burst;
 using Unity.Transforms;
-using Unity.Mathematics;
-using Unity.Collections;
-using Unity.Entities.UniversalDelegates;
-using Unity.Rendering;
-using Unity.VisualScripting;
 
 [BurstCompile]
 public partial struct SiloSystem : ISystem
 {
-    ComponentLookup<WorldTransform> m_WorldTransformLookup;
-
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
-        m_WorldTransformLookup = state.GetComponentLookup<WorldTransform>(true);
     }
 
     [BurstCompile]
@@ -30,51 +19,17 @@ public partial struct SiloSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        m_WorldTransformLookup.Update(ref state);
-
-        float radius = 1f;
-        const float debugRenderStepInDegrees = 20;
-        // Debug rendering (the white circle).
-        for (float angle = 0; angle < 360; angle += debugRenderStepInDegrees)
+        var config = SystemAPI.GetSingleton<Config>();
+        foreach (var silo in SystemAPI.Query<SiloAspect>())
         {
-            var a = new float3 (5f,5f,5f);
-            var b = new float3(5f, 5f, 5f);
-            math.sincos(math.radians(angle), out a.x, out a.z);
-            math.sincos(math.radians(angle + debugRenderStepInDegrees), out b.x, out b.z);
-            UnityEngine.Debug.DrawLine(a * radius, b * radius);
-        }
-
-        var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
-        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
-
-        var farmerSpawnJob = new SpawnWorker
-        {
-            WorldTransformLookup = m_WorldTransformLookup,
-            ECB = ecb
-        };
-
-        farmerSpawnJob.Schedule();
-    }
-
-    [BurstCompile]
-    partial struct SpawnWorker : IJobEntity
-    {
-        [ReadOnly] public ComponentLookup<WorldTransform> WorldTransformLookup;
-        public EntityCommandBuffer ECB;
-
-        void Execute(in SiloAspect silo)
-        {
-            var instance = ECB.Instantiate(silo.FarmerPrefab);
-            var spawnLocalToWorld = WorldTransformLookup[silo.FarmerSpawn];
-            var farmerTransform = LocalTransform.FromPosition(spawnLocalToWorld.Position);
-
-            farmerTransform.Scale = WorldTransformLookup[silo.FarmerPrefab].Scale;
-            ECB.SetComponent(instance, farmerTransform);
-            ECB.SetComponent(instance, new Farmer
+            silo.Cash += 1;
+            if (silo.Cash > silo.FarmerCost)
             {
-                moveSpeed = 5.0f,
-                moveTarget = new float3(15, 0, 0)
-            });
+                Entity newFarmer = state.EntityManager.Instantiate(config.FarmerPrefab);
+                //TODO Maybe transition this into LocalTransform
+                state.EntityManager.SetComponentData<WorldTransform>(newFarmer, silo.Transform.WorldTransform);
+                silo.Cash -= silo.FarmerCost;
+            }
         }
     }
 }
