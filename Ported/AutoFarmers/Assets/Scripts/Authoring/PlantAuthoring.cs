c3d53617f092b1dd74e0568beb0d9088abe723be
michaelsmilovitch <michael.smilovitch@unity3d.com>
2023-01-24 15:07:55 -0500

    Added plant authoring to plant prefab
    Also removed unneeded reference to plant prefab on plant itself, should be on plot instead

--- Ported/AutoFarmers/Assets/Scripts/Authoring/PlantAuthoring.cs
+++ Ported/AutoFarmers/Assets/Scripts/Authoring/PlantAuthoring.cs
@@ -2,23 +2,17 @@ using Unity.Entities;
 
 class PlantAuthoring : UnityEngine.MonoBehaviour
 {
-    public UnityEngine.GameObject plantPrefab;
-
     class PlantBaker : Baker<PlantAuthoring>
     {
         public override void Bake(PlantAuthoring authoring)
         {
-            AddComponent(new Plant
-            {
-                plantPrefab = GetEntity(authoring.plantPrefab)
-            });
+            AddComponent<Plant>();
         }
     }
 }
 
 struct Plant : IComponentData
 {
-    public Entity plantPrefab;
     float timePlanted; //to track the growth
     bool isReadyToPick; //true once fully grown
     bool pickedAndHeld; //true once picked by a farmer
