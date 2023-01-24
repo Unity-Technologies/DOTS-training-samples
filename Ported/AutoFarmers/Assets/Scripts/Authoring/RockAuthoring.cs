7dc34f7dde997c0a24c2eec6729f8e986ac8815a
Tibi Paunescu <tibi.paunescu@unity3d.com>
2023-01-24 13:08:20 -0800

    Add health to the rock

--- Ported/AutoFarmers/Assets/Scripts/Authoring/RockAuthoring.cs
+++ Ported/AutoFarmers/Assets/Scripts/Authoring/RockAuthoring.cs
@@ -2,15 +2,17 @@ using Unity.Entities;
 
 class RockAuthoring : UnityEngine.MonoBehaviour
 {
+    public const int MAX_ROCK_HEALTH = 100;
     class RockBaker : Baker<RockAuthoring>
     {
         public override void Bake(RockAuthoring authoring)
         {
-            AddComponent<Rock>();
+            AddComponent(new Rock { RockHealth = MAX_ROCK_HEALTH} );
         }
     }
 }
 
 struct Rock : IComponentData
 {
+    public int RockHealth;
 }
