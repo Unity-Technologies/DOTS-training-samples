ce9f11377b9a0cd538a9b2d5061025f9a7c4088d
michaelsmilovitch <michael.smilovitch@unity3d.com>
2023-01-24 14:51:20 -0500

    Updated PlantAuthoring
    Plant struct has several fields and takes in a prefab now

--- Ported/AutoFarmers/Assets/Scripts/Authoring/PlantAuthoring.cs
+++ Ported/AutoFarmers/Assets/Scripts/Authoring/PlantAuthoring.cs
@@ -2,16 +2,24 @@ using Unity.Entities;
 
 class PlantAuthoring : UnityEngine.MonoBehaviour
 {
+    public UnityEngine.GameObject plantPrefab;
+
     class PlantBaker : Baker<PlantAuthoring>
     {
         public override void Bake(PlantAuthoring authoring)
         {
-            AddComponent<Plant>();
+            AddComponent(new Plant
+            {
+                plantPrefab = GetEntity(authoring.plantPrefab)
+            });
         }
     }
 }
 
 struct Plant : IComponentData
 {
-     
+    public Entity plantPrefab;
+    float timePlanted; //to track the growth
+    bool isReadyToPick; //true once fully grown
+    bool pickedAndHeld; //true once picked by a farmer
 }
