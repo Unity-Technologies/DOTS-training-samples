bc8c0778f58e83b9bda4808292e0b83ce0358c13
Samuel Bellomo <samuel.bellomo@unity3d.com>
2023-01-25 10:29:48 -0500

    fix for temperatures

--- Ported/BucketBrigade/Assets/Scripts/Components/GridTemperatures.cs
+++ Ported/BucketBrigade/Assets/Scripts/Components/GridTemperatures.cs
@@ -1,7 +1,31 @@
+using System;
+using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
 
 public struct GridTemperatures : IComponentData
 {
-    public float[][] temperatures;
+    NativeArray<int> temperatures;
+    int sqrSize;
+    bool isInit;
+
+    public void Init(int sideSize)
+    {
+        temperatures = new NativeArray<int>(sideSize * sideSize, Allocator.Persistent);
+        sqrSize = sideSize;
+        isInit = true;
+    }
+
+    public int Get(int x, int y)
+    {
+        if (!isInit) throw new Exception("not initialized");
+        // 10 x 10: [0,0]==[0]; [5, 0]==[5+0]==[5]; [5, 5]==[5+5*10]==[55]; [9,9]==[9+9*10]==99; [10,9]==[10+9*10]==[100]==error; [9,10]==[9+10*10]==[109]==error
+        return temperatures[x + y * sqrSize];
+    }
+
+    public void Set(int x, int y, int value)
+    {
+        if (!isInit) throw new Exception("not initialized");
+        temperatures[x + y * sqrSize] = value;
+    }
 }
