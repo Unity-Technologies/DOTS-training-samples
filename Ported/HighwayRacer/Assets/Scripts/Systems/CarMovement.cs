63619177c4a426e706b7cd9d8113de589ffe1960
Dan Wright <dan.wright@unity3d.com>
2023-01-25 14:11:04 -0800

    Initial simple lane changes

--- Ported/HighwayRacer/Assets/Scripts/Systems/CarMovement.cs
+++ Ported/HighwayRacer/Assets/Scripts/Systems/CarMovement.cs
@@ -40,19 +40,12 @@ public partial struct CarMovement : ISystem
 
         foreach (var car in SystemAPI.Query<CarAspect>())
         {
-            // if (car.Distance > 100.0)
-            // {
-            //     car.Distance = 0.0f;
-            // }
             //get car in front of us
             Car neighbor = allCars[0];//assume there is a car and that we will have one close to us
             float neighborDelta = 1000.0f;
 
-            int rightCount = 0;
-            int leftCount = 0;
-
-            bool leftLaneOK = true;//set to false if we are at the leftmost lane
-            bool rightLaneOK = true;//set to false if we are at the rightmost lane
+            bool leftLaneOK = car.LaneNumber != config.NumLanes - 1;//set to false if we are at the leftmost lane
+            bool rightLaneOK = car.LaneNumber != 0;//set to false if we are at the rightmost lane
 
             foreach (var other in allCars)
             {
@@ -84,23 +77,19 @@ public partial struct CarMovement : ISystem
                 }
             }
 
-
             //if they are within range
             if(neighborDelta < (config.FollowClearance + (car.Length + neighbor.Length) / 2))
             {
                 //if they are slower than us, decelerate until we match their speed
-                //if
-                // if(leftLaneOK)
-                // {
-                //     //try to change lanes
-                //     int leftLane = car.LaneNumber + 1;
-                // }
-                // else if(rightLaneOK)
-                // {
-                //     //try to change lanes
-                //     int rightLane = car.LaneNumber + 1;
-                // }
-                // else
+                if(leftLaneOK)
+                {
+                    car.LaneNumber++;
+                }
+                else if(rightLaneOK)
+                {
+                    car.LaneNumber--;
+                }
+                else
                 {
                     //slow down
                     car.Speed = math.max(0.0f, car.Speed - car.Acceleration);
@@ -112,16 +101,16 @@ public partial struct CarMovement : ISystem
                 car.Speed = math.min(car.MaxSpeed, car.Speed + car.Acceleration);
             }
 
-            float xPos = -3.0f + 2.0f * car.LaneNumber;
-
-            //car.Position = new float3(xPos, 0.0f, car.Distance);
+            var lane = lanes[car.LaneNumber];
             car.Distance += car.Speed * SystemAPI.Time.DeltaTime;
+            if (car.Distance >= lane.LaneLength)
+            {
+                car.Distance -= lane.LaneLength;
+            }
 
             // TODO: QGU - Fix car transformation
-            //float4x4 carTransform = GetWorldTransformation(car.Distance, lane.LaneLength, lane.LaneRadius, 60.0f);
-            //state.EntityManager.SetComponentData(car.Self, LocalTransform.FromMatrix(carTransform));
-
-            car.Position = new float3(xPos, 0.0f, car.Distance);
+            float4x4 carTransform = GetWorldTransformation(car.Distance, lane.LaneLength, lane.LaneRadius, Config.SegmentLength);
+            state.EntityManager.SetComponentData(car.Self, LocalTransform.FromMatrix(carTransform));
         }
     }
 
@@ -155,29 +144,5 @@ public partial struct CarMovement : ISystem
             translation = math.rotate(rotation, translation);
             return float4x4.TRS(translation, rotation, new float3(1f, 1f, 1f));
         }
-    }
-
-    private int ShouldChangeLanes(CarAspect car, Unity.Collections.NativeArray<Car> allCars)
-    {
-        //look in the left lane
-        //look in the right lane
-
-
-        Car neighbor = allCars[0];//assume there is a car and that we will have one close to us
-        float neighborDelta = 1000.0f;
-        foreach (var other in allCars)
-        {
-            if(other.Index == car.Index) { continue; }
-            if(other.LaneNumber != car.LaneNumber) { continue; }
-
-            float delta = other.Distance - car.Distance;
-            if(delta >= 0.0f && delta < neighborDelta)
-            {
-                neighbor = other;
-                neighborDelta = delta;
-            }
-        }
-
-        return -1;
     }
 }
