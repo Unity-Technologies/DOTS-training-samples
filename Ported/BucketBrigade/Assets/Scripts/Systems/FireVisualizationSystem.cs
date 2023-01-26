9b11016a39776a0f1c699e2502a2f8a1701e1654
jilfranco-unity <jillian.franco@unity3d.com>
2023-01-26 10:39:51 -0500

    committing to pull stuff into my branch

--- Ported/BucketBrigade/Assets/Scripts/Systems/FireVisualizationSystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/Systems/FireVisualizationSystem.cs
@@ -1,11 +1,9 @@
+using Unity.Burst;
 using Unity.Entities;
-using Unity.Mathematics;
-using Unity.Transforms;
 using UnityEngine;
 
-using Random = UnityEngine.Random;
 
-partial class FireVisualizationSystem : SystemBase
+/*partial class FireVisualizationSystem : SystemBase
 {
     
     protected override void OnUpdate()
@@ -31,6 +29,38 @@ partial class FireVisualizationSystem : SystemBase
         //         color = new URPMaterialPropertyBaseColor();
         //     })
         //     .ScheduleParallel();
+    }
+}*/
+
+
+[BurstCompile]
+partial struct FireVisualizationSystem : ISystem
+{
+    
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<Config>();
+        state.RequireForUpdate<FireSimSystem>();
+    }
+
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var speed = 1;
+        var offsetRange = 0.2f;
+        var runningTime = ((float)SystemAPI.Time.ElapsedTime * speed) * offsetRange;
+
+        foreach (var fireCell in SystemAPI.Query<FireCellAspect>().WithAll<OnFireTag>())
+        {
+            var currentHeight = fireCell.DisplayHeight.ValueRO.height;
+            
+        }
     }
 }
 
