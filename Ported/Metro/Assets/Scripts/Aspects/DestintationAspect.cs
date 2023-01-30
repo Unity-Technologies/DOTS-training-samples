2c20fac2254ef4ba13c7ec23e43d126689ee0351
Lewis Hammond <lewis.hammond@unity3d.com>
2023-01-30 17:58:06 +0000

    DestinationAspect

---
+++ Ported/Metro/Assets/Scripts/Aspects/DestintationAspect.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+
+public readonly partial struct DestinationAspect : IAspect
+{
+    private const float defaultEpsilon = 0.1f;
+    public readonly TransformAspect transform;
+    public readonly RefRO<TargetDestination> target;
+
+    public bool IsAtDestination(float epsilon = defaultEpsilon)
+    {
+        return math.distancesq(transform.WorldPosition, target.ValueRO.TargetPosition) < defaultEpsilon;
+    }
+}
