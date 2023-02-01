794a2722ea6b564ac0275ba35b2098f5e35c0ffe
Nathan Adara <nathan.adara@unity3d.com>
2023-02-01 17:21:43 +0100

    CarCollisionSystem Optimisation 1
    - 16.65% (median 39.34 -> 32.79ms) improvement
    - Added bitwise representation logic (to be replaced in the CollisionComponent)
    - Added single-compare early out break
    - Added early out for front case random accesses
    - Conditional non-multiply index-seam-wrapping  evaluation
    
    - Add profile analyzer package
    - edit .gitignore

--- Ported/HighwayRacers/Assets/Scripts/Systems/CarCollisionSystem.cs
+++ Ported/HighwayRacers/Assets/Scripts/Systems/CarCollisionSystem.cs
@@ -127,54 +127,82 @@ partial struct CollisionJob : IJobEntity
     void Execute(ref CarCollision collision, in CarIndex carIndex, in CarPositionInLane positionInLane,
         ref URPMaterialPropertyBaseColor baseColor)
     {
-        collision.Front = false;
+        //TODO reduce amount of unnecessary data in structs
+        
+        //TODO refactor collision to use bitwise flags
+        collision.Front = false; 
         collision.Left = positionInLane.LaneIndex == 0;
         collision.Right = positionInLane.LaneIndex == MaxLaneIndex;
         collision.FrontVelocity = 0.0f;
         collision.FrontDistance = 0.0f;
+        
+        const byte _F_NONE = 0, _F_FRONT = 1, _F_LEFT = 2, _F_RIGHT = 4;
+        byte b_collFlags = 0;
+        b_collFlags |= collision.Left  ? _F_LEFT  : _F_NONE;
+        b_collFlags |= collision.Right ? _F_RIGHT : _F_NONE;
+        
 
 
         int startIndex = -(MaxLaneIndex - 1);
         int endIndex = 1 + 2 * (MaxLaneIndex - 1);
-        //TODO: Define how many lanes and use this to determine how many cars to check
+        
+        /*
+         * TODO Ordering revision
+         * [end:1] then
+         * [-1:start]
+         */
         for (int i = startIndex; i <= endIndex; i++)
         {
+            //full out if found colls on all sides
+            if (b_collFlags == (_F_FRONT | _F_LEFT | _F_RIGHT)) break;
             if (i == 0) continue;
+            
             int wrappedIndex = WrappedIndex(i + carIndex.Index, out int wrapDirection);
 
             var otherCar = CarEntities[wrappedIndex];
             var otherCarPosition = CarPositionInLaneLookup[otherCar.Value];
 
             int otherCarLane = otherCarPosition.LaneIndex;
+            float otherCarPositionValue = otherCarPosition.Position;
+            
+            //Wrap other car pos when appropriate
+            if (wrapDirection != 0) otherCarPositionValue += LaneLength * wrapDirection;
+            
             int lane = positionInLane.LaneIndex;
             
-            //Front:
             bool sameLane = otherCarLane == lane;
 
-            float distance = (otherCarPosition.Position + LaneLength * wrapDirection) - positionInLane.Position;
+            float distance = otherCarPositionValue - positionInLane.Position;
+            
             float absoluteDistance = math.abs(distance);
 
-            if (sameLane && distance > 0 && distance < CarRadius)
-            {
-                collision.Front = true;
-                collision.FrontVelocity = CarVelocityLookup[otherCar.Value].VelY;
-                collision.FrontDistance = CarRadius - distance; //calc distance to perfect following distance
-            }
-            else if (absoluteDistance > CarRadius)
+            if (absoluteDistance > CarRadius) //early out if not within importance radius
             {
                 continue;
+            }
+            
+            if ((b_collFlags & _F_FRONT) == 0 && sameLane && distance > 0) //
+            {
+                b_collFlags |= _F_FRONT;
+                
+                //TODO what if we didn't care about velocity/dist, used a PID or something in CarDrivingSystem
+                collision.FrontVelocity = CarVelocityLookup[otherCar.Value].VelY;
+                collision.FrontDistance = CarRadius - distance; //calc distance to perfect following distance
             }
 
             if (otherCarLane == lane + 1)
             {
-                collision.Right = true;
+                b_collFlags |= _F_RIGHT;
             }
             else if (otherCarLane == lane - 1)
             {
-                collision.Left = true;
+                b_collFlags |= _F_LEFT;
             }
         }
 
+        collision.Front = (b_collFlags & _F_FRONT) != 0;
+        collision.Left = (b_collFlags & _F_LEFT) != 0;
+        collision.Right = (b_collFlags & _F_RIGHT) != 0;
 
         //DEBUG
         if (collision.Front)
