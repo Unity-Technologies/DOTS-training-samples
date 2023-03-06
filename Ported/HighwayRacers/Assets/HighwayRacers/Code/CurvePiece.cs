56dcf54a6155f43d9c7946661b18270acce40539
LaurieCheers <73140792+LaurieCheers-unity@users.noreply.github.com>
2023-03-06 14:37:03 -0800

    Ported copy

---
+++ Ported/HighwayRacers/Assets/HighwayRacers/Code/CurvePiece.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HighwayRacers
+{
+
+    public class CurvePiece : HighwayPiece
+    {
+
+        public override float length(float lane)
+        {
+            return Highway.curvePieceLength(lane);
+        }
+
+    }
+
+}
