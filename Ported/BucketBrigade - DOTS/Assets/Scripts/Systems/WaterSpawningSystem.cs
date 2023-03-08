4b184dae9b722ed9818992ea8a6386659a7afe1e
Elfi Kühndorf <elfi.kuhndorf@unity3d.com>
2023-03-08 16:56:22 +0100

    spawn water

--- Ported/BucketBrigade - DOTS/Assets/Scripts/Systems/WaterSpawningSystem.cs
+++ Ported/BucketBrigade - DOTS/Assets/Scripts/Systems/WaterSpawningSystem.cs
@@ -1,15 +1,40 @@
+using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
+[BurstCompile]
 public partial struct WaterSpawningSystem : ISystem
 {
+    [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<Config>();
     }
 
+    [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        Debug.Log('h');
+        state.Enabled = false;
+        var ecbSingleton = SystemAPI.GetSingleton<BeginInitializationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+        var config = SystemAPI.GetSingleton<Config>();
+
+        {
+            var waterTile = ecb.Instantiate(config.Water);
+            ecb.SetComponent(waterTile, new LocalTransform
+            {
+                Position = new float3
+                {
+                    x = 10 + config.rows,
+                    y = 0,
+                    z = 10 + config.columns
+                },
+                Scale = 10f,
+                Rotation = quaternion.identity
+            });
+            
+        }
     }
 }
