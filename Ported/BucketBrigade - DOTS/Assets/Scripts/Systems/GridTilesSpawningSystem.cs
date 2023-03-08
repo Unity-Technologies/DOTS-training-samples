bd96e789b82da7177eb474598cef5f7fd2b5de8c
Michal Chrobot <michal.chrobot@unity3d.com>
2023-03-08 16:31:14 +0100

    Spawning ground tiles system

---
+++ Ported/BucketBrigade - DOTS/Assets/Scripts/Systems/GridTilesSpawningSystem.cs
@@ -0,0 +1,49 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+[BurstCompile]
+public partial struct GridTilesSpawningSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<Config>();
+    }
+    
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+      
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        state.Enabled = false;
+        var ecbSingleton = SystemAPI.GetSingleton<BeginInitializationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+        var config = SystemAPI.GetSingleton<Config>();
+
+        for (int i = 0; i <= config.columns; i++)
+        {
+            for (int j = 0; j <= config.rows; j++)
+            {
+                var groundTile = ecb.Instantiate(config.Ground);
+                ecb.SetComponent(groundTile, new LocalTransform
+                {
+                    Position = new float3
+                    {
+                        x = i,
+                        y = 0f,
+                        z = j
+                    },
+                    Scale = 1f,
+                    Rotation = quaternion.identity
+                });
+            }
+        }
+    }
+}
