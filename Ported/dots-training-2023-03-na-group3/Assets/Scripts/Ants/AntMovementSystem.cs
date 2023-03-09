1955416f11ba965ebcfab2bcd0ad59a7e7840c1a
geneyanza <116665334+geneyanza@users.noreply.github.com>
2023-03-09 08:20:24 -0800

    modified to add steering

--- Ported/dots-training-2023-03-na-group3/Assets/Scripts/Ants/AntMovementSystem.cs
+++ Ported/dots-training-2023-03-na-group3/Assets/Scripts/Ants/AntMovementSystem.cs
@@ -10,9 +10,16 @@ using Unity.Transforms;
 
 public partial struct AntMovementSystem : ISystem
 {
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<Random>();
+        state.RequireForUpdate<PheromoneData>();
+    }
+
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
+        var pheromones = SystemAPI.GetSingletonBuffer<PheromoneData>();
         RefRW<Random> random = SystemAPI.GetSingletonRW<Random>();
 
         foreach (MoveToPositionAspect moveToPositionAspect in SystemAPI.Query<MoveToPositionAspect>())
@@ -20,4 +27,24 @@ public partial struct AntMovementSystem : ISystem
             moveToPositionAspect.Move(SystemAPI.Time.DeltaTime, random);
         }
     }
+    float PheromoneSteering(float2 antposition,float distance, float antangle, int mapSize, DynamicBuffer<PheromoneData> pheromones) {
+        float output = 0;
+
+        for (int i=-1;i<=1;i+=2) {
+            float angle = antangle + i * math.PI*.25f;
+            float testX = antposition.x + math.cos(angle) * distance;
+            float testY = antposition.y + math.sin(angle) * distance;
+
+            if (testX <0 || testY<0 || testX>=mapSize || testY>=mapSize) {
+
+            } else
+            {
+                int index = (int)testX + (int)testY * mapSize;
+                float value = pheromones[index].value;
+                output += value*i;
+            }
+        }
+        return math.sign(output);
+    }
+    
 }
