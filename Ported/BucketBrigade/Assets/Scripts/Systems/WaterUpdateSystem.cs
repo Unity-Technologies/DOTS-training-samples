9814a0555e6db5fbae6e52c249f0bb6d4e7b37f1
StArr-Unity <stephen.arredondo@unity3d.com>
2023-03-09 12:42:22 -0500

    Water System

---
+++ Ported/BucketBrigade/Assets/Scripts/Systems/WaterUpdateSystem.cs
@@ -0,0 +1,34 @@
+using Components;
+using System;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Systems
+{
+    [UpdateBefore(typeof(TransformSystemGroup))]
+    public partial struct WaterUpdateSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<ConfigAuthoring.Config>();
+            state.RequireForUpdate<WaterAuthoring.Water>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var config = SystemAPI.GetSingleton<ConfigAuthoring.Config>();
+
+            foreach (var (transform, volume) in
+                     SystemAPI.Query<RefRW<LocalTransform>, RefRW<Volume>>()
+                     .WithAll<WaterAuthoring.Water>())
+            {
+                volume.ValueRW.value = math.clamp(volume.ValueRO.value + config.refillRate, 0, config.maxCapacity);
+                transform.ValueRW.Scale = math.lerp(0, 1, volume.ValueRO.value / config.maxCapacity);
+            }
+        }
+    }
+}
