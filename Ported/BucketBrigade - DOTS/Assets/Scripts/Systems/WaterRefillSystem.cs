212eb00f26e0578b8ab7afd33c2fd68d6b79e14b
Michal Chrobot <michal.chrobot@unity3d.com>
2023-03-15 22:13:31 +0100

    WaterRefillSystem implemented

---
+++ Ported/BucketBrigade - DOTS/Assets/Scripts/Systems/WaterRefillSystem.cs
@@ -0,0 +1,43 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+[BurstCompile]
+public partial struct WaterRefillSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<Config>();
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var config = SystemAPI.GetSingleton<Config>();
+        
+        new RefillWaterJob
+        {
+            config = config
+        }.ScheduleParallel();
+    }
+}
+
+[BurstCompile]
+partial struct RefillWaterJob : IJobEntity
+{
+    public Config config;
+
+    [BurstCompile]
+    private void Execute(RefRW<LocalTransform> transform, RefRW<Water> water)
+    {
+        if(water.ValueRO.CurrCapacity<water.ValueRO.MaxCapacity){
+            water.ValueRW.CurrCapacity += config.refillRate;
+            float3 newWaterScale= math.lerp(float3.zero, math.float3(1,0.01f,1), water.ValueRO.CurrCapacity/water.ValueRO.MaxCapacity);
+            transform.ValueRW.Scale = newWaterScale.x;
+        }
+    }
+}
