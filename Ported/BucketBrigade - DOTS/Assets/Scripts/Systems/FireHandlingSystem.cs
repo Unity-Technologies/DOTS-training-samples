ad62eade66875b86a4a486c0270a41306841dc8b
Thanos Soulis <thanos.soulis@unity3d.com>
2023-03-22 10:49:13 +0100

    Making the fire grow task is now finished

--- Ported/BucketBrigade - DOTS/Assets/Scripts/Systems/FireHandlingSystem.cs
+++ Ported/BucketBrigade - DOTS/Assets/Scripts/Systems/FireHandlingSystem.cs
@@ -1,6 +1,10 @@
 using Unity.Entities;
 using Unity.Burst;
 using Unity.Transforms;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Rendering;
+using Unity.Mathematics;
 
 [BurstCompile]
 [UpdateAfter(typeof(GridTilesSpawningSystem))]
@@ -8,33 +12,41 @@ public partial struct FireHandlingSystem : ISystem
 {
     public ComponentLookup<OnFire> m_OnFireActive;
 
-    //[BurstCompile]
+    [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
-        // I should make sure that this runs after every grid has spawned, right ?
-        //state.RequireForUpdate<GridTilesSpawningSystem>();
         state.RequireForUpdate<Config>();
-
-        m_OnFireActive = state.GetComponentLookup<OnFire>();
-        //UnityEngine.Debug.Log("OnCreate");
     }
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        //UnityEngine.Debug.Log("Update");
-
         var config = SystemAPI.GetSingleton<Config>();
 
-        // Updates the Component Lookup with the fresh World state
-        m_OnFireActive.Update(ref state);
+        //Get the ECB
+        var ECB = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged);
+        EntityCommandBuffer.ParallelWriter ecbParallel = ECB.AsParallelWriter();
 
+        // Create and schedule a Job that tests if the GroundTiles should be on fire
         var ignitionTestJob = new IgnitionTestJob
         {
-            OnFireActive = m_OnFireActive,
-            flashpoint = config.flashpoint
-        };
-        ignitionTestJob.ScheduleParallel();
+            //OnFireActive = m_OnFireActive,
+            config = config,
+            ecb = ecbParallel
+
+        }.ScheduleParallel(state.Dependency);
+
+        // Update Colour and Height of the fire based on the GroundTile temperature
+        var onFireTileUpdateJob = new FireUpdateJob
+        {
+            config = config,
+            time = UnityEngine.Time.time,
+            ecb = ecbParallel,
+        }.ScheduleParallel(ignitionTestJob);
+
+        // Making sure that the last scheduled job in our onUpdate,
+        // changes the last Job Dependency of the World state
+        state.Dependency = onFireTileUpdateJob;
 
     }
 }
@@ -42,14 +54,48 @@ public partial struct FireHandlingSystem : ISystem
 [BurstCompile]
 public partial struct IgnitionTestJob : IJobEntity
 {
-    [Unity.Collections.NativeDisableParallelForRestriction] public ComponentLookup<OnFire> OnFireActive;
-    public float flashpoint;
+    [ReadOnly] public Config config;
+    public EntityCommandBuffer.ParallelWriter ecb;
 
-    void Execute(Entity entity, Tile tile)
+    void Execute([EntityIndexInQuery]int index, Entity entity, Tile tile, ref LocalTransform transform)
+    {        
+        var isOnFire = tile.Temperature >= config.flashpoint;
+
+        ecb.SetComponentEnabled<OnFire>(index, entity, isOnFire);
+
+        if (!isOnFire)
+        {
+            ecb.SetComponent(index, entity, new URPMaterialPropertyBaseColor { Value = (UnityEngine.Vector4)config.colour_fireCell_neutral });
+
+            // Resetting ground height after it is not on fire
+            float3 pos = transform.Position;
+            pos.y = -(config.maxFlameHeight * 0.5f);
+            transform.Position = pos;
+        }
+    }
+}
+
+
+[BurstCompile]
+[WithAll(typeof(OnFire))]
+public partial struct FireUpdateJob: IJobEntity
+{
+    [ReadOnly] public Config config;
+    [ReadOnly] public float time;
+    public EntityCommandBuffer.ParallelWriter ecb;
+
+    void Execute([EntityIndexInQuery] int index, Entity entity, Tile tile, ref LocalTransform transform)
     {
-        
-        var isOnFire = tile.Temperature >= flashpoint;
-        //UnityEngine.Debug.LogFormat("Is on fire: {0}", isOnFire);
-        OnFireActive.SetComponentEnabled(entity, isOnFire);
+        // Handle Position
+        float3 pos = transform.Position;
+        pos.y = (-config.maxFlameHeight * 0.5f + (tile.Temperature * config.maxFlameHeight)) - config.flickerRange;
+        pos.y += (config.flickerRange * 0.5f) + UnityEngine.Mathf.PerlinNoise((time - index) * config.flickerRate - tile.Temperature, tile.Temperature) * config.flickerRange;
+        transform.Position = pos;
+
+        // Handle Color
+        UnityEngine.Vector4 groundColor(UnityEngine.Color cool, UnityEngine.Color hot)
+        { return UnityEngine.Color.Lerp(cool, hot, tile.Temperature); }
+
+        ecb.SetComponent(index, entity, new URPMaterialPropertyBaseColor { Value = groundColor(config.colour_fireCell_cool, config.colour_fireCell_hot) });
     }
 }
