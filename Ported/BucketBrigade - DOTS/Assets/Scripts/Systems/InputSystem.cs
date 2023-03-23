eed1646593be42052ee6812a214f2738e002130b
Elfi Kühndorf <elfi.kuhndorf@unity3d.com>
2023-03-23 17:03:02 +0100

    adjustments for input system and spawn location
    works for if camera position is set to 5 15 5 (x,y,z) and rotation to 90, 0, 0

--- Ported/BucketBrigade - DOTS/Assets/Scripts/Systems/InputSystem.cs
+++ Ported/BucketBrigade - DOTS/Assets/Scripts/Systems/InputSystem.cs
@@ -5,50 +5,58 @@ using UnityEngine;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using Unity.Rendering;
+using Unity.Scenes;
+using UnityEngine.SceneManagement;
 
 [UpdateAfter(typeof(GridTilesSpawningSystem))]
 public partial class InputSystem : SystemBase
 {
     public ComponentLookup<OnFire> m_OnFireActive;
     private Camera camera;
+    Vector3 m_DistanceFromCamera;
+    Plane m_Plane;
+    Tile Tile;
     
     protected override void OnCreate()
     {
         RequireForUpdate<Config>();
-      m_OnFireActive = GetComponentLookup<OnFire>();
+        m_OnFireActive = GetComponentLookup<OnFire>();
     }
 
     protected override void OnUpdate()
     {
-      m_OnFireActive.Update(this);
+        m_OnFireActive.Update(this);
         var config = SystemAPI.GetSingleton<Config>();
         var random = SystemAPI.GetSingleton<Random>();
         bool leftClick = Input.GetKeyDown(KeyCode.Mouse0);
+        bool resetClick = Input.GetKeyDown(KeyCode.R);
         camera = Camera.main;
         Ray mouseRay = camera.ScreenPointToRay(Input.mousePosition);
-        //var ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer((new WorldUnmanaged()));
-        
-        Entity tile;
-        var worldPos = ScreenToWorld(new float2(mouseRay.origin.x,mouseRay.origin.z));
-        //Debug.Log(worldPos);
+        var worldPos = ScreenToWorld(new float2(mouseRay.origin.x ,mouseRay.origin.z));
 
         foreach (var (tileTransform,entity) in SystemAPI.Query<RefRW<LocalTransform>>().WithEntityAccess().WithAll<Tile>())
         {
            if (leftClick)
-            {
-                var dist=Vector2.Distance(new Vector2(tileTransform.ValueRW.Position.x, tileTransform.ValueRW.Position.z), new Vector2(worldPos.x, worldPos.y));
-                //Debug.Log(tileTransform.ValueRW.Position);
-                if (dist < 0.5f)
-                {
-                    SystemAPI.SetComponent(entity, new URPMaterialPropertyBaseColor { Value = new float4(random.Value.NextFloat(0.5f,1f),0f,0f,1f) });
-                    var fireMove = tileTransform.ValueRW.Position + new float3(0,   random.Value.NextFloat(1,3)  , 0);
-                    tileTransform.ValueRW.Position = fireMove;
-                    Debug.Log(entity.Index);
-                }
+           {
+               var dist=Vector2.Distance(new Vector2(tileTransform.ValueRW.Position.x, tileTransform.ValueRW.Position.z), new Vector2(worldPos.x, worldPos.y));
+               if (dist < 0.5f*config.cellSize)
+               {
+                   /*Tile.Temperature = 1f;
+                   var isOnFire = Tile.Temperature >= config.flashpoint;
+                   SystemAPI.SetComponentEnabled<OnFire>(entity, isOnFire);*/
+                   SystemAPI.SetComponent(entity, new URPMaterialPropertyBaseColor { Value = (UnityEngine.Vector4)config.colour_fireCell_hot });
+                   var fireMove = tileTransform.ValueRW.Position + new float3(0,   random.Value.NextFloat(1,3)  , 0);
+                   tileTransform.ValueRW.Position = fireMove;
+                   Debug.Log("index:"+ entity.Index);
+               }
                 
-            }
+           }
+           
         }
-        // if position of mouse = within grid, fireTile & fireTile is not on fire then enable onFire and give random temperature
+        if (resetClick)
+        {
+            SceneManager.LoadScene("Scenes/MainScene" + "Scenes/MainScene/Elfi Sub Scene");
+        }
     }
     public float2 ScreenToWorld(float2 screenPos)
     {
@@ -60,4 +68,4 @@ public partial class InputSystem : SystemBase
     }
 }
 
-//4.9f, 5.1f, 0, 10,screenPos.x) for camera position at 5 15 5
+//4.9f, 5.1f, 0, 10, --> for camera position at 5 15 5
