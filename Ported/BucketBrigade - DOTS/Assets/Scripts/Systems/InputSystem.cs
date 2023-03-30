cf32a7d2f5f42878188ecf91471e58f17b7f7985
Elfi Kühndorf <elfi.kuhndorf@unity3d.com>
2023-03-30 11:46:18 +0200

    some cleanup

--- Ported/BucketBrigade - DOTS/Assets/Scripts/Systems/InputSystem.cs
+++ Ported/BucketBrigade - DOTS/Assets/Scripts/Systems/InputSystem.cs
@@ -3,10 +3,7 @@ using Unity.Entities;
 using Unity.Transforms;
 using UnityEngine;
 using Unity.Mathematics;
-using Unity.VisualScripting;
-using Unity.Rendering;
 using Unity.Scenes;
-using UnityEngine.SceneManagement;
 
 [UpdateAfter(typeof(GridTilesSpawningSystem))]
 public partial class InputSystem : SystemBase
@@ -16,23 +13,31 @@ public partial class InputSystem : SystemBase
     Vector3 m_DistanceFromCamera;
     Plane m_Plane;
     Tile Tile;
+    private EntityQuery newRequests;
     
     protected override void OnCreate()
-    {
+    { 
+        newRequests = GetEntityQuery(typeof(SceneLoader));
+        RequireForUpdate<SceneLoader>();
         RequireForUpdate<Config>();
         m_OnFireActive = GetComponentLookup<OnFire>();
     }
 
     protected override void OnUpdate()
     {
+        var requests = newRequests.ToComponentDataArray<SceneLoader>(Allocator.Persistent);
         m_OnFireActive.Update(this);
         var config = SystemAPI.GetSingleton<Config>();
         var random = SystemAPI.GetSingleton<Random>();
+        var sceneLoader = SystemAPI.GetSingleton<SceneLoader>();
         bool leftClick = Input.GetKeyDown(KeyCode.Mouse0);
         bool resetClick = Input.GetKeyDown(KeyCode.R);
         camera = Camera.main;
         Ray mouseRay = camera.ScreenPointToRay(Input.mousePosition);
         var worldPos = ScreenToWorld(new float2(mouseRay.origin.x ,mouseRay.origin.z));
+        var ecbSingleton = SystemAPI.GetSingleton<EndInitializationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(this.EntityManager.WorldUnmanaged);
+
 
         foreach (var (tileTransform,entity) in SystemAPI.Query<RefRW<LocalTransform>>().WithEntityAccess().WithAll<Tile>())
         {
@@ -41,26 +46,26 @@ public partial class InputSystem : SystemBase
                var dist=Vector2.Distance(new Vector2(tileTransform.ValueRW.Position.x, tileTransform.ValueRW.Position.z), new Vector2(worldPos.x, worldPos.y));
                if (dist < 0.5f*config.cellSize)
                {
-                   /*Tile.Temperature = 1f;
-                   var isOnFire = Tile.Temperature >= config.flashpoint;
-                   SystemAPI.SetComponentEnabled<OnFire>(entity, isOnFire);*/
-                   SystemAPI.SetComponent(entity, new URPMaterialPropertyBaseColor { Value = (UnityEngine.Vector4)config.colour_fireCell_hot });
-                   var fireMove = tileTransform.ValueRW.Position + new float3(0,   random.Value.NextFloat(1,3)  , 0);
-                   tileTransform.ValueRW.Position = fireMove;
-                   Debug.Log("index:"+ entity.Index);
+                   ecb.SetComponent(entity, new Tile { Temperature = config.flashpoint});
+                   ecb.SetComponentEnabled<OnFire>(entity,true);
                }
                 
            }
-           
         }
-        if (resetClick)
+        
+        /*if (resetClick)
         {
-            SceneManager.LoadScene("Scenes/MainScene" + "Scenes/MainScene/Elfi Sub Scene");
-        }
+            for (int i = 0; i < requests.Length; i += 1)
+            {
+                SceneSystem.LoadSceneAsync(World.Unmanaged, requests[i].SceneReference, new SceneSystem.LoadParameters());
+            }
+            requests.Dispose();
+            EntityManager.DestroyEntity(newRequests);
+        }*/
     }
     public float2 ScreenToWorld(float2 screenPos)
     {
-        //check if its withing bounds 
+        //check if its within bounds 
         var remapX=math.remap(4.9f, 5.1f, 0, 10,screenPos.x);
         var remapY=math.remap(4.9f, 5.1f, 0, 10,screenPos.y);
         var worldPos = new float2(remapX, remapY);
