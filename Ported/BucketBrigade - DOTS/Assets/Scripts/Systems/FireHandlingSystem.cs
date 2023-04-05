c8f0c3b85529fb238a141b93b38d9e7d44374ab0
Thanos Soulis <thanos.soulis@unity3d.com>
2023-04-05 12:03:35 +0200

    Fixing flickering

--- Ported/BucketBrigade - DOTS/Assets/Scripts/Systems/FireHandlingSystem.cs
+++ Ported/BucketBrigade - DOTS/Assets/Scripts/Systems/FireHandlingSystem.cs
@@ -48,10 +48,8 @@ public partial struct FireHandlingSystem : ISystem
             }
         }
 
-        var firePropagationJob = new FirePropagationJob
-        {
-           
-        }.ScheduleParallel(state.Dependency);
+        //Will be used when/if we convert fire propagation into a Parallel job
+        //var firePropagationJob = new FirePropagationJob{}.ScheduleParallel(state.Dependency);
 
         // Create and schedule a Job that tests if the GroundTiles should be on fire
         var ignitionTestJob = new IgnitionTestJob
@@ -59,7 +57,9 @@ public partial struct FireHandlingSystem : ISystem
             config = config,
             ecb = ecbParallel
 
-        }.ScheduleParallel(firePropagationJob);
+           
+        }.ScheduleParallel(state.Dependency);
+        //}.ScheduleParallel(firePropagationJob);
 
         // Update Colour and Height of the fire based on the GroundTile temperature
         var onFireTileUpdateJob = new FireUpdateJob
@@ -100,7 +100,7 @@ public partial struct IgnitionTestJob : IJobEntity
 
 
 [BurstCompile]
-
+[WithAll(typeof(OnFire))]
 public partial struct FireUpdateJob: IJobEntity
 {
     [ReadOnly] public Config config;
@@ -123,11 +123,11 @@ public partial struct FireUpdateJob: IJobEntity
     }
 }
 
-
+//Template for the FirePropagationJob
 [BurstCompile]
 public partial struct FirePropagationJob: IJobEntity
 {
-    void Execute([ChunkIndexInQuery] int index, Entity entity, Tile tile, LocalTransform transform)
+    void Execute([EntityIndexInChunk] int index, [ChunkIndexInQuery] int chunkIndex, Entity entity, Tile tile, LocalTransform transform)
     {
        
     }
