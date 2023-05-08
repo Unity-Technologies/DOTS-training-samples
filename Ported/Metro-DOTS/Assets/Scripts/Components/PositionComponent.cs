99f4e823fa4fe702965fa8ae95878bca1bfeebb6
David Broomfield <david.broomfield@unity3d.com>
2023-05-08 17:13:11 -0300

    Folder structure and position component

---
+++ Ported/Metro-DOTS/Assets/Scripts/Components/PositionComponent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public struct PositionComponent : IComponentData
+{
+    public Vector3 Position;
+}
