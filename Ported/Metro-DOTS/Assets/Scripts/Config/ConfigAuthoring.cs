967168f5494c18189af678e38ccbf9258b3862ba
jeremy.matthews <jeremy.matthews@unity3d.com>
2023-05-08 18:17:35 -0300

    Initial config scenes

---
+++ Ported/Metro-DOTS/Assets/Scripts/Config/ConfigAuthoring.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+namespace Metro
+{
+    public class ConfigAuthoring : MonoBehaviour
+    {
+        public int NumPassengers = 1000;
+        
+        class Baker : Baker<ConfigAuthoring>
+        {
+            public override void Bake(ConfigAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.None);
+                AddComponent(entity, new Config
+                {
+                    NumPassengers = authoring.NumPassengers
+                });
+            }
+        }
+        
+        public struct Config : IComponentData
+        {
+            public int NumPassengers;
+        }
+    }
+}
--- Ported/Metro-DOTS/Assets/Scripts/Systems/PassengerSystem.cs
+++ Ported/Metro-DOTS/Assets/Scripts/Systems/PassengerSystem.cs
@@ -1,10 +1,18 @@
+using Metro;
 using Unity.Entities;
+using UnityEngine;
 
-public class PassengerSystem : ISystem
+
+public partial struct PassengerSystem : ISystem
 {
     public void OnCreate(ref SystemState state) { }
 
     public void OnDestroy(ref SystemState state) { }
 
-    public void OnUpdate(ref SystemState state) { }
+    public void OnUpdate(ref SystemState state)
+    {
+        ///Debug.Log($"Update Passenger {state.World.Name}");
+        var singleton = SystemAPI.GetSingleton<ConfigAuthoring.Config>();
+        Debug.Log($"NumPassengers: {singleton.NumPassengers}");
+    }
 }
