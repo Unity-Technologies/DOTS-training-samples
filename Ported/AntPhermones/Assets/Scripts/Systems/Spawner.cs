2c209c0b464b4182f071efb7b241de6d0e1abf91
Emily Bouchard <emily.bouchard@unity3d.com>
2023-05-09 16:41:12 -0400

    fixed obstacle placement

--- Ported/AntPhermones/Assets/Scripts/Systems/Spawner.cs
+++ Ported/AntPhermones/Assets/Scripts/Systems/Spawner.cs
@@ -67,7 +67,7 @@ public partial struct Spawner: ISystem
                     var obstacle = state.EntityManager.Instantiate(colony.obstaclePrefab);
 
                     var localTransform = SystemAPI.GetComponentRW<LocalTransform>(obstacle, false);
-                    localTransform.ValueRW.Position = new float3(Mathf.Cos(angle) * ringRadius, Mathf.Sin(angle) * ringRadius, 0);
+                    localTransform.ValueRW.Position = new float3(mapSize * 0.5f + Mathf.Cos(angle) * ringRadius, mapSize * 0.5f + Mathf.Sin(angle) * ringRadius, 0);
                 }
             }
         }
