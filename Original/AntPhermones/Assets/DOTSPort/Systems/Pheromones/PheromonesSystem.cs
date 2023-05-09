6c4b2c64ca1def249f31807f0660003d52817d68
DimaTym <dima.tymofiienko@unity3d.com>
2023-05-09 16:51:59 -0400

    Added global settings

---
+++ Original/AntPhermones/Assets/DOTSPort/Systems/Pheromones/PheromonesSystem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public partial struct PheromonesSystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<AntData>();
+        state.RequireForUpdate<PheromoneBufferElement>();
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        state.Enabled = false; // temporary, until implementation
+    }
+}
+
+
+
