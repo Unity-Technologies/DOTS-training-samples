d621a6a337de52c93dcdd242437d06c680774c95
DimaTym <dima.tymofiienko@unity3d.com>
2023-05-09 13:35:36 -0400

    Added UI, TimeScale controls
    Had to import TextMeshPro

---
+++ Original/AntPhermones/Assets/DOTSPort/OtherScripts/UIDirectory.cs
@@ -0,0 +1,8 @@
+using TMPro;
+using UnityEngine;
+
+public class UIDirectory : MonoBehaviour
+{
+    public Canvas Canvas;
+    public TextMeshProUGUI TextCurrentTimeScale;
+}
---
+++ Original/AntPhermones/Assets/DOTSPort/Systems/UI/UIDirectoryInitSystem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TMPro;
+using Unity.Entities;
+using UnityEngine;
+
+public partial struct UIDirectoryInitSystem : ISystem
+{
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<TimeScaleControls>();
+    }
+    
+    public void OnUpdate(ref SystemState state)
+    {
+        state.Enabled = false;
+
+        var directory = GameObject.FindObjectOfType<UIDirectory>();
+        if (directory == null)
+        {
+            throw new Exception($"GameObject with type {nameof(UIDirectory)} not found.");
+        }
+
+        var directoryManaged = new UIDirectoryManaged();
+        directoryManaged.TextCurrentTimeScale = directory.TextCurrentTimeScale;
+        directoryManaged.Canvas = directory.Canvas;
+
+        var entity = state.EntityManager.CreateEntity();
+        state.EntityManager.AddComponentData(entity, directoryManaged);
+    }
+}
+
+public class UIDirectoryManaged: IComponentData
+{
+    public Canvas Canvas;
+    public TextMeshProUGUI TextCurrentTimeScale;
+    
+    // Every IComponentData class must have a no-arg constructor.
+    public UIDirectoryManaged()
+    {
+    }
+}
