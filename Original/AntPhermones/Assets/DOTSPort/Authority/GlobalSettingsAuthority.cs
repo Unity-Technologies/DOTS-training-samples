0ed1bf852282124194938c0dfee37829bc869a79
Steve Jakab <steve.jakab@unity3d.com>
2023-05-10 15:36:46 -0700

    Add ant goal strength to global settings.

--- Original/AntPhermones/Assets/DOTSPort/Authority/GlobalSettingsAuthority.cs
+++ Original/AntPhermones/Assets/DOTSPort/Authority/GlobalSettingsAuthority.cs
@@ -13,6 +13,7 @@ public class GlobalSettingsAuthority : MonoBehaviour
     public float AntRandomSteering = math.PI / 4;
     public float AntSpeed = 0.2f;
     public float AntAccel = 0.07f;
+    public float AntGoalSteerStrength = 0.04f;
     public float TrailAddSpeed = 0.5f;
     public float TrailDecay = 0.01f;
 
@@ -30,6 +31,7 @@ public class GlobalSettingsAuthority : MonoBehaviour
                 AntRandomSteering = authoring.AntRandomSteering,
                 AntSpeed = authoring.AntSpeed,
                 AntAccel = authoring.AntAccel,
+                AntGoalSteerStrength = authoring.AntGoalSteerStrength,
                 TrailAddSpeed = authoring.TrailAddSpeed,
                 TrailDecay = authoring.TrailDecay
             });
@@ -46,6 +48,7 @@ public struct GlobalSettings: IComponentData
     public float AntRandomSteering;
     public float AntSpeed;
     public float AntAccel;
+    public float AntGoalSteerStrength;
     public float TrailAddSpeed;
     public float TrailDecay;
 }
