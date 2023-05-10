2149fda1b50ac6dcd6cafdca8a934b88e5b087d0
John O'Neill <johnoneill@unity3d.com>
2023-05-10 14:52:57 -0400

    Update FireSystem.cs
    Made it all parallel

--- Ported/BucketBrigade/Assets/Scripts/FireSystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/FireSystem.cs
@@ -5,6 +5,8 @@ using Unity.Collections;
 using Unity.Rendering;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
+using Random = Unity.Mathematics.Random;
 
 public partial struct FireSystem : ISystem {
     [BurstCompile]
@@ -16,18 +18,13 @@ public partial struct FireSystem : ISystem {
     public void OnUpdate(ref SystemState state) {
         var config = SystemAPI.GetSingleton<Grid>();
         var targetQuery = SystemAPI.QueryBuilder().WithAll<Fire>().Build();
-        float4 startingColor = config.StartingGridColor;
-        float4 fullBurnColor = config.FullBurningGridColor;
-
-        foreach (var (meshColor, transform, fire) in SystemAPI.Query<RefRW<URPMaterialPropertyBaseColor>, RefRW<LocalTransform>, RefRO<Fire>>()) {
-            meshColor.ValueRW.Value = math.lerp(startingColor, fullBurnColor, fire.ValueRO.t);
-            transform.ValueRW.Position.y = math.lerp(-2.0f, 2.0f, fire.ValueRO.t);
-        }
 
         var job = new FireJob {
             neighoringFires = targetQuery.ToComponentDataArray<Fire>(state.WorldUpdateAllocator),
             rate = SystemAPI.Time.DeltaTime * config.FireGrowthRate,
-            spreadVal = config.FireSpreadValue
+            spreadVal = config.FireSpreadValue,
+            startingColor = config.StartingGridColor,
+            fullBurnColor = config.FullBurningGridColor,
         };
         state.Dependency = job.ScheduleParallel(state.Dependency);
     }
@@ -37,12 +34,12 @@ public partial struct FireSystem : ISystem {
 [BurstCompile]
 public partial struct FireJob : IJobEntity {
     [ReadOnly] public NativeArray<Fire> neighoringFires;
+    public float4 startingColor;
+    public float4 fullBurnColor;
     public float rate;
     public float spreadVal;
 
-    void Execute([EntityIndexInQuery] int index, ref Fire fire) {
-        //var allFires = chunk.GetNativeArray(ref FireHandle);
-        
+    void Execute([EntityIndexInQuery] int index, ref Fire fire, ref URPMaterialPropertyBaseColor color, ref LocalTransform transform) {
         if (fire.t > 0.0f) {
             fire.t = math.clamp(fire.t + rate, 0.0f, 1.0f);
 		} else {
@@ -53,8 +50,14 @@ public partial struct FireJob : IJobEntity {
             else if (index < neighoringFires.Length - 1 && neighoringFires[index + 1].t > spreadVal)
                 flashpoint = true;
 
-            if (flashpoint)
-                fire.t = math.EPSILON;
+            if (flashpoint) {
+                fire.t = Random.CreateFromIndex((uint)index).NextFloat(math.EPSILON, 0.15f);
+            }
         }
+
+        transform.Position.y = math.lerp(-2.0f, 2.0f, fire.t);
+        color.Value = math.lerp(startingColor, fullBurnColor, fire.t);
+
+        // Over the flashpoint and we add a sine anim
     }
 }
