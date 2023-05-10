7a321a9c934ab1752579716884a69b22d5cc9800
DimaTym <dima.tymofiienko@unity3d.com>
2023-05-10 14:25:26 -0400

    Added Pheromone background texture

---
+++ Original/AntPhermones/Assets/DOTSPort/OtherScripts/PheromoneMap.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PheromoneMap : MonoBehaviour
+{
+    Material m_PheromoneMaterial;
+    Texture2D m_PheromoneMapTexture;
+    
+    public Texture2D PheromoneMapTexture => m_PheromoneMapTexture;
+
+    void Awake()
+    {
+        m_PheromoneMaterial = GetComponent<MeshRenderer>().material;
+    }
+
+    public void CreateTexture(int width, int height)
+    {
+        transform.position = new Vector3(width / 2f, height/ 2f, 0);
+        transform.localScale = new Vector3(width, height, 1);
+        m_PheromoneMapTexture = new Texture2D(width, height);
+        var pixels = m_PheromoneMapTexture.GetPixels();
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            pixels[i] = Color.clear;
+        }
+        m_PheromoneMapTexture.SetPixels(pixels);
+        m_PheromoneMapTexture.Apply();
+        m_PheromoneMaterial.SetTexture("_BaseMap", m_PheromoneMapTexture);
+    }
+}
--- Original/AntPhermones/Assets/DOTSPort/Systems/Pheromones/PheromonesPresentationSystem.cs
+++ Original/AntPhermones/Assets/DOTSPort/Systems/Pheromones/PheromonesPresentationSystem.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
-using Unity.Mathematics;
-using Unity.Transforms;
 using UnityEngine;
 
 public partial struct PheromonesPresentationInitSystem : ISystem
@@ -10,49 +8,64 @@ public partial struct PheromonesPresentationInitSystem : ISystem
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<GlobalSettings>();
-        state.RequireForUpdate<PheromonesPresentationTexture>();
     }
 
     public void OnUpdate(ref SystemState state)
     {
         state.Enabled = false;
 
-        var settingsSingleton = SystemAPI.GetSingleton<GlobalSettings>();
-        var presentationEntity = SystemAPI.GetSingletonEntity<PheromonesPresentationTexture>();
-        SystemAPI.SetComponent(presentationEntity, new LocalToWorld() { Value = float4x4.TRS(
-            translation: new float3(0f, 0f, 0f),
-            rotation: quaternion.identity,
-            scale: new float3(settingsSingleton.MapSizeX, settingsSingleton.MapSizeY, 1)
-        )});
+        var globalSettings = SystemAPI.GetSingleton<GlobalSettings>();
+
+        var map = MonoBehaviour.FindObjectOfType<PheromoneMap>();
         
-        // TODO: Why code bellow doesn't work?
- //       var localToWorld = SystemAPI.GetComponent<LocalToWorld>(presentationEntity);
- //       localToWorld.Value = float4x4.TRS(
- //           translation: new float3(0f, 0f, 0f),
- //           rotation: quaternion.identity,
- //           scale: new float3(settingsSingleton.MapSizeX, settingsSingleton.MapSizeY, 1)
- //       );
-       // TODO: How to get particular mesh on particular Entity?
-       
-       // TODO: Regenerate mesh quad
+        map.CreateTexture(globalSettings.MapSizeX, globalSettings.MapSizeY);
+
+        var entity = state.EntityManager.CreateEntity();
+        state.EntityManager.AddComponentData(entity, new PheromoneTextureManaged()
+        {
+            texture = map.PheromoneMapTexture
+        });
+
+#if UNITY_EDITOR
+        state.EntityManager.SetName(entity, $"__Pheromone Texture Reference");
+#endif
     }
 }
 
-//[UpdateInGroup(typeof(PresentationSystemGroup))]
+[UpdateInGroup(typeof(PresentationSystemGroup))]
 public partial struct PheromonesPresentationSystem : ISystem
 {
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<GlobalSettings>();
-        state.RequireForUpdate<PheromonesPresentationTexture>();
         state.RequireForUpdate<PheromoneBufferElement>();
     }
 
     public void OnUpdate(ref SystemState state)
     {
         var pheromoneBufferElement = SystemAPI.GetSingletonBuffer<PheromoneBufferElement>();
+        var PheromoneTexture = SystemAPI.ManagedAPI.GetSingleton<PheromoneTextureManaged>();
         
-        // TODO: how to get texture from Entity?
+        var colorArray = PheromoneTexture.texture.GetPixels();
+        
+        for (int i = 0; i < colorArray.Length; i++)
+        {
+            colorArray[i].r = pheromoneBufferElement[i];
+        }
+        
+        PheromoneTexture.texture.SetPixels(colorArray);
+        PheromoneTexture.texture.Apply();
+    }
+}
+
+
+public class PheromoneTextureManaged: IComponentData
+{
+    public Texture2D texture;
+
+    // Every IComponentData class must have a no-arg constructor.
+    public PheromoneTextureManaged()
+    {
     }
 }
 
