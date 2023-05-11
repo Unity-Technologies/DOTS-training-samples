18f2902be3e0b7024fd76d0bd005b3df798ab992
John O'Neill <johnoneill@unity3d.com>
2023-05-11 09:33:04 -0400

    Update to ensure Grid and OmnibotSpawner are present before Grid initializes

--- Ported/BucketBrigade/Assets/Scripts/OmnibotSpawnSystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/OmnibotSpawnSystem.cs
@@ -11,8 +11,8 @@ public partial struct OmnibotSpawnerSystem : ISystem
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
-        // state.RequireForUpdate<OmnibotSpawner>();
-        // state.RequireForUpdate<Grid>();
+        state.RequireForUpdate<OmnibotSpawner>();
+        state.RequireForUpdate<Grid>();
     }
 
     [BurstCompile]
