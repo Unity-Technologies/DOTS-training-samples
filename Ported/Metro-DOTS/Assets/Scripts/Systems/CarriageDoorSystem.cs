27e8139f4d75a8573bd62df2671fefe5bccc0401
Amelia Mesdag <Amelia.Mesdag@unity3d.com>
2023-05-11 18:55:21 -0300

    Use the Jobs system to open and close train doors  - Added two separate systems to handle the open and close jobs  - Split opening and closing into IJobEntity struct

--- Ported/Metro-DOTS/Assets/Scripts/Systems/CarriageDoorSystem.cs
+++ Ported/Metro-DOTS/Assets/Scripts/Systems/CarriageDoorSystem.cs
@@ -4,57 +4,92 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 
-public partial struct DoorSystem : ISystem
+public partial struct OpenDoorSystem : ISystem
 {
-    [BurstCompile]
-    public void OnCreate(ref SystemState state)
-    {
-        //transformTypeHandle = state.GetComponentTypeHandle<LocalTransform>();
-        // This makes the system not update unless at least one entity exists that has the Door component.
-        state.RequireForUpdate<Door>();
-    }
-
-    [BurstCompile]
-    public void OnDestroy(ref SystemState state) { }
-
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        state.GetComponentLookup<Parent>(true);
+        // Get an EntityCommandBuffer from the BeginSimulationEntityCommandBufferSystem.
+        var ecbSingleton = SystemAPI.GetSingleton<
+            BeginSimulationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
 
-        var em = state.EntityManager;
-
-        foreach (var (transform, door, entity) in
-                 SystemAPI.Query<RefRW<LocalTransform>, RefRW<Door>>()
-                     .WithEntityAccess())
+        // Create the job.
+        var openJob = new OpenDoorJob
         {
-            if (em.IsComponentEnabled<UnloadingComponent>(entity))
-            {
-                door.ValueRW.Timer += SystemAPI.Time.DeltaTime;
+            DeltaTime = SystemAPI.Time.DeltaTime,
+            Ecb = ecb.AsParallelWriter()
+        };
+        var openJobHandle = openJob.Schedule(state.Dependency);
 
-                var lerp = math.min(1f, door.ValueRW.Timer / Door.OpeningTime);
-                transform.ValueRW.Position = math.lerp(door.ValueRO.ClosedPosition, door.ValueRO.OpenPosition, lerp);
+        // Schedule the job. Source generation creates and passes the query implicitly.
+        state.Dependency = openJobHandle;
+    }
+}
 
-                if (lerp >= 1f)
-                {
-                    door.ValueRW.Timer = 0f;
-                    em.SetComponentEnabled<UnloadingComponent>(entity, false);
-                }
-            }
+public partial struct CloseDoorSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        // Get an EntityCommandBuffer from the BeginSimulationEntityCommandBufferSystem.
+        var ecbSingleton = SystemAPI.GetSingleton<
+            BeginSimulationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+        
+        var closeJob = new CloseDoorJob
+        {
+            DeltaTime = SystemAPI.Time.DeltaTime,
+            Ecb = ecb.AsParallelWriter()
+        };
+        var closeJobHandle = closeJob.ScheduleParallel(state.Dependency);
+        
+        state.Dependency = closeJobHandle;
+    }
+}
 
-            if (em.IsComponentEnabled<DepartingComponent>(entity))
-            {
-                door.ValueRW.Timer += SystemAPI.Time.DeltaTime;
+[WithAll(typeof(Door), typeof(UnloadingComponent), typeof(LocalTransform))]
+[BurstCompile]
+public partial struct OpenDoorJob : IJobEntity
+{
+    public float DeltaTime;
+    public EntityCommandBuffer.ParallelWriter Ecb;
 
-                var lerp = math.min(1f, door.ValueRW.Timer / Door.OpeningTime);
-                transform.ValueRW.Position = math.lerp(door.ValueRO.OpenPosition, door.ValueRO.ClosedPosition, lerp);
+    [BurstCompile]
+    public void Execute([ChunkIndexInQuery] int chunkIndex, Entity entity, ref Door door, ref LocalTransform transform)
+    {
+        door.Timer += DeltaTime;
 
-                if (lerp >= 1f)
-                {
-                    door.ValueRW.Timer = 0f;
-                    em.SetComponentEnabled<DepartingComponent>(entity, false);
-                }
-            }
+        var lerp = math.min(1f, door.Timer / Door.OpeningTime);
+        transform.Position = math.lerp(door.ClosedPosition, door.OpenPosition, lerp);
+
+        if (lerp >= 1f)
+        {
+            door.Timer = 0f;
+            Ecb.SetComponentEnabled<UnloadingComponent>(chunkIndex, entity, false);
+        }
+    }
+}
+
+[WithAll(typeof(Door), typeof(DepartingComponent), typeof(LocalTransform))]
+[BurstCompile]
+public partial struct CloseDoorJob : IJobEntity
+{
+    public float DeltaTime;
+    public EntityCommandBuffer.ParallelWriter Ecb;
+
+    [BurstCompile]
+    public void Execute([ChunkIndexInQuery] int chunkIndex, Entity entity, ref Door door, ref LocalTransform transform)
+    {
+        door.Timer += DeltaTime;
+
+        var lerp = math.min(1f, door.Timer / Door.OpeningTime);
+        transform.Position = math.lerp(door.OpenPosition, door.ClosedPosition, lerp);
+
+        if (lerp >= 1f)
+        {
+            door.Timer = 0f;
+            Ecb.SetComponentEnabled<DepartingComponent>(chunkIndex, entity, false);
         }
     }
 }
