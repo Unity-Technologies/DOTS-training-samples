0ce8a46e67714dec375700fe1cf1aec95fe49513
Amelia Mesdag <Amelia.Mesdag@unity3d.com>
2023-05-11 12:50:32 -0300

    Add StationIDComponent

---
+++ Ported/Metro-DOTS/Assets/Scripts/Components/StationIDComponent.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+public struct StationIDComponent : IComponentData
+{
+    public int StationID;
+}
+
+public struct StationQueuesElement : IBufferElementData
+{
+    public Entity Queue;
+}
