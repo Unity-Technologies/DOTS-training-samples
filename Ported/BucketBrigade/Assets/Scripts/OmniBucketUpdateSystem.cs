e1f2327064739e9cbc73a03fad51c1dacc90b42d
Jay Edry <janimator0@gmail.com>
2023-05-12 10:40:23 -0400

    omnibucketupdate

--- Ported/BucketBrigade/Assets/Scripts/OmniBucketUpdateSystem.cs
+++ Ported/BucketBrigade/Assets/Scripts/OmniBucketUpdateSystem.cs
@@ -19,6 +19,7 @@ public partial struct OmniBucketSystem : ISystem
     {
         var omnibotLookup = SystemAPI.GetComponentLookup<Omnibot>();
         var localTransformLookup = SystemAPI.GetComponentLookup<LocalTransform>();
+        var config = SystemAPI.GetSingleton<Grid>();
 
         foreach (var (bucket, localTransform, pTMatrix) in SystemAPI
                      .Query<RefRW<OmniBucket>, RefRW<LocalTransform>, RefRW<PostTransformMatrix>>())
@@ -27,9 +28,17 @@ public partial struct OmniBucketSystem : ISystem
             var omnibotPosition = localTransformLookup[bucket.ValueRO.TargetOmniBotEntity].Position;
 
             // Assign OMniBot to Bucket
-            localTransform.ValueRW.Position = omnibotPosition + new float3(0,1,0);
-            pTMatrix.ValueRW.Value = float4x4.Scale(new float3(
-                1, omnibot.CurrentWaterCarryingVolume / omnibot.MaxWaterCapacity, 1));
+            localTransform.ValueRW.Position = omnibotPosition + new float3(0, config.BotScale / 2, 0);
+
+            if (omnibot.CurrentWaterCarryingVolume < math.EPSILON)
+            {
+                pTMatrix.ValueRW.Value = float4x4.Scale(new float3(0, 0, 0));
+            }
+            else
+            {
+                pTMatrix.ValueRW.Value = float4x4.Scale(new float3(
+                    1, omnibot.CurrentWaterCarryingVolume / omnibot.MaxWaterCapacity, 1));
+            }
         }
     }
 }
