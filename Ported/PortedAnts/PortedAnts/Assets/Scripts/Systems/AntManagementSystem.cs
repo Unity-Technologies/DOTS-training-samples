b00740426aa0103f582a1ca36df02bd1d42eda8d
Fedy Abi Chahla <fedy.abichahla@unity3d.com>
2023-05-24 10:05:19 +0200

    obstacle collision detection

--- Ported/PortedAnts/PortedAnts/Assets/Scripts/Systems/AntManagementSystem.cs
+++ Ported/PortedAnts/PortedAnts/Assets/Scripts/Systems/AntManagementSystem.cs
@@ -43,27 +43,29 @@ public partial struct AntsManagementSystem : ISystem
 
             var facingAngle = ant.ValueRO.facingAngle;
 
-            facingAngle += random.NextFloat(-config.RandomSteering, config.RandomSteering) * SystemAPI.Time.DeltaTime; //steering
+            facingAngle += random.NextFloat(-config.RandomSteering, config.RandomSteering);/// * SystemAPI.Time.DeltaTime; //steering
 
 
-            facingAngle = toTargetAngle;
+            //facingAngle = toTargetAngle;
 
             if (distanceToTarget < config.ObstacleRadius)
             {
                 ant.ValueRW.hasFood = !ant.ValueRO.hasFood;
-                color.ValueRW.Value = ant.ValueRO.hasFood ? config.AntHasFoodColor : config.AntHasNoFoodColor;
+                //color.ValueRW.Value = ant.ValueRO.hasFood ? config.AntHasFoodColor : config.AntHasNoFoodColor;
             }
 
             var speed = ant.ValueRO.speed * SystemAPI.Time.DeltaTime;
 
             float vx = math.cos(facingAngle) * speed;
             float vy = math.sin(facingAngle) * speed;
+            float ovx = vx;
+			float ovy = vy;
 
             
             if (position.x + vx < 0f || position.x + vx > config.MapSize)
             {
                 vx = -vx;
-                facingAngle += math.PI; //this should model reflection, not inversion (as it is here)
+                //facingAngle += math.PI; //this should model reflection, not inversion (as it is here)
             }
 
             position.x += vx;
@@ -71,10 +73,34 @@ public partial struct AntsManagementSystem : ISystem
             if (position.y + vy < 0f || position.y + vy > config.MapSize) // since the direction of vy is reversed, this logic needs revisited
             {
                 vy = -vy;
-                facingAngle += math.PI; //this should model reflection, not inversion (as it is here)
+                //facingAngle += math.PI; //this should model reflection, not inversion (as it is here)
             }
 
             position.y += vy; //this used to be += vy
+
+			float dx, dy, dist;
+
+            foreach (var obstacle in SystemAPI.Query<RefRO<Obstacle>>())
+            {
+			//for (int j=0;j<nearbyObstacles.Length;j++) {
+				dx = position.x - obstacle.ValueRO.position.x;
+				dy = position.y - obstacle.ValueRO.position.y;
+				float sqrDist = dx * dx + dy * dy;
+				if (sqrDist<config.ObstacleRadius*config.ObstacleRadius) {
+					dist = math.sqrt(sqrDist);
+					dx /= dist;
+					dy /= dist;
+					position.x = obstacle.ValueRO.position.x + dx * config.ObstacleRadius;
+					position.y = obstacle.ValueRO.position.y + dy * config.ObstacleRadius;
+
+					vx -= dx * (dx * vx + dy * vy) * 1.5f;
+					vy -= dy * (dx * vx + dy * vy) * 1.5f;
+				}
+			}
+
+			if (ovx != vx || ovy != vy) {
+				facingAngle = math.atan2(vy,vx);
+			}
 
             //I'm not sure that this is the most efficient way, but it intuitively feels like it to me
             ant.ValueRW.facingAngle = facingAngle;
