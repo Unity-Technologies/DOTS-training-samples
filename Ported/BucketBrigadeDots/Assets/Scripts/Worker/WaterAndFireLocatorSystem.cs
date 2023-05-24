89eefcb69fb4cbf45429b4647229a7ab0c3d2db2
Ted Wikman <ted.wikman@unity3d.com>
2023-05-24 09:21:05 +0200

    Fire/Water locator now using real water positions

--- Ported/BucketBrigadeDots/Assets/Scripts/Worker/WaterAndFireLocatorSystem.cs
+++ Ported/BucketBrigadeDots/Assets/Scripts/Worker/WaterAndFireLocatorSystem.cs
@@ -1,30 +1,43 @@
+using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 
 public partial struct WaterAndFireLocatorSystem : ISystem
 {
     const float k_DefaultGridSize = 0.3f;
     
+    [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<GameSettings>();
     }
     
+    [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
         if (UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.Space))
         {
             var gameSetting = SystemAPI.GetSingleton<GameSettings>();
-            
+
             var random = Random.CreateFromIndex((uint)SystemAPI.Time.ElapsedTime);
             foreach (var (teamData, teamState) in SystemAPI.Query<
                          RefRW<TeamData>, 
                          RefRW<TeamState>>())
             {
                 teamData.ValueRW.FirePosition = random.NextFloat2(float2.zero, gameSetting.RowsAndColumns * k_DefaultGridSize);
-                teamData.ValueRW.WaterPosition = random.NextFloat2(float2.zero, gameSetting.RowsAndColumns * k_DefaultGridSize);
+                teamData.ValueRW.WaterPosition = GetRandomWaterPosition(ref state, random);
                 teamState.ValueRW.Value = TeamStates.Idle;
             }
         }
+    }
+
+    float2 GetRandomWaterPosition(ref SystemState state, Random random)
+    {
+        var query = SystemAPI.QueryBuilder().WithAll<WaterCell, LocalToWorld>().Build();
+        var transforms = query.ToComponentDataArray<LocalToWorld>(Allocator.Temp);
+        var randomIndex = random.NextInt(0, transforms.Length);
+        return transforms[randomIndex].Position.xz;
     }
 }
